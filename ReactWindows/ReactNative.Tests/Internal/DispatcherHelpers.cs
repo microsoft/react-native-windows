629f4bbe6b4c149aab8c1ac4c2f94010a24f19c7
Eric Rozell <erozell@outlook.com>
2015-12-22 17:23:31 -0500

    Fixes #20 - adds unit tests for CatalystInstance
    Currently requires InternalsVisibleTo, we should go back and make this public and add doc comments (see #21).

--- ReactWindows/ReactNative.Tests/Internal/DispatcherHelpers.cs
+++ ReactWindows/ReactNative.Tests/Internal/DispatcherHelpers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Windows.System.Threading;
 using Windows.UI.Core;
 
 namespace ReactNative.Tests
@@ -9,6 +10,21 @@ namespace ReactNative.Tests
         public static async Task RunOnDispatcherAsync(Action action)
         {
             await App.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, new DispatchedHandler(action));
+        }
+
+        public static async Task<T> CallOnDispatcherAsync<T>(Func<T> func)
+        {
+            var tcs = new TaskCompletionSource<T>();
+
+            await RunOnDispatcherAsync(() =>
+            {
+                var result = func();
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                ThreadPool.RunAsync(_ => tcs.SetResult(result));
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            });
+
+            return await tcs.Task;
         }
     }
 }
