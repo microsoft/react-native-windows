c3e61c961f7d06cbab17839ba399b21df7666e99
Erik Schlegel <erik.schlegel@gmail.com>
2016-01-20 18:29:24 -0500

    Rebasing

---
+++ ReactWindows/ReactNative/Views/Image/ReactImage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageControl = Windows.UI.Xaml.Controls.Image;
+
+namespace ReactNative.Views.Image
+{
+    public class ReactImage : ImageControl
+    {
+
+    }
+}
