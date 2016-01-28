9410220272ba7a6e5392083e1ec645f52f4d2712
Erik Schlegel <erik.schlegel@gmail.com>
2016-01-28 06:59:08 -0500

    Rebasing

--- windows-packager/index.ios.js
+++ windows-packager/index.ios.js
@@ -1,13 +1,23 @@
 'use strict';
 
 var React = require('react-native');
-var {AppRegistry, View, Text} = React;
+var {AppRegistry, View, requireNativeComponent, Text, TextInput } = React;
+var TextInput = requireNativeComponent('TextInput', null);
 
     var ReactRoot = React.createClass({
+		
+	getInitialState: function(){
+		var text = "You can see me!";
+		return {value: text};
+	},
+
     render: function() {
         return (
-              <View elevation="1.0">
-	               <Text>Hello React Native, I'm running on Windows Phone</Text>
+              <View>
+		         <Text>Hello!</Text>
+				{(this.state && this.state.value) 
+			     ? <TextInput text={this.state.value}></TextInput> 
+				 : undefined}
               </View>
         );
     }
