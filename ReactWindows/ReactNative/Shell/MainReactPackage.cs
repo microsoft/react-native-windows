ed5dc34a67c4cede4b996a3deec179ba7a723b58
Eric Rozell <erozell@outlook.com>
2016-03-25 14:53:03 -0400

    feat(LauncherWindows): Adds LauncherWindows for Linking implementation
    React Native has a module, Linking, that checks for launchability of URIs and launches them accordingly. This changeset adds LauncherWindows, which is analagous to LinkingManagerIOS and IntentAndroid.
    
    We do not yet support the getInitialURL API, but have an open ticket to introduce this feature (#301).
    
    Fixes #299

--- ReactWindows/ReactNative/Shell/MainReactPackage.cs
+++ ReactWindows/ReactNative/Shell/MainReactPackage.cs
@@ -2,9 +2,10 @@
 using ReactNative.Modules.AppState;
 using ReactNative.Modules.Clipboard;
 using ReactNative.Modules.Core;
-using ReactNative.Modules.StatusBar;
+using ReactNative.Modules.Launch;
 using ReactNative.Modules.NetInfo;
 using ReactNative.Modules.Network;
+using ReactNative.Modules.StatusBar;
 using ReactNative.Modules.Storage;
 using ReactNative.Modules.Toast;
 using ReactNative.Modules.WebSocket;
@@ -42,6 +43,7 @@ namespace ReactNative.Shell
                 //new CameraRollManager(reactContext),
                 new ClipboardModule(),
                 //new DialogModule(reactContext),
+                new LauncherModule(),
                 //new LocationModule(reactContext),
                 new NetworkingModule(reactContext),
                 new NetInfoModule(reactContext),
