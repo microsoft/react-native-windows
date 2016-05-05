microsoft/react-native-windows

commit 0f708017cb7d2e21d56f2cdbf4f3edba4ddd1b60

feat(ElementInspector): Adds Element Inspector dev tooling to ReactWindows
Element Inspector is useful for tracing which component is responsible for a particular UI element. Element Inspector uses the UIManager API 'findSubviewIn', which also needed to be wired up and implemented. Also adding some infrastructure in preparation for HMR and FPS debugging.

Fixes #214 - Implement UIManagerModule.findSubviewIn
Fixes #355 - Add element inspector dev tooling

ReactWindows/ReactNative/DevSupport/DevInternalSettings.cs
@@ -1,13 +1,27 @@
-﻿using Windows.Storage;
+﻿using ReactNative.Modules.DevSupport;
+using System.Collections.Generic;
+using Windows.Storage;
 
 namespace ReactNative.DevSupport
 {
-    class DevInternalSettings
+    class DevInternalSettings : IDeveloperSettings
     {
-        private const string JSDevModeDebugKey = "js_dev_mode_debug";
+        private const string FpsDebugKey = "fps_debug";
         private const string DebugServerHostKey = "debug_http_host";
+        private const string JsDevModeDebugKey = "js_dev_mode_debug";
+        private const string AnimationsDebugKey = "animations_debug";
+        private const string JsMinifyDebugKey = "js_minify_debug";
+        private const string ElementInspectorKey = "inspector_debug";
         private const string ReloadOnJSChangeKey = "reload_on_js_change";
         private const string HotModuleReplacementKey = "hot_module_replacement";
+
+        private static readonly HashSet<string> s_triggerReload = new HashSet<string>
+        {
+            FpsDebugKey,
+            ReloadOnJSChangeKey,
+            JsDevModeDebugKey,
+            JsMinifyDebugKey,
+        };
 
         private readonly IDevSupportManager _debugManager;
 
@@ -21,14 +35,14 @@ namespace ReactNative.DevSupport
             get
             {
 #if DEBUG
-                return GetSetting(JSDevModeDebugKey, true);
+                return GetSetting(JsDevModeDebugKey, true);
 #else
                 return GetSetting(JSDevModeDebugKey, false);
 #endif
             }
             set
             {
-                SetSetting(JSDevModeDebugKey, value);
+                SetSetting(JsDevModeDebugKey, value);
             }
         }
 
@@ -44,6 +58,42 @@ namespace ReactNative.DevSupport
             }
         }
 
+        public bool IsAnimationFpsDebugEnabled
+        {
+            get
+            {
+                return GetSetting(ReloadOnJSChangeKey, false);
+            }
+            set
+            {
+                SetSetting(ReloadOnJSChangeKey, value);
+            }
+        }
+
+        public bool IsElementInspectorEnabled
+        {
+            get
+            {
+                return GetSetting(ElementInspectorKey, false);
+            }
+            set
+            {
+                SetSetting(ElementInspectorKey, value);
+            }
+        }
+
+        public bool IsFpsDebugEnabled
+        {
+            get
+            {
+                return GetSetting(FpsDebugKey, false);
+            }
+            set
+            {
+                SetSetting(FpsDebugKey, value);
+            }
+        }
+
         public bool IsHotModuleReplacementEnabled
         {
             get
@@ -53,6 +103,18 @@ namespace ReactNative.DevSupport
             set
             {
                 SetSetting(HotModuleReplacementKey, value);
+            }
+        }
+
+        public bool IsJavaScriptMinifyEnabled
+        {
+            get
+            {
+                return GetSetting(JsMinifyDebugKey, false);
+            }
+            set
+            {
+                SetSetting(JsMinifyDebugKey, value);
             }
         }
 
@@ -87,7 +149,11 @@ namespace ReactNative.DevSupport
         {
             var values = ApplicationData.Current.LocalSettings.Values;
             values[key] = value;
-            _debugManager.ReloadSettings();
+
+            if (s_triggerReload.Contains(key))
+            {
+                _debugManager.ReloadSettings();
+            }
         }
     }
 }

ReactWindows/ReactNative/CoreModulesPackage.cs
@@ -1,5 +1,7 @@
 ﻿using ReactNative.Bridge;
+using ReactNative.DevSupport;
 using ReactNative.Modules.Core;
+using ReactNative.Modules.DevSupport;
 using ReactNative.Tracing;
 using ReactNative.UIManager;
 using ReactNative.UIManager.Events;
@@ -71,6 +73,7 @@ namespace ReactNative
                 typeof(RCTNativeAppEventEmitter),
                 typeof(AppRegistry),
                 // TODO: some tracing module
+                typeof(HMRClient),
                 //typeof(RCTDebugComponentOwnership),
             };
         }

ReactWindows/ReactNative/DevSupport/DevSupportManager.cs
@@ -2,6 +2,7 @@
 using ReactNative.Bridge;
 using ReactNative.Common;
 using ReactNative.Modules.Core;
+using ReactNative.Modules.DevSupport;
 using ReactNative.Tracing;
 using System;
 using System.IO;
@@ -32,10 +33,11 @@ namespace ReactNative.DevSupport
         private bool _isShakeDetectorRegistered;
         private bool _isUsingJsProxy;
 
+        private ReactContext _currentContext;
         private RedBoxDialog _redBoxDialog;
         private Action _dismissRedBoxDialog;
         private bool _redBoxDialogOpen;
-        private DevOptionDialog _devOptionDialog;
+        private DevOptionDialog _devOptionsDialog;
 
         public DevSupportManager(
             IReactInstanceDevCommandsHandler reactInstanceCommandsHandler,
@@ -50,7 +52,15 @@ namespace ReactNative.DevSupport
             ReloadSettings();
         }
 
-        public string CachedJavaScriptBundleFile
+        public IDeveloperSettings DevSettings
+        {
+            get
+            {
+                return _devSettings;
+            }
+        }
+
+        public string DownloadedJavaScriptBundleFile
         {
             get
             {
@@ -113,22 +123,134 @@ namespace ReactNative.DevSupport
 
             if (IsEnabled)
             {
-                var javaScriptException = exception as JavaScriptException;
-                if (javaScriptException != null && javaScriptException.JavaScriptStackTrace != null)
-                {
-                    var stackTrace = StackTraceHelper.ConvertChakraStackTrace(javaScriptException.JavaScriptStackTrace);
-                    ShowNewError(exception.Message, stackTrace, NativeErrorCookie);
-                }
-                else
-                {
-                    Tracer.Error(ReactConstants.Tag, "Exception in native call from JavaScript.", exception);
-                    ShowNewNativeError(exception.Message, exception);
-                }
+                ShowNewNativeError(exception.Message, exception);
             }
             else
             {
                 ExceptionDispatchInfo.Capture(exception).Throw();
             }
+        }
+
+        public void ShowNewNativeError(string message, Exception exception)
+        {
+            var javaScriptException = exception as JavaScriptException;
+            if (javaScriptException != null && javaScriptException.JavaScriptStackTrace != null)
+            {
+                var stackTrace = StackTraceHelper.ConvertChakraStackTrace(javaScriptException.JavaScriptStackTrace);
+                ShowNewError(exception.Message, stackTrace, NativeErrorCookie);
+            }
+            else
+            {
+                Tracer.Error(ReactConstants.Tag, "Exception in native call from JavaScript.", exception);
+                ShowNewError(message, StackTraceHelper.ConvertNativeStackTrace(exception), NativeErrorCookie);
+            }
+        }
+
+        public void ShowNewJavaScriptError(string title, JArray details, int errorCookie)
+        {
+            ShowNewError(title, StackTraceHelper.ConvertJavaScriptStackTrace(details), errorCookie);
+        }
+
+        public void UpdateJavaScriptError(string message, JArray details, int errorCookie)
+        {
+            DispatcherHelpers.RunOnDispatcher(() =>
+            {
+                if (_redBoxDialog == null
+                    || !_redBoxDialogOpen
+                    || errorCookie != _redBoxDialog.ErrorCookie)
+                {
+                    return;
+                }
+
+                _redBoxDialog.Message = message;
+                _redBoxDialog.StackTrace = StackTraceHelper.ConvertJavaScriptStackTrace(details);
+            });
+        }
+
+        public void HideRedboxDialog()
+        {
+            var dismissRedBoxDialog = _dismissRedBoxDialog;
+            if (_redBoxDialogOpen && dismissRedBoxDialog != null)
+            {
+                dismissRedBoxDialog();
+            }
+        }
+
+        public void ShowDevOptionsDialog()
+        {
+            if (_devOptionsDialog != null || !IsEnabled)
+            {
+                return;
+            }
+
+            DispatcherHelpers.RunOnDispatcher(() =>
+            {
+                var options = new[]
+                {
+                    new DevOptionHandler(
+                        "Reload JavaScript",
+                        HandleReloadJavaScript),
+                    new DevOptionHandler(
+                        _isUsingJsProxy
+                            ? "Stop JS Remote Debugging"
+                            : "Start JS Remote Debugging",
+                        () =>
+                        {
+                            _isUsingJsProxy = !_isUsingJsProxy;
+                            HandleReloadJavaScript();
+                        }),
+                    new DevOptionHandler(
+                        _devSettings.IsReloadOnJavaScriptChangeEnabled
+                            ? "Disable Live Reload"
+                            : "Enable Live Reload",
+                        () =>
+                            _devSettings.IsReloadOnJavaScriptChangeEnabled =
+                                !_devSettings.IsReloadOnJavaScriptChangeEnabled),
+                    new DevOptionHandler(
+                        "Toggle Inspector",
+                        () =>
+                        {
+                            _devSettings.IsElementInspectorEnabled = !_devSettings.IsElementInspectorEnabled;
+                            _reactInstanceCommandsHandler.ToggleElementInspector();
+                        }),
+                };
+
+                _devOptionsDialog = new DevOptionDialog();
+                _devOptionsDialog.Closed += (_, __) =>
+                {
+                    _devOptionsDialog = null;
+                };
+
+                foreach (var option in options)
+                {
+                    _devOptionsDialog.Add(option.Name, option.OnSelect);
+                }
+
+                var asyncInfo = _devOptionsDialog.ShowAsync();
+
+                foreach (var option in options)
+                {
+                    option.AsyncInfo = asyncInfo;
+                }
+            });
+        }
+
+        public void OnNewReactContextCreated(ReactContext context)
+        {
+            ResetCurrentContext(context);
+        }
+
+        public void OnReactContextDestroyed(ReactContext context)
+        {
+            if (context == _currentContext)
+            {
+                ResetCurrentContext(null);
+            }
+        }
+
+        public Task<bool> IsPackagerRunningAsync()
+        {
+            return _devServerHelper.IsPackagerRunningAsync();
         }
 
         public async void HandleReloadJavaScript()
@@ -159,21 +281,12 @@ namespace ReactNative.DevSupport
             }
         }
 
-        public void HideRedboxDialog()
-        {
-            var dismissRedBoxDialog = _dismissRedBoxDialog;
-            if (_redBoxDialogOpen && dismissRedBoxDialog != null)
-            {
-                dismissRedBoxDialog();
-            }
-        }
-
         public void ReloadSettings()
         {
             if (_isDevSupportEnabled)
             {
                 RegisterDevOptionsMenuTriggers();
-                if (_devSettings.IsJavaScriptDevModeEnabled)
+                if (_devSettings.IsReloadOnJavaScriptChangeEnabled)
                 {
                     _pollingDisposable.Disposable =
                         _devServerHelper.StartPollingOnChangeEndpoint(HandleReloadJavaScript);
@@ -187,86 +300,40 @@ namespace ReactNative.DevSupport
             else
             {
                 UnregisterDevOptionsMenuTriggers();
+
+                if (_redBoxDialog != null)
+                {
+                    _dismissRedBoxDialog();
+                }
+
                 _pollingDisposable.Disposable = Disposable.Empty;
             }
         }
 
-        public void ShowDevOptionsDialog()
+        private void ResetCurrentContext(ReactContext context)
         {
-            DispatcherHelpers.RunOnDispatcher(() =>
+            if (_currentContext == context)
             {
-                if (_devOptionDialog != null || !IsEnabled)
-                {
-                    return;
-                }
+                return;
+            }
 
-                _devOptionDialog = new DevOptionDialog();
-                _devOptionDialog.Closed += (_, __) =>
-                {
-                    _devOptionDialog = null;
-                };
+            _currentContext = context;
 
-                var options = new[]
-                {
-                    new DevOptionHandler(
-                        "Reload JavaScript",
-                        HandleReloadJavaScript),
-                    new DevOptionHandler(
-                        _isUsingJsProxy
-                            ? "Stop Chrome Debugging"
-                            : "Debug in Chrome",
-                        () =>
-                        {
-                            _isUsingJsProxy = !_isUsingJsProxy;
-                            HandleReloadJavaScript();
-                        }),
-                    new DevOptionHandler(
-                        _devSettings.IsReloadOnJavaScriptChangeEnabled
-                            ? "Disable Live Reload"
-                            : "Enable Live Reload", 
-                        () => 
-                            _devSettings.IsReloadOnJavaScriptChangeEnabled = 
-                                !_devSettings.IsReloadOnJavaScriptChangeEnabled),
-                };
-
-                foreach (var option in options)
-                {
-                    _devOptionDialog.Add(option.Name, option.OnSelect);
-                }
-
-                var asyncInfo = _devOptionDialog.ShowAsync();
-
-                foreach (var option in options)
-                {
-                    option.AsyncInfo = asyncInfo;
-                }
-            });
-        }
-
-        public void ShowNewJavaScriptError(string title, JArray details, int exceptionId)
-        {
-            ShowNewError(title, StackTraceHelper.ConvertJavaScriptStackTrace(details), exceptionId);
-        }
-
-        public void ShowNewNativeError(string message, Exception exception)
-        {
-            ShowNewError(message, StackTraceHelper.ConvertNativeStackTrace(exception), NativeErrorCookie);
-        }
-
-        public void UpdateJavaScriptError(string message, JArray details, int errorCookie)
-        {
-            DispatcherHelpers.RunOnDispatcher(() =>
+            if (_devSettings.IsHotModuleReplacementEnabled && context != null)
             {
-                if (_redBoxDialog == null
-                    || !_redBoxDialogOpen
-                    || errorCookie != _redBoxDialog.ErrorCookie)
+                try
                 {
-                    return;
+                    var uri = new Uri(SourceUrl);
+                    var path = uri.PathAndQuery.Substring(1); // strip initial slash in path
+                    var host = uri.Host;
+                    var port = uri.Port;
+                    context.GetJavaScriptModule<HMRClient>().enable("windows", path, host, port);
                 }
-
-                _redBoxDialog.Message = message;
-                _redBoxDialog.StackTrace = StackTraceHelper.ConvertJavaScriptStackTrace(details);
-            });
+                catch (Exception ex)
+                {
+                    HandleException(ex);
+                }
+            }
         }
 
         private void ShowNewError(string message, IStackFrame[] stack, int errorCookie)

ReactWindows/ReactNative/DevSupport/DisabledDevSupportManager.cs
@@ -1,6 +1,9 @@
 ﻿using Newtonsoft.Json.Linq;
+using ReactNative.Bridge;
+using ReactNative.Modules.DevSupport;
 using System;
 using System.Runtime.ExceptionServices;
+using System.Threading.Tasks;
 using Windows.ApplicationModel.Core;
 using Windows.UI.Core;
 
@@ -8,7 +11,15 @@ namespace ReactNative.DevSupport
 {
     class DisabledDevSupportManager : IDevSupportManager
     {
-        public string CachedJavaScriptBundleFile
+        public IDeveloperSettings DevSettings
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public string DownloadedJavaScriptBundleFile
         {
             get
             {
@@ -59,6 +70,19 @@ namespace ReactNative.DevSupport
         }
 
         public void HideRedboxDialog()
+        {
+        }
+
+        public Task<bool> IsPackagerRunningAsync()
+        {
+            return Task.FromResult(false);
+        }
+
+        public void OnNewReactContextCreated(ReactContext context)
+        {
+        }
+
+        public void OnReactContextDestroyed(ReactContext context)
         {
         }
 

ReactWindows/ReactNative/DevSupport/HMRClient.cs
@@ -1,0 +1,36 @@
+﻿using ReactNative.Bridge;
+
+namespace ReactNative.DevSupport
+{
+    /// <summary>
+    /// JavaScript module for HMRClient.
+    /// 
+    /// The HMR(Hot Module Replacement)Client allows for the application to
+    /// receive updates from the packager server (over a web socket), allowing
+    /// for injection of JavaScript to the running application (without a 
+    /// refresh).
+    /// </summary>
+    public class HMRClient : JavaScriptModuleBase
+    {
+        /// <summary>
+        /// Enable the HMRClient so that the client will receive updates from
+        /// the packager server.
+        /// </summary>
+        /// <param name="platform">
+        /// The platform in which HMR updates will be enabled.
+        /// </param>
+        /// <param name="bundleEntry">
+        /// The path to the bundle entry file (e.g., index.windows.bundle).
+        /// </param>
+        /// <param name="host">
+        /// The host that the HMRClient should communicate with.
+        /// </param>
+        /// <param name="port">
+        /// The port that the HMRClient should communicate with on the host.
+        /// </param>
+        public void enable(string platform, string bundleEntry, string host, int port)
+        {
+            Invoke(platform, bundleEntry, host, port);
+        }
+    }
+}

ReactWindows/ReactNative/DevSupport/IDevSupportManager.cs
@@ -1,5 +1,8 @@
 ﻿using Newtonsoft.Json.Linq;
+using ReactNative.Bridge;
+using ReactNative.Modules.DevSupport;
 using System;
+using System.Threading.Tasks;
 
 namespace ReactNative.DevSupport
 {
@@ -11,9 +14,14 @@ namespace ReactNative.DevSupport
     public interface IDevSupportManager
     {
         /// <summary>
+        /// The developer settings.
+        /// </summary>
+        IDeveloperSettings DevSettings { get; }
+
+        /// <summary>
         /// The cached JavaScript bundle.
         /// </summary>
-        string CachedJavaScriptBundleFile { get; }
+        string DownloadedJavaScriptBundleFile { get; }
 
         /// <summary>
         /// Enables or disables the instance.
@@ -50,6 +58,26 @@ namespace ReactNative.DevSupport
         /// Dismisses the red box exception dialog.
         /// </summary>
         void HideRedboxDialog();
+
+        /// <summary>
+        /// Checks if the packager is running.
+        /// </summary>
+        /// <returns>
+        /// <code>true</code> if the packager is running, otherwise <code>false</code>.
+        /// </returns>
+        Task<bool> IsPackagerRunningAsync();
+
+        /// <summary>
+        /// Notify when a new React context is created.
+        /// </summary>
+        /// <param name="context">The React context.</param>
+        void OnNewReactContextCreated(ReactContext context);
+
+        /// <summary>
+        /// Notify when a new React context is destroyed.
+        /// </summary>
+        /// <param name="context">The React context.</param>
+        void OnReactContextDestroyed(ReactContext context);
 
         /// <summary>
         /// Reload developer settings.

ReactWindows/ReactNative/DevSupport/IReactInstanceDevCommandsHandler.cs
@@ -30,5 +30,10 @@ namespace ReactNative.DevSupport
         /// The JavaScript executor factory.
         /// </param>
         void OnReloadWithJavaScriptDebugger(Func<IJavaScriptExecutor> javaScriptExecutorFactory);
+
+        /// <summary>
+        /// Toggles the element inspector.
+        /// </summary>
+        void ToggleElementInspector();
     }
 }

ReactWindows/ReactNative/Modules/DevSupport/IDeveloperSettings.cs
@@ -1,0 +1,33 @@
+﻿namespace ReactNative.Modules.DevSupport
+{
+    /// <summary>
+    /// Provides access to React Native developer settings.
+    /// </summary>
+    public interface IDeveloperSettings
+    {
+        /// <summary>
+        /// Signals whether an overlay showing current FPS should be shown.
+        /// </summary>
+        bool IsFpsDebugEnabled { get; }
+
+        /// <summary>
+        /// Signals whether debug information about transitions should be displayed.
+        /// </summary>
+        bool IsAnimationFpsDebugEnabled { get; }
+
+        /// <summary>
+        /// Signals whether dev mode should be enabled in JavaScript bundles.
+        /// </summary>
+        bool IsJavaScriptDevModeEnabled { get; }
+
+        /// <summary>
+        /// Signals whether JavaScript bundle should be minified.
+        /// </summary>
+        bool IsJavaScriptMinifyEnabled { get; }
+
+        /// <summary>
+        /// Signals whether element inspector is enabled.
+        /// </summary>
+        bool IsElementInspectorEnabled { get; }
+    }
+}

commit 4c0ba56a7d68b2f6a4672cf75775343e28cfc08a

Fixes regression caused by 0f708017

ReactWindows/ReactNative/DevSupport/DevInternalSettings.cs
@@ -36,7 +36,7 @@ namespace ReactNative.DevSupport
 #if DEBUG
                 return GetSetting(JsDevModeDebugKey, true);
 #else
-                return GetSetting(JSDevModeDebugKey, false);
+                return GetSetting(JsDevModeDebugKey, false);
 #endif
             }
             set