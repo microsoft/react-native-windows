8aa0346a782a0d2ec6686e355c74a4098223e594
Eric Rozell <erozell@outlook.com>
2016-05-18 10:22:47 -0400

    refactor(ReactWindows): Reverting changes outside ReactWindows folder
    Starting to move towards a model where ReactWindows can be layered on top of React Native without requiring any core JS changes. The approach here is to take any of the files that had been modified for the Windows platform and copy them into the ReactWindows sub-folder, applying the .windows.js extension. The packager is already built to register the 'generic' module separately from the 'windows' module.

---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @noflow - get/set properties not yet supported by flow. also `...require(x)` is broken #6560135
+ */
+'use strict';
+
+var ReactWindows = {
+  // Components
+  get FlipViewWindows() { return require('FlipViewWindows'); },
+  get ProgressBarWindows() {return require('ProgressBarWindows');},
+  get SplitViewWindows() { return require('SplitViewWindows'); },
+};
+
+module.exports = ReactWindows;
