cfd5cee2c5f75b774f549f0f8dd31b574872b675
mattpodwysocki <matthew.podwysocki@gmail.com>
2016-05-26 14:31:19 -0400

    feat(CLI) - fixing per comments

--- local-cli/generator-windows/templates/src/Properties/AssemblyInfo.cs
+++ local-cli/generator-windows/templates/src/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MyApp")]
+[assembly: AssemblyTitle("<%= name %>")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MyApp")]
+[assembly: AssemblyProduct("<%= name %>")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
