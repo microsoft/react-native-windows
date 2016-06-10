d9c6f69faee340abd3ebc04d51f03aaa02c65402
mattpodwysocki <matthew.podwysocki@gmail.com>
2016-06-10 10:45:12 -0400

    feat(CLI) - runWindows add build script

--- local-cli/runWindows/runWindows.js
+++ local-cli/runWindows/runWindows.js
@@ -10,7 +10,11 @@ var MSBuildTools = require('./utils/msbuildtools');
  * Starts the app on a connected Windows emulator or mobile device.
  */
 function runWindows(options) {
-  var slnFiles = getSolutionFile();
+  // Fix up options
+  options.root = options.root || process.cwd();
+  options.arch === 'any cpu' && (options.arch = 'anycpu');
+
+  var slnFiles = getSolutionFile(options);
   if (slnFiles.length === 0) {
     console.log(chalk.red('Visual Studio Solution file not found. Maybe run rnpm windows first?'));
     return;
@@ -28,19 +32,20 @@ function runWindows(options) {
     return;
   }
 
+  // Restore the NuGet packages
+  child_process.execSync('../.nuget/nuget.exe restore ' + slnFile + ' -NonInteractive');
+
   // Get build/deploy options
   var buildType = options.release ? 'release' : 'debug';
-  var buildArchs = options.archs ? options.archs.split(' ') : ['anycpu'];
+  var buildArch = options.arch ? options.arch : 'anycpu';
   var deployTarget = options.target ? options.target : (options.emulator ? 'emulator' : 'device');
 
   var msBuildTools = MSBuildTools.findAvailableVersion();
-
-  // Restore the NuGet packages
-  child_process.execSync('../.nuget/nuget.exe restore ' + slnFile + ' -NonInteractive');
+  msBuildTools.buildProject(slnFile, buildType, buildArch, null);
 }
 
-function getSolutionFile() {
-  return glob.sync(path.join(process.cwd(), 'windows/*.sln'));
+function getSolutionFile(options) {
+  return glob.sync(path.join(options.root, 'windows/*.sln'));
 }
 
 module.exports = runWindows;
