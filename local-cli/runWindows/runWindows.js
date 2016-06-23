5d9cdb20644cf1775d1ac33db50c8a31fec61ba2
Matthew Podwysocki <matthewp@microsoft.com>
2016-06-23 12:42:29 -0700

    feat(CLI) - implement runWindows (#464)
    * feat(CLI) - implement runWindows initial commit
    
    * feat(CLI) - implement runWindows initial commit
    
    * feat(CLI) - implement runWindows add msbuild
    
    * feat(CLI) - add more msbuild
    
    * feat(CLI) - runWindows add engineStrict
    
    * feat(CLI) - runWindows add version test
    
    * feat(CLI) - runWindows add version test
    
    * feat(CLI) - runWindows add version test
    
    * feat(CLI) - runWindows finish build
    
    * feat(CLI) - runWindows adding build
    
    * feat(CLI) - runWindows add build script
    
    * feat(CLI) runWindows - fix csproj ref for NuGet restore
    
    * feat(CLI) runWindows - clean up test code
    
    * feat(runWindows) - Adding utils
    
    * feat(runWindows) - deploy and run the app
    
    * feat(runWindows) - now runs windows local
    
    * feat(runWindows) - refactor launch
    
    * feat(runWindows) - Document options
    
    * feat(runWindows) - add device/mobile
    
    * feat(runWindows) - refactoring files
    
    * feat(runWindows) - add RN server check
    
    * feat(runWindows) - promisify code
    
    * feat(runWindows) - classify version
    
    * feat(runWindows) - classify msbuild
    
    * feat(runWindows) - finishing app deploy on device/desktop
    
    * feat(runWindows) - fix as per comments
    
    * feat(runWindows) - add more EOL checks
    
    * feat(runWindows) - add path checks

---
+++ local-cli/runWindows/runWindows.js
@@ -0,0 +1,113 @@
+'use strict';
+
+const chalk = require('chalk');
+const build = require('./utils/build');
+const deploy = require('./utils/deploy');
+
+function runWindows(options) {
+  // Fix up options
+  options.root = options.root || process.cwd();
+  if (options.debug && options.release) {
+    console.log(chalk.red('Only one of "debug"/"release" options should be specified'));
+    return;
+  }
+
+  const slnFile = build.getSolutionFile(options);
+  if (!slnFile) {
+    console.error(chalk.red('Visual Studio Solution file not found. Maybe run "rnpm windows" first?'));
+    return;
+  }
+
+  try {
+    build.restoreNuGetPackages(options, slnFile);
+  } catch (e) {
+    console.error(chalk.red('Failed to restore the NuGet packages'));
+    return;
+  }
+
+  // Get build/deploy options
+  const buildType = options.release ? 'Release' : 'Debug';
+  const buildArch = options.arch ? options.arch : 'x86';
+
+  try {
+    build.cleanSolution(options);
+    build.buildSolution(slnFile, buildType, buildArch);
+  } catch (e) {
+    console.error(chalk.red(`Build failed with message ${e}. Check your build configuration.`));
+    return;
+  }
+
+  return deploy.startServerInNewWindow(options)
+    .then(() => {
+      if (options.desktop) {
+        return deploy.deployToDesktop(options);
+      }
+
+      if (options.device || options.emulator || options.target) {
+        return deploy.deployToDevice(options);
+      }
+    })
+    .catch(e => console.error(chalk.red(`Failed to deploy: ${e.message}`)));
+}
+
+/*
+// Example of running the Windows Command
+runWindows({
+  root: 'C:\\github\\hack\\myapp',
+  debug: true,
+  arch: 'x86',
+  nugetPath: 'C:\\github\\react\\react-native-windows\\local-cli\\runWindows\\.nuget\\nuget.exe',
+  desktop: true
+});
+*/
+
+/**
+ * Starts the app on a connected Windows emulator or mobile device.
+ * Options are the following:
+ *    root: String - The root of the application
+ *    debug: Boolean - Specifies debug build
+ *    release: Boolean - Specifies release build
+ *    arch: String - The build architecture (x86, x64, ARM, Any CPU)
+ *    desktop: Boolean - Deploy to the desktop
+ *    emulator: Boolean - Deploy to the emulator
+ *    device: Boolean - Deploy to a device
+ *    target: String - Device GUID to deploy to
+ */
+module.exports = {
+  name: 'run-windows',
+  description: 'builds your app and starts it on a connected Windows desktop, emulator or device',
+  func: runWindows,
+  options: [{
+    command: '--debug',
+    description: 'Specifies a debug build',
+    default: true,
+  }, {
+    command: '--release',
+    description: 'Specifies a release build',
+    default: false,
+  }, {
+    command: '--root [string]',
+    description: 'Override the root directory for the windows build which contains the windows folder.',
+    default: '',
+  }, {
+    command: '--arch [string]',
+    description: 'The build architecture (ARM, x86, x64)',
+    default: 'x86',
+  }, {
+    command: '--desktop',
+    description: 'Deploys the app to the desltop',
+    default: true,
+  }, {
+    command: '--emulator',
+    description: 'Deploys the app to an emulator',
+    default: false,
+  }, {
+    command: '--device',
+    description: 'Deploys the app to a connected device',
+    default: false,
+  }, {
+    command: '--target [string]',
+    description: 'Deploys the app to the specified GUID for a device.',
+    default: '',
+  }]
+};
---
+++ local-cli/runWindows/utils/deploy.js
@@ -0,0 +1,120 @@
+'use strict';
+
+const child_process = require('child_process');
+const spawn = child_process.spawn, execSync = child_process.execSync;
+const fs = require('fs');
+const http = require('http');
+const path = require('path');
+const chalk = require('chalk');
+const glob = require('glob');
+const parse = require('xml-parser');
+const WinAppDeployTool = require('./winappdeploytool');
+
+function getAppPackage(options) {
+  return glob.sync(path.join(options.root, 'windows/*/AppPackages/*'))[0];
+}
+
+function getWindowsStoreAppUtils(options) {
+  const appStorePath = path.join(options.root, 'node_modules/react-native-windows/local-cli/runWindows/utils/WindowsStoreAppUtils.ps1');
+  execSync(`powershell Unblock-File "${appStorePath}"`);
+  return appStorePath;
+}
+
+function getAppxManifest(options) {
+  const appxPath = glob.sync(path.join(options.root, 'windows/*/bin/*/*/AppxManifest.xml'))[0];
+  return parse(fs.readFileSync(appxPath, 'utf8'));
+}
+
+function handleResponseError(e) {
+  if (e.message.indexOf('Error code -2146233088')) {
+    throw new Error(`No Windows Mobile device was detected: ${e.message}`);
+  } else {
+    throw new Error(`Unexpected error deploying app: ${e.message}`);
+  }
+}
+
+// Errors: 0x80073d10 - bad architecture
+function deployToDevice(options) {
+  const appPackageFolder = getAppPackage(options);
+
+  const deployTarget = options.target ? options.target : (options.emulator ? 'emulator' : 'device');
+  const deployTool = new WinAppDeployTool();
+  const appxManifest = getAppxManifest(options);
+  const identity = appxManifest.root.children.filter(function (x) { return x.name === 'mp:PhoneIdentity'; })[0];
+  const appName = identity.attributes.PhoneProductId;
+
+  return new Promise(resolve => {
+    const device = deployTool.findDevice(deployTarget);
+
+    try {
+      deployTool.uninstallAppPackage(appName, device);
+    } catch (e) {
+      console.log(chalk.yellow('Failed to uninstall app from ' + device.name));
+    }
+
+    const appxFile = glob.sync(path.join(appPackageFolder, '*.appx'))[0];
+    try {
+      console.log(chalk.white(deployTool.installAppPackage(appxFile, device, true, false)));
+      resolve();
+    } catch (e) {
+      if (e.message.indexOf('Error code 2148734208 for command') !== -1) {
+        console.log(chalk.white(deployTool.installAppPackage(appxFile, device, true, true)));
+        resolve();
+      } else {
+        handleResponseError(e);
+      }
+    }
+  });
+}
+
+function deployToDesktop(options) {
+  const appPackageFolder = getAppPackage(options);
+
+  const windowsStoreAppUtils = getWindowsStoreAppUtils();
+  const appxManifest = getAppxManifest(options);
+  const identity = appxManifest.root.children.filter(function (x) { return x.name === 'Identity'; })[0];
+  const appName = identity.attributes.Name;
+  const script = glob.sync(path.join(appPackageFolder, 'Add-AppDevPackage.ps1'))[0];
+
+
+  return new Promise(resolve => {
+    console.log(chalk.green('Removing old version of the app'));
+    execSync(`powershell -ExecutionPolicy RemoteSigned Import-Module "${windowsStoreAppUtils}" ; Uninstall-App ${appName}`);
+
+    console.log(chalk.green('Installing new version of the app'));
+    execSync(`powershell -ExecutionPolicy RemoteSigned Import-Module "${windowsStoreAppUtils}"; Install-App "${script}"`);
+
+    console.log(chalk.green('Starting the app'));
+    execSync(`powershell -ExecutionPolicy RemoteSigned Import-Module "${windowsStoreAppUtils}"; Start-Locally ${appName}`);
+    resolve();
+  });
+}
+
+function startServerInNewWindow(options) {
+  return new Promise(resolve => {
+    http.get('http://localhost:8081/status', res => {
+      if (res === 200) {
+        console.log(chalk.green('React-Native Server already started'));
+        resolve();
+      }
+    }).on('error', () => resolve(launchServer(options)));
+  });
+}
+
+function launchServer(options) {
+  console.log(chalk.green('Starting the React-Native Server'));
+  const launchPackagerScript = path.join('node_modules/react-native/packager/launchPackager.bat');
+  const opts = {
+    cwd: options.root,
+    detached: true,
+    stdio: 'ignore'
+  };
+
+  return Promise.resolve(spawn('cmd.exe', ['/C', 'start', launchPackagerScript], opts));
+}
+
+module.exports = {
+  deployToDesktop,
+  deployToDevice,
+  startServerInNewWindow
+};
