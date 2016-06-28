7a000804c409f2f43b123635732b765476e96274
Matthew Podwysocki <matthewp@microsoft.com>
2016-06-28 07:37:18 -0700

    fix(CLI) - linting (#500)

--- local-cli/rnpm/windows/src/windows.js
+++ local-cli/rnpm/windows/src/windows.js
@@ -9,7 +9,8 @@
 
 'use strict';
 
-const childProcess = require('child_process');
+const chalk = require('chalk');
+const execSync = require('child_process').execSync;
 const fs = require('fs');
 const path = require('path');
 const semver = require('semver');
@@ -22,7 +23,7 @@ const REACT_NATIVE_WINDOWS_GENERATE_PATH = function() {
     'local-cli',
     'generate-windows.js'
   );
-}
+};
 
 const REACT_NATIVE_PACKAGE_JSON_PATH = function() {
   return path.resolve(
@@ -31,21 +32,21 @@ const REACT_NATIVE_PACKAGE_JSON_PATH = function() {
     'react-native',
     'package.json'
   );
-}
+};
 
 function getInstallPackage(version) {
-  var packageToInstall = 'react-native-windows';
+  let packageToInstall = 'react-native-windows';
 
-  var validVersion = semver.valid(version);
-  var validRange = semver.validRange(version);
-  if ((validVersion && !semver.gtr(validVersion, "0.26.*")) ||
+  const validVersion = semver.valid(version);
+  const validRange = semver.validRange(version);
+  if ((validVersion && !semver.gtr(validVersion, '0.26.*')) ||
       (!validVersion && validRange && semver.gtr('0.27.0', validRange))) {
     console.error(
       'Please upgrade react-native to ^0.27 or specify a --windowsVersion that is >=0.27.0'
     );
     process.exit(1);
   }
-  
+
   if (validVersion) {
     packageToInstall += '@' + validVersion;
   } else if (validRange) {
@@ -59,21 +60,21 @@ function getInstallPackage(version) {
 
 function getReactNativeVersion() {
   if (fs.existsSync(REACT_NATIVE_PACKAGE_JSON_PATH())) {
-    var version = JSON.parse(fs.readFileSync(REACT_NATIVE_PACKAGE_JSON_PATH(), 'utf-8')).version;
-    return semver.major(version) + '.' + semver.minor(version) + '.*';
+    const version = JSON.parse(fs.readFileSync(REACT_NATIVE_PACKAGE_JSON_PATH(), 'utf-8')).version;
+    return `${semver.major(version)}.${semver.minor(version)}.*`;
   }
 }
 
 module.exports = function windows(config, args, options) {
-  let name = args[0] ? args[0] : JSON.parse(fs.readFileSync('package.json', 'utf8')).name
-  let ns = options['namespace'] ? options['namespace'] : name;
-  let version = options['windowsVersion'] ? options['windowsVersion'] : getReactNativeVersion();
-  
-  let rnwPackage = getInstallPackage(version);
-  console.log('Installing ' + rnwPackage + '...');
-  childProcess.execSync('npm install --save ' + rnwPackage);
-  console.log(rnwPackage + ' successfully installed.');
-  
-  let generateWindows = require(REACT_NATIVE_WINDOWS_GENERATE_PATH());
+  const name = args[0] ? args[0] : JSON.parse(fs.readFileSync('package.json', 'utf8')).name;
+  const ns = options.namespace ? options.namespace : name;
+  const version = options.windowsVersion ? options.windowsVersion : getReactNativeVersion();
+
+  const rnwPackage = getInstallPackage(version);
+  console.log(chalk.green(`Installing ${rnwPackage}...`));
+  execSync(`npm install --save ${rnwPackage}`);
+  console.log(chalk.green(`${rnwPackage} successfully installed.`));
+
+  const generateWindows = require(REACT_NATIVE_WINDOWS_GENERATE_PATH());
   generateWindows(process.cwd(), name, ns);
-}
+};
