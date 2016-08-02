1818a0d9357de2fc61404bcf996d78101e4ecc45
Eric Rozell <erozell@outlook.com>
2016-08-02 13:54:41 -0400

    fix(RNPM): fix rnpm-plugin-windows to work with react-native-cli/rnpm/windows/index
    RNPM has recently moved to react-native, and there have been a few changes to the core behavior.  Making sure our plugin is compatible.

--- local-cli/rnpm/windows/index.js
+++ local-cli/rnpm/windows/index.js
@@ -1,12 +1,12 @@
-module.exports = [{
+module.exports = {
   func: require('./src/windows'),
   description: 'Generate React Native Windows template project',
   name: 'windows [name]',
   options: [{
-    flags: '--windowsVersion <version>',
+    command: '--windowsVersion [version]',
     description: 'The version of react-native-windows to use.'
   }, {
-    flags: '--namespace <namespace>',
+    command: '--namespace [namespace]',
     description: 'The native project namespace.'
   }]
-}];
+};
