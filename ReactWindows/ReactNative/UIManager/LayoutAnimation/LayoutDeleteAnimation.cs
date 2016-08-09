6c09144abb4d05edb8bdbda4ed04d5093075a784
Eric Rozell <erozell@outlook.com>
2016-08-09 12:33:28 -0400

    feat(LayoutAnimation): Adds delete animations for layout (#568)
    In cases where you want to fade out views or otherwise animate the layout operation that removes a view from the hierarchy, this change makes that possible.
    
    Fixes #508

---
+++ ReactWindows/ReactNative/UIManager/LayoutAnimation/LayoutDeleteAnimation.cs
@@ -0,0 +1,19 @@
+﻿namespace ReactNative.UIManager.LayoutAnimation
+{
+    /// <summary>
+    /// Layout animation for deleted views.
+    /// </summary>
+    class LayoutDeleteAnimation : BaseLayoutAnimation
+    {
+        /// <summary>
+        /// Signals if the animation should be performed in reverse.
+        /// </summary>
+        protected override bool IsReverse
+        {
+            get
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ ReactWindows/ReactNative/UIManager/ViewParentManagerExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Windows.UI.Xaml;
+
+namespace ReactNative.UIManager
+{
+    public static class ViewParentManagerExtensions
+    {
+        /// <summary>
+        /// Remove a view from a <see cref="IViewParentManager"/>. 
+        /// </summary>
+        /// <param name="viewManager">The view manager.</param>
+        /// <param name="parent">The parent view.</param>
+        /// <param name="view">The view to remove.</param>
+        public static void RemoveView(this IViewParentManager viewManager, DependencyObject parent, DependencyObject view)
+        {
+            for (var i = 0; i < viewManager.GetChildCount(parent); ++i)
+            {
+                if (viewManager.GetChildAt(parent, i) == view)
+                {
+                    viewManager.RemoveChildAt(parent, i);
+                    break;
+                }
+            }
+        }
+    }
+}
