47d8116b46dcb0d18d9b82124c973b3b15981f45
mattpodwysocki <matthew.podwysocki@gmail.com>
2016-08-15 13:29:51 -0400

    feat(ChakraBridge) - Add get/set global variables

--- ReactWindows/ReactNative.ChakraBridge/ChakraJavaScriptExecutor.cpp
+++ ReactWindows/ReactNative.ChakraBridge/ChakraJavaScriptExecutor.cpp
@@ -12,3 +12,58 @@ ChakraJavaScriptExecutor::~ChakraJavaScriptExecutor()
 {
 	this->host.Destroy();
 }
+
+void ChakraJavaScriptExecutor::SetGlobalVariable(String^ variableName, JsonValue^ value)
+{
+	JsErrorCode status = JsNoError;
+	
+	JsValueRef valueStringified;
+	String^ valueText = value->Stringify();
+	status = JsPointerToString(valueText->Data(), valueText->Length(), &valueStringified);
+	if (status != JsNoError)
+	{
+		// TODO: Throw right error
+	}
+
+	JsValueRef valueJson;
+	status = this->host.JsonParse(valueStringified, &valueJson);
+	if (status != JsNoError)
+	{
+		// TODO: Throw right error
+	}
+
+	status = this->host.SetGlobalVariable(variableName->Data(), valueJson);
+	if (status != JsNoError)
+	{
+		// TODO: Throw right error
+	}
+}
+
+JsonValue^ ChakraJavaScriptExecutor::GetGlobalVariable(String^ variableName)
+{
+	JsErrorCode status = JsNoError;
+
+	JsValueRef globalVariable;
+	status = this->host.GetGlobalVariable(variableName->Data(), &globalVariable);
+	if (status != JsNoError)
+	{
+		// TODO: Throw right error
+	}
+
+	JsValueRef globalVariableJson;
+	status = this->host.JsonStringify(globalVariable, &globalVariableJson);
+	if (status != JsNoError)
+	{
+		// TODO: Throw right error
+	}
+
+	const wchar_t* szBuf;
+	size_t bufLen;
+	status = JsStringToPointer(globalVariableJson, &szBuf, &bufLen);
+	if (status != JsNoError)
+	{
+		// TODO: Throw right error
+	}
+
+	return JsonValue::Parse(ref new String(szBuf, bufLen));
+}
--- ReactWindows/ReactNative.ChakraBridge/ChakraJavaScriptExecutor.h
+++ ReactWindows/ReactNative.ChakraBridge/ChakraJavaScriptExecutor.h
@@ -4,13 +4,20 @@
 #include <jsrt.h>
 #include "ChakraHost.h"
 
+using namespace Platform;
+using namespace Windows::Data::Json;
+
 namespace ReactNative { namespace ChakraBridge {
 
-	public ref class ChakraJavaScriptExecutor sealed
+[Windows::Foundation::Metadata::WebHostHidden]
+public ref class ChakraJavaScriptExecutor sealed
 {
 public:
 	ChakraJavaScriptExecutor();
 	virtual ~ChakraJavaScriptExecutor();
+
+	JsonValue^ GetGlobalVariable(String^ variableName);
+	void SetGlobalVariable(String^ variableName, JsonValue^ value);
 private:
 	ChakraHost host;
 };
