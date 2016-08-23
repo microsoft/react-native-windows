8d5a5f5093277ad0f186fc514808e60c044b0f3f
mattpodwysocki <matthew.podwysocki@gmail.com>
2016-08-23 17:36:01 -0400

    feat(ChakraBridge) - Add native interface for IJavaScriptExecutor

---
+++ ReactWindows/ReactNative/Chakra/Executor/NativeJavaScriptExecutor.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using ReactNative.Bridge;
+
+namespace ReactNative.Chakra.Executor
+{
+    /// <summary>
+    /// Uses the native C++ interface to the JSRT for Chakra.
+    /// </summary>
+    public class NativeJavaScriptExecutor : IJavaScriptExecutor
+    {
+        private readonly ChakraBridge.NativeJavaScriptExecutor _executor;
+
+        /// <summary>
+        /// Instantiates the <see cref="NativeJavaScriptExecutor"/>.
+        /// </summary>
+        public NativeJavaScriptExecutor()
+        {
+            _executor = new ChakraBridge.NativeJavaScriptExecutor();
+            Native.ThrowIfError((JavaScriptErrorCode)_executor.InitializeHost());
+        }
+
+        /// <summary>
+        /// Call the JavaScript method from the given module.
+        /// </summary>
+        /// <param name="moduleName">The module name.</param>
+        /// <param name="methodName">The method name.</param>
+        /// <param name="arguments">The arguments.</param>
+        /// <returns>The flushed queue of native operations.</returns>
+        public JToken CallFunctionReturnFlushedQueue(string moduleName, string methodName, JArray arguments)
+        {
+            if (moduleName == null)
+                throw new ArgumentNullException(nameof(moduleName));
+            if (methodName == null)
+                throw new ArgumentNullException(nameof(methodName));
+            if (arguments == null)
+                throw new ArgumentNullException(nameof(arguments));
+
+            var result = _executor.CallFunctionAndReturnFlushedQueue(moduleName, methodName, arguments.ToString(Formatting.None));
+            Native.ThrowIfError((JavaScriptErrorCode)result.ErrorCode);
+
+            return JToken.Parse(result.Result);
+        }
+
+        /// <summary>
+        /// Disposes the <see cref="NativeJavaScriptExecutor"/> instance.
+        /// </summary>
+        public void Dispose()
+        {
+            Native.ThrowIfError((JavaScriptErrorCode)_executor.DisposeHost());
+        }
+
+        /// <summary>
+        /// Flush the queue.
+        /// </summary>
+        /// <returns>The flushed queue of native operations.</returns>
+        public JToken FlushedQueue()
+        {
+            var result = _executor.FlushedQueue();
+            Native.ThrowIfError((JavaScriptErrorCode)result.ErrorCode);
+            return JToken.Parse(result.Result);
+        }
+
+        /// <summary>
+        /// Invoke the JavaScript callback.
+        /// </summary>
+        /// <param name="callbackId">The callback identifier.</param>
+        /// <param name="arguments">The arguments.</param>
+        /// <returns>The flushed queue of native operations.</returns>
+        public JToken InvokeCallbackAndReturnFlushedQueue(int callbackId, JArray arguments)
+        {
+            if (arguments == null)
+                throw new ArgumentNullException(nameof(arguments));
+
+            var result = _executor.InvokeCallbackAndReturnFlushedQueue(callbackId, arguments.ToString(Formatting.None));
+            Native.ThrowIfError((JavaScriptErrorCode)result.ErrorCode);
+            return JToken.Parse(result.Result);
+        }
+
+        /// <summary>
+        /// Runs the given script.
+        /// </summary>
+        /// <param name="script">The script.</param>
+        /// <param name="sourceUrl">The source URL.</param>
+        public void RunScript(string script, string sourceUrl)
+        {
+            if (script == null)
+                throw new ArgumentNullException(nameof(script));
+            if (sourceUrl == null)
+                throw new ArgumentNullException(nameof(sourceUrl));
+
+            try
+            {
+                var result = _executor.RunScript(script, sourceUrl);
+                Native.ThrowIfError((JavaScriptErrorCode)result.ErrorCode);
+            }
+            catch (JavaScriptScriptException ex)
+            {
+                var jsonError = JavaScriptValueToJTokenConverter.Convert(ex.Error);
+                var message = jsonError.Value<string>("message");
+                var stackTrace = jsonError.Value<string>("stack");
+                throw new Modules.Core.JavaScriptException(message ?? ex.Message, stackTrace, ex);
+            }
+        }
+
+        /// <summary>
+        /// Sets a global variable in the JavaScript runtime.
+        /// </summary>
+        /// <param name="propertyName">The global variable name.</param>
+        /// <param name="value">The value.</param>
+        public void SetGlobalVariable(string propertyName, JToken value)
+        {
+            if (propertyName == null)
+                throw new ArgumentNullException(nameof(propertyName));
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+
+            Native.ThrowIfError(
+                (JavaScriptErrorCode)_executor.SetGlobalVariable(propertyName, value.ToString(Formatting.None)));
+        }
+    }
+}
