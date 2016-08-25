ead35bbc379fb3539aa493d8189674a5b0836dd6
mattpodwysocki <matthew.podwysocki@gmail.com>
2016-08-25 16:41:24 -0400

    feat(I8N) - Add internationalization

---
+++ ReactWindows/ReactNative/Modules/Internationalization/InternationalizationModule.cs
@@ -0,0 +1,65 @@
+﻿using ReactNative.Bridge;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ReactNative.Modules.Internationalization
+{
+    /// <summary>
+    /// A module that allows JS to get/set right to left preferences.
+    /// </summary>
+    class InternationalizationModule : NativeModuleBase
+    {
+        private const string ModuleName = "I18nManager";
+        private const string IsRtl = "isRTL";
+        private const string LocalIdentifier = "localeIdentifier";
+
+
+        internal InternationalizationModule()
+        {
+
+        }
+
+        /// <summary>
+        /// Gets the module name.
+        /// </summary>
+        public override string Name
+        {
+            get
+            {
+                return ModuleName;
+            }
+        }
+
+        public override IReadOnlyDictionary<string, object> Constants
+        {
+            get
+            {
+                return new Dictionary<string, object>
+                {
+                    { IsRtl, InternationalizationUtil.IsRightToLeft },
+                    { LocalIdentifier, CultureInfo.CurrentCulture.Name }
+                };
+            }
+        }
+
+        /// <summary>
+        /// Sets whether to allow right to left.
+        /// </summary>
+        /// <param name="value">true to allow right to left; else false.</param>
+        [ReactMethod]
+        public void allowRTL(bool value)
+        {
+            InternationalizationUtil.IsRightToLeftAllowed = value;
+        }
+
+        /// <summary>
+        /// Sets whether to force right to left.  This is used for development purposes to force a language such as English to be RTL.
+        /// </summary>
+        /// <param name="value">true to force right to left; else false.</param>
+        [ReactMethod]
+        public void forceRTL(bool value)
+        {
+            InternationalizationUtil.IsRightToLeftForced = true;
+        }
+    }
+}
---
+++ ReactWindows/ReactNative/Modules/Internationalization/InternationalizationUtil.cs
@@ -0,0 +1,75 @@
+﻿using System.Globalization;
+using Windows.Storage;
+
+namespace ReactNative.Modules.Internationalization
+{
+    /// <summary>
+    /// Class used for right to left preferences.
+    /// </summary>
+    static class InternationalizationUtil
+    {
+        private const string AllowRTL = "RCTI18nUtil_allowRTL";
+        private const string ForceRTL = "RCTI18nUtil_forceRTL";
+
+        static InternationalizationUtil()
+        {
+            if (ApplicationData.Current.LocalSettings.Values[AllowRTL] == null)
+                ApplicationData.Current.LocalSettings.Values[AllowRTL] = false;
+
+            if (ApplicationData.Current.LocalSettings.Values[ForceRTL] == null)
+                ApplicationData.Current.LocalSettings.Values[ForceRTL] = false;
+        }
+
+        /// <summary>
+        /// Check if the system is using Right to Left. This only happens when the app:
+        /// - is forcing RTL layout regardless of language
+        /// - allows RTL layout when using RTL locale.
+        /// </summary>
+        public static bool IsRightToLeft
+        {
+            get
+            {
+                if (IsRightToLeftForced) return true;
+                return IsRightToLeftAllowed && IsDeviceRightToLeft;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets whether right to left is allowed.  Should be used very early during startup before the bridge is initialized.
+        /// </summary>
+        public static bool IsRightToLeftAllowed
+        {
+            get
+            {
+                return (bool)ApplicationData.Current.LocalSettings.Values[AllowRTL];
+            }
+            set
+            {
+                ApplicationData.Current.LocalSettings.Values[AllowRTL] = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets whether right to left is forced, used for development purposes.
+        /// </summary>
+        public static bool IsRightToLeftForced
+        {
+            get
+            {
+                return (bool)ApplicationData.Current.LocalSettings.Values[ForceRTL];
+            }
+            set
+            {
+                ApplicationData.Current.LocalSettings.Values[ForceRTL] = value;
+            }
+        }
+
+        private static bool IsDeviceRightToLeft
+        {
+            get
+            {
+                return CultureInfo.CurrentCulture.TextInfo.IsRightToLeft;
+            }
+        }
+    }
+}
