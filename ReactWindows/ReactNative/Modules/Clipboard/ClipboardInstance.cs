fcf03905ec7f34efa98b65235d6b015647c2310e
Matthew Podwysocki <matthewp@microsoft.com>
2016-08-31 18:06:01 -0400

    fix(Clipboard) - add interface/facade for testability (#661)
    * fix(Clipboard) - Add interface for testing
    
    * fix(Clipboard) - add implementation for clipboard to class
    
    * fix(Clipboard) - add mock to unit tests
    
    * fix(Clipboard) - add mock to unit tests

---
+++ ReactWindows/ReactNative/Modules/Clipboard/ClipboardInstance.cs
@@ -0,0 +1,22 @@
+﻿using Windows.ApplicationModel.DataTransfer;
+
+namespace ReactNative.Modules.Clipboard
+{
+    class ClipboardInstance : IClipboardInstance
+    {
+        public void Clear()
+        {
+            Windows.ApplicationModel.DataTransfer.Clipboard.Clear();
+        }
+
+        public DataPackageView GetContent()
+        {
+            return Windows.ApplicationModel.DataTransfer.Clipboard.GetContent();
+        }
+
+        public void SetContent(DataPackage package)
+        {
+            Windows.ApplicationModel.DataTransfer.Clipboard.SetContent(package);
+        }
+    }
+}
