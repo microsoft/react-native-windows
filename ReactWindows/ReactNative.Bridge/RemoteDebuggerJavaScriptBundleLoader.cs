280e3cbd2fce90938764923333699808e4447025
Matt Hargett <plaztiksyke@gmail.com>
2016-09-17 16:45:52 -0700

    feat(Portable): Move PCL-compatible interface and easily-convertible JavaScriptBundleLoader types to ReactNative.Bridge PCL.

--- ReactWindows/ReactNative.Bridge/RemoteDebuggerJavaScriptBundleLoader.cs
+++ ReactWindows/ReactNative.Bridge/RemoteDebuggerJavaScriptBundleLoader.cs
@@ -3,8 +3,9 @@ using System.Threading.Tasks;
 
 namespace ReactNative.Bridge
 {
-    class RemoteDebuggerJavaScriptBundleLoader : IJavaScriptBundleLoader
+    public class RemoteDebuggerJavaScriptBundleLoader : IJavaScriptBundleLoader
     {
+        private static readonly Task CompletedTask = Task.FromResult(true);
         private readonly string _proxySourceUrl;
 
         public RemoteDebuggerJavaScriptBundleLoader(string proxySourceUrl, string realSourceUrl)
@@ -20,7 +21,7 @@ namespace ReactNative.Bridge
 
         public Task InitializeAsync()
         {
-            return Task.CompletedTask;
+            return CompletedTask;
         }
 
         public void LoadScript(IReactBridge executor)
