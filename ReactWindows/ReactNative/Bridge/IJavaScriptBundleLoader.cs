a631894f77a9329504825c38ee523c9bad67dc2b
Matt Hargett <plaztiksyke@gmail.com>
2016-09-23 18:01:42 -0700

    Break up the JavaScriptBundleLoader file into discrete files for easier portability.

---
+++ ReactWindows/ReactNative/Bridge/IJavaScriptBundleLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+
+namespace ReactNative.Bridge
+{
+    /// <summary>
+    /// Stores JavaScript bundle information and allows the
+    /// <see cref="IReactInstance"/> to load a correct bundle through the
+    /// <see cref="IReactBridge"/>.
+    /// </summary>
+    public interface IJavaScriptBundleLoader
+    {
+        /// <summary>
+        /// The source URL of the bundle.
+        /// </summary>
+        string SourceUrl { get; }
+
+        /// <summary>
+        /// Initializes the JavaScript bundle loader.
+        /// </summary>
+        /// <returns>A task to await initialization.</returns>
+        Task InitializeAsync();
+
+        /// <summary>
+        /// Loads the bundle into a JavaScript bridge.
+        /// </summary>
+        /// <param name="bridge">The JavaScript bridge.</param>
+        void LoadScript(IReactBridge bridge);
+    }
+}
---
+++ ReactWindows/ReactNative/Bridge/RemoteDebuggerJavaScriptBundleLoader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ReactNative.Bridge
+{
+    class RemoteDebuggerJavaScriptBundleLoader : IJavaScriptBundleLoader
+    {
+        private readonly string _proxySourceUrl;
+
+        public RemoteDebuggerJavaScriptBundleLoader(string proxySourceUrl, string realSourceUrl)
+        {
+            _proxySourceUrl = proxySourceUrl;
+            SourceUrl = realSourceUrl;
+        }
+
+        public string SourceUrl
+        {
+            get;
+        }
+
+        public Task InitializeAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public void LoadScript(IReactBridge bridge)
+        {
+            if (bridge == null) throw new ArgumentNullException(nameof(bridge));
+
+            bridge.RunScript(_proxySourceUrl, SourceUrl);
+        }
+    }
+}
