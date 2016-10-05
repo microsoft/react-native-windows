bd8c147e729671c67b4050e2a791b62e5db43621
Kevin VanGelder <kevin@infinite.red>
2016-10-04 18:32:47 -0700

    initial .Net46 project

---
+++ ReactWindows/ReactNative.Net46/Tracing/Tracer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ReactNative.Tracing
+{
+    class NullDisposable : IDisposable
+    {
+        public void Dispose()
+        {
+
+        }
+    }
+    class NullLoggingActivityBuilder
+    {
+        public IDisposable Start()
+        {
+            return new NullDisposable();
+        }
+    }
+    static class Tracer
+    {
+        public const int TRACE_TAG_REACT_BRIDGE = 0;
+        public const int TRACE_TAG_REACT_APPS = 1;
+        public const int TRACE_TAG_REACT_VIEW = 2;
+
+        public static NullLoggingActivityBuilder Trace(int tag, string name)
+        {
+            return new NullLoggingActivityBuilder();
+        }
+
+        public static void Write(int tag, string eventName)
+        {
+        }
+
+        public static void Error(int tag, string eventName, Exception ex)
+        {
+        }
+    }
+}
