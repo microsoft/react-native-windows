279ea999054f5591186704f59cc922d44c74543a
Kevin VanGelder <kevin@infinite.red>
2016-10-05 12:49:17 -0700

    restructured and documented Net46 null objects and added debug flag to Net46 project so tests will pass

---
+++ ReactWindows/ReactNative.Net46/Tracing/NullLoggingActivityBuilder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ReactNative.Tracing
+{
+    /// <summary>
+    /// Temporary NullLoggingActivityBuilder.
+    /// </summary>
+    public class NullLoggingActivityBuilder
+    {
+        /// <summary>
+        /// Dummy method to satisfy interface requirements.
+        /// </summary>
+        /// <returns>An empty disposable object.</returns>
+        public IDisposable Start()
+        {
+            return new NullDisposable();
+        }
+
+        /// <summary>
+        /// Empty disposable object.
+        /// </summary>
+        class NullDisposable : IDisposable
+        {
+            /// <summary>
+            /// Dispose method to conform to IDisposable.
+            /// </summary>
+            public void Dispose()
+            {
+
+            }
+        }
+    }
+}
