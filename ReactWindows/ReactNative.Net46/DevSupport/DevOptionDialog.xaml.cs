60769f35a1c61d6488d10720859831317d147a34
Kevin VanGelder <kevin@infinite.red>
2016-10-13 19:15:03 -0700

    Migrate DevOptionDialog

---
+++ ReactWindows/ReactNative.Net46/DevSupport/DevOptionDialog.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ReactNative.DevSupport
+{
+    sealed partial class DevOptionDialog : Window
+    {
+        public DevOptionDialog()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Add button to dialog
+        /// </summary>
+        /// <param name="name">Displayed name of button</param>
+        /// <param name="onSelect">Action when button is pressed</param>
+        public void Add(string name, Action onSelect)
+        {
+            var button = new Button
+            {
+                Content = name,
+            };
+
+            button.Click += (sender, args) => onSelect();
+
+            OptionsStackPanel.Children.Add(button);
+        }
+    }
+}
