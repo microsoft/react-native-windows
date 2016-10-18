fb0725fc3fa35b88db0249e293392b6591c69880
Gant Laborde <gantman@gmail.com>
2016-10-17 19:10:01 -0500

    feat(Portable): MessageBox implementation for NET46 (#768)
    * MessageBox implementation for NET46
    
    * Clean up namespaces
    
    * format simplification

---
+++ ReactWindows/ReactNative.Shared/Modules/Dialog/DialogModuleHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace ReactNative.Modules.Dialog
+{
+    static class DialogModuleHelper
+    {
+        public const string ActionButtonClicked = "buttonClicked";
+        public const string ActionDismissed = "dismissed";
+
+        public const string KeyButtonPositive = "buttonPositive";
+        public const string KeyButtonNegative = "buttonNegative";
+
+        public const int KeyButtonPositiveValue = 0;
+        public const int KeyButtonNegativeValue = 1;
+    }
+}
