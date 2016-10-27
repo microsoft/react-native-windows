92f4475ec2f7343de7eca70ed89b2c76c74c9d67
Andrew Forster <anforste@bluejeansnet.com>
2016-10-27 10:34:41 -0700

    The NMock3 library is not compatiable with Universal Windows Projects... Any TestFixtures that utilize the NMock3 library need to be isolated in the .NET test project(s)...
    Moved NMock3 related tests from ReactiveNative.Shared.Tests to ReactNative46.Tests.
    
    The majority of tests in JavaScriptModuleBaseTests.cs utilized NMock so the entire file was moved from the ReactiveNative.Shared.Tests to the ReactNative.Net46.Tests projects.
    
    A handful of tests in JavaScriptModuleRegistryTests.cs utilized NMock. A specialized class inheriting from JavaScriptModuleRegistryTests was added to the ReactNative.Net46.Tests project.  The specialized class "JavaScriptModuleRegistryNMockTests" contains only the NMock dependent tests and the base class contains all tests that can be shared.
    
    All references to NMock have been removed from the test code in the ReactNative.Shared.Tests projects...
    
    A reference to the ReactNative.Shared.Tests has been added to the ReactNative.Tests project.   Additionally, the NUnit v3.41 nuGet package and the NUnit3TestAdapter v 3.5 nuGet package were added to the ReactNative.Tests project.
    
    The NUnit3TestAdapter v 3.41 nuGet package could not be loaded in the ReactNative.Tests project so v3.5 (the latest) was used. This creates a version mismatch with the NUnit package and the nuGet packages used in the ReactNative.Net46.Tests project...

--- ReactWindows/ReactNative.Shared.Tests/Bridge/JavaScriptModuleRegistryTests.cs
+++ ReactWindows/ReactNative.Shared.Tests/Bridge/JavaScriptModuleRegistryTests.cs
@@ -1,65 +1,18 @@
-﻿using Newtonsoft.Json.Linq;
-using NMock;
-using NMock.Syntax;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using ReactNative.Bridge;
-using ReactNative.UIManager;
-using ReactNative.UIManager.Events;
-using System.Threading;
 
 namespace ReactNative.Tests.Bridge
 {
     [TestFixture]
     public class JavaScriptModuleRegistryTests
     {
-        private MockFactory _mockFactory;
-        private Mock<IReactInstance> _mockReactInstance;
-        private JavaScriptModuleRegistry _registry;
-        private JavaScriptModuleRegistry.Builder _registryBuilder;
-
+        protected JavaScriptModuleRegistry _registry;
+        protected JavaScriptModuleRegistry.Builder _registryBuilder;
 
         [SetUp]
-        public void SetUp()
+        public virtual void SetUp()
         {
             _registryBuilder = new JavaScriptModuleRegistry.Builder();
-            _mockFactory = new MockFactory();
-            _mockReactInstance = _mockFactory.CreateMock<IReactInstance>();
-        }
-
-        [Test]
-        public void InvokesReactInstanceWhenFetchedModuleIsCalled()
-        {
-            _registry = _registryBuilder
-                .Add(typeof(RCTEventEmitter))
-                .Add(typeof(AppRegistry))
-                .Add(typeof(TestJavaScriptModule))
-                .Build();
-
-            var are = new AutoResetEvent(false);
-
-            var module = _registry.GetJavaScriptModule<TestJavaScriptModule>(_mockReactInstance.MockObject);
-
-            _mockReactInstance.Expects.One.Method(
-                _ => _.InvokeFunction(null, null, null, null))
-                .With(
-                    nameof(TestJavaScriptModule),
-                    nameof(TestJavaScriptModule.Foo),
-                    JArray.FromObject(new[] { 42 }),
-                    NMock.Is.StringContaining("_"))
-                .Will(Invoke.Action(() => are.Set()));
-
-            module.Foo(42);
-
-            are.WaitOne();
-        }
-
-        [Test]
-        public void ThrowsWhenUnknownModuleRequested()
-        {
-            _registry = _registryBuilder.Build();
-            Assert.That(
-                () => _registry.GetJavaScriptModule<TestJavaScriptModule>(_mockReactInstance.MockObject),
-                Throws.InvalidOperationException);
         }
 
 #if DEBUG
@@ -104,7 +57,7 @@ namespace ReactNative.Tests.Bridge
             private NoPublicConstructorJavaScriptModule() { }
         }
 
-        class TestJavaScriptModule : JavaScriptModuleBase
+        protected class TestJavaScriptModule : JavaScriptModuleBase
         {
             public void Bar()
             {
