afbb4327a53361d4e92321a52a4b39a51f8211b5
Kevin VanGelder <kevin@infinite.red>
2016-10-28 13:03:01 -0700

    cleanup font if block

--- ReactWindows/ReactNative.Shared/Views/Text/FontStyleHelpers.cs
+++ ReactWindows/ReactNative.Shared/Views/Text/FontStyleHelpers.cs
@@ -53,17 +53,50 @@ namespace ReactNative.Views.Text
                     Weight = (ushort)fontWeightNumeric,
                 };
 #else
-            if (fontWeightNumeric >= 0 && fontWeightNumeric <= 149) { return FontWeights.Thin; }
-            else if (fontWeightNumeric >= 150 && fontWeightNumeric <= 249) { return FontWeights.ExtraLight; }
-            else if (fontWeightNumeric >= 250 && fontWeightNumeric <= 349) { return FontWeights.Light; }
-            else if (fontWeightNumeric >= 350 && fontWeightNumeric <= 449) { return FontWeights.Normal; }
-            else if (fontWeightNumeric >= 450 && fontWeightNumeric <= 549) { return FontWeights.Medium; }
-            else if (fontWeightNumeric >= 550 && fontWeightNumeric <= 649) { return FontWeights.SemiBold; }
-            else if (fontWeightNumeric >= 650 && fontWeightNumeric <= 749) { return FontWeights.Bold; }
-            else if (fontWeightNumeric >= 750 && fontWeightNumeric <= 849) { return FontWeights.ExtraBold; }
-            else if (fontWeightNumeric >= 850 && fontWeightNumeric <= 949) { return FontWeights.Black; }
-            else if (fontWeightNumeric >= 950 && fontWeightNumeric <= 999) { return FontWeights.ExtraBlack; }
-            else { return FontWeights.Normal; }
+            if (fontWeightNumeric >= 0 && fontWeightNumeric <= 149)
+            {
+                return FontWeights.Thin;
+            }
+            else if (fontWeightNumeric >= 150 && fontWeightNumeric <= 249)
+            {
+                return FontWeights.ExtraLight;
+            }
+            else if (fontWeightNumeric >= 250 && fontWeightNumeric <= 349)
+            {
+                return FontWeights.Light;
+            }
+            else if (fontWeightNumeric >= 350 && fontWeightNumeric <= 449)
+            {
+                return FontWeights.Normal;
+            }
+            else if (fontWeightNumeric >= 450 && fontWeightNumeric <= 549)
+            {
+                return FontWeights.Medium;
+            }
+            else if (fontWeightNumeric >= 550 && fontWeightNumeric <= 649)
+            {
+                return FontWeights.SemiBold;
+            }
+            else if (fontWeightNumeric >= 650 && fontWeightNumeric <= 749)
+            {
+                return FontWeights.Bold;
+            }
+            else if (fontWeightNumeric >= 750 && fontWeightNumeric <= 849)
+            {
+                return FontWeights.ExtraBold;
+            }
+            else if (fontWeightNumeric >= 850 && fontWeightNumeric <= 949)
+            {
+                return FontWeights.Black;
+            }
+            else if (fontWeightNumeric >= 950 && fontWeightNumeric <= 999)
+            {
+                return FontWeights.ExtraBlack;
+            }
+            else
+            {
+                return FontWeights.Normal;
+            }
 #endif
         }
     }
