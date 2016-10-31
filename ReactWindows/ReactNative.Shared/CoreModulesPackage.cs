7c88e83f21f270b16a75bd51ff6b6e6513acb3d3
Kevin VanGelder <kevin@infinite.red>
2016-10-31 16:18:26 -0700

    feat(Shared): Migrate react page and dependencies (#846)
    * migrate ReactPage and dependencies to the Shared project

--- ReactWindows/ReactNative.Shared/CoreModulesPackage.cs
+++ ReactWindows/ReactNative.Shared/CoreModulesPackage.cs
@@ -7,6 +7,7 @@ using ReactNative.UIManager;
 using ReactNative.UIManager.Events;
 using System;
 using System.Collections.Generic;
+using System.Windows;
 
 namespace ReactNative
 {
@@ -39,12 +40,22 @@ namespace ReactNative
             using (Tracer.Trace(Tracer.TRACE_TAG_REACT_BRIDGE, "createUIManagerModule").Start())
             {
                 var viewManagerList = _reactInstanceManager.CreateAllViewManagers(reactContext);
+#if WINDOWS_UWP
                 uiManagerModule = new UIManagerModule(
                     reactContext, 
                     viewManagerList,
                     _uiImplementationProvider.Create(
                         reactContext, 
                         viewManagerList));
+#else
+                uiManagerModule = new UIManagerModule(
+                    reactContext,
+                    viewManagerList,
+                    _uiImplementationProvider.Create(
+                        reactContext,
+                        viewManagerList), 
+                    Application.Current.MainWindow);
+#endif
             }
 
             return new List<INativeModule>
