a468c894f61fd46f71402b6680dcbe1679198709
Andrew Forster <anforste@bluejeansnet.com>
2016-11-01 13:06:03 -0700

    Adding Playground.Net46 WPF application with basic ReactNative integration...

---
+++ ReactWindows/Playground.Net46/AppReactPage.cs
@@ -0,0 +1,59 @@
+﻿using ReactNative;
+using ReactNative.Modules.Core;
+using ReactNative.Shell;
+using System.Collections.Generic;
+
+namespace Playground.Net46
+{
+    class AppReactPage : ReactPage
+    {
+        public override string MainComponentName
+        {
+            get
+            {
+                return "Playground.Net46";
+            }
+        }
+
+        public override string JavaScriptMainModuleName
+        {
+            get
+            {
+                return "ReactWindows/Playground.Net46/index.windows";
+            }
+        }
+
+#if BUNDLE
+        public override string JavaScriptBundleFile
+        {
+            get
+            {
+                return "ms-appx:///ReactAssets/index.windows.bundle";
+            }
+        }
+#endif
+
+        public override List<IReactPackage> Packages
+        {
+            get
+            {
+                return new List<IReactPackage>
+                {
+                    new MainReactPackage(),
+                };
+            }
+        }
+
+        public override bool UseDeveloperSupport
+        {
+            get
+            {
+#if !BUNDLE || DEBUG
+                return true;
+#else
+                return false;
+#endif
+            }
+        }
+    }
+}
