44b941954f457fc9dc03204cb3c55c3242919085
Kevin VanGelder <kevin@infinite.red>
2016-12-05 11:14:18 -0800

    sorted imports

--- ReactWindows/ReactNative.Net46/Views/TextInput/ReactPasswordBoxManager.cs
+++ ReactWindows/ReactNative.Net46/Views/TextInput/ReactPasswordBoxManager.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json.Linq;
 using ReactNative.Reflection;
 using ReactNative.UIManager;
 using ReactNative.UIManager.Annotations;
 using ReactNative.Views.Text;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
