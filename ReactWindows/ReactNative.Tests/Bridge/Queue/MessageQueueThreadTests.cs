952ad354c9261ead44f10c96442e105c3912b8d8
Leon Kim <minusnine@gmail.com>
2016-12-06 15:14:54 +0900

    Fix MessageQueueThread_OneAtATime

--- ReactWindows/ReactNative.Tests/Bridge/Queue/MessageQueueThreadTests.cs
+++ ReactWindows/ReactNative.Tests/Bridge/Queue/MessageQueueThreadTests.cs
@@ -90,7 +90,7 @@ namespace ReactNative.Tests.Bridge.Queue
         }
 
         [TestMethod]
-        public async Task MessageQueueThread_OneAtATime()
+        public async Task MessageQueueThread_OneAtATimeAsync()
         {
             var uiThread = await CallOnDispatcherAsync(() => MessageQueueThread.Create(MessageQueueThreadSpec.DispatcherThreadSpec, ex => { Assert.Fail(); }));
             var backgroundThread = MessageQueueThread.Create(MessageQueueThreadSpec.Create("background", MessageQueueThreadKind.BackgroundSingleThread), ex => { Assert.Fail(); });
