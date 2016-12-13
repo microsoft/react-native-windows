41ae034b756a061408b36302d0a7335ed5537bcb
Leon Kim <minusnine@gmail.com>
2016-12-13 21:24:04 +0900

    Fix non-async method

--- ReactWindows/ReactNative.Net46/Views/Web/ReactWebViewManager.cs
+++ ReactWindows/ReactNative.Net46/Views/Web/ReactWebViewManager.cs
@@ -90,7 +90,7 @@ namespace ReactNative.Views.Web
         /// <param name="view">A webview instance.</param>
         /// <param name="source">A source for the webview (either static html or an uri).</param>
         [ReactProp("source")]
-        public async void SetSource(WebBrowser view, JObject source)
+        public void SetSource(WebBrowser view, JObject source)
         {
             if (source != null)
             {
