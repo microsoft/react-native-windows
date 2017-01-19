65e10446d67cc2b6dd6b1843e86d5315d9a2c088
Kevin VanGelder <kevin@infinite.red>
2017-01-19 12:41:42 -0800

    Fix Issue With Bundle Images (#950)
    * Use correct URI with bundle images in WPF

--- local-cli/generator-wpf/templates/src/AppReactPage.cs
+++ local-cli/generator-wpf/templates/src/AppReactPage.cs
@@ -1,6 +1,7 @@
 ﻿using ReactNative;
 using ReactNative.Modules.Core;
 using ReactNative.Shell;
+using System;
 using System.Collections.Generic;
 
 namespace <%= ns %>
@@ -10,13 +11,7 @@ namespace <%= ns %>
         public override string MainComponentName => "<%= name %>";
 
 #if BUNDLE
-        public override string JavaScriptBundleFile
-        {
-            get
-            {
-                return "ms-appx:///ReactAssets/index.windows.bundle";
-            }
-        }
+        public override string JavaScriptBundleFile => AppDomain.CurrentDomain.BaseDirectory + "ReactAssets/index.windows.bundle";
 #endif
 
         public override List<IReactPackage> Packages => new List<IReactPackage>
