e9ca8f153157b5545ec5516833308846a9824ece
Eric Rozell <erozell@outlook.com>
2017-02-07 12:38:46 -0500

    feat(Yoga): Adds support for Yoga flexbox engine (#941)
    Thanks to the great work by @pre10der89 and @mpodwysocki, there will soon be a NuGet package available for Facebook.Yoga. This changeset includes all the necessary modifications to integrate React Native for UWP and WPF with the Yoga flexbox engine.

---
+++ ReactWindows/ReactNative.Shared/UIManager/ReactShadowNodeExtensions.cs
@@ -0,0 +1,92 @@
+﻿using Facebook.Yoga;
+
+namespace ReactNative.UIManager
+{
+    static class ReactShadowNodeExtensions
+    {
+        public static float GetLeftBorderWidth(this ReactShadowNode node)
+        {
+            var width = node.GetBorder(EdgeSpacing.Left);
+            if (!YogaConstants.IsUndefined(width))
+            {
+                return width;
+            }
+
+            width = node.GetBorder(EdgeSpacing.Horizontal);
+            if (!YogaConstants.IsUndefined(width))
+            {
+                return width;
+            }
+
+            width = node.GetBorder(EdgeSpacing.All);
+            if (!YogaConstants.IsUndefined(width))
+            {
+                return width;
+            }
+
+            return 0.0f;
+        }
+
+        public static float GetPaddingValue(this ReactShadowNode node, int spacingType)
+        {
+            var padding = node.GetPadding(spacingType);
+            if (!YogaConstants.IsUndefined(padding))
+            {
+                return padding;
+            }
+
+            if (spacingType == EdgeSpacing.Left || spacingType == EdgeSpacing.Right)
+            {
+                padding = node.GetPadding(EdgeSpacing.Horizontal);
+            }
+
+            if (!YogaConstants.IsUndefined(padding))
+            {
+                return padding;
+            }
+
+            if (spacingType == EdgeSpacing.Top || spacingType == EdgeSpacing.Bottom)
+            {
+                padding = node.GetPadding(EdgeSpacing.Vertical);
+            }
+
+            if (!YogaConstants.IsUndefined(padding))
+            {
+                return padding;
+            }
+
+            return node.GetPadding(EdgeSpacing.All);
+        }
+
+        public static float GetPaddingSpace(this ReactShadowNode node, int spacingType)
+        {
+            var padding = node.GetPaddingValue(spacingType);
+            return YogaConstants.IsUndefined(padding)
+                ? 0.0f
+                : padding;
+        }
+
+        public static float GetTopBorderWidth(this ReactShadowNode node)
+        {
+            var width = node.GetBorder(EdgeSpacing.Top);
+            if (!YogaConstants.IsUndefined(width))
+            {
+                return width;
+            }
+
+            width = node.GetBorder(EdgeSpacing.Vertical);
+            if (!YogaConstants.IsUndefined(width))
+            {
+                return width;
+            }
+
+            width = node.GetBorder(EdgeSpacing.All);
+            if (!YogaConstants.IsUndefined(width))
+            {
+                return width;
+            }
+
+            return 0.0f;
+        }
+    }
+}
