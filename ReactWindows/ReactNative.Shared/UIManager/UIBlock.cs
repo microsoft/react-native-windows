8495837497964f3358ef02c54109f9000f62a3ec
Ryan Linton <ryan@infinite.red>
2017-03-03 13:35:33 -0800

    Add UIBlock interface

---
+++ ReactWindows/ReactNative.Shared/UIManager/UIBlock.cs
@@ -0,0 +1,17 @@
+﻿using ReactNative.UIManager;
+
+namespace ReactNative.Shared.UIManager
+{
+    /// <summary>
+    /// Interface that represents a block to execute on the UI thread.
+    /// Exposes NativeViewHierarchyManager for third party libraries.
+    /// </summary>
+    public interface UIBlock
+    {
+        /// <summary>
+        /// Executes the block.
+        /// </summary>
+        /// <param name="nativeViewHierarchyManager"></param>
+        void Execute(NativeViewHierarchyManager nativeViewHierarchyManager);
+    }
+}
