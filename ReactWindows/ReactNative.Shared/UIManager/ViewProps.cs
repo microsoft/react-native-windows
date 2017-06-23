c41518d1009b3e7200cf61c78ec6fa0b824b7f7b
Eric Rozell <erozell@outlook.com>
2017-06-23 14:34:53 +0000

    fix(ViewProps): Update layout-only view props (#1153)
    * With these view props missing from the layout-only list, extra views are being created that are truly layout-only.
    * Views with `pointerEvents` prop values of `box-none` and `auto` can be collapsed.

--- ReactWindows/ReactNative.Shared/UIManager/ViewProps.cs
+++ ReactWindows/ReactNative.Shared/UIManager/ViewProps.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
 namespace ReactNative.UIManager
 {
@@ -56,6 +57,9 @@ namespace ReactNative.UIManager
         public const string MaxHeight = "maxHeight";
 
         public const string AspectRatio = "aspectRatio";
+
+        // Props that sometimes may prevent us from collapsing views
+        public static string PointerEvents = "pointerEvents";
 
         // Properties that affect more than just layout
         public const string Disabled = "disabled";
@@ -138,10 +142,15 @@ namespace ReactNative.UIManager
                 AlignSelf,
                 Collapsible,
                 Flex,
+                FlexBasis,
                 FlexDirection,
+                FlexGrow,
+                FlexShrink,
                 FlexWrap,
                 JustifyContent,
                 Overflow,
+                AlignContent,
+                Display,
 
                 /* position */
                 Position,
@@ -180,13 +189,24 @@ namespace ReactNative.UIManager
         /// <summary>
         /// Checks if the property key is layout-only.
         /// </summary>
-        /// <param name="key">The key.</param>
+        /// <param name="props">The prop collection.</param>
+        /// <param name="prop">The prop name.</param>
         /// <returns>
         /// <b>true</b> if the property is layout-only, <b>false</b> otherwise.
         /// </returns>
-        public static bool IsLayoutOnly(string key)
+        public static bool IsLayoutOnly(ReactStylesDiffMap props, string prop)
         {
-            return s_layoutOnlyProperties.Contains(key);
+            if (s_layoutOnlyProperties.Contains(prop))
+            {
+                return true;
+            }
+            else if (PointerEvents == prop)
+            {
+                var value = props.GetProperty(prop).Value<string>();
+                return value == "auto" || value == "box-none";
+            }
+
+            return false;
         }
     }
 }
