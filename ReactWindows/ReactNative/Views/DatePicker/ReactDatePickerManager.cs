21ac947568ea369c3fbb08cbb26e6c644d1ff7d5
Diego Martín <diegomartinpi@gmail.com>
2017-07-05 21:10:20 +0200

    Native code for DatePicker (#230)
    Implements the basic native functionality for DatePicker (Still no support
    for Time or Calendar pickers).

---
+++ ReactWindows/ReactNative/Views/DatePicker/ReactDatePickerManager.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+using ReactNative.UIManager;
+using ReactNative.UIManager.Annotations;
+using ReactNative.UIManager.Events;
+using Newtonsoft.Json.Linq;
+
+namespace ReactNative.Views.DatePicker
+{
+    /// <summary>
+    /// A view manager responsible for rendering a DatePicker
+    /// </summary>
+    public class ReactDatePickerManager : BaseViewManager<Windows.UI.Xaml.Controls.DatePicker, ReactDatePickerShadowNode>
+    {
+        /// <summary>
+        /// The name of the view manager.
+        /// </summary>
+        public override string Name
+        {
+            get
+            {
+                return "RCTDatePicker";
+            }
+        }
+
+        /// <summary>
+        /// Sets the value of the picker
+        /// </summary>
+        /// <param name="view">The picker view element.</param>
+        /// <param name="date">The new value.</param>
+        [ReactProp("date")]
+        public void SetDate(Windows.UI.Xaml.Controls.DatePicker view, DateTime date)
+        {
+            view.Date = date;
+        }
+
+        /// <summary>
+        /// Sets the maximum allowed date of the picker.
+        /// </summary>
+        /// <param name="view">The picker view element.</param>
+        /// <param name="date">The value to set as maximum.</param>
+        [ReactProp("maximumDate")]
+        public void SetMaximumDate(Windows.UI.Xaml.Controls.DatePicker view, DateTime date)
+        {
+            view.MaxYear = date;
+        }
+
+        /// <summary>
+        /// Sets the minimum allowed date of the picker.
+        /// </summary>
+        /// <param name="view">The picker view element.</param>
+        /// <param name="date">The value to set as minimum.</param>
+        [ReactProp("minimumDate")]
+        public void SetMinimumDate(Windows.UI.Xaml.Controls.DatePicker view, DateTime date)
+        {
+            view.MinYear = date;
+        }
+
+        /// <summary>
+        /// This method should return the <see cref="ReactDatePickerShadowNode"/>
+        /// which will be then used for measuring the position and size of the
+        /// view.
+        /// </summary>
+        /// <returns>The shadow node instance.</returns>
+        public override ReactDatePickerShadowNode CreateShadowNodeInstance()
+        {
+            return new ReactDatePickerShadowNode();
+        }
+
+        /// <summary>
+        /// Implement this method to receive optional extra data enqueued from
+        /// the corresponding instance of <see cref="ReactShadowNode"/> in
+        /// <see cref="ReactShadowNode.OnCollectExtraUpdates"/>.
+        /// </summary>
+        /// <param name="root">The root view.</param>
+        /// <param name="extraData">The extra data.</param>
+        public override void UpdateExtraData(Windows.UI.Xaml.Controls.DatePicker root, object extraData)
+        {
+        }
+
+        /// <summary>
+        /// Unbind event handlers
+        /// </summary>
+        /// <param name="reactContext">The React context.</param>
+        /// <param name="view">The view.</param>
+        public override void OnDropViewInstance(ThemedReactContext reactContext, Windows.UI.Xaml.Controls.DatePicker view)
+        {
+            base.OnDropViewInstance(reactContext, view);
+            view.DateChanged -= OnDateChanged;
+        }
+
+        /// <summary>
+        /// Creates a new view instance of type <see cref="Windows.UI.Xaml.Controls.DatePicker"/>.
+        /// </summary>
+        /// <param name="reactContext">The React context.</param>
+        /// <returns>The view instance.</returns>
+        protected override Windows.UI.Xaml.Controls.DatePicker CreateViewInstance(ThemedReactContext reactContext)
+        {
+            Windows.UI.Xaml.Controls.DatePicker picker = new Windows.UI.Xaml.Controls.DatePicker();
+            return picker;
+        }
+
+        /// <summary>
+        /// Binds the <see cref="Windows.UI.Xaml.Controls.DatePicker.DateChanged"/> event to a handler
+        /// </summary>
+        /// <param name="reactContext">The react context</param>
+        /// <param name="view">The view instance</param>
+        protected override void AddEventEmitters(ThemedReactContext reactContext, Windows.UI.Xaml.Controls.DatePicker view)
+        {
+            base.AddEventEmitters(reactContext, view);
+            view.DateChanged += OnDateChanged;
+        }
+
+        /// <summary>
+        /// Date changed update handler
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event.</param>
+        private void OnDateChanged(object sender, DatePickerValueChangedEventArgs e)
+        {
+            var datePicker = (Windows.UI.Xaml.Controls.DatePicker)sender;
+            DateTimeOffset newDate = e.NewDate;
+
+            datePicker.GetReactContext().GetNativeModule<UIManagerModule>()
+                .EventDispatcher
+                .DispatchEvent(new ReactDatePickerEvent(datePicker.GetTag(), e.NewDate.Date));
+        }
+
+        /// <summary>
+        /// A DatePicker-specific event to communicate with JavaScript
+        /// </summary>
+        class ReactDatePickerEvent : Event
+        {
+            private readonly DateTimeOffset _date;
+            /// <summary>
+            /// Creates an instance of the event
+            /// </summary>
+            /// <param name="viewTag">The viewtag of the instantiating view.</param>
+            /// <param name="date">Date to include in the notification payload.</param>
+            public ReactDatePickerEvent(int viewTag, DateTimeOffset date) :
+                base(viewTag, TimeSpan.FromTicks(Environment.TickCount))
+            {
+                _date = date;
+            }
+
+            /// <summary>
+            /// Internal name of the event
+            /// </summary>
+            public override string EventName
+            {
+                get
+                {
+                    return "topChange";
+                }
+            }
+
+            /// <summary>
+            /// Dispatches the event through the JavaScript bridge
+            /// </summary>
+            /// <param name="eventEmitter"></param>
+            public override void Dispatch(RCTEventEmitter eventEmitter)
+            {
+                JObject eventData = new JObject
+                {
+                    {"date", _date },
+                };
+                eventEmitter.receiveEvent(ViewTag, EventName, eventData);
+            }
+        }
+    }
+}
---
+++ ReactWindows/ReactNative/Views/DatePicker/ReactDatePickerShadowNode.cs
@@ -0,0 +1,24 @@
+﻿using Facebook.Yoga;
+using ReactNative.UIManager;
+
+namespace ReactNative.Views.DatePicker
+{
+    /// <summary>
+    /// The shadow node implementation for DatePicker views.
+    /// </summary>
+    public class ReactDatePickerShadowNode : LayoutShadowNode
+    {
+        /// <summary>
+        /// INstantiates the <see cref="ReactDatePickerShadowNode"/> 
+        /// </summary>
+        public ReactDatePickerShadowNode()
+        {
+            MeasureFunction = MeasurePicker;
+        }
+
+        private static YogaSize MeasurePicker(YogaNode node, float width, YogaMeasureMode widthMode, float height, YogaMeasureMode heightMode)
+        {
+            return MeasureOutput.Make(width, 40f);
+        }
+    }
+}
