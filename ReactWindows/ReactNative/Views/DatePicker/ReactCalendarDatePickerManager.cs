9d7ef9ae7e6e26c872bd0202a159bda1a8575738
Diego Martín <diegomartinpi@gmail.com>
2017-07-08 20:25:45 +0200

    Minor refactor

--- ReactWindows/ReactNative/Views/DatePicker/ReactCalendarDatePickerManager.cs
+++ ReactWindows/ReactNative/Views/DatePicker/ReactCalendarDatePickerManager.cs
@@ -12,7 +12,7 @@ namespace ReactNative.Views.DatePicker
     /// <summary>
     /// A view manager responsible for rendering a CalendarDatePicker
     /// </summary>
-    public class ReactCalendarDatePickerManager : BaseViewManager<Windows.UI.Xaml.Controls.CalendarDatePicker, ReactCalendarDatePickerShadowNode>
+    public class ReactCalendarDatePickerManager : BaseViewManager<CalendarDatePicker, ReactCalendarDatePickerShadowNode>
     {
         /// <summary>
         /// The name of the view manager.
@@ -31,15 +31,12 @@ namespace ReactNative.Views.DatePicker
         /// <param name="view">The picker view element.</param>
         /// <param name="date">The new value.</param>
         [ReactProp("date")]
-        public void SetDate(Windows.UI.Xaml.Controls.CalendarDatePicker view, DateTime? date)
+        public void SetDate(CalendarDatePicker view, DateTime? date)
         {
             if (date.HasValue)
             {
                 view.Date = date.Value;
             }
-            /*
-            view.DayOfWeekFormat;
-            view.FirstDayOfWeek;*/    
         }
 
 
@@ -49,7 +46,7 @@ namespace ReactNative.Views.DatePicker
         /// <param name="view">The picker view element.</param>
         /// <param name="date">The value to set as maximum.</param>
         [ReactProp("maxDate")]
-        public void SetMaxDate(Windows.UI.Xaml.Controls.CalendarDatePicker view, DateTime? date)
+        public void SetMaxDate(CalendarDatePicker view, DateTime? date)
         {
             if (date.HasValue)
             {
@@ -63,7 +60,7 @@ namespace ReactNative.Views.DatePicker
         /// <param name="view">The picker view element.</param>
         /// <param name="date">The value to set as minimum.</param>
         [ReactProp("minDate")]
-        public void SetMinDate(Windows.UI.Xaml.Controls.CalendarDatePicker view, DateTime? date)
+        public void SetMinDate(CalendarDatePicker view, DateTime? date)
         {
             if (date.HasValue)
             {
@@ -77,7 +74,7 @@ namespace ReactNative.Views.DatePicker
         /// <param name="view">The picker view element.</param>
         /// <param name="placeholderText"></param>
         [ReactProp("placeholderText")]
-        public void SetPlaceholderText(Windows.UI.Xaml.Controls.CalendarDatePicker view, string placeholderText)
+        public void SetPlaceholderText(CalendarDatePicker view, string placeholderText)
         {
             view.PlaceholderText = placeholderText;
         }
@@ -88,7 +85,7 @@ namespace ReactNative.Views.DatePicker
         /// <param name="view">The picker view element.</param>
         /// <param name="identifier">The identifier, which must be one of the values defined in <see cref="Windows.Globalization.CalendarIdentifiers"/></param>
         [ReactProp("calendarIdentifier")]
-        public void SetCalendarIdentifier(Windows.UI.Xaml.Controls.CalendarDatePicker view, string identifier)
+        public void SetCalendarIdentifier(CalendarDatePicker view, string identifier)
         {
             if (identifier.Length > 0)
             {
@@ -102,7 +99,7 @@ namespace ReactNative.Views.DatePicker
         /// <param name="view">The picker instance.</param>
         /// <param name="day">The day.</param>
         [ReactProp("firstDayOfWeek")]
-        public void SetFirstDayOfWeek(Windows.UI.Xaml.Controls.CalendarDatePicker view, int day)
+        public void SetFirstDayOfWeek(CalendarDatePicker view, int day)
         {
             view.FirstDayOfWeek = (Windows.Globalization.DayOfWeek)day;
         }
@@ -125,7 +122,7 @@ namespace ReactNative.Views.DatePicker
         /// </summary>
         /// <param name="root">The root view.</param>
         /// <param name="extraData">The extra data.</param>
-        public override void UpdateExtraData(Windows.UI.Xaml.Controls.CalendarDatePicker root, object extraData)
+        public override void UpdateExtraData(CalendarDatePicker root, object extraData)
         {
         }
 
@@ -134,29 +131,29 @@ namespace ReactNative.Views.DatePicker
         /// </summary>
         /// <param name="reactContext">The React context.</param>
         /// <param name="view">The view.</param>
-        public override void OnDropViewInstance(ThemedReactContext reactContext, Windows.UI.Xaml.Controls.CalendarDatePicker view)
+        public override void OnDropViewInstance(ThemedReactContext reactContext, CalendarDatePicker view)
         {
             base.OnDropViewInstance(reactContext, view);
             view.DateChanged -= OnDateChanged;
         }
 
         /// <summary>
-        /// Creates a new view instance of type <see cref="Windows.UI.Xaml.Controls.CalendarDatePicker"/>.
+        /// Creates a new view instance of type <see cref="CalendarDatePicker"/>.
         /// </summary>
         /// <param name="reactContext">The React context.</param>
         /// <returns>The view instance.</returns>
-        protected override Windows.UI.Xaml.Controls.CalendarDatePicker CreateViewInstance(ThemedReactContext reactContext)
+        protected override CalendarDatePicker CreateViewInstance(ThemedReactContext reactContext)
         {
-            Windows.UI.Xaml.Controls.CalendarDatePicker picker = new Windows.UI.Xaml.Controls.CalendarDatePicker();
+            CalendarDatePicker picker = new CalendarDatePicker();
             return picker;
         }
 
         /// <summary>
-        /// Binds the <see cref="Windows.UI.Xaml.Controls.CalendarDatePicker.DateChanged"/> event to a handler
+        /// Binds the <see cref="CalendarDatePicker.DateChanged"/> event to a handler
         /// </summary>
         /// <param name="reactContext">The react context</param>
         /// <param name="view">The view instance</param>
-        protected override void AddEventEmitters(ThemedReactContext reactContext, Windows.UI.Xaml.Controls.CalendarDatePicker view)
+        protected override void AddEventEmitters(ThemedReactContext reactContext, CalendarDatePicker view)
         {
             base.AddEventEmitters(reactContext, view);
             view.DateChanged += OnDateChanged;
@@ -169,7 +166,7 @@ namespace ReactNative.Views.DatePicker
         /// <param name="e">The event.</param>
         private void OnDateChanged(object sender, CalendarDatePickerDateChangedEventArgs e)
         {
-            var datePicker = (Windows.UI.Xaml.Controls.CalendarDatePicker)sender;
+            var datePicker = (CalendarDatePicker)sender;
             if (!e.NewDate.HasValue)
             {
                 return;
