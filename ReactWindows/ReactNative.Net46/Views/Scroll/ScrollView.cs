992b0bfeaa0190510c9a9ec0bbf3cf88897325a1
Kudo Chien <kudo@cloudmosa.com>
2017-08-29 12:30:01 +0800

    Remove more unused using

--- ReactWindows/ReactNative.Net46/Views/Scroll/ScrollView.cs
+++ ReactWindows/ReactNative.Net46/Views/Scroll/ScrollView.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Controls;
+﻿using System.Windows.Controls;
 
 namespace ReactNative.Views.Scroll
 {
