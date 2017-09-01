5daf0c1f526fa488cb4728d8adfcd4e09fd5fba9
Eric Rozell <erozell@outlook.com>
2017-09-01 17:48:21 -0400

    feat(WebView): Adds `postMessage` callback and `injectJavaScript` command
    Allows communication between the React Native Web View and the React Native JS runtime with the WebView bridge.
    
    Fixes #987

---
+++ ReactWindows/ReactNative/Views/Web/Events/MessageEvent.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json.Linq;
+using ReactNative.UIManager.Events;
+using System;
+
+namespace ReactNative.Views.Web.Events
+{
+    class MessageEvent : Event
+    {
+        private readonly string _data;
+
+        public MessageEvent(int viewTag, string data)
+            : base(viewTag)
+        {
+            _data = data;
+        }
+
+        public override string EventName
+        {
+            get
+            {
+                return "topMessage";
+            }
+        }
+
+        public override bool CanCoalesce
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public override void Dispatch(RCTEventEmitter eventEmitter)
+        {
+            eventEmitter.receiveEvent(ViewTag, EventName, new JObject
+            {
+                { "data", _data },
+            });
+        }
+    }
+}
