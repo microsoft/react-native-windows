e0fe9a8830470a6ef8c22f9261e8bb6c5eb9d0a0
Eric Rozell <erozell@outlook.com>
2017-10-05 15:16:49 -0400

    feat(PlatformConstants): Adding native platform constants module
    PlatformConstants is needed as of React Native v0.49 to implement a version check that provides guardrails against running an incompatible JavaScript bundle against a given native build.

---
+++ ReactWindows/ReactNative.Shared/Modules/SystemInfo/PlatformConstantsModule.cs
@@ -0,0 +1,36 @@
+﻿using ReactNative.Bridge;
+using System.Collections.Generic;
+
+namespace ReactNative.Modules.SystemInfo
+{
+    /// <summary>
+    /// Module that exposes platform constants to JavaScript.
+    /// </summary>
+    public class PlatformConstantsModule : NativeModuleBase
+    {
+        /// <summary>
+        /// The native module name.
+        /// </summary>
+        public override string Name
+        {
+            get
+            {
+                return "PlatformConstants";
+            }
+        }
+
+        /// <summary>
+        /// The native module constants.
+        /// </summary>
+        public override IReadOnlyDictionary<string, object> Constants
+        {
+            get
+            {
+                return new Dictionary<string, object>
+                {
+                    { "reactNativeVersion", ReactNativeVersion.Version },
+                };
+            }
+        }
+    }
+}
