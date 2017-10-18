091c5b88820e944e6655254cdad53586406e7e2a
Rami Sayar <rami.sayar@gmail.com>
2017-10-18 14:40:01 -0400

    refactor(rnpm): Removed duplicate code in rnpm-plugin-wpf.
    Removed duplicate code in rnpm-plugin-wpf by depending on rnpm-plugin-windows.
    Matched version of rnpm-plugin-wpf to rnpm-plugin-windows.
    Updated react-native peer dependencies.

--- local-cli/rnpm/wpf/index.js
+++ local-cli/rnpm/wpf/index.js
@@ -1,16 +1,5 @@
-module.exports = {
-  func: require('./src/wpf'),
-  description: 'Generate React Native Windows template project on WPF',
-  name: 'wpf [name]',
-  options: [{
-    command: '--windowsVersion [version]',
-    description: 'The version of react-native-wpf to use.'
-  }, {
-    command: '--namespace [namespace]',
-    description: 'The native project namespace.'
-  }, {
-    command: '--verbose',
-    description: 'Enables logging',
-    default: false,
-  }]
-};
+const rnpmPlugin = require('rnpm-plugin-windows');
+
+module.exports = [
+  rnpmPlugin[1] // WPF
+]
