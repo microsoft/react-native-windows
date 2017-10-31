74a1310ca49f20f03ec14c081df7cdace0eaa503
Eric Rozell <erozell@outlook.com>
2017-10-31 14:59:24 -0400

    Revert "feat(HighContrast): Expose the XAML HighContrastChanged event to JS (#1443)"
    This reverts commit d4a357d06fdc281bf32f19d846311297fb04d3f5.

--- Libraries/Components/AccessibilityInfo/AccessibilityInfo.windows.js
+++ Libraries/Components/AccessibilityInfo/AccessibilityInfo.windows.js
@@ -1,4 +1,4 @@
-﻿/**
+/**
  * Copyright (c) 2015-present, Facebook, Inc.
  * All rights reserved.
  *
@@ -11,55 +11,32 @@
  */
 'use strict';
 
-var NativeModules = require('NativeModules');
-var RCTDeviceEventEmitter = require('RCTDeviceEventEmitter');
-
-var RCTAccessibilityInfo = NativeModules.AccessibilityInfo;
-var HIGH_CONTRAST_EVENT = 'highContrastDidChange';
-var _subscriptions = new Map();
-
-type AccessibilityEventName = $Enum<{
-  [HIGH_CONTRAST_EVENT]: string,
+type ChangeEventName = $Enum<{
+  change: string,
 }>;
+
+var warning = require('fbjs/lib/warning');
 
 var AccessibilityInfo = {
 
-  initialHighContrast: RCTAccessibilityInfo.initialHighContrast,
-  
-  fetch: function(): Promise {
+  fetch: function(): Promise<*> {
     return new Promise((resolve, reject) => {
-      resolve(false); // UWP does not have API to check if screen reader is being used
+      reject('AccessibilityInfo is not supported on this platform.');
     });
   },
 
-  fetchIsHighContrast: function (): Promise {
-    return RCTAccessibilityInfo.fetchIsHighContrast();
-  },  
-
   addEventListener: function (
-    eventName: AccessibilityEventName,
+    eventName: ChangeEventName,
     handler: Function
-  ): Object {
-    if (eventName !== HIGH_CONTRAST_EVENT) {
-      return {
-        remove() { }
-      };      
-    }
-
-    var listener = RCTDeviceEventEmitter.addListener(eventName, enabled => handler(enabled));
-
-    _subscriptions.set(handler, listener);
-
-    return listener;
+  ): void {
+    warning(false, 'AccessibilityInfo is not supported on this platform.');
   },
 
   removeEventListener: function(
-    eventName: AccessibilityEventName,
+    eventName: ChangeEventName,
     handler: Function
   ): void {
-    var listener = _subscriptions.get(handler);
-    listener && listener.remove();
-    _subscriptions.delete(handler);  
+    warning(false, 'AccessibilityInfo is not supported on this platform.');
   },
 
 };
