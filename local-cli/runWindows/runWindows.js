693819371a35100424d3f6544ebac70f7635abb0
Artem Egorov <v-aregor@microsoft.com>
2018-01-23 19:39:43 +0300

    Added --no-packager flag

--- local-cli/runWindows/runWindows.js
+++ local-cli/runWindows/runWindows.js
@@ -41,7 +41,7 @@ function runWindows(config, args, options) {
         return deploy.deployToDevice(options);
       } else {
         return deploy.deployToDesktop(options);
-      }
+      }   
     })
     .catch(e => console.error(chalk.red(`Failed to deploy: ${e.message}`)));
 }
@@ -69,6 +69,7 @@ runWindows({
  *    device: Boolean - Deploy to a device
  *    target: String - Device GUID to deploy to
  *    proxy: Boolean - Run using remote JS proxy
+ *    no-packager: String - Do not launch packager while building
  */
 module.exports = {
   name: 'run-windows',
@@ -100,5 +101,8 @@ module.exports = {
     command: '--verbose',
     description: 'Enables logging',
     default: false,
+  }, {
+    command: '--no-packager',
+    description: 'Do not launch packager while building'
   }]
 };
