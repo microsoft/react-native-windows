4bbdf3fd495d1928b1d6ac0450d425ed6ada7ef9
Eric Rozell <erozell@outlook.com>
2018-01-25 14:39:04 -0500

    fix(CLI): Adds CLI config and link plugin
    There is a pending change against the RN CLI to allow platform plugins to provide their own configuration and, optionally, plug into the `link` and `unlink` commands.
    
    In terms of net new changes, the only files that need to be reviewed are:
    - local-cli/link/index.js
    - local-cli/platform.js
    - package.json
    
    All other files are copy/paste from React Native.

---
+++ local-cli/core/windows/findProjectWithFilePattern.js
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+'use strict';
+
+const fs = require('fs');
+const glob = require('glob');
+const path = require('path');
+
+/**
+ * Find an C# project file
+ *
+ * @param {String} folder Name of the folder where to seek
+ * @return {String}
+ */
+module.exports = function findProjectWithFilePattern(folder, pattern) {
+  const csprojPaths = glob.sync(path.join('**', '*.csproj'), {
+    cwd: folder,
+    ignore: ['node_modules/**', '**/build/**', 'Examples/**', 'examples/**', '**/bin/**', '**/obj/**' ],
+  });
+
+  // Search all folders with *.csproj files for one that exports an `IReactPackage` 
+  for (var csprojPath of csprojPaths) {
+    const csprojFullPath = path.join(folder, csprojPath);
+    const csprojDir = path.dirname(csprojFullPath);
+    const files = glob.sync(path.join('**', '*.cs'), {
+      cwd: csprojDir,
+      ignore: ['node_modules/**', '**/build/**', 'Examples/**', 'examples/**', '**/bin/**', '**/obj/**' ],
+    });
+
+    const packages = files
+      .map(filePath => fs.readFileSync(path.join(csprojDir, filePath), 'utf8'))
+      .map(file => file.match(pattern))
+      .filter(match => match);
+
+    if (packages.length) {
+      return csprojFullPath;
+    }
+  }
+
+  return null;
+};
