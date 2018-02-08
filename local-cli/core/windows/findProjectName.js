a1a23c33e3efb4e366b4c16de6cd40fd76a45459
Eric Rozell <erozell@outlook.com>
2018-02-08 08:47:36 -0500

    fix(CLI): Adds CLI config and link plugin (#1601)
    There is a pending change against the RN CLI to allow platform plugins to provide their own configuration and, optionally, plug into the `link` and `unlink` commands.
    
    In terms of net new changes, the only files that need to be reviewed are:
    - local-cli/link/index.js
    - local-cli/platform.js
    - package.json
    
    All other files are copy/paste from React Native.

---
+++ local-cli/core/windows/findProjectName.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+'use strict';
+
+const fs = require('fs');
+
+/**
+ * Gets name of dependency project.
+ *
+ * @param {String} folder Path to dependency project .csproj
+ */
+module.exports = function findProjectGuid(path) {
+  const file = fs.readFileSync(path, 'utf8');
+  const result = file.match(/<AssemblyName>(.*)<\/AssemblyName>/);
+  return result ? result[1] : null;
+};
