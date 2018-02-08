4e80bd493aa0230bb6f5e145e8e0ff70aba8fa86
Alexander Bodalevsky <abodalevsky@hotmail.com>
2018-02-08 14:42:43 +0200

    Added WebSocket options parser

---
+++ ReactWindows/ReactNative.Shared/Modules/WebSocket/IWebSocketOptions.cs
@@ -0,0 +1,31 @@
+﻿namespace ReactNative.Shared.Modules.WebSocket
+{
+    /// <summary>
+    /// Represents WebSocket options
+    /// </summary>
+    internal interface IWebSocketOptions
+    {
+        /// <summary>
+        /// Gets a value indicating whether default proxy should be used for connection
+        /// </summary>
+        bool UseDefaultProxy { get; }
+
+        /// <summary>
+        /// Gets URI address of the proxy server that is used for connection.
+        /// If UseDefaultProxy is true - this parameter should be ignored
+        /// </summary>
+        string ProxyAddress { get; }
+
+        /// <summary>
+        /// Gets user name for proxy authentication.
+        /// If UseDefaultProxy is true - this parameter should be ignored
+        /// </summary>
+        string UserName { get; }
+
+        /// <summary>
+        /// Gets password for proxy authentication.
+        /// If UseDefaultProxy is true - this parameter should be ignored
+        /// </summary>        
+        string Password { get; }
+    }
+}
---
+++ ReactWindows/ReactNative.Shared/Modules/WebSocket/WebSocketOptions.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json.Linq;
+using ReactNative.Collections;
+using ReactNative.Shared.Modules.WebSocket;
+using System;
+
+namespace ReactNative.Modules.WebSocket
+{
+    internal class WebSocketOptions : IWebSocketOptions
+    {
+        private static readonly string ProxySection = "proxy";
+        private static readonly string ProxyUseDefault = "useDefault";
+        private static readonly string ProxyUri = "uri";
+        private static readonly string ProxyUser = "user";
+        private static readonly string ProxyPassword = "password";
+
+        public WebSocketOptions (JObject options)
+        {
+            if (options == null || !options.ContainsKey(ProxySection))
+            {
+                return;
+            }
+
+            var proxy = options[ProxySection];
+
+            UseDefaultProxy = (bool)(proxy.SelectToken(ProxyUseDefault, false) ?? false);
+            ProxyAddress = (string)(proxy.SelectToken(ProxyUri, false) ?? string.Empty);
+            UserName = (string)(proxy.SelectToken(ProxyUser, false) ?? string.Empty);
+            Password = (string)(proxy.SelectToken(ProxyPassword, false) ?? string.Empty);
+        }
+
+        #region IWebSocketOptions
+
+        /// <inheritdoc />
+        public bool UseDefaultProxy { get; private set; } = false;
+        /// <inheritdoc />
+        public string ProxyAddress { get; private set; } = string.Empty;
+        /// <inheritdoc />
+        public string UserName { get; private set; } = string.Empty;
+        /// <inheritdoc />
+        public string Password { get; private set; } = string.Empty;
+
+        #endregion
+    }
+}
