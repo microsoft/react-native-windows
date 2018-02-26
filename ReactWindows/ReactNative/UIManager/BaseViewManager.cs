6c68c3bf5b81dda9848fe2f353ca658888fe63b9
Radu Simionescu <reseul@hotmail.com>
2018-02-26 15:55:49 -0800

    Hardened one more dictionary for multi-windows scenarios. (#1672)
    Hardened one more dictionary for multi-windows scenarios.
    Updates The third parameter of ConcurrentDictionary.AddOrUpdate to better align with the API name (even though we don't use the "update" semantic)

--- ReactWindows/ReactNative/UIManager/BaseViewManager.cs
+++ ReactWindows/ReactNative/UIManager/BaseViewManager.cs
@@ -4,6 +4,7 @@ using ReactNative.Reflection;
 using ReactNative.Touch;
 using ReactNative.UIManager.Annotations;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using Windows.Foundation;
@@ -28,8 +29,8 @@ namespace ReactNative.UIManager
         where TFrameworkElement : FrameworkElement
         where TLayoutShadowNode : LayoutShadowNode
     {
-        private readonly IDictionary<TFrameworkElement, DimensionBoundProperties> _dimensionBoundProperties =
-            new Dictionary<TFrameworkElement, DimensionBoundProperties>();
+        private readonly ConcurrentDictionary<TFrameworkElement, DimensionBoundProperties> _dimensionBoundProperties =
+            new ConcurrentDictionary<TFrameworkElement, DimensionBoundProperties>();
 
         /// <summary>
         /// Set's the  <typeparamref name="TFrameworkElement"/> styling layout 
@@ -203,7 +204,7 @@ namespace ReactNative.UIManager
         {
             view.PointerEntered -= OnPointerEntered;
             view.PointerExited -= OnPointerExited;
-            _dimensionBoundProperties.Remove(view);
+            _dimensionBoundProperties.TryRemove(view, out _);
         }
 
         /// <summary>
@@ -292,7 +293,7 @@ namespace ReactNative.UIManager
             if (!_dimensionBoundProperties.TryGetValue(view, out properties))
             {
                 properties = new DimensionBoundProperties();
-                _dimensionBoundProperties.Add(view, properties);
+                _dimensionBoundProperties.AddOrUpdate(view, properties, (k, v) => properties);
             }
 
             return properties;
