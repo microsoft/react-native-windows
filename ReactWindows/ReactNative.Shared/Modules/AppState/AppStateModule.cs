d4cc243922215034ca3393e09faa6844acabbdad
Adam Comella <adcom@microsoft.com>
2018-03-06 10:37:55 -0800

    Fix AppState module's events for moving between BG and FG
    The AppState module fires an event to notify the app that it has moved between the background and foreground and vice versa. However, it currently fires these events based on the UWP Suspend and Resume events. These aren't the right hooks.
    
    The proper background/foreground hooks for UWP are `LeavingBackground` and `EnteringBackground`.
    
    Other changes:
    - The app state is now initialized to background to match UWP (https://docs.microsoft.com/en-us/windows/uwp/launch-resume/app-lifecycle#app-execution-state)
    - The initial state is reported synchronously to JavaScript through constants (iOS already does this, Android doesn't do it yet)

--- ReactWindows/ReactNative.Shared/Modules/AppState/AppStateModule.cs
+++ ReactWindows/ReactNative.Shared/Modules/AppState/AppStateModule.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
 using ReactNative.Bridge;
 using ReactNative.Modules.Core;
 
@@ -7,12 +8,15 @@ namespace ReactNative.Modules.AppState
     /// <summary>
     /// Native module for monitoring the state of the application.
     /// </summary>
-    public class AppStateModule : ReactContextNativeModuleBase, ILifecycleEventListener
+    public class AppStateModule : ReactContextNativeModuleBase, IBackgroundEventListener
     {
         private const string AppStateActive = "active";
         private const string AppStateBackground = "background";
 
-        private string _appState = "uninitialized";
+        // UWP apps are always activated/launched into the background. Afterwards, they
+        // may receive a `LeavingBackground` event. This information is described in
+        // https://docs.microsoft.com/en-us/windows/uwp/launch-resume/app-lifecycle#app-execution-state.
+        private string _appState = AppStateBackground;
 
         /// <summary>
         /// Instantiates the <see cref="AppStateModule"/>.
@@ -35,26 +39,40 @@ namespace ReactNative.Modules.AppState
         }
 
         /// <summary>
+        /// The constants exported by this module.
+        /// </summary>
+        public override IReadOnlyDictionary<string, object> Constants
+        {
+            get
+            {
+                return new Dictionary<string, object>
+                {
+                    { "initialAppState", _appState }
+                };
+            }
+        }
+
+        /// <summary>
         /// Initializes the module.
         /// </summary>
         public override void Initialize()
         {
-            Context.AddLifecycleEventListener(this);
+            Context.AddBackgroundEventListener(this);
         }
 
         /// <summary>
-        /// Called when the application host suspends.
+        /// Called when the host entered background mode.
         /// </summary>
-        public void OnSuspend()
+        public void OnEnteredBackground()
         {
             _appState = AppStateBackground;
             SendAppStateChangeEvent();
         }
 
         /// <summary>
-        /// Called when the application host resumes.
+        /// Called when the host is leaving background mode.
         /// </summary>
-        public void OnResume()
+        public void OnLeavingBackground()
         {
             _appState = AppStateActive;
             SendAppStateChangeEvent();
