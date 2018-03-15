dc474ee5706b69bab1202809d199303d7d150f1f
Eric Rozell <erozell@outlook.com>
2018-03-15 19:35:32 -0400

    feat(ServiceResolver): add static convenience method
    Adds method to call the service resolver directly from the static class and property to set the ServiceResolver.

--- ReactWindows/ReactNative.Shared/PlatformServices/ServiceResolver.cs
+++ ReactWindows/ReactNative.Shared/PlatformServices/ServiceResolver.cs
@@ -1,3 +1,8 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+
 namespace ReactNative.PlatformServices
 {
     /// <summary>
@@ -5,9 +10,34 @@ namespace ReactNative.PlatformServices
     /// </summary>
     public static class ServiceResolver
     {
+        private static IServiceProvider s_current = new CurrentPlatformServiceProvider();
+
         /// <summary>
         /// The current platform service provider.
         /// </summary>
-        public static IServiceProvider Instance { get; } = new CurrentPlatformServiceProvider();
+        public static IServiceProvider Current
+        {
+            get
+            {
+                return s_current;
+            }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException(nameof(value));
+
+                s_current = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets the platform-specific service instance.
+        /// </summary>
+        /// <typeparam name="T">Type of service.</typeparam>
+        /// <returns>The platform-specific instance.</returns>
+        public static T GetService<T>() where T : class
+        {
+            return s_current.GetService<T>();
+        }
     }
 }
