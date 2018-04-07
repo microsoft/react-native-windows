1e6992802610f9186a3672d9d8f840baafaac8ee
Matt Hargett <plaztiksyke@gmail.com>
2018-04-06 18:02:04 -0700

    Adjust app templates to include ReactNative.Interfaces, and include ReactNative.Interfaces in npm package.

---
+++ local-cli/generator-windows/templates/src/MainPage.cs
@@ -0,0 +1,52 @@
+﻿using ReactNative;
+using ReactNative.Modules.Core;
+using ReactNative.Shell;
+using System.Collections.Generic;
+
+namespace <%= ns %>
+{
+    class MainPage : ReactPage
+    {
+        public override string MainComponentName
+        {
+            get
+            {
+                return "<%= name %>";
+            }
+        }
+
+#if BUNDLE
+        public override string JavaScriptBundleFile
+        {
+            get
+            {
+                return "ms-appx:///ReactAssets/index.windows.bundle";
+            }
+        }
+#endif
+
+        public override List<IReactPackage> Packages
+        {
+            get
+            {
+                return new List<IReactPackage>
+                {
+                    new MainReactPackage(),
+                };
+            }
+        }
+
+        public override bool UseDeveloperSupport
+        {
+            get
+            {
+#if !BUNDLE || DEBUG
+                return true;
+#else
+                return false;
+#endif
+            }
+        }
+    }
+
+}
