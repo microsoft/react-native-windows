5c25815f04ed2dabb7bc183f3704e00b03721e64
Radu Simionescu <radus@microsoft.com>
2018-08-09 18:45:14 -0700

    missing using

--- ReactWindows/ReactNative/UIManager/UIViewOperationQueue.cs
+++ ReactWindows/ReactNative/UIManager/UIViewOperationQueue.cs
@@ -7,6 +7,7 @@ using ReactNative.Modules.Core;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Windows.ApplicationModel.Core;
