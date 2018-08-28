453d3bcc33bd628d1d873e47f3648a33b58ab9ef
George Mangov <gmango@microsoft.com>
2018-08-27 18:18:03 -0700

    More tests.

--- ReactWindows/ReactNative.Tests/Views/Text/ReactTextViewManagerTests.cs
+++ ReactWindows/ReactNative.Tests/Views/Text/ReactTextViewManagerTests.cs
@@ -26,6 +26,32 @@ namespace ReactNative.Tests.Views.Text
             Assert.AreEqual(text, "test");
         }
 
+        [UTFAC.UITestMethod]
+        public void ReactTextViewManager_Selection_SpanText()
+        {
+            // <Span><Run>Sample test text.</Run><Run>Second text.</Run></Span><Run>Third text.</Run><Span><Run>Fourth text.</Run></Span>
+            Span span = new Span();
+            Run run = new Run();
+            run.Text = "Sample test text.";
+            span.Inlines.Add(run);
+            Run run1 = new Run();
+            run1.Text = "Second text.";
+            span.Inlines.Add(run1);
+            Run run2 = new Run();
+            run2.Text = "Third text.";
+            Span span1 = new Span();
+            Run run3 = new Run();
+            run3.Text = "Fourth text.";
+            span1.Inlines.Add(run3);
+
+            RichTextBlock rtb = CreateWithInlines(new Inline[] { span, run2, span1 });
+            Paragraph paragraph = rtb.Blocks.First() as Paragraph;
+            TextPointer start = paragraph.ContentStart.GetPositionAtOffset(50, LogicalDirection.Forward);
+            TextPointer end = paragraph.ContentStart.GetPositionAtOffset(54, LogicalDirection.Forward);
+            var text = ReactTextViewManager.GetStringByStartAndEndPointers(rtb, start, end);
+            Assert.AreEqual(text, "Four");
+        }
+
         private RichTextBlock CreateWithInlines(Inline[] inlines)
         {
             RichTextBlock rtb = new RichTextBlock();
