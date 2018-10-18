9d6905a7d0b1ce9ccd66896635027acdc1a59a0a
Eric Rozell <erozell@outlook.com>
2018-10-17 18:14:54 -0700

    Simple text, i.e., text with only a single raw text node as a child (or an array with one raw text child), can be transformed into a TextBlock with a Text property, instead of a RichTextBlock with a Paragraph of Inlines. This change detects in the React Native Text node has a single raw text child/array element, and if so, uses the RCTSimpleText native component.
    Fixes #1252

--- ReactWindows/ReactNative/Shell/MainReactPackage.cs
+++ ReactWindows/ReactNative/Shell/MainReactPackage.cs
@@ -83,6 +83,7 @@ namespace ReactNative.Shell
         {
             return new List<IViewManager>
             {
+                new ReactSimpleTextViewManager(),
                 new ReactFlipViewManager(),
                 new ReactImageManager(),
                 new ReactProgressBarViewManager(),
