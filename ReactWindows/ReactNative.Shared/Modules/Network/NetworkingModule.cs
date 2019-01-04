79c33c3f4375a3f566e68bffd1586a4340a39158
Shrinath Gupta <shrinath161@gmail.com>
2019-01-05 00:29:07 +0530

    fix(NetworkingModule): Custom handling for Authorization headers
    Authorization headers are being parsed incorrectly when added via the `HttpRequestHeaderCollection.Add` method. Instead, in cases where the Authorization header is composed of two parts delimited by a space (i.e., a scheme and a token), we can set the Authorization header directly. 
    
    Fixes #2040, #782, #1542

--- ReactWindows/ReactNative.Shared/Modules/Network/NetworkingModule.cs
+++ ReactWindows/ReactNative.Shared/Modules/Network/NetworkingModule.cs
@@ -485,6 +485,20 @@ namespace ReactNative.Modules.Network
                 var key = header[0];
                 switch (key.ToLowerInvariant())
                 {
+#if WINDOWS_UWP
+                    case "authorization":
+                        var authParts = header[1].Trim().Split(new[] { ' ' }, 2);
+                        if (authParts.Length == 2)
+                        {
+                            request.Headers.Authorization = new HttpCredentialsHeaderValue(authParts[0].Trim(), authParts[1].Trim());
+                        }
+                        else
+                        {
+                            request.Headers.Add(key, header[1]);
+                        }
+                        
+                        break;
+#endif
                     case "content-encoding":
                     case "content-length":
                     case "content-type":
