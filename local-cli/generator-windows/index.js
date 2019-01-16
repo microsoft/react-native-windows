249f70253db7f68812d55bd6f89cbc3e04f4bd74
in4margaret <in4margaret@gmail.com>
2019-01-16 15:51:33 -0500

    Refactored common function into generator-common

--- local-cli/generator-windows/index.js
+++ local-cli/generator-windows/index.js
@@ -1,51 +1,19 @@
 'use strict';
-const fs = require('fs');
 const chalk = require('chalk');
 const path = require('path');
-const copyAndReplace = require('react-native-local-cli/util/copyAndReplace');
-const walk = require('react-native-local-cli/util/walk');
-const prompt = require('react-native-local-cli/generator/promptSync')();
 const username = require('username');
 const uuid = require('uuid');
 const childProcess = require('child_process');
 const os = require('os');
+const {
+  createDir,
+  copyAndReplaceAll,
+  copyAndReplaceWithChangedCallback
+} = require('../generator-common');
 
 const windowsDir = 'windows';
 const reactAssetsDir = 'ReactAssets';
 const projDir = 'proj';
-
-function createDir(destPath) {
-  if (!fs.existsSync(destPath)) {
-    fs.mkdirSync(destPath);
-  }
-}
-
-function copyAndReplaceWithChangedCallback(srcPath, destRoot, relativeDestPath, replacements) {
-  if (!replacements) {
-    replacements = {};
-  }
-  const contentChangedCallback = (_, contentChanged) =>
-    upgradeFileContentChangedCallback(
-      srcPath,
-      relativeDestPath,
-      contentChanged
-    );
-
-  copyAndReplace(
-    srcPath,
-    path.join(destRoot, relativeDestPath),
-    replacements,
-    contentChangedCallback
-  );
-}
-
-function copyAndReplaceAll(srcPath, destPath, relativeDestDir, replacements) {
-  walk(srcPath).forEach(absoluteSrcFilePath => {
-    const filename = path.relative(srcPath, absoluteSrcFilePath);
-    const relativeDestPath = path.join(relativeDestDir, filename);
-    copyAndReplaceWithChangedCallback(absoluteSrcFilePath, destPath, relativeDestPath, replacements);
-  });
-}
 
 function generateCertificate(srcPath, destPath, newProjectName, currentUser) {
   console.log('Generating self-signed certificate...');
@@ -129,41 +97,6 @@ function copyProjectTemplateAndReplace(
 
   console.log(chalk.white.bold('To run your app on UWP:'));
   console.log(chalk.white('   react-native run-windows'));
-}
-
-function upgradeFileContentChangedCallback(
-  absoluteSrcFilePath,
-  relativeDestPath,
-  contentChanged
-) {
-  if (contentChanged === 'new') {
-    console.log(`${chalk.bold('new')} ${relativeDestPath}`);
-    return 'overwrite';
-  }
-  if (contentChanged === 'changed') {
-    console.log(
-      `${chalk.bold(relativeDestPath)} ` +
-      `has changed in the new version.\nDo you want to keep your ${relativeDestPath} or replace it with the ` +
-      `latest version?\nIf you ever made any changes ` +
-      `to this file, you'll probably want to keep it.\n` +
-      `You can see the new version here: ${absoluteSrcFilePath}\n` +
-      `Do you want to replace ${relativeDestPath}? ` +
-      `Answer y to replace, n to keep your version: `
-    );
-    const answer = prompt();
-    if (answer === 'y') {
-      console.log(`Replacing ${relativeDestPath}`);
-      return 'overwrite';
-    }
-    console.log(`Keeping your ${relativeDestPath}`);
-    return 'keep';
-  }
-  if (contentChanged === 'identical') {
-    return 'keep';
-  }
-  throw new Error(
-    `Unknown file changed state: ${relativeDestPath}, ${contentChanged}`
-  );
 }
 
 module.exports = {
