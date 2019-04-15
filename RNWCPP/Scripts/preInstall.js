01ddba556cdd0d50450d6be427bbd06fdd832d15
Marlene Cota <marlenecota@gmail.com>
2019-04-15 13:30:34 -0700

    Update npmignore and preinstall for CLI (#2295)

--- RNWCPP/Scripts/preInstall.js
+++ RNWCPP/Scripts/preInstall.js
@@ -2,11 +2,20 @@
 const execSync = require('child_process').execSync;
 const path = require('path');
 const fs = require('fs');
-const rootPath = execSync('git rev-parse --show-toplevel');
 
 // The default value of our devDependencies assumes that the react-native package is available on a NPM feed
 // For users outside of sdx-platform, we should redirect the dev dependency to a tar from github
-if (path.normalize(rootPath.toString().trim()) === path.resolve(__dirname, '../..')) {
+let replaceDep = true;
+
+try {
+    const rootPath = execSync('git rev-parse --show-toplevel');
+    replaceDep = path.normalize(rootPath.toString().trim()) === path.resolve(__dirname, '../..');
+}
+catch (err) {
+    // CLI will fail at the 'git rev-parse' call because it's not in a git repo, but we still need to redirect the dependency
+}
+
+if (replaceDep) {
     const pkgJsonPath = path.resolve(__dirname, '../package.json');
     let pkgJson = JSON.parse(fs.readFileSync(pkgJsonPath, 'utf8'));
 
