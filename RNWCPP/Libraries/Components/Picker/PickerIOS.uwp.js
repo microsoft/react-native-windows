4489e5b8860331d5c0bc84d2511879ecb0d2fe16
Andy Himberger <andrewh@microsoft.com>
2019-04-16 16:55:47 -0700

    update headers, convert uwp stubs to use unimplementedview  (fixes some typos in some files llike DatePickerMacOS.uwp)

--- RNWCPP/Libraries/Components/Picker/PickerIOS.uwp.js
+++ RNWCPP/Libraries/Components/Picker/PickerIOS.uwp.js
@@ -1,13 +1,5 @@
-/**
- * Copyright (c) 2015-present, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- *
- * This is a controlled component version of RCTPickerIOS
- */
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 'use strict';
 
 module.exports = require('UnimplementedView');
