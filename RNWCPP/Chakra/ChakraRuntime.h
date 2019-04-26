ebd1b56272dcb63f102f4098267e69cf2b2bcf86
Anandraj Govindan <mganandraj@outlook.com>
2019-04-26 14:18:55 +0530

    Cross platform/engine/storage abstraction for original and prepared scripts
    // ScriptStore manages versioned storage of both source scripts and prepared scripts. We intent to pass a ScriptSTore implementation
    // directly into the JSI runtime from host. It will enable the following,
    // 1. Pass richer data and metadata on the source scripts from host to JSI runtime without being constrained by the various layers in between.
    // 2. Potential performance benefits as it will allow us to directly stream the source script and the prepared scripts from arbitrary storages and encoding without being constrained by intermediate layers.
    // 3. Consolidate Source script versioning and storage logic into a single component.
    // 4. Consolidate prepared script storage and persistance formats across platforms and aross JS-engines.
    // 5. Allow co-existance of multiple Javascript engines, versions, strategies. (Some more work required to support multi-version)
    // 6. Allow platform/storage/engine specific extensions for better performance.

--- RNWCPP/Chakra/ChakraRuntime.h
+++ RNWCPP/Chakra/ChakraRuntime.h
@@ -4,22 +4,62 @@
 #pragma once
 
 #include <jsi/jsi.h>
+#include <jsi/ScriptStore.h>
+
+namespace facebook { 
+namespace react {
+  class MemoryTracker;
+  class MessageQueueThread;
+}}
 
 namespace facebook {
 namespace react {
 namespace chakra {
 
+enum class LogLevel
+{
+  Trace = 0,
+  Info = 1,
+  Warning = 2,
+  Error = 3,
+  Fatal = 4,
+};
+
+using Logger = std::function<void(const char* message, LogLevel logLevel)>;
+
+struct ChakraRuntimeArgs
+{
+  bool enableJITCompilation { true };
+
+  Logger loggingCallback {};
+
+  // Debugger configuration.
+  bool enableDebugging { false };
+  bool debuggerBreakOnNextLine { false };
+  bool enableNativePerformanceNow { true };
+  int debuggerPort { 9229 };
+  std::string debuggerRuntimeName;
+
+  // Memory governance.
+  size_t runtimeMemoryLimit { 0 };
+  std::shared_ptr<MemoryTracker> memoryTracker;
+
+  std::shared_ptr<MessageQueueThread> jsQueue;
+
+  // Script store which manages script and prepared script storage and versioning.
+  std::unique_ptr<jsi::ScriptStore> scriptStore;
+  std::unique_ptr<jsi::PreparedScriptStore> preparedScriptStore;
+};
+
 #ifndef JSI_CORE
 
-using Logger = std::function<void(const std::string& message, unsigned int logLevel)>;
-
-std::unique_ptr<jsi::Runtime> makeChakraRuntime(ChakraInstanceArgs&& args, std::shared_ptr<MessageQueueThread> jsQueue, Logger logger);
+std::unique_ptr<jsi::Runtime> makeChakraRuntime(ChakraRuntimeArgs&& args);
 
 #else // JSI_CORE
 
 struct IPromiseContinuation
 {
-	virtual void registerPromise(facebook::jsi::Function&& func) = 0;
+  virtual void registerPromise(facebook::jsi::Function&& func) = 0;
 };
 
 std::unique_ptr<jsi::Runtime> makeChakraRuntime(IPromiseContinuation&);
