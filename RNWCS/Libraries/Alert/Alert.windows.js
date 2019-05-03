28405d43ad2db707e7971514f04df5c9df6fe7f0
Eric Rozell <erozell@outlook.com>
2019-05-03 14:48:04 -0400

    fix(Metro): Gets metro bundler running for v0.59
    Added windows platform moniker to metro config.
    
    Temporarily removing the Alert module, follow on work to fix create the native / cross-platform alert manager based on changes to the Alert module in react-native@0.59.

--- RNWCS/Libraries/Alert/Alert.windows.js
+++ RNWCS/Libraries/Alert/Alert.windows.js
@@ -14,11 +14,7 @@
  */
 'use strict';
 
-const AlertIOS = require('AlertIOS');
 const NativeModules = require('NativeModules');
-const Platform = require('Platform');
-
-import type { AlertType, AlertButtonStyle } from 'AlertIOS';
 
 export type Buttons = Array<{
   text?: string,
@@ -62,72 +58,7 @@ class Alert {
     options?: Options,
     type?: AlertType,
   ): void {
-    if (Platform.OS === 'ios') {
-      if (typeof type !== 'undefined') {
-        console.warn('Alert.alert() with a 5th "type" parameter is deprecated and will be removed. Use AlertIOS.prompt() instead.');
-        AlertIOS.alert(title, message, buttons, type);
-        return;
-      }
-      AlertIOS.alert(title, message, buttons);
-    } else if (Platform.OS === 'android') {
-      AlertAndroid.alert(title, message, buttons, options);
-    } else if (Platform.OS === 'windows') {
-      AlertWindows.alert(title, message, buttons, options);
-    }
-  }
-}
-
-/**
- * Wrapper around the Android native module.
- */
-class AlertAndroid {
-
-  static alert(
-    title: ?string,
-    message?: ?string,
-    buttons?: Buttons,
-    options?: Options,
-  ): void {
-    var config = {
-      title: title || '',
-      message: message || '',
-    };
-
-    if (options) {
-      config = {...config, cancelable: options.cancelable};
-    }
-    // At most three buttons (neutral, negative, positive). Ignore rest.
-    // The text 'OK' should be probably localized. iOS Alert does that in native.
-    var validButtons: Buttons = buttons ? buttons.slice(0, 3) : [{text: 'OK'}];
-    var buttonPositive = validButtons.pop();
-    var buttonNegative = validButtons.pop();
-    var buttonNeutral = validButtons.pop();
-    if (buttonNeutral) {
-      config = {...config, buttonNeutral: buttonNeutral.text || '' };
-    }
-    if (buttonNegative) {
-      config = {...config, buttonNegative: buttonNegative.text || '' };
-    }
-    if (buttonPositive) {
-      config = {...config, buttonPositive: buttonPositive.text || '' };
-    }
-    NativeModules.DialogManagerAndroid.showAlert(
-      config,
-      (errorMessage) => console.warn(errorMessage),
-      (action, buttonKey) => {
-        if (action === NativeModules.DialogManagerAndroid.buttonClicked) {
-          if (buttonKey === NativeModules.DialogManagerAndroid.buttonNeutral) {
-            buttonNeutral.onPress && buttonNeutral.onPress();
-          } else if (buttonKey === NativeModules.DialogManagerAndroid.buttonNegative) {
-            buttonNegative.onPress && buttonNegative.onPress();
-          } else if (buttonKey === NativeModules.DialogManagerAndroid.buttonPositive) {
-            buttonPositive.onPress && buttonPositive.onPress();
-          }
-        } else if (action === NativeModules.DialogManagerAndroid.dismissed) {
-          options && options.onDismiss && options.onDismiss();
-        }
-      }
-    );
+    AlertWindows.alert(title, message, buttons, options);
   }
 }
 
