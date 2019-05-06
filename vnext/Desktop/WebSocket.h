d473d14d7f020250f0d2f124b1f3232da4cfdc2f
Julio César Rocha <JunielKatarn@users.noreply.github.com>
2019-05-06 14:49:32 -0700

    Enable WebSocket resource unit tests. (#2415)
    * Define test WebSocket types.
    
    * Use CRLF for C++ files.
    
    * Try basic_stream_socket over IWebSocketStream.
    
    * Use boost::beast::test::stream.
    
    * Defined async_connect(test::stream& ...
    
    * Move custom async_connect into WebSocket.cpp.
    
    * Defined MockStreamLayer subtype.
    
    * Use errc::make_error_code.
    
    * Define MockStream, equivalent to websocket::stream
    
    * Implement async read and write in MockStreamLayer.
    
    * Override boost::beast::http::async_read and write.
    
    * Move TestWebSocket implementation to CPP file.
    
    * Stop including <boost/beast/experimental/test/stream.hpp>.
    
    * Simplify template decls for SecureWebSocket.
    
    * Simplified WebSocket template declaration.
    
    * Updated namespace for WebSocket-related APIs.
    
    * Reset default platform to x64 in SLN.
    
    * Re-located BaseWebSocketTest.

--- vnext/Desktop/WebSocket.h
+++ vnext/Desktop/WebSocket.h
@@ -4,13 +4,14 @@
 #pragma once
 
 #include <boost/beast/websocket.hpp>
+#include <boost/beast/websocket/ssl.hpp>
 #include <thread>
 #include <queue>
 #include "IWebSocket.h"
 #include "Utils.h"
 
-namespace facebook {
-namespace react {
+namespace Microsoft {
+namespace React {
 
 template<typename Protocol, typename Socket, typename Resolver>
 class BaseWebSocket : public IWebSocket
@@ -89,15 +90,25 @@ public:
   #pragma endregion
 };
 
-template<typename Protocol, typename Socket = boost::asio::basic_stream_socket<Protocol>, typename Resolver = boost::asio::ip::basic_resolver<Protocol>>
-class WebSocket : public BaseWebSocket<Protocol, Socket, Resolver>
+class WebSocket
+  : public  BaseWebSocket
+            <
+              boost::asio::ip::tcp,
+              boost::asio::basic_stream_socket<boost::asio::ip::tcp>,
+              boost::asio::ip::basic_resolver<boost::asio::ip::tcp>
+            >
 {
 public:
   WebSocket(Url&& url);
 };
 
-template<typename Protocol, typename Socket, typename Resolver = boost::asio::ip::basic_resolver<Protocol>>
-class SecureWebSocket : public BaseWebSocket<Protocol, Socket, Resolver>
+class SecureWebSocket
+  : public  BaseWebSocket
+            <
+              boost::asio::ip::tcp,
+              boost::asio::ssl::stream<boost::asio::ip::tcp::socket>,
+              boost::asio::ip::basic_resolver<boost::asio::ip::tcp>
+            >
 {
   #pragma region BaseWebSocket overrides
 
@@ -109,4 +120,99 @@ public:
   SecureWebSocket(Url&& url);
 };
 
-} } // namespace facebook::react
+namespace Test {
+
+// See <boost/beast/experimental/test/stream.hpp>
+class MockStream
+{
+  friend void teardown
+  (
+    boost::beast::websocket::role_type,
+    MockStream&,
+    boost::system::error_code&
+  ) {}
+
+  template<class TeardownHandler>
+  friend void async_teardown
+  (
+    boost::beast::websocket::role_type,
+    MockStream&,
+    TeardownHandler&&
+  ) {}
+
+  boost::asio::io_context& m_context;
+
+public:
+  MockStream(boost::asio::io_context& context);
+
+  std::function<boost::system::error_code()> ConnectResult;
+
+  boost::beast::websocket::detail::sec_ws_key_type Key;
+
+  using is_deflate_supported = std::integral_constant<bool, false>;
+
+  using next_layer_type = MockStream;
+
+  using lowest_layer_type = MockStream;
+
+  using executor_type = boost::asio::io_context::executor_type;
+
+  using lowest_layer_type = MockStream;
+
+  lowest_layer_type& lowest_layer();
+
+  lowest_layer_type const& lowest_layer() const;
+
+  void binary(bool value);
+
+  bool got_binary() const;
+
+  bool got_text() const;
+
+  void auto_fragment(bool value);
+
+  bool auto_fragment() const;
+
+  void write_buffer_size(std::size_t amount);
+
+  std::size_t write_buffer_size() const;
+
+  template<class DynamicBuffer, class ReadHandler>
+  BOOST_ASIO_INITFN_RESULT_TYPE(ReadHandler, void(boost::system::error_code, std::size_t))
+  async_read(DynamicBuffer& buffer, ReadHandler&& handler);
+
+  template<class ConstBufferSequence, class WriteHandler>
+  BOOST_ASIO_INITFN_RESULT_TYPE(WriteHandler, void(boost::system::error_code, std::size_t))
+  async_write(ConstBufferSequence const& buffers, WriteHandler&& handler);
+
+  template<class WriteHandler>
+  BOOST_ASIO_INITFN_RESULT_TYPE(WriteHandler, void(boost::system::error_code))
+  async_ping(boost::beast::websocket::ping_data const& payload, WriteHandler&& handler);
+
+  template<class CloseHandler>
+  BOOST_ASIO_INITFN_RESULT_TYPE(CloseHandler, void(boost::system::error_code))
+  async_close(boost::beast::websocket::close_reason const& cr, CloseHandler&& handler);
+
+  // AsyncStream compliance
+  template<class MutableBufferSequence, class ReadHandler>
+  BOOST_ASIO_INITFN_RESULT_TYPE(ReadHandler, void(boost::system::error_code, std::size_t))
+  async_read_some(MutableBufferSequence const& buffers, ReadHandler&& handler);
+
+  template<class ConstBufferSequence, class WriteHandler>
+  BOOST_ASIO_INITFN_RESULT_TYPE(WriteHandler, void(boost::system::error_code, std::size_t))
+  async_write_some(ConstBufferSequence const& buffers, WriteHandler&& handler);
+
+  boost::asio::io_context::executor_type get_executor() noexcept;
+};
+
+class TestWebSocket : public BaseWebSocket<boost::asio::ip::tcp, MockStream, boost::asio::ip::basic_resolver<boost::asio::ip::tcp>>
+{
+public:
+  TestWebSocket(facebook::react::Url&& url);
+
+  void SetConnectResult(std::function<boost::system::error_code()>&& resultFunc);
+};
+
+} // namespace Microsoft::React::Test
+
+} } // namespace Microsoft::React
