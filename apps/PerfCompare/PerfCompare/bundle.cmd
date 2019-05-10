9f4659d941427b92de2c9f36c36a714b24ad7200
Jon Thysell (JAUNTY) <jthysell@microsoft.com>
2019-05-10 15:13:12 -0700

    Removed bundle.cmd from pre-build

--- apps/PerfCompare/PerfCompare/bundle.cmd
+++ apps/PerfCompare/PerfCompare/bundle.cmd
@@ -18,7 +18,7 @@ xcopy /q %AppRoot%images\* %AppName%\images\
 xcopy /q %AppRoot%app.json %AppName%\
 xcopy /q %AppRoot%index.js %AppName%\
 
-call react-native bundle --platform windows --entry-file %AppName%\index.js --bundle-output %AppRoot%ReactAssets\index.windows.bundle --assets-dest %AppRoot%ReactAssets
+call react-native bundle --platform windows --dev false --entry-file %AppName%\index.js --bundle-output %AppRoot%ReactAssets\index.windows.bundle --assets-dest %AppRoot%ReactAssets
 
 rmdir /s /q %AppName%
 
@@ -37,7 +37,7 @@ xcopy /q %AppRoot%images\* %AppName%\images\
 xcopy /q %AppRoot%app.json %AppName%\
 xcopy /q %AppRoot%index.js %AppName%\
 
-call react-native bundle  --platform uwp --entry-file %AppName%\index.js --bundle-output %AppRoot%Bundle\index.uwp.bundle --assets-dest %AppRoot%Bundle
+call react-native bundle  --platform uwp --dev false --entry-file %AppName%\index.js --bundle-output %AppRoot%Bundle\index.uwp.bundle --assets-dest %AppRoot%Bundle
 
 rmdir /s /q %AppName%
 
