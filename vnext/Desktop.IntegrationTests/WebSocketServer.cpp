218ac7132219750e9c3e483b8e3d1f56d1dc293a
Julio C. Rocha <julio@rochsquadron.net>
2019-05-09 19:10:58 -0700

    Implement server and session read/write cycle.

--- vnext/Desktop.IntegrationTests/WebSocketServer.cpp
+++ vnext/Desktop.IntegrationTests/WebSocketServer.cpp
@@ -1,11 +1,82 @@
 #include "WebSocketServer.h"
 
-using boost::asio::ip::tcp;
+#include <boost/asio/bind_executor.hpp>
+
+using namespace boost::asio;
+
 using boost::system::error_code;
 
 namespace Microsoft {
 namespace React {
 namespace Test {
+
+#pragma region WebSocketSession
+
+WebSocketSession::WebSocketSession(io_context& context)
+  : m_context{ context }
+  , m_stream{ context }
+  , m_strand{ m_stream.get_executor() }
+{
+}
+
+WebSocketSession::WebSocketSession(io_context& context, ip::tcp::socket socket)
+  : m_context{ context }
+  , m_stream{ std::move(socket) }
+  , m_strand{ m_stream.get_executor() }
+{
+}
+
+void WebSocketSession::Start()
+{
+  m_stream.async_accept(bind_executor(m_strand, std::bind(&WebSocketSession::OnAccept, shared_from_this(), std::placeholders::_1)));
+}
+
+void WebSocketSession::OnAccept(error_code ec)
+{
+  if (ec)
+    return;//TODO: fail
+
+  Read();
+}
+
+void WebSocketSession::Read()
+{
+  m_stream.async_read(m_buffer, bind_executor(m_strand, std::bind(
+    &WebSocketSession::OnRead,
+    shared_from_this(),
+    std::placeholders::_1,
+    std::placeholders::_2
+  )));
+}
+
+void WebSocketSession::OnRead(error_code ec, size_t /*transferred*/)
+{
+  if (boost::beast::websocket::error::closed == ec)
+    return;
+
+  if (ec)
+    ;//TODO: fail
+
+  m_stream.text(m_stream.got_text());
+  m_stream.async_write(m_buffer.data(), bind_executor(m_strand, std::bind(
+    &WebSocketSession::OnWrite,
+    shared_from_this(),
+    std::placeholders::_1,
+    std::placeholders::_2
+  )));
+}
+
+void WebSocketSession::OnWrite(error_code ec, size_t /*transferred*/)
+{
+  if (ec)
+    return; //TODO: fail
+
+  m_buffer.consume(m_buffer.size());
+
+  Read();
+}
+
+#pragma endregion // WebSocketSession
 
 #pragma region WebSocketServer
 
@@ -13,7 +84,7 @@ WebSocketServer::WebSocketServer(uint16_t port)
   : m_acceptor{ m_context }
   , m_socket{ m_context }
 {
-  tcp::endpoint ep{boost::asio::ip::make_address("0.0.0.0"), port };
+  ip::tcp::endpoint ep{ip::make_address("0.0.0.0"), port };
   error_code ec;
 
   m_acceptor.open(ep.protocol(), ec);
@@ -22,7 +93,7 @@ WebSocketServer::WebSocketServer(uint16_t port)
     return; //
   }
 
-  m_acceptor.set_option(boost::asio::socket_base::reuse_address(true), ec);
+  m_acceptor.set_option(socket_base::reuse_address(true), ec);
   if (ec)
   {
     return; //
@@ -34,11 +105,39 @@ WebSocketServer::WebSocketServer(uint16_t port)
     return; //
   }
 
-  m_acceptor.listen(boost::asio::socket_base::max_listen_connections, ec);
+  m_acceptor.listen(socket_base::max_listen_connections, ec);
   if (ec)
   {
     return; //
   }
+} // constructor(uint16_t)
+
+void WebSocketServer::Start()
+{
+  if (!m_acceptor.is_open())
+    return;
+
+  Accept();
+}
+
+void WebSocketServer::Accept()
+{
+  m_acceptor.async_accept(m_socket, std::bind(&WebSocketServer::OnAccept, shared_from_this(), std::placeholders::_1));
+}
+
+void WebSocketServer::OnAccept(error_code ec)
+{
+  if (ec)
+  {
+    //TODO: fail
+  }
+  else
+  {
+    std::make_shared<WebSocketSession>(m_context, std::move(m_socket))->Start();
+  }
+
+  //TODO: Accept again.
+  //Accept();
 }
 
 #pragma endregion // WebSocketServer
--- vnext/Desktop.IntegrationTests/WebSocketServer.h
+++ vnext/Desktop.IntegrationTests/WebSocketServer.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <boost/asio/strand.hpp>
 #include <boost/beast/websocket.hpp>
+#include <IWebSocket.h>
 #include <thread>
 
 namespace Microsoft {
@@ -10,9 +12,27 @@ namespace Test {
 class WebSocketSession : std::enable_shared_from_this<WebSocketSession>
 {
   boost::asio::io_context& m_context;
+  boost::beast::websocket::stream<boost::asio::ip::tcp::socket> m_stream;
+  boost::asio::strand<boost::asio::io_context::executor_type> m_strand;
+  boost::beast::multi_buffer m_buffer;
+
+  std::function<void()> m_connectHandler;
+  std::function<void()> m_pingHandler;
+  std::function<void(std::size_t)> m_writeHandler;
+  std::function<void(std::size_t, const std::string&)> m_readHandler;
+  std::function<void(IWebSocket::CloseCode, const std::string&)> m_closeHandler;
+  std::function<void(IWebSocket::Error&&)> m_errorHandler;
 
 public:
-  WebSocketSession(boost::asio::io_context& context);
+  WebSocketSession(boost::asio::io_context& context);//TODO: Remove?
+  WebSocketSession(boost::asio::io_context& context, boost::asio::ip::tcp::socket socket);
+
+  void Start();
+  void Read();
+
+  void OnAccept(boost::system::error_code ec);
+  void OnRead(boost::system::error_code ec, std::size_t transferred);
+  void OnWrite(boost::system::error_code ec, std::size_t transferred);
 };
 
 class WebSocketServer : public std::enable_shared_from_this<WebSocketServer>
@@ -21,6 +41,10 @@ class WebSocketServer : public std::enable_shared_from_this<WebSocketServer>
   boost::asio::io_context m_context;
   boost::asio::ip::tcp::acceptor m_acceptor;
   boost::asio::ip::tcp::socket m_socket;
+
+  void Accept();
+
+  void OnAccept(boost::system::error_code ec);
 
 public:
   WebSocketServer(std::uint16_t port);
