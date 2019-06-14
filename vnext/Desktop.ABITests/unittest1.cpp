b61d4f58086ccbdb5bc79e546c0e9880dc258d7b
Andreas Eulitz <aeulitz@microsoft.com>
2019-06-13 18:03:43 -0700

    add ABITest project

---
+++ vnext/Desktop.ABITests/unittest1.cpp
@@ -0,0 +1,18 @@
+//#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ReactWindowsDesktopABITests
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}
