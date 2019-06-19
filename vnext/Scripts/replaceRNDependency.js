61e9909efa9246669c8f56a7eceecf13f5dbf7b9
REDMOND\acoates <acoates@microsoft.com>
2019-06-19 12:28:16 -0700

    Cleanup various files from the npm package

--- vnext/Scripts/replaceRNDependency.js
+++
@@ -1,9 +0,0 @@
-// @ts-check
-const path = require('path');
-const fs = require('fs');
-
-const pkgJsonPath = path.resolve(__dirname, '../package.json');
-let pkgJson = JSON.parse(fs.readFileSync(pkgJsonPath, 'utf8'));
-pkgJson.rnDepVersion = pkgJson.devDependencies['react-native'];
-pkgJson.devDependencies['react-native'] = `https://github.com/Microsoft/react-native/archive/v${pkgJson.devDependencies['react-native']}.tar.gz`;
-fs.writeFileSync(pkgJsonPath, JSON.stringify(pkgJson, null, 2));
