971616130331de159a91e09428444a6167d6fae9
Julio C. Rocha <julio@rochsquadron.net>
2019-06-19 15:27:15 -0700

    Removed Stubs.cpp.

--- vnext/Desktop/Stubs.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-#include "pch.h"
-
-// Required by Linker.
-extern "C" void __cdecl All_office_binaries_must_use_OLIBC(void) { };
