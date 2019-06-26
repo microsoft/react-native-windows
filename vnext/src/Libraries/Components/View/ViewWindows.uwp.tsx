e65679ac3bf7592c9dc3186831f1f3783ce7908e
CanHua Li <canli@microsoft.com>
2019-06-26 11:32:08 -0700

    add licensed header

--- vnext/src/Libraries/Components/View/ViewWindows.uwp.tsx
+++ vnext/src/Libraries/Components/View/ViewWindows.uwp.tsx
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 import { IViewWindowsProps } from './ViewWindowsProps';
 import * as React from 'react';
 import { View } from 'react-native';
--- vnext/src/Libraries/Components/View/ViewWindowsProps.ts
+++ vnext/src/Libraries/Components/View/ViewWindowsProps.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 import { IKeyboardProps } from '../Keyboard/KeyboardExtProps';
 import { ViewProps } from 'react-native';
 
