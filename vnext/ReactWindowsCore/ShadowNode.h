113ff6a24588db5c50a4e9c74b4c67834ebe3b80
CanHua Li <canli@microsoft.com>
2019-06-28 22:40:35 -0700

    tab to space

--- vnext/ReactWindowsCore/ShadowNode.h
+++ vnext/ReactWindowsCore/ShadowNode.h
@@ -12,27 +12,27 @@ class IViewManager;
 
 struct ShadowNode
 {
-	ShadowNode(const ShadowNode& that) = delete;
-	ShadowNode& operator=(ShadowNode const&) = delete;
-	ShadowNode() = default;
-	virtual ~ShadowNode();
+  ShadowNode(const ShadowNode& that) = delete;
+  ShadowNode& operator=(ShadowNode const&) = delete;
+  ShadowNode() = default;
+  virtual ~ShadowNode();
 
-	virtual void updateProperties(const folly::dynamic&& props);
-	virtual void onDropViewInstance() = 0;
-	virtual void dispatchCommand(int64_t commandId, const folly::dynamic& commandArgs);
-	virtual void removeAllChildren() = 0;
-	virtual void AddView(ShadowNode& child, int64_t index) = 0;
-	virtual void RemoveChildAt(int64_t indexToRemove) = 0;
-	virtual void createView() = 0;
+  virtual void updateProperties(const folly::dynamic&& props);
+  virtual void onDropViewInstance() = 0;
+  virtual void dispatchCommand(int64_t commandId, const folly::dynamic& commandArgs);
+  virtual void removeAllChildren() = 0;
+  virtual void AddView(ShadowNode& child, int64_t index) = 0;
+  virtual void RemoveChildAt(int64_t indexToRemove) = 0;
+  virtual void createView() = 0;
   virtual void focus() = 0;
   virtual void blur() = 0;
 
-	int64_t m_tag { 0 };
-	std::string m_className;
-	std::vector<int64_t> m_children;
-	int64_t m_parent = -1;
-	IViewManager* m_viewManager = nullptr;
-	bool m_zombie = false;
+  int64_t m_tag { 0 };
+  std::string m_className;
+  std::vector<int64_t> m_children;
+  int64_t m_parent = -1;
+  IViewManager* m_viewManager = nullptr;
+  bool m_zombie = false;
 };
 
 }}
