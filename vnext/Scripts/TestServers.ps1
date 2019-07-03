936558948e17a9c46b83f35fb02cc3958e6db067
Julio C. Rocha <julio@rochsquadron.net>
2019-07-02 19:54:51 -0700

    Defined Start-Node and Start-Npm.

--- vnext/Scripts/TestServers.ps1
+++ vnext/Scripts/TestServers.ps1
@@ -15,49 +15,25 @@ function Start-Npm {
 	Start-Process	-FilePath $NpmPath `
 					-PassThru `
 					-NoNewWindow $NoNewWindow `
-					-WorkingDirectory = ({Get-Location}, {$WorkingDirectory})[$WorkingDirectory]
+					-WorkingDirectory = ({Get-Location}, {$WorkingDirectory})[$WorkingDirectory] `
+					-Verb 'run start' `
+					-OutVariable npmProcessId
+
+	return $npmProcessId
 }
 
 function Start-Node {
 	param (
-		[string] $NodePath = (Get-Command node.exe).Definition
+		[string] $ScriptPath,
+		[string] $NodePath = (Get-Command node.exe).Definition,
+		[switch] $NoNewWindow
 	)
 
+	Start-Process	-FilePath $NodePath `
+					-PassThru `
+					-NoNewWindow $NoNewWindow `
+					-ArgumentList $ScriptPath `
+					-OutVariable nodeProcId
 
+	return $nodeProcId
 }
-
-Push-Location
-Set-Location -Path $ReactNativeLocation
-
-Start-Process	-FilePath (Get-Command node.exe).Definition `
-				-PassThru `
-				-NoNewWindow `
-				-WorkingDirectory $ReactNativeLocation `
-				-ArgumentList "run start" `
-				-OutVariable packagerProc
-
-Write-Host 'Started packager'
-
-Write-Host 'Starting WebSocket server'
-
-Start-Process	-FilePath (Get-Command node.exe).Definition `
-				-PassThru `
-				-ArgumentList "${SourcesDirectory}\IntegrationTests\websocket_integration_test_server.js" `
-				-OutVariable wsProc
-
-Write-Host 'Started WebSocket server'
-
-if ($Preload) {
-	Write-Host 'Preloading bundles'
-
-	Start-Sleep -Seconds 12
-
-	# Preload the RNTesterApp integration bundle for better performance in integration tests.
-	Invoke-WebRequest -Uri "http://localhost:8081/IntegrationTests/IntegrationTestsAppWin.bundle?platform=windesktop&dev=true" | Out-Null
-
-	Write-Host 'Done preloading bundles.'
-}
-
-Pop-Location
-
-return ($packagerProc, $wsProc)
