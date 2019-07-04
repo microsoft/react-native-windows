30ab9e9a56b3e04d005cfdd4f1aa54a0ad9d5d91
Julio C. Rocha <julio@rochsquadron.net>
2019-07-04 02:25:57 -0700

    Allow multiple assemblies in unit tests.

--- vnext/Scripts/UnitTest.ps1
+++ vnext/Scripts/UnitTest.ps1
@@ -5,12 +5,23 @@
 #
 param (
 	[ValidateSet('x64', 'x86')]
-	$Platform = 'x64',
+	[string] $Platform = 'x64',
+
 	[ValidateSet('Debug', 'Release')]
-	$Configuration = 'Debug',
+	[string] $Configuration = 'Debug',
+
 	[string[]] $Tests,
-	[string] $Assembly = "$PSScriptRoot\..\target\$Platform\$Configuration\React.Windows.Desktop.UnitTests\React.Windows.Desktop.UnitTests.dll",
+
+	[ValidateScript({Test-Path $_})]
+	[string[]] $Assemblies =
+	(
+		"$PSScriptRoot\..\target\$Platform\$Configuration\" +
+		"React.Windows.Desktop.UnitTests\React.Windows.Desktop.UnitTests.dll"
+	),
+
+	[ValidateScript({Test-Path $_})]
 	[string] $VsTest = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
 )
 
-& $VsTest $Assembly --InIsolation --Platform:$Platform ('', "--Tests:$($Tests -join ',')")[$Tests.Count -gt 0]
+# Run Unit Test assemblies.
+& $VsTest ($Assemblies -join ' ') --InIsolation --Platform:$Platform ('', "--Tests:$($Tests -join ',')")[$Tests.Count -gt 0]
