0259966ee3d0b660391b67bee287176ac5f40371
CanHua Li <canli@microsoft.com>
2019-07-09 11:22:47 -0700

    rollback cppwinrtincludes.h

--- vnext/include/CppWinRTIncludes.h
+++ vnext/include/CppWinRTIncludes.h
@@ -4,43 +4,22 @@
 
 #pragma once
 
-#include <winrt/base.h>
-
 #include <winrt/Windows.Foundation.h>
-
-#include <winrt/Windows.UI.Xaml.h>
-
-#include <winrt/Windows.UI.Xaml.Controls.h>
-
-#include <winrt/Windows.UI.Xaml.Controls.Primitives.h>
-
-#include <winrt/Windows.UI.Xaml.Input.h>
-
-#include <winrt/Windows.UI.Text.h>
 #include <winrt/Windows.UI.Composition.h>
-
+#include <winrt/Windows.UI.Text.h>
+#include <winrt/Windows.UI.Xaml.Controls.Primitives.h>
+#include <winrt/Windows.UI.Xaml.Controls.h>
 #include <winrt/Windows.UI.Xaml.Hosting.h>
 #include <winrt/Windows.UI.Xaml.h>
 
-#include <winrt/Windows.Globalization.h>
-
-#include <winrt/Windows.Globalization.DateTimeFormatting.h>
-
-namespace winrt
-{
-using namespace ::winrt::Windows::UI::Core;
-using namespace ::winrt::Windows::Foundation;
-using namespace ::winrt::Windows::Foundation::Collections;
+namespace winrt {
 using namespace ::winrt::Windows::UI::Xaml;
-using namespace ::winrt::Windows::UI::Xaml::Controls;
-using namespace ::winrt::Windows::UI::Xaml::Controls::Primitives;
-using namespace ::winrt::Windows::UI::Composition;
-
-// using namespace ::winrt::Windows::UI::Xaml::Controls;
-using Control = ::winrt::Windows::UI::Xaml::Controls::Control;
-using ComboBox = ::winrt::Windows::UI::Xaml::Controls::ComboBox;
-using ContentControl = ::winrt::Windows::UI::Xaml::Controls::ContentControl;
-using CheckBox = ::winrt::Windows::UI::Xaml::Controls::CheckBox;
-using ToggleSwitch = ::winrt::Windows::UI::Xaml::Controls::ToggleSwitch;
-}
-
+using namespace ::winrt::Windows::UI::Core;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Composition;
+using namespace Windows::UI::Xaml::Hosting;
+} // namespace winrt
