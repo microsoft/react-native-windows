c72ca604ec34d6fcca2207126fffa9be3f3d511a
Marlene Cota <mcota@microsoft.com>
2019-07-12 22:15:35 -0700

    clang-format: add rule for pch.h and generated files

--- vnext/Universal.UnitTests/Tests/StringConversionTests_Universal.cpp
+++ vnext/Universal.UnitTests/Tests/StringConversionTests_Universal.cpp
@@ -1,10 +1,10 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+#include "pch.h"
 #include <../Chakra/Utf8DebugExtensions.h>
 #include <CppUnitTest.h>
 #include <string>
 #include <vector>
-#include "pch.h"
 
 using namespace facebook::react;
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
