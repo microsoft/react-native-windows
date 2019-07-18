38fa411e07ff97631fca833ce9482737cb22c610
Eric Rozell <erozell@outlook.com>
2019-07-18 08:36:08 -0400

    fix(Metro): fixes hasteImpl path for v59

--- current/metro.config.js
+++ current/metro.config.js
@@ -1,13 +1,11 @@
 "use strict";
 
 var path = require("path");
-const blacklist = require("metro-config/src/defaults/blacklist");
-const defaultPolyfills = require("react-native/rn-get-polyfills");
 
 var config = {
   resolver: {
     platforms: [ 'windows' ],
-    hasteImplModulePath: require.resolve('./jest/hasteImpl'),
+    hasteImplModulePath: path.resolve(__dirname, 'jest/hasteImpl'),
   },
 };
 
