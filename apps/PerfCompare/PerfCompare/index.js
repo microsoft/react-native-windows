0072d33d89e999eb192e45f6affd9dd0295b5fc9
BALAJI.J.B <jbbalajics@gmail.com>
2019-07-27 09:49:34 +0530

    import react and component in proper way
    the following way of importing multiple modules is suggested in react.

--- apps/PerfCompare/PerfCompare/index.js
+++ apps/PerfCompare/PerfCompare/index.js
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import React, { Component } from 'react';
+import { React, Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
