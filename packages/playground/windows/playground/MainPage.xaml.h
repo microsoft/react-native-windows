ff3f024b3bb6ed92ae73307b294d1d0ebbd1c83f
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2019-07-28 05:40:30 -0700

    Move playground package to c++ (#2837)
    * Move playground app to c++, so it can import custom viewmanagers
    
    * Change files
    
    * Minor build fixes
    
    * Missing file
    
    * Update folly location when using public RN
    
    * Only build playground against RNFork

---
+++ packages/playground/windows/playground/MainPage.xaml.h
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// MainPage.xaml.h
+// Declaration of the MainPage class.
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+#include "HostingPane.xaml.h"
+#include "Utilities/RelayCommand.h"
+
+namespace Playground {
+
+[Windows::Foundation::Metadata::WebHostHidden] public ref class MainPage
+    sealed {
+ public:
+  MainPage();
+
+ private:
+  void RemovePane(HostingPane ^ pane);
+  void AddPane(HostingPane ^ pane);
+  void UpdatePaneCommandState();
+
+ private:
+  Platform::Collections::Vector<HostingPane ^> m_hostingPanes;
+
+  RelayCommand ^ m_addPaneCommand;
+  RelayCommand ^ m_removePaneCommand;
+};
+
+} // namespace Playground
