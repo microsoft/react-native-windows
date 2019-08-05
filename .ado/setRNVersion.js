ac1a3d6dfb164d3ca41f8f714e435a6a77d9452b
Andrew Coates (REDMOND) <acoates@microsoft.com>
2019-08-04 17:04:37 -0700

    Remove change files for private packages

--- .ado/setRNVersion.js
+++ .ado/setRNVersion.js
@@ -1,6 +1,11 @@
 // @ts-check
 // Script to update dependencies to a specific version of react-native
 // This can be triggered using lerna to update the whole repo
+// Its is run in two phases, first run it without providing the repo root
+// and this script will update the package.json.  Then run it again providing
+// the repo root and it will generate a change file.
+// It needs to run in two phases because beachball wont generate a changefile if
+// there are any uncommited changes (like from changing the package.json)
 
 const path = require('path');
 const fs = require('fs');
@@ -23,16 +28,18 @@ if (!fs.existsSync(pkgJsonPath)) {
 const pkgJson = require(pkgJsonPath);
 
 if (process.argv.length === 4) {
-  // Generate change file if repo root is provided
-  const rootPath = process.argv[3];
+  // Generate change file if package no private and repo root is provided
+  if (!pkgJson.private) {
+    const rootPath = process.argv[3];
 
-  // Generate change file
-  const beachballPath = path.resolve(rootPath, 'node_modules/beachball/bin/beachball.js');
-  const changeType = pkgJson.name === 'react-native-windows' ? 'prerelease' : 'minor';
-  const changeDescription = `"Updating react-native to version: ${rnVersion}"`;
-  child_process.execSync(`node ${beachballPath} change --package ${pkgJson.name} --type ${changeType} --message ${changeDescription}`);
+    // Generate change file
+    const beachballPath = path.resolve(rootPath, 'node_modules/beachball/bin/beachball.js');
+    const changeType = pkgJson.name === 'react-native-windows' ? 'prerelease' : 'minor';
+    const changeDescription = `"Updating react-native to version: ${rnVersion}"`;
+    child_process.execSync(`node ${beachballPath} change --package ${pkgJson.name} --type ${changeType} --message ${changeDescription}`);
+  }
 } else {
-  // Update package.json for new version
+  // Update package.json for new version if repo root is not provided
 
   function replaceDeps(dependencies) {
     if (dependencies && dependencies['react-native']) {
