c9d39a15caca2010fa2ec1e865b4ad2ad0aa8149
Ruriko Araki <ruaraki@microsoft.com>
2019-08-06 17:43:31 -0700

    Fix switch shimmy issue

--- vnext/src/Libraries/Components/Switch/Switch.uwp.tsx
+++ vnext/src/Libraries/Components/Switch/Switch.uwp.tsx
@@ -29,10 +29,12 @@ const RCTSwitch = requireNativeComponent('RCTSwitch');
 class Switch extends React.Component<ISwitchProps> {
   // tslint:disable-next-line:no-any
   private _rctSwitch: any;
+  private _lastNativeValue: boolean;
 
   public constructor(props: ISwitchProps) {
     super(props);
     this._rctSwitch = React.createRef();
+    this._lastNativeValue = false;
   }
 
   public render(): JSX.Element {
@@ -50,20 +52,28 @@ class Switch extends React.Component<ISwitchProps> {
     );
   }
 
+  componentDidUpdate = () => {
+    // This is necessary in case native updates the switch and JS decides
+    // that the update should be ignored and we should stick with the value
+    // that we have in JS.
+    const value = this.props.value === true;
+
+    if (this._lastNativeValue !== value && this._rctSwitch) {
+      this._rctSwitch.setNativeProps({value});
+    }
+  };
+
   private _setRef = (switchRef: Switch /*RCTSwitch*/) => {
     this._rctSwitch = switchRef;
   };
 
   private _onChange = (event: ISwitchChangeEvent) => {
-    if (this._rctSwitch) {
-      // Force value of native switch in order to control it.
-      const value = this.props.value === true;
-      this._rctSwitch.setNativeProps({value});
-    }
-
     this.props.onChange && this.props.onChange(event);
     this.props.onValueChange &&
       this.props.onValueChange(event.nativeEvent.value);
+
+    this._lastNativeValue = event.nativeEvent.value;
+    this.forceUpdate();
   };
 }
 
