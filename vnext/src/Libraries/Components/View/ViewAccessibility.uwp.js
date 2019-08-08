892d0d0cdeec88c705e9c4c2f62cc2372c3e065e
Jon Thysell (JAUNTY) <jthysell@microsoft.com>
2019-08-08 16:45:02 -0700

    Added list, listitem roles to ViewAccessibility.uwp.js

---
+++ vnext/src/Libraries/Components/View/ViewAccessibility.uwp.js
@@ -0,0 +1,162 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ * @flow strict
+ */
+
+'use strict';
+
+export type AccessibilityTrait =
+  | 'none'
+  | 'button'
+  | 'link'
+  | 'header'
+  | 'search'
+  | 'image'
+  | 'selected'
+  | 'plays'
+  | 'key'
+  | 'text'
+  | 'summary'
+  | 'disabled'
+  | 'frequentUpdates'
+  | 'startsMedia'
+  | 'adjustable'
+  | 'allowsDirectInteraction'
+  | 'pageTurn' // [TODO(macOS ISS#2323203)
+  | 'group'
+  | 'list'; // ]TODO(macOS ISS#2323203)
+
+export type AccessibilityTraits =
+  | AccessibilityTrait
+  | $ReadOnlyArray<AccessibilityTrait>;
+
+export type AccessibilityComponentType =
+  | 'none'
+  | 'button'
+  | 'radiobutton_checked'
+  | 'radiobutton_unchecked';
+
+// [TODO(android ISS)
+export type AccessibilityNodeInfoProp = {
+  clickable: boolean,
+}; // ]TODO(android ISS)
+
+// This must be kept in sync with the AccessibilityRolesMask in RCTViewManager.m
+export type AccessibilityRole =
+  | 'none'
+  | 'button'
+  | 'link'
+  | 'search'
+  | 'image'
+  | 'keyboardkey'
+  | 'text'
+  | 'adjustable'
+  | 'imagebutton'
+  | 'header'
+  | 'summary'
+  | 'alert'
+  | 'checkbox'
+  | 'combobox'
+  | 'menu'
+  | 'menubar'
+  | 'menuitem'
+  | 'progressbar'
+  | 'radio'
+  | 'radiogroup'
+  | 'scrollbar'
+  | 'spinbutton'
+  | 'switch'
+  | 'tab'
+  | 'tablist'
+  | 'timer'
+  | 'toolbar'
+  | 'list' // RNW-only
+  | 'listitem'; // RNW-only
+
+export type AccessibilityState =
+  | 'selected'
+  | 'disabled'
+  | 'checked'
+  | 'unchecked'
+  | 'busy'
+  | 'expanded'
+  | 'collapsed';
+
+export type AccessibilityStates =
+  | AccessibilityState
+  | $ReadOnlyArray<AccessibilityState>;
+
+module.exports = {
+  AccessibilityTraits: [
+    'none',
+    'button',
+    'link',
+    'header',
+    'search',
+    'image',
+    'selected',
+    'plays',
+    'key',
+    'text',
+    'summary',
+    'disabled',
+    'frequentUpdates',
+    'startsMedia',
+    'adjustable',
+    'allowsDirectInteraction',
+    'pageTurn',
+    'group', // [TODO(macOS ISS#2323203)
+    'list', // ]TODO(macOS ISS#2323203)
+  ],
+  AccessibilityComponentTypes: [
+    'none',
+    'button',
+    'radiobutton_checked',
+    'radiobutton_unchecked',
+  ],
+  AccessibilityRoles: [
+    'none',
+    'button',
+    'link',
+    'search',
+    'image',
+    'keyboardkey',
+    'text',
+    'adjustable',
+    'imagebutton',
+    'header',
+    'summary',
+    'alert',
+    'checkbox',
+    'combobox',
+    'menu',
+    'menubar',
+    'menuitem',
+    'progressbar',
+    'radio',
+    'radiogroup',
+    'scrollbar',
+    'spinbutton',
+    'switch',
+    'tab',
+    'tablist',
+    'timer',
+    'toolbar',
+    'list', // RNW-only
+    'listitem', // RNW-only
+  ],
+  AccessibilityStates: [
+    'selected',
+    'disabled',
+    'checked',
+    'unchecked',
+    'busy',
+    'expanded',
+    'collapsed',
+  ],
+};
