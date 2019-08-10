1013777f57a0e758c176e4e9b2258fbde691a0cc
Ruriko Araki <ruaraki@microsoft.com>
2019-08-10 12:18:07 -0700

    Fix excessive toggles on the Switch component (#2900)
    * Convert the Switch component into Typescript for UWP only.
    
    * Update index.uwp.ts to include new Switch.uwp file
    
    * Fix switch shimmy issue
    
    * Support custom colors in Switch.uwp.tsx
    
    * Support the deprecated color props with warnings
    
    * Change files

---
+++ vnext/src/Libraries/Components/Switch/SwitchProps.ts
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ * @format
+ */
+
+import {ViewProps} from 'react-native';
+
+export interface ISwitchTrackColorProps {
+  false?: string;
+  true?: string;
+}
+
+export interface ISwitchProps extends ViewProps {
+  disabled?: boolean;
+  value?: boolean;
+  thumbColor?: string;
+  trackColor?: ISwitchTrackColorProps;
+  onChange?: (event: ISwitchChangeEvent) => void;
+  onValueChange?: (value: boolean) => void;
+
+  // deprecated
+  thumbTintColor?: string;
+  tintColor?: string;
+  onTintColor?: string;
+}
+
+export interface ISwitchChangeEvent {
+  nativeEvent: {
+    value: boolean;
+  };
+}
