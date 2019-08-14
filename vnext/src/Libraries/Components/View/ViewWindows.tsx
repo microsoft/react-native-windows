1540a280143df771c788546c0a016b85b777e6ba
REDMOND\acoates <acoates@microsoft.com>
2019-08-14 10:12:54 -0700

    Start API documentation

--- vnext/src/Libraries/Components/View/ViewWindows.tsx
+++ vnext/src/Libraries/Components/View/ViewWindows.tsx
@@ -4,9 +4,16 @@
  * @format
  */
 
+import * as React from 'react';
 import {IViewWindowsProps} from './ViewWindowsProps';
-import React = require('react');
 
+/**
+ * Same as {@link https://facebook.github.io/react-native/docs/view | react-native's View}, but with extra windows specific functionality
+ *
+ * @remarks
+ * Prop type: {@link IViewWindowsProps}.
+ *
+ */
 export class ViewWindows extends React.Component<IViewWindowsProps> {
   public render(): JSX.Element | null {
     return null;
