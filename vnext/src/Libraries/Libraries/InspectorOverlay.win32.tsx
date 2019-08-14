76538a6d840d954b9ff995fdb1161f1d2ce646e4
Luan Nguyen <kinhln@microsoft.com>
2019-08-13 17:57:00 -0700

    Fork Inspector code to avoid using Dimensions type.

---
+++ vnext/src/Libraries/Libraries/InspectorOverlay.win32.tsx
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) Microsoft, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ * @flow
+ */
+
+'use strict';
+
+const ElementBox = require('ElementBox');
+const React = require('React');
+const StyleSheet = require('StyleSheet');
+const UIManager = require('UIManager');
+const View = require('View');
+
+import type {PressEvent} from 'CoreEventTypes';
+import type {ViewStyleProp} from 'StyleSheet';
+
+type Inspected = $ReadOnly<{|
+  frame?: Object,
+  style?: ViewStyleProp,
+|}>;
+
+type Props = $ReadOnly<{|
+  inspected?: Inspected,
+  inspectedViewTag?: ?number,
+  onTouchViewTag: (tag: number, frame: Object, pointerY: number) => mixed,
+|}>;
+
+class InspectorOverlay extends React.Component<Props> {
+  findViewForTouchEvent = (e: PressEvent) => {
+    const {locationX, locationY} = e.nativeEvent.touches[0];
+    UIManager.findSubviewIn(
+      this.props.inspectedViewTag,
+      [locationX, locationY],
+      (nativeViewTag, left, top, width, height) => {
+        this.props.onTouchViewTag(
+          nativeViewTag,
+          {left, top, width, height},
+          locationY,
+        );
+      },
+    );
+  };
+
+  shouldSetResponser = (e: PressEvent): boolean => {
+    this.findViewForTouchEvent(e);
+    return true;
+  };
+
+  render() {
+    let content = null;
+    if (this.props.inspected) {
+      content = (
+        <ElementBox
+          frame={this.props.inspected.frame}
+          style={this.props.inspected.style}
+        />
+      );
+    }
+
+    return (
+      <View
+        onStartShouldSetResponder={this.shouldSetResponser}
+        onResponderMove={this.findViewForTouchEvent}
+        style={[styles.inspector, {height: "100%"}]}>
+        {content}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  inspector: {
+    backgroundColor: 'transparent',
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    right: 0,
+  },
+});
+
+module.exports = InspectorOverlay;
