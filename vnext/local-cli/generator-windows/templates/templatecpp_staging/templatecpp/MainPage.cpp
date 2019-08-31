d01cd2c82653bd264741deaa1fc139eece0eb402
Keith Melmon <kmelmon@microsoft.com>
2019-08-30 20:14:35 -0700

    temporary staging area for new cppwinrt template

---
+++ vnext/local-cli/generator-windows/templates/templatecpp_staging/templatecpp/MainPage.cpp
@@ -0,0 +1,42 @@
+﻿#include "pch.h"
+#include "MainPage.h"
+#include "MainPage.g.cpp"
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+using namespace react::uwp;
+
+namespace winrt::templatecpp::implementation
+{
+	const wchar_t* JSFILENAME = L"App.windows";
+	const wchar_t* JSCOMPONENTNAME = L"templatecpp";
+
+    MainPage::MainPage()
+    {
+        InitializeComponent();
+		LoadReact();
+    }
+
+	void MainPage::LoadReact()
+	{
+		InstanceSettings settings;
+
+		settings.UseLiveReload = true;
+		settings.UseWebDebugger = true;
+
+		auto instance = Instance::Create(winrt::hstring(JSFILENAME));
+		instance.Start(settings);
+
+		RootElement().Instance(instance);
+
+		const wchar_t* initialProps = L"{ "
+			L"\"one\":\"1\""
+			L", \"two\":\"2\""
+			L"}";
+
+		RootElement().InitialProps(winrt::hstring(initialProps));
+
+		RootElement().JsComponentName(JSCOMPONENTNAME);
+		RootElement().StartRender();
+	}
+}
