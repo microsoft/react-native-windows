7187ff5b2a6dc7b29a515addd10ad32a2ddcc4b7
Julio C. Rocha <julio@rochsquadron.net>
2019-09-12 20:52:57 -0700

    Add JSI to default unit tests.

--- vnext/Scripts/UnitTest.ps1
+++ vnext/Scripts/UnitTest.ps1
@@ -17,7 +17,10 @@ param (
 	[System.IO.FileInfo[]] $Assemblies =
 	(
 		"$PSScriptRoot\..\target\$Platform\$Configuration\" +
-		"React.Windows.Desktop.UnitTests\React.Windows.Desktop.UnitTests.dll"
+		"React.Windows.Desktop.UnitTests\React.Windows.Desktop.UnitTests.dll",
+
+		"$PSScriptRoot\..\target\$Platform\$Configuration\" +
+		"JSI.Desktop.UnitTests\JSI.Desktop.UnitTests.exe"
 	),
 
 	[System.IO.FileInfo] $VsTest = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
