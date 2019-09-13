ae102f53749aec718b628ee8a06eb8cb43154cc3
Di Da <dida@microsoft.com>
2019-09-13 08:53:19 -0700

    MultiLine TextInput should accept enter key (#3113)
    * MultiLine TextInput should accepts Enter key
    
    * run format
    
    * Change files
    
    * Minor change

--- packages/E2ETest/wdio/pages/TextInputTestPage.ts
+++ packages/E2ETest/wdio/pages/TextInputTestPage.ts
@@ -4,7 +4,10 @@
  */
 
 import { BasePage, By } from './BasePage';
-import { TEXTINPUT_ON_TEXTINPUT } from '../../app/Consts';
+import {
+  TEXTINPUT_ON_TEXTINPUT,
+  ML_TEXTINPUT_ON_TEXTINPUT,
+} from '../../app/Consts';
 
 class TextInputTestPage extends BasePage {
   isPageLoaded() {
@@ -15,12 +18,30 @@ class TextInputTestPage extends BasePage {
     this.textInput.setValue(text);
   }
 
+  clearAndTypeOnMLTextInput(text: string) {
+    this.multiLineTextInput.setValue(text);
+  }
+
+  appendNewLineOnMLText(text: string) {
+    this.multiLineTextInput.addValue('End');
+    this.multiLineTextInput.addValue('Enter');
+    this.multiLineTextInput.addValue(text);
+  }
+
   getTextInputText() {
     return this.textInput.getText();
   }
 
+  getMLTextInputText() {
+    return this.multiLineTextInput.getText();
+  }
+
   private get textInput() {
     return By(TEXTINPUT_ON_TEXTINPUT);
+  }
+
+  private get multiLineTextInput() {
+    return By(ML_TEXTINPUT_ON_TEXTINPUT);
   }
 }
 
