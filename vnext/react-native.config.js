57bb45817a68b69e5670f7300d629705befa4816
REDMOND\acoates <acoates@microsoft.com>
2019-09-17 14:11:34 -0700

    fix RNTester bundles

--- vnext/react-native.config.js
+++ vnext/react-native.config.js
@@ -1,6 +1,7 @@
 // @ts-check
 
 module.exports = {
+    // **** This section defined commands and options on how to provide the windows platform to external applications
     commands: [
       require('./local-cli/runWindows/runWindows'),
     ],
@@ -11,4 +12,8 @@ module.exports = {
         dependencyConfig: (projectRoot, dependencyParams) => null,
       },
     },
+
+    // *****
+    // This is only used when building bundles within react-native-windows.
+    reactNativePath: '.',
   };
