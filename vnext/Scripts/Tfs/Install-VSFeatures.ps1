858eed160dc544396389eea1c1b31b51905fc476
Julio C. Rocha <julio.rocha@microsoft.com>
2019-09-20 20:37:03 -0700

    Update

--- vnext/Scripts/Tfs/Install-VSFeatures.ps1
+++ vnext/Scripts/Tfs/Install-VSFeatures.ps1
@@ -14,12 +14,12 @@ Invoke-WebRequest -Method Get `
 	-Uri $InstallerUri `
 	-OutFile $VsInstaller
 
-Invoke-WebRequest -Method Get `
-	-Uri 'https://download.microsoft.com/download/8/3/4/834E83F6-C377-4DCE-A757-69A418B6C6DF/Collect.exe' `
-	-OutFile ${env:System_DefaultWorkingDirectory}\Collect.exe
+# Invoke-WebRequest -Method Get `
+# 	-Uri 'https://download.microsoft.com/download/8/3/4/834E83F6-C377-4DCE-A757-69A418B6C6DF/Collect.exe' `
+# 	-OutFile ${env:System_DefaultWorkingDirectory}\Collect.exe
 
-Write-Host "Current VC versions:"
-Get-ChildItem "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Tools\MSVC\"
+# Write-Host "Current VC versions:"
+# Get-ChildItem "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Tools\MSVC\"
 
 $argumentList = `
 	'modify',
@@ -39,14 +39,14 @@ Start-Process `
 	-PassThru `
 	-OutVariable returnCode
 
-# Should generate ${env:Temp}\vslogs.zip
-Start-Process `
-	-FilePath "${env:System_DefaultWorkingDirectory}\Collect.exe" `
-	-Wait `
-	-PassThru
+# # Should generate ${env:Temp}\vslogs.zip
+# Start-Process `
+# 	-FilePath "${env:System_DefaultWorkingDirectory}\Collect.exe" `
+# 	-Wait `
+# 	-PassThru
 
-mkdir -Force ${env:System_DefaultWorkingDirectory}\vslogs
-Copy-Item ${env:TEMP}\vslogs.zip ${env:System_DefaultWorkingDirectory}\vslogs\
+# mkdir -Force ${env:System_DefaultWorkingDirectory}\vslogs
+# Copy-Item ${env:TEMP}\vslogs.zip ${env:System_DefaultWorkingDirectory}\vslogs\
 
-Write-Host "VC versions after installation:"
-Get-ChildItem "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Tools\MSVC\"
+# Write-Host "VC versions after installation:"
+# Get-ChildItem "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Tools\MSVC\"
