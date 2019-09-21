62888c79fbb5c16c8d13007417bf9a73a3082e85
Julio C. Rocha <julio@rochsquadron.net>
2019-09-21 16:52:53 -0700

    Re-enabled Universal builds.

--- vnext/Scripts/Tfs/Install-VSFeatures.ps1
+++ vnext/Scripts/Tfs/Install-VSFeatures.ps1
@@ -14,13 +14,6 @@ Invoke-WebRequest -Method Get `
 	-Uri $InstallerUri `
 	-OutFile $VsInstaller
 
-# Invoke-WebRequest -Method Get `
-# 	-Uri 'https://download.microsoft.com/download/8/3/4/834E83F6-C377-4DCE-A757-69A418B6C6DF/Collect.exe' `
-# 	-OutFile ${env:System_DefaultWorkingDirectory}\Collect.exe
-
-# Write-Host "Current VC versions:"
-# Get-ChildItem "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Tools\MSVC\"
-
 $argumentList = `
 	'modify',
 	'--installPath', "`"$VsInstallPath`"" ,
@@ -38,15 +31,3 @@ Start-Process `
 	-Wait `
 	-PassThru `
 	-OutVariable returnCode
-
-# # Should generate ${env:Temp}\vslogs.zip
-# Start-Process `
-# 	-FilePath "${env:System_DefaultWorkingDirectory}\Collect.exe" `
-# 	-Wait `
-# 	-PassThru
-
-# mkdir -Force ${env:System_DefaultWorkingDirectory}\vslogs
-# Copy-Item ${env:TEMP}\vslogs.zip ${env:System_DefaultWorkingDirectory}\vslogs\
-
-# Write-Host "VC versions after installation:"
-# Get-ChildItem "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Tools\MSVC\"
