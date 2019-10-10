4172500ec5552179a660f593364ac5e485c2760e
Yichen Yao <yicyao@microsoft.com>
2019-10-09 18:25:14 -0700

    Add Common.vcxproj and move unicode.* and utilities.h into it.

--- vnext/Desktop.UnitTests/UnicodeConversionTest.cpp
+++ vnext/Desktop.UnitTests/UnicodeConversionTest.cpp
@@ -3,7 +3,7 @@
 
 #include <CppUnitTest.h>
 #include <string>
-#include "../ReactWindowsCore/unicode.h"
+#include "../Common/unicode.h"
 #include "UnicodeTestStrings.h"
 
 using facebook::react::unicode::utf16ToUtf8;
