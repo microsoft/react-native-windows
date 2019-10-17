fd46b522006320d3fb34611ff55e0586862898fa
Alexander Sklar <asklar@winse.microsoft.com>
2019-10-17 16:15:06 -0700

    bump

--- .ado/VSComponentList.ps1
+++ .ado/VSComponentList.ps1
@@ -1,8 +1,14 @@
+$dir = $env:temp
+if ($env:Agent_TempDirectory -ne $null)
+{
+    $dir = $env:Agent_TempDirectory
+}
+Write-Output "hello" |  Out-File $dir\foobar.txt
 $installerPath = 'C:\Program Files (x86)\Microsoft Visual Studio\Installer\'
 $installationPath = . $installerPath\vswhere.exe -latest -property installationPath
-$vsconfig = "$env:temp\vsconfig"
+$vsconfig = "$dir\vsconfig"
 Write-Host "VSConfig will be at $vsconfig"
-$p = Start-Process -PassThru $installerPath\vs_installer.exe -ArgumentList "export --installpath `"$installationPath`" --quiet --config $vsconfig" 
+$p = Start-Process -PassThru $installerPath\vs_installer.exe -RedirectStandardError $dir\err -RedirectStandardOutput $dir\out -ArgumentList "export --installpath `"$installationPath`" --quiet --config $vsconfig" 
 $p.WaitForExit()
 $x = [Datetime]::Now.AddSeconds(60)
 while (!(Test-Path $vsconfig) -and ([datetime]::Now -lt $x))
@@ -11,4 +17,6 @@ while (!(Test-Path $vsconfig) -and ([datetime]::Now -lt $x))
     Write-Host "Waiting for vsconfig file..."
 }
 
+gc $dir\err
+gc $dir\out
 gc $vsconfig
