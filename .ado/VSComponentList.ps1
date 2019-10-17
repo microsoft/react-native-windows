456dc75d4a57e7a6578a520f1840c2b19a179105
Alexander Sklar <asklar@winse.microsoft.com>
2019-10-17 15:13:16 -0700

    bump

--- .ado/VSComponentList.ps1
+++ .ado/VSComponentList.ps1
@@ -1,3 +1,4 @@
 $p = Start-Process -PassThru 'C:\Program Files (x86)\Microsoft Visual Studio\Installer\vs_installer.exe' -ArgumentList "export --installpath `"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise`" --quiet --config $env:temp\vsconfig"
 $p.WaitForExit()
+Sleep 30
 gc $env:temp\vsconfig
