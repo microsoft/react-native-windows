1cd1d5fe48d2f304c5b1b4597c7a9930c76183ff
Alexander Sklar <asklar@winse.microsoft.com>
2019-10-28 17:42:06 -0700

    Fix crash in RS5 due to missing facade property

--- vnext/ReactUWP/Views/ReactControl.h
+++ vnext/ReactUWP/Views/ReactControl.h
@@ -25,6 +25,9 @@ using namespace Windows::UI::Xaml::Media;
 namespace react {
 namespace uwp {
 
+enum class TriBit { Undefined = -1, NotSet = 0, Set = 1 };
+extern TriBit g_HasActualSizeProperty;
+
 class ReactControl : public std::enable_shared_from_this<ReactControl>, public IXamlReactControl {
  public:
   ReactControl(IXamlRootView *parent, XamlView rootView);
