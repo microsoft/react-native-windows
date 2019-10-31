e7fae895515c45c27cc7d1a49e66d45cd3f95f78
Julio C. Rocha <julio.rocha@microsoft.com>
2019-10-31 15:09:30 -0700

    Update DevSupportManager.

--- vnext/Desktop/DevSupportManager.cpp
+++ vnext/Desktop/DevSupportManager.cpp
@@ -59,14 +59,15 @@ string DevSupportManager::GetJavaScriptFromServer(
   try {
     Url url(bundleUrl);
     auto const resolveResult = m_resolver.resolve(url.host, url.port);
-    tcp::socket socket{m_context};
-    connect(socket, resolveResult);
+    boost::beast::tcp_stream stream{ m_context };
+
+    stream.connect(resolveResult);
 
     request<string_body> request{verb::get, url.Target(), 11};
     request.set(field::host, url.host);
     request.set(field::user_agent, BOOST_BEAST_VERSION_STRING);
 
-    write(socket, request);
+    write(stream, request);
 
     flat_buffer buffer;
     response<string_body> response;
@@ -74,11 +75,16 @@ string DevSupportManager::GetJavaScriptFromServer(
     parser<false, string_body> p{std::move(response)};
     p.body_limit(25 * 1024 * 1024); // 25MB (boost default of 1MB is too small for dev bundles)
 
-    read(socket, buffer, p.base());
+    read(stream, buffer, p);
     response = p.release();
     std::stringstream jsStringStream;
     jsStringStream << response.body();
     // TODO: Check if UTF-8 processing is required.
+
+    boost::system::error_code ec;
+    stream.socket().shutdown(tcp::socket::shutdown_both, ec);
+    if (ec && ec != boost::beast::errc::not_connected)
+      throw boost::beast::system_error(ec);
 
     return jsStringStream.str();
   } catch (const std::exception &e) {
@@ -93,21 +99,27 @@ void DevSupportManager::StartPollingLiveReload(const string &debugHost, std::fun
       try {
         Url url(DevServerHelper::get_OnChangeEndpointUrl(debugHost));
         auto const resolveResult = m_resolver.resolve(url.host, url.port);
-        tcp::socket socket{m_context};
-        connect(socket, resolveResult);
+
+        boost::beast::tcp_stream stream{ m_context };
+        stream.connect(resolveResult);
 
         request<string_body> request{verb::get, url.Target(), 11 /*HTTP 1.1*/};
         request.set(field::host, url.host);
         request.set(field::user_agent, BOOST_BEAST_VERSION_STRING);
         request.set(field::connection, "keep-alive");
 
-        write(socket, request);
+        write(stream, request);
         flat_buffer buffer;
         response<dynamic_body> response;
-        read(socket, buffer, response);
+        read(stream, buffer, response);
 
         if (response.result_int() == 205 /*ResetContent*/ && !token.is_canceled())
           onChangeCallback();
+
+        boost::system::error_code ec;
+        stream.socket().shutdown(tcp::socket::shutdown_both, ec);
+        if (ec && ec != boost::beast::errc::not_connected)
+          throw boost::beast::system_error(ec);
       } catch (const std::exception & /*e*/) {
         // Just let the live reload stop working when the connection fails,
         // rather than bringing down the app.
@@ -122,19 +134,24 @@ task<void> DevSupportManager::LaunchDevToolsAsync(const string &debugHost, const
   return t.then([=]() {
     Url url(DevServerHelper::get_LaunchDevToolsCommandUrl(debugHost));
     auto const resolveResult = m_resolver.resolve(url.host, url.port);
-    tcp::socket socket{m_context};
-    connect(socket, resolveResult);
+    boost::beast::tcp_stream stream{ m_context };
+    stream.connect(resolveResult);
 
     request<string_body> request{verb::get, url.Target(), 11};
     request.set(field::host, url.host);
     request.set(field::user_agent, BOOST_BEAST_VERSION_STRING);
 
-    write(socket, request);
+    write(stream, request);
 
     flat_buffer buffer;
     response<dynamic_body> response;
 
-    read(socket, buffer, response);
+    read(stream, buffer, response);
+
+    boost::system::error_code ec;
+    stream.socket().shutdown(tcp::socket::shutdown_both, ec);
+    if (ec && ec != boost::beast::errc::not_connected)
+      throw boost::beast::system_error(ec);
   });
 }
 
