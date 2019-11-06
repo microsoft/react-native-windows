333b206988be661cb1cdf9301760b441ba24facc
Canhua Li <licanhua@live.com>
2019-11-06 15:28:40 -0800

    Use Microsoft.Windows.CppWinRT 2.0 (#3596)
    * use Microsoft.Windows.CppWinRT
    remove CreateViewManagersTests
    
    * Change files
    
    * remove unused code
    
    * fix format
    
    * pipeline run
    
    * delete folders to reduce disk usage
    
    * correct display name

--- vnext/ReactUWP/Pch/pch.h
+++ vnext/ReactUWP/Pch/pch.h
@@ -17,6 +17,7 @@
 
 #include <Unknwn.h>
 
+#include <winrt/Windows.Foundation.Collections.h>
 #include <winrt/Windows.Foundation.Metadata.h>
 #include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.UI.Text.h>
