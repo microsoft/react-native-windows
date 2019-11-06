2ac13b62b2ae7beb311c35de680645d4dab97c51
Julio C. Rocha <julio.rocha@microsoft.com>
2019-11-06 14:24:33 -0800

    Corrected namespace usages in DevSupportManager.

--- vnext/Desktop/DevSupportManager.cpp
+++ vnext/Desktop/DevSupportManager.cpp
@@ -16,14 +16,14 @@
 #include "Executors/WebSocketJSExecutor.h"
 #include "Utils.h"
 
-using namespace boost::asio;
-using namespace boost::asio::ip;
 using namespace boost::beast;
-using namespace boost::beast::http;
 using namespace Concurrency;
-using namespace std;
 
+using boost::asio::ip::tcp;
 using Microsoft::React::WebSocketJSExecutor;
+using std::make_unique;
+using std::shared_ptr;
+using std::string;
 
 namespace facebook {
 namespace react {
@@ -59,32 +59,32 @@ string DevSupportManager::GetJavaScriptFromServer(
   try {
     Url url(bundleUrl);
     auto const resolveResult = m_resolver.resolve(url.host, url.port);
-    boost::beast::tcp_stream stream{m_context};
+    tcp_stream stream{m_context};
 
     stream.connect(resolveResult);
 
-    request<string_body> request{verb::get, url.Target(), 11};
-    request.set(field::host, url.host);
-    request.set(field::user_agent, BOOST_BEAST_VERSION_STRING);
+    http::request<http::string_body> request{http::verb::get, url.Target(), 11};
+    request.set(http::field::host, url.host);
+    request.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
 
-    write(stream, request);
+    http::write(stream, request);
 
     flat_buffer buffer;
-    response<string_body> response;
+    http::response<http::string_body> response;
 
-    parser<false, string_body> p{std::move(response)};
+    http::parser<false, http::string_body> p{std::move(response)};
     p.body_limit(25 * 1024 * 1024); // 25MB (boost default of 1MB is too small for dev bundles)
 
-    read(stream, buffer, p);
+    http::read(stream, buffer, p);
     response = p.release();
     std::stringstream jsStringStream;
     jsStringStream << response.body();
     // TODO: Check if UTF-8 processing is required.
 
-    boost::system::error_code ec;
+    error_code ec;
     stream.socket().shutdown(tcp::socket::shutdown_both, ec);
-    if (ec && ec != boost::beast::errc::not_connected)
-      throw boost::beast::system_error(ec);
+    if (ec && ec != errc::not_connected)
+      throw system_error(ec);
 
     return jsStringStream.str();
   } catch (const std::exception &e) {
@@ -100,26 +100,26 @@ void DevSupportManager::StartPollingLiveReload(const string &debugHost, std::fun
         Url url(DevServerHelper::get_OnChangeEndpointUrl(debugHost));
         auto const resolveResult = m_resolver.resolve(url.host, url.port);
 
-        boost::beast::tcp_stream stream{m_context};
+        tcp_stream stream{m_context};
         stream.connect(resolveResult);
 
-        request<string_body> request{verb::get, url.Target(), 11 /*HTTP 1.1*/};
-        request.set(field::host, url.host);
-        request.set(field::user_agent, BOOST_BEAST_VERSION_STRING);
-        request.set(field::connection, "keep-alive");
+        http::request<http::string_body> request{http::verb::get, url.Target(), 11 /*HTTP 1.1*/};
+        request.set(http::field::host, url.host);
+        request.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+        request.set(http::field::connection, "keep-alive");
 
-        write(stream, request);
+        http::write(stream, request);
         flat_buffer buffer;
-        response<dynamic_body> response;
-        read(stream, buffer, response);
+        http::response<http::dynamic_body> response;
+        http::read(stream, buffer, response);
 
         if (response.result_int() == 205 /*ResetContent*/ && !token.is_canceled())
           onChangeCallback();
 
-        boost::system::error_code ec;
+        error_code ec;
         stream.socket().shutdown(tcp::socket::shutdown_both, ec);
-        if (ec && ec != boost::beast::errc::not_connected)
-          throw boost::beast::system_error(ec);
+        if (ec && ec != errc::not_connected)
+          throw system_error(ec);
       } catch (const std::exception & /*e*/) {
         // Just let the live reload stop working when the connection fails,
         // rather than bringing down the app.
@@ -134,24 +134,24 @@ task<void> DevSupportManager::LaunchDevToolsAsync(const string &debugHost, const
   return t.then([=]() {
     Url url(DevServerHelper::get_LaunchDevToolsCommandUrl(debugHost));
     auto const resolveResult = m_resolver.resolve(url.host, url.port);
-    boost::beast::tcp_stream stream{m_context};
+    tcp_stream stream{m_context};
     stream.connect(resolveResult);
 
-    request<string_body> request{verb::get, url.Target(), 11};
-    request.set(field::host, url.host);
-    request.set(field::user_agent, BOOST_BEAST_VERSION_STRING);
+    http::request<http::string_body> request{http::verb::get, url.Target(), 11};
+    request.set(http::field::host, url.host);
+    request.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
 
-    write(stream, request);
+    http::write(stream, request);
 
     flat_buffer buffer;
-    response<dynamic_body> response;
+    http::response<http::dynamic_body> response;
 
-    read(stream, buffer, response);
+    http::read(stream, buffer, response);
 
-    boost::system::error_code ec;
+    error_code ec;
     stream.socket().shutdown(tcp::socket::shutdown_both, ec);
-    if (ec && ec != boost::beast::errc::not_connected)
-      throw boost::beast::system_error(ec);
+    if (ec && ec != errc::not_connected)
+      throw system_error(ec);
   });
 }
 
