18eb7583ae5a287ccb59b0c8f5fb7865b9762f16
Yichen Yao <yicyao@microsoft.com>
2019-11-18 16:28:28 -0800

    Clean up JSI.Desktop.UnitTests.

--- vnext/JSI.Desktop.UnitTests/ChakraCoreRuntimeUnitTests.cpp
+++ vnext/JSI.Desktop.UnitTests/ChakraCoreRuntimeUnitTests.cpp
@@ -1,12 +1,6 @@
 #include "JsiRuntimeUnitTests.h"
 
-#include "JSI/Shared/ChakraRuntimeArgs.h"
-#include "JSI/Shared/ChakraRuntimeFactory.h"
-#include "MemoryTracker.h"
-
-// TODO (yicyao): #2730 Introduces a vcxitem for shared test code and move this
-// there.
-#include <IntegrationTests/TestMessageQueueThread.h>
+#include "ChakraCoreRuntimeFactory.h"
 
 #include <gtest/gtest.h>
 
@@ -15,27 +9,15 @@
 using facebook::jsi::JsiRuntimeUnitTests;
 using facebook::jsi::Runtime;
 using facebook::jsi::RuntimeFactory;
-using facebook::react::CreateMemoryTracker;
-using facebook::react::MessageQueueThread;
-using Microsoft::JSI::ChakraRuntimeArgs;
-using Microsoft::JSI::makeChakraRuntime;
-using Microsoft::React::Test::TestMessageQueueThread;
+using Microsoft::JSI::ChakraCoreRuntimeArgs;
+using Microsoft::JSI::makeChakraCoreRuntime;
 
 // TODO (yicyao): #2729 We need to add tests for ChakraCoreRuntime specific
 // behaviors such as ScriptStore. This may require us to bring back JSITestBase.
 
 std::vector<RuntimeFactory> runtimeGenerators() {
-  return {[]() -> std::unique_ptr<Runtime> {
-    ChakraRuntimeArgs args{};
-
-    args.jsQueue = std::make_shared<TestMessageQueueThread>();
-
-    std::shared_ptr<MessageQueueThread> memoryTrackerCallbackQueue = std::make_shared<TestMessageQueueThread>();
-
-    args.memoryTracker = CreateMemoryTracker(std::move(memoryTrackerCallbackQueue));
-
-    return makeChakraRuntime(std::move(args));
-  }};
+  return {
+      []() -> std::unique_ptr<Runtime> { return makeChakraCoreRuntime(std::make_shared<ChakraCoreRuntimeArgs>()); }};
 }
 
 INSTANTIATE_TEST_CASE_P(ChakraRuntimeTest, JsiRuntimeUnitTests, ::testing::ValuesIn(runtimeGenerators()));
