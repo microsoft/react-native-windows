d197d4f59f1db6a59fa09aa5199231a4f19ca060
Canhua Li <licanhua@live.com>
2019-11-25 17:00:57 -0800

    changes

--- packages/microsoft-reactnative-sampleapps/windows/SampleAppCPP/pch.h
+++ packages/microsoft-reactnative-sampleapps/windows/SampleAppCPP/pch.h
@@ -25,3 +25,9 @@
 
 #include <winrt/SampleLibraryCS.SampleLibraryCS_XamlTypeInfo.h>
 #include <winrt/SampleLibraryCS.h>
+
+#include <winrt/Microsoft.UI.Xaml.Automation.Peers.h>
+#include <winrt/Microsoft.UI.Xaml.Controls.Primitives.h>
+#include <winrt/Microsoft.UI.Xaml.Controls.h>
+#include <winrt/Microsoft.UI.Xaml.Media.h>
+#include <winrt/Microsoft.UI.Xaml.XamlTypeInfo.h>
