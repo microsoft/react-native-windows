395b3e2ce1f0bdeaccd9dbdb089ad9e075b27f7d
Canhua Li <licanhua@live.com>
2019-12-03 15:16:43 -0800

    Add dependency WinUI 2.3 framework package (#3707)
    * changes
    
    * WinUI dependency
    
    * update cppwinrt version
    
    * rollback cppwinrt version for SampleApp
    
    * WinUI 2.3
    
    * fix build error
    
    * Fix e2e test

--- vnext/local-cli/generator-windows/templates/cpp/src/pch.h
+++ vnext/local-cli/generator-windows/templates/cpp/src/pch.h
@@ -17,3 +17,9 @@
 
 #include <winrt/Microsoft.ReactNative.Bridge.h>
 #include <winrt/Microsoft.ReactNative.h>
+
+#include <winrt/Microsoft.UI.Xaml.Automation.Peers.h>
+#include <winrt/Microsoft.UI.Xaml.Controls.Primitives.h>
+#include <winrt/Microsoft.UI.Xaml.Controls.h>
+#include <winrt/Microsoft.UI.Xaml.Media.h>
+#include <winrt/Microsoft.UI.Xaml.XamlTypeInfo.h>
