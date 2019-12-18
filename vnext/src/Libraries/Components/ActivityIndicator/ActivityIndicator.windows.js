086d96d78ecdb1e282f535739de4599e24dab439
Keith Melmon <kmelmon@microsoft.com>
2019-12-17 17:09:24 -0800

    CR comments

--- vnext/src/Libraries/Components/ActivityIndicator/ActivityIndicator.windows.js
+++ vnext/src/Libraries/Components/ActivityIndicator/ActivityIndicator.windows.js
@@ -1,8 +1,6 @@
 /**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
  *
  * @format
  * @flow
