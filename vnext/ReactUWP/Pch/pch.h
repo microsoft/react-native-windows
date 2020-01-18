7fd2ae98906df341446e26f311590ada1217d59d
Vladimir Morozov <vmorozov@microsoft.com>
2020-01-18 13:38:25 -0800

    Changed pch.h yo use angle brackets.

--- vnext/ReactUWP/Pch/pch.h
+++ vnext/ReactUWP/Pch/pch.h
@@ -20,11 +20,11 @@
 #include <winrt/Windows.Foundation.Collections.h>
 #include <winrt/Windows.Foundation.Metadata.h>
 #include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.UI.Composition.h>
 #include <winrt/Windows.UI.Text.h>
 #include <winrt/Windows.UI.Xaml.Controls.h>
 #include <winrt/Windows.UI.Xaml.Media.h>
 #include <winrt/Windows.UI.Xaml.h>
-#include "winrt/Windows.UI.Composition.h"
 
 #include <winrt/Microsoft.UI.Xaml.Automation.Peers.h>
 #include <winrt/Microsoft.UI.Xaml.Controls.Primitives.h>
