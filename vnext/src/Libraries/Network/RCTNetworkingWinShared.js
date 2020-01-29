d7f70938cf8aa42098e51f1c62ba3a7b6266e505
Nick Gerleman <nick@nickgerleman.com>
2020-01-29 14:01:45 -0800

    Share RCTNetworking Library Between windows and windesktop
    In microsoft/react-native, this file was named as a base .js file,
    leading it to be shared among platforms. When we copied it over, we
    defined it only for windows. This breaks bundling of desktop integration
    tests when using upstream which rely on the file.

---
+++ vnext/src/Libraries/Network/RCTNetworkingWinShared.js
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ *
+ * @format
+ * @flow
+ */
+
+'use strict';
+
+const NativeEventEmitter = require('../EventEmitter/NativeEventEmitter');
+const RCTNetworkingNative = require('../BatchedBridge/NativeModules')
+  .Networking;
+const convertRequestBody = require('./convertRequestBody');
+
+import type {RequestBody} from './convertRequestBody';
+
+import type {NativeResponseType} from './XMLHttpRequest';
+
+class RCTNetworking extends NativeEventEmitter {
+  constructor() {
+    super(RCTNetworkingNative);
+  }
+
+  sendRequest(
+    method: string,
+    trackingName: string,
+    url: string,
+    headers: Object,
+    data: RequestBody,
+    responseType: NativeResponseType,
+    incrementalUpdates: boolean,
+    timeout: number,
+    callback: (requestId: number) => any,
+    withCredentials: boolean,
+  ) {
+    const body = convertRequestBody(data);
+    RCTNetworkingNative.sendRequest(
+      {
+        method,
+        url,
+        data: {...body, trackingName},
+        headers,
+        responseType,
+        incrementalUpdates,
+        timeout,
+        withCredentials,
+      },
+      callback,
+    );
+  }
+
+  abortRequest(requestId: number) {
+    RCTNetworkingNative.abortRequest(requestId);
+  }
+
+  clearCookies(callback: (result: boolean) => any) {
+    RCTNetworkingNative.clearCookies(callback);
+  }
+}
+
+module.exports = new RCTNetworking();
