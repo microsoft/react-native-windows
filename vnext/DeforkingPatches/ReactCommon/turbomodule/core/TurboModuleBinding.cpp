a3817eede0c7813913c99e122e739b04898336fd
Jon Thysell (JAUNTY) <jthysell@microsoft.com>
2020-02-04 15:20:57 -0800

    Removed Managed.Unit tests from build for arm64

--- vnext/DeforkingPatches/ReactCommon/turbomodule/core/TurboModuleBinding.cpp
+++ vnext/DeforkingPatches/ReactCommon/turbomodule/core/TurboModuleBinding.cpp
@@ -7,8 +7,8 @@
 
 #include "TurboModuleBinding.h"
 
-#include <string>
 #include <stdexcept>
+#include <string>
 
 #include <cxxreact/SystraceSection.h>
 #include <jsireact/LongLivedObject.h>
@@ -21,8 +21,9 @@ namespace react {
 /**
  * Public API to install the TurboModule system.
  */
-TurboModuleBinding::TurboModuleBinding(const TurboModuleProviderFunctionType &moduleProvider)
-  : moduleProvider_(moduleProvider) {}
+TurboModuleBinding::TurboModuleBinding(
+    const TurboModuleProviderFunctionType &moduleProvider)
+    : moduleProvider_(moduleProvider) {}
 
 void TurboModuleBinding::install(
     jsi::Runtime &runtime,
@@ -34,7 +35,11 @@ void TurboModuleBinding::install(
           runtime,
           jsi::PropNameID::forAscii(runtime, "__turboModuleProxy"),
           1,
-          [binding](jsi::Runtime& rt, const jsi::Value& thisVal, const jsi::Value* args, size_t count) {
+          [binding](
+              jsi::Runtime &rt,
+              const jsi::Value &thisVal,
+              const jsi::Value *args,
+              size_t count) {
             return binding->jsProxy(rt, thisVal, args, count);
           }));
 }
@@ -43,7 +48,8 @@ void TurboModuleBinding::invalidate() const {
   LongLivedObjectCollection::get().clear();
 }
 
-std::shared_ptr<TurboModule> TurboModuleBinding::getModule(const std::string &name) {
+std::shared_ptr<TurboModule> TurboModuleBinding::getModule(
+    const std::string &name) {
   std::shared_ptr<TurboModule> module = nullptr;
   {
     SystraceSection s("TurboModuleBinding::getModule", "module", name);
@@ -53,12 +59,13 @@ std::shared_ptr<TurboModule> TurboModuleBinding::getModule(const std::string &na
 }
 
 jsi::Value TurboModuleBinding::jsProxy(
-    jsi::Runtime& runtime,
-    const jsi::Value& thisVal,
-    const jsi::Value* args,
+    jsi::Runtime &runtime,
+    const jsi::Value &thisVal,
+    const jsi::Value *args,
     size_t count) {
   if (count != 1) {
-    throw std::invalid_argument("TurboModuleBinding::jsProxy arg count must be 1");
+    throw std::invalid_argument(
+        "TurboModuleBinding::jsProxy arg count must be 1");
   }
   std::string moduleName = args[0].getString(runtime).utf8(runtime);
   std::shared_ptr<TurboModule> module = getModule(moduleName);
