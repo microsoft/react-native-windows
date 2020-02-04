48e484fb749f77422aec10e9c0e5a74fc2deb076
Luan Nguyen <kinhln@microsoft.com>
2020-02-04 14:58:26 -0800

    Implement Picker component for win32.

--- packages/react-native-win32/src/RNTester/js/RNTesterList.win32.ts
+++ packages/react-native-win32/src/RNTester/js/RNTesterList.win32.ts
@@ -54,6 +54,10 @@ const ComponentExamples: Array<IRNTesterExample> = [
     key: 'ImageWin32Test',
     module: require('../../Libraries/Image/Tests/ImageWin32Test'),
   },
+  {
+    key: 'PickerExample',
+    module: require('../../Libraries/Components/Picker/Tests/PickerWin32Example'),
+  }
 ];
 
 const APIExamples: Array<IRNTesterExample> = [
