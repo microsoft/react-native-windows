f0d1caea94684f28fec227399597259603b847e3
Vladimir Morozov <vmorozov@microsoft.com>
2020-02-06 11:12:55 -0800

    Removed using System.Runtime.CompilerServices from JSValueWriter

--- vnext/Microsoft.ReactNative.SharedManaged/JSValueWriter.cs
+++ vnext/Microsoft.ReactNative.SharedManaged/JSValueWriter.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Runtime.CompilerServices;
 using System.Threading;
 
 namespace Microsoft.ReactNative.Managed
