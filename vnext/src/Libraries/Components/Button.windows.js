26a93fae8a49595fb06382d5c5f71e20f763158e
Keith Melmon <kmelmon@microsoft.com>
2020-02-05 16:38:24 -0800

    merge Button

--- vnext/src/Libraries/Components/Button.windows.js
+++ vnext/src/Libraries/Components/Button.windows.js
@@ -1,21 +1,25 @@
 /**
- * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Copyright (c) Microsoft Corporation.
  * Licensed under the MIT License.
+ * 
  * @format
  * @flow
  */
 
 'use strict';
 
+const Platform = require('../Utilities/Platform');
 const React = require('react');
 const StyleSheet = require('../StyleSheet/StyleSheet');
 const Text = require('../Text/Text');
-const TouchableHighlight = require('./Touchable/TouchableHighlight'); // [TODO(windows ISS)
+const TouchableNativeFeedback = require('./Touchable/TouchableNativeFeedback');
+const TouchableOpacity = require('./Touchable/TouchableOpacity');
+const TouchableHighlight = require('./Touchable/TouchableHighlight'); // [Windows]
 const View = require('./View/View');
 
 const invariant = require('invariant');
 
-import type {PressEvent} from 'CoreEventTypes';
+import type {PressEvent} from '../Types/CoreEventTypes';
 
 type ButtonProps = $ReadOnly<{|
   /**
@@ -29,6 +33,11 @@ type ButtonProps = $ReadOnly<{|
   onPress: (event?: PressEvent) => mixed,
 
   /**
+   * If true, doesn't play system sound on touch (Android Only)
+   **/
+  touchSoundDisabled?: ?boolean,
+
+  /**
    * Color of the text (iOS), or background color of the button (Android)
    */
   color?: ?string,
@@ -39,13 +48,45 @@ type ButtonProps = $ReadOnly<{|
   hasTVPreferredFocus?: ?boolean,
 
   /**
+   * TV next focus down (see documentation for the View component).
+   *
+   * @platform android
+   */
+  nextFocusDown?: ?number,
+
+  /**
+   * TV next focus forward (see documentation for the View component).
+   *
+   * @platform android
+   */
+  nextFocusForward?: ?number,
+
+  /**
+   * TV next focus left (see documentation for the View component).
+   *
+   * @platform android
+   */
+  nextFocusLeft?: ?number,
+
+  /**
+   * TV next focus right (see documentation for the View component).
+   *
+   * @platform android
+   */
+  nextFocusRight?: ?number,
+
+  /**
+   * TV next focus up (see documentation for the View component).
+   *
+   * @platform android
+   */
+  nextFocusUp?: ?number,
+
+  /**
    * Text to display for blindness accessibility features
    */
   accessibilityLabel?: ?string,
-  /**
-   * Hint text to display blindness accessibility features
-   */
-  accessibilityHint?: ?string, // TODO(OSS Candidate ISS#2710739)
+
   /**
    * If true, disable all interactions for this component.
    */
@@ -57,22 +98,59 @@ type ButtonProps = $ReadOnly<{|
   testID?: ?string,
 |}>;
 
+/**
+ * A basic button component that should render nicely on any platform. Supports
+ * a minimal level of customization.
+ *
+ * <center><img src="img/buttonExample.png"></img></center>
+ *
+ * If this button doesn't look right for your app, you can build your own
+ * button using [TouchableOpacity](docs/touchableopacity.html)
+ * or [TouchableNativeFeedback](docs/touchablenativefeedback.html).
+ * For inspiration, look at the [source code for this button component](https://github.com/facebook/react-native/blob/master/Libraries/Components/Button.js).
+ * Or, take a look at the [wide variety of button components built by the community](https://js.coach/react-native?search=button).
+ *
+ * Example usage:
+ *
+ * ```
+ * import { Button } from 'react-native';
+ * ...
+ *
+ * <Button
+ *   onPress={onPressLearnMore}
+ *   title="Learn More"
+ *   color="#841584"
+ *   accessibilityLabel="Learn more about this purple button"
+ * />
+ * ```
+ *
+ */
+
 class Button extends React.Component<ButtonProps> {
-  render() {
+  render(): React.Node {
     const {
       accessibilityLabel,
-      accessibilityHint,
       color,
       onPress,
+      touchSoundDisabled,
       title,
       hasTVPreferredFocus,
+      nextFocusDown,
+      nextFocusForward,
+      nextFocusLeft,
+      nextFocusRight,
+      nextFocusUp,
       disabled,
       testID,
     } = this.props;
     const buttonStyles = [styles.button];
     const textStyles = [styles.text];
     if (color) {
-      buttonStyles.push({backgroundColor: color});
+      if (Platform.OS === 'ios') {
+        textStyles.push({color: color});
+      } else {
+        buttonStyles.push({backgroundColor: color});
+      }
     }
     const accessibilityStates = [];
     if (disabled) {
@@ -84,44 +162,97 @@ class Button extends React.Component<ButtonProps> {
       typeof title === 'string',
       'The title prop of a Button must be a string',
     );
-    const formattedTitle = title;
+    const formattedTitle =
+      Platform.OS === 'android' ? title.toUpperCase() : title;
+    // [Windows - render a TouchableHighlight
+    const Touchable = TouchableHighlight;
+    //  Platform.OS === 'android' ? TouchableNativeFeedback : TouchableOpacity;
+    // Windows]
     return (
-      <TouchableHighlight
+      <Touchable
         accessibilityLabel={accessibilityLabel}
-        accessibilityHint={accessibilityHint}
         accessibilityRole="button"
         accessibilityStates={accessibilityStates}
         hasTVPreferredFocus={hasTVPreferredFocus}
+        nextFocusDown={nextFocusDown}
+        nextFocusForward={nextFocusForward}
+        nextFocusLeft={nextFocusLeft}
+        nextFocusRight={nextFocusRight}
+        nextFocusUp={nextFocusUp}
         testID={testID}
         disabled={disabled}
-        onPress={onPress}>
+        onPress={onPress}
+        touchSoundDisabled={touchSoundDisabled}>
         <View style={buttonStyles}>
           <Text style={textStyles} disabled={disabled}>
             {formattedTitle}
           </Text>
         </View>
-      </TouchableHighlight>
+      </Touchable>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  button: {
-    backgroundColor: '#2196F3',
-    borderRadius: 2,
-  },
+  button: Platform.select({
+    ios: {},
+    android: {
+      elevation: 4,
+      // Material design blue from https://material.google.com/style/color.html#color-color-palette
+      backgroundColor: '#2196F3',
+      borderRadius: 2,
+    },
+    // [Windows
+    windows: {
+      backgroundColor: '#2196F3',
+      borderRadius: 2,
+    },
+    // Windows]
+}),
   text: {
     textAlign: 'center',
     padding: 8,
-    color: 'white',
-    fontWeight: '500',
+    ...Platform.select({
+      ios: {
+        // iOS blue from https://developer.apple.com/ios/human-interface-guidelines/visual-design/color/
+        color: '#007AFF',
+        fontSize: 18,
+      },
+      android: {
+        color: 'white',
+        fontWeight: '500',
+      },
+      // [Windows
+      windows: {
+        color: 'white',
+        fontWeight: '500',
+      },
+      // Windows]
+    }),
   },
-  buttonDisabled: {
-    backgroundColor: '#dfdfdf',
-  },
-  textDisabled: {
-    color: '#a1a1a1',
-  },
+  buttonDisabled: Platform.select({
+    ios: {},
+    android: {
+      elevation: 0,
+      backgroundColor: '#dfdfdf',
+    },
+    windows: {
+      backgroundColor: '#dfdfdf',
+    },
+  }),
+  textDisabled: Platform.select({
+    ios: {
+      color: '#cdcdcd',
+    },
+    android: {
+      color: '#a1a1a1',
+    },
+    // [Windows
+    windows: {
+      color: '#a1a1a1',
+    },
+    // Windows]
+  }),
 });
 
 module.exports = Button;
