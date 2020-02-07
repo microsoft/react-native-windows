a3c2150debc666507a0d76cac8f501c5901c2be6
Di Da <dida@ntdev.microsoft.com>
2020-02-07 09:57:33 -0800

    Fix some recent eslint errors

--- vnext/src/Libraries/Components/Button.windows.js
+++ vnext/src/Libraries/Components/Button.windows.js
@@ -1,7 +1,7 @@
 /**
  * Copyright (c) Microsoft Corporation.
  * Licensed under the MIT License.
- * 
+ *
  * @format
  * @flow
  */
@@ -208,7 +208,7 @@ const styles = StyleSheet.create({
       borderRadius: 2,
     },
     // Windows]
-}),
+  }),
   text: {
     textAlign: 'center',
     padding: 8,
