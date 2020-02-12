9d933b059ae0928f5d505b9b9a629f2058d67c0e
Andrew Coates (REDMOND) <acoates@microsoft.com>
2020-02-11 21:58:02 -0800

    Do not use default export to export PersonaCoin, and export PersonCoinTypes correctly

--- packages/react-native-win32/src/Libraries/PersonaCoin/PersonaCoin.tsx
+++ packages/react-native-win32/src/Libraries/PersonaCoin/PersonaCoin.tsx
@@ -7,8 +7,6 @@ import { PersonaCoinDefaultProps } from './PersonaCoinPropTypes';
 
 const RCTPersonaCoin = requireNativeComponent('RCTPersonaCoin');
 
-const PersonaCoin: React.FunctionComponent<IPersonaCoinProps> = (props: IPersonaCoinProps) => <RCTPersonaCoin {...props} />;
+export const PersonaCoin: React.FunctionComponent<IPersonaCoinProps> = (props: IPersonaCoinProps) => <RCTPersonaCoin {...props} />;
 PersonaCoin.defaultProps = PersonaCoinDefaultProps;
 PersonaCoin.displayName = 'PersonaCoin';
-
-export default PersonaCoin;
