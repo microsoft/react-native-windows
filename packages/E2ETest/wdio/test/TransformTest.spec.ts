99c634a9b89faa5381bb7069e306e32135672588
Kai Guo <kaigu@microsoft.com>
2020-02-24 14:59:20 -0800

    Comment out test

--- packages/E2ETest/wdio/test/TransformTest.spec.ts
+++ packages/E2ETest/wdio/test/TransformTest.spec.ts
@@ -19,10 +19,13 @@ describe('TransformTest', () => {
     assert.ok(result.includes('x=0;'), 'measureLayout response x=20');
   });
 
-  it('Apply 0.5 scale transform', () => {
-    TransformTestPage.clickApplyScaleTransform();
-    const result = TransformTestPage.clickMeasureLayoutAndGetResult();
-    assert.ok(result.includes('width=30'), 'measureLayout response width=30');
-    assert.ok(result.includes('x=15;'), 'measureLayout response x=15');
-  });
+  // Uncomment and run this test on 19h1+ when Platform.Version is available.
+  // https://github.com/microsoft/react-native-windows/issues/4176
+
+  // it('Apply 0.5 scale transform', () => {
+  //   TransformTestPage.clickApplyScaleTransform();
+  //   const result = TransformTestPage.clickMeasureLayoutAndGetResult();
+  //   assert.ok(result.includes('width=30'), 'measureLayout response width=30');
+  //   assert.ok(result.includes('x=15;'), 'measureLayout response x=15');
+  // });
 });
