647cf4311cf750d723f2c4a73599c234eeefd5fd
lamxdoan <lamdoan@microsoft.com>
2020-02-27 16:10:18 -0800

    Cherry-pick tabIndex fixes from master (#4207)
    * TextBox should have a default tabIndex of 0 (#4204)
    
    * TextBox should have a default tabIndex of 0
    
    * Change files
    
    * Set IsTabStop to false when tabIndex is negative (#4180)
    
    * Set IsTabStop to false when tabIndex is negative
    
    * Change files
    
    * remove unneeded static_cast
    
    * whoops actually did need the static_cast
    
    * clearValue and set -1 as default

--- vnext/ReactUWP/Views/ViewViewManager.cpp
+++ vnext/ReactUWP/Views/ViewViewManager.cpp
@@ -87,8 +87,15 @@ class ViewShadowNode : public ShadowNodeBase {
   void TabIndex(int32_t tabIndex) {
     m_tabIndex = tabIndex;
 
-    if (IsControl())
-      GetControl().TabIndex(m_tabIndex);
+    if (IsControl()) {
+      if (tabIndex < 0) {
+        GetControl().IsTabStop(false);
+        GetControl().ClearValue(winrt::Control::TabIndexProperty());
+      } else {
+        GetControl().IsTabStop(true);
+        GetControl().TabIndex(tabIndex);
+      }
+    }
   }
 
   bool OnClick() {
@@ -203,7 +210,7 @@ class ViewShadowNode : public ShadowNodeBase {
 
   bool m_enableFocusRing = true;
   bool m_onClick = false;
-  int32_t m_tabIndex = std::numeric_limits<std::int32_t>::max();
+  int32_t m_tabIndex = -1;
 
   winrt::ContentControl::GotFocus_revoker m_contentControlGotFocusRevoker{};
   winrt::ContentControl::LostFocus_revoker m_contentControlLostFocusRevoker{};
