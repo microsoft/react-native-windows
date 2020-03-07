178e2aa46c72711747d8d1eac10f798bb8ef668d
Kai Guo <kaigu@microsoft.com>
2020-03-06 17:25:00 -0800

    Add helpers

--- vnext/Microsoft.ReactNative/XamlHelper.cpp
+++ vnext/Microsoft.ReactNative/XamlHelper.cpp
@@ -11,11 +11,24 @@
 namespace winrt::Microsoft::ReactNative::implementation {
 
 Windows::UI::Xaml::Media::Brush XamlHelper::BrushFrom(JSValueArgWriter const &valueProvider) noexcept {
+  auto value = GetFollyDynamicFromValueProvider(valueProvider);
+  return react::uwp::BrushFrom(value);
+}
+
+Windows::UI::Color XamlHelper::ColorFrom(JSValueArgWriter const &valueProvider) noexcept {
+  auto value = GetFollyDynamicFromValueProvider(valueProvider);
+  return react::uwp::ColorFrom(value);
+}
+
+bool XamlHelper::IsValidColorValue(JSValueArgWriter const &valueProvider) noexcept {
+  auto value = GetFollyDynamicFromValueProvider(valueProvider);
+  return react::uwp::IsValidColorValue(value);
+}
+
+folly::dynamic XamlHelper::GetFollyDynamicFromValueProvider(JSValueArgWriter const &valueProvider) noexcept {
   auto writer = winrt::make_self<DynamicWriter>();
   valueProvider(*writer);
-
-  auto value = writer->TakeValue();
-  return react::uwp::BrushFrom(value);
+  return writer->TakeValue();
 }
 
 } // namespace winrt::Microsoft::ReactNative::implementation
