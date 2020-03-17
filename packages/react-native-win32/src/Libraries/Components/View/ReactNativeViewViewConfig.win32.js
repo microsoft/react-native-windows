72bf38ab6eef13a677556d9dc5fa7f8e3267a9b1
lenahong <lehon@microsoft.com>
2020-03-17 15:16:43 -0700

    Add cursor prop to ViewWin32 (#4227)
    * Add cursor prop to ViewWin32
    
    * Change files
    
    * api file updated
    
    * create type Cursor and add test
    
    * ran yarn api
    
    * remove layout from ViewWin32 props
    
    * update api files
    
    * Change files
    
    * update change type

--- packages/react-native-win32/src/Libraries/Components/View/ReactNativeViewViewConfig.win32.js
+++ packages/react-native-win32/src/Libraries/Components/View/ReactNativeViewViewConfig.win32.js
@@ -372,7 +372,8 @@ const ReactNativeViewConfig = {
     accessibilityHelpText: true,
     onMouseEnter: true,
     onMouseLeave: true,
-    tooltip: true
+    tooltip: true,
+    cursor: true,
     // Windows]
   },
 };
