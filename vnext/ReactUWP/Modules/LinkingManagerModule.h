f880a99a915a334670ded6125096ca2868f271f6
Vladimir Morozov <vmorozov@microsoft.com>
2020-03-21 08:39:00 -0700

    Implemented Linking module protocol activation

--- vnext/ReactUWP/Modules/LinkingManagerModule.h
+++ vnext/ReactUWP/Modules/LinkingManagerModule.h
@@ -6,20 +6,26 @@
 #include <cxxreact/CxxModule.h>
 #include <folly/dynamic.h>
 
-namespace react {
-namespace uwp {
+namespace react::uwp {
 
-class LinkingManagerModule final : public facebook::xplat::module::CxxModule {
- public:
-  LinkingManagerModule();
-  virtual ~LinkingManagerModule();
+struct LinkingManagerModule final : facebook::xplat::module::CxxModule {
+  LinkingManagerModule() noexcept;
+  ~LinkingManagerModule() noexcept override;
 
-  // CxxModule
+  static void OpenUri(winrt::Windows::Foundation::Uri const &uri) noexcept;
+  void HandleOpenUri(winrt::hstring const &uri) noexcept;
+
+ public: // CxxModule
   std::string getName() override;
   std::map<std::string, folly::dynamic> getConstants() override;
-  auto getMethods() -> std::vector<Method> override;
+  std::vector<Method> getMethods() override;
 
   static const char *name;
+
+ private:
+  static std::mutex s_mutex;
+  static winrt::Windows::Foundation::Uri s_initialUri;
+  static std::vector<LinkingManagerModule *> s_linkingModules;
 };
-} // namespace uwp
-} // namespace react
+
+} // namespace react::uwp
