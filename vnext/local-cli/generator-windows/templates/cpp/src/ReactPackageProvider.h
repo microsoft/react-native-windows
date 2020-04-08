f4ca47692485f0991c4b1175deac5d7f4b19b74f
Kai Guo <guokai.ok@gmail.com>
2020-04-08 12:43:01 -0700

    Remove "Using namespace" in ReactPackageProvider.h (#4525)
    * Remove using namespace
    
    * Change files

--- vnext/local-cli/generator-windows/templates/cpp/src/ReactPackageProvider.h
+++ vnext/local-cli/generator-windows/templates/cpp/src/ReactPackageProvider.h
@@ -2,17 +2,13 @@
 
 #include "winrt/Microsoft.ReactNative.h"
 // clang-format off
-using namespace winrt::Microsoft::ReactNative;
-
 namespace winrt::<%=ns%>::implementation
 {
-
-    struct ReactPackageProvider : winrt::implements<ReactPackageProvider, IReactPackageProvider>
+    struct ReactPackageProvider : winrt::implements<ReactPackageProvider, winrt::Microsoft::ReactNative::IReactPackageProvider>
     {
     public: // IReactPackageProvider
-        void CreatePackage(IReactPackageBuilder const &packageBuilder) noexcept;
+        void CreatePackage(winrt::Microsoft::ReactNative::IReactPackageBuilder const &packageBuilder) noexcept;
     };
-
 } // namespace winrt::<%=ns%>::implementation
 
 // clang-format on
