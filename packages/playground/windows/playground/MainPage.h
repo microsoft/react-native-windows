ddbfb0469a074511d93b3e10b3dfdfe39a2e9e73
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2020-04-09 12:45:00 -0700

    Move playground app to use Microsoft.ReactNative (#4534)
    * Move playground app to use Microsoft.ReactNative
    
    * formatting
    
    * Bunch of code review feedback
    
    * Change files
    
    * formatting
    
    * Delete unneeded gitignore
    
    * Minor removal of diff
    
    * Minor changes to try to avoid having to update cert all the time
    
    * Fix bundle command
    
    * No need to build rntester seperately, the release build of playground will do it from msbuild
    
    * Remove test property

---
+++ packages/playground/windows/playground/MainPage.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "MainPage.g.h"
+
+namespace winrt::playground::implementation {
+
+struct MainPage : MainPageT<MainPage> {
+  MainPage();
+
+  void OnLoadClick(
+      Windows::Foundation::IInspectable const & /*sender*/,
+      Windows::UI::Xaml::RoutedEventArgs const & /*args*/);
+
+ private:
+  Microsoft::ReactNative::ReactNativeHost Host() noexcept;
+  Microsoft::ReactNative::ReactInstanceSettings InstanceSettings() noexcept;
+  Windows::Foundation::Collections::IVector<Microsoft::ReactNative::IReactPackageProvider> PackageProviders() noexcept;
+
+  Microsoft::ReactNative::ReactRootView m_reactRootView;
+  Microsoft::ReactNative::ReactNativeHost m_host;
+  Microsoft::ReactNative::ReactInstanceSettings m_instanceSettings;
+  Windows::Foundation::Collections::IVector<Microsoft::ReactNative::IReactPackageProvider> m_packageProviders;
+
+  bool m_useWebDebugger{false};
+
+ public:
+  void x_entryPointCombo_SelectionChanged(
+      winrt::Windows::Foundation::IInspectable const &sender,
+      winrt::Windows::UI::Xaml::Controls::SelectionChangedEventArgs const &e);
+};
+
+} // namespace winrt::playground::implementation
+namespace winrt::playground::factory_implementation {
+struct MainPage : MainPageT<MainPage, implementation::MainPage> {};
+} // namespace winrt::playground::factory_implementation
