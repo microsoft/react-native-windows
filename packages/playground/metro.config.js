45c4c5b0cbcfcc7f9549cbe67211be473a019ec9
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2020-04-15 15:22:11 -0700

    This is an alternate metro bundler fix for multiple out of tree platforms (#4576)
    * Use a custom resolver wrapper to allow metro to run for multiple platforms at once
    
    * Change files
    
    * More simplification of metro configs
    
    * CI fixes
    
    * Fix bundle command on init app to properly use windows platform
    
    * More simplification of metro configs
    
    * Fix asset loading

--- packages/playground/metro.config.js
+++ packages/playground/metro.config.js
@@ -8,9 +8,6 @@ const fs = require('fs');
 const path = require('path');
 const blacklist = require('metro-config/src/defaults/blacklist');
 
-const rnPath = fs.realpathSync(
-  path.resolve(require.resolve('react-native/package.json'), '..'),
-);
 const rnwPath = fs.realpathSync(
   path.resolve(require.resolve('react-native-windows/package.json'), '..'),
 );
@@ -25,20 +22,14 @@ module.exports = {
   ],
 
   resolver: {
+    resolveRequest: require('react-native-windows/metro-react-native-platform').reactNativePlatformResolver(
+      {windows: 'react-native-windows'},
+    ),
     extraNodeModules: {
-      // Redirect react-native to react-native-windows
-      'react-native': rnwPath,
+      // Redirect react-native-windows to avoid symlink (metro doesn't like symlinks)
       'react-native-windows': rnwPath,
     },
-    // Include the macos platform in addition to the defaults because the fork includes macos, but doesn't declare it
-    platforms: ['ios', 'android', 'windesktop', 'windows', 'web', 'macos'],
-    // Since there are multiple copies of react-native, we need to ensure that metro only sees one of them
-    // This should go in RN 0.61 when haste is removed
     blacklistRE: blacklist([
-      new RegExp(
-        `${(path.resolve(rnPath) + path.sep).replace(/[/\\]/g, '/')}.*`,
-      ),
-
       // This stops "react-native run-windows" from causing the metro server to crash if its already running
       new RegExp(
         `${path.resolve(__dirname, 'windows').replace(/[/\\]/g, '/')}.*`,
@@ -46,6 +37,9 @@ module.exports = {
     ]),
   },
   transformer: {
+    // The cli defaults this to a full path to react-native, which bypasses the reactNativePlatformResolver above
+    // Hopefully we can fix the default in the future
+    assetRegistryPath: 'react-native/Libraries/Image/AssetRegistry',
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
