19f12dadcdeefa06db4203510d58ee6af64b4221
Alexander Sklar <asklar@winse.microsoft.com>
2020-04-15 07:19:18 -0700

    add test metadata/selection behavior

--- packages/E2ETest/run_wdio.js
+++ packages/E2ETest/run_wdio.js
@@ -5,10 +5,53 @@ const xml2js = require('xml2js');
 const parser = new xml2js.Parser({ attrkey: 'ATTR' });
 
 const specFolder = 'wdio/test';
-let opts = process.argv
-  .splice(2)
-  .map(spec => path.join(specFolder, spec + '.spec.ts'));
 
+function GetMetadata(specPath) {
+  const contents = fs.readFileSync(specPath);
+  const metadataTag = '// @metadata ';
+  const metadataStart = contents.indexOf(metadataTag);
+  if (metadataStart != -1) {
+    let metadata = contents
+      .toString()
+      .substr(metadataStart + metadataTag.length)
+      .split(/[\r\n]/)[0];
+    return metadata.split(' ');
+  }
+  return [];
+}
+
+const filters = {
+  SkipCI: specPath => {
+    return process.env.BUILD_QUEUEDBY == 'GitHub';
+  },
+};
+
+// Returns true if the spec is to run.
+// Specs marked SkipCI are excluded from CI (identified by environment variables in the ADO lab)
+function FilterSpec(specPath) {
+  const metadata = GetMetadata(specPath);
+  for (let i = 0; i < metadata.length; i++) {
+    if (filters[metadata[i]](specPath)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function SelectSpecs(folder) {
+  let specs = [];
+  if (process.argv.length > 2) {
+    specs = process.argv.splice(2).map(spec => spec + '.spec.ts');
+  } else {
+    specs = fs.readdirSync(folder).filter(x => x.endsWith('.spec.ts'));
+  }
+  specs = specs.map(spec => path.join(folder, spec)).filter(FilterSpec);
+  return specs;
+}
+
+let opts = SelectSpecs(specFolder);
+console.log(opts);
+process.exit(0);
 const wdio = new Launcher('wdio.conf.js', { specs: opts });
 
 function parseLog(logfile) {
