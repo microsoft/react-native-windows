d54bbcabb54baee0d08dabdf80e0a6c1b07b6051
Julio C. Rocha <julio@rochsquadron.net>
2020-04-15 15:16:21 -0700

    Define MockDataWriter

--- vnext/Desktop.UnitTests/WinRTNetworkingMocks.cpp
+++ vnext/Desktop.UnitTests/WinRTNetworkingMocks.cpp
@@ -5,11 +5,12 @@
 
 using namespace winrt::Windows::Foundation;
 using namespace winrt::Windows::Networking::Sockets;
+using namespace winrt::Windows::Storage::Streams;
 
+using std::exception;
 using winrt::auto_revoke_t;
 using winrt::event_token;
 using winrt::param::hstring;
-using winrt::Windows::Storage::Streams::IOutputStream;
 
 namespace Microsoft::React::Test {
 
@@ -20,21 +21,21 @@ IOutputStream MockMessageWebSocket::OutputStream() const {
   if (OutputStreamMock)
     return OutputStreamMock();
 
-  throw std::exception("Not implemented");
+  throw exception("Not implemented");
 }
 
 IAsyncAction MockMessageWebSocket::ConnectAsync(Uri const &uri) const {
   if (ConnectAsyncMock)
     return ConnectAsyncMock(uri);
 
-  throw std::exception("Not implemented");
+  throw exception("Not implemented");
 }
 
 void MockMessageWebSocket::SetRequestHeader(hstring const &headerName, hstring const &headerValue) const {
   if (SetRequestHeaderMock)
-    SetRequestHeaderMock(headerName, headerValue);
+    return SetRequestHeaderMock(headerName, headerValue);
 
-  throw std::exception("Not implemented");
+  throw exception("Not implemented");
 }
 
 event_token MockMessageWebSocket::Closed(
@@ -42,7 +43,7 @@ event_token MockMessageWebSocket::Closed(
   if (ClosedTokenMock)
     return ClosedTokenMock(eventHandler);
 
-  throw std::exception("Not implemented");
+  throw exception("Not implemented");
 }
 
 IWebSocket::Closed_revoker MockMessageWebSocket::Closed(
@@ -51,7 +52,7 @@ IWebSocket::Closed_revoker MockMessageWebSocket::Closed(
   if (ClosedRevokerMock)
     return ClosedRevokerMock(autoRevoke, eventHandler);
 
-  throw std::exception("Not implemented");
+  throw exception("Not implemented");
 }
 
 void MockMessageWebSocket::Closed(event_token const &eventCookie) const noexcept {
@@ -61,9 +62,9 @@ void MockMessageWebSocket::Closed(event_token const &eventCookie) const noexcept
 
 void MockMessageWebSocket::Close(uint16_t code, hstring const &reason) const {
   if (CloseMock)
-    CloseMock(code, reason);
+    return CloseMock(code, reason);
 
-  throw std::exception("Not implemented");
+  throw exception("Not implemented");
 }
 
 // IMessageWebSocket
@@ -71,14 +72,14 @@ MessageWebSocketControl MockMessageWebSocket::Control() const {
   if (ControlMock)
     return ControlMock();
 
-  throw std::exception("Not implemented");
+  throw exception("Not implemented");
 }
 
 MessageWebSocketInformation MockMessageWebSocket::Information() const {
   if (InformationMock)
-    InformationMock();
+    return InformationMock();
 
-  throw std::exception("Not implemented");
+  throw exception("Not implemented");
 }
 
 event_token MockMessageWebSocket::MessageReceived(
@@ -86,7 +87,7 @@ event_token MockMessageWebSocket::MessageReceived(
   if (MessageReceivedTokenMock)
     return MessageReceivedTokenMock(eventHandler);
 
-  throw std::exception("Not implemented");
+  throw exception("Not implemented");
 }
 
 IMessageWebSocket::MessageReceived_revoker MockMessageWebSocket::MessageReceived(
@@ -95,7 +96,7 @@ IMessageWebSocket::MessageReceived_revoker MockMessageWebSocket::MessageReceived
   if (MessageReceivedRevokerMock)
     return MessageReceivedRevokerMock(autoRevoke, eventHandler);
 
-  throw std::exception("Not implemented");
+  throw exception("Not implemented");
 }
 
 void MockMessageWebSocket::MessageReceived(event_token const &eventCookie) const noexcept {
@@ -104,5 +105,196 @@ void MockMessageWebSocket::MessageReceived(event_token const &eventCookie) const
 }
 
 #pragma endregion MockMessageWebSocket
+
+#pragma region MockDataWriter
+
+uint32_t MockDataWriter::UnstoredBufferLength() const {
+  if (UnstoredBufferLengthMock)
+    return UnstoredBufferLengthMock();
+
+  throw exception("Not implemented");
+}
+
+UnicodeEncoding MockDataWriter::UnicodeEncoding() const {
+  if (GetUnicodeEncodingMock)
+    return GetUnicodeEncodingMock();
+
+  throw exception("Not implemented");
+}
+
+void MockDataWriter::UnicodeEncoding(winrt::Windows::Storage::Streams::UnicodeEncoding const &value) const {
+  if (SetUnicodeEncodingMock)
+    return SetUnicodeEncodingMock(value);
+
+  throw exception("Not implemented");
+}
+
+ByteOrder MockDataWriter::ByteOrder() const {
+  if (GetByteOrderMock)
+    return GetByteOrderMock();
+
+  throw exception("Not implemented");
+}
+
+void MockDataWriter::ByteOrder(winrt::Windows::Storage::Streams::ByteOrder const &value) const {
+  if (SetByteOrderMock)
+    return SetByteOrderMock(value);
+
+  throw exception("Not implemented");
+}
+
+void MockDataWriter::WriteByte(uint8_t value) const {
+  if (WriteByteMock)
+    return WriteByteMock(value);
+
+  throw exception("Not implemented");
+}
+
+void MockDataWriter::WriteBytes(winrt::array_view<uint8_t const> value) const {
+  if (WriteBytesMock)
+    return WriteBytesMock(value);
+
+  throw exception("Not implemented");
+}
+
+void MockDataWriter::WriteBuffer(IBuffer const &buffer) const {
+  if (WriteBufferMock)
+    return WriteBufferMock(buffer);
+
+  throw exception("Not implemented");
+}
+
+void MockDataWriter::WriteBuffer(IBuffer const &buffer, uint32_t start, uint32_t count) const {
+  if (WriteBufferRangeMock)
+    return WriteBufferRangeMock(buffer, start, count);
+
+  throw exception("Not implemented");
+}
+
+void MockDataWriter::WriteBoolean(bool value) const {
+  if (WriteBooleanMock)
+    return WriteBooleanMock(value);
+
+  throw exception("Not implemented");
+}
+
+void MockDataWriter::WriteGuid(winrt::guid const &value) const {
+  if (WriteGuidMock)
+    return WriteGuidMock(value);
+
+  throw exception("Not implemented");
+}
+
+void MockDataWriter::WriteInt16(int16_t value) const {
+  if (WriteInt16Mock)
+    return WriteInt16Mock(value);
+
+  throw exception("Not implemented");
+}
+
+void MockDataWriter::WriteInt32(int32_t value) const {
+  if (WriteInt32Mock)
+    return WriteInt32Mock(value);
+
+  throw exception("Not implemented");
+}
+
+void MockDataWriter::WriteInt64(int64_t value) const {
+  if (WriteInt64Mock)
+    return WriteInt64Mock(value);
+
+  throw exception("Not implemented");
+}
+
+void MockDataWriter::WriteUInt16(uint16_t value) const {
+  if (WriteUInt16Mock)
+    return WriteUInt16Mock(value);
+}
+
+void MockDataWriter::WriteUInt32(uint32_t value) const {
+  if (WriteUInt32Mock)
+    return WriteUInt32Mock(value);
+
+  throw exception("Not implemented");
+}
+
+void MockDataWriter::WriteUInt64(uint64_t value) const {
+  if (WriteUInt64Mock)
+    return WriteUInt64Mock(value);
+
+  throw exception("Not implemented");
+}
+
+void MockDataWriter::WriteSingle(float value) const {
+  if (WriteSingleMock)
+    return WriteSingleMock(value);
+
+  throw exception("Not implemented");
+}
+
+void MockDataWriter::WriteDouble(double value) const {
+  if (WriteDoubleMock)
+    return WriteDoubleMock(value);
+
+  throw exception("Not implemented");
+}
+
+void MockDataWriter::WriteDateTime(DateTime const &value) const {
+  if (WriteDateTimeMock)
+    return WriteDateTimeMock(value);
+
+  throw exception("Not implemented");
+}
+
+void MockDataWriter::WriteTimeSpan(TimeSpan const &value) const {
+  if (WriteTimeSpanMock)
+    return WriteTimeSpanMock(value);
+
+  throw exception("Not implemented");
+}
+
+uint32_t MockDataWriter::WriteString(hstring const &value) const {
+  if (WriteStringMock)
+    return WriteStringMock(value);
+
+  throw exception("Not implemented");
+}
+
+uint32_t MockDataWriter::MeasureString(hstring const &value) const {
+  if (MeasureStringMock)
+    return MeasureStringMock(value);
+
+  throw exception("Not implemented");
+}
+
+DataWriterStoreOperation MockDataWriter::StoreAsync() const {
+  if (StoreAsyncMock)
+    return StoreAsyncMock();
+
+  throw exception("Not implemented");
+}
+
+IAsyncOperation<bool> MockDataWriter::FlushAsync() const {
+  if (FlushAsyncMock)
+    return FlushAsyncMock();
+
+  throw exception("Not implemented");
+}
+
+IBuffer MockDataWriter::DetachBuffer() const {
+  if (DetachBufferMock)
+    return DetachBufferMock();
+
+  throw exception("Not implemented");
+}
+
+IOutputStream MockDataWriter::DetachStream() const {
+  if (DetachStreamMock)
+    return DetachStreamMock();
+
+  throw exception("Not implemented");
+}
+
+#pragma endregion MockDataWriter
 
 } // namespace Microsoft::React::Test
--- vnext/Desktop.UnitTests/WinRTNetworkingMocks.h
+++ vnext/Desktop.UnitTests/WinRTNetworkingMocks.h
@@ -114,4 +114,74 @@ struct MockMessageWebSocket : public winrt::Windows::Networking::Sockets::IMessa
 
 }; // MockMessageWebSocket
 
+struct MockDataWriter : public winrt::Windows::Storage::Streams::IDataWriter {
+#pragma region Mocks
+
+  std::function<std::uint32_t() /*const*/> UnstoredBufferLengthMock;
+  std::function<winrt::Windows::Storage::Streams::UnicodeEncoding() /*const*/> GetUnicodeEncodingMock;
+  std::function<void(winrt::Windows::Storage::Streams::UnicodeEncoding const &value) /*const*/> SetUnicodeEncodingMock;
+  std::function<winrt::Windows::Storage::Streams::ByteOrder() /*const*/> GetByteOrderMock;
+  std::function<void(winrt::Windows::Storage::Streams::ByteOrder const &value) /*const*/> SetByteOrderMock;
+  std::function<void(std::uint8_t value) /*const*/> WriteByteMock;
+  std::function<void(winrt::array_view<std::uint8_t const> value) /*const*/> WriteBytesMock;
+  std::function<void(winrt::Windows::Storage::Streams::IBuffer const &buffer) /*const*/> WriteBufferMock;
+  std::function<
+      void(winrt::Windows::Storage::Streams::IBuffer const &buffer, std::uint32_t start, std::uint32_t count) /*const*/>
+      WriteBufferRangeMock;
+  std::function<void(bool value) /*const*/> WriteBooleanMock;
+  std::function<void(winrt::guid const &value) /*const*/> WriteGuidMock;
+  std::function<void(std::int16_t value) /*const*/> WriteInt16Mock;
+  std::function<void(std::int32_t value) /*const*/> WriteInt32Mock;
+  std::function<void(std::int64_t value) /*const*/> WriteInt64Mock;
+  std::function<void(std::uint16_t value) /*const*/> WriteUInt16Mock;
+  std::function<void(std::uint32_t value) /*const*/> WriteUInt32Mock;
+  std::function<void(std::uint64_t value) /*const*/> WriteUInt64Mock;
+  std::function<void(float value) /*const*/> WriteSingleMock;
+  std::function<void(double value) /*const*/> WriteDoubleMock;
+  std::function<void(winrt::Windows::Foundation::DateTime const &value) /*const*/> WriteDateTimeMock;
+  std::function<void(winrt::Windows::Foundation::TimeSpan const &value) /*const*/> WriteTimeSpanMock;
+  std::function<std::uint32_t(winrt::param::hstring const &value) /*const*/> WriteStringMock;
+  std::function<std::uint32_t(winrt::param::hstring const &value) /*const*/> MeasureStringMock;
+  std::function<winrt::Windows::Storage::Streams::DataWriterStoreOperation() /*const*/> StoreAsyncMock;
+  std::function<winrt::Windows::Foundation::IAsyncOperation<bool>() /*const*/> FlushAsyncMock;
+  std::function<winrt::Windows::Storage::Streams::IBuffer() /*const*/> DetachBufferMock;
+  std::function<winrt::Windows::Storage::Streams::IOutputStream() /*const*/> DetachStreamMock;
+
+#pragma endregion Mocks
+
+#pragma region IDataWriter overrides
+
+  std::uint32_t UnstoredBufferLength() const;
+  winrt::Windows::Storage::Streams::UnicodeEncoding UnicodeEncoding() const;
+  void UnicodeEncoding(winrt::Windows::Storage::Streams::UnicodeEncoding const &value) const;
+  winrt::Windows::Storage::Streams::ByteOrder ByteOrder() const;
+  void ByteOrder(winrt::Windows::Storage::Streams::ByteOrder const &value) const;
+  void WriteByte(std::uint8_t value) const;
+  void WriteBytes(winrt::array_view<std::uint8_t const> value) const;
+  void WriteBuffer(winrt::Windows::Storage::Streams::IBuffer const &buffer) const;
+  void WriteBuffer(winrt::Windows::Storage::Streams::IBuffer const &buffer, std::uint32_t start, std::uint32_t count)
+      const;
+  void WriteBoolean(bool value) const;
+  void WriteGuid(winrt::guid const &value) const;
+  void WriteInt16(std::int16_t value) const;
+  void WriteInt32(std::int32_t value) const;
+  void WriteInt64(std::int64_t value) const;
+  void WriteUInt16(std::uint16_t value) const;
+  void WriteUInt32(std::uint32_t value) const;
+  void WriteUInt64(std::uint64_t value) const;
+  void WriteSingle(float value) const;
+  void WriteDouble(double value) const;
+  void WriteDateTime(winrt::Windows::Foundation::DateTime const &value) const;
+  void WriteTimeSpan(winrt::Windows::Foundation::TimeSpan const &value) const;
+  std::uint32_t WriteString(winrt::param::hstring const &value) const;
+  std::uint32_t MeasureString(winrt::param::hstring const &value) const;
+  winrt::Windows::Storage::Streams::DataWriterStoreOperation StoreAsync() const;
+  winrt::Windows::Foundation::IAsyncOperation<bool> FlushAsync() const;
+  winrt::Windows::Storage::Streams::IBuffer DetachBuffer() const;
+  winrt::Windows::Storage::Streams::IOutputStream DetachStream() const;
+
+#pragma endregion IDataWriter overrides
+
+}; // MockDataWriter
+
 } // namespace Microsoft::React::Test
