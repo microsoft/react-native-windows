4c320cffcdae239af2f00bba1982f71d65146d05
Alexander Sklar <asklar@microsoft.com>
2020-04-16 10:03:43 -0700

    Fix E2E Tests, add CI break on test failure, improve TreeDumpLibrary (#4585)
    Improve/refactor e2etest and TreeDumpLibrary in a number of ways:
    
    most importantly is that we've had test break but not fail the PR or CI - this is because wdio which we use as a test harness, does not set its exit code on success/failure, which is needed for Azure Pipelines to know whether it should fail the build or not.
    image
    
    Because we were blind to test failures, the outputs have drifted away from the original masters. I'm re-baselining them based on what we have today. Component owners should do a pass on the new masters to make sure nothing looks weird (e.g. ImageRTL still shows FlowDirection=LeftToRight). Filed #4589 to track this.
    
    changes the test app UI to provide links to the master and output files, and to diff the two. I wanted to launch vscode in diff mode for that but since we are a UWP app, the only way we can launch a program like that is to use a protocol handler, which vscode doesn't expose for diffing. Filed: Provide a protocol handler for diffing two files. For the time being, we'll copy the command line to launch vscode in diff mode for the two files.
    
    Adds the ability for the tree dump library to output json which will allow us to write tests that validate the output from the javascript side
    
    Adds logic for optionally output the name of elements. I plan to turn this and json output on soon but wanted to get a baseline PR/CI first.
    
    e2etest was crashing because of a bug in Switch: #4596 . Disabled that control in the test app for the time being.
    
    Added logic to the pipeline to capture crash dumps when e2etest app crashes
    
    Added checks to make sure we don't try to build/run the e2etest app in scenarios where it isn't really supported because the masters would not match (anything other than Release|x64 at 100% DPI). There are bugs tracking these limitations that we must fix before rtm: #4122 and #4619

--- packages/E2ETest/windows/ReactUWPTestApp/copyTreeDump.cmd
+++ packages/E2ETest/windows/ReactUWPTestApp/copyTreeDump.cmd
@@ -1,1 +1,1 @@
-copy %LocalAppData%\Packages\ReactUWPTestApp_kc2bncckyf4ap\LocalState\TreeDump\*.out Assets\TreeDump\*.txt
+@copy /d %LocalAppData%\Packages\ReactUWPTestApp_kc2bncckyf4ap\LocalState\TreeDump\*.json %~dp0%\Assets\TreeDump\masters\*.json
