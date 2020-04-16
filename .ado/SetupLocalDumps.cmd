9b02533ba8a8ca7cb607643af93c1268b808895b
Alexander Sklar <asklar@microsoft.com>
2020-04-16 08:45:21 -0700

    disable Switch due to bug 4596

--- .ado/SetupLocalDumps.cmd
+++ .ado/SetupLocalDumps.cmd
@@ -1,3 +1,11 @@
+setlocal
+set CRASHDUMPS_FOLDER=%BUILD_ARTIFACTSTAGINGDIRECTORY%\ReactUWPTestAppTreeDump\CrashDumps
 reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\Windows Error Reporting\LocalDumps" /s
-reg add  "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\Windows Error Reporting\LocalDumps\ReactUWPTestApp.exe" /v DumpFolder /t REG_SZ /d %BUILD_ARTIFACTSTAGINGDIRECTORY%\ReactUWPTestAppTreeDump
+reg add  "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\Windows Error Reporting\LocalDumps\ReactUWPTestApp.exe" /v DumpFolder /t REG_SZ /d %CRASHDUMPS_FOLDER%
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\Windows Error Reporting\LocalDumps\ReactUWPTestApp.exe" /v DumpType /t REG_DWORD /d 2
+if not exist %CRASHDUMPS_FOLDER% ( 
+    md %CRASHDUMPS_FOLDER%
+) else (
+    del %CRASHDUMPS_FOLDER%\* /s /q
+)
+endlocal
