daeed94d7487f346cc78ee8a282ea8605f67a04d
Andreas Eulitz <aeulitz@microsoft.com>
2020-04-17 12:57:37 -0700

    create MS.ReactNative.IntegrationTests project

---
+++ vnext/Microsoft.ReactNative.IntegrationTests/FootInTheDoor.cpp
@@ -0,0 +1,11 @@
+#include "pch.h"
+
+namespace Microsoft::ReactNative::IntegrationTests {
+
+TEST_CLASS (FootInTheDoorClass) {
+  TEST_METHOD(TestReadCustomType) {
+    TestCheck(1 == 1);
+  }
+};
+
+} // namespace Microsoft::ReactNative::IntegrationTests
---
+++ vnext/Microsoft.ReactNative.IntegrationTests/pch.h
@@ -0,0 +1,8 @@
+#pragma once
+
+
+#define NOMINMAX
+
+#include "gtest/gtest.h"
+#include "motifCpp/gTestAdapter.h"
+#include "motifCpp/testCheck.h"
