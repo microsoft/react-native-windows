c779043f0dd5e57f66b7609a8a09270aa78ad1b7
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2020-04-21 15:00:12 -0700

    Opening the devmenu should attempt to connect to dev tools (#4660)
    * Fix devtools connection to match RN0.62
    
    * Change files
    
    * review feedback

--- vnext/Microsoft.ReactNative/Views/ReactRootControl.cpp
+++ vnext/Microsoft.ReactNative/Views/ReactRootControl.cpp
@@ -564,6 +564,12 @@ void ReactRootControl::ShowDeveloperMenu() noexcept {
     auto xamlRootGrid{xamlRootView.as<winrt::Grid>()};
     xamlRootGrid.Children().Append(m_developerMenuRoot);
   }
+
+  // Notify instance that dev menu is shown -- This is used to trigger a connection to dev tools
+  if (auto instance = m_weakReactInstance.GetStrongPtr()) {
+    query_cast<Mso::React::ILegacyReactInstance &>(*instance).CallJsFunction(
+        "RCTNativeAppEventEmitter", "emit", folly::dynamic::array("RCTDevMenuShown"));
+  }
 }
 
 void ReactRootControl::DismissDeveloperMenu() noexcept {
