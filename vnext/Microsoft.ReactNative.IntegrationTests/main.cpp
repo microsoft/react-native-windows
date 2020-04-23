bdc22d99d63998802228ef08ffe2327f317dedd3
Andreas Eulitz <aeulitz@microsoft.com>
2020-04-22 23:28:55 -0700

    update

--- vnext/Microsoft.ReactNative.IntegrationTests/main.cpp
+++ vnext/Microsoft.ReactNative.IntegrationTests/main.cpp
@@ -2,7 +2,7 @@
 #include "motifCpp/gTestAdapter.h"
 
 int main(int argc, char **argv) {
-    Mso::UnitTests::GTest::RegisterUnitTests();
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+  Mso::UnitTests::GTest::RegisterUnitTests();
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
--- vnext/Microsoft.ReactNative.IntegrationTests/pch.h
+++ vnext/Microsoft.ReactNative.IntegrationTests/pch.h
@@ -1,6 +1,5 @@
 #pragma once
 
-
 #define NOMINMAX
 
 #include "gtest/gtest.h"
