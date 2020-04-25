2f056b53535915f2370cf8b664b4f6fa272d1075
Alexander Sklar <asklar@microsoft.com>
2020-04-25 15:28:28 -0700

    use XAML_NAMESPACE macro in IDL

--- vnext/include/NamespaceRedirect.h
+++ vnext/include/NamespaceRedirect.h
@@ -6,11 +6,3 @@
 #define XAML_NAMESPACE Microsoft.UI.Xaml
 #endif
 
-#define CONCAT_(X, Y) X##Y
-#define CONCAT(X, Y) CONCAT_(X, Y)
-#define MAKE_CONCAT(X) CONCAT(XAML_NAMESPACE, X)
-#define DOT .
-
-/// WUXMUX_TYPE(x) generates a the equivalent of Windows.UI.Xaml.x or Microsoft.UI.Xaml.x,
-// depending on whether WINUI3_SUPPORT is defined or not.
-#define WUXMUX_TYPE(X) MAKE_CONCAT(CONCAT(DOT, X))
