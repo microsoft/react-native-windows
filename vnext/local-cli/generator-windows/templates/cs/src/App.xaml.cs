bee9699d2618e5198a716191e28db86341389840
Kai Guo <kaigu@microsoft.com>
2020-05-04 12:03:38 -0700

    Add MIT license in templates

--- vnext/local-cli/generator-windows/templates/cs/src/App.xaml.cs
+++ vnext/local-cli/generator-windows/templates/cs/src/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.ReactNative;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.ReactNative;
 
 namespace <%=ns%>
 {
