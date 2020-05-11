43b3367b52b6090ebb5e883ae0c6643bfc5f66c8
Pat Boyd <ppatboyd@outlook.com>
2020-05-11 14:39:46 -0700

    Add focus function to the reference while maintaining ref-erability of the forwarded ref

--- packages/react-native-win32/src/Libraries/Components/View/ViewWin32.tsx
+++ packages/react-native-win32/src/Libraries/Components/View/ViewWin32.tsx
@@ -6,7 +6,7 @@
 
 import * as React from 'react';
 import RN = require('react-native');
-import { View } from 'react-native';
+import { View, findNodeHandle, NativeModules } from 'react-native';
 import { IViewWin32Props, UseFrom } from './ViewWin32.Props';
 
 /**
@@ -37,26 +37,53 @@ export const ViewWin32 = React.forwardRef(
         }
       });
     }
+    const [labeledByTarget, setLabeledByTarget] = React.useState(null);
+    const [describedByTarget, setDescribedByTarget] = React.useState(null);
+    /**
+     * Process accessibility refs into node handles after initial DOM render, before sent across the bridge.
+     * useLayoutEffect will invalidate the render to assess the ref-based accessibility properties.
+     */
+    const {accessibilityLabeledBy, accessibilityDescribedBy, ...rest} = props;
+    React.useLayoutEffect(() => {
+      if (accessibilityLabeledBy !== undefined && accessibilityLabeledBy?.current !== null)
+      {
+        setLabeledByTarget(findNodeHandle(accessibilityLabeledBy.current as
+          | null
+          | number
+          | React.Component<any, any, any>
+          | React.ComponentClass<any, any>));
+      }
+
+      if (accessibilityDescribedBy !== undefined && accessibilityDescribedBy?.current !== null)
+      {
+        setDescribedByTarget(findNodeHandle(accessibilityDescribedBy.current as
+          | null
+          | number
+          | React.Component<any, any, any>
+          | React.ComponentClass<any, any>));
+      }
+    }, [accessibilityLabeledBy, accessibilityDescribedBy]);
 
     /**
-     *
+     * Add focus() as a callable function to the forwarded reference.
      */
+    const focusRef = React.useRef<ViewWin32>();
+    React.useImperativeHandle(ref, () => ({
+      focus: () => {
+        NativeModules.UIManager.dispatchViewManagerCommand(
+          findNodeHandle(focusRef.current),
+          NativeModules.UIManager.getViewManagerConfig('RCTView').Commands.focus,
+          null
+          );
+      },
+      ...focusRef.current,
+    }));
 
-    /**
-     * Add focus() as a callable function.  PROBLEM: findNodeHandle doesn't work, focusRef.current is undefined ...
-     */
-    // const focusRef = React.useRef<ViewWin32>();
-    // React.useImperativeHandle(ref, () => ({
-    //   focus: () => {
-    //     NativeModules.UIManager.dispatchViewManagerCommand(
-    //       findNodeHandle(focusRef.current),
-    //       NativeModules.UIManager.getViewManagerConfig('RCTView').Commands.focus,
-    //       null
-    //       );
-    //   },
-    // }));
-
-    return <View ref={ref} {...(props as InnerViewWin32Props)} />;
+    return <View ref={focusRef}
+    {...(rest as InnerViewWin32Props)}
+    {...((labeledByTarget !== null) ? {accessibilityLabeledBy:labeledByTarget} : {})}
+    {...((describedByTarget !== null) ? {accessibilityDescribedBy:describedByTarget} : {})}
+    />;
   });
 
 export type ViewWin32 = ViewWin32Type;
