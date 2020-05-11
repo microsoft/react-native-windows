b4f83e05aea0e1171b9222fc5a1076ba1a9f5c98
Andrew Coates (REDMOND) <acoates@microsoft.com>
2020-05-11 12:58:38 -0700

    Use spec file for DevSettings NativeModule

--- vnext/Microsoft.ReactNative/Modules/DevSettingsModule.cpp
+++ vnext/Microsoft.ReactNative/Modules/DevSettingsModule.cpp
@@ -6,53 +6,65 @@
 
 namespace Microsoft::ReactNative {
 
-auto DevSettingsModule::getConstants() -> std::map<std::string, folly::dynamic> {
-  return {};
+void DevSettings::Initialize(winrt::Microsoft::ReactNative::ReactContext const &reactContext) noexcept {
+  m_context = reactContext.Handle();
 }
 
-const char *DevSettingsModule::name = "DevSettings";
-std::string DevSettingsModule::getName() {
-  return name;
+struct ReloadFunctor
+    : winrt::implements<ReloadFunctor, winrt::default_interface<winrt::Windows::Foundation::IInspectable>> {
+  ReloadFunctor(Mso::VoidFunctor &&func) : m_func(std::move(func)) {}
+
+  void operator()() {
+    m_func();
+  }
+
+ private:
+  Mso::VoidFunctor m_func;
+};
+
+/*static*/ void DevSettings::SetReload(Mso::React::ReactOptions const &options, Mso::VoidFunctor &&func) noexcept {
+  options.Properties.Set(ReloadProperty(), winrt::make<ReloadFunctor>(std::move(func)));
 }
 
-DevSettingsModule::DevSettingsModule(Mso::VoidFunctor &&reload) : m_reload(std::move(reload)) {}
-
-void DevSettingsModule::reload() {
-  m_reload();
-}
-void DevSettingsModule::setHotLoadingEnabled(bool /*isHotLoadingEnabled*/) {
-  assert(false);
-}
-void DevSettingsModule::setIsDebuggingRemotely(bool /*isDebuggingRemotelyEnabled*/) {
-  assert(false);
-}
-void DevSettingsModule::setLiveReloadEnabled(bool /*setLiveReloadEnabled*/) {
-  assert(false);
-}
-void DevSettingsModule::setProfilingEnabled(bool /*setProfilingEnabled*/) {
-  assert(false);
-}
-void DevSettingsModule::toggleElementInspector() {
-  assert(false);
+/*static*/ winrt::Microsoft::ReactNative::IReactPropertyName DevSettings::ReloadProperty() noexcept {
+  return winrt::Microsoft::ReactNative::ReactPropertyBagHelper::GetName(
+      winrt::Microsoft::ReactNative::ReactPropertyBagHelper::GetNamespace(L"DevSettings"), L"Reload");
 }
 
-// iOS only.
-void DevSettingsModule::setIsShakeToShowDevMenuEnabled(bool /*enabled*/) {
+void DevSettings::reload() noexcept {
+  (*winrt::get_self<ReloadFunctor>(m_context.Properties().Get(ReloadProperty())))();
+}
+
+void DevSettings::reloadWithReason(std::string /*reason*/) noexcept {
+  reload();
+}
+
+void DevSettings::onFastRefresh() noexcept {
+  // noop
+}
+
+void DevSettings::setHotLoadingEnabled(bool isHotLoadingEnabled) noexcept {
   assert(false);
 }
 
-auto DevSettingsModule::getMethods() -> std::vector<facebook::xplat::module::CxxModule::Method> {
-  return {
-      Method("reload", [this](folly::dynamic args) { reload(); }),
-      Method("setHotLoadingEnabled", [this](folly::dynamic args) { setHotLoadingEnabled(args[0].getBool()); }),
-      Method("setIsDebuggingRemotely", [this](folly::dynamic args) { setIsDebuggingRemotely(args[0].getBool()); }),
-      Method("setLiveReloadEnabled", [this](folly::dynamic args) { setLiveReloadEnabled(args[0].getBool()); }),
-      Method("setProfilingEnabled", [this](folly::dynamic args) { setProfilingEnabled(args[0].getBool()); }),
-      Method("toggleElementInspector", [this](folly::dynamic args) { toggleElementInspector(); }),
-      Method(
-          "setIsShakeToShowDevMenuEnabled",
-          [this](folly::dynamic args) { setIsShakeToShowDevMenuEnabled(args[0].getBool()); }),
-  };
+void DevSettings::setIsDebuggingRemotely(bool isDebuggingRemotelyEnabled) noexcept {
+  assert(false);
+}
+
+void DevSettings::setProfilingEnabled(bool isProfilingEnabled) noexcept {
+  assert(false);
+}
+
+void DevSettings::toggleElementInspector() noexcept {
+  assert(false);
+}
+
+void DevSettings::addMenuItem(std::string title) noexcept {
+  assert(false);
+}
+
+void DevSettings::setIsShakeToShowDevMenuEnabled(bool enabled) noexcept {
+  assert(false);
 }
 
 } // namespace Microsoft::ReactNative
--- vnext/Microsoft.ReactNative/Modules/DevSettingsModule.h
+++ vnext/Microsoft.ReactNative/Modules/DevSettingsModule.h
@@ -5,30 +5,30 @@
 #include <cxxreact/CxxModule.h>
 #include <functional/functor.h>
 
+#include <NativeModules.h>
+#include <ReactHost/React.h>
+
 namespace Microsoft::ReactNative {
 
-class DevSettingsModule : public facebook::xplat::module::CxxModule {
- public:
-  DevSettingsModule(Mso::VoidFunctor &&reload);
+REACT_MODULE(DevSettings)
+struct DevSettings {
+  REACT_INIT(Initialize) void Initialize(winrt::Microsoft::ReactNative::ReactContext const &reactContext) noexcept;
 
-  static const char *name;
+  REACT_METHOD(reload) void reload() noexcept;
+  REACT_METHOD(reloadWithReason) void reloadWithReason(std::string reason) noexcept;
+  REACT_METHOD(onFastRefresh) void onFastRefresh() noexcept;
+  REACT_METHOD(setHotLoadingEnabled) void setHotLoadingEnabled(bool isHotLoadingEnabled) noexcept;
+  REACT_METHOD(setIsDebuggingRemotely) void setIsDebuggingRemotely(bool isDebuggingRemotelyEnabled) noexcept;
+  REACT_METHOD(setProfilingEnabled) void setProfilingEnabled(bool isProfilingEnabled) noexcept;
+  REACT_METHOD(toggleElementInspector) void toggleElementInspector() noexcept;
+  REACT_METHOD(addMenuItem) void addMenuItem(std::string title) noexcept;
+  REACT_METHOD(setIsShakeToShowDevMenuEnabled) void setIsShakeToShowDevMenuEnabled(bool enabled) noexcept;
 
-  std::string getName() override;
-  std::map<std::string, folly::dynamic> getConstants() override;
-  std::vector<Method> getMethods() override;
-
-  void reload();
-  void setHotLoadingEnabled(bool isHotLoadingEnabled);
-  void setIsDebuggingRemotely(bool isDebuggingRemotelyEnabled);
-  void setLiveReloadEnabled(bool setLiveReloadEnabled);
-  void setProfilingEnabled(bool setProfilingEnabled);
-  void toggleElementInspector();
-
-  // iOS only.
-  void setIsShakeToShowDevMenuEnabled(bool enabled);
+  static void SetReload(Mso::React::ReactOptions const &options, Mso::VoidFunctor &&func) noexcept;
 
  private:
-  Mso::VoidFunctor m_reload;
+  static winrt::Microsoft::ReactNative::IReactPropertyName ReloadProperty() noexcept;
+  winrt::Microsoft::ReactNative::IReactContext m_context;
 };
 
 } // namespace Microsoft::ReactNative
