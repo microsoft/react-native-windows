96a27622fcc2686bc2d7128a874001fa83eced61
Andreas Eulitz <aeulitz@microsoft.com>
2020-05-16 16:14:07 -0700

    add test methods

--- vnext/Desktop.ABITests/ReactNativeHostTests.cpp
+++ vnext/Desktop.ABITests/ReactNativeHostTests.cpp
@@ -4,9 +4,11 @@
 #include "pch.h"
 #include <CppUnitTest.h>
 #include <winrt/Microsoft.ReactNative.h>
+#include <winrt/Windows.Foundation.Collections.h>
 #include "ActivationFactory.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace winrt::Microsoft::ReactNative;
 
 namespace ABITests {
 
@@ -18,11 +20,36 @@ TEST_CLASS (ReactNativeHostTests) {
 
   TEST_METHOD(Activation_Succeeds) {
     try {
-      winrt::Microsoft::ReactNative::ReactNativeHost host{};
+      ReactNativeHost host{};
       Assert::IsTrue(true);
     } catch (...) {
       Assert::Fail();
     }
+  }
+
+  // Functions under test:
+  // IVector<IReactPackageProvider> PackageProviders { get; set; };
+  // ReactInstanceSettings InstanceSettings { get; set; };
+
+  TEST_METHOD(PackageProviders_AsConstructed_IsEmpty) {
+    ReactNativeHost host{};
+    Assert::AreEqual(0u, host.PackageProviders().Size());
+  }
+
+  class MockPackageProvider
+      : public winrt::implements<MockPackageProvider, ::winrt::Microsoft::ReactNative::IReactPackageProvider> {
+   public:
+    MockPackageProvider(){};
+    void CreatePackage(IReactPackageBuilder const &packageBuilder){};
+  };
+
+  TEST_METHOD(PackageProviders_Append_ReflectsAddition) {
+    winrt::Microsoft::ReactNative::ReactNativeHost host{};
+
+    IReactPackageProvider packageProvider = ::winrt::make<MockPackageProvider>();
+
+    host.PackageProviders().Append(packageProvider);
+    Assert::AreEqual(1u, host.PackageProviders().Size());
   }
 };
 
