9c9a671a0a233b6b7cc3f8a898fa150046f8cbd8
Zihan Chen (MSFT) <zihanc@microsoft.com>
2020-05-21 13:09:52 -0700

    Create empty test project: Microsoft.ReactNative.ComponentTests

---
+++ vnext/Microsoft.ReactNative.ComponentTests/pch.h
@@ -0,0 +1,23 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#pragma once
+
+#define NOGDI
+#define NOMINMAX
+
+#include <unknwn.h>
+
+#undef GetCurrentTime
+
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Foundation.h>
+#include "winrt/Microsoft.ReactNative.h"
+
+#include "gtest/gtest.h"
+#include "motifCpp/gTestAdapter.h"
+#include "motifCpp/testCheck.h"
+
+#ifndef CXXUNITTESTS
+#define CXXUNITTESTS
+#endif // CXXUNITTESTS
