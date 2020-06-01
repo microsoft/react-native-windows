959e1bc43da92d98376b97ec945a5ccdfc3bfbe1
Nick Gerleman <ngerlem@microsoft.com>
2020-06-01 08:18:12 -0700

    Prompt fixes

--- packages/react-native-platform-override/src/OverridePrompt.ts
+++ packages/react-native-platform-override/src/OverridePrompt.ts
@@ -56,22 +56,22 @@ export async function promptForOverrideDetails(): Promise<
       choices: [
         {
           name:
-            'Derived from upstream      (E.g. a Windows implementation of an existing component)',
+            'Derived from upstream            (E.g. a Windows implementation of an existing component)',
           value: 'derived',
           short: 'Derived',
         },
         {
-          name: 'Patches to upstream        (E.g. commenting out code)',
+          name: 'Patches to upstream              (E.g. commenting out code)',
           value: 'patch',
           short: 'Patch',
         },
         {
-          name: 'Independent platform logic (E.g. Windows-specific modules)',
+          name: 'Independent platform logic       (E.g. Windows-specific modules)',
           value: 'platform',
           short: 'Platform',
         },
         {
-          name: 'ExactCopy of an upstream file',
+          name: 'Exact copy of an upstream file',
           value: 'copy',
           short: 'Copy',
         },
@@ -90,11 +90,10 @@ export async function promptForOverrideDetails(): Promise<
         ['copy', 'patch'].includes(res.type),
       type: 'input',
       validate: validateIssueNumber,
-      transformer: answer => Number.parseInt(answer, 10),
+      filter: filterIssueNumber,
+      askAnswered: true,
       name: 'issue',
-      prefix:
-        'Copying or patching code from upstream adds significant technical debt. Please create an issue using the "Deforking" label to track making changes so that code can be shared.',
-      message: 'GitHub Issue number:',
+      message: 'Issue number tracking removal:',
     },
     {
       when: res => ['copy', 'patch', 'derived'].includes(res.type),
@@ -105,9 +104,14 @@ export async function promptForOverrideDetails(): Promise<
   ]);
 }
 
+function filterIssueNumber(answer: string): number | string {
+  const parsedInt = Number.parseInt(answer, 10);
+  return Number.isInteger(parsedInt) ? parsedInt : '';
+}
+
 function validateIssueNumber(answer: string): boolean | string {
   return (
     Number.isInteger(Number.parseInt(answer, 10)) ||
-    'Github issue must be a number'
+    'Issue must be a number'
   );
 }
