081cffd2caea6a0629847550015e4ed9b01c7b94
Andrew Coates (REDMOND) <acoates@microsoft.com>
2020-06-01 14:24:26 -0700

    Latest CELA open source policy is to not include All right reserved

--- vnext/Microsoft.ReactNative.SharedManaged/ReactPromise.cs
+++ vnext/Microsoft.ReactNative.SharedManaged/ReactPromise.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
 // The code is adopted from Facebook's ReactNative implementation for Java.
