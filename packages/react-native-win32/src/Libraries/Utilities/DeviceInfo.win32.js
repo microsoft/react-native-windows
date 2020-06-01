3f17d742ee5045994fda3f10d2a489bde71659e3
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2020-06-01 15:17:26 -0700

    CELA open source policy no longer recommends all right reserved in license header (#5083)
    * Latest CELA open source policy is to not include All right reserved
    
    * Change files

--- packages/react-native-win32/src/Libraries/Utilities/DeviceInfo.win32.js
+++ packages/react-native-win32/src/Libraries/Utilities/DeviceInfo.win32.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Copyright (c) Microsoft Corporation.
  * Licensed under the MIT License.
  *
  * Device info doesn't can't be a global set of data for win32
