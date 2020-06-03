25777475ef40a8e6114d62ab98b4ad9eb74f8dcf
Jon Thysell (JAUNTY) <jthysell@microsoft.com>
2020-06-02 17:19:36 -0700

    Addressing feedback: vstools function names and comments

--- vnext/local-cli/runWindows/utils/vstools.js
+++ vnext/local-cli/runWindows/utils/vstools.js
@@ -14,28 +14,55 @@ const projectTypeGuidsByLanguage = {
   cs: '{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}',
 };
 
-function hasBlock(lines, block) {
-  var startIndex = lines.indexOf(block[0]);
+/**
+ * Checks is the given block of lines exists within an array of lines.
+ * @param {array} lines - The array of lines to search.
+ * @param {array} block - The block of lines to search for.
+ * @return {boolean} - True if the block of lines does exist within lines.
+ */
+function linesContainsBlock(lines, block) {
+  if (block.length > 0) {
+    var startIndex = lines.indexOf(block[0]);
 
-  if (startIndex >= 0) {
-    for (let i = 1; i < block.length; i++) {
-      if (lines[startIndex + i] !== block[i]) {
-        return false;
+    if (startIndex >= 0) {
+      for (let i = 1; i < block.length; i++) {
+        if (lines[startIndex + i] !== block[i]) {
+          return false;
+        }
       }
+      return true;
     }
-    return true;
   }
 
   return false;
 }
 
-function insertBlock(lines, block, index) {
+/**
+ * Insert the given block of lines into an array of lines.
+ * @param {array} lines - The array of lines to insert into.
+ * @param {array} block - The block of lines to insert.
+ * @param {number} index - The index to perform the insert.
+ */
+function insertBlockIntoLines(lines, block, index) {
   for (let i = 0; i < block.length; i++) {
     lines.splice(index + i, 0, block[i]);
   }
 }
 
-function getBlockContents(lines, startLine, endLine, includeStartEnd = true) {
+/**
+ * Search through an array of lines for a block of lines starting with startLine and ending with endLine.
+ * @param {array} lines - The array of lines to search.
+ * @param {string} startLine - The first line of the block.
+ * @param {string} endLine - The last line of the block.
+ * @param {boolean} includeStartEnd - Include the start and end lines in the result.
+ * @return {array} The found block of lines, if found.
+ */
+function getBlockContentsFromLines(
+  lines,
+  startLine,
+  endLine,
+  includeStartEnd = true,
+) {
   const startIndex = lines.indexOf(startLine);
   const endIndex = lines.indexOf(endLine, startIndex);
 
@@ -50,6 +77,14 @@ function getBlockContents(lines, startLine, endLine, includeStartEnd = true) {
   return [];
 }
 
+/**
+ * Adds the necessary info from a VS project into a VS solution file so that it will build.
+ * @param {string} slnFile - The Absolute path to the target VS solution file.
+ * @param {object} project - The object representing the project info.
+ * @param {boolean} verbose - If true, enable verbose logging.
+ * @param {boolean} checkMode - It true, don't make any changes.
+ * @return {boolean} Whether any changes were necessary.
+ */
 function addProjectToSolution(
   slnFile,
   project,
@@ -85,19 +120,19 @@ function addProjectToSolution(
     'EndProject',
   ];
 
-  if (!hasBlock(slnLines, projectEntryBlock)) {
+  if (!linesContainsBlock(slnLines, projectEntryBlock)) {
     if (verbose) {
       console.log(chalk.yellow('Missing project entry block.'));
     }
 
     const globalIndex = slnLines.indexOf('Global');
-    insertBlock(slnLines, projectEntryBlock, globalIndex);
+    insertBlockIntoLines(slnLines, projectEntryBlock, globalIndex);
     contentsChanged = true;
   }
 
   // Check for the project configuration platforms
 
-  const slnConfigs = getBlockContents(
+  const slnConfigs = getBlockContentsFromLines(
     slnLines,
     '\tGlobalSection(SolutionConfigurationPlatforms) = preSolution',
     '\tEndGlobalSection',
