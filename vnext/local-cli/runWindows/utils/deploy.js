201b8819c610f0f7fe7ec874a97bfc5889277275
Alexander Sklar <asklar@microsoft.com>
2020-06-02 21:58:56 -0700

    workaround for UWP Packaging VS 16.5-16.6 regression backport to 0.61 (#5100)
    * work around VS16.5-16.6 regression in UWP packaging
    
    * Change files

--- vnext/local-cli/runWindows/utils/deploy.js
+++ vnext/local-cli/runWindows/utils/deploy.js
@@ -12,6 +12,8 @@ const http = require('http');
 const path = require('path');
 const glob = require('glob');
 const parse = require('xml-parser');
+const child_process = require('child_process');
+const EOL = require('os').EOL;
 const WinAppDeployTool = require('./winappdeploytool');
 const {
   newInfo,
@@ -50,11 +52,21 @@ function getAppPackage(options) {
   if (!appPackage && options.release) {
     // in the latest vs, Release is removed
     newWarn(
-      'No package found in *_Release_* folder, remove _Release_ and check again',
+      'No package found in *_Release_* folder, removing the _Release_ prefix and checking again',
     );
-    appPackage = glob.sync(
-      `windows/{*/AppPackages,AppPackages/*}/*_${options.arch}_*`,
-    )[0];
+
+    const rootGlob = `${options.root}/windows/{*/AppPackages,AppPackages/*}`;
+    const newGlob = `${rootGlob}/*_${
+      options.arch === 'x86' ? 'Win32' : options.arch
+    }_${options.release ? '' : 'Debug_'}Test`;
+
+    const result = glob.sync(newGlob);
+    if (result.length > 1) {
+      newWarn(`More than one app package found: ${result}`);
+    } else if (result.length === 1) {
+      // we're good
+    }
+    appPackage = glob.sync(newGlob)[0];
   }
 
   if (!appPackage) {
@@ -161,6 +173,34 @@ async function deployToDesktop(options, verbose) {
   const script = glob.sync(
     path.join(appPackageFolder, 'Add-AppDevPackage.ps1'),
   )[0];
+
+  // This path is maintained and VS has promised to keep it valid.
+  const vsWherePath = path.join(
+    process.env['ProgramFiles(x86)'] || process.env.ProgramFiles,
+    '/Microsoft Visual Studio/Installer/vswhere.exe',
+  );
+
+  const vsVersion = child_process
+    .execSync(
+      `"${vsWherePath}" -version 16 -property catalog_productDisplayVersion`,
+    )
+    .toString()
+    .split(EOL)[0];
+
+  if (vsVersion.startsWith('16.5') || vsVersion.startsWith('16.6')) {
+    // VS 16.5 and 16.6 introduced a regression in packaging where the certificates created in the UI will render the package uninstallable.
+    // This will be fixed in 16.7. In the meantime we need to copy the Add-AppDevPackage that has the fix for this EKU issue:
+    // https://developercommunity.visualstudio.com/content/problem/1012921/uwp-packaging-generates-incompatible-certificate.html
+    if (verbose) {
+      newWarn(
+        'Applying Add-AppDevPackage.ps1 workaround for VS 16.5-16.6 bug - see https://developercommunity.visualstudio.com/content/problem/1012921/uwp-packaging-generates-incompatible-certificate.html',
+      );
+    }
+    fs.copyFileSync(
+      path.join(path.resolve(__dirname), 'Add-AppDevPackage.ps1'),
+      script,
+    );
+  }
 
   const args = ['remoteDebugging', options.proxy ? 'true' : 'false'];
 
