310466f222e616496244824794163d42455f1ae4
Danny van Velzen 🁴 <dannyvv@microsoft.com>
2020-06-03 18:55:39 -0700

    Update AutoFixAssertionMessage

--- vnext/Microsoft.ReactNative.Managed.CodeGen.UnitTests/AutoFixShouldNotBeCheckedIn.cs
+++ vnext/Microsoft.ReactNative.Managed.CodeGen.UnitTests/AutoFixShouldNotBeCheckedIn.cs
@@ -8,7 +8,10 @@ namespace Microsoft.ReactNative.Managed.CodeGen.UnitTests
     [TestMethod]
     public void EnsureAutoFixIsFalse()
     {
-      Assert.IsFalse(AutoFixLkgs, "Do Not check AutoFixLkgs in as true, since it will not catch any failures otherwise.");
+      Assert.IsFalse(AutoFixLkgs, @"Do Not check AutoFixLkgs in as true.
+The base test class has a boolean called `AutoFixLkgs` that you can set to regenerate the lkg's.
+This is very usefull in case there are a lot of changes in the codegen that touch a lot of tests. 
+This test fails if that flag is turned on. This makes sure that nobody accidentally creates a check-in that sets the flag to true and have all the tests pass form here on out because the LKG matches");
     }
   }
 }
