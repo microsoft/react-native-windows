2f480aade637916bae39b834d4bb5130eea0637c
Zihan Chen (MSFT) <zihanc@microsoft.com>
2020-06-04 22:05:12 -0700

    Fix code review comments

--- vnext/Microsoft.ReactNative/Modules/ClipboardModule.cpp
+++ vnext/Microsoft.ReactNative/Modules/ClipboardModule.cpp
@@ -16,18 +16,19 @@ void Clipboard::getString(React::ReactPromise<React::JSValue> result) noexcept {
   auto jsDispatcher = m_reactContext.JSDispatcher();
   auto promise = result;
   m_reactContext.UIDispatcher().Post(
-      [=]() -> winrt::fire_and_forget {
+      [jsDispatcher, promise]() -> winrt::fire_and_forget {
         auto jsDispatcher2 = jsDispatcher;
         auto promise2 = promise;
         winrt::Windows::ApplicationModel::DataTransfer::DataPackageView data =
             winrt::Windows::ApplicationModel::DataTransfer::Clipboard::GetContent();
         try {
           std::wstring text = std::wstring(co_await data.GetTextAsync());
-          jsDispatcher2.Post([=] { promise2.Resolve(React::JSValue{Microsoft::Common::Unicode::Utf16ToUtf8(text)}); });
+          jsDispatcher2.Post(
+              [promise2, text] { promise2.Resolve(React::JSValue{Microsoft::Common::Unicode::Utf16ToUtf8(text)}); });
         } catch (winrt::hresult_error const &e) {
-          jsDispatcher2.Post([=] { promise2.Reject(std::wstring(e.message()).c_str()); });
+          jsDispatcher2.Post([promise2, e] { promise2.Reject(std::wstring(e.message()).c_str()); });
         } catch (...) {
-          jsDispatcher2.Post([=] { promise2.Reject(React::ReactError()); });
+          jsDispatcher2.Post([promise2] { promise2.Reject(React::ReactError()); });
         }
       } // namespace Microsoft::ReactNative
   );
