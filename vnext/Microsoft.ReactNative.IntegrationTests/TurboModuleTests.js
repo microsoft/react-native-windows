987432677491afa6495f043a84788b9247f0374a
Zihan Chen (MSFT) <zihanc@microsoft.com>
2020-06-10 15:33:19 -0700

    Implement SyncMethod and Constant for TurboModule

--- vnext/Microsoft.ReactNative.IntegrationTests/TurboModuleTests.js
+++ vnext/Microsoft.ReactNative.IntegrationTests/TurboModuleTests.js
@@ -3,4 +3,25 @@ import * as TurboModuleRegistry from '../Libraries/TurboModule/TurboModuleRegist
 const sampleTurboModule = TurboModuleRegistry.getEnforcing('SampleTurboModule');
 
 // function calls will be added to cover complete signatures
-sampleTurboModule.voidFunction();
+
+try {
+  sampleTurboModule
+    .promiseFunction('something', 1, true);
+
+  sampleTurboModule
+    .syncFunctionResult(
+      sampleTurboModule
+        .syncFunction('something', 2, false)
+    );
+
+  sampleTurboModule
+    .constants(
+      sampleTurboModule.constantString,
+      sampleTurboModule.constantInt
+    );
+
+  sampleTurboModule
+    .succeeded();
+} catch (err) {
+  sampleTurboModule.onError(typeof err);
+}
