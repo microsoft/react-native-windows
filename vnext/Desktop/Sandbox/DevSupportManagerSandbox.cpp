a5b5dae8f90c8467d4a04592c7d34f94dfd9b139
Julio César Rocha <JunielKatarn@users.noreply.github.com>
2020-06-17 09:41:25 -0700

    [0.60] Drop LazyDevSupportManager (#4984) (#5257)
    * Drop LazyDevSupportManager (#4984)
    
    * Drop LazySupportManager
    
    * Change files
    
    * Remove obsolete comment
    
    * Remove obsolete comment
    
    * Rename buffer0 to buffer
    
    * yarn format
    
    * Remove unneeded string allocations
    
    * Update DevSupportManager.cpp
    
    * clang format
    
    * Revert unrelated changes
    
    * Update used namespace for DevSupportManager
    
    * yarn format
    
    * Use patch release type

--- vnext/Desktop/Sandbox/DevSupportManagerSandbox.cpp
+++ vnext/Desktop/Sandbox/DevSupportManagerSandbox.cpp
@@ -15,18 +15,19 @@
 
 using namespace std;
 
-namespace facebook {
-namespace react {
+namespace Microsoft::React {
 #ifdef PATCH_RN
 
-JSECreator DevSupportManager::LoadJavaScriptInSandboxMode(const DevSettings &settings) {
-  return [this, settings](shared_ptr<ExecutorDelegate> delegate, shared_ptr<MessageQueueThread> jsQueue) {
-    auto sandboxJSE = make_unique<SandboxJSExecutor>(delegate, jsQueue);
-    auto sandboxEndpoint = std::make_shared<NamedPipeEndpoint>(settings.sandboxPipeName);
+facebook::react::JSECreator Microsoft::React::DevSupportManager::LoadJavaScriptInSandboxMode(
+    const facebook::react::DevSettings &settings) {
+  return [this, settings](
+             shared_ptr<facebook::react::ExecutorDelegate> delegate,
+             shared_ptr<facebook::react::MessageQueueThread> jsQueue) {
+    auto sandboxJSE = make_unique<facebook::react::SandboxJSExecutor>(delegate, jsQueue);
+    auto sandboxEndpoint = std::make_shared<facebook::react::NamedPipeEndpoint>(settings.sandboxPipeName);
     sandboxJSE->ConnectAsync(sandboxEndpoint, settings.errorCallback).wait();
     return sandboxJSE;
   };
 }
 #endif // PATCH_RN
-} // namespace react
-} // namespace facebook
+} // namespace Microsoft::React
