6dfe927d7fd4a75aab609df3cc25c19f473f8403
Alexander Sklar <asklar@microsoft.com>
2020-06-27 01:27:09 -0700

    add std::move

--- vnext/Microsoft.ReactNative/Utils/ManagedResource.h
+++ vnext/Microsoft.ReactNative/Utils/ManagedResource.h
@@ -2,17 +2,19 @@
 // Licensed under the MIT License.
 
 #pragma once
+#include <utility>
 
-template<typename T, typename closeFn_t>
+template <typename T, typename closeFn_t>
 class managed_resource {
   T handle;
-  closeFn_t* closeFn;
+  closeFn_t *closeFn;
+
  public:
   operator T() const {
     return handle;
   }
 
-  managed_resource(T &&t, closeFn_t &&fn) : handle(t), closeFn(fn) {}
+  managed_resource(T const &&t, closeFn_t &&fn) : handle(std::move(t)), closeFn(std::move(fn)) {}
   managed_resource(const managed_resource &) = delete;
   managed_resource &operator=(const managed_resource &) = delete;
   ~managed_resource() {
