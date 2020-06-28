b98f40e0def418757ccda6c37798b364677b44fc
Alexander Sklar <asklar@microsoft.com>
2020-06-28 13:55:12 -0700

    mkdir outputpath

--- .ado/TestWACK.ps1
+++ .ado/TestWACK.ps1
@@ -4,13 +4,14 @@ $exceptions = @(
     'API __C_specific_handler in kernel32.dll is not supported for this application type. Microsoft.ReactNative.dll calls this API.'
 );
 
+if (!(Test-Path $OutputPath)) {
+    mkdir -Path $OutputPath
+}
+
 if ($Configuration -eq 'Release') {
     $appxPath = Resolve-Path $PSScriptRoot\..\packages\playground\windows\AppPackages\playground\playground_1.0.0.0_${Platform}_Test\playground_1.0.0.0_$Platform.appx
     gci $appxPath
 
-    if (!(Test-Path $OutputPath)) {
-        mkdir -Path $OutputPath
-    }
     & "C:\Program Files (x86)\Windows Kits\10\App Certification Kit\appcert.exe" test -appxpackagepath $appxPath -reportoutputpath $OutputPath\result.xml
 
     $xml = [xml] (gc $OutputPath\result.xml)
