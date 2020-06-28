6b1189620570e823a4ecaa6b29d3779761a08409
Alexander Sklar <asklar@microsoft.com>
2020-06-28 00:22:41 -0700

    exception for __C_specific_handler for now

--- .ado/TestWACK.ps1
+++ .ado/TestWACK.ps1
@@ -1,5 +1,9 @@
 param([string]$Platform, [string]$Configuration, [string]$OutputPath)
 
+$exceptions = @(
+    'API __C_specific_handler in kernel32.dll is not supported for this application type. Microsoft.ReactNative.dll calls this API.'
+);
+
 if ($Configuration -eq 'Release') {
     $appxPath = Resolve-Path $PSScriptRoot\..\packages\playground\windows\AppPackages\playground\playground_1.0.0.0_${Platform}_Test\playground_1.0.0.0_$Platform.appx
     gci $appxPath
@@ -9,11 +13,12 @@ if ($Configuration -eq 'Release') {
     }
     & "C:\Program Files (x86)\Windows Kits\10\App Certification Kit\appcert.exe" test -appxpackagepath $appxPath -reportoutputpath $OutputPath\result.xml
 
-    $xml=[xml] (gc $OutputPath\result.xml)
+    $xml = [xml] (gc $OutputPath\result.xml)
 
     $supportedApiTest = ($xml.REPORT.REQUIREMENTS.REQUIREMENT | Where-Object -Property TITLE -EQ  "Supported API test").TEST;
-    if ($supportedApiTest.RESULT.'#cdata-section' -eq 'FAIL') {
-        $errors = $supportedApiTest.MESSAGES.MESSAGE.TEXT -join "`n"
+    $errors = $supportedApiTest.MESSAGES.MESSAGE.TEXT | Where-Object { $_ -notin $exceptions }
+
+    if ($errors.Length -ne 0) {
         throw $errors
     }
 }
