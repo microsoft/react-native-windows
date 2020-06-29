59f869000f2e3644ad8259ee1804df2c8cb9def3
Nick Gerleman <ngerlem@microsoft.com>
2020-06-29 09:29:32 -0700

    Specify CommonJS in shared tsconfig instead of just-task

--- packages/@rnw-scripts/just-task/just-task.js
+++ packages/@rnw-scripts/just-task/just-task.js
@@ -6,15 +6,21 @@
  * @ts-check
  */
 
-const {cleanTask, eslintTask, jestTask, series, task, tscTask} = require('just-scripts');
+const {
+  cleanTask,
+  eslintTask,
+  jestTask,
+  series,
+  task,
+  tscTask,
+} = require('just-scripts');
 
 task('clean', cleanTask(['lib-commonjs']));
 
-task('ts:commonjs', tscTask({ module: 'commonjs', outDir: 'lib-commonjs' }));
-task('ts', series('ts:commonjs'));
+task('ts', tscTask({outDir: 'lib-commonjs'}));
 
 task('jest', jestTask());
-task('jest:watch', jestTask({ watch: true }));
+task('jest:watch', jestTask({watch: true}));
 
 task('build', series('ts'));
 task('test', series('jest'));
@@ -22,7 +28,7 @@ task('test', series('jest'));
 task('rebuild', series('clean', 'build'));
 
 task('eslint', eslintTask());
-task('eslint:fix', eslintTask({ fix: true }));
+task('eslint:fix', eslintTask({fix: true}));
 
 task('lint', series('eslint'));
 task('lint:fix', series('eslint:fix'));
