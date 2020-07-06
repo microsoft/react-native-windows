33b917ef5b5ab64821ee0c91e70fc9c4e2832ee8
Nick Gerleman <ngerlem@microsoft.com>
2020-07-06 09:51:41 -0700

    Add Support for Binary Overrides to react-native-platform-override (#5428)
    * Add Support for Binary Overrides to react-native-platform-override
    
    This change lays some of the groundwork needed for react-native-platform-override to automatically absorb all new files from the currently unpublished RNTester and IntegrationTests (along with forking/tracking any resources).
    
    This change comes in a few parts:
    - Move filesystem abstractions to use Buffers instead of strings
      - Set eslint environment to Node to allow global Buffers
    - Deal with binary implications of patching and three-way-merge
      - Git will not write conflict markers to merged binaries, need to fix assumptions
      - We should only do textual changes to utf8 buffers
    - Update has algorithm
      - We still need to normalize to CRLF for text
      - We should not do any coercions to binaries
    - Fixup generateManifest script and regen anything that needs it
      - We needed to change how getNpmPackage works to enable this
    - Remove hardcoded filter in Override FS abstraction
      - Allow globs in manifest to ignore files to enforce
      - This is also future work needed for package local manifests
    - Update tests
      - Add new end to end tests to validate three-way-merge and scratch repository usage
      - Add more UTs
      - Update Babel config for Jest to closer match tsconfig
    
    * Change files

--- packages/@rnw-scripts/eslint-config/eslintrc.js
+++ packages/@rnw-scripts/eslint-config/eslintrc.js
@@ -6,16 +6,13 @@
  * @ts-check
  */
 
-module.exports = { 
-  extends: [
-    "@react-native-community",
-    "prettier"
-  ],
-  rules: { 
-    "react-native/no-inline-styles" : "off",
+module.exports = {
+  extends: ['@react-native-community', 'prettier'],
+  rules: {
+    'react-native/no-inline-styles': 'off',
   },
-  ignorePatterns: [
-    'lib/**',
-    'lib-commonjs/**'
-  ]
+  env: {
+    node: true,
+  },
+  ignorePatterns: ['lib/**', 'lib-commonjs/**'],
 };
--- packages/react-native-platform-override/src/test/Hash.test.ts
+++ packages/react-native-platform-override/src/test/Hash.test.ts
@@ -5,7 +5,8 @@
  * @format
  */
 
-import {hashContent} from '../Hash';
+import {Hasher, hashContent} from '../Hash';
+import isutf8 from 'isutf8';
 
 test('hashContent - Different Strings', () => {
   expect(hashContent('a')).not.toBe(hashContent('b'));
@@ -21,4 +22,35 @@ test('hashContent - Repeated Different Line Endings', () => {
 
 test('hashContent - Line Endings are Visible', () => {
   expect(hashContent('a')).not.toBe(hashContent('a\n\n'));
+});
+
+test('hashContent - Different Binary (Not UTF8)', () => {
+  // Characters sequences starting with > 0xF4 are not valid UTF8
+  const binary1 = Buffer.from(new Int8Array([0xf8, 1, 42]));
+  const binary2 = Buffer.from(new Int8Array([0xff, 1, 42]));
+
+  // Make sure we really are testing invalid strings
+  expect(isutf8(binary1)).toBe(false);
+  expect(isutf8(binary2)).toBe(false);
+
+  expect(hashContent(Buffer.from(binary1))).not.toBe(
+    hashContent(Buffer.from(binary2)),
+  );
+});
+
+test('Hasher - Same as hashContent', () => {
+  expect(new Hasher().feedContent(Buffer.from('abc')).digest()).toBe(
+    hashContent('abc'),
+  );
+});
+
+test('Hasher - Multiple Buffers', () => {
+  const hash1 = new Hasher()
+    .feedContent(Buffer.from('a'))
+    .feedContent(Buffer.from('b'))
+    .digest();
+
+  const hash2 = new Hasher().feedContent(Buffer.from('ab')).digest();
+
+  expect(hash1).toBe(hash2);
 });
