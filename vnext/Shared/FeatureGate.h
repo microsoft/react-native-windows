3e2ed9f6df622a19236dc16cfe1a3cde9f43d2b2
Julio C. Rocha <julio.rocha@microsoft.com>
2020-07-08 19:52:10 -0700

    Add documentation

--- vnext/Shared/FeatureGate.h
+++ vnext/Shared/FeatureGate.h
@@ -4,8 +4,17 @@
 
 namespace Microsoft::React {
 
+/// <summary>
+/// Sets a global boolean value identified by a random string.
+/// </summary>
+/// <param name="name">Global boolean key</param>
 void __cdecl SetFeatureGate(std::string &&name) noexcept;
 
+/// <summary>
+/// Retrieves a global boolean value for the given key.
+/// </summary>
+/// <param name="name">Global boolean key</param>
+/// <returns>Value stored for the given key, or false if the entry doesn't exist (default)</returns>
 const bool __cdecl GetFeatureGate(std::string &&name) noexcept;
 
 } // namespace Microsoft::React
