fb45ed3858b5f564305f10ac50d2a0fcfb3bc912
Alexander Sklar <asklar@microsoft.com>
2020-07-09 22:32:41 -0700

    Adjust island window size for title bar/menu bar and scrollbar (#5485)
    * adjust island window size for title bar/menu bar
    
    * also adjust for revealed scrollbar's width

--- packages/playground/windows/playground-win32/Playground-Win32.cpp
+++ packages/playground/windows/playground-win32/Playground-Win32.cpp
@@ -153,7 +153,15 @@ struct WindowData {
     HWND interopHwnd;
     winrt::check_hresult(interop->get_WindowHandle(&interopHwnd));
 
-    MoveWindow(interopHwnd, 0, 0, windowPosition->cx, windowPosition->cy, TRUE);
+    constexpr int logBoxHeight = 100;
+    constexpr int scrollbarWidth = 24;
+    MoveWindow(
+        interopHwnd,
+        0,
+        0,
+        windowPosition->cx > scrollbarWidth ? windowPosition->cx - scrollbarWidth : windowPosition->cx,
+        windowPosition->cy > logBoxHeight ? windowPosition->cy - logBoxHeight : windowPosition->cy,
+        TRUE);
 
     return 0;
   }
