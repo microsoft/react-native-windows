9f80a3478f8d0dd4ac98cc2d3ef2aa44850e7414
Julio César Rocha <JunielKatarn@users.noreply.github.com>
2020-07-09 18:54:35 -0700

    Make IWebSocketResource methods noexcept (#5487)
    * Make IWebSocketResource methods noexcept.

--- vnext/Shared/WinRTWebSocketResource.h
+++ vnext/Shared/WinRTWebSocketResource.h
@@ -47,89 +47,91 @@ class WinRTWebSocketResource : public IWebSocketResource, public std::enable_sha
   WinRTWebSocketResource(
       winrt::Windows::Networking::Sockets::IMessageWebSocket &&socket,
       winrt::Windows::Foundation::Uri &&uri,
-      std::vector<winrt::Windows::Security::Cryptography::Certificates::ChainValidationResult> certExeptions);
+      std::vector<winrt::Windows::Security::Cryptography::Certificates::ChainValidationResult> certExeptions) noexcept;
 
-  winrt::Windows::Foundation::IAsyncAction PerformConnect();
-  winrt::fire_and_forget PerformPing();
-  winrt::fire_and_forget PerformWrite();
-  winrt::fire_and_forget PerformClose();
+  winrt::Windows::Foundation::IAsyncAction PerformConnect() noexcept;
+  winrt::fire_and_forget PerformPing() noexcept;
+  winrt::fire_and_forget PerformWrite() noexcept;
+  winrt::fire_and_forget PerformClose() noexcept;
 
   void OnMessageReceived(
       winrt::Windows::Networking::Sockets::IWebSocket const &sender,
       winrt::Windows::Networking::Sockets::IMessageWebSocketMessageReceivedEventArgs const &args);
-  void Synchronize();
+  void Synchronize() noexcept;
 
  public:
   WinRTWebSocketResource(
       winrt::Windows::Networking::Sockets::IMessageWebSocket &&socket,
       winrt::Windows::Storage::Streams::IDataWriter &&writer,
       winrt::Windows::Foundation::Uri &&uri,
-      std::vector<winrt::Windows::Security::Cryptography::Certificates::ChainValidationResult> &&certExeptions);
+      std::vector<winrt::Windows::Security::Cryptography::Certificates::ChainValidationResult>
+          &&certExeptions) noexcept;
 
   WinRTWebSocketResource(
       const std::string &urlString,
-      std::vector<winrt::Windows::Security::Cryptography::Certificates::ChainValidationResult> &&certExeptions);
+      std::vector<winrt::Windows::Security::Cryptography::Certificates::ChainValidationResult>
+          &&certExeptions) noexcept;
 
-  ~WinRTWebSocketResource() override;
+  ~WinRTWebSocketResource() noexcept override;
 
 #pragma region IWebSocketResource
 
   /// <summary>
   /// <see cref="IWebSocketResource::Connect" />
   /// </summary>
-  void Connect(const Protocols &protocols, const Options &options) override;
+  void Connect(const Protocols &protocols, const Options &options) noexcept override;
 
   /// <summary>
   /// <see cref="IWebSocketResource::Ping" />
   /// </summary>
-  void Ping() override;
+  void Ping() noexcept override;
 
   /// <summary>
   /// <see cref="IWebSocketResource::Send" />
   /// </summary>
-  void Send(const std::string &message) override;
+  void Send(const std::string &message) noexcept override;
 
   /// <summary>
   /// <see cref="IWebSocketResource::SendBinary" />
   /// </summary>
-  void SendBinary(const std::string &base64String) override;
+  void SendBinary(const std::string &base64String) noexcept override;
 
   /// <summary>
   /// <see cref="IWebSocketResource::Close" />
   /// </summary>
-  void Close(CloseCode code, const std::string &reason) override;
+  void Close(CloseCode code, const std::string &reason) noexcept override;
 
-  ReadyState GetReadyState() const override;
+  ReadyState GetReadyState() const noexcept override;
 
   /// <summary>
   /// <see cref="IWebSocketResource::SetOnConnect" />
   /// </summary>
-  void SetOnConnect(std::function<void()> &&handler) override;
+  void SetOnConnect(std::function<void()> &&handler) noexcept override;
 
   /// <summary>
   /// <see cref="IWebSocketResource::SetOnPing" />
   /// </summary>
-  void SetOnPing(std::function<void()> &&handler) override;
+  void SetOnPing(std::function<void()> &&handler) noexcept override;
 
   /// <summary>
   /// <see cref="IWebSocketResource::SetOnSend" />
   /// </summary>
-  void SetOnSend(std::function<void(std::size_t)> &&handler) override;
+  void SetOnSend(std::function<void(std::size_t)> &&handler) noexcept override;
 
   /// <summary>
   /// <see cref="IWebSocketResource::SetOnMessage" />
   /// </summary>
-  void SetOnMessage(std::function<void(std::size_t, const std::string &)> &&handler) override;
+  void SetOnMessage(std::function<void(std::size_t, const std::string &)> &&handler) noexcept override;
 
   /// <summary>
   /// <see cref="IWebSocketResource::SetOnClose" />
   /// </summary>
-  void SetOnClose(std::function<void(CloseCode, const std::string &)> &&handler) override;
+  void SetOnClose(std::function<void(CloseCode, const std::string &)> &&handler) noexcept override;
 
   /// <summary>
   /// <see cref="IWebSocketResource::SetOnError" />
   /// </summary>
-  void SetOnError(std::function<void(Error &&)> &&handler) override;
+  void SetOnError(std::function<void(Error &&)> &&handler) noexcept override;
 
 #pragma endregion IWebSocketResource
 };
