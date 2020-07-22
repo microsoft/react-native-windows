ec7acb6a9e2133d9f1b957a279eabb7f9bcd6513
Keith Melmon <kmelmon@microsoft.com>
2020-07-21 23:46:13 -0700

    adding an actual call into wdio to force its hand

--- packages/E2ETest/wdio/test/AAA_SmokeTest.spec.ts
+++ packages/E2ETest/wdio/test/AAA_SmokeTest.spec.ts
@@ -2,7 +2,11 @@
  * Copyright (c) Microsoft Corporation.
  * Licensed under the MIT License.
  */
+import { By } from '../pages/BasePage';
+import { LOGIN_TESTPAGE } from 'react-native-windows/RNTester/js/examples-win/LegacyTests/Consts';
 
 describe('SmokeTest', () => {
-  it('SmokeTest', () => {});
+  it('SmokeTest', () => {
+    By(LOGIN_TESTPAGE);
+  });
 });
