f71611fd4259195ba4a804720670bf2155f96b3b
Keith Melmon <kmelmon@microsoft.com>
2020-07-21 18:49:38 -0700

    stub

---
+++ packages/E2ETest/wdio/test/VisitAllPages.spec.ts
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT License.
+ */
+
+import assert from 'assert';
+
+//import assert from 'assert';
+
+describe('VisitAllPagesTest', () => {
+  it('Test', () => {
+    let dummy = 10;
+    assert(dummy === 10, 'yay');
+  });
+});
