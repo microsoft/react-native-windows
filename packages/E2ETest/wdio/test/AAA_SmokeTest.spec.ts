ab51f866a66f75fc94f9aae28a9f6076bf84bb43
Keith Melmon <kmelmon@microsoft.com>
2020-07-22 17:44:21 -0700

    comment

--- packages/E2ETest/wdio/test/AAA_SmokeTest.spec.ts
+++ packages/E2ETest/wdio/test/AAA_SmokeTest.spec.ts
@@ -6,6 +6,10 @@ import HomePage from '../pages/HomePage';
 
 describe('SmokeTest', () => {
   it('SmokeTest', () => {
+    // Only click the button to go to the ControlStyle test page,
+    // to workaround an instability in WinAppDriver (?) on first test launch,
+    // which causes the click to not actually work if the control needs to
+    // first be scrolled into view.
     HomePage.clickControlStylePageButton();
   });
 });
