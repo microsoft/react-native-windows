19f9e2ee9f8736f515f6a50f3ff1bc5ce01c4589
Keith Melmon <kmelmon@microsoft.com>
2020-07-22 18:08:06 -0700

    so far so good, try #3

--- packages/E2ETest/wdio/test/AAA_SmokeTest.spec.ts
+++ packages/E2ETest/wdio/test/AAA_SmokeTest.spec.ts
@@ -6,7 +6,7 @@ import HomePage from '../pages/HomePage';
 
 describe('SmokeTest', () => {
   it('SmokeTest', () => {
-    // Only click the button to go to the ControlStyle test page,
+    // Only click the button to try to go to the ControlStyle test page,
     // to workaround an instability in WinAppDriver (?) on first test launch,
     // which causes the click to not actually work if the control needs to
     // first be scrolled into view.
