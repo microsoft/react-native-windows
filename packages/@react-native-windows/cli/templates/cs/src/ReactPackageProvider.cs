88f4a1666af9a730877ab531044ead69201cf728
Danny van Velzen 🁴 <dannyvv@microsoft.com>
2020-08-04 13:22:48 -0700

    Switch to C# partial class

--- packages/@react-native-windows/cli/templates/cs/src/ReactPackageProvider.cs
+++ packages/@react-native-windows/cli/templates/cs/src/ReactPackageProvider.cs
@@ -1,9 +1,6 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
 using Microsoft.ReactNative;
 
-namespace ReactUWPTestApp
+namespace {{ namespace }}
 {
     public partial class ReactPackageProvider : IReactPackageProvider
     {
