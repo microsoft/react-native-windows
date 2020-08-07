3a195aaa6f238919aadf2b186b96158dd4976893
Julio C. Rocha <julio.rocha@microsoft.com>
2020-08-07 16:29:24 -0700

    Add Get/SetRuntimeOptionInt.

--- vnext/Shared/FeatureGate.h
+++ vnext/Shared/FeatureGate.h
@@ -11,13 +11,17 @@ namespace Microsoft::React {
 /// Sets a global boolean value identified by a random string.
 /// </summary>
 /// <param name="name">Global boolean key</param>
-void __cdecl SetFeatureGate(std::string &&name, bool value) noexcept;
+void __cdecl SetRuntimeOptionBool(std::string &&name, bool value) noexcept;
+
+void __cdecl SetRuntimeOptionInt(std::string &&name, std::int32_t value) noexcept;
 
 /// <summary>
 /// Retrieves a global boolean value for the given key.
 /// </summary>
 /// <param name="name">Global boolean key</param>
 /// <returns>Value stored for the given key, or false if the entry doesn't exist (default)</returns>
-const bool __cdecl GetFeatureGate(const std::string &name) noexcept;
+const bool __cdecl GetRuntimeOptionBool(const std::string &name) noexcept;
+
+const std::int32_t __cdecl GetRuntimeOptionInt(const std::string &name) noexcept;
 
 } // namespace Microsoft::React
