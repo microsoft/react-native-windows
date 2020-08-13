377d1472419832ce4a7804e1cde12deaf5902c09
Vladimir Morozov <vmorozov@microsoft.com>
2020-08-12 19:49:17 -0700

    Move new Chakra JSI Universal tests into MS.RN.ComponentTests

--- vnext/JSI/Universal.UnitTests/TestMain.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-
-#include "gtest/gtest.h"
-
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
