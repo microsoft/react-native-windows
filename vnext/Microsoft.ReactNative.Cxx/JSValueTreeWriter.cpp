bab7b5ee2815905b48e83b9e3de6a7ff125f7c39
Zihan Chen (MSFT) <zihanc@microsoft.com>
2020-09-12 11:05:16 -0700

    ...

--- vnext/Microsoft.ReactNative.Cxx/JSValueTreeWriter.cpp
+++ vnext/Microsoft.ReactNative.Cxx/JSValueTreeWriter.cpp
@@ -1,6 +1,16 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+///////////////////////////////////////////////////////////////////////////////
+//                              IMPORTANT
+//
+// This file is used in both react-native-windows and react-native-macos
+//     windows: vntext/Microsoft.ReactNative.Cxx
+//     macOS:   RNTester/RNTester-macOS/TurboModuleCxx
+// You are required to commit exactly the same content to both repo
+// It will be eventually moved to a shared repo
+///////////////////////////////////////////////////////////////////////////////
+
 #include "pch.h"
 #include "JSValueTreeWriter.h"
 #include "Crash.h"
---
+++ vnext/Microsoft.ReactNative.Cxx/ShareToMac.bat
@@ -0,0 +1,14 @@
+pushd .
+copy Crash.h                    ..\..\..\react-native-macos\RNTester\RNTester-macOS\TurboModuleCxx
+copy StructInfo.h               ..\..\..\react-native-macos\RNTester\RNTester-macOS\TurboModuleCxx
+copy JSValue.h                  ..\..\..\react-native-macos\RNTester\RNTester-macOS\TurboModuleCxx
+copy JSValue.cpp                ..\..\..\react-native-macos\RNTester\RNTester-macOS\TurboModuleCxx
+copy JSValueTreeReader.h        ..\..\..\react-native-macos\RNTester\RNTester-macOS\TurboModuleCxx
+copy JSValueTreeReader.cpp      ..\..\..\react-native-macos\RNTester\RNTester-macOS\TurboModuleCxx
+copy JSValueTreeWriter.h        ..\..\..\react-native-macos\RNTester\RNTester-macOS\TurboModuleCxx
+copy JSValueTreeWriter.cpp      ..\..\..\react-native-macos\RNTester\RNTester-macOS\TurboModuleCxx
+copy JSValueReader.h            ..\..\..\react-native-macos\RNTester\RNTester-macOS\TurboModuleCxx
+copy JSValueWriter.h            ..\..\..\react-native-macos\RNTester\RNTester-macOS\TurboModuleCxx
+copy ModuleRegistration.h       ..\..\..\react-native-macos\RNTester\RNTester-macOS\TurboModuleCxx
+copy ModuleRegistration.cpp     ..\..\..\react-native-macos\RNTester\RNTester-macOS\TurboModuleCxx
+popd
