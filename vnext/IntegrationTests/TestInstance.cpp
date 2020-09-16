a17463f1bb7e9d37c50f43e906a192e1499275e9
Alexander Sklar <asklar@winse.microsoft.com>
2020-09-15 23:14:34 -0700

    remove typeinfo

--- vnext/IntegrationTests/TestInstance.cpp
+++ vnext/IntegrationTests/TestInstance.cpp
@@ -141,7 +141,3 @@ void TestShadowNode::createView() {}
 #pragma endregion
 
 } // namespace Microsoft::React::Test
-
-namespace folly {
-char const *const dynamic::TypeInfo<dynamic::ObjectImpl>::name = "object";
-}
