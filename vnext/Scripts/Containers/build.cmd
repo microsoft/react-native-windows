4bc269e6e037fc9c0815c271405581112109ce6e
Danny van Velzen 🁴 <dannyvv@microsoft.com>
2020-09-22 10:32:28 -0700

    Run validations on containers

--- vnext/Scripts/Containers/build.cmd
+++ vnext/Scripts/Containers/build.cmd
@@ -1,1 +1,16 @@
-@echo off
+REM @echo off
+setlocal
+set containerDate=20200930.08
+set containerCommit=d3a6c9fbf429adad31de2ae5679cba57de735a79
+
+set prereqContainerName=rnwci.azurecr.io/pr/prereqs:%containerDate%
+set warmedUpContainerName=rnwci.azurecr.io/pr/warmedup:%containerCommit%
+
+pushd PreReq
+copy ..\..\rnw-dependencies.ps1 .
+docker build -m 12G -t %prereqContainerName% --compress .
+popd
+
+pushd WarmCache
+docker build -m 12G --buildArg IMAGE=%prereqContainerName% -t %warmedUpContainerName% --compress .
+popd
