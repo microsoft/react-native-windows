6f8827b30993271ba4fbd07fc8266e3831bdc735
Julio C. Rocha <julio.rocha@microsoft.com>
2020-09-25 04:56:44 -0700

    Update XHRTest.

--- vnext/src/IntegrationTests/XHRTest.js
+++ vnext/src/IntegrationTests/XHRTest.js
@@ -31,78 +31,83 @@ class XHRTest extends React.Component<{}, Object> {
   xhr: ?XMLHttpRequest = null;
 
   _download = () => {
-    let xhr;
-    if (this.xhr) {
-      xhr = this.xhr;
-      xhr.abort();
-    } else {
-      xhr = this.xhr = new XMLHttpRequest();
-    }
-
-    const onreadystatechange = () => {
-      if (xhr.readyState === xhr.HEADERS_RECEIVED) {
-        const contentLength = parseInt(
-          xhr.getResponseHeader('Content-Length'),
-          10,
-        );
-        this.setState({
-          contentLength,
-          responseLength: 0,
-        });
-      } else if (xhr.readyState === xhr.LOADING && xhr.response) {
-        this.setState({
-          responseLength: xhr.response.length,
-        });
-      }
-    };
-
-    if (this.state.readystateHandler) {
-      xhr.onreadystatechange = onreadystatechange;
-    }
-    //ISS:2306365 - Uncomment when native module is complete.
-    //if (this.state.progressHandler) {
-    //  xhr.onprogress = onprogress;
-    //}
-    if (this.state.arraybuffer) {
-      xhr.responseType = 'arraybuffer';
-    }
-    xhr.onload = () => {
-      this.setState({downloading: false});
-      if (xhr.status === 200) {
-        let responseType = `Response is a string, ${
-          xhr.response.length
-        } characters long.`;
-        if (xhr.response instanceof ArrayBuffer) {
-          responseType = `Response is an ArrayBuffer, ${
-            xhr.response.byteLength
-          } bytes long.`;
-        }
-        Alert.alert('Download complete!', responseType);
-      } else if (xhr.status !== 0) {
-        Alert.alert(
-          'Error',
-          `Server returned HTTP status of ${xhr.status}: ${xhr.responseText}`,
-        );
+    return new Promise((resolve, reject) => {
+      let xhr;
+      if (this.xhr) {
+        xhr = this.xhr;
+        xhr.abort();
       } else {
-        Alert.alert('Error', xhr.responseText);
+        xhr = this.xhr = new XMLHttpRequest();
       }
-    };
-    xhr.open('GET', 'http://aleph.gutenberg.org/cache/epub/100/pg100.txt.utf8');
-    // Avoid gzip so we can actually show progress
-    xhr.setRequestHeader('Accept-Encoding', '');
-    xhr.send();
 
-    this.setState({downloading: true});
+      const onreadystatechange = () => {
+        if (xhr.readyState === xhr.HEADERS_RECEIVED) {
+          const contentLength = parseInt(
+            xhr.getResponseHeader('Content-Length'),
+            10,
+          );
+          this.setState({
+            contentLength,
+            responseLength: 0,
+          });
+        } else if (xhr.readyState === xhr.LOADING && xhr.response) {
+          this.setState({
+            responseLength: xhr.response.length,
+          });
+        }
+      };
+
+      if (this.state.readystateHandler) {
+        xhr.onreadystatechange = onreadystatechange;
+      }
+      //ISS:2306365 - Uncomment when native module is complete.
+      //if (this.state.progressHandler) {
+      //  xhr.onprogress = onprogress;
+      //}
+      if (this.state.arraybuffer) {
+        xhr.responseType = 'arraybuffer';
+      }
+      xhr.onload = () => {
+        this.setState({downloading: false});
+        if (xhr.status === 200) {
+          let responseType = `Response is a string, ${
+            xhr.response.length
+          } characters long.`;
+          if (xhr.response instanceof ArrayBuffer) {
+            responseType = `Response is an ArrayBuffer, ${
+              xhr.response.byteLength
+            } bytes long.`;
+          }
+          Alert.alert('Download complete!', responseType);
+        } else if (xhr.status !== 0) {
+          Alert.alert(
+            'Error',
+            `Server returned HTTP status of ${xhr.status}: ${xhr.responseText}`,
+          );
+        } else {
+          Alert.alert('Error', xhr.responseText);
+        }
+      };
+      xhr.open('GET', 'https://github.com/microsoft/react-native-windows/raw/master/.github/hero2.png');
+      // Avoid gzip so we can actually show progress
+      xhr.setRequestHeader('Accept-Encoding', '');
+      xhr.send();
+
+      this.setState({downloading: true});
+    });
+
   };
 
   componentDidMount() {
     try {
-      this._download();
+      async () => await this._download();
+      TestModule.markTestPassed(true);
+      return;
     } catch (e) {
       console.error(e);
     }
 
-    TestModule.markTestPassed(true);
+    TestModule.markTestPassed(false);
   }
 
   render(): React.Node {
