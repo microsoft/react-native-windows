7baf74ec48b7d8d7ab78d9349f95a13272311eb2
Ryan Fowler <ryan.fowler@singlewire.com>
2020-09-28 12:14:06 -0600

    Fix second oncreate (#6099)
    * Don't reload the ReactNativeHost multiple times
    
    When OnActivate is called on an already-running instance,
    the Host should not be reloaded so we don't lose the
    native modules.
    
    * Change files

--- vnext/Microsoft.ReactNative/ReactApplication.cpp
+++ vnext/Microsoft.ReactNative/ReactApplication.cpp
@@ -182,7 +182,8 @@ void ReactApplication::OnCreate(Windows::ApplicationModel::Activation::IActivate
 
   // Do not repeat app initialization when the Window already has content,
   // just ensure that the window is active
-  if (rootFrame == nullptr) {
+  bool previouslyInitialized = (rootFrame != nullptr);
+  if (!previouslyInitialized) {
     // Create a Frame to act as the navigation context and associate it with
     // a SuspensionManager key
     rootFrame = Frame();
@@ -198,8 +199,10 @@ void ReactApplication::OnCreate(Windows::ApplicationModel::Activation::IActivate
 
   ApplyArguments(Host(), args.c_str());
 
-  // Nudge the ReactNativeHost to create the instance and wrapping context
-  Host().ReloadInstance();
+  if (!previouslyInitialized) {
+    // Nudge the ReactNativeHost to create the instance and wrapping context
+    Host().ReloadInstance();
+  }
 
   Window::Current().Activate();
 }
