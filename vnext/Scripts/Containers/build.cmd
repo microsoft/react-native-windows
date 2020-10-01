9ad9f4b2034a637bda4878df57e9e6c6553fcba2
Danny van Velzen 🁴 <dannyvv@microsoft.com>
2020-10-01 13:09:25 -0700

    new container

--- vnext/Scripts/Containers/build.cmd
+++ vnext/Scripts/Containers/build.cmd
@@ -1,7 +1,7 @@
 REM @echo off
 setlocal
-set containerDate=20200930.15
-set containerCommit=d3a6c9fbf429adad31de2ae5679cba57de735a79
+set containerDate=20201001.10
+set containerCommit=ec54f8674c43d4fddc34f9e8ee9ce1b71124f572
 
 set prereqContainerName=rnwci.azurecr.io/pr/prereqs:%containerDate%
 set warmedUpContainerName=rnwci.azurecr.io/pr/warmedup:%containerCommit%
@@ -15,6 +15,6 @@ pushd WarmCache
 docker build -m 12G --build-arg IMAGE=%prereqContainerName% -t %warmedUpContainerName% --compress .
 popd
 
-az acr login -n rnwci
+call az acr login -n rnwci
 docker push  rnwci.azurecr.io/pr/warmedup:%prereqContainerName%
 docker push  rnwci.azurecr.io/pr/warmedup:%warmedUpContainerName%
