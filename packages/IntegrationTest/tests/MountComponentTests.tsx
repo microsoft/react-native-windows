20a733333b96dc17d89b41db621a7a05700b6433
Marlene Cota <mcota@microsoft.com>
2020-10-01 15:18:58 -0700

    remove version check in MountComponentTests.tsx

--- packages/IntegrationTest/tests/MountComponentTests.tsx
+++ packages/IntegrationTest/tests/MountComponentTests.tsx
@@ -95,13 +95,6 @@ componentTest(
 componentTest.skip('Modal', mountAndMeasure(RN.Modal));
 
 componentTest('Picker', props => {
-  // Using AsyncStorage will YellowBox, which crashes on pre-1903 (including
-  // CI) trying to load an image (#6085)
-  if ((RN.Platform.Version as number) < 8) {
-    props.pass();
-    return <RN.View />;
-  }
-
   const PickerTestComponent = mountAndMeasure(
     React.forwardRef<RN.Image>((_, ref) => (
       <RN.Picker ref={ref}>
