f0995aacf13d17c3f2ad801f92ef567def30dbf5
Julio C. Rocha <julio.rocha@microsoft.com>
2020-10-01 19:23:19 -0700

    Add CreateModules.cpp

---
+++ vnext/Microsoft.ReactNative/Modules/CreateModules.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#include <WinRTWebSocketResource.h>
+
+namespace Microsoft::React {
+
+std::shared_ptr<IWebSocketResource> IWebSocketResource::Make(std::string &&urlString) {
+  return nullptr;
+}
+} // namespace Microsoft::React
