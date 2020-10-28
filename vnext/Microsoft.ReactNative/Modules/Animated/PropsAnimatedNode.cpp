6c5a31a867b2635adbbf1ca4d0042002f922ece8
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2020-10-28 09:08:28 -0700

    review feedback

--- vnext/Microsoft.ReactNative/Modules/Animated/PropsAnimatedNode.cpp
+++ vnext/Microsoft.ReactNative/Modules/Animated/PropsAnimatedNode.cpp
@@ -155,36 +155,9 @@ void PropsAnimatedNode::StartAnimations() {
 }
 
 void PropsAnimatedNode::DisposeCompletedAnimation(int64_t valueTag) {
-  /*
-  if (m_expressionAnimations.count(valueTag)) {
-    if (const auto target = GetUIElement()) {
-      // We should start and stop the expression animations if there are
-      // no active animations. Suspending the active expression animations
-      // while they are not in use causes subsequent key frame animations
-      // which target the providing property set to never fire their completed
-      // events. I can't explain this and for now and commenting out the code.
-      // Fixing this will prevent memory bloat as the current solutions never
-      // stops the expression animations that are built as a part of this
-      // animation solution. Tracked by issue #3280.
-      target.StopAnimation(m_expressionAnimations.at(valueTag));
-      m_suspendedExpressionAnimationTags.push_back(valueTag);
-    }
-  }
-  */
 }
 
 void PropsAnimatedNode::ResumeSuspendedAnimations(int64_t valueTag) {
-  /*
-  const auto iterator =
-      std::find(m_suspendedExpressionAnimationTags.begin(), m_suspendedExpressionAnimationTags.end(), valueTag);
-  if (iterator != m_suspendedExpressionAnimationTags.end()) {
-    if (const auto target = GetUIElement()) {
-      // See comment above, tracked by issue #3280
-      target.StartAnimation(m_expressionAnimations.at(valueTag));
-      m_suspendedExpressionAnimationTags.erase(iterator);
-    }
-  }
-  */
 }
 
 void PropsAnimatedNode::MakeAnimation(int64_t valueNodeTag, FacadeType facadeType) {
--- vnext/Microsoft.ReactNative/Views/ReactRootControl.cpp
+++ vnext/Microsoft.ReactNative/Views/ReactRootControl.cpp
@@ -246,7 +246,7 @@ void ReactRootControl::ClearLoadingUI() noexcept {
     auto xamlRootGrid{xamlRootView.as<winrt::Grid>()};
 
     auto children = xamlRootGrid.Children();
-    uint32_t index{0};
+    uint32_t index;
     if (m_greenBoxGrid && children.IndexOf(m_greenBoxGrid, index)) {
       children.RemoveAt(index);
     }
