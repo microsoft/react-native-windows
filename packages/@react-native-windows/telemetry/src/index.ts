7804604785cbcff8ce19ab6ff308f90ae2f37db3
Alexander Sklar <asklar@microsoft.com>
2020-11-04 01:33:02 -0800

    create telemetry package

---
+++ packages/@react-native-windows/telemetry/src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT License.
+ * @format
+ */
+
+import {telClient} from './telemetry';
+
+export const telemetryClient = telClient;
---
+++ packages/@react-native-windows/telemetry/src/test/sanitize.test.ts
@@ -0,0 +1,129 @@
+/**
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT License.
+ *
+ * @format
+ */
+
+import * as telemetry from '../telemetry';
+import * as appInsights from 'applicationinsights';
+
+test('Sanitize message, no-op', () => {
+  // do stuff
+  expect(telemetry.sanitizeMessage('')).toEqual('');
+  expect(telemetry.sanitizeMessage('some text')).toEqual('some text');
+});
+
+test('Sanitize message, project_dir', () => {
+  expect(
+    telemetry.sanitizeMessage(`this is the cwd: '${process.cwd()}'`),
+  ).toEqual('this is the cwd:  [project_dir]\\???(47)');
+  expect(
+    telemetry.sanitizeMessage(
+      `this is the cwd: '${process.cwd().toUpperCase()}'`,
+    ),
+  ).toEqual('this is the cwd:  [project_dir]\\???(47)');
+  expect(
+    telemetry.sanitizeMessage(
+      `this is the cwd: '${process.cwd().toLowerCase()}'`,
+    ),
+  ).toEqual('this is the cwd:  [project_dir]\\???(47)');
+  expect(
+    telemetry.sanitizeMessage(
+      `this is the cwd: '${process.cwd()}' and something else`,
+    ),
+  ).toEqual('this is the cwd:  [project_dir]\\???(47)  and something else');
+  expect(
+    telemetry.sanitizeMessage(
+      `this is the cwd: ${process.cwd()} and something else`,
+    ),
+  ).toEqual('this is the cwd: [project_dir]\\...');
+});
+
+test('Sanitize message, node_modules', () => {
+  expect(
+    telemetry.sanitizeMessage(
+      `this is the cwd: '${process.cwd()}\\node_modules'`,
+    ),
+  ).toEqual('this is the cwd:  [project_dir]\\???(60)');
+  expect(
+    telemetry.sanitizeMessage(
+      `this is the cwd: '${process.cwd()}\\node_modules\\'`,
+    ),
+  ).toEqual('this is the cwd:  node_modules\\');
+  expect(
+    telemetry.sanitizeMessage(
+      `this is the cwd: '${process.cwd().toUpperCase()}\\NODE_MODULES\\'`,
+    ),
+  ).toEqual('this is the cwd:  node_modules\\');
+  expect(
+    telemetry.sanitizeMessage(
+      `this is the cwd: '${process.cwd().toLowerCase()}\\NODE_MODULES\\'`,
+    ),
+  ).toEqual('this is the cwd:  node_modules\\');
+  expect(
+    telemetry.sanitizeMessage(
+      `this is the cwd: '${process.cwd()}\\node_modules\\' and something else`,
+    ),
+  ).toEqual('this is the cwd:  node_modules\\  and something else');
+  expect(
+    telemetry.sanitizeMessage(
+      `this is the cwd: ${process.cwd()}\\node_modules and something else that could be part of the path`,
+    ),
+  ).toEqual('this is the cwd: [project_dir]\\...');
+  expect(
+    telemetry.sanitizeMessage(
+      `this is the cwd: ${process.cwd()}\\node_modules\\ and something else that could be part of the path`,
+    ),
+  ).toEqual('this is the cwd: [project_dir]\\...');
+});
+
+test('Sanitize stack frame', () => {
+  const emptyFrame: appInsights.Contracts.StackFrame = {
+    level: 0,
+    method: '',
+    fileName: '',
+    assembly: 'asdf',
+    line: 0,
+  };
+  telemetry.sanitizeFrame(emptyFrame);
+  expect(emptyFrame).toEqual({
+    level: 0,
+    assembly: '',
+    fileName: '.',
+    method: '',
+    line: 0,
+  });
+
+  const frame1: appInsights.Contracts.StackFrame = {
+    method: '',
+    fileName: `${process.cwd()}\\foo.js`,
+    assembly: 'asdf',
+    level: 0,
+    line: 0,
+  };
+  telemetry.sanitizeFrame(frame1);
+  expect(frame1).toEqual({
+    assembly: '',
+    fileName: 'telemetry\\foo.js',
+    method: '',
+    level: 0,
+    line: 0,
+  });
+
+  const frame2: appInsights.Contracts.StackFrame = {
+    method: `myMethod (something ${process.cwd()}`,
+    fileName: `${process.cwd()}\\foo.js`,
+    assembly: 'asdf',
+    level: 1,
+    line: 42,
+  };
+  telemetry.sanitizeFrame(frame2);
+  expect(frame2).toEqual({
+    assembly: '',
+    fileName: 'telemetry\\foo.js',
+    method: 'myMethod',
+    level: 1,
+    line: 42,
+  });
+});
