2f0d665975655a9bd2317403c7f5a3805bac7f78
Alexander Sklar <asklar@microsoft.com>
2020-11-05 02:59:41 -0800

    lint

--- packages/@react-native-windows/telemetry/src/test/sanitize.test.ts
+++ packages/@react-native-windows/telemetry/src/test/sanitize.test.ts
@@ -40,7 +40,11 @@ test('Sanitize message, project_dir', () => {
     telemetry.sanitizeMessage(
       `this is the cwd: ${process.cwd()} and something else`,
     ),
-  ).toEqual(`this is the cwd: [project_dir]\\???(${(process.cwd() + ' and something else').length})`);
+  ).toEqual(
+    `this is the cwd: [project_dir]\\???(${
+      (process.cwd() + ' and something else').length
+    })`,
+  );
 });
 
 test('Sanitize message, node_modules', () => {
@@ -77,12 +81,19 @@ test('Sanitize message, node_modules', () => {
     telemetry.sanitizeMessage(
       `this is the cwd: ${process.cwd()}\\node_modules and something else that could be part of the path`,
     ),
-  ).toEqual(`this is the cwd: [project_dir]\\???(${(process.cwd() + '\\node_modules and something else that could be part of the path').length})`);
+  ).toEqual(
+    `this is the cwd: [project_dir]\\???(${
+      (
+        process.cwd() +
+        '\\node_modules and something else that could be part of the path'
+      ).length
+    })`,
+  );
   expect(
     telemetry.sanitizeMessage(
       `this is the cwd: ${process.cwd()}\\node_modules\\ a file under nm`,
     ),
-    ).toEqual(`this is the cwd: node_modules\\ a file under nm`);
+  ).toEqual(`this is the cwd: node_modules\\ a file under nm`);
 });
 
 test('Sanitize message, other path', () => {
@@ -103,8 +114,18 @@ test('Sanitize message, other path', () => {
     ),
   ).toEqual(`Cannot find module  react-native/package.json 
       Require stack:
-      - [appdata]\\???(${(process.env.APPDATA + '\\npm-cache\\_npx\\1384\\node_modules\\react-native-windows-init\\lib-commonjs\\Cli.js').length})
-      - [appdata]\\???(${(process.env.APPDATA + '\\npm-cache\\_npx\\1384\\node_modules\\react-native-windows-init\\bin.js').length})`);
+      - [appdata]\\???(${
+        (
+          process.env.APPDATA +
+          '\\npm-cache\\_npx\\1384\\node_modules\\react-native-windows-init\\lib-commonjs\\Cli.js'
+        ).length
+      })
+      - [appdata]\\???(${
+        (
+          process.env.APPDATA +
+          '\\npm-cache\\_npx\\1384\\node_modules\\react-native-windows-init\\bin.js'
+        ).length
+      })`);
 });
 
 test('Sanitize stack frame', () => {
@@ -219,7 +240,9 @@ test('thrown exception a->b, hello path', done => {
       const data = (envelope.data as any).baseData;
       expect(data.exceptions).toBeDefined();
       expect(data.exceptions.length).toEqual(1);
-      expect(data.exceptions[0].message).toEqual(`hello [project_dir]\\???(${process.cwd().length})`);
+      expect(data.exceptions[0].message).toEqual(
+        `hello [project_dir]\\???(${process.cwd().length})`,
+      );
 
       const stack = data.exceptions[0].parsedStack;
       expect(stack).toBeDefined();
