555cf61f6082b736fe4fd34a19b27d9c77f07a5c
Nick Gerleman <ngerlem@microsoft.com>
2020-11-08 11:52:36 -0800

    whoops

--- packages/react-native-win32/.eslintrc.js
+++ packages/react-native-win32/.eslintrc.js
@@ -4,10 +4,6 @@ module.exports = {
     // Not clean
     "react-hooks/exhaustive-deps": "warn",
     "react-hooks/rules-of-hooks": "warn",
-
-    // @typescript/eslint rules requiring compilation don't work with noEmit
-    '@typescript-eslint/no-floating-promises': 'off',
-    '@typescript-eslint/no-misused-promises': 'off',
   },
-  parserOptions: {project : null},
+  parserOptions: {tsconfigRootDir : __dirname},
 };
--- vnext/.eslintrc.js
+++ vnext/.eslintrc.js
@@ -1,9 +1,4 @@
 module.exports = {
   extends: ['@rnw-scripts'],
-  rules: {
-    // @typescript/eslint rules requiring compilation don't work with noEmit
-    '@typescript-eslint/no-floating-promises': 'off',
-    '@typescript-eslint/no-misused-promises': 'off',
-  },
-  parserOptions: {project : null},
+  parserOptions: {tsconfigRootDir : __dirname},
 };
