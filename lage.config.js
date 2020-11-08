cf4199820c7490a837bdf5c15c20735b4feb2cc0
Nick Gerleman <ngerlem@microsoft.com>
2020-11-08 10:56:11 -0800

    Lerna to Lage
    This gives us incremental and scoped JS builds, with more intelligent parallelism. I haven't configured fine-grained caching for the more esoteric projects, but as is, 12/17 projects will cache outputs. The incremental build of the repo takes ~15s on my machine.

---
+++ lage.config.js
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT License.
+ * @format
+ */
+
+module.exports = {
+  pipeline: {
+    build: ["^build"],
+    lint: ["build"],
+    'lint:fix': [],
+    test: ["build"],
+    format: [],
+    'format:verify': [],
+    clean: [],
+  },
+  cacheOptions: {
+    outputGlob: [
+      "lib/**",
+      "lib-commonjs/**",
+    ],
+  }
+};
