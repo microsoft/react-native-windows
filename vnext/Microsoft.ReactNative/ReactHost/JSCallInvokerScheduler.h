50580e89e298bea2216551b3b529456987cdf97b
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2020-11-09 10:28:27 -0800

    formatting

--- vnext/Microsoft.ReactNative/ReactHost/JSCallInvokerScheduler.h
+++ vnext/Microsoft.ReactNative/ReactHost/JSCallInvokerScheduler.h
@@ -3,10 +3,9 @@
 namespace facebook::react {
 class CallInvoker;
 class MessageQueueThread;
-}
+} // namespace facebook::react
 
 namespace Mso {
-
 
 MSO_GUID(IJSCallInvokerQueueScheduler, "f4ea9a4a-aa44-4c85-8f6e-f2ebc3bdf27f")
 struct IJSCallInvokerQueueScheduler : IUnknown {
@@ -17,4 +16,4 @@ Mso::CntPtr<IDispatchQueueScheduler> MakeJSCallInvokerScheduler(
     std::shared_ptr<facebook::react::CallInvoker> callInvoker,
     Mso::Functor<void(const Mso::ErrorCode &)> &&errorHandler,
     Mso::Promise<void> &&whenQuit = nullptr) noexcept;
-}
+} // namespace Mso
