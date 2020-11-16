da3bfb6defd16cec04eb609babc5dfde5e28f576
Alexander Sklar <asklar@microsoft.com>
2020-11-16 12:19:06 -0800

    Embed docs in winmd (#6508)
    * Embed documentation of WinRT APIs in the winmd
    
    * more from docs
    
    * Change files
    
    * move to mrn namespace
    
    * pr fb

--- vnext/Microsoft.ReactNative/DocString.h
+++ vnext/Microsoft.ReactNative/DocString.h
@@ -5,7 +5,9 @@
 
 #ifdef USE_DOCSTRING
 #define DOC_STRING(x) [doc_string(x)]
+#define DOC_DEFAULT(x) [doc_default(x)]
 import "DocString.idl";
 #else
 #define DOC_STRING(x)
+#define DOC_DEFAULT(x)
 #endif
