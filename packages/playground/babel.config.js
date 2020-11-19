08178c3247738323331c1e90461ce97960f29259
Nick Gerleman <ngerlem@microsoft.com>
2020-11-18 18:46:14 -0800

    Disable babel-plugin-codegen

--- packages/playground/babel.config.js
+++ packages/playground/babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [require('../../vnext/src/babel-plugin-codegen')],
+  // plugins: [require('../../vnext/src/babel-plugin-codegen')], #6570
 };
