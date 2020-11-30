e1c35513bb0b5c33c80b98cd2d047f7da5b07df0
CanHua Li <canli@microsoft.com>
2020-11-29 19:38:26 -0800

    remove appium

--- packages/E2ETest/wdio.conf.js
+++ packages/E2ETest/wdio.conf.js
@@ -125,13 +125,9 @@ exports.config = {
   // Services take over a specific job you don't want to take care of. They enhance
   // your test setup with almost no effort. Unlike plugins, they don't add new
   // commands. Instead, they hook themselves up into the test process.
-  services: [
-    [
-      'winappdriver',
-      {
-        logPath: './reports/',
-      },
-    ],
+  services: [['winappdriver'], {
+      logPath: './reports/',
+    },
   ],
 
   //
