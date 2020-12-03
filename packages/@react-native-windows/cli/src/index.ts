821b51f06e73744bcc5164d048223b545f495447
Alexander Sklar <asklar@microsoft.com>
2020-12-03 13:22:43 -0800

    .

--- packages/@react-native-windows/cli/src/index.ts
+++ packages/@react-native-windows/cli/src/index.ts
@@ -6,7 +6,7 @@
 
 import * as fs from 'fs';
 import * as path from 'path';
-import {CodedError, Telemetry} from '@react-native-windows/telemetry';
+import {Telemetry} from '@react-native-windows/telemetry';
 
 import {
   copyProjectTemplateAndReplace,
