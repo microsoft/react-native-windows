4d45a39634f8c76bd11b28d01dc63bd8169c7b36
samuelfreiberg <safreibe@microsoft.com>
2020-12-04 14:05:29 -0800

    Adding Polite/Aggressive Focus APIs

--- packages/react-native-win32/src/Libraries/Components/View/ViewWin32.tsx
+++ packages/react-native-win32/src/Libraries/Components/View/ViewWin32.tsx
@@ -89,6 +89,24 @@ export const ViewWin32 = React.forwardRef(
               );
           };
         }
+
+        // Polite focus functionality - Window does not take focus
+        localRef.politeFocus = () => {
+          NativeModules.UIManager.dispatchViewManagerCommand(
+            findNodeHandle(localRef),
+            NativeModules.UIManager.getViewManagerConfig('RCTView').Commands.politefocus,
+            null
+            );
+        };
+
+        // Aggressive focus functionality - Window does take focus
+        localRef.aggressiveFocus = () => {
+          NativeModules.UIManager.dispatchViewManagerCommand(
+            findNodeHandle(localRef),
+            NativeModules.UIManager.getViewManagerConfig('RCTView').Commands.aggressivefocus,
+            null
+            );
+        };        
       },
     });
 
