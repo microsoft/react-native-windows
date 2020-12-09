de3d2a40f42ff637d1748ce38238ddba9f3cfbd5
samuelfreiberg <safreibe@microsoft.com>
2020-12-09 15:56:44 -0800

    Nit export

--- packages/react-native-win32/src/Libraries/Utilities/FocusManager.win32.js
+++ packages/react-native-win32/src/Libraries/Utilities/FocusManager.win32.js
@@ -5,7 +5,7 @@ import { findNodeHandle, NativeModules } from 'react-native';
 /*
 ** This is a helper class intended to allow usage of Polite/Aggressive Focus for win32.
 */
-class FocusManager {
+export default class FocusManager {
 
     // This function takes in a ref to a React Component and a bool value. If setWindowFocus = true, call aggressive focus.
     // Else, call polite focus
