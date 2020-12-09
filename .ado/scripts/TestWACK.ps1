9de869eb8749cac5c588f01a01fadc59494a1b1c
dannyvv <dannyvv@microsoft.com>
2020-12-09 08:49:06 -0800

    Cleanup .ado folder by moving scripts into separate folder (#6707)

--- .ado/scripts/TestWACK.ps1
+++ .ado/scripts/TestWACK.ps1
@@ -8,7 +8,7 @@ if (!(Test-Path $OutputPath)) {
 }
 
 if ($Configuration -eq 'Release') {
-    $appxPath = Resolve-Path $PSScriptRoot\..\packages\playground\windows\AppPackages\playground\playground_1.0.0.0_${Platform}_Test\playground_1.0.0.0_$Platform.appx
+    $appxPath = Resolve-Path $PSScriptRoot\..\..\packages\playground\windows\AppPackages\playground\playground_1.0.0.0_${Platform}_Test\playground_1.0.0.0_$Platform.appx
     gci $appxPath
 
     & "C:\Program Files (x86)\Windows Kits\10\App Certification Kit\appcert.exe" test -appxpackagepath $appxPath -reportoutputpath $OutputPath\result.xml
