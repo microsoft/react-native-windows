5d7ca74001355f55202d3570b74d1f0121357cfe
samuelfreiberg <safreibe@microsoft.com>
2020-12-10 09:20:34 -0800

    Adding @flow and @format and Microsoft Copyright header

--- packages/react-native-win32/src/Libraries/Utilities/FocusManager.win32.js
+++ packages/react-native-win32/src/Libraries/Utilities/FocusManager.win32.js
@@ -1,36 +1,39 @@
+/**
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT License.
+ *
+ * @format
+ * @flow
+ */
 'use strict';
 
-import { findNodeHandle, NativeModules } from 'react-native';
+import {findNodeHandle, NativeModules} from 'react-native';
 
 /*
-** This is a helper class intended to allow usage of Polite/Aggressive Focus for win32.
-*/
-export default class FocusManager {
-
-    // This function takes in a ref to a React Component and a bool value. If setWindowFocus = true, call aggressive focus.
-    // Else, call polite focus
-    static Focus(ref, setWindowFocus)
-    {
-        if(ref)
-        {
-            if(setWindowFocus)
-            {
-                NativeModules.UIManager.dispatchViewManagerCommand(
-                    findNodeHandle(ref),
-                    NativeModules.UIManager.getViewManagerConfig('RCTView').Commands.aggressivefocus,
-                    null
-                );
-            }
-            else 
-            {
-                NativeModules.UIManager.dispatchViewManagerCommand(
-                    findNodeHandle(ref),
-                    NativeModules.UIManager.getViewManagerConfig('RCTView').Commands.politefocus,
-                    null
-                );
-            }
-        }
+ ** This is a helper class intended to allow usage of Polite/Aggressive Focus for win32.
+ */
+class FocusManager {
+  // This function takes in a ref to a React Component and a bool value. If setWindowFocus = true, call aggressive focus.
+  // Else, call polite focus
+  static Focus(ref, setWindowFocus) {
+    if (ref) {
+      if (setWindowFocus) {
+        NativeModules.UIManager.dispatchViewManagerCommand(
+          findNodeHandle(ref),
+          NativeModules.UIManager.getViewManagerConfig('RCTView').Commands
+            .aggressivefocus,
+          null,
+        );
+      } else {
+        NativeModules.UIManager.dispatchViewManagerCommand(
+          findNodeHandle(ref),
+          NativeModules.UIManager.getViewManagerConfig('RCTView').Commands
+            .politefocus,
+          null,
+        );
+      }
     }
+  }
 }
 
 module.exports = FocusManager;
