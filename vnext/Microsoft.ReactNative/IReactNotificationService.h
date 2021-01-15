cbee0c1c6d300f3c21fe93eb80155a979ef1c3e1
Vladimir Morozov <vmorozov@microsoft.com>
2021-01-14 16:59:04 -0800

    Fix ReactNotificationService for notifications between app and modules

--- vnext/Microsoft.ReactNative/IReactNotificationService.h
+++ vnext/Microsoft.ReactNative/IReactNotificationService.h
@@ -7,9 +7,17 @@
 #include <object/objectRefCount.h>
 #include <winrt/Microsoft.ReactNative.h>
 #include <atomic>
-#include <map>
 #include <mutex>
+#include <unordered_map>
 #include <vector>
+
+namespace std {
+
+// Specialization to use IReactPropertyName in std::unordered_map.
+template <>
+struct hash<winrt::Microsoft::ReactNative::IReactPropertyName> : winrt::impl::hash_base {};
+
+} // namespace std
 
 namespace winrt::Microsoft::ReactNative::implementation {
 
@@ -30,6 +38,15 @@ struct ReactNotificationArgs : implements<ReactNotificationArgs, IReactNotificat
   IInspectable m_data;
 };
 
+// The ReactNotificationService manages notification subscriptions and sending messages.
+// A notification service may have a parent notification service most for the automatic
+// unsubscription when the notification service is deleted.
+// All notifications are always sent from the root notification service that doe snot have parent service.
+//
+// The ReactNotificationService is optimized for sending notifications vs modifying the list of subscriptions.
+// It means that it is very cheap to send messages vs adding or removing subscriptions.
+// It is done through the use of subscription snapshots. The list of the subscriptions is always immutable.
+// When we add or remove subscription, we create a new list of subscriptions and then atomically replace the old one.
 struct ReactNotificationService : implements<ReactNotificationService, IReactNotificationService> {
   ReactNotificationService();
   explicit ReactNotificationService(IReactNotificationService const parentNotificationService) noexcept;
@@ -52,6 +69,7 @@ struct ReactNotificationService : implements<ReactNotificationService, IReactNot
  private:
   // We treat subscription snapshots as immutable data.
   using SubscriptionSnapshot = std::vector<IReactNotificationSubscription>;
+  // To provide correct lifetime management, the snapshot must be ref-counted.
   using SubscriptionSnapshotPtr = Mso::RefCountedPtr<SubscriptionSnapshot>;
 
   void ModifySubscriptions(
@@ -59,9 +77,9 @@ struct ReactNotificationService : implements<ReactNotificationService, IReactNot
       Mso::FunctorRef<SubscriptionSnapshot(SubscriptionSnapshot const &)> const &modifySnapshot);
 
  private:
+  const IReactNotificationService m_parentNotificationService;
   std::mutex m_mutex;
-  std::map<IReactPropertyName, SubscriptionSnapshotPtr> m_subscriptions;
-  IReactNotificationService m_parentNotificationService;
+  std::unordered_map<IReactPropertyName, SubscriptionSnapshotPtr> m_subscriptions;
 };
 
 struct ReactNotificationServiceHelper {
@@ -72,12 +90,18 @@ struct ReactNotificationServiceHelper {
 
 struct ReactNotificationSubscription : implements<ReactNotificationSubscription, IReactNotificationSubscription> {
   ReactNotificationSubscription(
+      IReactNotificationSubscription const &parentSubscription,
+      weak_ref<ReactNotificationService> &&notificationService,
+      IReactPropertyName const &notificationName,
+      IReactDispatcher const &dispatcher) noexcept;
+  ReactNotificationSubscription(
       weak_ref<ReactNotificationService> &&notificationService,
       IReactPropertyName const &notificationName,
       IReactDispatcher const &dispatcher,
       ReactNotificationHandler const &handler) noexcept;
   ~ReactNotificationSubscription() noexcept;
 
+  IReactNotificationService NotificationService() const noexcept;
   IReactPropertyName NotificationName() const noexcept;
   IReactDispatcher Dispatcher() const noexcept;
   bool IsSubscribed() const noexcept;
@@ -85,10 +109,11 @@ struct ReactNotificationSubscription : implements<ReactNotificationSubscription,
   void CallHandler(IInspectable const &sender, IReactNotificationArgs const &args) noexcept;
 
  private:
-  weak_ref<ReactNotificationService> m_notificationService;
-  IReactPropertyName m_notificationName;
-  IReactDispatcher m_dispatcher;
-  ReactNotificationHandler m_handler;
+  const IReactNotificationSubscription m_parentSubscription{nullptr};
+  const weak_ref<ReactNotificationService> m_notificationService;
+  const IReactPropertyName m_notificationName;
+  const IReactDispatcher m_dispatcher;
+  const ReactNotificationHandler m_handler;
   std::atomic_bool m_isSubscribed{true};
 };
 
