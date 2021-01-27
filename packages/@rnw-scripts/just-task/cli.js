c5be4f9d9a323abc3e5cbd20129bbb2a509f0faf
Danny van Velzen 🁴 <dannyvv@microsoft.com>
2021-01-26 22:01:42 -0800

    Update comment for default config

--- packages/@rnw-scripts/just-task/cli.js
+++ packages/@rnw-scripts/just-task/cli.js
@@ -8,9 +8,11 @@
 
 const path = require('path');
 
-// Adding just.config.js here is a temporary workaround to not requiring a just.config file in each project.
-// This will not allow users to specify a custom override, but I'm sending a PR to the just libary to 
-// add an option for a defaultConfig option that is a fallback in case it is not specified in individual projects.
+// Adding default config fallback to just just-task.js. If there is no build
+// customization, there is no need to define a just configuraiotn file like
+// just.config.ts or just-task.js etc. If users do define that file, that will
+// superseded this default config file and users are encouraged to manually
+// import this file and override only the tasks that need customization.
 process.argv.push("--default-config", path.resolve(__dirname, "just-task.js"));
 
 // Runs the main code of just-scripts
