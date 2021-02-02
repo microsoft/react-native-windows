0ef2dc0e9590b067f9734359f40a13f026e7707e
samuelfreiberg <safreibe@microsoft.com>
2021-02-02 13:12:05 -0800

    Polishing test file

--- packages/react-native-win32/src/RNTester/APIExamples/FocusManagerAPI.tsx
+++ packages/react-native-win32/src/RNTester/APIExamples/FocusManagerAPI.tsx
@@ -24,30 +24,37 @@ const styles = StyleSheet.create({
   });
 
 export const PoliteFocusTest: React.FunctionComponent<{}> = () => {
+    const [isPoliteFocus, changePoliteFocus] = React.useState('Not focused');
+    const [isAggressiveFocus, changeAggressiveFocus] = React.useState('Not focused');
+
     const buttonRef = React.useRef();
     const buttonRef2 = React.useRef<View>();
     
     const politeFocusOnPress = () => {
       FocusManager.focus(buttonRef.current, false);
+      changePoliteFocus('Polite Focus Set');
     }
   
     const aggresiveFocusOnPress = () => {
       FocusManager.focus(buttonRef.current, true);
+      changeAggressiveFocus('Aggressive Focus Set');
     }
   
     return (
-        <View>
-          <Text>Polite Focus Test</Text>
-          <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around', marginVertical: 5 }}>
+        <View style={{ flexDirection: 'row', alignItems: 'center', marginVertical: 5 }}>
+          <View style={{ flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around', marginVertical: 5 }}>
+            <Text>Polite Focus Test</Text>
             <TouchableHighlight onPress={politeFocusOnPress}>
               <View ref={buttonRef} style={styles.blackbox} />
             </TouchableHighlight>
+            <Text>{isPoliteFocus}</Text>
           </View>
-          <Text>Aggressive Focus Test</Text>
-          <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around', marginVertical: 5 }}>
+          <View style={{ flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around', marginVertical: 5, marginLeft: 10 }}>
+            <Text>Aggressive Focus Test</Text>
             <TouchableHighlight onPress={aggresiveFocusOnPress}>
-              <View ref={buttonRef2} style={styles.blackbox} />
+              <View ref={buttonRef2} style={styles.blackbox} />  
             </TouchableHighlight>
+            <Text>{isAggressiveFocus}</Text>
           </View>        
         </View>
     );
