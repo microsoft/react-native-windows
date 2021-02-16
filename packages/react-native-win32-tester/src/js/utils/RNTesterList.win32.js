256ebd32fc2f2177e6415c27f9dc2ca3f9ddcf15
Pat Boyd <ppatboyd@outlook.com>
2021-02-16 11:26:26 -0800

    lint fixes

--- packages/react-native-win32-tester/src/js/utils/RNTesterList.win32.js
+++ packages/react-native-win32-tester/src/js/utils/RNTesterList.win32.js
@@ -83,7 +83,7 @@ const ComponentExamples: Array<RNTesterExample> = [
   {
     key: 'ViewWin32Test',
     module: require('@office-iss/react-native-win32/Libraries/Components/View/Tests/ViewWin32Test'),
-  }, 
+  },
   {
     key: 'TextWin32Example',
     module: require('@office-iss/react-native-win32/Libraries/Components/Text/Tests/TextWin32Test'),
