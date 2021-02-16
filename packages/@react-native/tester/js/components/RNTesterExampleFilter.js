b1ebd430bf97625e4ead0b1de266e7cc7979b400
Pat Boyd <ppatboyd@outlook.com>
2021-02-16 11:36:26 -0800

    undo lint fix in copy-override

--- packages/@react-native/tester/js/components/RNTesterExampleFilter.js
+++ packages/@react-native/tester/js/components/RNTesterExampleFilter.js
@@ -20,6 +20,8 @@ const {
   Image,
 } = require('react-native');
 import {RNTesterThemeContext} from './RNTesterTheme';
+import type {RNTesterExample} from '../types/RNTesterTypes';
+
 import type {SectionData} from '../types/RNTesterTypes';
 
 type Props = {
