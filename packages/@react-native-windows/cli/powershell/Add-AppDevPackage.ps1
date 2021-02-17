ba25733c4bc09d74796d80a011ac9ddf5fc67adf
dannyvv <dannyvv@microsoft.com>
2021-02-17 15:11:43 -0800

    Avoid MaxPath issue with telemetry in UnitTest project (#7155)
    * Avoid MaxPath issue with telemetry in UnitTest project
    
    * Change files

--- packages/@react-native-windows/cli/powershell/Add-AppDevPackage.ps1
+++ packages/@react-native-windows/cli/powershell/Add-AppDevPackage.ps1
@@ -107,10 +107,10 @@ function PrintMessageAndExit($ErrorMessage, $ReturnCode)
     {
         # Log telemetry data regarding the use of the script if possible.
         # There are three ways that this can be disabled:
-        #   1. If the "TelemetryDependencies" folder isn't present.  This can be excluded at build time by setting the MSBuild property AppxLogTelemetryFromSideloadingScript to false
+        #   1. If the "TelDeps" folder isn't present.  This can be excluded at build time by setting the MSBuild property AppxLogTelemetryFromSideloadingScript to false
         #   2. If the SkipLoggingTelemetry switch is passed to this script.
         #   3. If Visual Studio telemetry is disabled via the registry.
-        $TelemetryAssembliesFolder = (Join-Path $PSScriptRoot "TelemetryDependencies")
+        $TelemetryAssembliesFolder = (Join-Path $PSScriptRoot "TelDeps")
         if (!$SkipLoggingTelemetry -And (Test-Path $TelemetryAssembliesFolder))
         {
             $job = Start-Job -FilePath (Join-Path $TelemetryAssembliesFolder "LogSideloadingTelemetry.ps1") -ArgumentList $TelemetryAssembliesFolder, "VS/DesignTools/SideLoadingScript/AddAppDevPackage", $ReturnCode, $ProcessorArchitecture
