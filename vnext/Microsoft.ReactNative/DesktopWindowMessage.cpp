231296fc78175b5632cd8dabd945bc8b9c2f8875
Alexander Sklar <asklar@microsoft.com>
2021-02-18 01:33:57 -0800

    [Perf] Make DesktopWindowMessage a struct (#7156)
    * [Perf] Make DesktopWindowMessage a struct
    
    * Change files

--- vnext/Microsoft.ReactNative/DesktopWindowMessage.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-#include "pch.h"
-#include "DesktopWindowMessage.h"
-#include "DesktopWindowMessage.g.cpp"
-
-namespace winrt::Microsoft::ReactNative::implementation {}
