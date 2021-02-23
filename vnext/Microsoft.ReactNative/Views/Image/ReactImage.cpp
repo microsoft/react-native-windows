8061f7790bec2df9edf2cd33b98c896ff3bb5ead
Eric Rozell <erozell@outlook.com>
2021-02-22 19:15:57 -0500

    Removes irrelevant TODO for #7194

--- vnext/Microsoft.ReactNative/Views/Image/ReactImage.cpp
+++ vnext/Microsoft.ReactNative/Views/Image/ReactImage.cpp
@@ -227,7 +227,6 @@ winrt::fire_and_forget ReactImage::SetBackground(bool fireLoadEndEvent) {
 
   if (auto strong_this{weak_this.get()}) {
     // If the image source has been updated since this operation started, do not continue
-    // TODO: #7194 This "cancellation" will prevent onLoad / onLoadEnd events from firing for canceled source props
     if (currentImageSourceId != strong_this->m_imageSourceId) {
       co_return;
     }
