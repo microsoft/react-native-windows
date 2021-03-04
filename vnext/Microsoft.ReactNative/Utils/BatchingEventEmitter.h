1978ec3197e7f62c6f8d9e720c5baa85d8fa2753
Nick Gerleman <ngerlem@microsoft.com>
2021-03-04 12:43:13 -0800

    Fix comment typos

--- vnext/Microsoft.ReactNative/Utils/BatchingEventEmitter.h
+++ vnext/Microsoft.ReactNative/Utils/BatchingEventEmitter.h
@@ -22,21 +22,21 @@ struct BatchedEvent {
 
 namespace winrt::Microsoft::ReactNative {
 
-//! Emits queued events from native to JS in queued batches (at most once per-native frame). Events within a batch may
-//! be coalesced. The batch is finished at the time the JS thread starts to process it. I.e. it is possible for a batch
-//! to last for multiple frames if the JS thread is blocked. This is by-design as it allows our coalescing strategy to
+//! Emits events from native to JS in queued batches (at most once per-native frame). Events within a batch may be
+//! coalesced. The batch is finished at the time the JS thread starts to process it. I.e. it is possible for a batch to
+//! last for multiple frames if the JS thread is blocked. This is by-design as it allows our coalescing strategy to
 //! account for long operations on the JS thread.
 struct BatchingEventEmitter : public std::enable_shared_from_this<BatchingEventEmitter> {
  public:
   BatchingEventEmitter(Mso::CntPtr<const Mso::React::IReactContext> &&context) noexcept;
 
-  //! Queues an event to be fired via RTCEventEmitter.receiveEvent().
+  //! Queues an event to be fired via RCTEventEmitter, calling receiveEvent() by default.
   void EmitJSEvent(int64_t tag, winrt::hstring &&eventName, JSValue &&eventObject) noexcept;
   void
   EmitJSEvent(winrt::hstring &&emitterMethod, int64_t tag, winrt::hstring &&eventName, JSValue &&eventObject) noexcept;
 
-  //! Queues an event to be fired via RTCEventEmitter.receiveEvent(). Existing events in the batch with the same name
-  //! and tag will be removed.
+  //! Queues an event to be fired via RCTEventEmitter, calling receiveEvent() by default. Existing events in the batch
+  //! with the same name and tag will be removed.
   void EmitCoalescingJSEvent(int64_t tag, winrt::hstring &&eventName, JSValue &&eventObject) noexcept;
   void EmitCoalescingJSEvent(
       winrt::hstring &&emitterMethod,
