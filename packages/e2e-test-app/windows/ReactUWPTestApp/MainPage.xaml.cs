416f1097d8a7c78042b94d840c28c788861f45e0
Igor Klemenski <rectified95@users.noreply.github.com>
2021-03-04 11:01:59 -0800

    Bump version of XamlTreeDump library (#7265)
    * Bump version of XamlTreeDump library
    
    * Update packages/e2e-test-app/windows/ReactUWPTestApp/ReactUWPTestApp.csproj
    
    updating to latest-latest
    
    * Update MainPage.xaml.cs
    
    * Update MainPage.xaml.cs
    
    Co-authored-by: Igor Klemenski <igklemen@microsoft.com>
    Co-authored-by: Alexander Sklar <asklar@microsoft.com>

--- packages/e2e-test-app/windows/ReactUWPTestApp/MainPage.xaml.cs
+++ packages/e2e-test-app/windows/ReactUWPTestApp/MainPage.xaml.cs
@@ -46,7 +46,7 @@ namespace ReactUWPTestApp
                 }
             }
 
-            var rootDump = VisualTreeDumper.DumpTree(this, null, additionalProperties, DumpTreeMode.Json);
+            var rootDump = VisualTreeDumper.DumpTree(this, null, additionalProperties, new AttachedProperty[] {});
             var element = VisualTreeDumper.FindElementByAutomationId(JsonObject.Parse(rootDump), accessibilityId);
 
             return element;
