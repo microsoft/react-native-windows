b5b556462f38f71204982a0c278b0a0d5d88b177
Julio C. Rocha <julio.rocha@microsoft.com>
2021-03-23 20:38:13 -0700

    Move hook into pch.cpp

--- vnext/Desktop.DLL/pch.cpp
+++ vnext/Desktop.DLL/pch.cpp
@@ -2,3 +2,31 @@
 // Licensed under the MIT License.
 
 #include "pch.h"
+
+#include <delayimp.h>
+
+FARPROC WINAPI ChakraDelayLoadHook(unsigned dliNotify, DelayLoadInfo *pdli) {
+  switch (dliNotify) {
+    case dliStartProcessing:
+      break;
+
+    case dliNotePreLoadLibrary:
+      break;
+
+    case dliNotePreGetProcAddress:
+      break;
+
+    case dliFailLoadLib:
+      break;
+
+    case dliFailGetProc:
+      break;
+
+    case dliNoteEndProcessing:
+      break;
+  }
+
+  return NULL;
+}
+
+decltype(__pfnDliNotifyHook2) __pfnDliNotifyHook2 = ChakraDelayLoadHook;
