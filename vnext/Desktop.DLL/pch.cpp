970d60baeae818ff35df8f6b304c94879db2d51a
Julio C. Rocha <julio.rocha@microsoft.com>
2021-03-23 23:36:03 -0700

    Define ForceSystemChakra runtime option

--- vnext/Desktop.DLL/pch.cpp
+++ vnext/Desktop.DLL/pch.cpp
@@ -3,13 +3,16 @@
 
 #include "pch.h"
 
+#include <RuntimeOptions.h>
+
+// Windows API
 #include <delayimp.h>
 
 static bool flag = false;
 FARPROC WINAPI ChakraDelayLoadHook(unsigned dliNotify, DelayLoadInfo *pdli) {
   switch (dliNotify) {
     case dliNotePreLoadLibrary:
-      if (0 == lstrcmpA(pdli->szDll, "ChakraCore.dll")) {
+      if (Microsoft::React::GetRuntimeOptionBool("ForceSystemChakra") && 0 == lstrcmpA(pdli->szDll, "ChakraCore.dll")) {
         return reinterpret_cast<FARPROC>(LoadLibrary(L"Chakra.dll"));
       }
       break;
