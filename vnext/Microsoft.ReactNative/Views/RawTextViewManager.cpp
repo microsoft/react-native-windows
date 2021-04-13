microsoft/react-native-windows

commit 278a3c1355de878c380d2025953f998f0ff6c8b2

Fixes textTransform recursion (#7538)
* Fixes textTransform recursion

This change includes a number of fixes to the approach to textTransform.
There were 4 issues with the previous textTransform approach that this
diff solves:
1. The textTransform prop was only applied to the parent TextBlock when
it had a single raw text child
2. The textTransform prop on a virtual text Span only recursed one level
3. The one-level textTransform approach had a bug where the last raw
text value overwrote all Run text values (#7534)
4. The textTransform prop did not update correctly when changed

Rather than holding state in the TransformableText struct, this change
keeps the originalText state exclusively in RawTextShadowNode, and only
when a parent VirtualTextShadowNode or TextShadowNode has a
textTransform setting (that is not "none" or undefined). The
VirtualTextShadowNode and TextShadowNode holds only the TextTransform
prop value (and not any originalText value which was causing the issue
in #7534). When a child is added to either a TextShadowNode or
VirtualTextShadowNode, we now attempt to recursively apply the
textTransform on the child sub-tree (wherever textTransform is not
explicitly overwritten). Whenever the textTransform prop changes on
VirtualTextShadowNode or TextShadowNode, we now attempt to recurse over
all children of that node to apply the textTransform (wherever it is not
explicitly overwritten).

Fixes #7534
Fixes #7535

* Removes RawTextShadowNode originalText when no longer needed.

* Re-apply textTransform to updated raw text nodes

The previous algorithm did not account for the mutability of raw text
nodes. This change applies the inherited textTransform value directly to
the raw text node.

* Extracts TextTransform into separate header

TransformableText doesn't compile when included in TextViewManager.h

* Extends RNTester example with a few corner cases for textTransform

vnext/Microsoft.ReactNative/Views/RawTextViewManager.cpp
@@ -5,6 +5,7 @@
 
 #include "RawTextViewManager.h"
 #include "TextViewManager.h"
+#include "VirtualTextViewManager.h"
 
 #include <Views/ShadowNodeBase.h>
 
@@ -49,22 +50,8 @@ bool RawTextViewManager::UpdateProperty(
 
   if (propertyName == "text") {
     run.Text(react::uwp::asHstring(propertyValue));
-
-    if (nodeToUpdate->GetParent() != -1) {
-      if (auto uiManager = GetNativeUIManager(*m_context).lock()) {
-        const ShadowNodeBase *parent =
-            static_cast<ShadowNodeBase *>(uiManager->getHost()->FindShadowNodeForTag(nodeToUpdate->GetParent()));
-        if (parent && parent->m_children.size() == 1) {
-          auto view = parent->GetView();
-          auto textBlock = view.try_as<winrt::TextBlock>();
-          if (textBlock != nullptr) {
-            textBlock.Text(run.Text());
-          }
-        }
-
-        NotifyAncestorsTextChanged(nodeToUpdate);
-      }
-    }
+    static_cast<RawTextShadowNode *>(nodeToUpdate)->originalText = winrt::hstring{};
+    NotifyAncestorsTextChanged(nodeToUpdate);
   } else {
     return Super::UpdateProperty(nodeToUpdate, propertyName, propertyValue);
   }
@@ -75,10 +62,33 @@ void RawTextViewManager::NotifyAncestorsTextChanged(ShadowNodeBase *nodeToUpdate
   if (auto uiManager = GetNativeUIManager(GetReactContext()).lock()) {
     auto host = uiManager->getHost();
     ShadowNodeBase *parent = static_cast<ShadowNodeBase *>(host->FindShadowNodeForTag(nodeToUpdate->GetParent()));
+    TextTransform textTransform = TextTransform::Undefined;
     while (parent) {
       auto viewManager = parent->GetViewManager();
-      if (!std::wcscmp(viewManager->GetName(), L"RCTText")) {
+      const auto nodeType = viewManager->GetName();
+      if (!std::wcscmp(nodeType, L"RCTText")) {
+        const auto textViewManager = static_cast<TextViewManager *>(viewManager);
+        if (textTransform == TextTransform::Undefined) {
+          textTransform = textViewManager->GetTextTransformValue(parent);
+        }
+
+        VirtualTextShadowNode::ApplyTextTransform(
+            *nodeToUpdate, textTransform, /* forceUpdate = */ false, /* isRoot = */ false);
+
+        if (parent->m_children.size() == 1) {
+          auto view = parent->GetView();
+          auto textBlock = view.try_as<winrt::TextBlock>();
+          if (textBlock != nullptr) {
+            const auto run = nodeToUpdate->GetView().try_as<winrt::Run>();
+            if (run != nullptr) {
+              textBlock.Text(run.Text());
+            }
+          }
+        }
+
         (static_cast<TextViewManager *>(viewManager))->OnDescendantTextPropertyChanged(parent);
+      } else if (!std::wcscmp(nodeType, L"RCTVirtualText") && textTransform == TextTransform::Undefined) {
+        textTransform = static_cast<VirtualTextShadowNode *>(parent)->textTransform;
       }
       parent = static_cast<ShadowNodeBase *>(host->FindShadowNodeForTag(parent->GetParent()));
     }

packages/@react-native-windows/tester/src/js/examples-win/Text/TextExample.windows.tsx
@@ -8,7 +8,13 @@
 // Image inline in Text removed
 
 import * as React from 'react';
-import {/*Image,*/ StyleSheet, Text, View, TextStyle} from 'react-native';
+import {
+  /*Image,*/ StyleSheet,
+  Text,
+  View,
+  TextStyle,
+  TouchableWithoutFeedback,
+} from 'react-native';
 const RNTesterBlock = require('../../components/RNTesterBlock');
 const RNTesterPage = require('../../components/RNTesterPage');
 
@@ -124,7 +130,15 @@ export class BackgroundColorDemo extends React.Component<{}> {
   }
 }
 
-export class TextExample extends React.Component<{}> {
+export class TextExample extends React.Component<
+  {},
+  {toggle1: boolean; toggle2: boolean; toggle3: boolean}
+> {
+  constructor(props: any) {
+    super(props);
+    this.state = {toggle1: false, toggle2: false, toggle3: false};
+  }
+
   public render() {
     const lorumIpsum =
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dapibus felis eget augue condimentum suscipit. Suspendisse hendrerit, libero aliquet malesuada tempor, urna nibh consectetur tellus, vitae efficitur quam erat non mi. Maecenas vitae eros sit amet quam vestibulum porta sed sit amet tellus. Fusce quis lectus congue, fringilla arcu id, luctus urna. Cras sagittis ornare mauris sit amet dictum. Vestibulum feugiat laoreet fringilla. Vivamus ac diam vehicula felis venenatis sagittis vitae ultrices elit. Curabitur libero augue, laoreet quis orci vitae, congue euismod massa. Aenean nec odio sed urna vehicula fermentum non a magna. Quisque ut commodo neque, eget eleifend odio. Sed sit amet lacinia sem. Suspendisse in metus in purus scelerisque vestibulum. Nam metus dui, efficitur nec metus non, tincidunt pharetra sapien. Praesent id convallis metus, ut malesuada arcu. Quisque quam libero, pharetra eu tellus ac, aliquam fringilla erat. Quisque tempus in lorem ac suscipit.';
@@ -134,7 +148,7 @@ export class TextExample extends React.Component<{}> {
         <RNTesterBlock title="textTransform">
           <View>
             <Text style={{textTransform: 'uppercase'}}>
-              This text should be uppercased.
+              <Text>This</Text> text should be uppercased.
             </Text>
             <Text style={{textTransform: 'lowercase'}}>
               This TEXT SHOULD be lowercased.
@@ -153,7 +167,10 @@ export class TextExample extends React.Component<{}> {
             <Text>
               Should be "ABC":
               <Text style={{textTransform: 'uppercase'}}>
-                a<Text>b</Text>c
+                a
+                <Text>
+                  b<Text>c</Text>
+                </Text>
               </Text>
             </Text>
             <Text>
@@ -162,6 +179,41 @@ export class TextExample extends React.Component<{}> {
                 x<Text style={{textTransform: 'none'}}>y</Text>z
               </Text>
             </Text>
+            <Text>
+              <Text>
+                Should be "xYz":
+                <Text>
+                  x<Text style={{textTransform: 'uppercase'}}>y</Text>z
+                </Text>
+              </Text>
+            </Text>
+            <Text onPress={() => this.setState({toggle1: !this.state.toggle1})}>
+              Click to toggle uppercase:{' '}
+              <Text
+                style={{
+                  textTransform: this.state.toggle1 ? 'uppercase' : 'none',
+                }}>
+                Hello
+              </Text>
+            </Text>
+            <Text onPress={() => this.setState({toggle2: !this.state.toggle2})}>
+              Click to change raw text:{' '}
+              <Text style={{textTransform: 'uppercase'}}>
+                Hello {this.state.toggle2 ? 'Earth' : 'World'}
+              </Text>
+            </Text>
+            <TouchableWithoutFeedback
+              onPress={() => this.setState({toggle3: !this.state.toggle3})}>
+              <View>
+                <Text>
+                  Click to toggle fast text on next line (should remain
+                  uppercase):
+                </Text>
+                <Text style={{textTransform: 'uppercase'}}>
+                  {this.state.toggle3 ? 'Hello' : 'Howdy'}
+                </Text>
+              </View>
+            </TouchableWithoutFeedback>
           </View>
         </RNTesterBlock>
         <RNTesterBlock title="Wrap">

vnext/Microsoft.ReactNative/Microsoft.ReactNative.vcxproj
@@ -305,6 +305,7 @@
     <ClInclude Include="Utils\PropertyUtils.h" />
     <ClInclude Include="Utils\ResourceBrushUtils.h" />
     <ClInclude Include="Utils\StandardControlResourceKeyNames.h" />
+    <ClInclude Include="Utils\TextTransform.h" />
     <ClInclude Include="Utils\TransformableText.h" />
     <ClInclude Include="Utils\UwpPreparedScriptStore.h" />
     <ClInclude Include="Utils\UwpScriptStore.h" />

vnext/Microsoft.ReactNative/Microsoft.ReactNative.vcxproj.filters
@@ -677,6 +677,9 @@
     <ClInclude Include="ReactHost\JSCallInvokerScheduler.h">
       <Filter>ReactHost</Filter>
     </ClInclude>
+    <ClInclude Include="Utils\TextTransform.h">
+      <Filter>Utils</Filter>
+    </ClInclude>
   </ItemGroup>
   <ItemGroup>
     <Midl Include="IJSValueReader.idl" />

vnext/Microsoft.ReactNative/Utils/TextTransform.h
@@ -1,0 +1,8 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#pragma once
+
+namespace Microsoft::ReactNative {
+enum class TextTransform : uint8_t { Undefined, None, Uppercase, Lowercase, Capitalize };
+}

vnext/Microsoft.ReactNative/Utils/TransformableText.h
@@ -3,17 +3,15 @@
 
 #pragma once
 
+#include <Utils/TextTransform.h>
+
 namespace Microsoft::ReactNative {
 struct TransformableText final {
-  enum class TextTransform : uint8_t { Undefined, None, Uppercase, Lowercase, Capitalize };
-
-  TextTransform textTransform{TextTransform::Undefined};
-  std::wstring originalText{};
-
-  std::wstring TransformText() const noexcept {
+  static winrt::hstring TransformText(const winrt::hstring &originalText, TextTransform textTransform) noexcept {
     if (textTransform == TextTransform::Undefined || textTransform == TextTransform::None) {
       return originalText;
     }
+
     DWORD dwMapFlags{};
     switch (textTransform) {
       case TextTransform::Uppercase:
@@ -31,12 +29,7 @@ struct TransformableText final {
     }
 
     const int reqChars = LCMapStringW(
-        LOCALE_NAME_USER_DEFAULT,
-        dwMapFlags,
-        originalText.c_str(),
-        static_cast<int>(originalText.length()),
-        nullptr,
-        0);
+        LOCALE_NAME_USER_DEFAULT, dwMapFlags, originalText.c_str(), static_cast<int>(originalText.size()), nullptr, 0);
 
     std::wstring str;
     str.resize(reqChars);
@@ -44,12 +37,13 @@ struct TransformableText final {
         LOCALE_NAME_USER_DEFAULT,
         dwMapFlags,
         originalText.c_str(),
-        static_cast<int>(originalText.length()),
+        static_cast<int>(originalText.size()),
         str.data(),
         reqChars);
     str.resize(nChars);
     assert(nChars == reqChars);
-    return str;
+    winrt::hstring result{str.c_str()};
+    return result;
   }
 
   static TextTransform GetTextTransform(const winrt::Microsoft::ReactNative::JSValue &propertyValue) noexcept {

vnext/Microsoft.ReactNative/Views/RawTextViewManager.h
@@ -3,11 +3,16 @@
 
 #pragma once
 
+#include <Views/ShadowNodeBase.h>
 #include <Views/ViewManagerBase.h>
-
 #include <folly/dynamic.h>
 
 namespace Microsoft::ReactNative {
+
+struct RawTextShadowNode final : public ShadowNodeBase {
+  using Super = ShadowNodeBase;
+  winrt::hstring originalText{};
+};
 
 class RawTextViewManager : public ViewManagerBase {
   using Super = ViewManagerBase;
@@ -16,6 +21,9 @@ class RawTextViewManager : public ViewManagerBase {
   RawTextViewManager(const Mso::React::IReactContext &context);
 
   const wchar_t *GetName() const override;
+  ShadowNode *createShadow() const override {
+    return new RawTextShadowNode();
+  }
 
   void SetLayoutProps(
       ShadowNodeBase &nodeToUpdate,

vnext/Microsoft.ReactNative/Views/TextViewManager.cpp
@@ -5,6 +5,7 @@
 
 #include "TextViewManager.h"
 
+#include <Views/RawTextViewManager.h>
 #include <Views/ShadowNodeBase.h>
 #include <Views/VirtualTextViewManager.h>
 
@@ -43,18 +44,18 @@ class TextShadowNode final : public ShadowNodeBase {
   }
 
   void AddView(ShadowNode &child, int64_t index) override {
+    auto &childNode = static_cast<ShadowNodeBase &>(child);
+    VirtualTextShadowNode::ApplyTextTransform(
+        childNode, textTransform, /* forceUpdate = */ false, /* isRoot = */ false);
+
     if (index == 0) {
-      auto run = static_cast<ShadowNodeBase &>(child).GetView().try_as<winrt::Run>();
+      auto run = childNode.GetView().try_as<winrt::Run>();
       if (run != nullptr) {
         m_firstChildNode = &child;
         auto textBlock = this->GetView().as<xaml::Controls::TextBlock>();
-        std::wstring text(run.Text().c_str());
-        transformableText.originalText = text;
-        text = transformableText.TransformText();
-        textBlock.Text(winrt::hstring(text));
-
+        textBlock.Text(run.Text());
         if (m_ColorValue) {
-          AddHighlighter(m_ColorValue.value(), text.size());
+          AddHighlighter(m_ColorValue.value(), textBlock.Text().size());
         }
 
         m_prevCursorEnd += textBlock.Text().size();
@@ -124,7 +125,7 @@ class TextShadowNode final : public ShadowNodeBase {
     Super::RemoveChildAt(indexToRemove);
   }
 
-  TransformableText transformableText{};
+  TextTransform textTransform{TextTransform::Undefined};
 };
 
 TextViewManager::TextViewManager(const Mso::React::IReactContext &context) : Super(context) {}
@@ -155,8 +156,9 @@ bool TextViewManager::UpdateProperty(
   } else if (TryUpdateFontProperties(textBlock, propertyName, propertyValue)) {
   } else if (propertyName == "textTransform") {
     auto textNode = static_cast<TextShadowNode *>(nodeToUpdate);
-    auto textTransform = TransformableText::GetTextTransform(propertyValue);
-    textNode->transformableText.textTransform = textTransform;
+    textNode->textTransform = TransformableText::GetTextTransform(propertyValue);
+    VirtualTextShadowNode::ApplyTextTransform(
+        *textNode, textNode->textTransform, /* forceUpdate = */ true, /* isRoot = */ true);
   } else if (TryUpdatePadding(nodeToUpdate, textBlock, propertyName, propertyValue)) {
   } else if (TryUpdateTextAlignment(textBlock, propertyName, propertyValue)) {
   } else if (TryUpdateTextTrimming(textBlock, propertyName, propertyValue)) {
@@ -258,6 +260,14 @@ void TextViewManager::OnDescendantTextPropertyChanged(ShadowNodeBase *node) {
       }
     }
   }
+}
+
+TextTransform TextViewManager::GetTextTransformValue(ShadowNodeBase *node) {
+  if (!std::wcscmp(node->GetViewManager()->GetName(), GetName())) {
+    return static_cast<TextShadowNode *>(node)->textTransform;
+  }
+
+  return TextTransform::Undefined;
 }
 
 } // namespace Microsoft::ReactNative

commit 002160e7dc15dd5ec68cbdb283ec79211c2385ee

Fixes bug where raw text update overwrites all text
After fixing issues with textTransform, 278a3c13 introduced a bug where
updates to a raw text node can overwrite the TextBlock parent if it has
only a single Span child inline. This change ensures we check that the
updated raw text Run is indeed the direct child of a TextBlock.

vnext/Microsoft.ReactNative/Views/RawTextViewManager.cpp
@@ -63,6 +63,7 @@ void RawTextViewManager::NotifyAncestorsTextChanged(ShadowNodeBase *nodeToUpdate
     auto host = uiManager->getHost();
     ShadowNodeBase *parent = static_cast<ShadowNodeBase *>(host->FindShadowNodeForTag(nodeToUpdate->GetParent()));
     TextTransform textTransform = TextTransform::Undefined;
+    auto isNested = false;
     while (parent) {
       auto viewManager = parent->GetViewManager();
       const auto nodeType = viewManager->GetName();
@@ -75,7 +76,7 @@ void RawTextViewManager::NotifyAncestorsTextChanged(ShadowNodeBase *nodeToUpdate
         VirtualTextShadowNode::ApplyTextTransform(
             *nodeToUpdate, textTransform, /* forceUpdate = */ false, /* isRoot = */ false);
 
-        if (parent->m_children.size() == 1) {
+        if (!isNested && parent->m_children.size() == 1) {
           auto view = parent->GetView();
           auto textBlock = view.try_as<winrt::TextBlock>();
           if (textBlock != nullptr) {
@@ -90,7 +91,9 @@ void RawTextViewManager::NotifyAncestorsTextChanged(ShadowNodeBase *nodeToUpdate
       } else if (!std::wcscmp(nodeType, L"RCTVirtualText") && textTransform == TextTransform::Undefined) {
         textTransform = static_cast<VirtualTextShadowNode *>(parent)->textTransform;
       }
+
       parent = static_cast<ShadowNodeBase *>(host->FindShadowNodeForTag(parent->GetParent()));
+      isNested = true;
     }
   }
 }