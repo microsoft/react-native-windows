dac28d771a058630d98f33f0e0f9ae1be225c3ce
Alexander Sklar <asklar@microsoft.com>
2021-04-16 09:56:04 -0700

    Avoid running Yoga layout on subtrees with RequireNativeLayout (#7525)
    * Layout wrongly overwrites width/height coming from view manager
    
    * Change files
    
    * Avoid running yoga layout for nodes whose parent's viewmanager has RequireNativeLayout
    
    * format

--- vnext/Microsoft.ReactNative/Views/ViewManagerBase.cpp
+++ vnext/Microsoft.ReactNative/Views/ViewManagerBase.cpp
@@ -317,6 +317,17 @@ void ViewManagerBase::SetLayoutProps(
     float top,
     float width,
     float height) {
+  if (auto uiManager = GetNativeUIManager(GetReactContext()).lock()) {
+    auto parent = nodeToUpdate.GetParent();
+    if (parent != -1) {
+      const auto &parentShadowNode = uiManager->getHost()->GetShadowNodeForTag(parent);
+      const auto &parentVM = parentShadowNode.m_viewManager;
+      if (parentVM->RequiresNativeLayout()) {
+        return;
+      }
+    }
+  }
+
   auto element = viewToUpdate.as<xaml::UIElement>();
   if (element == nullptr) {
     // TODO: Assert
