a917737ee06bfd2856a26e5e5dcf09c6607688e2
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2021-04-23 09:59:08 -0700

    Build fixes

--- vnext/stubs/fmt/core.h
+++
@@ -1,9 +0,0 @@
-// stub, not used by RNW
-// Remove with https://github.com/microsoft/react-native-windows/issues/7634
-
-namespace fmt {
-    std::basic_string<char> format(std::string s, size_t arg1, size_t arg2) {
-        assert(false);
-        std::terminate();
-    }
-}
