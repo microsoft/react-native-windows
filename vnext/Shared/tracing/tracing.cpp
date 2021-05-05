dca8fb60d93db67089fc6a618d7ca5cbe26cbd7c
Alexander Sklar <asklar@microsoft.com>
2021-05-05 01:46:55 -0700

    Port ETW eventing to 0.64

--- vnext/Shared/tracing/tracing.cpp
+++ vnext/Shared/tracing/tracing.cpp
@@ -3,15 +3,19 @@
 
 #include "pch.h"
 
-#include <windows.h>
-#include "etw/react_native_windows.h"
-
-#include "tracing/fbsystrace.h"
-
+#include <TraceLoggingProvider.h>
 #include <jsi/jsi.h>
+#include "tracing/fbsystrace.h"
 
 #include <array>
 #include <string>
+
+// Define the GUID to use in TraceLoggingProviderRegister
+// {910FB9A1-75DD-4CF4-BEEC-DA21341F20C8}
+TRACELOGGING_DEFINE_PROVIDER(
+    g_hTraceLoggingProvider,
+    "Microsoft.ReactNativeWindows",
+    (0x910fb9a1, 0x75dd, 0x4cf4, 0xbe, 0xec, 0xda, 0x21, 0x34, 0x1f, 0x20, 0xc8));
 
 using namespace facebook;
 
@@ -29,7 +33,13 @@ std::mutex g_pages_mutex;
     s_tracker_[cookie] = std::chrono::high_resolution_clock::now();
   }
 
-  EventWriteNATIVE_ASYNC_BEGIN_FLOW(tag, name, cookie, 0);
+  TraceLoggingWrite(
+      g_hTraceLoggingProvider,
+      "SystraceNativeAsyncFlow",
+      TraceLoggingString("begin", "op"),
+      TraceLoggingString(name, "profile_name"),
+      TraceLoggingUInt64(tag, "tag"),
+      TraceLoggingInt64(cookie, "cookie"));
 }
 
 /*static */ void FbSystraceAsyncFlow::end(uint64_t tag, const char *name, int cookie) {
@@ -48,7 +58,13 @@ std::mutex g_pages_mutex;
     }
   }
 
-  EventWriteNATIVE_ASYNC_END_FLOW(tag, name, cookie, duration);
+  TraceLoggingWrite(
+      g_hTraceLoggingProvider,
+      "SystraceNativeAsyncFlow",
+      TraceLoggingString("end", "op"),
+      TraceLoggingString(name, "profile_name"),
+      TraceLoggingUInt64(tag, "tag"),
+      TraceLoggingInt64(cookie, "cookie"));
 }
 
 } // namespace fbsystrace
@@ -67,57 +83,99 @@ void trace_begin_section(
     const std::string &profile_name,
     std::array<std::string, SYSTRACE_SECTION_MAX_ARGS> &&args,
     uint8_t size) {
-  EventWriteNATIVE_BEGIN_SECTION(
-      id,
-      tag,
-      profile_name.c_str(),
-      args[0].c_str(),
-      args[1].c_str(),
-      args[2].c_str(),
-      args[3].c_str(),
-      args[4].c_str(),
-      args[5].c_str(),
-      args[6].c_str(),
-      args[7].c_str());
+  TraceLoggingWrite(
+      g_hTraceLoggingProvider,
+      "SystraceNativeSection",
+      TraceLoggingString("begin", "op"),
+      TraceLoggingString(profile_name.c_str(), "profile_name"),
+      TraceLoggingUInt64(tag, "tag"),
+      TraceLoggingString(args[0].c_str(), "arg0"),
+      TraceLoggingString(args[1].c_str(), "arg1"),
+      TraceLoggingString(args[2].c_str(), "arg2"),
+      TraceLoggingString(args[3].c_str(), "arg3"),
+      TraceLoggingString(args[4].c_str(), "arg4"),
+      TraceLoggingString(args[5].c_str(), "arg5"),
+      TraceLoggingString(args[6].c_str(), "arg6"),
+      TraceLoggingString(args[7].c_str(), "arg7"));
 }
 
 void trace_end_section(uint64_t id, uint64_t tag, const std::string &profile_name, double duration) {
-  EventWriteNATIVE_END_SECTION(id, tag, profile_name.c_str(), duration);
+  TraceLoggingWrite(
+      g_hTraceLoggingProvider,
+      "SystraceNativeSection",
+      TraceLoggingString("end", "op"),
+      TraceLoggingString(profile_name.c_str(), "profile_name"),
+      TraceLoggingUInt64(tag, "tag"),
+      TraceLoggingFloat64(duration, "duration"));
 }
 
 void syncSectionBeginJSHook(uint64_t tag, const std::string &profile_name, const std::string &args) {
-  EventWriteJS_BEGIN_SECTION(
-      0, tag, profile_name.c_str(), args.c_str(), nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr);
+  TraceLoggingWrite(
+      g_hTraceLoggingProvider,
+      "SystraceJSSection",
+      TraceLoggingString("begin", "op"),
+      TraceLoggingUInt64(tag, "tag"),
+      TraceLoggingString(profile_name.c_str(), "profile_name"),
+      TraceLoggingString(args.c_str(), "arg"));
 }
 
 void syncSectionEndJSHook(uint64_t tag) {
-  EventWriteJS_END_SECTION(0, tag, "", 0);
+  TraceLoggingWrite(
+      g_hTraceLoggingProvider, "SystraceJSSection", TraceLoggingString("end", "op"), TraceLoggingUInt64(tag, "tag"));
 }
 
 void asyncSectionBeginJSHook(uint64_t tag, const std::string &profile_name, int cookie) {
-  EventWriteJS_ASYNC_BEGIN_SECTION(tag, profile_name.c_str(), cookie, 0);
+  TraceLoggingWrite(
+      g_hTraceLoggingProvider,
+      "SystraceJSAsyncSection",
+      TraceLoggingString("begin", "op"),
+      TraceLoggingString(profile_name.c_str(), "profile_name"),
+      TraceLoggingUInt64(tag, "tag"),
+      TraceLoggingInt64(cookie, "cookie"));
 }
 
 void asyncSectionEndJSHook(uint64_t tag, const std::string &profile_name, int cookie) {
-  EventWriteJS_ASYNC_END_SECTION(tag, profile_name.c_str(), cookie, 0);
+  TraceLoggingWrite(
+      g_hTraceLoggingProvider,
+      "SystraceJSAsyncSection",
+      TraceLoggingString("end", "op"),
+      TraceLoggingString(profile_name.c_str(), "profile_name"),
+      TraceLoggingUInt64(tag, "tag"),
+      TraceLoggingInt64(cookie, "cookie"));
 }
 
 void asyncFlowBeginJSHook(uint64_t tag, const std::string &profile_name, int cookie) {
-  EventWriteJS_ASYNC_BEGIN_FLOW(tag, profile_name.c_str(), cookie, 0);
+  TraceLoggingWrite(
+      g_hTraceLoggingProvider,
+      "SystraceJSAsyncFlow",
+      TraceLoggingString("begin", "op"),
+      TraceLoggingString(profile_name.c_str(), "profile_name"),
+      TraceLoggingUInt64(tag, "tag"),
+      TraceLoggingInt64(cookie, "cookie"));
 }
 
 void asyncFlowEndJSHook(uint64_t tag, const std::string &profile_name, int cookie) {
-  EventWriteJS_ASYNC_END_FLOW(tag, profile_name.c_str(), cookie, 0);
+  TraceLoggingWrite(
+      g_hTraceLoggingProvider,
+      "SystraceJSAsyncFlow",
+      TraceLoggingString("end", "op"),
+      TraceLoggingString(profile_name.c_str(), "profile_name"),
+      TraceLoggingUInt64(tag, "tag"),
+      TraceLoggingInt64(cookie, "cookie"));
 }
 
 void counterJSHook(uint64_t tag, const std::string &profile_name, int value) {
-  EventWriteJS_COUNTER(tag, profile_name.c_str(), value);
+  TraceLoggingWrite(
+      g_hTraceLoggingProvider,
+      "SystraceCounter",
+      TraceLoggingString(profile_name.c_str(), "profile_name"),
+      TraceLoggingUInt64(tag, "tag"),
+      TraceLoggingInt64(value, "value"));
 }
 
 void initializeJSHooks(jsi::Runtime &runtime) {
-  // Don't hook up unless the provider is enabled.
-  if (!EventEnabledJS_ASYNC_BEGIN_FLOW())
-    return;
+  // TODO:: Assess the performance impact of hooking up the JS trace events. Especially when the tracing is not enabled.
+  // If significant, this should be put under flag based on devsettings
 
   runtime.global().setProperty(runtime, "__RCTProfileIsProfiling", true);
 
@@ -317,7 +375,12 @@ void initializeJSHooks(jsi::Runtime &runtime) {
 }
 
 void initializeETW() {
-  EventRegisterReact_Native_Windows_Provider();
+  // Register the provider
+  static bool etwInitialized = false;
+  if (!etwInitialized) {
+    TraceLoggingRegister(g_hTraceLoggingProvider);
+    etwInitialized = true;
+  }
 }
 
 } // namespace tracing
