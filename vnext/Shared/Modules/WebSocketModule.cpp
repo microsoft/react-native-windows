41197b331209c717057b14c827a808b8c11a06e1
Julio César Rocha <JunielKatarn@users.noreply.github.com>
2021-05-07 15:47:58 -0700

    Remove legacy UWP WebSocket module (#7736)
    * Remove legacy UWP WebSocket module
    
    * Change files
    
    * Drop WINRT conditional compilation for WS module

--- vnext/Shared/Modules/WebSocketModule.cpp
+++ vnext/Shared/Modules/WebSocketModule.cpp
@@ -227,4 +227,8 @@ shared_ptr<IWebSocketResource> WebSocketModule::GetOrCreateWebSocket(int64_t id,
 
 #pragma endregion private members
 
+/*extern*/ std::unique_ptr<facebook::xplat::module::CxxModule> CreateWebSocketModule() noexcept {
+  return make_unique<WebSocketModule>();
+}
+
 } // namespace Microsoft::React
