32a720a1ede0374d6b89aced4107f4ead9e5d35e
Chiara Mooney <34109996+chiaramooney@users.noreply.github.com>
2021-06-03 12:23:10 -0700

    Update Button Hover

--- vnext/src/Libraries/Components/Button.windows.js
+++ vnext/src/Libraries/Components/Button.windows.js
@@ -27,113 +27,113 @@ import type {PressEvent} from '../Types/CoreEventTypes';
 
 type ButtonProps = $ReadOnly<{|
   /**
-    Text to display inside the button. On Android the given title will be
-    converted to the uppercased form.
-   */
+     Text to display inside the button. On Android the given title will be
+     converted to the uppercased form.
+    */
   title: string,
 
   /**
-    Handler to be called when the user taps the button. The first function
-    argument is an event in form of [PressEvent](pressevent).
-   */
+     Handler to be called when the user taps the button. The first function
+     argument is an event in form of [PressEvent](pressevent).
+    */
   onPress: (event?: PressEvent) => mixed,
 
   /**
-    If `true`, doesn't play system sound on touch.
-
-    @platform android
-
-    @default false
-   */
+     If `true`, doesn't play system sound on touch.
+ 
+     @platform android
+ 
+     @default false
+    */
   touchSoundDisabled?: ?boolean,
 
   /**
-    Color of the text (iOS), or background color of the button (Android).
-
-    @default {@platform android} '#2196F3'
-    @default {@platform ios} '#007AFF'
-   */
+     Color of the text (iOS), or background color of the button (Android).
+ 
+     @default {@platform android} '#2196F3'
+     @default {@platform ios} '#007AFF'
+    */
   color?: ?ColorValue,
 
   /**
-    TV preferred focus.
-
-    @platform tv
-
-    @default false
-   */
+     TV preferred focus.
+ 
+     @platform tv
+ 
+     @default false
+    */
   hasTVPreferredFocus?: ?boolean,
 
   /**
-    Designates the next view to receive focus when the user navigates down. See
-    the [Android documentation][android:nextFocusDown].
-
-    [android:nextFocusDown]:
-    https://developer.android.com/reference/android/view/View.html#attr_android:nextFocusDown
-
-    @platform android, tv
-   */
+     Designates the next view to receive focus when the user navigates down. See
+     the [Android documentation][android:nextFocusDown].
+ 
+     [android:nextFocusDown]:
+     https://developer.android.com/reference/android/view/View.html#attr_android:nextFocusDown
+ 
+     @platform android, tv
+    */
   nextFocusDown?: ?number,
 
   /**
-    Designates the next view to receive focus when the user navigates forward.
-    See the [Android documentation][android:nextFocusForward].
-
-    [android:nextFocusForward]:
-    https://developer.android.com/reference/android/view/View.html#attr_android:nextFocusForward
-
-    @platform android, tv
-   */
+     Designates the next view to receive focus when the user navigates forward.
+     See the [Android documentation][android:nextFocusForward].
+ 
+     [android:nextFocusForward]:
+     https://developer.android.com/reference/android/view/View.html#attr_android:nextFocusForward
+ 
+     @platform android, tv
+    */
   nextFocusForward?: ?number,
 
   /**
-    Designates the next view to receive focus when the user navigates left. See
-    the [Android documentation][android:nextFocusLeft].
-
-    [android:nextFocusLeft]:
-    https://developer.android.com/reference/android/view/View.html#attr_android:nextFocusLeft
-
-    @platform android, tv
-   */
+     Designates the next view to receive focus when the user navigates left. See
+     the [Android documentation][android:nextFocusLeft].
+ 
+     [android:nextFocusLeft]:
+     https://developer.android.com/reference/android/view/View.html#attr_android:nextFocusLeft
+ 
+     @platform android, tv
+    */
   nextFocusLeft?: ?number,
 
   /**
-    Designates the next view to receive focus when the user navigates right. See
-    the [Android documentation][android:nextFocusRight].
-
-    [android:nextFocusRight]:
-    https://developer.android.com/reference/android/view/View.html#attr_android:nextFocusRight
-
-    @platform android, tv
-   */
+     Designates the next view to receive focus when the user navigates right. See
+     the [Android documentation][android:nextFocusRight].
+ 
+     [android:nextFocusRight]:
+     https://developer.android.com/reference/android/view/View.html#attr_android:nextFocusRight
+ 
+     @platform android, tv
+    */
   nextFocusRight?: ?number,
 
   /**
-    Designates the next view to receive focus when the user navigates up. See
-    the [Android documentation][android:nextFocusUp].
-
-    [android:nextFocusUp]:
-    https://developer.android.com/reference/android/view/View.html#attr_android:nextFocusUp
-
-    @platform android, tv
-   */
+     Designates the next view to receive focus when the user navigates up. See
+     the [Android documentation][android:nextFocusUp].
+ 
+     [android:nextFocusUp]:
+     https://developer.android.com/reference/android/view/View.html#attr_android:nextFocusUp
+ 
+     @platform android, tv
+    */
   nextFocusUp?: ?number,
 
   /**
-    Text to display for blindness accessibility features.
-   */
+     Text to display for blindness accessibility features.
+    */
   accessibilityLabel?: ?string,
 
   /**
-    If `true`, disable all interactions for this component.
-
-    @default false
-   */
+     If `true`, disable all interactions for this component.
+ 
+     @default false
+    */
   disabled?: ?boolean,
 
   /**
-    Used to locate this view in end-to-end tests.
-   */
+     Used to locate this view in end-to-end tests.
+    */
   testID?: ?string,
 
   /**
@@ -143,122 +143,122 @@ type ButtonProps = $ReadOnly<{|
 
   // [Windows
   /**
-    Set the order in which elements receive focus when the user navigates through them by pressing Tab.
-   */
+     Set the order in which elements receive focus when the user navigates through them by pressing Tab.
+    */
   tabIndex?: ?number,
   // Windows]
 |}>;
 
 /**
-  A basic button component that should render nicely on any platform. Supports a
-  minimal level of customization.
-
-  If this button doesn't look right for your app, you can build your own button
-  using [TouchableOpacity](touchableopacity) or
-  [TouchableWithoutFeedback](touchablewithoutfeedback). For inspiration, look at
-  the [source code for this button component][button:source]. Or, take a look at
-  the [wide variety of button components built by the community]
-  [button:examples].
-
-  [button:source]:
-  https://github.com/facebook/react-native/blob/master/Libraries/Components/Button.js
-
-  [button:examples]:
-  https://js.coach/?menu%5Bcollections%5D=React%20Native&page=1&query=button
-
-  ```jsx
-  <Button
-    onPress={onPressLearnMore}
-    title="Learn More"
-    color="#841584"
-    accessibilityLabel="Learn more about this purple button"
-  />
-  ```
-
-  ```SnackPlayer name=Button%20Example
-  import React from 'react';
-  import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
-
-  const Separator = () => (
-    <View style={styles.separator} />
-  );
-
-  const App = () => (
-    <SafeAreaView style={styles.container}>
-      <View>
-        <Text style={styles.title}>
-          The title and onPress handler are required. It is recommended to set accessibilityLabel to help make your app usable by everyone.
-        </Text>
-        <Button
-          title="Press me"
-          onPress={() => Alert.alert('Simple Button pressed')}
-        />
-      </View>
-      <Separator />
-      <View>
-        <Text style={styles.title}>
-          Adjust the color in a way that looks standard on each platform. On  iOS, the color prop controls the color of the text. On Android, the color adjusts the background color of the button.
-        </Text>
-        <Button
-          title="Press me"
-          color="#f194ff"
-          onPress={() => Alert.alert('Button with adjusted color pressed')}
-        />
-      </View>
-      <Separator />
-      <View>
-        <Text style={styles.title}>
-          All interaction for the component are disabled.
-        </Text>
-        <Button
-          title="Press me"
-          disabled
-          onPress={() => Alert.alert('Cannot press this one')}
-        />
-      </View>
-      <Separator />
-      <View>
-        <Text style={styles.title}>
-          This layout strategy lets the title define the width of the button.
-        </Text>
-        <View style={styles.fixToText}>
-          <Button
-            title="Left button"
-            onPress={() => Alert.alert('Left button pressed')}
-          />
-          <Button
-            title="Right button"
-            onPress={() => Alert.alert('Right button pressed')}
-          />
-        </View>
-      </View>
-    </SafeAreaView>
-  );
-
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      marginHorizontal: 16,
-    },
-    title: {
-      textAlign: 'center',
-      marginVertical: 8,
-    },
-    fixToText: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-    },
-    separator: {
-      marginVertical: 8,
-      borderBottomColor: '#737373',
-      borderBottomWidth: StyleSheet.hairlineWidth,
-    },
-  });
-
-  export default App;
-  ```
- */
+   A basic button component that should render nicely on any platform. Supports a
+   minimal level of customization.
+ 
+   If this button doesn't look right for your app, you can build your own button
+   using [TouchableOpacity](touchableopacity) or
+   [TouchableWithoutFeedback](touchablewithoutfeedback). For inspiration, look at
+   the [source code for this button component][button:source]. Or, take a look at
+   the [wide variety of button components built by the community]
+   [button:examples].
+ 
+   [button:source]:
+   https://github.com/facebook/react-native/blob/master/Libraries/Components/Button.js
+ 
+   [button:examples]:
+   https://js.coach/?menu%5Bcollections%5D=React%20Native&page=1&query=button
+ 
+   ```jsx
+   <Button
+     onPress={onPressLearnMore}
+     title="Learn More"
+     color="#841584"
+     accessibilityLabel="Learn more about this purple button"
+   />
+   ```
+ 
+   ```SnackPlayer name=Button%20Example
+   import React from 'react';
+   import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
+ 
+   const Separator = () => (
+     <View style={styles.separator} />
+   );
+ 
+   const App = () => (
+     <SafeAreaView style={styles.container}>
+       <View>
+         <Text style={styles.title}>
+           The title and onPress handler are required. It is recommended to set accessibilityLabel to help make your app usable by everyone.
+         </Text>
+         <Button
+           title="Press me"
+           onPress={() => Alert.alert('Simple Button pressed')}
+         />
+       </View>
+       <Separator />
+       <View>
+         <Text style={styles.title}>
+           Adjust the color in a way that looks standard on each platform. On  iOS, the color prop controls the color of the text. On Android, the color adjusts the background color of the button.
+         </Text>
+         <Button
+           title="Press me"
+           color="#f194ff"
+           onPress={() => Alert.alert('Button with adjusted color pressed')}
+         />
+       </View>
+       <Separator />
+       <View>
+         <Text style={styles.title}>
+           All interaction for the component are disabled.
+         </Text>
+         <Button
+           title="Press me"
+           disabled
+           onPress={() => Alert.alert('Cannot press this one')}
+         />
+       </View>
+       <Separator />
+       <View>
+         <Text style={styles.title}>
+           This layout strategy lets the title define the width of the button.
+         </Text>
+         <View style={styles.fixToText}>
+           <Button
+             title="Left button"
+             onPress={() => Alert.alert('Left button pressed')}
+           />
+           <Button
+             title="Right button"
+             onPress={() => Alert.alert('Right button pressed')}
+           />
+         </View>
+       </View>
+     </SafeAreaView>
+   );
+ 
+   const styles = StyleSheet.create({
+     container: {
+       flex: 1,
+       justifyContent: 'center',
+       marginHorizontal: 16,
+     },
+     title: {
+       textAlign: 'center',
+       marginVertical: 8,
+     },
+     fixToText: {
+       flexDirection: 'row',
+       justifyContent: 'space-between',
+     },
+     separator: {
+       marginVertical: 8,
+       borderBottomColor: '#737373',
+       borderBottomWidth: StyleSheet.hairlineWidth,
+     },
+   });
+ 
+   export default App;
+   ```
+  */
 
 class Button extends React.Component<ButtonProps, {hover: boolean}> {
   // [Windows
@@ -266,6 +266,9 @@ class Button extends React.Component<ButtonProps, {hover: boolean}> {
     super(props);
     this.state = {
       hover: false,
+      // Workaround for MUX2.5; remove after update to MUX2.6 is completed.
+      // https://github.com/microsoft/react-native-windows/issues/7425
+      pressed: false,
     };
   }
   // Windows]
@@ -338,15 +341,26 @@ class Button extends React.Component<ButtonProps, {hover: boolean}> {
           onPress={onPress}
           tabIndex={tabIndex}
           touchSoundDisabled={touchSoundDisabled}
+          // Workaround for MUX2.5; remove after update to MUX2.6 is completed.
+          // https://github.com/microsoft/react-native-windows/issues/7425
           underlayColor={PlatformColor('ButtonBackgroundPressed')}
+          onShowUnderlay={() => {
+            this.setState({pressed: true});
+          }}
+          onHideUnderlay={() => {
+            this.setState({pressed: false});
+          }}
           style={
-            this.state.hover
+            this.state.hover && !this.state.pressed
               ? [
                   buttonStyles,
                   {
                     backgroundColor: PlatformColor(
                       'ButtonBackgroundPointerOver',
                     ),
+                    // Workaround for MUX2.5; remove after update to MUX2.6 is completed.
+                    // https://github.com/microsoft/react-native-windows/issues/7425
+                    opacity: 0.1,
                   },
                 ]
               : buttonStyles
