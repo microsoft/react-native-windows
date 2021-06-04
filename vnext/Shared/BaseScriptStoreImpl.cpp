29e81ab898abd90d5ac65d467a411d27b4404893
Julio César Rocha <JunielKatarn@users.noreply.github.com>
2021-06-04 04:56:42 -0700

    Add JSI.MemoryMappedScriptStore runtime option to control usage of MemoryMappedBuffer (#7943)
    * Condition using memory mapped buffer to runtime option JSI.MemoryMappedScriptStore
    
    * Change files

--- vnext/Shared/BaseScriptStoreImpl.cpp
+++ vnext/Shared/BaseScriptStoreImpl.cpp
@@ -6,6 +6,8 @@
 #include "BaseScriptStoreImpl.h"
 #include "MemoryMappedBuffer.h"
 
+#include <RuntimeOptions.h>
+
 // C++/WinRT
 #include <winrt/base.h>
 
@@ -144,7 +146,26 @@ std::unique_ptr<const jsi::Buffer> LocalFileSimpleBufferStore::getBuffer(const s
     std::terminate();
   }
 
-  return Microsoft::JSI::MakeMemoryMappedBuffer(winrt::to_hstring(storeDirectory_ + bufferId).c_str());
+  if (Microsoft::React::GetRuntimeOptionBool("JSI.MemoryMappedScriptStore")) {
+    return Microsoft::JSI::MakeMemoryMappedBuffer(winrt::to_hstring(storeDirectory_ + bufferId).c_str());
+  } else {
+    // Treat buffer id as the relative path fragment.
+    std::ifstream file(storeDirectory_ + bufferId, std::ios::binary | std::ios::ate);
+
+    if (!file) {
+      return nullptr;
+    }
+
+    std::streamsize size = file.tellg();
+    file.seekg(0, std::ios::beg);
+
+    auto buffer = std::make_unique<ByteArrayBuffer>(static_cast<size_t>(size));
+    if (!file.read(reinterpret_cast<char *>(buffer->data()), size)) {
+      return nullptr;
+    }
+
+    return buffer;
+  }
 }
 
 bool LocalFileSimpleBufferStore::persistBuffer(
