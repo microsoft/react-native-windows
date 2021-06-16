d3684e0beb8630947a1868c69753bd47d6ac924f
REDMOND\agnel <agnel@microsoft.com>
2021-06-16 11:43:52 -0500

    display:none visibility fix

--- vnext/Microsoft.ReactNative/Views/FrameworkElementViewManager.cpp
+++ vnext/Microsoft.ReactNative/Views/FrameworkElementViewManager.cpp
@@ -499,6 +499,17 @@ bool FrameworkElementViewManager::UpdateProperty(
     } else if (propertyName == "accessibilityActions") {
       auto value = json_type_traits<winrt::IVector<winrt::react::uwp::AccessibilityAction>>::parseJson(propertyValue);
       DynamicAutomationProperties::SetAccessibilityActions(element, value);
+    } else if (propertyName == "display") {
+      if (propertyValue.Type() == winrt::Microsoft::ReactNative::JSValueType::String) {
+        auto value = propertyValue.AsString();
+        if (value == "none") {
+          element.Visibility(xaml::Visibility::Collapsed);
+        } else {
+          element.Visibility(xaml::Visibility::Visible);
+        }
+      } else if (propertyValue.IsNull()) {
+        element.ClearValue(xaml::UIElement::VisibilityProperty());
+      }
     } else {
       return Super::UpdateProperty(nodeToUpdate, propertyName, propertyValue);
     }
