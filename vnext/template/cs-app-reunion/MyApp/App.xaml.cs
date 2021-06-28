203ce56544acf94d734ef5034ce7a4f894c97165
Alexander Sklar <asklar@microsoft.com>
2021-06-28 00:02:51 -0700

    Add starting app - need to template-ize it

--- vnext/template/cs-app-reunion/MyApp/App.xaml.cs
+++ vnext/template/cs-app-reunion/MyApp/App.xaml.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
--- vnext/template/cs-app-reunion/MyApp/MainWindow.xaml.cs
+++ vnext/template/cs-app-reunion/MyApp/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.ReactNative;
+using Microsoft.ReactNative;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
