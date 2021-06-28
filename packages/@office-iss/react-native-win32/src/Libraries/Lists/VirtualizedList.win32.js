8b3cac79e62176eff11cbf7c3caa9332fc6e833a
Nick Gerleman <ngerlem@microsoft.com>
2021-06-28 04:11:33 -0700

    Hook into overrides and rn-win32

---
+++ packages/@office-iss/react-native-win32/src/Libraries/Lists/VirtualizedList.win32.js
@@ -0,0 +1,9 @@
+/**
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT License.
+ *
+ * @format
+ * @flow-strict
+ */
+
+module.exports = require('@react-native-windows/virtualized-list');
