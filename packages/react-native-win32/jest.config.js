e17e8f6d57e386a7050738ec95a1585281b02f8f
Nick Gerleman <ngerlem@microsoft.com>
2021-06-29 11:07:45 -0700

    [0.64] Enable List UTs in react-native-win32 (#8121) (#8137)
    * [0.64] Enable List UTs in react-native-win32 (#8121)
    
    * Enable List UTs in react-native-win32
    
    This replicates react-native-windows work to enable UTs, but for react-native-win32. We share some of the special resolvers between packages. This will need to be backported across a few different branches.
    
    Note that this change partially overlaps with #8101 which has a matching change to extract and modify the main jest resolver. This bit will merge with the first change which needs it.
    
    * Change files
    
    * revert vnext overrides change
    
    * remove accidental file

---
+++ packages/react-native-win32/jest.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  verbose: true,
+  resolver: './jest-resolver.js',
+  snapshotResolver: './jest-snapshot-resolver.js',
+  transform: {
+    '^.+\\.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp)$':
+      '<rootDir>/jest/assetFileTransformer.js',
+    '.*': './jest/preprocessor.js',
+  },
+  transformIgnorePatterns: [],
+  setupFiles: ['./jest/setup.js'],
+  timers: 'fake',
+  testRegex: '/__tests__/.*-test\\.js$',
+  testPathIgnorePatterns: [
+    // Only run the version of the tests that are part of the merged source output
+    'src',
+  ],
+  haste: {
+    defaultPlatform: 'win32',
+    platforms: ['win32']
+  },
+  unmockedModulePathPatterns: [
+    'react',
+    'Libraries/Renderer',
+    'promise',
+    'source-map',
+    'fastpath',
+    'denodeify',
+  ],
+  testEnvironment: 'node',
+  collectCoverageFrom: ['Libraries/**/*.js'],
+  coveragePathIgnorePatterns: [
+    '/__tests__/',
+    '/vendor/',
+    '<rootDir>/Libraries/react-native/',
+  ],
+};
