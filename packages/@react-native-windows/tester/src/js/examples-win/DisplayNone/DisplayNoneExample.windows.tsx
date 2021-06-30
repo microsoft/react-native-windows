4968682f984acb756943db2abd71cf2a5cbc1e32
Nick Gerleman <ngerlem@microsoft.com>
2021-06-29 23:29:11 -0700

    Attempt to fix e2e-test-app intermittency
    We have seen some test intermittency after a WinUI update, where a switch will often have a different background color, and differently rounded height/width than expected. Because of intermittency and visual differences we think this might be the result of dumping the visual tree during an animation.
    
    There are some arhitecutral improvements we could make to E2E infrastructure to add stability in case such as this, like shallow dumping (not deeply recursing into WinUI children), better syncrhonization logic, or fuzzier logic.
    
    In this specific example we can provide an alternate solution which works around correct contraints. The purpose of the intermittent test is to validate that "diplay: none" is propagated to a textbox. It currently does this by generating a snapshot of a wider parent component, which includes the switch the test interacts with.
    
    This change prevents us from trying to dump the visual tree of the component where we have direct transient interactions, since it is unneeded for the test. We do also have other tests which I believe dump switches in on/off state directly.

--- packages/@react-native-windows/tester/src/js/examples-win/DisplayNone/DisplayNoneExample.windows.tsx
+++ packages/@react-native-windows/tester/src/js/examples-win/DisplayNone/DisplayNoneExample.windows.tsx
@@ -12,26 +12,19 @@ const RNTesterPage = require('../../components/RNTesterPage');
 export class DisplayNoneExample extends React.Component<{}> {
   state = {
     displayNone: false,
-    displayNone2: false,
-    textState: 'TextInput1...',
-    textState2: 'TextInput2...',
+    textState: 'TextInput...',
   };
 
   public render() {
     return (
       <RNTesterPage>
         <RNTesterBlock title="Display:none style with TextInput">
-          <View testID="view-component-switch-view">
+          <View>
             <View>
-              <Text>toggle display:none TextInput1</Text>
+              <Text>toggle display:none TextInput</Text>
               <Switch
                 onValueChange={this._handlePress}
                 value={this.state.displayNone}
-              />
-              <Text>toggle display:none TextInput2</Text>
-              <Switch
-                onValueChange={this._handlePress2}
-                value={this.state.displayNone2}
                 testID="toggle-display:none"
               />
             </View>
@@ -40,13 +33,7 @@ export class DisplayNoneExample extends React.Component<{}> {
                 style={{height: 40, borderColor: 'gray', borderWidth: 1}}
                 onChangeText={text => this._handleChangeText(text)}
                 value={this.state.textState}
-              />
-            </View>
-            <View style={{display: this.state.displayNone2 ? 'none' : 'flex'}}>
-              <TextInput
-                style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-                onChangeText={text => this._handleChangeText2(text)}
-                value={this.state.textState2}
+                testID="example-textbox"
               />
             </View>
           </View>
@@ -59,23 +46,14 @@ export class DisplayNoneExample extends React.Component<{}> {
     this.setState({displayNone: !this.state.displayNone});
   };
 
-  _handlePress2 = () => {
-    this.setState({displayNone2: !this.state.displayNone2});
-  };
-
   _handleChangeText = (text: string) => {
     this.setState({textState: text});
-  };
-
-  _handleChangeText2 = (text: string) => {
-    this.setState({textState2: text});
   };
 }
 
 export const displayName = (_undefined?: string) => {};
 export const title = 'Display:none Style';
-export const description =
-  'Style prop which will collapse the element in XAML tree.';
+export const description = 'Tests that "display: none" is propagated to native';
 export const examples = [
   {
     render: function(): JSX.Element {
