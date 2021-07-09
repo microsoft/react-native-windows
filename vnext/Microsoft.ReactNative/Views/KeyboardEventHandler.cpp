1d99aa7aac653dd43eefb2a361208973cd01ecec
Eric Rozell <erozell@outlook.com>
2021-07-08 21:58:21 -0400

    Adds timestamp to onKeyUp/Down events (#8006)
    * Adds timestamp to onKeyUp/Down events
    
    In order to better support perf testing metrics for events driven by
    either pointer events or keyboard events (e.g., triggered by Pressable,
    which can be triggered by either keyboard or pointer), this adds a
    timestamp property to the native event data for `onKeyDown` and
    `onKeyUp`.
    
    Ideally this would use a native timestamp from the KeyRoutedEventArgs to
    get the actual time the key event occurred, but this is a sufficient
    approximation absent a XAML API to retrive this.
    
    We use `GetTickCount64()` because that matches the behavior for the
    PointerRoutedEventArgs.
    
    Fixes #8004
    
    * Change files
    
    * Update keyboard-reconcile-desktop proposal
    
    * Use the same approach for keyboard event timestamps as performance.now()

--- vnext/Microsoft.ReactNative/Views/KeyboardEventHandler.cpp
+++ vnext/Microsoft.ReactNative/Views/KeyboardEventHandler.cpp
@@ -20,6 +20,7 @@ static constexpr auto EVENT_PHASE = "handledEventPhase";
 static constexpr auto KEY = "key";
 static constexpr auto TARGET = "target";
 static constexpr auto CODE = "code";
+static constexpr auto TIMESTAMP = "timestamp";
 
 template <>
 struct json_type_traits<Microsoft::ReactNative::HandledKeyboardEvent> {
@@ -55,9 +56,9 @@ std::vector<HandledKeyboardEvent> KeyboardHelper::FromJS(winrt::Microsoft::React
   return json_type_traits<std::vector<HandledKeyboardEvent>>::parseJson(obj);
 }
 
-static folly::dynamic ToEventData(ReactKeyboardEvent event) {
+static folly::dynamic ToEventData(ReactKeyboardEvent event, double timestamp) {
   return folly::dynamic::object(TARGET, event.target)(ALT_KEY, event.altKey)(CTRL_KEY, event.ctrlKey)(KEY, event.key)(
-      META_KEY, event.metaKey)(SHIFT_KEY, event.shiftKey)(CODE, event.code);
+      META_KEY, event.metaKey)(SHIFT_KEY, event.shiftKey)(CODE, event.code)(TIMESTAMP, timestamp);
 }
 
 KeyboardEventBaseHandler::KeyboardEventBaseHandler(KeyboardEventCallback &&keyDown, KeyboardEventCallback &&keyUp)
@@ -210,6 +211,8 @@ void UpdateModifiedKeyStatusTo(T &event) {
 void PreviewKeyboardEventHandlerOnRoot::DispatchEventToJs(
     std::string &&eventName,
     xaml::Input::KeyRoutedEventArgs const &args) {
+  const auto timestamp =
+      std::chrono::duration<double, std::milli>(std::chrono::steady_clock::now().time_since_epoch()).count();
   if (auto source = args.OriginalSource().try_as<xaml::FrameworkElement>()) {
     auto reactId = getViewId(*m_context, source);
     if (reactId.isValid) {
@@ -219,7 +222,7 @@ void PreviewKeyboardEventHandlerOnRoot::DispatchEventToJs(
       event.key = KeyboardHelper::FromVirtualKey(args.Key(), event.shiftKey, event.capLocked);
       event.code = KeyboardHelper::CodeFromVirtualKey(args.OriginalKey());
 
-      m_context->DispatchEvent(event.target, std::move(eventName), ToEventData(event));
+      m_context->DispatchEvent(event.target, std::move(eventName), ToEventData(event, timestamp));
     }
   }
 }
