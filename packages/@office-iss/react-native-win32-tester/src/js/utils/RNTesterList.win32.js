c0db5f75f70e4cb2876442279a9dafea3f341e1b
Nick Gerleman <ngerlem@microsoft.com>
2021-07-15 04:03:45 -0700

    Integrate June 9 Nightly RN Build (#8147)
    * Integrate June 9 Nightly RN Build
    
    Commits: https://github.com/facebook/react-native/compare/0d32aef3a...386dbd943
    
    Has ACoates fixes for RNTester theming, and testID in NewAppScreen header.
    
    Will manuallly validate that the new RNTester example in https://github.com/facebook/react-native/commit/7463f6d0fd105a2074db78b620b3895753b13cbf works as expected on UWP. It does not run on NetUI due to using instance-global dimension APIs.
    
    * Update snapshots
    
    * Fork UIManagerBinding
    
    * Fix fabric build
    
    * Change files
    
    * save lockfile
    
    * Workaround Babel transform bug

--- packages/@office-iss/react-native-win32-tester/src/js/utils/RNTesterList.win32.js
+++ packages/@office-iss/react-native-win32-tester/src/js/utils/RNTesterList.win32.js
@@ -101,9 +101,15 @@ const ComponentExamples: Array<RNTesterExample> = [
     key: 'SectionListExample',
     category: 'ListView',
     module: require('../examples/SectionList/SectionListExample'),
+  },
+  {
+    key: 'SwipeableCardExample',
+    category: 'UI',
+    module: require('../examples/SwipeableCardExample/SwipeableCardExample'),
   }*/,
   {
     key: 'SwitchExample',
+    category: 'UI',
     module: require('../examples/Switch/SwitchExample'),
   },
   {
@@ -161,12 +167,12 @@ const APIExamples: Array<RNTesterExample> = [
   {
     key: 'AnimatedExample',
     category: 'UI',
-    module: require('../examples/Animated/AnimatedExample'),
+    module: require('../examples/Animated/AnimatedExample').default,
   },
   {
     key: 'Animation - GratuitousAnimation',
     category: 'UI',
-    module: require('../examples/Animated/AnimatedGratuitousApp/AnExApp'),
+    module: require('../examples/AnimatedGratuitousApp/AnExApp'),
   },
   {
     key: 'AppearanceExample',
