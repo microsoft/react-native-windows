a2f0217ff7452fdf183ff2d85c7d862a92404908
Jon Thysell (JAUNTY) <jthysell@microsoft.com>
2021-08-16 13:42:23 -0700

    Added tests

--- packages/@react-native-windows/cli/src/e2etest/projects/MissingProjectFilesLib/react-native.config.js
+++ packages/@react-native-windows/cli/src/e2etest/projects/MissingProjectFilesLib/react-native.config.js
@@ -2,19 +2,10 @@ const projectConfig = {
   sourceDir: 'windows',
 };
 
-const expectedConfig = {
-  sourceDir: 'windows',
-  solutionFile: null,
-  projects: [],
-  nugetPackages: [],
-};
-
 module.exports = {
   dependency: {
     platforms: {
       windows: projectConfig,
     }
   },
-  expectedConfig: expectedConfig,
-  expectedConfigIgnoringOverride: expectedConfig,
 };
