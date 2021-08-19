7bfb864ce2c65b0f8750ec118310b4b47376108a
Nick Gerleman <ngerlem@microsoft.com>
2021-08-19 09:58:27 -0700

    udpate another reference

--- packages/e2e-test-app/test/visitAllPages.test.ts
+++ packages/e2e-test-app/test/visitAllPages.test.ts
@@ -24,8 +24,8 @@ type RNTesterList = {
 
 const testerList: RNTesterList = require('@react-native-windows/tester/js/utils/RNTesterList');
 
-const apiExamples = testerList.APIExamples.map(e => e.module.title);
-const componentExamples = testerList.ComponentExamples.map(e => e.module.title);
+const apiExamples = testerList.APIs.map(e => e.module.title);
+const componentExamples = testerList.Components.map(e => e.module.title);
 
 describe('visitAllPages', () => {
   for (const component of componentExamples) {
