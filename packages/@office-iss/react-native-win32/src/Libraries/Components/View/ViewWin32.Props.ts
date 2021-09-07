db3021986fcc05b7f10084b0c97be30e8246455f
samuelfreiberg <safreibe@microsoft.com>
2021-09-07 09:58:54 -0700

    Nit - Removing whitespace

--- packages/@office-iss/react-native-win32/src/Libraries/Components/View/ViewWin32.Props.ts
+++ packages/@office-iss/react-native-win32/src/Libraries/Components/View/ViewWin32.Props.ts
@@ -177,7 +177,6 @@ export type BasePropsWin32 = {
   */
   accessibilityControls?: React.RefObject<any>;
 
-
   /**
   * Identifies the ItemType property, which is a text string describing the type of the automation element.
   * ItemType is used to obtain information about items in a list, tree view, or data grid. For example, an item in a file directory view might be a "Document File" or a "Folder".
