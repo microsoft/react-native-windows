98e027094514c89b6327cd13f87c237aa52ba510
Julio C. Rocha <julio.rocha@microsoft.com>
2021-09-11 02:38:35 -0700

    Add TODO to rename MakeNapiJsiRuntime2

--- vnext/Shared/JSI/NapiJsiRuntime.h
+++ vnext/Shared/JSI/NapiJsiRuntime.h
@@ -14,6 +14,7 @@ namespace Microsoft::JSI {
 
 ///
 // NapiJsiRuntime factory function.
+// TODO: Rename as MakeNapiJsiRuntime2 once code is dropped from V8-JSI.
 ///
 std::unique_ptr<facebook::jsi::Runtime> __cdecl MakeNapiJsiRuntime2(napi_env env) noexcept;
 
