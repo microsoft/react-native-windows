c8a0c9b89753b5a453d34f5d100a68aa416fdc6b
Julio C. Rocha <julio.rocha@microsoft.com>
2021-09-11 02:38:50 -0700

    Fix typo

--- vnext/Shared/JSI/NapiJsiRuntime.h
+++ vnext/Shared/JSI/NapiJsiRuntime.h
@@ -14,7 +14,7 @@ namespace Microsoft::JSI {
 
 ///
 // NapiJsiRuntime factory function.
-// TODO: Rename as MakeNapiJsiRuntime2 once code is dropped from V8-JSI.
+// TODO: Rename as MakeNapiJsiRuntime once code is dropped from V8-JSI.
 ///
 std::unique_ptr<facebook::jsi::Runtime> __cdecl MakeNapiJsiRuntime2(napi_env env) noexcept;
 
