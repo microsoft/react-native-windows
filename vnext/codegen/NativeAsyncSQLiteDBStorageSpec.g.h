a35c39ad154024cc8f3434975655fe49b3cb6177
Zihan Chen (MSFT) <53799235+ZihanChen-MSFT@users.noreply.github.com>
2021-09-13 16:07:08 -0700

    `yarn build`

--- vnext/codegen/NativeAsyncSQLiteDBStorageSpec.g.h
+++ vnext/codegen/NativeAsyncSQLiteDBStorageSpec.g.h
@@ -13,14 +13,62 @@
 
 namespace Microsoft::ReactNativeSpecs {
 
+REACT_STRUCT(AsyncSQLiteDBStorageSpec_multiGet_callback_errors_element)
+struct AsyncSQLiteDBStorageSpec_multiGet_callback_errors_element {
+    REACT_FIELD(message)
+    std::string message;
+    REACT_FIELD(key)
+    std::optional<std::string> key;
+};
+
+REACT_STRUCT(AsyncSQLiteDBStorageSpec_multiSet_callback_errors_element)
+struct AsyncSQLiteDBStorageSpec_multiSet_callback_errors_element {
+    REACT_FIELD(message)
+    std::string message;
+    REACT_FIELD(key)
+    std::optional<std::string> key;
+};
+
+REACT_STRUCT(AsyncSQLiteDBStorageSpec_multiMerge_callback_errors_element)
+struct AsyncSQLiteDBStorageSpec_multiMerge_callback_errors_element {
+    REACT_FIELD(message)
+    std::string message;
+    REACT_FIELD(key)
+    std::optional<std::string> key;
+};
+
+REACT_STRUCT(AsyncSQLiteDBStorageSpec_multiRemove_callback_errors_element)
+struct AsyncSQLiteDBStorageSpec_multiRemove_callback_errors_element {
+    REACT_FIELD(message)
+    std::string message;
+    REACT_FIELD(key)
+    std::optional<std::string> key;
+};
+
+REACT_STRUCT(AsyncSQLiteDBStorageSpec_clear_callback_error)
+struct AsyncSQLiteDBStorageSpec_clear_callback_error {
+    REACT_FIELD(message)
+    std::string message;
+    REACT_FIELD(key)
+    std::optional<std::string> key;
+};
+
+REACT_STRUCT(AsyncSQLiteDBStorageSpec_getAllKeys_callback_error)
+struct AsyncSQLiteDBStorageSpec_getAllKeys_callback_error {
+    REACT_FIELD(message)
+    std::string message;
+    REACT_FIELD(key)
+    std::optional<std::string> key;
+};
+
 struct AsyncSQLiteDBStorageSpec : winrt::Microsoft::ReactNative::TurboModuleSpec {
   static constexpr auto methods = std::tuple{
-      Method<void(std::vector<std::string>, Callback<std::optional<std::vector<React::JSValueObject>>, std::optional<std::vector<std::vector<std::string>>>>) noexcept>{0, L"multiGet"},
-      Method<void(std::vector<std::vector<std::string>>, Callback<std::optional<std::vector<React::JSValueObject>>>) noexcept>{1, L"multiSet"},
-      Method<void(std::vector<std::vector<std::string>>, Callback<std::optional<std::vector<React::JSValueObject>>>) noexcept>{2, L"multiMerge"},
-      Method<void(std::vector<std::string>, Callback<std::optional<std::vector<React::JSValueObject>>>) noexcept>{3, L"multiRemove"},
-      Method<void(Callback<React::JSValueObject>) noexcept>{4, L"clear"},
-      Method<void(Callback<std::optional<React::JSValueObject>, std::optional<std::vector<std::string>>>) noexcept>{5, L"getAllKeys"},
+      Method<void(std::vector<std::string>, Callback<std::optional<std::vector<AsyncSQLiteDBStorageSpec_multiGet_callback_errors_element>>, std::optional<std::vector<std::vector<std::string>>>>) noexcept>{0, L"multiGet"},
+      Method<void(std::vector<std::vector<std::string>>, Callback<std::optional<std::vector<AsyncSQLiteDBStorageSpec_multiSet_callback_errors_element>>>) noexcept>{1, L"multiSet"},
+      Method<void(std::vector<std::vector<std::string>>, Callback<std::optional<std::vector<AsyncSQLiteDBStorageSpec_multiMerge_callback_errors_element>>>) noexcept>{2, L"multiMerge"},
+      Method<void(std::vector<std::string>, Callback<std::optional<std::vector<AsyncSQLiteDBStorageSpec_multiRemove_callback_errors_element>>>) noexcept>{3, L"multiRemove"},
+      Method<void(Callback<AsyncSQLiteDBStorageSpec_clear_callback_error>) noexcept>{4, L"clear"},
+      Method<void(Callback<std::optional<AsyncSQLiteDBStorageSpec_getAllKeys_callback_error>, std::optional<std::vector<std::string>>>) noexcept>{5, L"getAllKeys"},
   };
 
   template <class TModule>
@@ -30,33 +78,33 @@ struct AsyncSQLiteDBStorageSpec : winrt::Microsoft::ReactNative::TurboModuleSpec
     REACT_SHOW_METHOD_SPEC_ERRORS(
           0,
           "multiGet",
-          "    REACT_METHOD(multiGet) void multiGet(std::vector<std::string> const & keys, std::function<void(std::optional<std::vector<React::JSValueObject>>, std::optional<std::vector<std::vector<std::string>>>)> const & callback) noexcept { /* implementation */ }}\n"
-          "    REACT_METHOD(multiGet) static void multiGet(std::vector<std::string> const & keys, std::function<void(std::optional<std::vector<React::JSValueObject>>, std::optional<std::vector<std::vector<std::string>>>)> const & callback) noexcept { /* implementation */ }}\n");
+          "    REACT_METHOD(multiGet) void multiGet(std::vector<std::string> const & keys, std::function<void(std::optional<std::vector<AsyncSQLiteDBStorageSpec_multiGet_callback_errors_element>>, std::optional<std::vector<std::vector<std::string>>>)> const & callback) noexcept { /* implementation */ }}\n"
+          "    REACT_METHOD(multiGet) static void multiGet(std::vector<std::string> const & keys, std::function<void(std::optional<std::vector<AsyncSQLiteDBStorageSpec_multiGet_callback_errors_element>>, std::optional<std::vector<std::vector<std::string>>>)> const & callback) noexcept { /* implementation */ }}\n");
     REACT_SHOW_METHOD_SPEC_ERRORS(
           1,
           "multiSet",
-          "    REACT_METHOD(multiSet) void multiSet(std::vector<std::vector<std::string>> const & kvPairs, std::function<void(std::optional<std::vector<React::JSValueObject>>)> const & callback) noexcept { /* implementation */ }}\n"
-          "    REACT_METHOD(multiSet) static void multiSet(std::vector<std::vector<std::string>> const & kvPairs, std::function<void(std::optional<std::vector<React::JSValueObject>>)> const & callback) noexcept { /* implementation */ }}\n");
+          "    REACT_METHOD(multiSet) void multiSet(std::vector<std::vector<std::string>> const & kvPairs, std::function<void(std::optional<std::vector<AsyncSQLiteDBStorageSpec_multiSet_callback_errors_element>>)> const & callback) noexcept { /* implementation */ }}\n"
+          "    REACT_METHOD(multiSet) static void multiSet(std::vector<std::vector<std::string>> const & kvPairs, std::function<void(std::optional<std::vector<AsyncSQLiteDBStorageSpec_multiSet_callback_errors_element>>)> const & callback) noexcept { /* implementation */ }}\n");
     REACT_SHOW_METHOD_SPEC_ERRORS(
           2,
           "multiMerge",
-          "    REACT_METHOD(multiMerge) void multiMerge(std::vector<std::vector<std::string>> const & kvPairs, std::function<void(std::optional<std::vector<React::JSValueObject>>)> const & callback) noexcept { /* implementation */ }}\n"
-          "    REACT_METHOD(multiMerge) static void multiMerge(std::vector<std::vector<std::string>> const & kvPairs, std::function<void(std::optional<std::vector<React::JSValueObject>>)> const & callback) noexcept { /* implementation */ }}\n");
+          "    REACT_METHOD(multiMerge) void multiMerge(std::vector<std::vector<std::string>> const & kvPairs, std::function<void(std::optional<std::vector<AsyncSQLiteDBStorageSpec_multiMerge_callback_errors_element>>)> const & callback) noexcept { /* implementation */ }}\n"
+          "    REACT_METHOD(multiMerge) static void multiMerge(std::vector<std::vector<std::string>> const & kvPairs, std::function<void(std::optional<std::vector<AsyncSQLiteDBStorageSpec_multiMerge_callback_errors_element>>)> const & callback) noexcept { /* implementation */ }}\n");
     REACT_SHOW_METHOD_SPEC_ERRORS(
           3,
           "multiRemove",
-          "    REACT_METHOD(multiRemove) void multiRemove(std::vector<std::string> const & keys, std::function<void(std::optional<std::vector<React::JSValueObject>>)> const & callback) noexcept { /* implementation */ }}\n"
-          "    REACT_METHOD(multiRemove) static void multiRemove(std::vector<std::string> const & keys, std::function<void(std::optional<std::vector<React::JSValueObject>>)> const & callback) noexcept { /* implementation */ }}\n");
+          "    REACT_METHOD(multiRemove) void multiRemove(std::vector<std::string> const & keys, std::function<void(std::optional<std::vector<AsyncSQLiteDBStorageSpec_multiRemove_callback_errors_element>>)> const & callback) noexcept { /* implementation */ }}\n"
+          "    REACT_METHOD(multiRemove) static void multiRemove(std::vector<std::string> const & keys, std::function<void(std::optional<std::vector<AsyncSQLiteDBStorageSpec_multiRemove_callback_errors_element>>)> const & callback) noexcept { /* implementation */ }}\n");
     REACT_SHOW_METHOD_SPEC_ERRORS(
           4,
           "clear",
-          "    REACT_METHOD(clear) void clear(std::function<void(React::JSValueObject const &)> const & callback) noexcept { /* implementation */ }}\n"
-          "    REACT_METHOD(clear) static void clear(std::function<void(React::JSValueObject const &)> const & callback) noexcept { /* implementation */ }}\n");
+          "    REACT_METHOD(clear) void clear(std::function<void(AsyncSQLiteDBStorageSpec_clear_callback_error const &)> const & callback) noexcept { /* implementation */ }}\n"
+          "    REACT_METHOD(clear) static void clear(std::function<void(AsyncSQLiteDBStorageSpec_clear_callback_error const &)> const & callback) noexcept { /* implementation */ }}\n");
     REACT_SHOW_METHOD_SPEC_ERRORS(
           5,
           "getAllKeys",
-          "    REACT_METHOD(getAllKeys) void getAllKeys(std::function<void(std::optional<React::JSValueObject>, std::optional<std::vector<std::string>>)> const & callback) noexcept { /* implementation */ }}\n"
-          "    REACT_METHOD(getAllKeys) static void getAllKeys(std::function<void(std::optional<React::JSValueObject>, std::optional<std::vector<std::string>>)> const & callback) noexcept { /* implementation */ }}\n");
+          "    REACT_METHOD(getAllKeys) void getAllKeys(std::function<void(std::optional<AsyncSQLiteDBStorageSpec_getAllKeys_callback_error>, std::optional<std::vector<std::string>>)> const & callback) noexcept { /* implementation */ }}\n"
+          "    REACT_METHOD(getAllKeys) static void getAllKeys(std::function<void(std::optional<AsyncSQLiteDBStorageSpec_getAllKeys_callback_error>, std::optional<std::vector<std::string>>)> const & callback) noexcept { /* implementation */ }}\n");
   }
 };
 
