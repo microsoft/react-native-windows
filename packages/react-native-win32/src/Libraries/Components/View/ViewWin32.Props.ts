47ceaac64f88816796d67d08affb9beea67d6344
Samuel Freiberg <32719011+samuelfreiberg@users.noreply.github.com>
2021-09-13 12:05:51 -0700

    [0.64] Adding Accessibility Prop accessibilityItemType on ViewWin32 (#8545)
    * Adding accessibilityItemType to ViewWin32
    
    * Adding to the testpage
    
    * Change files
    
    * Removing whitespace
    
    Co-authored-by: Andrew Coates <30809111+acoates-ms@users.noreply.github.com>

--- packages/react-native-win32/src/Libraries/Components/View/ViewWin32.Props.ts
+++ packages/react-native-win32/src/Libraries/Components/View/ViewWin32.Props.ts
@@ -175,7 +175,13 @@ export type BasePropsWin32 = {
   * This is mainly used for a Textbox with a Dropdown PeoplePicker-type list.  This allows an 
   * accessibility tool to query those other providers for properties and listen to their events.
   */
-   accessibilityControls?: React.RefObject<any>;   
+   accessibilityControls?: React.RefObject<any>;
+
+  /**
+  * Identifies the ItemType property, which is a text string describing the type of the automation element.
+  * ItemType is used to obtain information about items in a list, tree view, or data grid. For example, an item in a file directory view might be a "Document File" or a "Folder".
+  */
+   accessibilityItemType?: string;
 };
 
 export type ViewWin32OmitTypes = RN.ViewPropsAndroid &
