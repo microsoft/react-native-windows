62d758e317e7f6a5ad762582f86f082ba1b70841
Eric Rozell <erozell@outlook.com>
2021-09-14 11:15:34 -0400

    Reducing to minimum API surface to support fixes for pointerEvents

--- vnext/Microsoft.ReactNative/ReactPointerEventArgs.cpp
+++ vnext/Microsoft.ReactNative/ReactPointerEventArgs.cpp
@@ -12,13 +12,7 @@ xaml::Input::PointerRoutedEventArgs ReactPointerEventArgs::Args() const noexcept
 }
 
 PointerEventKind ReactPointerEventArgs::Kind() const noexcept {
-  return m_kind.has_value() ? m_kind.value() : m_originalKind;
-}
-
-void ReactPointerEventArgs::Kind(PointerEventKind const &kind) noexcept {
-  if (m_originalKind == PointerEventKind::CaptureLost && kind == PointerEventKind::End) {
-    m_kind = kind;
-  }
+  return m_kind;
 }
 
 winrt::IInspectable ReactPointerEventArgs::Target() const noexcept {
@@ -29,25 +23,9 @@ void ReactPointerEventArgs::Target(winrt::IInspectable const &target) noexcept {
   m_target = target;
 }
 
-void ReactPointerEventArgs::AllowUncaptured() {
-  m_uncapturedAllowed = true;
-}
-
-void ReactPointerEventArgs::PreventDefault() {
-  m_defaultPrevented = true;
-}
-
 ReactPointerEventArgs::ReactPointerEventArgs(
     PointerEventKind kind,
     xaml::Input::PointerRoutedEventArgs const &args) noexcept
-    : m_originalKind{kind}, m_args{args} {}
-
-bool ReactPointerEventArgs::DefaultPrevented() const noexcept {
-  return m_defaultPrevented;
-}
-
-bool ReactPointerEventArgs::UncapturedAllowed() const noexcept {
-  return m_uncapturedAllowed;
-}
+    : m_kind{kind}, m_args{args} {}
 
 } // namespace winrt::Microsoft::ReactNative::implementation
