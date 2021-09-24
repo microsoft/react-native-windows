fd1408e6ce9c6904ac2929d8cb61f379bd6984fa
Anandraj <anandrag@microsoft.com>
2021-09-24 11:35:48 -0700

    More diagnostics

--- vnext/Scripts/Tracing/Start-Tracing.ps1
+++ vnext/Scripts/Tracing/Start-Tracing.ps1
@@ -5,6 +5,18 @@ param(
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $wprpPath = Join-Path -Path $scriptPath -ChildPath "rnw.wprp"
 
+[System.Environment]::OSVersion.Version
+
+(Get-WmiObject -class Win32_OperatingSystem).Caption
+
+Get-WmiObject -class Win32_OperatingSystem
+
+If (!(test-path "c:\windows\system32\icu.dll")) {
+   Write-Host "c:\windows\system32\icu.dll not found."   
+} else {
+   Write-Host "c:\windows\system32\icu.dll found."   
+}
+
 if (!(Get-Command "wpr.exe" -ErrorAction SilentlyContinue)) { 
    throw "
    ################################################################
