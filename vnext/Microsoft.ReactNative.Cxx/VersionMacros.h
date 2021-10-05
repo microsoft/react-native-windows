fa434170fa9ccdd61fb8e4d68f713cbb06e6db89
Nick Gerleman <ngerlem@microsoft.com>
2021-10-05 09:36:32 -0700

    yarn format

--- vnext/Microsoft.ReactNative.Cxx/VersionMacros.h
+++ vnext/Microsoft.ReactNative.Cxx/VersionMacros.h
@@ -4,18 +4,16 @@
 #pragma once
 
 #ifdef RNW_CANARY
-  #define RNW_VERSION_AT_LEAST(major, minor, patch) true
-  #define RNW_VERSION_LESS_THAN(major, minor, patch) false
+#define RNW_VERSION_AT_LEAST(major, minor, patch) true
+#define RNW_VERSION_LESS_THAN(major, minor, patch) false
 #else
 
-  #define RNW_VERSION_AT_LEAST(major, minor, patch) \
-    (RNW_MAJOR > major) || \
-    (RNW_MAJOR == major && RNW_MINOR > minor) || \
-    (RNW_MAJOR == major && RNW_MINOR == minor && RNW_PATCH >= patch)
-  
-  #define RNW_VERSION_LESS_THAN(major, minor, patch) \
-    (RNW_MAJOR < major) || \
-    (RNW_MAJOR == major && RNW_MINOR < minor) || \
-    (RNW_MAJOR == major && RNW_MINOR == minor < RNW_PATCH < patch)
+#define RNW_VERSION_AT_LEAST(major, minor, patch)                     \
+  (RNW_MAJOR > major) || (RNW_MAJOR == major && RNW_MINOR > minor) || \
+      (RNW_MAJOR == major && RNW_MINOR == minor && RNW_PATCH >= patch)
+
+#define RNW_VERSION_LESS_THAN(major, minor, patch)                    \
+  (RNW_MAJOR < major) || (RNW_MAJOR == major && RNW_MINOR < minor) || \
+      (RNW_MAJOR == major && RNW_MINOR == minor < RNW_PATCH < patch)
 
 #endif
