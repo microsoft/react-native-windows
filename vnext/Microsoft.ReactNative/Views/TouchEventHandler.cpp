microsoft/react-native-windows

commit e7735a8912803e52113fa4d871764de2f2977dc4

Allow pointer events on selectable Text (#7813)
* Allow pointer events on selectable Text

When TextBlock::IsTextSelectionEnabled is set to true, pointer events
are canceled because the events are always marked as handled by the
TextBlock, thus the events never reach the React Native root view. This
change subscribes a TouchEventHandler to Text when the `selectable` prop
is set to true, and also conditionally fires the `onTouchEnd` event when
PointerCaptureLost fires when text is *not* selected as a result of the
pointer events (e.g., click-and-drag or the second click of a double
click). This allows us to create `onPress` callbacks that mimic the
behavior of the XAML hyperlink component. In order for the correct
page layout dimensions to be reported, we also need to provide the root
view to TouchEventHandler.

Fixes #7812

* Change files

* Handle overrides of PointerCaptureLost events to emit `onTouchEnd` as opposed to `onTouchCancel`.

* yarn lint --fix

* yarn format

Co-authored-by: Nick Gerleman <ngerlem@microsoft.com>

vnext/Microsoft.ReactNative/Views/TouchEventHandler.cpp
@@ -35,6 +35,7 @@ std::vector<int64_t> GetTagsForBranch(INativeUIManagerHost *host, int64_t tag, i
 
 TouchEventHandler::TouchEventHandler(const Mso::React::IReactContext &context, bool fabric)
     : m_xamlView(nullptr),
+      m_rootView(nullptr),
       m_context(&context),
       m_fabric(fabric),
       m_batchingEventEmitter{
@@ -44,33 +45,49 @@ TouchEventHandler::~TouchEventHandler() {
   RemoveTouchHandlers();
 }
 
-void TouchEventHandler::AddTouchHandlers(XamlView xamlView) {
+void TouchEventHandler::AddTouchHandlers(XamlView xamlView, XamlView rootView, bool handledEventsToo) {
   auto uiElement(xamlView.as<xaml::UIElement>());
   if (uiElement == nullptr) {
     assert(false);
     return;
   }
 
-  m_xamlView = xamlView;
-
   RemoveTouchHandlers();
 
-  m_pressedRevoker = uiElement.PointerPressed(winrt::auto_revoke, {this, &TouchEventHandler::OnPointerPressed});
-  m_releasedRevoker = uiElement.PointerReleased(winrt::auto_revoke, {this, &TouchEventHandler::OnPointerReleased});
-  m_canceledRevoker = uiElement.PointerCanceled(winrt::auto_revoke, {this, &TouchEventHandler::OnPointerCanceled});
-  m_captureLostRevoker =
-      uiElement.PointerCaptureLost(winrt::auto_revoke, {this, &TouchEventHandler::OnPointerCaptureLost});
-  m_exitedRevoker = uiElement.PointerExited(winrt::auto_revoke, {this, &TouchEventHandler::OnPointerExited});
-  m_movedRevoker = uiElement.PointerMoved(winrt::auto_revoke, {this, &TouchEventHandler::OnPointerMoved});
+  m_xamlView = xamlView;
+  m_rootView = rootView;
+  m_pressedHandler = winrt::box_value(winrt::PointerEventHandler{this, &TouchEventHandler::OnPointerPressed});
+  m_releasedHandler = winrt::box_value(winrt::PointerEventHandler{this, &TouchEventHandler::OnPointerReleased});
+  m_canceledHandler = winrt::box_value(winrt::PointerEventHandler{this, &TouchEventHandler::OnPointerCanceled});
+  m_captureLostHandler = winrt::box_value(winrt::PointerEventHandler{this, &TouchEventHandler::OnPointerCaptureLost});
+  m_exitedHandler = winrt::box_value(winrt::PointerEventHandler{this, &TouchEventHandler::OnPointerExited});
+  m_movedHandler = winrt::box_value(winrt::PointerEventHandler{this, &TouchEventHandler::OnPointerMoved});
+  uiElement.AddHandler(xaml::UIElement::PointerPressedEvent(), m_pressedHandler, handledEventsToo);
+  uiElement.AddHandler(xaml::UIElement::PointerReleasedEvent(), m_releasedHandler, handledEventsToo);
+  uiElement.AddHandler(xaml::UIElement::PointerCanceledEvent(), m_canceledHandler, handledEventsToo);
+  uiElement.AddHandler(xaml::UIElement::PointerCaptureLostEvent(), m_captureLostHandler, handledEventsToo);
+  uiElement.AddHandler(xaml::UIElement::PointerExitedEvent(), m_exitedHandler, handledEventsToo);
+  uiElement.AddHandler(xaml::UIElement::PointerMovedEvent(), m_movedHandler, handledEventsToo);
 }
 
 void TouchEventHandler::RemoveTouchHandlers() {
-  m_pressedRevoker.revoke();
-  m_releasedRevoker.revoke();
-  m_canceledRevoker.revoke();
-  m_captureLostRevoker.revoke();
-  m_exitedRevoker.revoke();
-  m_movedRevoker.revoke();
+  if (m_xamlView) {
+    auto uiElement(m_xamlView.as<xaml::UIElement>());
+    uiElement.RemoveHandler(xaml::UIElement::PointerPressedEvent(), m_pressedHandler);
+    uiElement.RemoveHandler(xaml::UIElement::PointerReleasedEvent(), m_releasedHandler);
+    uiElement.RemoveHandler(xaml::UIElement::PointerCanceledEvent(), m_canceledHandler);
+    uiElement.RemoveHandler(xaml::UIElement::PointerCaptureLostEvent(), m_captureLostHandler);
+    uiElement.RemoveHandler(xaml::UIElement::PointerExitedEvent(), m_exitedHandler);
+    uiElement.RemoveHandler(xaml::UIElement::PointerMovedEvent(), m_movedHandler);
+    m_pressedHandler = nullptr;
+    m_releasedHandler = nullptr;
+    m_canceledHandler = nullptr;
+    m_captureLostHandler = nullptr;
+    m_exitedHandler = nullptr;
+    m_movedHandler = nullptr;
+    m_rootView = nullptr;
+    m_xamlView = nullptr;
+  }
 }
 
 winrt::Microsoft::ReactNative::BatchingEventEmitter &TouchEventHandler::BatchingEmitter() noexcept {
@@ -197,7 +214,12 @@ void TouchEventHandler::OnPointerConcluded(TouchEventType eventType, const winrt
     UpdateReactPointer(m_pointers[*optPointerIndex], args, sourceElement);
 
   if (m_pointers[*optPointerIndex].isLeftButton) {
-    DispatchTouchEvent(eventType, *optPointerIndex);
+    // In case a PointerCaptureLost event should be treated as an "end" event,
+    // check the ReactPointerEventArgs Kind property before emitting the event.
+    const auto adjustedEventType = reactArgs.Kind() == winrt::Microsoft::ReactNative::PointerEventKind::End
+        ? TouchEventType::End
+        : TouchEventType::Cancel;
+    DispatchTouchEvent(adjustedEventType, *optPointerIndex);
   }
 
   m_pointers.erase(cbegin(m_pointers) + *optPointerIndex);
@@ -250,7 +272,7 @@ void TouchEventHandler::UpdateReactPointer(
     ReactPointer &pointer,
     const winrt::PointerRoutedEventArgs &args,
     xaml::UIElement sourceElement) {
-  auto rootPoint = args.GetCurrentPoint(m_xamlView.as<xaml::FrameworkElement>());
+  auto rootPoint = args.GetCurrentPoint(m_rootView.as<xaml::FrameworkElement>());
   auto point = args.GetCurrentPoint(sourceElement);
   auto props = point.Properties();
   auto keyModifiers = static_cast<uint32_t>(args.KeyModifiers());

packages/@react-native-windows/tester/src/js/examples-win/LegacyTests/SelectableTextTestPage.tsx
@@ -1,0 +1,74 @@
+/**
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT License.
+ * @format
+ */
+
+import React from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
+
+interface IPressableTextState {
+  count: number;
+  isSelectable: boolean;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'flex-start',
+  },
+  pressable: {
+    color: 'blue',
+    textDecorationLine: 'underline',
+  },
+});
+
+export class SelectableTextTests extends React.Component<
+  {},
+  IPressableTextState
+> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      count: 0,
+      isSelectable: false,
+    };
+  }
+  public render() {
+    const increment = () => this.setState({count: this.state.count + 1});
+    return (
+      <View style={styles.container}>
+        <Text testID="pressed-state">Pressed: {this.state.count} times.</Text>
+        <Text testID="text-example" selectable={this.state.isSelectable}>
+          Text before{' '}
+          <Text onPress={increment} style={styles.pressable}>
+            click here
+          </Text>{' '}
+          text after
+        </Text>
+        <Button
+          testID="toggle-selectable-button"
+          title="Toggle Selectable"
+          onPress={() =>
+            this.setState({isSelectable: !this.state.isSelectable})
+          }
+        />
+        <Button
+          testID="clear-state-button"
+          title="Clear State"
+          onPress={() => this.setState({count: 0, isSelectable: false})}
+        />
+      </View>
+    );
+  }
+}
+
+export const displayName = (_undefined?: string) => {};
+export const title = 'LegacySelectableTextTest';
+export const description = 'Legacy e2e test for selectable Text hit testing';
+export const examples = [
+  {
+    render: function(): JSX.Element {
+      return <SelectableTextTests />;
+    },
+  },
+];

packages/@react-native-windows/tester/src/js/utils/RNTesterList.windows.js
@@ -151,6 +151,10 @@ const Components: Array<RNTesterModuleInfo> = [
     module: require('../examples-win/LegacyTests/ImageTestPage'),
   },
   {
+    key: 'LegacySelectableTextTest',
+    module: require('../examples-win/LegacyTests/SelectableTextTestPage'),
+  },
+  {
     key: 'LegacyTextHitTestTest',
     module: require('../examples-win/LegacyTests/TextHitTestPage'),
   },

packages/e2e-test-app/test/LegacySelectableTextTest.test.ts
@@ -1,0 +1,62 @@
+/**
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT License.
+ *
+ * @format
+ */
+
+import {app} from '@react-native-windows/automation';
+import {dumpVisualTree} from '@react-native-windows/automation-commands';
+import {goToComponentExample} from './RNTesterNavigation';
+
+beforeAll(async () => {
+  await goToComponentExample('LegacySelectableTextTest');
+});
+
+describe('LegacySelectableTextTest', () => {
+  beforeEach(async () => {
+    await clearState();
+  });
+
+  test('PressableWhenNotSelectable', async () => {
+    const textExample = await app.findElementByTestID('text-example');
+    await textExample.click();
+    const dump = await dumpVisualTree('pressed-state');
+    expect(dump).toMatchSnapshot();
+  });
+
+  test('DoubleClickWhenNotSelectable', async () => {
+    const textExample = await app.findElementByTestID('text-example');
+    await textExample.doubleClick();
+    const dump = await dumpVisualTree('pressed-state');
+    expect(dump).toMatchSnapshot();
+  });
+
+  test('PressableWhenSelectable', async () => {
+    await toggleSelectable();
+    const textExample = await app.findElementByTestID('text-example');
+    await textExample.click();
+    const dump = await dumpVisualTree('pressed-state');
+    expect(dump).toMatchSnapshot();
+  });
+
+  test('DoubleClickWhenSelectable', async () => {
+    await toggleSelectable();
+    const textExample = await app.findElementByTestID('text-example');
+    await textExample.doubleClick();
+    const dump = await dumpVisualTree('pressed-state');
+    expect(dump).toMatchSnapshot();
+  });
+});
+
+async function clearState() {
+  const clearButton = await app.findElementByTestID('clear-state-button');
+  await clearButton.click();
+}
+
+async function toggleSelectable() {
+  const toggleButton = await app.findElementByTestID(
+    'toggle-selectable-button',
+  );
+  await toggleButton.click();
+}

packages/e2e-test-app/test/__snapshots__/LegacySelectableTextTest.test.ts.snap
@@ -1,0 +1,81 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`LegacySelectableTextTest DoubleClickWhenNotSelectable 1`] = `
+Object {
+  "AutomationId": "pressed-state",
+  "Clip": null,
+  "FlowDirection": "LeftToRight",
+  "Foreground": "#E4000000",
+  "Height": 19,
+  "HorizontalAlignment": "Stretch",
+  "Left": 0,
+  "Margin": "0,0,0,0",
+  "Padding": "0,0,0,0",
+  "Text": "Pressed: 2 times.",
+  "Top": 0,
+  "VerticalAlignment": "Stretch",
+  "Visibility": "Visible",
+  "Width": 103,
+  "XamlType": "Windows.UI.Xaml.Controls.TextBlock",
+}
+`;
+
+exports[`LegacySelectableTextTest DoubleClickWhenSelectable 1`] = `
+Object {
+  "AutomationId": "pressed-state",
+  "Clip": null,
+  "FlowDirection": "LeftToRight",
+  "Foreground": "#E4000000",
+  "Height": 19,
+  "HorizontalAlignment": "Stretch",
+  "Left": 0,
+  "Margin": "0,0,0,0",
+  "Padding": "0,0,0,0",
+  "Text": "Pressed: 1 times.",
+  "Top": 0,
+  "VerticalAlignment": "Stretch",
+  "Visibility": "Visible",
+  "Width": 103,
+  "XamlType": "Windows.UI.Xaml.Controls.TextBlock",
+}
+`;
+
+exports[`LegacySelectableTextTest PressableWhenNotSelectable 1`] = `
+Object {
+  "AutomationId": "pressed-state",
+  "Clip": null,
+  "FlowDirection": "LeftToRight",
+  "Foreground": "#E4000000",
+  "Height": 19,
+  "HorizontalAlignment": "Stretch",
+  "Left": 0,
+  "Margin": "0,0,0,0",
+  "Padding": "0,0,0,0",
+  "Text": "Pressed: 1 times.",
+  "Top": 0,
+  "VerticalAlignment": "Stretch",
+  "Visibility": "Visible",
+  "Width": 103,
+  "XamlType": "Windows.UI.Xaml.Controls.TextBlock",
+}
+`;
+
+exports[`LegacySelectableTextTest PressableWhenSelectable 1`] = `
+Object {
+  "AutomationId": "pressed-state",
+  "Clip": null,
+  "FlowDirection": "LeftToRight",
+  "Foreground": "#E4000000",
+  "Height": 19,
+  "HorizontalAlignment": "Stretch",
+  "Left": 0,
+  "Margin": "0,0,0,0",
+  "Padding": "0,0,0,0",
+  "Text": "Pressed: 1 times.",
+  "Top": 0,
+  "VerticalAlignment": "Stretch",
+  "Visibility": "Visible",
+  "Width": 103,
+  "XamlType": "Windows.UI.Xaml.Controls.TextBlock",
+}
+`;

vnext/Microsoft.ReactNative/ReactPointerEventArgs.cpp
@@ -15,6 +15,12 @@ PointerEventKind ReactPointerEventArgs::Kind() const noexcept {
   return m_kind;
 }
 
+void ReactPointerEventArgs::Kind(PointerEventKind kind) noexcept {
+  // The only event type change that is supported is CaptureLost to End.
+  assert(kind == PointerEventKind::End && m_kind == PointerEventKind::CaptureLost);
+  m_kind = kind;
+}
+
 winrt::IInspectable ReactPointerEventArgs::Target() const noexcept {
   return m_target;
 }

vnext/Microsoft.ReactNative/ReactPointerEventArgs.h
@@ -11,6 +11,7 @@ struct ReactPointerEventArgs : ReactPointerEventArgsT<ReactPointerEventArgs> {
   xaml::Input::PointerRoutedEventArgs Args() const noexcept;
 
   PointerEventKind Kind() const noexcept;
+  void Kind(PointerEventKind kind) noexcept;
 
   winrt::IInspectable Target() const noexcept;
   void Target(winrt::IInspectable const &target) noexcept;

vnext/Microsoft.ReactNative/ReactPointerEventArgs.idl
@@ -48,9 +48,15 @@ namespace Microsoft.ReactNative {
     XAML_NAMESPACE.Input.PointerRoutedEventArgs Args {
       get;
     };
-    DOC_STRING("Gets the pointer event kind.")
+    DOC_STRING(
+      "Gets or sets the pointer event kind. The only valid override is "
+      "@PointerEventKind.CaptureLost to @PointerEventKind.End to handle cases "
+      "where PointerCaptureLost events on ReactRootView can be safely treated "
+      "as PointerReleased events, e.g., for pointer events on selectable text."
+    )
     PointerEventKind Kind {
       get;
+      set;
     };
     DOC_STRING("Gets or sets the React target for the pointer event.")
     Object Target {

commit 9ac254a659c10a9d3b44e5f45186130aabb5a4ea

Fixes bug introduced by e7735a8
In order to properly set the `pageX` and `pageY` values in
`TouchEventHandler`, we need to ensure the root view is set, which is
used to calculate these properties. By default, the root view is not
provided to the `TouchEventHandler` so we should treat the supplied
`xamlView` argument as the root view.

vnext/Microsoft.ReactNative/Views/TouchEventHandler.cpp
@@ -55,7 +55,7 @@ void TouchEventHandler::AddTouchHandlers(XamlView xamlView, XamlView rootView, b
   RemoveTouchHandlers();
 
   m_xamlView = xamlView;
-  m_rootView = rootView;
+  m_rootView = rootView != nullptr ? rootView : xamlView;
   m_pressedHandler = winrt::box_value(winrt::PointerEventHandler{this, &TouchEventHandler::OnPointerPressed});
   m_releasedHandler = winrt::box_value(winrt::PointerEventHandler{this, &TouchEventHandler::OnPointerReleased});
   m_canceledHandler = winrt::box_value(winrt::PointerEventHandler{this, &TouchEventHandler::OnPointerCanceled});