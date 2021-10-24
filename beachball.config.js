3d191bf9794bf19869676de1558760f502650aca
Nick Gerleman <ngerlem@microsoft.com>
2021-10-24 02:01:05 -0700

    Stamp RNW Version into MSBuild Props (#8772)
    * Stamp RNW Version into MSBuild Props
    
    This change allows community modules to query against the RNW version used at compile-time. This creates a foundation for conditional compilation against new features/APIs/quirks.
    
    In order to calculate the new version number, existing DLL stamping code is run after the NPM package is published. Because we want to insert the version into the NPM package, we need to hook-in earlier. We use beachball prePublish hook as a mechanism to do this,. For clarity, we also commit the generated version to the repo, alongside the change to update package.json.
    
    Using beachball hooks requires us moving our root beachball config from our packge.json to a JS file. We previously mutated beachball args in the package.json, so we need to rewrite some of the code which generates beachball config.
    
    Props source of truth is a generated PropertySheet, exposed to internal and external projects. Preprocessor definitions mirroring the props are exposed by default to C++ apps/libs via external property sheets in the npm package.
    
    Stamping is done via a JS script and mustache templates (ala generator). This should make it easy to stamp versions into more file formats in the future (E.g. for runtime queries in C++/C#).
    
    * ordering
    
    * Add macros
    
    * Update beachball.config.js
    
    * yarn format
    
    * fiz
    
    * Update vnext/template/cpp-lib/src/pch.h
    
    * formatting and git
    
    * fix comparison
    
    * Use existing commit message for familiarity
    
    * Update TypeScript in package.json
    
    * parens and subsequent `yarn format`
    
    * inverted quotes

---
+++ beachball.config.js
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT License.
+ * @format
+ * @ts-check
+ */
+
+const {execSync} = require('child_process');
+
+module.exports = {
+  ...require('@rnw-scripts/generated-beachball-config'),
+  
+  // Do not generate tags for monorepo packages by default, to avoid a GitHub
+  // release for every package.
+  gitTags: false,
+
+  // Commit message used when publishing
+  message: 'applying package updates ***NO_CI***',
+
+  hooks: {
+    // Stamp versions when we publish a new package
+    prepublish: (_packagePath, name, version) => {
+      if (name === 'react-native-windows') {
+        console.log(`Stamping RNW Version ${version}`);
+
+        // prepublish is run before bumping or commiting. Append an additional
+        // commit to check-in stamped versions when we push.
+        
+        execSilent(`yarn stamp-version ${version}`);
+        execSilent('git add --all');
+        execSilent(`git commit --message "Stamp RNW ${version}"`);
+      }
+    }
+  }
+}
+
+function execSilent(command) {
+  execSync(command, {stdio: 'pipe'});
+}
