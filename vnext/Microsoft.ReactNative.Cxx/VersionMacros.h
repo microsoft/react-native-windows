59260716e89c413017b3c3075b78e07b5264c0d6
Nick Gerleman <ngerlem@microsoft.com>
2021-10-24 00:59:55 -0700

    parens and subsequent `yarn format`

--- vnext/Microsoft.ReactNative.Cxx/VersionMacros.h
+++ vnext/Microsoft.ReactNative.Cxx/VersionMacros.h
@@ -8,12 +8,12 @@
 #define RNW_VERSION_LESS_THAN(major, minor, patch) false
 #else
 
-#define RNW_VERSION_AT_LEAST(major, minor, patch)                     \
-  (RNW_MAJOR > major) || (RNW_MAJOR == major && RNW_MINOR > minor) || \
-      (RNW_MAJOR == major && RNW_MINOR == minor && RNW_PATCH >= patch)
+#define RNW_VERSION_AT_LEAST(major, minor, patch)                      \
+  ((RNW_MAJOR > major) || (RNW_MAJOR == major && RNW_MINOR > minor) || \
+   (RNW_MAJOR == major && RNW_MINOR == minor && RNW_PATCH >= patch))
 
-#define RNW_VERSION_LESS_THAN(major, minor, patch)                    \
-  (RNW_MAJOR < major) || (RNW_MAJOR == major && RNW_MINOR < minor) || \
-      (RNW_MAJOR == major && RNW_MINOR == minor && RNW_PATCH < patch)
+#define RNW_VERSION_LESS_THAN(major, minor, patch)                     \
+  ((RNW_MAJOR < major) || (RNW_MAJOR == major && RNW_MINOR < minor) || \
+   (RNW_MAJOR == major && RNW_MINOR == minor && RNW_PATCH < patch))
 
 #endif
