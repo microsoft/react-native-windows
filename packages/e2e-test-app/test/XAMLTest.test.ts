216c8ea2bddf2beffca8be1fb732b28b44ca1073
Alexander Sklar <asklar@microsoft.com>
2021-11-06 16:11:00 -0700

    add test for isOpen property and eventing

--- packages/e2e-test-app/test/XAMLTest.test.ts
+++ packages/e2e-test-app/test/XAMLTest.test.ts
@@ -7,6 +7,7 @@
 
 import {dumpVisualTree} from '@react-native-windows/automation-commands';
 import {goToComponentExample} from './RNTesterNavigation';
+import {app} from '@react-native-windows/automation';
 
 beforeAll(async () => {
   await goToComponentExample('XAML');
@@ -16,5 +17,17 @@ describe('XamlTest', () => {
   test('Layout', async () => {
     const dump = await dumpVisualTree('ReactNativeXAMLRoot');
     expect(dump).toMatchSnapshot();
+  });
+  test('MenuFlyoutIsOpen', async () => {
+    const tappableTextBlock = await app.findElementByTestID(
+      'tappableTextBlock',
+    );
+    await tappableTextBlock.click();
+    const dump = await dumpVisualTree('MenuFlyout');
+    expect(dump).toMatchSnapshot('ReactNativeXAML-MenuFlyout');
+    const menuOption2 = await app.findElementByTestID('menuOption2');
+    await menuOption2.click();
+    const text = await tappableTextBlock.getText();
+    expect(text).toBe('menu option 2 clicked');
   });
 });
