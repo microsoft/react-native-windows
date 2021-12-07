617e04ff4e018981423c64a1d14e7790ad0260ad
Chiara Mooney <34109996+chiaramooney@users.noreply.github.com>
2021-12-07 09:38:28 -0800

    Adjust Line Endings

--- packages/@react-native/tester/NativeComponentExample/js/MyNativeView.js
+++ packages/@react-native/tester/NativeComponentExample/js/MyNativeView.js
@@ -8,55 +8,55 @@
  * @format
  */
 
- import * as React from 'react';
- import {useRef, useState} from 'react';
- import {View, Button} from 'react-native';
- import RNTMyNativeView, {
-   Commands as RNTMyNativeViewCommands,
- } from './MyNativeViewNativeComponent';
- import type {MyNativeViewType} from './MyNativeViewNativeComponent';
- 
- const colors = [
-   '#0000FF',
-   '#FF0000',
-   '#00FF00',
-   '#003300',
-   '#330000',
-   '#000033',
- ];
- 
- // This is an example component that migrates to use the new architecture.
- export default function MyNativeView(props: {}): React.Node {
-   const ref = useRef<React.ElementRef<MyNativeViewType> | null>(null);
-   const [opacity, setOpacity] = useState(1.0);
-   return (
-     <View style={{flex: 1}}>
-       <RNTMyNativeView ref={ref} style={{flex: 1}} opacity={opacity} />
-       <Button
-         title="Change Background"
-         onPress={() => {
-           if (ref.current) {
-             RNTMyNativeViewCommands.callNativeMethodToChangeBackgroundColor(
-               ref.current,
-               colors[Math.floor(Math.random() * 5)],
-             );
-           }
-         }}
-       />
-       <Button
-         title="Set Opacity"
-         onPress={() => {
-           setOpacity(Math.random());
-         }}
-       />
-       <Button
-         title="Console.log Measure"
-         onPress={() => {
-           ref.current?.measure((x, y, width, height) => {
-             console.log(x, y, width, height);
-           });
-         }}
-       />
-     </View>
-   );
- }
+import * as React from 'react';
+import {useRef, useState} from 'react';
+import {View, Button} from 'react-native';
+import RNTMyNativeView, {
+  Commands as RNTMyNativeViewCommands,
+} from './MyNativeViewNativeComponent';
+import type {MyNativeViewType} from './MyNativeViewNativeComponent';
+
+const colors = [
+  '#0000FF',
+  '#FF0000',
+  '#00FF00',
+  '#003300',
+  '#330000',
+  '#000033',
+];
+
+// This is an example component that migrates to use the new architecture.
+export default function MyNativeView(props: {}): React.Node {
+  const ref = useRef<React.ElementRef<MyNativeViewType> | null>(null);
+  const [opacity, setOpacity] = useState(1.0);
+  return (
+    <View style={{flex: 1}}>
+      <RNTMyNativeView ref={ref} style={{flex: 1}} opacity={opacity} />
+      <Button
+        title="Change Background"
+        onPress={() => {
+          if (ref.current) {
+            RNTMyNativeViewCommands.callNativeMethodToChangeBackgroundColor(
+              ref.current,
+              colors[Math.floor(Math.random() * 5)],
+            );
+          }
+        }}
+      />
+      <Button
+        title="Set Opacity"
+        onPress={() => {
+          setOpacity(Math.random());
+        }}
+      />
+      <Button
+        title="Console.log Measure"
+        onPress={() => {
+          ref.current?.measure((x, y, width, height) => {
+            console.log(x, y, width, height);
+          });
+        }}
+      />
+    </View>
+  );
+}
--- packages/@react-native/tester/NativeComponentExample/js/MyNativeViewNativeComponent.js
+++ packages/@react-native/tester/NativeComponentExample/js/MyNativeViewNativeComponent.js
@@ -8,31 +8,31 @@
  * @format
  */
 
- import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
- import codegenNativeCommands from 'react-native/Libraries/Utilities/codegenNativeCommands';
- import type {HostComponent} from 'react-native/Libraries/Renderer/shims/ReactNativeTypes';
- import type {ViewProps} from 'react-native/Libraries/Components/View/ViewPropTypes';
- import type {Float} from 'react-native/Libraries/Types/CodegenTypes';
- import * as React from 'react';
- 
- type NativeProps = $ReadOnly<{|
-   ...ViewProps,
-   opacity?: Float,
- |}>;
- 
- export type MyNativeViewType = HostComponent<NativeProps>;
- 
- interface NativeCommands {
-   +callNativeMethodToChangeBackgroundColor: (
-     viewRef: React.ElementRef<MyNativeViewType>,
-     color: string,
-   ) => void;
- }
- 
- export const Commands: NativeCommands = codegenNativeCommands<NativeCommands>({
-   supportedCommands: ['callNativeMethodToChangeBackgroundColor'],
- });
- 
- export default (codegenNativeComponent<NativeProps>(
-   'RNTMyNativeView',
- ): MyNativeViewType);
+import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
+import codegenNativeCommands from 'react-native/Libraries/Utilities/codegenNativeCommands';
+import type {HostComponent} from 'react-native/Libraries/Renderer/shims/ReactNativeTypes';
+import type {ViewProps} from 'react-native/Libraries/Components/View/ViewPropTypes';
+import type {Float} from 'react-native/Libraries/Types/CodegenTypes';
+import * as React from 'react';
+
+type NativeProps = $ReadOnly<{|
+  ...ViewProps,
+  opacity?: Float,
+|}>;
+
+export type MyNativeViewType = HostComponent<NativeProps>;
+
+interface NativeCommands {
+  +callNativeMethodToChangeBackgroundColor: (
+    viewRef: React.ElementRef<MyNativeViewType>,
+    color: string,
+  ) => void;
+}
+
+export const Commands: NativeCommands = codegenNativeCommands<NativeCommands>({
+  supportedCommands: ['callNativeMethodToChangeBackgroundColor'],
+});
+
+export default (codegenNativeComponent<NativeProps>(
+  'RNTMyNativeView',
+): MyNativeViewType);
