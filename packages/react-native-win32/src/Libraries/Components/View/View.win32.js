acfb47a7924f4d4e34e020838423bbbab026d670
Saad Najmi <saadnajmi2@gmail.com>
2022-01-10 13:17:57 -0600

    [0.64] Port windows fork of Pressable to win32 (#9350)
    * Port windows fork of Pressable to win32 (#9132)
    
    * Port windows Pressable override to win32
    
    * Change files
    
    * Add override
    
    * lint fix
    
    * More overrides, more windows comments
    
    * Change files
    
    * fix overrides
    
    * Add win32 to HoverState
    
    * Overrride types maybe?
    
    * Add keyboarding types to View.win32.js
    
    * override ViewPropTypes.win32.js
    
    * fix flowconfig
    
    * redo overrides from windows fork
    
    * yarn lint:fix + some lines I forgot to copy
    
    * move to other path
    
    * validate-overrides
    
    * yarn lint:fix again
    
    * forgot some props
    
    * move one more file
    
    * lint:fix once more

--- packages/react-native-win32/src/Libraries/Components/View/View.win32.js
+++ packages/react-native-win32/src/Libraries/Components/View/View.win32.js
@@ -15,7 +15,6 @@ import type {ViewProps} from './ViewPropTypes';
 const React = require('react');
 import ViewNativeComponent from './ViewNativeComponent';
 const TextAncestor = require('../../Text/TextAncestor');
-import warnOnce from '../../Utilities/warnOnce'; // [Windows]
 import invariant from 'invariant'; // [Windows]
 
 export type Props = ViewProps;
@@ -31,16 +30,14 @@ const View: React.AbstractComponent<
   ViewProps,
   React.ElementRef<typeof ViewNativeComponent>,
 > = React.forwardRef((props: ViewProps, forwardedRef) => {
-  // [Win32 Intercept props to warn about them going away
+  // [Windows
+  invariant(
+    // $FlowFixMe Wanting to catch untyped usages
+    !props || props.acceptsKeyboardFocus === undefined,
+    'Support for the "acceptsKeyboardFocus" property has been removed in favor of "focusable"',
+  );
+  // Windows]
 
-  // $FlowFixMe react-native-win32 doesn't have forked types in Flow yet
-  if (props.acceptsKeyboardFocus !== undefined) {
-    warnOnce(
-      'deprecated-acceptsKeyboardFocus',
-      '"acceptsKeyboardFocus" has been deprecated in favor of "focusable" and will be removed soon',
-    );
-  }
-  // Win32]
   return (
     // [Windows
     // In core this is a TextAncestor.Provider value={false} See
@@ -52,10 +49,10 @@ const View: React.AbstractComponent<
           !hasTextAncestor,
           'Nesting of <View> within <Text> is not currently supported.',
         );
-
         return <ViewNativeComponent {...props} ref={forwardedRef} />;
       }}
     </TextAncestor.Consumer>
+    // Windows]
   );
 });
 
