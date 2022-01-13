5eec6a528ffd17c6dd8dd6d056919c41334fd4b0
Julio C. Rocha <julio.rocha@microsoft.com>
2022-01-13 02:36:18 -0800

    Declare IWebSocketModuleProxy

---
+++ vnext/Shared/IWebSocketModuleProxy.h
@@ -0,0 +1,24 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#pragma once
+
+// Standard Library
+#include <memory>
+#include <string>
+
+namespace Microsoft::React {
+
+/// <summary>
+/// Provides partial access to WebSocketModule methods directly to other native modules
+/// without switching to the JavaScript queue thread.
+/// </summary>
+struct IWebSocketModuleProxy {
+  static std::weak_ptr<IWebSocketModuleProxy> GetInstance() noexcept;
+
+  virtual ~IWebSocketModuleProxy() noexcept {}
+
+  virtual void SendBinary(std::string &&base64String, int64_t id) noexcept = 0;
+};
+
+} // namespace Microsoft::React
