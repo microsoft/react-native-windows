9fca66f33839400fe1c162100b14a0294c923fc4
Anandraj <anandrag@microsoft.com>
2022-01-24 20:59:29 -0800

    Fixing build break in Desktop integration tests

--- vnext/Desktop.IntegrationTests/ChakraRuntimeHolder.h
+++ vnext/Desktop.IntegrationTests/ChakraRuntimeHolder.h
@@ -9,10 +9,10 @@
 
 namespace Microsoft::React::Test {
 
-class ChakraRuntimeHolder : public facebook::jsi::RuntimeHolderLazyInit {
+class ChakraRuntimeHolder : public Microsoft::JSI::RuntimeHolderLazyInit {
  public:
   std::shared_ptr<facebook::jsi::Runtime> getRuntime() noexcept override;
-  RuntimeType getRuntimeType() noexcept override;
+  facebook::react::JSIEngineOverride getRuntimeType() noexcept override;
 
   ChakraRuntimeHolder(
       std::shared_ptr<facebook::react::DevSettings> devSettings,
