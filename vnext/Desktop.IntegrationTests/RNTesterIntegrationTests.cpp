61a2f06c0d48109540733a55e6928fa3d0ca0155
Julio C. Rocha <julio.rocha@microsoft.com>
2022-02-07 00:25:44 -0800

    Using SetRtOpt in InitModule

--- vnext/Desktop.IntegrationTests/RNTesterIntegrationTests.cpp
+++ vnext/Desktop.IntegrationTests/RNTesterIntegrationTests.cpp
@@ -23,12 +23,14 @@ std::wstring ToString<TestStatus>(const TestStatus &status) {
 } // namespace Microsoft::VisualStudio::CppUnitTestFramework
 
 TEST_MODULE_INITIALIZE(InitModule) {
-  Microsoft::React::SetRuntimeOptionBool("WebSocket.AcceptSelfSigned", true);
-  Microsoft::React::SetRuntimeOptionBool("UseBeastWebSocket", false);
-  Microsoft::React::SetRuntimeOptionBool("Http.UseResourcedModule", true);
+  using Microsoft::React::SetRuntimeOptionBool;
+
+  SetRuntimeOptionBool("WebSocket.AcceptSelfSigned", true);
+  SetRuntimeOptionBool("UseBeastWebSocket", false);
+  SetRuntimeOptionBool("Http.UseResourcedModule", true);
 
   // WebSocketJSExecutor can't register native log hooks.
-  Microsoft::React::SetRuntimeOptionBool("RNTester.UseWebDebugger", false);
+  SetRuntimeOptionBool("RNTester.UseWebDebugger", false);
 }
 
 TEST_CLASS (RNTesterIntegrationTests) {
