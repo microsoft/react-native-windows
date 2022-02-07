923f55d528c6e6797d44ae9c761b4d6585f8b705
Nick Gerleman <ngerlem@microsoft.com>
2022-02-07 03:53:13 -0800

    Fix bug around missing packages

--- beachball.config.js
+++ beachball.config.js
@@ -46,8 +46,8 @@ function correctChangeType(changeInfo) {
 }
 
 function isPrerelease(packageName) {
-  const {version} = findRepoPackageSync(packageName)?.json;
-  return version && version.includes('-');
+  const packageJson = findRepoPackageSync(packageName)?.json;
+  return packageJson && packageJson.version.includes('-');
 }
 
 function formatComment(comment) {
