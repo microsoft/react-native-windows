38f594ce17f85f19689c6def20039c9b4fdbb734
Nick Gerleman <ngerlem@microsoft.com>
2022-02-12 07:35:04 -0800

    Add a diagnostic

--- beachball.config.js
+++ beachball.config.js
@@ -1,1 +1,8 @@
-module.exports = require("@rnw-scripts/beachball-config");
+try {
+  module.exports = require("@rnw-scripts/beachball-config");
+} catch (ex) {
+  if (ex.code === 'MODULE_NOT_FOUND') {
+    console.error('Please run `yarn build` before running `yarn change`.');
+    process.exit(1);
+  }
+}
