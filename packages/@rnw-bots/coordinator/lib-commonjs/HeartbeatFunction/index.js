dfc6ca9c55b67a3d2186884d0e1110c6d4690a97
Nick Gerleman <ngerlem@microsoft.com>
2022-02-12 07:11:21 -0800

    Add beachball change transformer  (#9488)
    * Add beachball change transformer
    
    Beachball has a relatively recent option, which allows modifying changefiles before trying to bump/publish. We can use this to workaround two common painpoints.
    
    1. Changes backported to 0.69 and later will not need any modification to changefiles when cherry-picking.
    2. We do some formatting to create consistency of omiting the version in change messages in our changelogs
    
    * Remove stale file
    
    * Change files
    
    * Fix bug around missing packages
    
    * Fuller JS env in setup
    
    * Simplify logic
    
    * Fix template path
    
    * use remote midgard-yarn on ubuntu agent
    
    * yaml
    
    * Build more
    
    * Move beachball config to its own package
    
    * Consistency
    
    * Cleanup yarn install logic for hosted vs managed images
    
    * Fixup lockfile issues
    
    * Update min to node 14
    
    * Import shared variables and rename
    
    * Variable fixup

--- packages/@rnw-bots/coordinator/lib-commonjs/HeartbeatFunction/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = async (context) => {
-    context.log('Hello world');
-};
-//# sourceMappingURL=index.js.map
