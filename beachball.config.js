8d75c55291b56681caf3fddd983251a4472ad905
Nick Gerleman <ngerlem@microsoft.com>
2022-02-12 10:02:17 -0800

    Fix beachball check

--- beachball.config.js
+++ beachball.config.js
@@ -2,7 +2,9 @@ try {
   module.exports = require("@rnw-scripts/beachball-config");
 } catch (ex) {
   if (ex.code === 'MODULE_NOT_FOUND') {
-    console.error('Please run `yarn build` before running `yarn change`.');
+    console.error(ex);
+    console.error('Please be sure to run `yarn build` before running `yarn change`.');
     process.exit(1);
   }
+  throw ex;
 }
