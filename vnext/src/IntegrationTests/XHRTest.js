366bfcc1ad38968bcee3987116c19c07f3d9b9d3
Julio C. Rocha <julio.rocha@microsoft.com>
2022-02-12 04:34:27 -0800

    Update XHR test

--- vnext/src/IntegrationTests/XHRTest.js
+++ vnext/src/IntegrationTests/XHRTest.js
@@ -14,120 +14,42 @@ const {AppRegistry, View} = ReactNative;
 const {TestModule} = ReactNative.NativeModules;
 
 type State = {
-  done: ?number,
+  statusCode: ?number,
   xhr:  ?XMLHttpRequest,
 };
 
 class XHRTest extends React.Component<{...}, State> {
-  //state: Object = {
-  //  downloading: false,
-  //  // set by onreadystatechange
-  //  contentLength: 1,
-  //  responseLength: 0,
-  //  // set by onprogress
-  //  progressTotal: 1,
-  //  progressLoaded: 0,
-
-  //  readystateHandler: false,
-  //  progressHandler: true,
-  //  arraybuffer: false,
-  //};
 
   state: State = {
-    done: 0,
+    statusCode: 0,
     xhr: null,
   };
 
-  //xhr: ?XMLHttpRequest = null;
-
-  //_download = () => {
-  //  return new Promise((resolve, reject) => {
-  //    let xhr;
-  //    if (this.xhr) {
-  //      xhr = this.xhr;
-  //      xhr.abort();
-  //    } else {
-  //      xhr = this.xhr = new XMLHttpRequest();
-  //    }
-
-  //    const onreadystatechange = () => {
-  //      if (xhr.readyState === xhr.HEADERS_RECEIVED) {
-  //        const contentLength = parseInt(
-  //          xhr.getResponseHeader('Content-Length'),
-  //          10,
-  //        );
-  //        this.setState({
-  //          contentLength,
-  //          responseLength: 0,
-  //        });
-  //      } else if (xhr.readyState === xhr.LOADING && xhr.response) {
-  //        this.setState({
-  //          responseLength: xhr.response.length,
-  //        });
-  //      }
-  //    };
-
-  //    if (this.state.readystateHandler) {
-  //      xhr.onreadystatechange = onreadystatechange;
-  //    }
-  //    //ISS:2306365 - Uncomment when native module is complete.
-  //    //if (this.state.progressHandler) {
-  //    //  xhr.onprogress = onprogress;
-  //    //}
-  //    if (this.state.arraybuffer) {
-  //      xhr.responseType = 'arraybuffer';
-  //    }
-  //    xhr.onload = () => {
-  //      this.setState({downloading: false});
-  //      if (xhr.status === 200) {
-  //        let responseType = `Response is a string, ${xhr.response.length} characters long.`;
-  //        if (xhr.response instanceof ArrayBuffer) {
-  //          responseType = `Response is an ArrayBuffer, ${xhr.response.byteLength} bytes long.`;
-  //        }
-  //        console.log('Download complete!', responseType);
-  //      } else if (xhr.status !== 0) {
-  //        console.error(
-  //          'Error',
-  //          `Server returned HTTP status of ${xhr.status}: ${xhr.responseText}`,
-  //        );
-  //      } else {
-  //        console.error('Error', xhr.responseText);
-  //      }
-  //    };
-  //    xhr.open('GET', 'https://en.wikipedia.org/favicon.ico');
-  //    // Avoid gzip so we can actually show progress
-  //    xhr.setRequestHeader('Accept-Encoding', '');
-  //    xhr.send();
-
-  //    this.setState({downloading: true});
-  //  });
-  //};
-
-  _do = () => {
-    this.xhr.onload = () => {
-      console.log('onload');
-
-      this.setState({
-        done: 1
-      });
-    };
-    this.xhr.open('GET', 'https://raw.githubusercontent.com/microsoft/react-native-windows/react-native-windows_v0.67.1/NuGet.Config');
-    this.xhr.setRequestHeader('Accept-Encoding', '');
-    this.xhr.send();
-  };
-
+  //TODO: Move to a new JS test.
   _fetch = () => {
-    fetch('https://raw.githubusercontent.com/microsoft/react-native-windows/react-native-windows_v0.67.1/NuGet.Config').then(res => {
+    fetch('SOME_URL').then(res => {
       console.log('fetched');
       return res.text();
     }).then(text => {
-      console.log('textd');
+      console.log('texted');
     });
   };
 
-  _done = () => {
-    console.log(`_done ${this.state.done}`);
-    return this.state.done === 1;
+  _get = () => {
+    this.xhr = new XMLHttpRequest();
+    this.xhr.onloadend = () => {
+      this.setState({
+        statusCode: this.xhr.status,
+      });
+    };
+    this.xhr.open('GET', 'https://raw.githubusercontent.com/microsoft/react-native-windows/react-native-windows_v0.67.1/NuGet.Config');
+    this.xhr.setRequestHeader('Accept-Encoding', 'utf-8');
+    this.xhr.send();
+  };
+
+  _getSucceeded = () => {
+    console.log(`_getSucceeded [${this.state.statusCode}],[${this.xhr.responseText.length}]`);
+    return this.state.statusCode === 200 && this.xhr.responseText.length === 387;
   };
 
   _waitFor = (condition: any, timeout: any, callback: any) => {
@@ -148,10 +70,8 @@ class XHRTest extends React.Component<{...}, State> {
   };
 
   componentDidMount() {
-    this.xhr = new XMLHttpRequest();
-    this._do();
-    //this._fetch();
-    this._waitFor(this._done, 5, doneSucceeded => {
+    this._get();
+    this._waitFor(this._getSucceeded, 5, (doneSucceeded) => {
       TestModule.markTestPassed(doneSucceeded);
     });
   }
