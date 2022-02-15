61fb9ca437da08f8c7275732134600625db5aa7d
Nick Gerleman <ngerlem@microsoft.com>
2022-02-15 07:14:57 -0800

    Lazy Install `sourcemap-support` when `react-native-windows` config is hooked (#9529)
    * Lazy Install `sourcemap-support` when `react-native-windows` config is hooked
    
    We ship compiled JavaScript to end-users running the CLI. We use
    "source-map-support" to install a sourcemap-aware exception handler, so that
    stack traces shown to end-users (and included in our issues), resolve
    correctly to the TS sources. Resolution is only done when an exception occurs,
    but there is some initial overhead to installing the exception handler.
    We want to do this before our code is hooked, to ensure the handler is
    installed, but doing this as part of global initialization adds a cost to
    anyone who depends on `react-native-windows`, and parses their
    `react-native.config`, regardless of whether they call the functions. We
    delay installing the support from require-time, to function invocation time,
    which should catch most cases without the penalty.
    
    This does not hook the code to get health-checks, which is not currently designed to run lazily.
    
    * Change files
    
    * Update vnext/react-native.config.js
    
    * yarn lint:fix

--- vnext/react-native.config.js
+++ vnext/react-native.config.js
@@ -1,18 +1,52 @@
-// @ts-check
-require('source-map-support').install();
+/**
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT License.
+ *
+ * @ts check
+ * @format
+ */
 
 const cli = require('@react-native-windows/cli');
 
 module.exports = {
   // **** This section defined commands and options on how to provide the Windows platform to external applications
-  commands: cli.commands,
+  commands: cli.commands.map((command) => ({
+    ...command,
+    func: lazyInstallSourcemaps(command.func),
+  })),
   platforms: {
     windows: {
       linkConfig: () => null,
-      projectConfig: cli.projectConfig,
-      dependencyConfig: cli.dependencyConfig,
+      projectConfig: lazyInstallSourcemaps(cli.projectConfig),
+      dependencyConfig: lazyInstallSourcemaps(cli.dependencyConfig),
       npmPackageName: 'react-native-windows',
     },
   },
-  healthChecks: cli.getHealthChecks()
+  get healthChecks() {
+    return cli.getHealthChecks();
+  },
 };
+
+// We ship compiled JavaScript to end-users running the CLI. We use
+// "source-map-support" to install a sourcemap-aware exception handler, so that
+// stack traces shown to end-users (and included in our issues), resolve
+// correctly to the TS sources. Resolution is only done when an exception occurs,
+// but there is some initial overhead to installing the exception handler.
+//
+// We want to do this before our code is hooked, to ensure the handler is
+// installed, but doing this as part of global initialization adds a cost to
+// anyone who depends on `react-native-windows`, and parses their
+// `react-native.config`, regardless of whether they call the functions. We
+// delay installing the support from require-time, to function invocation time,
+// which should catch most cases without the penalty.
+let sourcemapSupportInstalled = false;
+function lazyInstallSourcemaps(fn) {
+  return (...args) => {
+    if (!sourcemapSupportInstalled) {
+      require('source-map-support').install();
+      sourcemapSupportInstalled = true;
+    }
+
+    return fn(...args);
+  };
+}
