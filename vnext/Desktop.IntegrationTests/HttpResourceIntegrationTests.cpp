093f103626f7303d4b5f998457f4f8ef16e4ecbd
Julio C. Rocha <julio.rocha@microsoft.com>
2022-03-13 09:49:36 -0700

    Remove unused sample code

--- vnext/Desktop.IntegrationTests/HttpResourceIntegrationTests.cpp
+++ vnext/Desktop.IntegrationTests/HttpResourceIntegrationTests.cpp
@@ -35,172 +35,14 @@ namespace http = beast::http; // from <boost/beast/http.hpp>
 namespace net = boost::asio; // from <boost/asio.hpp>
 using tcp = boost::asio::ip::tcp; // from <boost/asio/ip/tcp.hpp>
 
-// Return a reasonable mime type based on the extension of a file.
-beast::string_view mime_type(beast::string_view path) {
-  using beast::iequals;
-  auto const ext = [&path] {
-    auto const pos = path.rfind(".");
-    if (pos == beast::string_view::npos)
-      return beast::string_view{};
-    return path.substr(pos);
-  }();
-  if (iequals(ext, ".htm"))
-    return "text/html";
-  if (iequals(ext, ".html"))
-    return "text/html";
-  if (iequals(ext, ".php"))
-    return "text/html";
-  if (iequals(ext, ".css"))
-    return "text/css";
-  if (iequals(ext, ".txt"))
-    return "text/plain";
-  if (iequals(ext, ".js"))
-    return "application/javascript";
-  if (iequals(ext, ".json"))
-    return "application/json";
-  if (iequals(ext, ".xml"))
-    return "application/xml";
-  if (iequals(ext, ".swf"))
-    return "application/x-shockwave-flash";
-  if (iequals(ext, ".flv"))
-    return "video/x-flv";
-  if (iequals(ext, ".png"))
-    return "image/png";
-  if (iequals(ext, ".jpe"))
-    return "image/jpeg";
-  if (iequals(ext, ".jpeg"))
-    return "image/jpeg";
-  if (iequals(ext, ".jpg"))
-    return "image/jpeg";
-  if (iequals(ext, ".gif"))
-    return "image/gif";
-  if (iequals(ext, ".bmp"))
-    return "image/bmp";
-  if (iequals(ext, ".ico"))
-    return "image/vnd.microsoft.icon";
-  if (iequals(ext, ".tiff"))
-    return "image/tiff";
-  if (iequals(ext, ".tif"))
-    return "image/tiff";
-  if (iequals(ext, ".svg"))
-    return "image/svg+xml";
-  if (iequals(ext, ".svgz"))
-    return "image/svg+xml";
-  return "application/text";
-}
-
-// Append an HTTP rel-path to a local filesystem path.
-// The returned path is normalized for the platform.
-std::string path_cat(beast::string_view base, beast::string_view path) {
-  if (base.empty())
-    return std::string(path);
-  std::string result(base);
-#ifdef BOOST_MSVC
-  char constexpr path_separator = '\\';
-  if (result.back() == path_separator)
-    result.resize(result.size() - 1);
-  result.append(path.data(), path.size());
-  for (auto &c : result)
-    if (c == '/')
-      c = path_separator;
-#else
-  char constexpr path_separator = '/';
-  if (result.back() == path_separator)
-    result.resize(result.size() - 1);
-  result.append(path.data(), path.size());
-#endif
-  return result;
-}
-
 // This function produces an HTTP response for the given
 // request. The type of the response object depends on the
 // contents of the request, so the interface requires the
 // caller to pass a generic lambda for receiving the response.
 template <class Body, class Allocator, class Send>
 void handle_request(
-    beast::string_view doc_root,
     http::request<Body, http::basic_fields<Allocator>> &&req,
     Send &&send) {
-  // Returns a bad request response
-  //auto const bad_request = [&req](beast::string_view why) {
-  //  http::response<http::string_body> res{http::status::bad_request, req.version()};
-  //  res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-  //  res.set(http::field::content_type, "text/html");
-  //  res.keep_alive(req.keep_alive());
-  //  res.body() = std::string(why);
-  //  res.prepare_payload();
-  //  return res;
-  //};
-
-  //// Returns a not found response
-  //auto const not_found = [&req](beast::string_view target) {
-  //  http::response<http::string_body> res{http::status::not_found, req.version()};
-  //  res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-  //  res.set(http::field::content_type, "text/html");
-  //  res.keep_alive(req.keep_alive());
-  //  res.body() = "The resource '" + std::string(target) + "' was not found.";
-  //  res.prepare_payload();
-  //  return res;
-  //};
-
-  //// Returns a server error response
-  //auto const server_error = [&req](beast::string_view what) {
-  //  http::response<http::string_body> res{http::status::internal_server_error, req.version()};
-  //  res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-  //  res.set(http::field::content_type, "text/html");
-  //  res.keep_alive(req.keep_alive());
-  //  res.body() = "An error occurred: '" + std::string(what) + "'";
-  //  res.prepare_payload();
-  //  return res;
-  //};
-
-  //// Make sure we can handle the method
-  //if (req.method() != http::verb::get && req.method() != http::verb::head)
-  //  return send(bad_request("Unknown HTTP-method"));
-
-  //// Request path must be absolute and not contain "..".
-  //if (req.target().empty() || req.target()[0] != '/' || req.target().find("..") != beast::string_view::npos)
-  //  return send(bad_request("Illegal request-target"));
-
-  //// Build the path to the requested file
-  //std::string path = path_cat(doc_root, req.target());
-  //if (req.target().back() == '/')
-  //  path.append("index.html");
-
-  //// Attempt to open the file
-  //beast::error_code ec;
-  //http::file_body::value_type body;
-  //body.open(path.c_str(), beast::file_mode::scan, ec);
-
-  //// Handle the case where the file doesn't exist
-  //if (ec == beast::errc::no_such_file_or_directory)
-  //  return send(not_found(req.target()));
-
-  //// Handle an unknown error
-  //if (ec)
-  //  return send(server_error(ec.message()));
-
-  //// Cache the size since we need it after the move
-  //auto const size = body.size();
-
-  //// Respond to HEAD request
-  //if (req.method() == http::verb::head) {
-  //  http::response<http::empty_body> res{http::status::ok, req.version()};
-  //  res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-  //  res.set(http::field::content_type, mime_type(path));
-  //  res.content_length(size);
-  //  res.keep_alive(req.keep_alive());
-  //  return send(std::move(res));
-  //}
-
-  //// Respond to GET request
-  //http::response<http::file_body> res{
-  //    std::piecewise_construct, std::make_tuple(std::move(body)), std::make_tuple(http::status::ok, req.version())};
-  //res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-  //res.set(http::field::content_type, mime_type(path));
-  //res.content_length(size);
-  //res.keep_alive(req.keep_alive());
-  //return send(std::move(res));
 
   Microsoft::React::Test::DynamicResponse res;
   res.result(http::status::ok);
@@ -244,15 +86,14 @@ class session : public std::enable_shared_from_this<session> {
 
   beast::tcp_stream stream_;
   beast::flat_buffer buffer_;
-  std::shared_ptr<std::string const> doc_root_;
   http::request<http::string_body> req_;
   std::shared_ptr<void> res_;
   send_lambda lambda_;
 
  public:
   // Take ownership of the stream
-  session(tcp::socket &&socket, std::shared_ptr<std::string const> const &doc_root)
-      : stream_(std::move(socket)), doc_root_(doc_root), lambda_(*this) {}
+  session(tcp::socket &&socket)
+      : stream_(std::move(socket)), lambda_(*this) {}
 
   // Start the asynchronous operation
   void run() {
@@ -286,7 +127,7 @@ class session : public std::enable_shared_from_this<session> {
       return fail(ec, "read");
 
     // Send the response
-    handle_request(*doc_root_, std::move(req_), lambda_);
+    handle_request(std::move(req_), lambda_);
   }
 
   void on_write(bool close, beast::error_code ec, std::size_t bytes_transferred) {
@@ -323,11 +164,10 @@ class session : public std::enable_shared_from_this<session> {
 class listener : public std::enable_shared_from_this<listener> {
   net::io_context &ioc_;
   tcp::acceptor acceptor_;
-  std::shared_ptr<std::string const> doc_root_;
 
  public:
-  listener(net::io_context &ioc, tcp::endpoint endpoint, std::shared_ptr<std::string const> const &doc_root)
-      : ioc_(ioc), acceptor_(net::make_strand(ioc)), doc_root_(doc_root) {
+  listener(net::io_context &ioc, tcp::endpoint endpoint)
+      : ioc_(ioc), acceptor_(net::make_strand(ioc)) {
     beast::error_code ec;
 
     // Open the acceptor
@@ -375,7 +215,7 @@ class listener : public std::enable_shared_from_this<listener> {
       fail(ec, "accept");
     } else {
       // Create the session and run it
-      std::make_shared<session>(std::move(socket), doc_root_)->run();
+      std::make_shared<session>(std::move(socket))->run();
     }
 
     // Accept another connection
@@ -383,38 +223,7 @@ class listener : public std::enable_shared_from_this<listener> {
   }
 };
 
-//------------------------------------------------------------------------------
-
-int mains(int argc, char *argv[]) {
-  // Check command line arguments.
-  if (argc != 5) {
-    std::cerr << "Usage: http-server-async <address> <port> <doc_root> <threads>\n"
-              << "Example:\n"
-              << "    http-server-async 0.0.0.0 8080 . 1\n";
-    return EXIT_FAILURE;
-  }
-  auto const address = net::ip::make_address(argv[1]);
-  auto const port = static_cast<unsigned short>(std::atoi(argv[2]));
-  auto const doc_root = std::make_shared<std::string>(argv[3]);
-  auto const threads = std::max<int>(1, std::atoi(argv[4]));
-
-  // The io_context is required for all I/O
-  net::io_context ioc{threads};
-
-  // Create and launch a listening port
-  std::make_shared<listener>(ioc, tcp::endpoint{address, port}, doc_root)->run();
-
-  // Run the I/O service on the requested number of threads
-  std::vector<std::thread> v;
-  v.reserve(threads - 1);
-  for (auto i = threads - 1; i > 0; --i)
-    v.emplace_back([&ioc] { ioc.run(); });
-  ioc.run();
-
-  return EXIT_SUCCESS;
-}
-
-}
+} // falco
 
 using namespace Microsoft::React;
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
@@ -432,12 +241,11 @@ TEST_CLASS (HttpResourceIntegrationTest) {
   TEST_METHOD(Vinime) {
     auto const address = boost::asio::ip::make_address("0.0.0.0");
     unsigned short const port = 5556;
-    auto const docRoot = std::make_shared<string>("nel");
     int const threadCount = 1;
 
     boost::asio::io_context ioc{threadCount};
 
-    std::make_shared<falco::listener>(ioc, boost::asio::ip::tcp::endpoint{address, port}, docRoot)->run();
+    std::make_shared<falco::listener>(ioc, boost::asio::ip::tcp::endpoint{address, port})->run();
 
     std::vector<std::thread> threads;
     threads.reserve(threadCount - 1);
