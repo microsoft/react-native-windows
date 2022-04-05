9351df40c7d976151e8b5e2257a049a6c806cd9b
Julio C. Rocha <julio.rocha@microsoft.com>
2022-04-05 14:30:13 -0700

    Add ABI-safe runtime options free functions.
    - Microsoft_React_SetRuntimeOptionBool
    - Microsoft_React_SetRuntimeOptionInt
    - Microsoft_React_SetRuntimeOptionString
    - Microsoft_React_GetRuntimeOptionBool
    - Microsoft_React_GetRuntimeOptionInt
    - Microsoft_React_GetRuntimeOptionString

--- vnext/Shared/RuntimeOptions.cpp
+++ vnext/Shared/RuntimeOptions.cpp
@@ -8,7 +8,7 @@ using std::mutex;
 using std::string;
 
 namespace {
-std::unordered_map<string, int32_t> g_runtimeOptions;
+std::unordered_map<string, int32_t> g_runtimeOptionInts;
 std::unordered_map<string, string> g_runtimeOptionStrings;
 mutex g_runtimeOptionsMutex;
 } // namespace
@@ -17,12 +17,12 @@ namespace Microsoft::React {
 
 void __cdecl SetRuntimeOptionBool(string &&name, bool value) noexcept {
   lock_guard<mutex> guard{g_runtimeOptionsMutex};
-  g_runtimeOptions.insert_or_assign(std::move(name), value ? 1 : 0);
+  g_runtimeOptionInts.insert_or_assign(std::move(name), value ? 1 : 0);
 }
 
 void __cdecl SetRuntimeOptionInt(string &&name, int32_t value) noexcept {
   lock_guard<mutex> guard{g_runtimeOptionsMutex};
-  g_runtimeOptions.insert_or_assign(std::move(name), value);
+  g_runtimeOptionInts.insert_or_assign(std::move(name), value);
 }
 
 void __cdecl SetRuntimeOptionString(const char *name, const char *value) noexcept {
@@ -38,8 +38,8 @@ void __cdecl SetRuntimeOptionString(const char *name, const char *value) noexcep
 
 const bool __cdecl GetRuntimeOptionBool(const string &name) noexcept {
   lock_guard<mutex> guard{g_runtimeOptionsMutex};
-  auto itr = g_runtimeOptions.find(name);
-  if (itr != g_runtimeOptions.end())
+  auto itr = g_runtimeOptionInts.find(name);
+  if (itr != g_runtimeOptionInts.end())
     return itr->second == 1;
 
   return false;
@@ -47,8 +47,8 @@ const bool __cdecl GetRuntimeOptionBool(const string &name) noexcept {
 
 const int32_t __cdecl GetRuntimeOptionInt(const string &name) noexcept {
   lock_guard<mutex> guard{g_runtimeOptionsMutex};
-  auto itr = g_runtimeOptions.find(name);
-  if (itr != g_runtimeOptions.end())
+  auto itr = g_runtimeOptionInts.find(name);
+  if (itr != g_runtimeOptionInts.end())
     return itr->second;
 
   return 0;
@@ -64,3 +64,69 @@ const char *__cdecl GetRuntimeOptionString(const char *name) noexcept {
 }
 
 } // namespace Microsoft::React
+
+void __cdecl Microsoft_React_SetRuntimeOptionBool(const char* name, bool value) noexcept
+{
+  if (!name)
+    return;
+
+  lock_guard<mutex> guard{g_runtimeOptionsMutex};
+  if (value)
+    g_runtimeOptionInts.insert_or_assign(name, value ? 1 : 0);
+  else
+    g_runtimeOptionInts.erase(name);
+}
+
+void __cdecl Microsoft_React_SetRuntimeOptionInt(const char* name, int32_t value) noexcept
+{
+  if (!name)
+    return;
+
+  lock_guard<mutex> guard{g_runtimeOptionsMutex};
+  if (value)
+    g_runtimeOptionInts.insert_or_assign(name, value);
+  else
+    g_runtimeOptionInts.erase(name);
+}
+
+void __cdecl Microsoft_React_SetRuntimeOptionString(const char* name, const char* value) noexcept
+{
+  if (!name)
+    return;
+
+  lock_guard<mutex> guard{g_runtimeOptionsMutex};
+  if (value)
+    g_runtimeOptionStrings.insert_or_assign(name, value);
+  else
+    g_runtimeOptionStrings.erase(name);
+}
+
+const bool __cdecl Microsoft_React_GetRuntimeOptionBool(const char* name) noexcept
+{
+  lock_guard<mutex> guard{g_runtimeOptionsMutex};
+  auto itr = g_runtimeOptionInts.find(name);
+  if (itr != g_runtimeOptionInts.end())
+    return itr->second == 1;
+
+  return false;
+}
+
+const int32_t __cdecl Microsoft_React_GetRuntimeOptionInt(const char* name) noexcept
+{
+  lock_guard<mutex> guard{g_runtimeOptionsMutex};
+  auto itr = g_runtimeOptionInts.find(name);
+  if (itr != g_runtimeOptionInts.end())
+    return itr->second;
+
+  return 0;
+}
+
+const char* __cdecl Microsoft_React_GetRuntimeOptionString(const char* name) noexcept
+{
+  lock_guard<mutex> guard{g_runtimeOptionsMutex};
+  auto itr = g_runtimeOptionStrings.find(name);
+  if (itr != g_runtimeOptionStrings.cend())
+    return itr->second.c_str();
+
+  return nullptr;
+}
--- vnext/Shared/RuntimeOptions.h
+++ vnext/Shared/RuntimeOptions.h
@@ -47,3 +47,42 @@ const std::int32_t __cdecl GetRuntimeOptionInt(const std::string &name) noexcept
 const char *__cdecl GetRuntimeOptionString(const char *name) noexcept;
 
 } // namespace Microsoft::React
+
+/// <summary>
+/// Sets a global boolean value identified by an arbitrary string.
+/// </summary>
+/// <param name="name">Global key</param>
+void __cdecl Microsoft_React_SetRuntimeOptionBool(const char *name, bool value) noexcept;
+
+/// <summary>
+/// Sets a global signed integer value identified by an arbitrary string.
+/// </summary>
+/// <param name="name">Global boolean key</param>
+void __cdecl Microsoft_React_SetRuntimeOptionInt(const char *name, int32_t value) noexcept;
+
+/// <summary>
+/// Sets a global signed integer value identified by an arbitrary string.
+/// </summary>
+/// <param name="name">Global string key</param>
+void __cdecl Microsoft_React_SetRuntimeOptionString(const char *name, const char *value) noexcept;
+
+/// <summary>
+/// Retrieves a global boolean value for the given key.
+/// </summary>
+/// <param name="name">Global boolean key</param>
+/// <returns>Value stored for the given key, or false if the entry doesn't exist (default)</returns>
+const bool __cdecl Microsoft_React_GetRuntimeOptionBool(const char *name) noexcept;
+
+/// <summary>
+/// Retrieves a global boolean value for the given key.
+/// </summary>
+/// <param name="name">Global key</param>
+/// <returns>Value stored for the given key, or 0 if the entry doesn't exist (default)</returns>
+const int32_t __cdecl Microsoft_React_GetRuntimeOptionInt(const char *name) noexcept;
+
+/// <summary>
+/// Retrieves a global string value for the given key.
+/// </summary>
+/// <param name="name">Global key</param>
+/// <returns>Value stored for the given key, or nullptr if the entry doesn't exist (default)</returns>
+const char *__cdecl Microsoft_React_GetRuntimeOptionString(const char *name) noexcept;
