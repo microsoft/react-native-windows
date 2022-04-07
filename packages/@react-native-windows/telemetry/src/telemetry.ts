b6f8fd9bcab4e7d53467e35d2bc6892aec004bd2
Jon Thysell <jthysell@microsoft.com>
2022-04-07 11:32:32 -0700

    Stop Telemetry background threads running after test completion (#9801)
    * Stop Telemetry background threads running after test completion
    
    This PR disables the ApplicationInsights (AI) internal logging. We used to enable it during our tests to get more info, but all this logging happens on a background thread which interferes with the tests exiting cleanly and clutters up the test output.
    
    This PR also removes the CorrelationIDManager workaround (to kill its background thread) since issue https://github.com/microsoft/ApplicationInsights-node.js/issues/833 was fixed and it no longer runs indefinitely.
    
    This PR also disables AI's StatsBeat - yet another source of unnecessary background threads which interfere with our tests.
    
    * Change files
    
    * lint fix
    
    * reconcile linting and test failure
    
    * bump ai explicitly

--- packages/@react-native-windows/telemetry/src/telemetry.ts
+++ packages/@react-native-windows/telemetry/src/telemetry.ts
@@ -145,10 +145,7 @@ export class Telemetry {
 
   /** Sets up Telemetry.client. */
   private static setupClient() {
-    appInsights.Configuration.setInternalLogging(
-      Telemetry.isTest,
-      Telemetry.isTest,
-    );
+    appInsights.Configuration.setInternalLogging(false, false);
 
     Telemetry.client = new appInsights.TelemetryClient(
       Telemetry.options.setupString,
@@ -162,6 +159,13 @@ export class Telemetry {
     }
 
     Telemetry.client.config.disableAppInsights = Telemetry.isTest;
+    Telemetry.client.config.disableStatsbeat = true;
+
+    // Despite trying to disable the statsbeat, it might still be running: https://github.com/microsoft/ApplicationInsights-node.js/issues/943
+    // So we want to disable it, and despite the method's typing, getStatsbeat() _can_ return undefined
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    Telemetry.client.getStatsbeat()?.enable(false);
+
     Telemetry.client.channel.setUseDiskRetryCaching(!Telemetry.isTest);
   }
 
