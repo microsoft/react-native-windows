24e70e327561ea29278afe9b9bc3c76bf4438ace
Julio C. Rocha <julio.rocha@microsoft.com>
2022-04-19 15:11:08 -0700

    Use scoped_lock for runtime options

--- vnext/Shared/RuntimeOptions.cpp
+++ vnext/Shared/RuntimeOptions.cpp
@@ -3,8 +3,8 @@
 #include <mutex>
 #include <unordered_map>
 
-using std::lock_guard;
 using std::mutex;
+using std::scoped_lock;
 using std::string;
 
 namespace {
@@ -16,17 +16,17 @@ mutex g_runtimeOptionsMutex;
 namespace Microsoft::React {
 
 void __cdecl SetRuntimeOptionBool(string &&name, bool value) noexcept {
-  lock_guard<mutex> guard{g_runtimeOptionsMutex};
+  scoped_lock lock{g_runtimeOptionsMutex};
   g_runtimeOptionInts.insert_or_assign(std::move(name), value ? 1 : 0);
 }
 
 void __cdecl SetRuntimeOptionInt(string &&name, int32_t value) noexcept {
-  lock_guard<mutex> guard{g_runtimeOptionsMutex};
+  scoped_lock lock{g_runtimeOptionsMutex};
   g_runtimeOptionInts.insert_or_assign(std::move(name), value);
 }
 
 const bool __cdecl GetRuntimeOptionBool(const string &name) noexcept {
-  lock_guard<mutex> guard{g_runtimeOptionsMutex};
+  scoped_lock lock{g_runtimeOptionsMutex};
   auto itr = g_runtimeOptionInts.find(name);
   if (itr != g_runtimeOptionInts.end())
     return itr->second == 1;
@@ -35,7 +35,7 @@ const bool __cdecl GetRuntimeOptionBool(const string &name) noexcept {
 }
 
 const int32_t __cdecl GetRuntimeOptionInt(const string &name) noexcept {
-  lock_guard<mutex> guard{g_runtimeOptionsMutex};
+  scoped_lock lock{g_runtimeOptionsMutex};
   auto itr = g_runtimeOptionInts.find(name);
   if (itr != g_runtimeOptionInts.end())
     return itr->second;
@@ -49,7 +49,7 @@ void __cdecl Microsoft_React_SetRuntimeOptionBool(const char *name, bool value) 
   if (!name)
     return;
 
-  lock_guard<mutex> guard{g_runtimeOptionsMutex};
+  scoped_lock lock{g_runtimeOptionsMutex};
   if (value)
     g_runtimeOptionInts.insert_or_assign(name, value ? 1 : 0);
   else
@@ -60,7 +60,7 @@ void __cdecl Microsoft_React_SetRuntimeOptionInt(const char *name, int32_t value
   if (!name)
     return;
 
-  lock_guard<mutex> guard{g_runtimeOptionsMutex};
+  scoped_lock lock{g_runtimeOptionsMutex};
   if (value)
     g_runtimeOptionInts.insert_or_assign(name, value);
   else
@@ -71,7 +71,7 @@ void __cdecl Microsoft_React_SetRuntimeOptionString(const char *name, const char
   if (!name)
     return;
 
-  lock_guard<mutex> guard{g_runtimeOptionsMutex};
+  scoped_lock lock{g_runtimeOptionsMutex};
   if (value)
     g_runtimeOptionStrings.insert_or_assign(name, value);
   else
@@ -79,7 +79,7 @@ void __cdecl Microsoft_React_SetRuntimeOptionString(const char *name, const char
 }
 
 const bool __cdecl Microsoft_React_GetRuntimeOptionBool(const char *name) noexcept {
-  lock_guard<mutex> guard{g_runtimeOptionsMutex};
+  scoped_lock lock{g_runtimeOptionsMutex};
   auto itr = g_runtimeOptionInts.find(name);
   if (itr != g_runtimeOptionInts.end())
     return itr->second == 1;
@@ -88,7 +88,7 @@ const bool __cdecl Microsoft_React_GetRuntimeOptionBool(const char *name) noexce
 }
 
 const int32_t __cdecl Microsoft_React_GetRuntimeOptionInt(const char *name) noexcept {
-  lock_guard<mutex> guard{g_runtimeOptionsMutex};
+  scoped_lock lock{g_runtimeOptionsMutex};
   auto itr = g_runtimeOptionInts.find(name);
   if (itr != g_runtimeOptionInts.end())
     return itr->second;
@@ -97,7 +97,7 @@ const int32_t __cdecl Microsoft_React_GetRuntimeOptionInt(const char *name) noex
 }
 
 const char *__cdecl Microsoft_React_GetRuntimeOptionString(const char *name) noexcept {
-  lock_guard<mutex> guard{g_runtimeOptionsMutex};
+  scoped_lock lock{g_runtimeOptionsMutex};
   auto itr = g_runtimeOptionStrings.find(name);
   if (itr != g_runtimeOptionStrings.cend()) {
     auto size = itr->second.size() * sizeof(char) + 1 /*NULL termination*/;
