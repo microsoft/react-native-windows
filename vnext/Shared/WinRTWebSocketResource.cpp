ff8d49642d7b81cd6a2a693fa58c4f5977f2085a
Julio César Rocha <JunielKatarn@users.noreply.github.com>
2022-04-20 21:58:45 -0700

    Handle abrupt WebSocket connection interruption (#9829)
    * Add UnplugServer test
    
    * Handle exceptions in args.GetDataReader()
    
    * Consolidate catch clauses in MessageReceived
    
    * Change files

--- vnext/Shared/WinRTWebSocketResource.cpp
+++ vnext/Shared/WinRTWebSocketResource.cpp
@@ -316,7 +316,22 @@ void WinRTWebSocketResource::Connect(string &&url, const Protocols &protocols, c
           }
         } catch (hresult_error const &e) {
           if (self->m_errorHandler) {
-            self->m_errorHandler({Utilities::HResultToString(e), ErrorType::Receive});
+            string errorMessage;
+            ErrorType errorType;
+            // See
+            // https://docs.microsoft.com/uwp/api/windows.networking.sockets.messagewebsocketmessagereceivedeventargs.getdatareader?view=winrt-19041#remarks
+            if (e.code() == WININET_E_CONNECTION_ABORTED) {
+              errorMessage = "[0x80072EFE] Underlying TCP connection suddenly terminated";
+              errorType = ErrorType::Connection;
+              self->m_errorHandler({errorMessage, errorType});
+
+              // Note: We are not clear whether all read-related errors should close the socket.
+              self->Close(CloseCode::BadPayload, std::move(errorMessage));
+            } else {
+              errorMessage = Utilities::HResultToString(e);
+              errorType = ErrorType::Receive;
+              self->m_errorHandler({errorMessage, errorType});
+            }
           }
         }
       });
