8d8d3c0bb69fc0e414993fa07c521dbecb6e4230
Chiara Mooney <34109996+chiaramooney@users.noreply.github.com>
2022-04-26 10:19:07 -0700

    Fix Lint 2

--- vnext/.eslintrc.js
+++ vnext/.eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   extends: ['@rnw-scripts'],
   parserOptions: {tsconfigRootDir : __dirname},
+  overrides: [
+    {
+      files: ['*d.ts'],
+      // Also not clean
+      rules: {
+        '@typescript-eslint/no-unused-vars': 'off',
+      },
+    },
+  ],
 };
