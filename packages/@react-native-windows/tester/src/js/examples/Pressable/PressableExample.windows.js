64d7265714137cb8a408ea5d0a17edf0e92574f7
TatianaKapos <tatianakapos@microsoft.com>
2022-05-02 16:43:27 -0700

    integrate 5-1-22

--- packages/@react-native-windows/tester/src/js/examples/Pressable/PressableExample.windows.js
+++ packages/@react-native-windows/tester/src/js/examples/Pressable/PressableExample.windows.js
@@ -20,6 +20,7 @@ import {
   View,
   Switch,
 } from 'react-native';
+import ReactNativeFeatureFlags from 'react-native/Libraries/ReactNative/ReactNativeFeatureFlags';
 
 const {useEffect, useRef, useState} = React;
 
@@ -256,6 +257,25 @@ function PressableDisabled() {
   );
 }
 
+function PressableHoverStyle() {
+  const [hovered, setHovered] = useState(false);
+  return (
+    <View style={styles.row}>
+      <Pressable
+        style={[
+          {
+            backgroundColor: hovered ? 'rgb(210, 230, 255)' : 'white',
+          },
+          styles.wrapperCustom,
+        ]}
+        onHoverIn={() => setHovered(true)}
+        onHoverOut={() => setHovered(false)}>
+        <Text style={styles.text}>Hover Me</Text>
+      </Pressable>
+    </View>
+    );
+}
+
 function PressableFocusCallbacks() {
   const [timesPressed, setTimesPressed] = useState(0);
 
@@ -475,7 +495,8 @@ exports.description = 'Component for making views pressable.';
 exports.title = 'Pressable';
 exports.category = 'UI';
 exports.documentationURL = 'https://reactnative.dev/docs/pressable';
-exports.examples = [
+
+const examples = [
   {
     title: 'Change content based on Press',
     render(): React.Node {
@@ -686,3 +707,14 @@ exports.examples = [
     },
   },
 ];
+
+if (ReactNativeFeatureFlags.shouldPressibilityUseW3CPointerEventsForHover()) {
+  examples.push({
+    title: 'Change style based on Hover',
+    render(): React.Node {
+      return <PressableHoverStyle />;
+    },
+  });
+}
+
+exports.examples = examples;
