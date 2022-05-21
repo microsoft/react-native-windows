87f358ce7a222cd74caa66a9539cbdfbdb2e6b65
Julio César Rocha <JunielKatarn@users.noreply.github.com>
2022-05-20 19:48:48 -0700

    Implement Blob module (#9352)
    * Added BlobModule and IWSModuleContHandler headers
    
    * Implement Blob module
    
    * Avoid raw self pointer in BlobModule
    
    * Implement WebSocketModule msg processing
    
    * clang format
    
    * Don't return until websocketMessage event is sent
    
    * Define CreateBlobModule()
    
    * Add DEF exports
    
    * Add Blob JS tests
    
    * Add Blob JS tests
    
    * Change files
    
    * yarn lint
    
    * Add overrides
    
    * Register BlobModule in DesktopTestRunner
    
    * Keep ignoring WebSocketBlob test by default
    
    * Add BlobModule to default modules list
    
    * Allow 'blob' responseType in HTTP module
    
    * Ensure React Instance can be accessed when using older versions of react-native
    
    * Emit error message on createFromParts failure
    
    * Remove redundant extra modules in Desktop integration tests
    
    * Declare IWebSocketModuleProxy
    
    * Remove Blob and WS module factories from DLL boundary
    
    * Implement IWebSocketModuleProxy
    
    * clang format
    
    * Update packages.lock
    
    * Use winrt::array_view directly in ResolveMessage
    
    * Define InstanceImpl::m_transitionalModuleProperties
    
    * Include CreateModules.h in projects accessing MSRN.Cxx
    
    * Define WinRT class WebSocketModuleContentHandler
    
    - Have BlobModule constructor register the content handler in transitive
      property bag CxxNativeModule/WebSocketModuleContentHandler
    
    * Have WebSocketModule use IInspectable as props arg
    
    * Use property bag instead of global singletons for blob helpers
    
    * Store blob helpers in prop bag as weak_ptr
    
    * Replace remaining lock_guard<mutex> in BlobModule
    
    * Define IUriHandler, IReqBodyHandler, IRespHandler.
    
    * IHttpResource::SendRequest - add folly::dynamic data arg
    
    * Add data arg to test SendRequest calls
    
    * First implementation for BlobModuleUriHandler
    
    * Remove WebSocketModuleContentHandler WinRT class
    
    * Implement IBlobPersistor, MemoryBlobPersistor
    
    * clang format
    
    * Update yarn.lock
    
    * Update RctRootVieTagGen location
    
    * Implement addNetworkingHandler
    
    * Fix createFromParts buffer persistence
    
    * Drop WebSocketModule s_sharedState in favor of property bag
    
    * Disable back WebSocketBlob test
    
    * Rename iProperties to inspectableProperties
    
    * Pass ReactContext properties to CreateHttpModule in InstanceWin
    
    * Remove WebSocketModule constructor from x86 DLL boundary
    
    * yarn lint
    
    * Update packages.lock
    
    * Make transitional property bag non-member
    
    * Use blobURIScheme wherever possible
    
    * Pass request content as folly::dynaic.
    
    - Pass request ID directly from JavaScript layer.
    
    * Use constexpr for folly indexes
    
    * Implement GetMimeTypeFromUri
    
    * Finish BlobModule handler implementations.
    
    * Remove unused includes
    
    * Ensure HttpModule::m_resource is set
    
    * clang format
    
    * clang format
    
    * Allow blob responseType
    
    * Use winrt::to_hstring instead of Utf8ToUtf16
    
    * Pass inspectableProperties down to WinRTHttpResource
    
    * Implement IHttpModuleProxy via WinRTHttpResource
    
    * Consume URI handler
    
    - IHttpResource
      - Rename SetOnRequest to SetOnRequestSuccess
      - Declare SetOnBlobData to pass complex (non-string) response data
    
    * Consume IRequestBodyHandler
    
    * Consume IResponseHandler
    
    * Ensure properties exist in bag before using value
    
    * Update packages lock
    
    * Add missing call to Modules::SendEvent
    
    * Fix Shared filters
    
    * Rename SetOnBlobData to SetOnData (different args)
    
    * Correctly retrieve blob slices
    
    * Correctly retrieve blob slices
    
    * Clang format
    
    * Update project filters
    
    * Drop BlobModuleUriHandler
    
    * Continue handling requests when not blob-supported
    
    * Add BlobTest
    
    * Update packages.lock.json
    
    * Define FileReaderModule
    
    * Implement FileReaderModule
    
    * Complete BlobTest
    
    * Make IBlobPersistor::ResolveMessage throw std::invalid_argument
    
    * Fail on Content-Encoding parsing even if no error handler
    
    * Remove MIME mappings. Currently unused
    
    * MemoryBlobPersistor::ResolveMessage throw on out of bounds
    
    * lint
    
    * Enable BlobTest by default
    
    * Disable Blob test in CI (may hang)

--- vnext/Microsoft.ReactNative/Base/CoreNativeModules.cpp
+++ vnext/Microsoft.ReactNative/Base/CoreNativeModules.cpp
@@ -19,93 +19,82 @@
 
 namespace Microsoft::ReactNative {
 
-using winrt::Microsoft::ReactNative::ReactPropertyBag;
+  using winrt::Microsoft::ReactNative::ReactPropertyBag;
 
-namespace {
+  namespace {
 
-using winrt::Microsoft::ReactNative::ReactPropertyId;
+    using winrt::Microsoft::ReactNative::ReactPropertyId;
 
-bool HasPackageIdentity() noexcept {
-  static const bool hasPackageIdentity = []() noexcept {
-    auto packageStatics = winrt::get_activation_factory<winrt::Windows::ApplicationModel::IPackageStatics>(
-        winrt::name_of<winrt::Windows::ApplicationModel::Package>());
-    auto abiPackageStatics = static_cast<winrt::impl::abi_t<winrt::Windows::ApplicationModel::IPackageStatics> *>(
-        winrt::get_abi(packageStatics));
-    winrt::com_ptr<winrt::impl::abi_t<winrt::Windows::ApplicationModel::IPackage>> dummy;
-    return abiPackageStatics->get_Current(winrt::put_abi(dummy)) !=
-        winrt::impl::hresult_from_win32(APPMODEL_ERROR_NO_PACKAGE);
-  }();
+    bool HasPackageIdentity() noexcept {
+      static const bool hasPackageIdentity = []() noexcept {
+        auto packageStatics = winrt::get_activation_factory<winrt::Windows::ApplicationModel::IPackageStatics>(
+          winrt::name_of<winrt::Windows::ApplicationModel::Package>());
+        auto abiPackageStatics = static_cast<winrt::impl::abi_t<winrt::Windows::ApplicationModel::IPackageStatics> *>(
+          winrt::get_abi(packageStatics));
+        winrt::com_ptr<winrt::impl::abi_t<winrt::Windows::ApplicationModel::IPackage>> dummy;
+        return abiPackageStatics->get_Current(winrt::put_abi(dummy)) !=
+          winrt::impl::hresult_from_win32(APPMODEL_ERROR_NO_PACKAGE);
+      }();
 
-  return hasPackageIdentity;
-}
+      return hasPackageIdentity;
+    }
 
-ReactPropertyId<bool> HttpUseMonolithicModuleProperty() noexcept {
-  static ReactPropertyId<bool> propId{
-      L"ReactNative.Http"
-      L"UseMonolithicModule"};
-  return propId;
-}
+    ReactPropertyId<bool> HttpUseMonolithicModuleProperty() noexcept {
+      static ReactPropertyId<bool> propId{
+          L"ReactNative.Http"
+          L"UseMonolithicModule" };
+      return propId;
+    }
 
-} // namespace
+  } // namespace
 
-std::vector<facebook::react::NativeModuleDescription> GetCoreModules(
-    const std::shared_ptr<facebook::react::MessageQueueThread> &batchingUIMessageQueue,
+  std::vector<facebook::react::NativeModuleDescription> GetCoreModules(
+    const std::shared_ptr<facebook::react::MessageQueueThread>& batchingUIMessageQueue,
     const std::shared_ptr<facebook::react::MessageQueueThread>
-        &jsMessageQueue, // JS engine thread (what we use for external modules)
-    Mso::CntPtr<AppearanceChangeListener> &&appearanceListener,
-    Mso::CntPtr<Mso::React::IReactContext> &&context) noexcept {
-  std::vector<facebook::react::NativeModuleDescription> modules;
+    & jsMessageQueue, // JS engine thread (what we use for external modules)
+    Mso::CntPtr<AppearanceChangeListener>&& appearanceListener,
+    Mso::CntPtr<Mso::React::IReactContext>&& context) noexcept {
+    std::vector<facebook::react::NativeModuleDescription> modules;
 
-  modules.emplace_back(
+    modules.emplace_back(
       "Networking",
       [props = context->Properties()]() { return Microsoft::React::CreateHttpModule(props); },
       jsMessageQueue);
 
-  if (!ReactPropertyBag(context->Properties()).Get(HttpUseMonolithicModuleProperty())) {
     modules.emplace_back(
-        Microsoft::React::GetBlobModuleName(),
-        [props = context->Properties()]() { return Microsoft::React::CreateBlobModule(props); },
-        batchingUIMessageQueue);
-
-    modules.emplace_back(
-        Microsoft::React::GetFileReaderModuleName(),
-        [props = context->Properties()]() { return Microsoft::React::CreateFileReaderModule(props); },
-        batchingUIMessageQueue);
-  }
-
-  modules.emplace_back(
       "Timing",
       [batchingUIMessageQueue]() { return facebook::react::CreateTimingModule(batchingUIMessageQueue); },
       batchingUIMessageQueue);
 
-  // Note: `context` is moved to remove the reference from the current scope.
-  // This should either be the last usage of `context`, or the std::move call should happen later in this method.
-  modules.emplace_back(
+    // Note: `context` is moved to remove the reference from the current scope.
+    // This should either be the last usage of `context`, or the std::move call should happen later in this method.
+    modules.emplace_back(
       NativeAnimatedModule::name,
       [context = std::move(context)]() mutable { return std::make_unique<NativeAnimatedModule>(std::move(context)); },
       batchingUIMessageQueue);
 
-  modules.emplace_back(
+    modules.emplace_back(
       AppearanceModule::Name,
       [appearanceListener = std::move(appearanceListener)]() mutable {
         return std::make_unique<AppearanceModule>(std::move(appearanceListener));
       },
       jsMessageQueue);
 
-  // AsyncStorageModule doesn't work without package identity (it indirectly depends on
-  // Windows.Storage.StorageFile), so check for package identity before adding it.
-  modules.emplace_back(
+    // AsyncStorageModule doesn't work without package identity (it indirectly depends on
+    // Windows.Storage.StorageFile), so check for package identity before adding it.
+    modules.emplace_back(
       "AsyncLocalStorage",
       []() -> std::unique_ptr<facebook::xplat::module::CxxModule> {
         if (HasPackageIdentity()) {
           return std::make_unique<facebook::react::AsyncStorageModule>(L"asyncStorage");
-        } else {
+        }
+        else {
           return std::make_unique<facebook::react::AsyncStorageModuleWin32>();
         }
       },
       jsMessageQueue);
 
-  return modules;
-}
+    return modules;
+  }
 
 } // namespace Microsoft::ReactNative
--- vnext/Shared/Modules/HttpModule.cpp
+++ vnext/Shared/Modules/HttpModule.cpp
@@ -25,30 +25,30 @@ using winrt::Windows::Foundation::IInspectable;
 
 namespace {
 
-using Microsoft::React::Modules::SendEvent;
-using Microsoft::React::Networking::IHttpResource;
+  using Microsoft::React::Modules::SendEvent;
+  using Microsoft::React::Networking::IHttpResource;
 
-constexpr char moduleName[] = "Networking";
+  constexpr char moduleName[] = "Networking";
 
-// React event names
-constexpr char completedResponse[] = "didCompleteNetworkResponse";
-constexpr char receivedResponse[] = "didReceiveNetworkResponse";
-constexpr char receivedData[] = "didReceiveNetworkData";
-constexpr char receivedDataProgress[] = "didReceiveNetworkDataProgress";
+  // React event names
+  constexpr char completedResponse[] = "didCompleteNetworkResponse";
+  constexpr char receivedResponse[] = "didReceiveNetworkResponse";
+  constexpr char receivedData[] = "didReceiveNetworkData";
+  constexpr char receivedDataProgress[] = "didReceiveNetworkDataProgress";
 
-static void SetUpHttpResource(
+  static void SetUpHttpResource(
     shared_ptr<IHttpResource> resource,
     weak_ptr<Instance> weakReactInstance,
-    IInspectable &inspectableProperties) {
-  resource->SetOnRequestSuccess([weakReactInstance](int64_t requestId) {
-    auto args = dynamic::array(requestId);
+    IInspectable& inspectableProperties) {
+    resource->SetOnRequestSuccess([weakReactInstance](int64_t requestId) {
+      auto args = dynamic::array(requestId);
 
     SendEvent(weakReactInstance, completedResponse, std::move(args));
-  });
+      });
 
-  resource->SetOnResponse([weakReactInstance](int64_t requestId, IHttpResource::Response &&response) {
-    dynamic headers = dynamic::object();
-    for (auto &header : response.Headers) {
+    resource->SetOnResponse([weakReactInstance](int64_t requestId, IHttpResource::Response&& response) {
+      dynamic headers = dynamic::object();
+    for (auto& header : response.Headers) {
       headers[header.first] = header.second;
     }
 
@@ -56,148 +56,148 @@ static void SetUpHttpResource(
     dynamic args = dynamic::array(requestId, response.StatusCode, headers, response.Url);
 
     SendEvent(weakReactInstance, receivedResponse, std::move(args));
-  });
+      });
 
-  resource->SetOnData([weakReactInstance](int64_t requestId, string &&responseData) {
-    SendEvent(weakReactInstance, receivedData, dynamic::array(requestId, std::move(responseData)));
+    resource->SetOnData([weakReactInstance](int64_t requestId, string&& responseData) {
+      SendEvent(weakReactInstance, receivedData, dynamic::array(requestId, std::move(responseData)));
 
     // TODO: Move into separate method IF not executed right after onData()
     SendEvent(weakReactInstance, completedResponse, dynamic::array(requestId));
-  });
+      });
 
-  // Explicitly declaring function type to avoid type inference ambiguity.
-  std::function<void(int64_t, dynamic &&)> onDataDynamic = [weakReactInstance](
-                                                               int64_t requestId, dynamic &&responseData) {
-    SendEvent(weakReactInstance, receivedData, dynamic::array(requestId, std::move(responseData)));
-  };
-  resource->SetOnData(std::move(onDataDynamic));
+    // Explicitly declaring function type to avoid type inference ambiguity.
+    std::function<void(int64_t, dynamic&&)> onDataDynamic = [weakReactInstance](
+      int64_t requestId, dynamic&& responseData) {
+        SendEvent(weakReactInstance, receivedData, dynamic::array(requestId, std::move(responseData)));
+    };
+    resource->SetOnData(std::move(onDataDynamic));
 
-  resource->SetOnError([weakReactInstance](int64_t requestId, string &&message, bool isTimeout) {
-    dynamic args = dynamic::array(requestId, std::move(message));
+    resource->SetOnError([weakReactInstance](int64_t requestId, string&& message) {
+      dynamic args = dynamic::array(requestId, std::move(message));
     if (isTimeout) {
       args.push_back(true);
     }
 
     SendEvent(weakReactInstance, completedResponse, std::move(args));
-  });
-}
+      });
+  }
 
 } // namespace
 
 namespace Microsoft::React {
 
-HttpModule::HttpModule(IInspectable const &inspectableProperties) noexcept
-    : m_holder{std::make_shared<ModuleHolder>()},
-      m_inspectableProperties{inspectableProperties},
-      m_resource{IHttpResource::Make(inspectableProperties)} {
-  m_holder->Module = this;
-}
+  HttpModule::HttpModule(IInspectable const& inspectableProperties) noexcept
+    : m_holder{ std::make_shared<ModuleHolder>() },
+    m_inspectableProperties{ inspectableProperties },
+    m_resource{ IHttpResource::Make(inspectableProperties) } {
+    m_holder->Module = this;
+  }
 
-HttpModule::~HttpModule() noexcept /*override*/ {
-  m_holder->Module = nullptr;
-}
+  HttpModule::~HttpModule() noexcept /*override*/ {
+    m_holder->Module = nullptr;
+  }
 
 #pragma region CxxModule
 
-string HttpModule::getName() /*override*/ {
-  return moduleName;
-}
+  string HttpModule::getName() /*override*/ {
+    return moduleName;
+  }
 
-std::map<string, dynamic> HttpModule::getConstants() {
-  return {};
-}
+  std::map<string, dynamic> HttpModule::getConstants() {
+    return {};
+  }
 
-// clang-format off
-std::vector<facebook::xplat::module::CxxModule::Method> HttpModule::getMethods() {
+  // clang-format off
+  std::vector<facebook::xplat::module::CxxModule::Method> HttpModule::getMethods() {
 
-  return
-  {
+    return
     {
-      "sendRequest",
-      [weakHolder = weak_ptr<ModuleHolder>(m_holder)](dynamic args, Callback cxxCallback)
       {
-        auto holder = weakHolder.lock();
-        if (!holder) {
-          return;
-        }
-
-        auto resource = holder->Module->m_resource;
-        if (!holder->Module->m_isResourceSetup)
+        "sendRequest",
+        [weakHolder = weak_ptr<ModuleHolder>(m_holder)](dynamic args, Callback cxxCallback)
         {
-          SetUpHttpResource(resource, holder->Module->getInstance(), holder->Module->m_inspectableProperties);
-          holder->Module->m_isResourceSetup = true;
-        }
-
-        auto params = facebook::xplat::jsArgAsObject(args, 0);
-        IHttpResource::Headers headers;
-        for (auto& header : params["headers"].items()) {
-          headers.emplace(header.first.getString(), header.second.getString());
-        }
-
-        resource->SendRequest(
-          params["method"].asString(),
-          params["url"].asString(),
-          params["requestId"].asInt(),
-          std::move(headers),
-          std::move(params["data"]),
-          params["responseType"].asString(),
-          params["incrementalUpdates"].asBool(),
-          static_cast<int64_t>(params["timeout"].asDouble()),
-          params["withCredentials"].asBool(),
-          [cxxCallback = std::move(cxxCallback)](int64_t requestId) {
-            cxxCallback({requestId});
+          auto holder = weakHolder.lock();
+          if (!holder) {
+            return;
           }
-        );
-      }
-    },
-    {
-      "abortRequest",
-      [weakHolder = weak_ptr<ModuleHolder>(m_holder)](dynamic args)
+
+          auto resource = holder->Module->m_resource;
+          if (!holder->Module->m_isResourceSetup)
+          {
+            SetUpHttpResource(resource, holder->Module->getInstance(), holder->Module->m_inspectableProperties);
+            holder->Module->m_isResourceSetup = true;
+          }
+
+          auto params = facebook::xplat::jsArgAsObject(args, 0);
+          IHttpResource::Headers headers;
+          for (auto& header : params["headers"].items()) {
+            headers.emplace(header.first.getString(), header.second.getString());
+          }
+
+          resource->SendRequest(
+            params["method"].asString(),
+            params["url"].asString(),
+            params["requestId"].asInt(),
+            std::move(headers),
+            std::move(params["data"]),
+            params["responseType"].asString(),
+            params["incrementalUpdates"].asBool(),
+            static_cast<int64_t>(params["timeout"].asDouble()),
+            params["withCredentials"].asBool(),
+            [cxxCallback = std::move(cxxCallback)](int64_t requestId) {
+              cxxCallback({requestId});
+            }
+          );
+        }
+      },
       {
-        auto holder = weakHolder.lock();
-        if (!holder)
+        "abortRequest",
+        [weakHolder = weak_ptr<ModuleHolder>(m_holder)](dynamic args)
         {
-          return;
-        }
+          auto holder = weakHolder.lock();
+          if (!holder)
+          {
+            return;
+          }
 
-        auto resource = holder->Module->m_resource;
-        if (!holder->Module->m_isResourceSetup)
-        {
-          SetUpHttpResource(resource, holder->Module->getInstance(), holder->Module->m_inspectableProperties);
-          holder->Module->m_isResourceSetup = true;
-        }
+          auto resource = holder->Module->m_resource;
+          if (!holder->Module->m_isResourceSetup)
+          {
+            SetUpHttpResource(resource, holder->Module->getInstance(), holder->Module->m_inspectableProperties);
+            holder->Module->m_isResourceSetup = true;
+          }
 
-        resource->AbortRequest(facebook::xplat::jsArgAsInt(args, 0));
-      }
-    },
-    {
-      "clearCookies",
-      [weakHolder = weak_ptr<ModuleHolder>(m_holder)](dynamic args)
+          resource->AbortRequest(facebook::xplat::jsArgAsInt(args, 0));
+        }
+      },
       {
-        auto holder = weakHolder.lock();
-        if (!holder)
+        "clearCookies",
+        [weakHolder = weak_ptr<ModuleHolder>(m_holder)](dynamic args)
         {
-          return;
-        }
+          auto holder = weakHolder.lock();
+          if (!holder)
+          {
+            return;
+          }
 
-        auto resource = holder->Module->m_resource;
-        if (!holder->Module->m_isResourceSetup)
-        {
-          SetUpHttpResource(resource, holder->Module->getInstance(), holder->Module->m_inspectableProperties);
-          holder->Module->m_isResourceSetup = true;
-        }
+          auto resource = holder->Module->m_resource;
+          if (!holder->Module->m_isResourceSetup)
+          {
+            SetUpHttpResource(resource, holder->Module->getInstance(), holder->Module->m_inspectableProperties);
+            holder->Module->m_isResourceSetup = true;
+          }
 
-        resource->ClearCookies();
+          resource->ClearCookies();
+        }
       }
-    }
-  };
-}
-// clang-format on
+    };
+  }
+  // clang-format on
 
 #pragma endregion CxxModule
 
-/*extern*/ const char *GetHttpModuleName() noexcept {
-  return moduleName;
-}
+  /*extern*/ const char* GetHttpModuleName() noexcept {
+    return moduleName;
+  }
 
 } // namespace Microsoft::React
