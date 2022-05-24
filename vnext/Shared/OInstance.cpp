e25041698cd8b4ecf42c02c0457ea416fc8590db
Julio C. Rocha <julio.rocha@microsoft.com>
2022-05-23 19:13:33 -0700

    Disable BlobModule in UWP

--- vnext/Shared/OInstance.cpp
+++ vnext/Shared/OInstance.cpp
@@ -614,6 +614,8 @@ std::vector<std::unique_ptr<NativeModule>> InstanceImpl::GetDefaultNativeModules
       []() { return std::make_unique<StatusBarManagerModule>(); },
       nativeQueue));
 
+// TODO: Follow up - Blob module not supported in UWP. Need to define property bag lifetime and onwership.
+#if (defined(_MSC_VER) && !defined(WINRT))
   modules.push_back(std::make_unique<CxxNativeModule>(
       m_innerInstance,
       Microsoft::React::GetBlobModuleName(),
@@ -625,6 +627,7 @@ std::vector<std::unique_ptr<NativeModule>> InstanceImpl::GetDefaultNativeModules
       Microsoft::React::GetFileReaderModuleName(),
       [transitionalProps]() { return Microsoft::React::CreateFileReaderModule(transitionalProps); },
       nativeQueue));
+#endif
 
   return modules;
 }
