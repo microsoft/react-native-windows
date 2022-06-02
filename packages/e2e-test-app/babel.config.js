d6cd6a3d9508a22a9f8e682b565957d7fa58f1d0
TatianaKapos <tatianakapos@microsoft.com>
2022-06-02 16:47:13 -0700

    remove E2E test warnings

--- packages/e2e-test-app/babel.config.js
+++ packages/e2e-test-app/babel.config.js
@@ -1,3 +1,4 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [["@babel/plugin-proposal-private-methods", { "loose": true }],["@babel/plugin-proposal-private-property-in-object", { "loose": true }]],
 };
