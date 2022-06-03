40c93cbb06e389929227091a762fd36c69df24d7
TatianaKapos <tatianakapos@microsoft.com>
2022-06-02 17:32:14 -0700

    try without plugins

--- packages/e2e-test-app/babel.config.js
+++ packages/e2e-test-app/babel.config.js
@@ -1,4 +1,3 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [["@babel/plugin-proposal-private-methods", { "loose": true }],["@babel/plugin-proposal-private-property-in-object", { "loose": true }]],
 };
