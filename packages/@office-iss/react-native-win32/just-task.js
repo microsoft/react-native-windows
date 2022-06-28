20f0e2cc74ecf1eef4b445d7723725b520f4f6c9
Jon Thysell (JAUNTY) <jthysell@microsoft.com>
2022-06-28 11:59:55 -0700

    Added dynamically generated cgmanifest.json as part of win32 build

--- packages/@office-iss/react-native-win32/just-task.js
+++ packages/@office-iss/react-native-win32/just-task.js
@@ -20,8 +20,38 @@ const {
 require('@rnw-scripts/just-task');
 require('@rnw-scripts/just-task/react-native-tasks');
 
+const hermesUtils = require('react-native/scripts/hermes/hermes-utils');
+
 option('production');
 option('clean');
+
+task('cgmanifests', () => {
+  // Get hermes version used
+  const hermesTag = hermesUtils.readHermesTag();
+  const hermesTagSHA = hermesUtils.getHermesTagSHA(hermesTag);
+
+  // Construct CG manifest object
+  const cgmanifest = {
+    Registrations: [
+      {
+        Component: {
+          Type: 'git',
+          Git: {
+            RepositoryUrl: 'https://github.com/facebook/hermes',
+            CommitHash: hermesTagSHA,
+          },
+        },
+        DevelopmentDependency: false,
+      },
+    ],
+  };
+
+  // Write CG manifest object to JSON
+  fs.writeFileSync(
+    path.resolve(__dirname, 'cgmanifest.json'),
+    JSON.stringify(cgmanifest, null, 2),
+  );
+});
 
 task('ts', tscTask());
 
@@ -38,6 +68,7 @@ task(
     condition('clean', () => argv().clean),
     'copyRNLibraries',
     'ts',
+    'cgmanifests',
   ),
 );
 
