30b92950b3386c83809a9142c46a5f3bd5fb5d18
Agne Lukoseviciute <agnel@microsoft.com>
2022-08-10 17:42:57 -0700

    example for testing

--- packages/playground/Samples/image.tsx
+++ packages/playground/Samples/image.tsx
@@ -5,7 +5,15 @@
  */
 import React from 'react';
 import {Picker} from '@react-native-picker/picker';
-import {AppRegistry, Image, View, Text, Switch, StyleSheet} from 'react-native';
+import {
+  AppRegistry,
+  Image,
+  View,
+  Text,
+  Switch,
+  StyleSheet,
+  PlatformColor,
+} from 'react-native';
 
 const largeImageUri =
   'https://cdn.freebiesupply.com/logos/large/2x/react-logo-png-transparent.png';
@@ -69,9 +77,11 @@ export default class Bootstrap extends React.Component<
         <View style={styles.rowContainer}>
           <Text style={styles.title}>ResizeMode</Text>
           <Picker
-            style={{width: 125}}
+            style={styles.picker}
             selectedValue={this.state.selectedResizeMode}
-            onValueChange={value => this.setState({selectedResizeMode: value})}>
+            onValueChange={(value) =>
+              this.setState({selectedResizeMode: value})
+            }>
             <Picker.Item label="cover" value="cover" />
             <Picker.Item label="contain" value="contain" />
             <Picker.Item label="stretch" value="stretch" />
@@ -82,9 +92,9 @@ export default class Bootstrap extends React.Component<
         <View style={styles.rowContainer}>
           <Text style={styles.title}>Image Source</Text>
           <Picker
-            style={{width: 125}}
+            style={styles.picker}
             selectedValue={this.state.selectedSource}
-            onValueChange={value => this.switchImageUri(value)}>
+            onValueChange={(value) => this.switchImageUri(value)}>
             <Picker.Item label="small" value="small" />
             <Picker.Item label="large" value="large" />
             <Picker.Item label="data" value="data" />
@@ -95,9 +105,9 @@ export default class Bootstrap extends React.Component<
         <View style={styles.rowContainer}>
           <Text style={styles.title}>Blur Radius</Text>
           <Picker
-            style={{width: 125}}
+            style={styles.picker}
             selectedValue={this.state.blurRadius}
-            onValueChange={value => this.setState({blurRadius: value})}>
+            onValueChange={(value) => this.setState({blurRadius: value})}>
             <Picker.Item label="0" value={0} />
             <Picker.Item label="5" value={5} />
             <Picker.Item label="10" value={10} />
@@ -106,12 +116,13 @@ export default class Bootstrap extends React.Component<
         <View style={styles.rowContainer}>
           <Text style={styles.title}>Tint Color</Text>
           <Picker
-            style={{width: 125}}
+            style={styles.picker}
             selectedValue={this.state.tintColor}
-            onValueChange={value => this.setState({tintColor: value})}>
+            onValueChange={(value) => this.setState({tintColor: value})}>
             <Picker.Item label="None" value="transparent" />
             <Picker.Item label="Purple" value="purple" />
             <Picker.Item label="Green" value="green" />
+            <Picker.Item label="SystemAccentColor" value="platformcolor" />
           </Picker>
         </View>
         <View style={styles.rowContainer}>
@@ -130,7 +141,9 @@ export default class Bootstrap extends React.Component<
             style={[
               styles.image,
               this.state.includeBorder ? styles.imageWithBorder : {},
-              {tintColor: this.state.tintColor},
+              this.state.tintColor === 'platformcolor'
+                ? styles.imageWithPlatformColor
+                : {tintColor: this.state.tintColor},
             ]}
             source={
               this.state.selectedSource === 'svg'
@@ -158,6 +171,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginBottom: 5,
   },
+  picker: {
+    width: 175,
+  },
   imageContainer: {
     marginTop: 5,
     backgroundColor: 'orange',
@@ -173,6 +189,9 @@ const styles = StyleSheet.create({
     borderWidth: 10,
     borderColor: 'green',
     backgroundColor: 'red',
+  },
+  imageWithPlatformColor: {
+    tintColor: PlatformColor('SystemAccentColor'),
   },
   title: {
     fontWeight: 'bold',
