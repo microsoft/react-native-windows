4bb1f6a41e01c89a921302dc9e490d96a3ed0c7a
Eric Rozell <erozell@outlook.com>
2022-08-16 21:31:33 -0400

    Adds GetReactRootView method to XamlUIService
    We emulate this behavior in our app, it's probably better to expose the
    core functionality to walk the shadow node tree (rather than using
    VisualTreeHelpers).

--- vnext/Microsoft.ReactNative/XamlUIService.h
+++ vnext/Microsoft.ReactNative/XamlUIService.h
@@ -22,6 +22,8 @@ struct XamlUIService : XamlUIServiceT<XamlUIService> {
       hstring const &eventName,
       JSValueArgWriter const &eventDataArgWriter) noexcept;
 
+  xaml::FrameworkElement GetReactRootElementForView(xaml::FrameworkElement const &view) noexcept;
+
   static void SetXamlRoot(IReactPropertyBag const &properties, xaml::XamlRoot const &xamlRoot) noexcept;
   static void SetAccessibleRoot(
       IReactPropertyBag const &properties,
