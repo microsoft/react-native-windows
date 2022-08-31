91889d203d232f3f19a490c1f6dd5b590ded7624
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2022-08-30 21:11:59 -0700

    [0.69] Allow batches to complete during load (#10472)
    * Allow batches to complete during load (#10298)
    
    * Allow batches to complete during load
    
    * Change files
    
    * fix
    
    * update change file

--- vnext/Shared/Threading/BatchingQueueThread.cpp
+++ vnext/Shared/Threading/BatchingQueueThread.cpp
@@ -69,19 +69,25 @@ BatchingQueueThread::BatchingQueueThread(
 void BatchingQueueThread::decoratedNativeCallInvokerReady(
     std::weak_ptr<facebook::react::Instance> wkInstance) noexcept {
   std::scoped_lock lck(m_mutex);
-  m_callInvoker->invokeAsync([wkInstance, this] {
-    if (auto instance = wkInstance.lock()) {
-      std::scoped_lock lckQuitting(m_mutexQuitting);
+  if (auto instance = wkInstance.lock()) {
+    // When items were queued in the undecoratedNativeCallInvoker it will not have called
+    // recordTurboModuleAsyncMethodCall. Calling invokeAsync on the decoratedNativeCallInvoker
+    // ensures that the queue is properly flushed, on the next batch complete
+    auto decoratedCallInvoker = instance->getDecoratedNativeCallInvoker(m_callInvoker);
+    decoratedCallInvoker->invokeAsync([decoratedCallInvoker, wkInstance, this] {
+      if (auto instance = wkInstance.lock()) {
+        std::scoped_lock lckQuitting(m_mutexQuitting);
 
-      // If we are shutting down, then then the mutex is being held in quitSynchronous
-      // Which is waiting for this task to complete, so we cannot take the mutex if quitSynchronous
-      // is running. -- and since we are shutting down anyway, we can just skip this work.
-      if (!m_quitting) {
-        std::scoped_lock lck(m_mutex);
-        m_callInvoker = instance->getDecoratedNativeCallInvoker(m_callInvoker);
+        // If we are shutting down, then then the mutex is being held in quitSynchronous
+        // Which is waiting for this task to complete, so we cannot take the mutex if quitSynchronous
+        // is running. -- and since we are shutting down anyway, we can just skip this work.
+        if (!m_quitting) {
+          std::scoped_lock lck(m_mutex);
+          m_callInvoker = decoratedCallInvoker;
+        }
       }
-    }
-  });
+    });
+  }
 }
 
 BatchingQueueThread::~BatchingQueueThread() noexcept {}
