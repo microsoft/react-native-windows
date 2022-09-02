b5a780246d938ea5064257b9a2ad7cbd2a2d3d42
Julio C. Rocha <julio.rocha@microsoft.com>
2022-09-02 01:32:07 -0700

    Implement mocks in ManualRedirectSucceeds

--- vnext/Desktop.UnitTests/RedirectHttpFilterUnitTest.cpp
+++ vnext/Desktop.UnitTests/RedirectHttpFilterUnitTest.cpp
@@ -1,20 +1,14 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-#undef WINRT_LEAN_AND_MEAN // TODO:Remove
-
 #include <CppUnitTest.h>
 
 #include <Networking/RedirectHttpFilter.h>
+#include "../Shared/Networking/WinRTTypes.h"
 #include "WinRTNetworkingMocks.h"
 
-//// Boost Library
-//#include <boost/beast/http.hpp>
-//
-//// Standard Library
-//#include <future>
-
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace winrt::Windows::Web::Http;
 
 using std::make_shared;
 //using std::promise;//TODO: keep?
@@ -40,8 +34,24 @@ TEST_CLASS (RedirectHttpFilterUnitTest) {
   TEST_METHOD(ManualRedirectSucceeds) {
     auto mockFilter1 = winrt::make<MockHttpBaseFilter>();
     auto mockFilter2 = winrt::make<MockHttpBaseFilter>();
-    auto filter = winrt::make<Networking::RedirectHttpFilter>(
-      std::move(mockFilter1), std::move(mockFilter2));
+
+    auto redirectCount = 0;
+    auto url1 = L"http://initialhost";
+    auto url2 = L"http://redirecthost";
+    auto mockPtr1{mockFilter1.as<MockHttpBaseFilter>()};
+    mockPtr1->Mocks.SendRequestAsync = [&redirectCount](HttpRequestMessage const &request) -> Networking::ResponseOperation {
+      HttpResponseMessage response{};
+
+      if (request.RequestUri().Host() == L"initialhost")
+        response.StatusCode(HttpStatusCode::MovedPermanently);
+      else if (request.RequestUri().Host() == L"redirecthost")
+        response.StatusCode(HttpStatusCode::Ok);
+
+      co_return response;
+    };
+
+    auto filter = winrt::make<Networking::RedirectHttpFilter>(std::move(mockFilter1), std::move(mockFilter2));
+    filter.try_as<IHttpBaseProtocolFilter>().AllowAutoRedirect(false);
   }
 };
 
