625fc80e100e19a7beb780595e920096d3eb2b3d
Julio C. Rocha <julio.rocha@microsoft.com>
2022-09-07 02:40:50 -0700

    Add test MaxAllowedRedirectsSucceeds

--- vnext/Desktop.UnitTests/RedirectHttpFilterUnitTest.cpp
+++ vnext/Desktop.UnitTests/RedirectHttpFilterUnitTest.cpp
@@ -144,6 +144,57 @@ TEST_CLASS (RedirectHttpFilterUnitTest) {
     Assert::AreEqual(L"Response Content", content.c_str());
   }
 
+  TEST_METHOD(MaxAllowedRedirectsSucceeds) {
+    auto url1 = L"http://initialhost";
+    auto url2 = L"http://redirecthost";
+    auto mockFilter1 = winrt::make<MockHttpBaseFilter>();
+    auto mockFilter2 = winrt::make<MockHttpBaseFilter>();
+
+    mockFilter1.as<MockHttpBaseFilter>()->Mocks.SendRequestAsync =
+        [&url2](HttpRequestMessage const &request) -> ResponseOperation {
+      HttpResponseMessage response;
+
+      auto uri2 = Uri{url2 + winrt::to_hstring(L"?redirCount=1")};
+      response.Headers().Location(uri2);
+      response.StatusCode(HttpStatusCode::MovedPermanently);
+      response.Content(HttpStringContent{L""});
+
+      co_return response;
+    };
+    mockFilter2.as<MockHttpBaseFilter>()->Mocks.SendRequestAsync =
+        [&url2](HttpRequestMessage const &request) -> ResponseOperation {
+      HttpResponseMessage response;
+
+      auto query = request.RequestUri().QueryParsed().GetFirstValueByName(L"redirCount");
+      auto redirCount = std::stoi(query.c_str());
+      if (redirCount >= 3) {
+        response.StatusCode(HttpStatusCode::Ok);
+        response.Content(HttpStringContent{L"Response Content"});
+      } else {
+        redirCount++;
+        response.Headers().Location(Uri{url2 + winrt::to_hstring(L"?redirCount=") + winrt::to_hstring(redirCount)});
+        response.StatusCode(HttpStatusCode::MovedPermanently);
+        response.Content(HttpStringContent{L"Redirecting: " + winrt::to_hstring(redirCount)});
+      }
+
+      co_return response;
+    };
+
+    auto filter = winrt::make<RedirectHttpFilter>(3, std::move(mockFilter1), std::move(mockFilter2));
+    auto client = HttpClient{filter};
+    auto request = HttpRequestMessage(HttpMethod::Get(), Uri{url1});
+    auto sendOp = client.SendRequestAsync(request);
+    sendOp.get();
+    auto response = sendOp.GetResults();
+
+    Assert::AreEqual(HttpStatusCode::Ok, response.StatusCode());
+
+    auto contentOp = response.Content().ReadAsStringAsync();
+    contentOp.get();
+    auto content = contentOp.GetResults();
+    Assert::AreEqual(L"Response Content", content.c_str());
+  }
+
   TEST_METHOD(TooManyRedirectsFails)
   {
     auto url1 = L"http://initialhost";
