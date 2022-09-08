555aeb48ceffa3a58f69427c31ecd43d8b4c0aff
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2022-09-08 14:34:12 -0700

    format

--- packages/integration-test-app/windows/integrationtest/ReactPackageProvider.cpp
+++ packages/integration-test-app/windows/integrationtest/ReactPackageProvider.cpp
@@ -7,9 +7,12 @@ using namespace winrt::Microsoft::ReactNative;
 namespace winrt::integrationtest::implementation {
 
 void ReactPackageProvider::CreatePackage(IReactPackageBuilder const &packageBuilder) noexcept {
-  // We do not use TurboModules for the TestModule, since the integration tests are specifically targetting NativeModules NOT TurboModules
-  // Ex: https://github.com/facebook/react-native/blob/153aedce413ef73f5e026abdfcf1346a37cec219/IntegrationTests/AppEventsTest.js#L16
-  AddAttributedModules(packageBuilder, false); 
+  // We do not use TurboModules for the TestModule, since the integration tests are specifically targetting
+  // NativeModules NOT TurboModules.
+  //
+  // See here for an example usage of NativeModules.TestModule:
+  // https://github.com/facebook/react-native/blob/153aedce413ef73f5e026abdfcf1346a37cec219/IntegrationTests/AppEventsTest.js#L16
+  AddAttributedModules(packageBuilder, false);
 }
 
 } // namespace winrt::integrationtest::implementation
