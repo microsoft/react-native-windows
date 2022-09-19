0f5229bd413fc7df3778c4937dbfa31482afe190
Andrew <30809111+acoates-ms@users.noreply.github.com>
2022-09-19 13:33:35 -0700

    Cleanup some UseFabric in proj files

--- vnext/Desktop.DLL/pch.h
+++ vnext/Desktop.DLL/pch.h
@@ -3,11 +3,10 @@
 
 #pragma once
 
-#include <windows.h>
+#define _USE_MATH_DEFINES 
+#include <Math.h>
 
-#ifndef M_PI
-#define M_PI 3.14159265358979323846
-#endif
+#include <windows.h>
 
 #ifdef GetCurrentTime
 #undef GetCurrentTime
