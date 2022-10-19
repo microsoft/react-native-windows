0047cb4c17d5acf915ac8e3593a6f79c8d0683ef
tudorms <tudor.mihai@microsoft.com>
2022-10-18 17:36:23 -0700

    Revert the playground changes

--- packages/playground/windows/playground-win32/pch.h
+++ packages/playground/windows/playground-win32/pch.h
@@ -13,6 +13,7 @@
 
 #include <UI.Xaml.Hosting.DesktopWindowXamlSource.h>
 #include <winrt/Microsoft.ReactNative.h>
+#include <winrt/ReactNativePicker.h>
 #include <winrt/Windows.Foundation.Collections.h>
 #include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.UI.Xaml.Hosting.h>
