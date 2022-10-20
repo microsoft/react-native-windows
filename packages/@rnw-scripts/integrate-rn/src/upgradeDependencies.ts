84b54fba774cf494421774cd9850ae7e41a83cf8
Vladimir Morozov <vmoroz@users.noreply.github.com>
2022-10-20 11:52:39 -0700

    Revert changes for `yarn check`

--- packages/@rnw-scripts/integrate-rn/src/upgradeDependencies.ts
+++ packages/@rnw-scripts/integrate-rn/src/upgradeDependencies.ts
@@ -9,14 +9,12 @@ import _ from 'lodash';
 import path from 'path';
 import semver from 'semver';
 import {
-  WritableNpmPackage,
   enumerateRepoPackages,
   findPackage,
   findRepoPackage,
 } from '@react-native-windows/package-utils';
 import runCommand from './runCommand';
 import {upgradeOverrides} from 'react-native-platform-override';
-import findRepoRoot from '@react-native-windows/find-repo-root';
 
 /**
  * Describes the dependencies of a package
@@ -103,44 +101,7 @@ export default async function upgradeDependencies(
     }),
   );
 
-  await upgradeResolutions(reactNativeDiff, repoConfigDiff);
-
   await runCommand('yarn install');
-}
-
-/**
- * Upgrades all packages in the monorepo cwd resides in to use a new version
- * of react-native, updating non-react-native dependencies, devDependencies,
- * and peerDependencies where appropriate.
- */
-async function upgradeResolutions(
-  reactNativeDiff: PackageDiff,
-  repoConfigDiff: PackageDiff,
-) {
-  const repoRoot = await findRepoRoot();
-  const rootPkg = await WritableNpmPackage.fromPath(repoRoot);
-  if (rootPkg) {
-    const combinedDependencies = Object.assign(
-      {},
-      reactNativeDiff.newPackage.dependencies || {},
-      reactNativeDiff.newPackage.peerDependencies || {},
-      reactNativeDiff.newPackage.devDependencies || {},
-      repoConfigDiff.newPackage.dependencies || {},
-      repoConfigDiff.newPackage.peerDependencies || {},
-      repoConfigDiff.newPackage.devDependencies || {},
-    );
-    for (const key of Object.keys(rootPkg.json.resolutions)) {
-      const newProp = combinedDependencies[key];
-      if (newProp) {
-        rootPkg.json.resolutions[key] = bumpSemver(
-          rootPkg.json.resolutions[key],
-          newProp,
-        );
-      }
-    }
-
-    await rootPkg.setJson(rootPkg.json);
-  }
 }
 
 /**
@@ -187,22 +148,7 @@ async function upgradeReactNative(
     }
   }
 
-  const repoRoot = await findRepoRoot();
-  const rootPkg = await WritableNpmPackage.fromPath(repoRoot);
-  if (rootPkg) {
-    if (rootPkg.json.resolutions && rootPkg.json.resolutions['react-native']) {
-      await rootPkg.mergeProps({
-        resolutions: {
-          'react-native': bumpSemver(
-            rootPkg.json.resolutions['react-native'],
-            newReactNativeVersion,
-          ),
-        },
-      });
-    }
-  }
-
-  await runCommand('yarn install --no-script');
+  await runCommand('yarn install');
   const newJson = (await findPackage('react-native', findRnOpts))!.json;
 
   return extractPackageDiff(origJson, newJson);
@@ -414,7 +360,7 @@ function ensureValidReactNativePeerDep(
   }
 
   // If we have a range, such as in our stable branches, only bump if needed,
-  // as changing the peer dependency is a breaking change. Any prerelease may
+  // as changing the peer depenedncy is a breaking change. Any prerelease may
   // be breaking.
   const peerDep = pkg.peerDependencies['react-native'];
 
@@ -446,7 +392,7 @@ function ensureValidReactNativePeerDep(
 }
 
 /**
- * Ensure that a package fulfills peer dependencies for react-native if relying on it
+ * Ensure that a package fulfills peer depenedncies for react-native if relying on it
  */
 function ensureReactNativePeerDepsSatisfied(
   pkg: LocalPackageDeps,
@@ -483,7 +429,7 @@ function bumpSemver(origVersion: string, newVersion: string): string {
     throw new Error(`Unable to bump invalid semver '${origVersion}'`);
   }
 
-  // Semver allows multiple ranges, hyphen ranges, star ranges, etc. Don't try
+  // Semver allows multiple ranges, hypen ranges, star ranges, etc. Don't try
   // to reason about how to bump all of those and just bail if we see them.
   const simpleSemver = /([\^~]?)(\d+\.\d+(\.\d+)?(-\w+\.\d+)?)/;
   if (!simpleSemver.test(origVersion)) {
