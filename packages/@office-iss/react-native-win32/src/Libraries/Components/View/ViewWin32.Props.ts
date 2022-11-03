72dc5c6ff50b009bbec20f402f56944269ae8416
Krystal Kramer <krsiler@microsoft.com>
2022-11-02 21:03:06 -0400

    [0.70] Add accessibilityLevel as a prop on TextWin32 to set heading level (#10825)
    * Add accessibilityLevel as a prop on TextWin32 to set heading level (#10823)
    
    * Add accessibilityLevel to BasePropsWin32
    
    * Change files
    
    * Add test case with no "header" role set
    
    * Remove extra accessibilityLevel prop from ViewWin32 (#10824)
    
    * Remove duplicate accessibilityLevel prop
    
    * Change files
    
    * Fix misspelling

--- packages/@office-iss/react-native-win32/src/Libraries/Components/View/ViewWin32.Props.ts
+++ packages/@office-iss/react-native-win32/src/Libraries/Components/View/ViewWin32.Props.ts
@@ -178,6 +178,12 @@ export type BasePropsWin32 = {
     * ItemType is used to obtain information about items in a list, tree view, or data grid. For example, an item in a file directory view might be a "Document File" or a "Folder".
     */
    accessibilityItemType?: string;
+
+   /**
+    * Defines the level of an element in a hierarchical structure or the heading level of a text element.
+    * Note: accessibilityRole="header" must be used if using this property to define a heading level.
+    */
+    accessibilityLevel?: number;
 };
 
 export type ViewWin32OmitTypes = RN.ViewPropsAndroid &
@@ -212,7 +218,6 @@ export interface IViewWin32Props extends Omit<RN.ViewProps, ViewWin32OmitTypes>,
   *
   */
   accessibilityDescription?: string;
-  accessibilityLevel?: number;
   accessibilityPositionInSet?: number;
 
   accessibilitySetSize?: number;
