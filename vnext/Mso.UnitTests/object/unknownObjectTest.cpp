8e5cbb78ffdcc5f08fcf0aa4d95762380f5701ec
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2022-11-08 16:46:34 -0800

    suppress new c++ warning CI hits

--- vnext/Mso.UnitTests/object/unknownObjectTest.cpp
+++ vnext/Mso.UnitTests/object/unknownObjectTest.cpp
@@ -454,7 +454,7 @@ struct AsyncDeleter2 {
     try {
       // Ideally we want to show here how to use dispatch queues, but we cannot add DispatchQueue liblet dependency
       // here.
-#pragma warning(suppress : 4834) // discarding return value of function with 'nodiscard' attribute
+#pragma warning(suppress : 4834, 4858) // discarding return value of function with 'nodiscard' attribute
       std::async(std::launch::async, [obj]() noexcept { TObject::RefCountPolicy::template Delete(obj); });
     } catch (...) {
       VerifyElseCrashTag(false, 0x01003709 /* tag_bad2j */);
