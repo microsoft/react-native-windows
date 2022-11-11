07e1e9b1c45464e0c0bf19d58deb911d3e16e520
Chiara Mooney <34109996+chiaramooney@users.noreply.github.com>
2022-11-11 12:22:56 -0800

    Fix Flow Windows

--- vnext/src/Libraries/Components/View/View.windows.js
+++ vnext/src/Libraries/Components/View/View.windows.js
@@ -151,6 +151,7 @@ const View: React.AbstractComponent<
 
     const _keyDown = (event: KeyEvent) => {
       if (otherProps.keyDownEvents && event.isPropagationStopped() !== true) {
+         // $FlowFixMe - keyDownEvents was already checked to not be undefined
         for (const el of otherProps.keyDownEvents) {
           if (event.nativeEvent.code == el.code && el.handledEventPhase == 3) {
             event.stopPropagation();
@@ -162,6 +163,7 @@ const View: React.AbstractComponent<
 
     const _keyUp = (event: KeyEvent) => {
       if (otherProps.keyUpEvents && event.isPropagationStopped() !== true) {
+         // $FlowFixMe - keyDownEvents was already checked to not be undefined
         for (const el of otherProps.keyUpEvents) {
           if (event.nativeEvent.code == el.code && el.handledEventPhase == 3) {
             event.stopPropagation();
@@ -173,6 +175,7 @@ const View: React.AbstractComponent<
 
     const _keyDownCapture = (event: KeyEvent) => {
       if (otherProps.keyDownEvents && event.isPropagationStopped() !== true) {
+         // $FlowFixMe - keyDownEvents was already checked to not be undefined
         for (const el of otherProps.keyDownEvents) {
           if (event.nativeEvent.code == el.code && el.handledEventPhase == 1) {
             event.stopPropagation();
@@ -184,6 +187,7 @@ const View: React.AbstractComponent<
 
     const _keyUpCapture = (event: KeyEvent) => {
       if (otherProps.keyUpEvents && event.isPropagationStopped() !== true) {
+         // $FlowFixMe - keyDownEvents was already checked to not be undefined
         for (const el of otherProps.keyUpEvents) {
           if (event.nativeEvent.code == el.code && el.handledEventPhase == 1) {
             event.stopPropagation();
