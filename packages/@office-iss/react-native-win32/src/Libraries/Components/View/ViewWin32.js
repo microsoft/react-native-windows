6fb4fcc48e399902168b22fcc5c7613a239af82d
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2023-02-07 15:00:18 -0800

    lint:fix

--- packages/@office-iss/react-native-win32/src/Libraries/Components/View/ViewWin32.js
+++ packages/@office-iss/react-native-win32/src/Libraries/Components/View/ViewWin32.js
@@ -2,7 +2,7 @@
  * Copyright: Microsoft 2015
  *
  * RN-specific implementation of the cross-platform View abstraction.
- * 
+ *
  * @flow strict-local
  * @format
  */
@@ -22,52 +22,70 @@ import warnOnce from '../../Utilities/warnOnce';
  * Basic View component with additional Win32 specific functionality
  */
 
-
-const ViewWin32: React.AbstractComponent<
-  ViewProps,
-  any,
-> = React.forwardRef(
+const ViewWin32: React.AbstractComponent<ViewProps, any> = React.forwardRef(
   (
     {
       accessibilityLabeledBy,
-      accessibilityDescribedBy, 
-      accessibilityControls, 
+      accessibilityDescribedBy,
+      accessibilityControls,
       ...rest
     }: ViewProps,
     forwardedRef,
   ) => {
-
     /**
      * Process accessibility refs into node handles after initial DOM render, before sent across the bridge.
      * useLayoutEffect will invalidate the render to assess the ref-based accessibility properties.
-     * 
+     *
      * RN core moved these properties to use NativeID instead of Refs, so this is only kept for back compat
      */
 
-    const [labeledByTarget, setLabeledByTarget] = React.useState<number|null>(null);
-    const [describedByTarget, setDescribedByTarget] = React.useState<number|null>(null);
-    const [controlsTarget, setControlsTarget] = React.useState<number|null>(null);
+    const [labeledByTarget, setLabeledByTarget] = React.useState<number | null>(
+      null,
+    );
+    const [describedByTarget, setDescribedByTarget] = React.useState<
+      number | null,
+    >(null);
+    const [controlsTarget, setControlsTarget] = React.useState<number | null>(
+      null,
+    );
 
     useLayoutEffect(() => {
-      if (accessibilityLabeledBy !== undefined && accessibilityLabeledBy.current !== null)
-      {
-        warnOnce('accessibilityLabeledByAsRef', 'accessibilityLabeledBy has been deprecated.  Instead use accessibilityLabelledBy (double L), and specify it as a string matching the nativeID of the target component.  Support of specifying accessibilityLabeledBy will be removed in a future release');
+      if (
+        accessibilityLabeledBy !== undefined &&
+        accessibilityLabeledBy.current !== null
+      ) {
+        warnOnce(
+          'accessibilityLabeledByAsRef',
+          'accessibilityLabeledBy has been deprecated.  Instead use accessibilityLabelledBy (double L), and specify it as a string matching the nativeID of the target component.  Support of specifying accessibilityLabeledBy will be removed in a future release',
+        );
         setLabeledByTarget(findNodeHandle(accessibilityLabeledBy.current));
       }
     }, [accessibilityLabeledBy]);
 
     useLayoutEffect(() => {
-      if ((typeof accessibilityDescribedBy !== "string") && accessibilityDescribedBy !== undefined && accessibilityDescribedBy.current !== null)
-      {
-        warnOnce('accessibilityDescribedByRef', 'accessibilityDescribedBy should be specfied as a string matching the nativeID of the target component.  Support of specifying accessibilityDescribedBy using a ref is deprecated and will be removed in a future release');
+      if (
+        typeof accessibilityDescribedBy !== 'string' &&
+        accessibilityDescribedBy !== undefined &&
+        accessibilityDescribedBy.current !== null
+      ) {
+        warnOnce(
+          'accessibilityDescribedByRef',
+          'accessibilityDescribedBy should be specfied as a string matching the nativeID of the target component.  Support of specifying accessibilityDescribedBy using a ref is deprecated and will be removed in a future release',
+        );
         setDescribedByTarget(findNodeHandle(accessibilityDescribedBy.current));
       }
     }, [accessibilityDescribedBy]);
-    
+
     useLayoutEffect(() => {
-      if((typeof accessibilityControls !== "string") && accessibilityControls !== undefined && accessibilityControls.current !== null)
-      {
-        warnOnce('accessibilityControlsRef', 'accessibilityControls should be specfied as a string matching the nativeID of the target component.  Support of specifying accessibilityControls using a ref is deprecated and will be removed in a future release');
+      if (
+        typeof accessibilityControls !== 'string' &&
+        accessibilityControls !== undefined &&
+        accessibilityControls.current !== null
+      ) {
+        warnOnce(
+          'accessibilityControlsRef',
+          'accessibilityControls should be specfied as a string matching the nativeID of the target component.  Support of specifying accessibilityControls using a ref is deprecated and will be removed in a future release',
+        );
         setControlsTarget(findNodeHandle(accessibilityControls.current));
       }
     }, [accessibilityControls]);
@@ -78,23 +96,22 @@ const ViewWin32: React.AbstractComponent<
     const focusRef = useRef(null);
 
     const setLocalRef = useCallback(
-      (instance : View | null) => {
-        focusRef.current = instance;  
+      (instance: View | null) => {
+        focusRef.current = instance;
         /**
          * Add focus() as a callable function to the forwarded reference.
          */
-        if (instance)
-        {
+        if (instance) {
           instance.focus = () => {
             UIManager.dispatchViewManagerCommand(
               findNodeHandle(instance),
               UIManager.getViewManagerConfig('RCTView').Commands.focus,
-              null
-              );
+              null,
+            );
           };
         }
-    },
-    [focusRef],
+      },
+      [focusRef],
     );
 
     const setNativeRef = useMergeRefs<View | null>({
@@ -102,16 +119,30 @@ const ViewWin32: React.AbstractComponent<
       forwardedRef,
     });
 
-    return <View ref={setNativeRef}
-    {...rest}
-    {...((controlsTarget !== null) ? {accessibilityControls:controlsTarget} : {})}
-    {...((labeledByTarget !== null) ? {accessibilityLabeledBy:labeledByTarget} : {})}
-    {...((describedByTarget !== null) ? {accessibilityDescribedBy:describedByTarget} : {})}
-    {...((typeof accessibilityDescribedBy === "string") ? {accessibilityDescribedBy} : {})}
-    {...((typeof accessibilityControls === "string") ? {accessibilityControls} : {})}
-    />;
-  });
-  
+    return (
+      <View
+        ref={setNativeRef}
+        {...rest}
+        {...(controlsTarget !== null
+          ? {accessibilityControls: controlsTarget}
+          : {})}
+        {...(labeledByTarget !== null
+          ? {accessibilityLabeledBy: labeledByTarget}
+          : {})}
+        {...(describedByTarget !== null
+          ? {accessibilityDescribedBy: describedByTarget}
+          : {})}
+        {...(typeof accessibilityDescribedBy === 'string'
+          ? {accessibilityDescribedBy}
+          : {})}
+        {...(typeof accessibilityControls === 'string'
+          ? {accessibilityControls}
+          : {})}
+      />
+    );
+  },
+);
+
 ViewWin32.displayName = 'ViewWin32';
 
 module.exports = ViewWin32;
