547417c04b6889165dccf7664162f2641833e594
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2023-02-27 11:00:33 -0800

    move enum plugin to shared babel config

--- packages/e2e-test-app/babel.config.js
+++ packages/e2e-test-app/babel.config.js
@@ -1,9 +1,3 @@
 module.exports = {
   extends: "@rnw-scripts/babel-react-native-config",
-  presets: [
-    "module:metro-react-native-babel-preset"
-  ],
-  plugins: [
-    "babel-plugin-transform-flow-enums"
-  ]
 };
