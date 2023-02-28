277df791a86a5360b125d841ecd2149b8a175e53
Danny van Velzen <dannyvv@microsoft.com>
2023-02-28 14:40:07 -0800

    Undo the validation logic of the extra argument

--- packages/@rnw-scripts/stamp-version/src/stampVersion.ts
+++ packages/@rnw-scripts/stamp-version/src/stampVersion.ts
@@ -15,10 +15,10 @@ import renderPropsFile from './renderPropsFile';
 
 const {argv} = yargs
   .check(args => {
-    if (args._.length === 2 && semver.valid(<string>args._[0])) {
+    if (args._.length === 1 && semver.valid(<string>args._[0])) {
       return true;
     } else {
-      throw new Error('Usage: stamp-version <version> <commitid>');
+      throw new Error('Usage: stamp-version <version>');
     }
   })
   .showHelpOnFail(false);
