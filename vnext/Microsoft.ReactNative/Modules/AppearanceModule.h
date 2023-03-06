c870737e59d838fc707babf6e8a35d6f48c89985
Chiara Mooney <34109996+chiaramooney@users.noreply.github.com>
2023-03-06 14:31:45 -0800

    Fix Method Declaration

--- vnext/Microsoft.ReactNative/Modules/AppearanceModule.h
+++ vnext/Microsoft.ReactNative/Modules/AppearanceModule.h
@@ -18,7 +18,7 @@ struct Appearance : std::enable_shared_from_this<Appearance> {
   REACT_INIT(Initialize)
   void Initialize(winrt::Microsoft::ReactNative::ReactContext const &reactContext) noexcept;
 
-  REACT_SYNC_METHOD(setColorScheme)
+  REACT_METHOD(setColorScheme)
   void setColorScheme(std::string style) noexcept;
 
   REACT_SYNC_METHOD(getColorScheme)
