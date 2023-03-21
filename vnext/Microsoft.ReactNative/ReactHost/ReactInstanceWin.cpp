b0dffbaaf533e48341f3fe433b4caaf2d7354ed1
tudorms <48035227+tudorms@users.noreply.github.com>
2023-03-21 09:58:11 -0700

    [0.68] CrashManager: handle abort signal
    This PR backports #11374 to 0.68.
    
    If there is a noexcept function on the stack, the CRT just calls abort instead of calling the handler registered via SetUnhandledExceptionFilter.
    
    This change registers a handler for the SIG_ABRT (the abort signal) to attempt to collect crash details.
    
    As before, nothing happens by default, this code path is only enabled if EnableDefaultCrashHandler is set to true.

--- vnext/Microsoft.ReactNative/ReactHost/ReactInstanceWin.cpp
+++ vnext/Microsoft.ReactNative/ReactHost/ReactInstanceWin.cpp
@@ -411,6 +411,7 @@ void ReactInstanceWin::Initialize() noexcept {
 
           devSettings->waitingForDebuggerCallback = GetWaitingForDebuggerCallback();
           devSettings->debuggerAttachCallback = GetDebuggerAttachCallback();
+          devSettings->enableDefaultCrashHandler = m_options.EnableDefaultCrashHandler();
 
 #ifndef CORE_ABI
           devSettings->showDevMenuCallback = [weakThis]() noexcept {
