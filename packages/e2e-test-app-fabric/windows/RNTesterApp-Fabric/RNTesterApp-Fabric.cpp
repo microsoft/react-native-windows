375c9beb04f6d0a8522bfc1fda7c6273f7d2c65b
Chiara Mooney <34109996+chiaramooney@users.noreply.github.com>
2023-03-21 16:10:42 -0700

    Remove Extra File

--- packages/e2e-test-app-fabric/windows/RNTesterApp-Fabric/RNTesterApp-Fabric.cpp
+++ packages/e2e-test-app-fabric/windows/RNTesterApp-Fabric/RNTesterApp-Fabric.cpp
@@ -56,8 +56,8 @@ struct CompReactPackageProvider
 };
 
 // Global Variables:
-WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
-WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+WCHAR szTitle[MAX_LOADSTRING]; // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING]; // the main window class name
 
 winrt::Windows::System::DispatcherQueueController g_dispatcherQueueController{nullptr};
 winrt::Windows::UI::Composition::Compositor g_compositor{nullptr};
@@ -66,103 +66,100 @@ constexpr auto WindowDataProperty = L"WindowData";
 constexpr PCWSTR c_windowClassName = L"MS_REACTNATIVE_RNTESTER_COMPOSITION";
 
 // Forward declarations of functions included in this code module:
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 int RunRNTester(int showCmd, bool useWebDebugger);
 
 struct WindowData {
-        static HINSTANCE s_instance;
-        static constexpr uint16_t defaultDebuggerPort = 9229;
+  static HINSTANCE s_instance;
+  static constexpr uint16_t defaultDebuggerPort = 9229;
 
-        std::wstring m_bundleFile = LR"(index)";
-        bool m_windowInited{false};
-        winrt::Microsoft::ReactNative::CompositionHwndHost m_CompositionHwndHost{nullptr};
-        winrt::Microsoft::ReactNative::ReactNativeHost m_host{nullptr};
-        winrt::Microsoft::ReactNative::ReactInstanceSettings m_instanceSettings{nullptr};
+  std::wstring m_bundleFile = LR"(index)";
+  bool m_windowInited{false};
+  winrt::Microsoft::ReactNative::CompositionHwndHost m_CompositionHwndHost{nullptr};
+  winrt::Microsoft::ReactNative::ReactNativeHost m_host{nullptr};
+  winrt::Microsoft::ReactNative::ReactInstanceSettings m_instanceSettings{nullptr};
 
-        bool m_useWebDebugger{false};
-        bool m_fastRefreshEnabled{true};
-        bool m_useDirectDebugger{false};
-        bool m_breakOnNextLine{false};
-        uint16_t m_debuggerPort{defaultDebuggerPort};
-        xaml::ElementTheme m_theme{xaml::ElementTheme::Default};
+  bool m_useWebDebugger{false};
+  bool m_fastRefreshEnabled{true};
+  bool m_useDirectDebugger{false};
+  bool m_breakOnNextLine{false};
+  uint16_t m_debuggerPort{defaultDebuggerPort};
+  xaml::ElementTheme m_theme{xaml::ElementTheme::Default};
 
-        WindowData(const winrt::Microsoft::ReactNative::CompositionHwndHost &compHost)
-            : m_CompositionHwndHost(compHost) {
-                winrt::Microsoft::ReactNative::Composition::CompositionUIService::SetCompositionContext(
-                    InstanceSettings().Properties(),
-                    winrt::Microsoft::ReactNative::Composition::CompositionContextHelper::CreateContext(g_compositor));
-        }
+  WindowData(const winrt::Microsoft::ReactNative::CompositionHwndHost &compHost) : m_CompositionHwndHost(compHost) {
+    winrt::Microsoft::ReactNative::Composition::CompositionUIService::SetCompositionContext(
+        InstanceSettings().Properties(),
+        winrt::Microsoft::ReactNative::Composition::CompositionContextHelper::CreateContext(g_compositor));
+  }
 
-        static WindowData *GetFromWindow(HWND hwnd) {
-                auto data = reinterpret_cast<WindowData *>(GetProp(hwnd, WindowDataProperty));
-                return data;
-        }
+  static WindowData *GetFromWindow(HWND hwnd) {
+    auto data = reinterpret_cast<WindowData *>(GetProp(hwnd, WindowDataProperty));
+    return data;
+  }
 
-        winrt::Microsoft::ReactNative::ReactNativeHost Host() noexcept {
-                if (!m_host) {
-                        m_host = winrt::Microsoft::ReactNative::ReactNativeHost();
-                        m_host.InstanceSettings(InstanceSettings());
-                }
+  winrt::Microsoft::ReactNative::ReactNativeHost Host() noexcept {
+    if (!m_host) {
+      m_host = winrt::Microsoft::ReactNative::ReactNativeHost();
+      m_host.InstanceSettings(InstanceSettings());
+    }
 
-                return m_host;
-        }
-        winrt::Microsoft::ReactNative::ReactInstanceSettings InstanceSettings() noexcept {
-                if (!m_instanceSettings) {
-                        m_instanceSettings = winrt::Microsoft::ReactNative::ReactInstanceSettings();
-                }
+    return m_host;
+  }
+  winrt::Microsoft::ReactNative::ReactInstanceSettings InstanceSettings() noexcept {
+    if (!m_instanceSettings) {
+      m_instanceSettings = winrt::Microsoft::ReactNative::ReactInstanceSettings();
+    }
 
-                return m_instanceSettings;
-        }
+    return m_instanceSettings;
+  }
 
-        LRESULT RenderApp(HWND hwnd) {
-                PCWSTR appName = L"RNTesterApp";
+  LRESULT RenderApp(HWND hwnd) {
+    PCWSTR appName = L"RNTesterApp";
 
-                WCHAR workingDir[MAX_PATH];
-                GetCurrentDirectory(MAX_PATH, workingDir);
+    WCHAR workingDir[MAX_PATH];
+    GetCurrentDirectory(MAX_PATH, workingDir);
 
-                auto host = Host();
-                // Disable until we have a 3rd party story for custom components
-                // RegisterAutolinkedNativeModulePackages(host.PackageProviders()); // Includes any
-                // autolinked modules
+    auto host = Host();
+    // Disable until we have a 3rd party story for custom components
+    // RegisterAutolinkedNativeModulePackages(host.PackageProviders()); // Includes any
+    // autolinked modules
 
-                host.InstanceSettings().JavaScriptBundleFile(m_bundleFile);
+    host.InstanceSettings().JavaScriptBundleFile(m_bundleFile);
 
-                host.InstanceSettings().UseWebDebugger(m_useWebDebugger);
-                host.InstanceSettings().UseDirectDebugger(m_useDirectDebugger);
-                host.InstanceSettings().BundleRootPath(
-                    std::wstring(L"file:").append(workingDir).append(L"\\Bundle\\").c_str());
-                host.InstanceSettings().DebuggerBreakOnNextLine(m_breakOnNextLine);
-                host.InstanceSettings().UseFastRefresh(m_fastRefreshEnabled);
-                host.InstanceSettings().DebuggerPort(m_debuggerPort);
-                host.InstanceSettings().UseDeveloperSupport(true);
+    host.InstanceSettings().UseWebDebugger(m_useWebDebugger);
+    host.InstanceSettings().UseDirectDebugger(m_useDirectDebugger);
+    host.InstanceSettings().BundleRootPath(std::wstring(L"file:").append(workingDir).append(L"\\Bundle\\").c_str());
+    host.InstanceSettings().DebuggerBreakOnNextLine(m_breakOnNextLine);
+    host.InstanceSettings().UseFastRefresh(m_fastRefreshEnabled);
+    host.InstanceSettings().DebuggerPort(m_debuggerPort);
+    host.InstanceSettings().UseDeveloperSupport(true);
 
-                host.PackageProviders().Append(winrt::make<CompReactPackageProvider>());
-                winrt::Microsoft::ReactNative::ReactCoreInjection::SetTopLevelWindowId(
-                    host.InstanceSettings().Properties(), reinterpret_cast<uint64_t>(hwnd));
+    host.PackageProviders().Append(winrt::make<CompReactPackageProvider>());
+    winrt::Microsoft::ReactNative::ReactCoreInjection::SetTopLevelWindowId(
+        host.InstanceSettings().Properties(), reinterpret_cast<uint64_t>(hwnd));
 
-                // Nudge the ReactNativeHost to create the instance and wrapping context
-                host.ReloadInstance();
+    // Nudge the ReactNativeHost to create the instance and wrapping context
+    host.ReloadInstance();
 
-                winrt::Microsoft::ReactNative::ReactViewOptions viewOptions;
-                viewOptions.ComponentName(appName);
-                m_CompositionHwndHost.ReactViewHost(
-                    winrt::Microsoft::ReactNative::ReactCoreInjection::MakeViewHost(host, viewOptions));
+    winrt::Microsoft::ReactNative::ReactViewOptions viewOptions;
+    viewOptions.ComponentName(appName);
+    m_CompositionHwndHost.ReactViewHost(
+        winrt::Microsoft::ReactNative::ReactCoreInjection::MakeViewHost(host, viewOptions));
 
-                auto windowData = WindowData::GetFromWindow(hwnd);
-                if (!windowData->m_windowInited) {
-                    m_CompositionHwndHost.Initialize((uint64_t)hwnd);
-                    windowData->m_windowInited = true;
-                }
-                return 0;
-        }
+    auto windowData = WindowData::GetFromWindow(hwnd);
+    if (!windowData->m_windowInited) {
+      m_CompositionHwndHost.Initialize((uint64_t)hwnd);
+      windowData->m_windowInited = true;
+    }
+    return 0;
+  }
 
-        LRESULT TranslateMessage(UINT message, WPARAM wparam, LPARAM lparam) noexcept {
-                if (m_CompositionHwndHost) {
-                        return static_cast<LRESULT>(m_CompositionHwndHost.TranslateMessage(message, wparam, lparam));
-                }
-                return 0;
-        }
-
+  LRESULT TranslateMessage(UINT message, WPARAM wparam, LPARAM lparam) noexcept {
+    if (m_CompositionHwndHost) {
+      return static_cast<LRESULT>(m_CompositionHwndHost.TranslateMessage(message, wparam, lparam));
+    }
+    return 0;
+  }
 };
 
 extern "C" IMAGE_DOS_HEADER __ImageBase;
@@ -173,115 +170,114 @@ HINSTANCE WindowData::s_instance = reinterpret_cast<HINSTANCE>(&__ImageBase);
 //
 //  PURPOSE: Processes messages for the main window.
 //
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+  auto windowData = WindowData::GetFromWindow(hWnd);
+  if (windowData) {
+    auto result = WindowData::GetFromWindow(hWnd)->TranslateMessage(message, wParam, lParam);
+    if (result)
+      return result;
+  }
+
+  switch (message) {
+    case WM_DESTROY: {
+      delete WindowData::GetFromWindow(hWnd);
+      SetProp(hWnd, WindowDataProperty, 0);
+      PostQuitMessage(0);
+      return 0;
+    }
+    case WM_NCCREATE: {
+      auto cs = reinterpret_cast<CREATESTRUCT *>(lParam);
+      auto windowData = static_cast<WindowData *>(cs->lpCreateParams);
+      WINRT_ASSERT(windowData);
+      SetProp(hWnd, WindowDataProperty, reinterpret_cast<HANDLE>(windowData));
+      break;
+    }
+    case WM_GETOBJECT: {
+      if (lParam == UiaRootObjectId) {
         auto windowData = WindowData::GetFromWindow(hWnd);
-        if (windowData) {
-                auto result = WindowData::GetFromWindow(hWnd)->TranslateMessage(message, wParam, lParam);
-                if (result)
-                        return result;
-        }
+        if (!windowData->m_windowInited)
+          break;
 
-        switch (message) {
-                case WM_DESTROY: {
-                        delete WindowData::GetFromWindow(hWnd);
-                        SetProp(hWnd, WindowDataProperty, 0);
-                        PostQuitMessage(0);
-                        return 0;
-                }
-                case WM_NCCREATE: {
-                        auto cs = reinterpret_cast<CREATESTRUCT *>(lParam);
-                        auto windowData = static_cast<WindowData *>(cs->lpCreateParams);
-                        WINRT_ASSERT(windowData);
-                        SetProp(hWnd, WindowDataProperty, reinterpret_cast<HANDLE>(windowData));
-                        break;
-                }
-                case WM_GETOBJECT: {
-                        if (lParam == UiaRootObjectId) {
-                          auto windowData = WindowData::GetFromWindow(hWnd);
-                          if (!windowData->m_windowInited)
-                            break;
+        auto hwndHost = windowData->m_CompositionHwndHost;
+        winrt::com_ptr<IRawElementProviderSimple> spReps;
+        hwndHost.UiaProvider().as(spReps);
+        LRESULT lResult = UiaReturnRawElementProvider(hWnd, wParam, lParam, spReps.get());
+        return lResult;
+      }
+    }
+  }
 
-                          auto hwndHost = windowData->m_CompositionHwndHost;
-                          winrt::com_ptr<IRawElementProviderSimple> spReps;
-                          hwndHost.UiaProvider().as(spReps);
-                          LRESULT lResult = UiaReturnRawElementProvider(hWnd, wParam, lParam, spReps.get());
-                          return lResult;
-                        }
-                }
-        }
-
-        return DefWindowProc(hWnd, message, wParam, lParam);
+  return DefWindowProc(hWnd, message, wParam, lParam);
 }
 
 int RunRNTester(int showCmd, bool useWebDebugger) {
-        constexpr PCWSTR appName = L"React Native Tester (Composition)";
+  constexpr PCWSTR appName = L"React Native Tester (Composition)";
 
-        auto windowData = std::make_unique<WindowData>(winrt::Microsoft::ReactNative::CompositionHwndHost());
-        HWND hwnd = CreateWindow(
-            c_windowClassName,
-            appName,
-            WS_OVERLAPPEDWINDOW,
-            CW_USEDEFAULT,
-            CW_USEDEFAULT,
-            CW_USEDEFAULT,
-            CW_USEDEFAULT,
-            nullptr,
-            nullptr,
-            WindowData::s_instance,
-            windowData.get());
+  auto windowData = std::make_unique<WindowData>(winrt::Microsoft::ReactNative::CompositionHwndHost());
+  HWND hwnd = CreateWindow(
+      c_windowClassName,
+      appName,
+      WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT,
+      CW_USEDEFAULT,
+      CW_USEDEFAULT,
+      CW_USEDEFAULT,
+      nullptr,
+      nullptr,
+      WindowData::s_instance,
+      windowData.get());
 
-        WINRT_VERIFY(hwnd);
-        winrt::check_win32(!hwnd);
+  WINRT_VERIFY(hwnd);
+  winrt::check_win32(!hwnd);
 
-        windowData.release();
+  windowData.release();
 
-        ShowWindow(hwnd, showCmd);
-        UpdateWindow(hwnd);
-        SetFocus(hwnd);
-        WindowData::GetFromWindow(hwnd)->RenderApp(hwnd);
+  ShowWindow(hwnd, showCmd);
+  UpdateWindow(hwnd);
+  SetFocus(hwnd);
+  WindowData::GetFromWindow(hwnd)->RenderApp(hwnd);
 
-        HACCEL hAccelTable = LoadAccelerators(WindowData::s_instance, MAKEINTRESOURCE(IDC_RNTESTER_COMPOSITION));
+  HACCEL hAccelTable = LoadAccelerators(WindowData::s_instance, MAKEINTRESOURCE(IDC_RNTESTER_COMPOSITION));
 
-        MSG msg = {};
-        while (GetMessage(&msg, nullptr, 0, 0)) {
-                if (!TranslateAccelerator(hwnd, hAccelTable, &msg)) {
-                        TranslateMessage(&msg);
-                        DispatchMessage(&msg);
-                }
-        }
-        return (int)msg.wParam;
+  MSG msg = {};
+  while (GetMessage(&msg, nullptr, 0, 0)) {
+    if (!TranslateAccelerator(hwnd, hAccelTable, &msg)) {
+      TranslateMessage(&msg);
+      DispatchMessage(&msg);
+    }
+  }
+  return (int)msg.wParam;
 }
 
 _Use_decl_annotations_ int CALLBACK WinMain(HINSTANCE instance, HINSTANCE, PSTR /* commandLine */, int showCmd) {
-        WNDCLASSEXW wcex = {};
-        wcex.cbSize = sizeof(WNDCLASSEX);
-        wcex.style = CS_HREDRAW | CS_VREDRAW;
-        wcex.lpfnWndProc = &WndProc;
-        wcex.cbClsExtra = DLGWINDOWEXTRA;
-        wcex.cbWndExtra = sizeof(WindowData *);
-        wcex.hInstance = WindowData::s_instance;
-        wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
-        wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-        wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_RNTESTER_COMPOSITION);
-        wcex.lpszClassName = c_windowClassName;
-        wcex.hIcon = LoadIconW(instance, MAKEINTRESOURCEW(IDI_ICON1));
-        ATOM classId = RegisterClassEx(&wcex);
-        WINRT_VERIFY(classId);
-        winrt::check_win32(!classId);
+  WNDCLASSEXW wcex = {};
+  wcex.cbSize = sizeof(WNDCLASSEX);
+  wcex.style = CS_HREDRAW | CS_VREDRAW;
+  wcex.lpfnWndProc = &WndProc;
+  wcex.cbClsExtra = DLGWINDOWEXTRA;
+  wcex.cbWndExtra = sizeof(WindowData *);
+  wcex.hInstance = WindowData::s_instance;
+  wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+  wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+  wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_RNTESTER_COMPOSITION);
+  wcex.lpszClassName = c_windowClassName;
+  wcex.hIcon = LoadIconW(instance, MAKEINTRESOURCEW(IDI_ICON1));
+  ATOM classId = RegisterClassEx(&wcex);
+  WINRT_VERIFY(classId);
+  winrt::check_win32(!classId);
 
-        DispatcherQueueOptions options{
-            sizeof(DispatcherQueueOptions), /* dwSize */
-            DQTYPE_THREAD_CURRENT, /* threadType */
-            DQTAT_COM_ASTA /* apartmentType */
-        };
+  DispatcherQueueOptions options{
+      sizeof(DispatcherQueueOptions), /* dwSize */
+      DQTYPE_THREAD_CURRENT, /* threadType */
+      DQTAT_COM_ASTA /* apartmentType */
+  };
 
-        // Need to have a Dispatcher on the current thread to be able to create a Compositor
-        winrt::check_hresult(CreateDispatcherQueueController(
-            options,
-            reinterpret_cast<ABI::Windows::System::IDispatcherQueueController **>(
-                winrt::put_abi(g_dispatcherQueueController))));
+  // Need to have a Dispatcher on the current thread to be able to create a Compositor
+  winrt::check_hresult(CreateDispatcherQueueController(
+      options,
+      reinterpret_cast<ABI::Windows::System::IDispatcherQueueController **>(
+          winrt::put_abi(g_dispatcherQueueController))));
 
-        g_compositor = winrt::Windows::UI::Composition::Compositor();
-        return RunRNTester(showCmd, false);
+  g_compositor = winrt::Windows::UI::Composition::Compositor();
+  return RunRNTester(showCmd, false);
 }
