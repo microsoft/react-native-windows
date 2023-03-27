e250c7402a5a34ce790a4e49ad2e100de93104aa
Chiara Mooney <34109996+chiaramooney@users.noreply.github.com>
2023-03-27 12:04:52 -0700

    Address Feedback

--- packages/e2e-test-app-fabric/jest.setup.js
+++ packages/e2e-test-app-fabric/jest.setup.js
@@ -14,21 +14,4 @@ const {LogBox} = require('react-native');
 const screenshotDir = './errorShots';
 fs.mkdirSync(screenshotDir, {recursive: true});
 
-// Register to screenshot on each test failure
-// TODO - use a jest reporter to create screenshots
-/*
-global.jasmine.addReporter({
-  specDone: async result => {
-    if (result.status === 'failed') {
-      const friendlySpecName = sanitizeFilename(
-        `${result.fullName.replace(/\s/g, '-')}.png`,
-      );
-
-      const filename = path.join(screenshotDir, friendlySpecName);
-      await global.browser.saveScreenshot(filename);
-    }
-  },
-});
-*/
-
 LogBox.ignoreAllLogs(true);
