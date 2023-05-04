d0549331ce2e237f19ab7a0410e89fbbe59785cf
Andrew <30809111+acoates-ms@users.noreply.github.com>
2023-05-03 18:51:29 -0700

    Keep name consistent

--- packages/@react-native-windows/cli/src/healthCheckList.ts
+++ packages/@react-native-windows/cli/src/healthCheckList.ts
@@ -1,6 +1,6 @@
 // Store list of health checks here to avoid having to launch PowerShell on every react-native config call
 export const HealthCheckList = [
-  [false, "FreeSpace", "Free space on E: > 15 GB"],
+  [false, "FreeSpace", "Free space on current drive > 15 GB"],
   [false, "InstalledMemory", "Installed memory >= 16 GB"],
   [true, "WindowsVersion", "Windows version >= 10.0.16299.0"],
   [true, "DeveloperMode", "Developer mode is on"],
