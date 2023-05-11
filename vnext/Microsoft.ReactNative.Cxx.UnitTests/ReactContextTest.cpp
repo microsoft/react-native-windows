24a95892896de67ae58a2293a18865673a49bc63
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2023-05-11 13:09:37 -0700

    Add option to load ship bundles from metro (#11595)
    * Add option to load ship bundles from metro
    
    * Change files
    
    * fix
    
    * format
    
    * keep default value the same as before
    
    * better filter file update

--- vnext/Microsoft.ReactNative.Cxx.UnitTests/ReactContextTest.cpp
+++ vnext/Microsoft.ReactNative.Cxx.UnitTests/ReactContextTest.cpp
@@ -111,6 +111,10 @@ struct ReactContextStub : implements<ReactContextStub, IReactContext> {
     VerifyElseCrashSz(false, "Not implemented");
   }
 
+  bool RequestDevBundle() noexcept {
+    VerifyElseCrashSz(false, "Not implemented");
+  }
+
   LoadingState LoadingState() noexcept {
     VerifyElseCrashSz(false, "Not implemented");
   }
