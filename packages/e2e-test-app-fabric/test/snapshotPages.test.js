04ce7d4a433dbaf6d7686307616088a1db7a619b
Chiara Mooney <34109996+chiaramooney@users.noreply.github.com>
2023-05-15 16:15:23 -0700

    Save State

--- packages/e2e-test-app-fabric/test/snapshotPages.test.js
+++ packages/e2e-test-app-fabric/test/snapshotPages.test.js
@@ -1,11 +1,6 @@
 const React = require('react');
 import {create, act} from 'react-test-renderer';
 const Example = require('@react-native-windows/tester/js/examples/View/ViewExample.windows');
-const ReactNative = require('react-native');
-
-function Control(props) {
-  return <ReactNative.Switch />;
-}
 
 test('Control', () => {
   const tree = create('View').toJSON();
