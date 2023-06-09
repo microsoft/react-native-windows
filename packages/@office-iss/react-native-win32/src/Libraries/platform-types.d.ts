c763debec5be129b090583b03532bc109d5f0590
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2023-06-08 18:48:16 -0700

    [Win32] Type definition files shouldnt use win32 extension (#11726)
    * Type definition files shouldnt use win32 extension
    
    * Change files

--- packages/@office-iss/react-native-win32/src/Libraries/platform-types.d.ts
+++ packages/@office-iss/react-native-win32/src/Libraries/platform-types.d.ts
@@ -5,12 +5,12 @@
 *        need to also be added to index.win32.js
 */
 
-import {IViewWin32Props as IViewWin32PropsOnly} from './Components/View/ViewPropTypes.win32';
-import { AccessibilityPropsWin32 } from '@office-iss/react-native-win32/Libraries/Components/View/ViewAccessibility.win32';
-export { AccessibilityPropsWin32 } from '@office-iss/react-native-win32/Libraries/Components/View/ViewAccessibility.win32';
+import {IViewWin32Props as IViewWin32PropsOnly} from './Components/View/ViewPropTypes';
+import { AccessibilityPropsWin32 } from '@office-iss/react-native-win32/Libraries/Components/View/ViewAccessibility';
+export { AccessibilityPropsWin32 } from '@office-iss/react-native-win32/Libraries/Components/View/ViewAccessibility';
 export type IViewWin32Props = IViewWin32PropsOnly & AccessibilityPropsWin32;
 export {ViewWin32} from './Components/View/ViewWin32';
-export {IKeyboardEvent, IHandledKeyboardEvent, EventPhase} from './Components/View/ViewPropTypes.win32';
+export {IKeyboardEvent, IHandledKeyboardEvent, EventPhase} from './Components/View/ViewPropTypes';
 import {ITextWin32Props as ITextWin32PropsOnly} from './Components/Text/TextWin32.Props';
 export type ITextWin32Props = ITextWin32PropsOnly & AccessibilityPropsWin32;
 export {TextWin32TextStyle } from './Components/Text/TextWin32.Props';
