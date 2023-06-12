693e578aeaebb22df61adc0890147387907d603b
Jon Thysell <jthysell@microsoft.com>
2023-06-12 16:27:59 -0700

    [0.72] Migrate eslint-config from `@react-native-community` to `@react-native` (#11746)
    \## Description
    
    This PR upgrades the dependency as it was re-named upstream.
    
    \### Type of Change
    - Bug fix (non-breaking change which fixes an issue)
    
    \### Why
    What is the motivation for this change? Add a few sentences describing
    the context and overall goals of the pull request's commits.
    
    Closes #11742
    
    \### What
    Changed the dependencies in `@rnw-scripts/eslint-config`'s package.json.
    
    \## Screenshots
    N/A
    
    \## Testing
    Ran `yarn lint` and saw no changes.

--- packages/@rnw-scripts/eslint-config/eslintrc.js
+++ packages/@rnw-scripts/eslint-config/eslintrc.js
@@ -7,7 +7,7 @@
  */
 
 module.exports = {
-  extends: ['@react-native-community', 'prettier'],
+  extends: ['@react-native', 'prettier'],
   rules: {
     'jest/no-disabled-tests': 'off',
     'react-native/no-inline-styles': 'off',
