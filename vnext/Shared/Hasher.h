e91557cfef86a9b020697af03c71a42f7fff6b88
tudorms <tudor.mihai@microsoft.com>
2023-06-19 16:12:11 -0700

    Address CR comments

--- vnext/Shared/Hasher.h
+++ vnext/Shared/Hasher.h
@@ -1,12 +1,18 @@
 #include <bcrypt.h>
+#include <optional>
 #include <vector>
 
 namespace Microsoft::ReactNative {
+  std::optional<std::vector<std::uint8_t>> GetSHA256Hash(const void* pb, size_t cb);
+
   class SHA256Hasher
   {
   public:
     SHA256Hasher();
     ~SHA256Hasher();
+
+    SHA256Hasher(const SHA256Hasher&) = delete;
+    SHA256Hasher& operator=(const SHA256Hasher&) = delete;
 
     std::vector<std::uint8_t> GetHashValue();
     void HashData(const void* pb, size_t cb);
@@ -15,8 +21,5 @@ namespace Microsoft::ReactNative {
     BCRYPT_ALG_HANDLE m_hAlg;
     BCRYPT_HASH_HANDLE m_hHash;
     std::vector<std::uint8_t> m_hashObject;
-
-    SHA256Hasher(const SHA256Hasher&);
-    SHA256Hasher& operator=(const SHA256Hasher&);
   };
 } // namespace Microsoft::ReactNative
