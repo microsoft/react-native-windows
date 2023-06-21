9fda1d8b70562bfb1d312fcc5d982b42a875aa1b
Vladimir Morozov <vmorozov@microsoft.com>
2023-06-20 19:37:06 -0700

    Format code

--- vnext/Shared/Hasher.h
+++ vnext/Shared/Hasher.h
@@ -2,21 +2,20 @@
 #include <vector>
 
 namespace Microsoft::ReactNative {
-  class SHA256Hasher
-  {
-  public:
-    SHA256Hasher();
-    ~SHA256Hasher();
+class SHA256Hasher {
+ public:
+  SHA256Hasher();
+  ~SHA256Hasher();
 
-    std::vector<std::uint8_t> GetHashValue();
-    void HashData(const void* pb, uint32_t cb);
+  std::vector<std::uint8_t> GetHashValue();
+  void HashData(const void *pb, uint32_t cb);
 
-  private:
-    BCRYPT_ALG_HANDLE m_hAlg;
-    BCRYPT_HASH_HANDLE m_hHash;
-    std::vector<std::uint8_t> m_hashObject;
+ private:
+  BCRYPT_ALG_HANDLE m_hAlg;
+  BCRYPT_HASH_HANDLE m_hHash;
+  std::vector<std::uint8_t> m_hashObject;
 
-    SHA256Hasher(const SHA256Hasher&);
-    SHA256Hasher& operator=(const SHA256Hasher&);
-  };
+  SHA256Hasher(const SHA256Hasher &);
+  SHA256Hasher &operator=(const SHA256Hasher &);
+};
 } // namespace Microsoft::ReactNative
