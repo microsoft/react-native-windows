d61f98556dc604a1edd92875d1e4dd244765152d
Julio César Rocha <JunielKatarn@users.noreply.github.com>
2023-07-03 11:44:04 -0700

    Define HTTP TurboModule (#11825)
    * Declare TurboModule
    
    * Generate NativeNetworkingWindows spec
    
    * Define HttpTurboModule
    
    * Begin resource setup
    
    * Initialize: Up to SetOnData
    
    * Define wchar_t event names
    
    * Migrate IRequestBodyHandler to JSValue
    
    IMPORTANT: Migrate and fix RequestArgs::Data
    
    * Migrate IResponseHandler to JSValue
    
    * Rename m_onDataDynamic as m_onDataObject
    
    * Implement Microsoft::React::Modules::ToJSValue
    
    * Migrate IHttpResource to JSValue
    
    * Fix Playground flavors ordering
    
    * Finish implementing Initialize
    
    * Define ToDynamic
    
    * Change files
    
    * Revert module.g.cpp
    
    * Revert "Revert module.g.cpp"
    
    This reverts commit 2f6c88a0a4e5a50427aa6c23dc4c212358bbed04.
    
    * Define target SetCppWinRTReferences
    
    * clang format
    
    * Revert temporary symbol exports
    
    * Fix RedirectHttpFilterUnitTest import order
    
    * Preserve include ordering in RedirectHttpFilterUnitTest.cpp
    
    * Fix JSValueArray list constructor
    
    * Drop RCTNetworkingNative in favor of NativeNetworkingWindows
    
    * Add NativeNetworkingWindows to overrides
    
    * Fix overrides.json newline
    
    * Desktop int. tests: set RN_PLATFORM to win32
    
    * Use iterator to lookup content types
    
    * Revert module.g.cpp to main
    
    * Use iterator for ToRequestBody content type
    
    * Drop namespace from JSValueObject usage
    
    * Finish HttpTurboModule
    
    * clang format
    
    * Use iOS HTTP spec
    
    * Use DynamicReader and DynamicWriter for folly conversions
    
    * Revert overrides.json
    
    * Don't add newline to overrides.json
    
    * Make RCTNetworking.windows.js a copy of RCTNetworking.ios.js
    
    * Override upgrade

--- vnext/Shared/Modules/CxxModuleUtilities.cpp
+++ vnext/Shared/Modules/CxxModuleUtilities.cpp
@@ -2,6 +2,8 @@
 // Licensed under the MIT License.
 
 #include "CxxModuleUtilities.h"
+#include <DynamicReader.h>
+#include <DynamicWriter.h>
 
 namespace msrn = winrt::Microsoft::ReactNative;
 
@@ -23,6 +25,27 @@ void SendEvent(
     std::wstring_view &&eventName,
     msrn::JSValueObject &&args) noexcept {
   reactContext.EmitJSEvent(L"RCTDeviceEventEmitter", std::move(eventName), std::move(args));
+}
+
+void SendEvent(
+    msrn::ReactContext const &reactContext,
+    std::wstring_view &&eventName,
+    msrn::JSValueArray &&args) noexcept {
+  reactContext.EmitJSEvent(L"RCTDeviceEventEmitter", std::move(eventName), std::move(args));
+}
+
+msrn::JSValue ToJSValue(dynamic &value) noexcept {
+  auto reader = winrt::make<msrn::DynamicReader>(value);
+  auto result = msrn::JSValue::ReadFrom(reader);
+
+  return result;
+}
+
+dynamic ToDynamic(const msrn::JSValue &value) noexcept {
+  auto argWriter = msrn::MakeJSValueArgWriter(value);
+  auto result = msrn::DynamicWriter::ToDynamic(argWriter)[0];
+
+  return result;
 }
 
 } // namespace Microsoft::React::Modules
