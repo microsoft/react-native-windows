d86b7ce058937d3da0b2ddf7ba4e99de2501458e
Jon Thysell (JAUNTY) <jthysell@microsoft.com>
2023-07-11 15:16:01 -0700

    Re-add fixed ImageRequest

---
+++ vnext/Microsoft.ReactNative/Fabric/ImageRequest.cpp
@@ -0,0 +1,40 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#include "pch.h"
+
+#include <react/renderer/imagemanager/ImageRequest.h>
+
+namespace facebook::react {
+
+ImageRequest::ImageRequest(
+    ImageSource imageSource,
+    std::shared_ptr<const ImageTelemetry> telemetry,
+    SharedFunction<> cancellationFunction)
+    : imageSource_(std::move(imageSource)),
+      telemetry_(std::move(telemetry)),
+      cancelRequest_(std::move(cancellationFunction)) {
+  coordinator_ = std::make_shared<ImageResponseObserverCoordinator>();
+}
+
+void ImageRequest::cancel() const {
+  cancelRequest_();
+}
+
+const ImageSource &ImageRequest::getImageSource() const {
+  return imageSource_;
+}
+
+const std::shared_ptr<const ImageTelemetry> &ImageRequest::getSharedTelemetry() const {
+  return telemetry_;
+}
+
+const ImageResponseObserverCoordinator &ImageRequest::getObserverCoordinator() const {
+  return *coordinator_;
+}
+
+const std::shared_ptr<const ImageResponseObserverCoordinator> &ImageRequest::getSharedObserverCoordinator() const {
+  return coordinator_;
+}
+
+} // namespace facebook::react
