d82b2f70618aa0d842043c539cbee00938cbec53
Chiara Mooney <34109996+chiaramooney@users.noreply.github.com>
2023-07-25 17:14:54 -0700

    Save State: Min Repro BatchedBridge

--- packages/e2e-test-app-fabric/test/snapshotPages.test.js
+++ packages/e2e-test-app-fabric/test/snapshotPages.test.js
@@ -5,14 +5,9 @@ test('Control', () => {
   const tree = create('View').toJSON();
   expect(tree).toMatchSnapshot();
 });
-/*
+
 test('Control2', () => {
-  global.__fbBatchedBridgeConfig = true;
-  const PlatformConstants = 'react-native/Libraries/Utilities/NativePlatformConstantsAndroid.js';
-
-  jest.mock('react-native/Libraries/Utilities/NativePlatformConstantsAndroid.js');
-
-  const Example = require('@react-native-windows/tester/js/examples-win/Switch/SwitchExample.windows');
+  const Example = require('@react-native-windows/tester/js/examples-win/Switch/SwitchExample');
   const tree = create(<Example/>).toJSON();
   expect(tree).toMatchSnapshot();
-});*/
+});
