3f17360943b1b27b85a5b62fbb6ef0d00892cb78
Julio C. Rocha <julio.rocha@microsoft.com>
2023-08-04 02:54:24 -0700

    Format FetchTest.js

--- vnext/src/IntegrationTests/FetchTest.js
+++ vnext/src/IntegrationTests/FetchTest.js
@@ -10,37 +10,34 @@
 const React = require('react');
 const ReactNative = require('react-native');
 
-const { AppRegistry, View } = ReactNative;
+const {AppRegistry, View} = ReactNative;
 
-const { TestModule } = ReactNative.NativeModules;
+const {TestModule} = ReactNative.NativeModules;
 
-const uri = 'https://raw.githubusercontent.com/microsoft/react-native-windows/main/.yarnrc.yml';
+const uri =
+  'https://raw.githubusercontent.com/microsoft/react-native-windows/main/.yarnrc.yml';
 const expectedContent = 'enableScripts: false';
 
 class FetchTest extends React.Component {
-
   constructor() {
     super();
-    this.state = { data: 'NONE' };
+    this.state = {data: 'NONE'};
   }
 
   async componentDidMount() {
     const response = await fetch(uri);
     const text = await response.text();
-    this.setState({ data: text });
+    this.setState({data: text});
 
     if (this.state.data === expectedContent) {
       TestModule.markTestPassed(true);
     } else {
       TestModule.markTestPassed(false);
     }
-
   }
 
   render() {
-    return (
-      <View />
-    );
+    return <View />;
   }
 }
 
