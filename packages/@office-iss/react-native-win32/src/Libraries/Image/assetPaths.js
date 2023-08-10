f508bce6ac0283b831d045750d6ab42eb5964de9
Andrew Coates <acoates@microsoft.com>
2023-08-10 13:56:16 -0700

    fix

--- packages/@office-iss/react-native-win32/src/Libraries/Image/assetPaths.js
+++ packages/@office-iss/react-native-win32/src/Libraries/Image/assetPaths.js
@@ -12,7 +12,7 @@
 // https://learn.microsoft.com/en-us/windows/win32/fileio/maximum-file-path-limitation
 // Assets in nested node_modules (common when using pnpm) - end up creating very long paths
 // Using this function we shorten longer paths to prevent paths from hitting the path limit
-function ensureShortPath(str: string) {
+function ensureShortPath(str: string): string {
   if (str.length < 40) return str;
 
   const assetsPrefix = 'assets/';
@@ -21,12 +21,12 @@ function ensureShortPath(str: string) {
     console.warn(`Unexpected asset uri - ${str} may not load correctly.`);
   }
 
-  str = str.slice(assetsPrefix.length);
+  const postStr = str.slice(assetsPrefix.length);
   var hash = 0,
     i,
     chr;
-  for (i = 0; i < str.length; i++) {
-    chr = str.charCodeAt(i);
+  for (i = 0; i < postStr.length; i++) {
+    chr = postStr.charCodeAt(i);
     hash = (hash << 5) - hash + chr;
     hash |= 0; // Convert to 32bit integer
   }
