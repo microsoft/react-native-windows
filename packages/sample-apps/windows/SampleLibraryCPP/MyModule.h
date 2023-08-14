c2908bf0e3487f951199710b17487fcf0e28f6eb
Zihan Chen (MSFT) <53799235+ZihanChen-MSFT@users.noreply.github.com>
2023-08-14 14:05:42 -0700

    Update MyModule.h

--- packages/sample-apps/windows/SampleLibraryCPP/MyModule.h
+++ packages/sample-apps/windows/SampleLibraryCPP/MyModule.h
@@ -3,8 +3,8 @@
 
 #pragma once
 
-// a demo of codegenConfig.windows.separateDataTypes to true in package.json
-// it generates data types in a separate files, but one doesn't include another
+// a demo of setting codegenConfig.windows.separateDataTypes to true in package.json
+// it generates data types in a separate file, but one doesn't include another
 // including them in a expected order is required
 #include "..\..\codegen\NativeMyModuleDataTypes.g.h"
 #include "..\..\codegen\NativeMyModuleSpec.g.h"
