d279bfeefc461766998a50a0d46908c8e8dd9b1e
Krystal Kramer <krsiler@microsoft.com>
2023-08-18 14:21:11 -0400

    minor fix

--- packages/@office-iss/react-native-win32/src/Libraries/Image/Image.win32.js
+++ packages/@office-iss/react-native-win32/src/Libraries/Image/Image.win32.js
@@ -186,8 +186,8 @@ const BaseImage = (props: ImagePropsType, forwardedRef) => {
     disabled: ariaDisabled ?? props.accessibilityState?.disabled,
     expanded: ariaExpanded ?? props.accessibilityState?.expanded,
     selected: ariaSelected ?? props.accessibilityState?.selected,
-    multiselectable: accessibilityState?.multiselectable,
-    required: accessibilityState?.required,
+    multiselectable: props.accessibilityState?.multiselectable,
+    required: props.accessibilityState?.required,
   };
   const accessibilityLabel = props['aria-label'] ?? props.accessibilityLabel;
 
