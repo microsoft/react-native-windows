26ab909b9293f0e5c1ae2b5d5961b3ad854d3552
Jon Thysell (JAUNTY) <jthysell@microsoft.com>
2023-08-24 16:05:38 -0700

    Update to use newly renamed `@react-native/babel-preset` and `@react-native/metro-babel-transformer`

--- packages/@rnw-scripts/babel-react-native-config/babel.config.js
+++ packages/@rnw-scripts/babel-react-native-config/babel.config.js
@@ -8,7 +8,7 @@
 
 module.exports = {
   presets: [
-    'module:metro-react-native-babel-preset',
+    'module:@react-native/babel-preset',
   ],
   plugins: [
     "babel-plugin-transform-flow-enums"
