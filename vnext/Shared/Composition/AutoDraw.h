d34b35a66bce6aaefea2d55dcff5646d6d673dab
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2023-09-05 12:25:25 -0700

    format

--- vnext/Shared/Composition/AutoDraw.h
+++ vnext/Shared/Composition/AutoDraw.h
@@ -5,34 +5,30 @@
 
 namespace Microsoft::ReactNative::Composition {
 
-class AutoDrawDrawingSurface
-{
-public:
-	AutoDrawDrawingSurface(winrt::Microsoft::ReactNative::Composition::ICompositionDrawingSurface& drawingSurface, POINT *offset) noexcept
-	{
+class AutoDrawDrawingSurface {
+ public:
+  AutoDrawDrawingSurface(
+      winrt::Microsoft::ReactNative::Composition::IDrawingSurfaceBrush &drawingSurface,
+      POINT *offset) noexcept {
     drawingSurface.as(m_drawingSurfaceInterop);
     m_drawingSurfaceInterop->BeginDraw(m_d2dDeviceContext.put(), offset);
-	}
+  }
 
-	~AutoDrawDrawingSurface() noexcept
-	{
-    if (m_d2dDeviceContext)
-    {
+  ~AutoDrawDrawingSurface() noexcept {
+    if (m_d2dDeviceContext) {
       m_drawingSurfaceInterop->EndDraw();
     }
-	}
+  }
 
-	operator ID2D1DeviceContext*() noexcept
-	{
-		return m_d2dDeviceContext.get();
-	}
+  operator ID2D1DeviceContext *() noexcept {
+    return m_d2dDeviceContext.get();
+  }
 
-	ID2D1DeviceContext* GetRenderTarget() noexcept
-	{
-		return m_d2dDeviceContext.get();
-	}
+  ID2D1DeviceContext *GetRenderTarget() noexcept {
+    return m_d2dDeviceContext.get();
+  }
 
-private:
+ private:
   winrt::com_ptr<ICompositionDrawingSurfaceInterop> m_drawingSurfaceInterop;
   winrt::com_ptr<ID2D1DeviceContext> m_d2dDeviceContext;
 };
