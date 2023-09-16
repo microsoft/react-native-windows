128d9f0ba8221d29a3d9aa1030d2bde04a42a85e
Julio César Rocha <JunielKatarn@users.noreply.github.com>
2023-09-15 17:50:30 -0700

    [0.71] Revert "Update V8 JSI version to 0.71.8 (#12043)" (#12142)
    * Revert "[0.71] Update V8 JSI version to 0.71.8 (#12043)"
    
    This reverts commit dcb75bb13724478583d873251e4ab7e353447ba8.
    
    * Revert "[0.71] ABI-safe Hermes API and new JSI for Node-API (#11696) (#12033)"
    
    This reverts commit fbed06a9b21ced5eb94aa9c8b42e7ac0934483c2.
    
    * Change files

--- vnext/Shared/HermesSamplingProfiler.cpp
+++ vnext/Shared/HermesSamplingProfiler.cpp
@@ -3,51 +3,19 @@
 
 #include "pch.h"
 
-#include <hermes/hermes_api.h>
+#include <hermes/hermes.h>
 #include <chrono>
 #include <future>
 
-#include "HermesRuntimeHolder.h"
 #include "HermesSamplingProfiler.h"
-#include "IReactDispatcher.h"
-#include "ReactPropertyBag.h"
+#include "HermesShim.h"
 #include "Utils.h"
-
-using namespace winrt::Microsoft::ReactNative;
-using namespace facebook::react;
 
 namespace Microsoft::ReactNative {
 
 namespace {
-
-// Implements an awaiter for Mso::DispatchQueue
-auto resume_in_dispatcher(const IReactDispatcher &dispatcher) noexcept {
-  struct awaitable {
-    awaitable(const IReactDispatcher &dispatcher) noexcept : dispatcher_(dispatcher) {}
-
-    bool await_ready() const noexcept {
-      return false;
-    }
-
-    void await_resume() const noexcept {}
-
-    void await_suspend(std::experimental::coroutine_handle<> resume) noexcept {
-      callback_ = [context = resume.address()]() noexcept {
-        std::experimental::coroutine_handle<>::from_address(context)();
-      };
-      dispatcher_.Post(std::move(callback_));
-    }
-
-   private:
-    IReactDispatcher dispatcher_;
-    ReactDispatcherCallback callback_;
-  };
-
-  return awaitable{dispatcher};
-}
-
 std::future<std::string> getTraceFilePath() noexcept {
-  std::string hermesDataPath = co_await Microsoft::React::getApplicationDataPath(L"Hermes");
+  auto hermesDataPath = co_await Microsoft::React::getApplicationDataPath(L"Hermes");
   std::ostringstream os;
   auto now = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch())
                  .count();
@@ -55,59 +23,39 @@ std::future<std::string> getTraceFilePath() noexcept {
   os << hermesDataPath << "\\cpu_" << now << ".cpuprofile";
   co_return os.str();
 }
-
 } // namespace
 
-std::atomic_bool HermesSamplingProfiler::s_isStarted{false};
+bool HermesSamplingProfiler::s_isStarted = false;
 std::string HermesSamplingProfiler::s_lastTraceFilePath;
 
 std::string HermesSamplingProfiler::GetLastTraceFilePath() noexcept {
   return s_lastTraceFilePath;
 }
 
-winrt::fire_and_forget HermesSamplingProfiler::Start(
-    Mso::CntPtr<Mso::React::IReactContext> const &reactContext) noexcept {
-  bool expectedIsStarted = false;
-  if (s_isStarted.compare_exchange_strong(expectedIsStarted, true)) {
-    IReactDispatcher jsDispatcher = implementation::ReactDispatcher::GetJSDispatcher(reactContext->Properties());
-    ReactPropertyBag propertyBag = ReactPropertyBag(reactContext->Properties());
-
-    co_await resume_in_dispatcher(jsDispatcher);
-    std::shared_ptr<HermesRuntimeHolder> hermesRuntimeHolder = HermesRuntimeHolder::loadFrom(propertyBag);
-    hermesRuntimeHolder->addToProfiling();
-
+winrt::fire_and_forget HermesSamplingProfiler::Start() noexcept {
+  if (!s_isStarted) {
+    s_isStarted = true;
     co_await winrt::resume_background();
-    HermesRuntimeHolder::enableSamplingProfiler();
+    HermesShim::enableSamplingProfiler();
   }
 
   co_return;
 }
 
-std::future<std::string> HermesSamplingProfiler::Stop(
-    Mso::CntPtr<Mso::React::IReactContext> const &reactContext) noexcept {
-  bool expectedIsStarted = true;
-  if (s_isStarted.compare_exchange_strong(expectedIsStarted, false)) {
-    IReactDispatcher jsDispatcher = implementation::ReactDispatcher::GetJSDispatcher(reactContext->Properties());
-    ReactPropertyBag propertyBag = ReactPropertyBag(reactContext->Properties());
-
+std::future<std::string> HermesSamplingProfiler::Stop() noexcept {
+  if (s_isStarted) {
+    s_isStarted = false;
     co_await winrt::resume_background();
-    HermesRuntimeHolder::disableSamplingProfiler();
-
+    HermesShim::disableSamplingProfiler();
     s_lastTraceFilePath = co_await getTraceFilePath();
-    HermesRuntimeHolder::dumpSampledTraceToFile(s_lastTraceFilePath);
-
-    co_await resume_in_dispatcher(jsDispatcher);
-    std::shared_ptr<HermesRuntimeHolder> hermesRuntimeHolder = HermesRuntimeHolder::loadFrom(propertyBag);
-    hermesRuntimeHolder->removeFromProfiling();
-
-    co_await winrt::resume_background();
+    HermesShim::dumpSampledTraceToFile(s_lastTraceFilePath);
   }
 
   co_return s_lastTraceFilePath;
 }
 
 bool HermesSamplingProfiler::IsStarted() noexcept {
-  return s_isStarted.load();
+  return s_isStarted;
 }
 
 } // namespace Microsoft::ReactNative
---
+++ vnext/Shared/V8JSIRuntimeHolder.cpp
@@ -0,0 +1,70 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#include "pch.h"
+
+#include <V8JsiRuntime.h>
+#include "V8JSIRuntimeHolder.h"
+
+#include <atomic>
+#include <queue>
+
+using namespace facebook;
+using namespace facebook::react;
+
+namespace facebook {
+namespace react {
+
+class TaskRunnerAdapter : public v8runtime::JSITaskRunner {
+ public:
+  TaskRunnerAdapter(std::shared_ptr<facebook::react::MessageQueueThread> jsQueue) : jsQueue_(std::move(jsQueue)) {}
+
+  void postTask(std::unique_ptr<v8runtime::JSITask> task) override {
+    std::shared_ptr<v8runtime::JSITask> shared_task(task.release());
+    jsQueue_->runOnQueue([shared_task2 = std::move(shared_task)]() { shared_task2->run(); });
+  }
+
+ private:
+  TaskRunnerAdapter(const TaskRunnerAdapter &) = delete;
+  TaskRunnerAdapter &operator=(const TaskRunnerAdapter &) = delete;
+
+  std::shared_ptr<facebook::react::MessageQueueThread> jsQueue_;
+};
+
+facebook::react::JSIEngineOverride V8JSIRuntimeHolder::getRuntimeType() noexcept {
+  return facebook::react::JSIEngineOverride::V8;
+}
+
+std::shared_ptr<facebook::jsi::Runtime> V8JSIRuntimeHolder::getRuntime() noexcept {
+  std::call_once(once_flag_, [this]() { initRuntime(); });
+
+  if (!runtime_)
+    std::terminate();
+
+  // V8JsiRuntime is not thread safe as of now.
+  if (own_thread_id_ != std::this_thread::get_id())
+    std::terminate();
+
+  return runtime_;
+}
+
+void V8JSIRuntimeHolder::initRuntime() noexcept {
+  v8runtime::V8RuntimeArgs args{};
+
+  if (debuggerPort_ > 0)
+    args.inspectorPort = debuggerPort_;
+
+  args.flags.enableInspector = useDirectDebugger_;
+  args.flags.waitForDebugger = debuggerBreakOnNextLine_;
+  args.debuggerRuntimeName = debuggerRuntimeName_;
+
+  args.foreground_task_runner = std::make_shared<TaskRunnerAdapter>(jsQueue_);
+  args.preparedScriptStore = std::move(preparedScriptStore_);
+
+  runtime_ = v8runtime::makeV8Runtime(std::move(args));
+
+  own_thread_id_ = std::this_thread::get_id();
+}
+
+} // namespace react
+} // namespace facebook
