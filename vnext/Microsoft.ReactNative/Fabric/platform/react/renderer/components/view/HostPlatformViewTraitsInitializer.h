93a156b14d081c6606b75ef20d2f7bc8f2a9f6c3
Eric Rozell <erozell@outlook.com>
2023-09-20 11:23:16 -0400

    [Fabric] Cleans up overrides for HostPlatformViewProps (#12160)
    * [Fabric] Cleans up overrides for HostPlatformViewProps
    
    React Native mobile recently refactored ViewProps to more easily allow host platform overrides in out of tree platforms.
    
    This change moves the logic from WindowsViewProps.h in a bespoke location to an isomorphic location under `vnext/Microsoft.ReactNative/Fabric/platform` in HostPlatformViewProps.h, which is the name of the file Fabric expects for host platform extensions. It also moves the overrides for view flattening traits to HostPlatformViewTraitsInitializer and fixes up props parsing for WindowsViewEvents when the propIteratorSetter feature is not enabled.
    
    * Change files
    
    ---------
    
    Co-authored-by: Eric Rozell <ericroz@meta.com>

--- vnext/Microsoft.ReactNative/Fabric/platform/react/renderer/components/view/HostPlatformViewTraitsInitializer.h
+++ vnext/Microsoft.ReactNative/Fabric/platform/react/renderer/components/view/HostPlatformViewTraitsInitializer.h
@@ -1,9 +1,5 @@
-/*
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
 
 #pragma once
 
@@ -13,20 +9,15 @@
 namespace facebook::react::HostPlatformViewTraitsInitializer {
 
 inline bool formsStackingContext(ViewProps const &viewProps) {
-  return viewProps.elevation != 0;
+  return viewProps.windowsEvents.bits.any();
 }
 
 inline bool formsView(ViewProps const &viewProps) {
-#ifdef ANDROID
-  return viewProps.nativeBackground.has_value() || viewProps.nativeForeground.has_value() || viewProps.focusable ||
-      viewProps.hasTVPreferredFocus || viewProps.needsOffscreenAlphaCompositing ||
-      viewProps.renderToHardwareTextureAndroid;
-#endif
   return viewProps.focusable;
 }
 
 inline ShadowNodeTraits::Trait extraTraits() {
-  return ShadowNodeTraits::Trait::AndroidMapBufferPropsSupported;
+  return ShadowNodeTraits::Trait::None;
 }
 
 } // namespace facebook::react::HostPlatformViewTraitsInitializer
