fa460bc6a0e33b38a0d1e0772ba5a4eb9468d664
Chiara Mooney <34109996+chiaramooney@users.noreply.github.com>
2023-10-12 13:56:30 -0700

    Add Dumps for Page Tiles

--- packages/e2e-test-app-fabric/test/dumpUIA.test.ts
+++ packages/e2e-test-app-fabric/test/dumpUIA.test.ts
@@ -9,18 +9,38 @@ import {app} from '@react-native-windows/automation';
 import {verifyNoErrorLogs} from './Helpers';
 import {dumpVisualTree} from '@react-native-windows/automation-commands';
 
+type RNTesterExampleModule = {
+  title: string;
+  description: string;
+};
+
+type RNTesterModuleInfo = {
+  key: string;
+  module: RNTesterExampleModule;
+};
+
+type RNTesterList = {
+  APIs: RNTesterModuleInfo[];
+  Components: RNTesterModuleInfo[];
+};
+
+const testerList: RNTesterList = require('@react-native-windows/tester/js/utils/RNTesterList');
+
+const apiExamples = testerList.APIs.map(e => e.module.title);
+const componentExamples = testerList.Components.map(e => e.module.title);
+
 afterEach(async () => {
   await verifyNoErrorLogs();
 });
 
-describe('dumpUIA', () => {
-  test('Button UIA Dump', async () => {
+describe('Home Page UIA Tree Dump', () => {
+  test('Components Tab UIA Dump', async () => {
     const componentsTabButton = await app.findElementByTestID('components-tab');
     await componentsTabButton.waitForDisplayed({timeout: 20000});
     const dump = await dumpVisualTree('components-tab');
     expect(dump).toMatchSnapshot();
   });
-  test('Search Bar UIA Dump', async () => {
+  test('APIs Tab UIA Dump', async () => {
     const dump = await dumpVisualTree('apis-tab');
     expect(dump).toMatchSnapshot();
   });
@@ -28,6 +48,24 @@ describe('dumpUIA', () => {
     const dump = await dumpVisualTree('explorer_search');
     expect(dump).toMatchSnapshot();
   });
+  for (const component of componentExamples) {
+    test(component, async () => {
+      const componentsTabButton = await app.findElementByTestID(
+        'components-tab',
+      );
+      await componentsTabButton.click();
+      const dump = await dumpVisualTree(component);
+      expect(dump).toMatchSnapshot();
+    });
+  }
+  for (const api of apiExamples) {
+    test(api, async () => {
+      const apisTabButton = await app.findElementByTestID('apis-tab');
+      await apisTabButton.click();
+      const dump = await dumpVisualTree(api);
+      expect(dump).toMatchSnapshot();
+    });
+  }
 });
 
 export {};
