7d828dba507605838079818205add6b952896569
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2023-11-16 07:51:34 -0800

    fix

--- packages/e2e-test-app-fabric/test/ButtonComponentTest.test.ts
+++ packages/e2e-test-app-fabric/test/ButtonComponentTest.test.ts
@@ -5,7 +5,7 @@
  * @format
  */
 
-//import {dumpVisualTree} from '@react-native-windows/automation-commands';
+import {dumpVisualTree} from '@react-native-windows/automation-commands';
 import {goToComponentExample} from './RNTesterNavigation';
 import {verifyNoErrorLogs} from './Helpers';
 import {app} from '@react-native-windows/automation';
@@ -22,48 +22,44 @@ afterEach(async () => {
 });
 
 describe('Button Tests', () => {
-  test('Control', () => {
-    expect(true).toBe(true);
+  test('Buttons have default styling', async () => {
+    const component = await app.findElementByTestID('button_default_styling');
+    await component.waitForDisplayed({timeout: 5000});
+    const dump = await dumpVisualTree('button_default_styling');
+    expect(dump).toMatchSnapshot();
   });
-  // Disable until tests pass CI
-  // test('Buttons have default styling', async () => {
-  //   const component = await app.findElementByTestID('button_default_styling');
-  //   await component.waitForDisplayed({timeout: 5000});
-  //   const dump = await dumpVisualTree('button_default_styling');
-  //   expect(dump).toMatchSnapshot();
-  // });
-  // test('Buttons can have custom colors', async () => {
-  //   const component = await app.findElementByTestID('cancel_button');
-  //   await component.waitForDisplayed({timeout: 5000});
-  //   const dump = await dumpVisualTree('cancel_button');
-  //   expect(dump).toMatchSnapshot();
-  // });
-  // test('Buttons can be disabled', async () => {
-  //   const component = await app.findElementByTestID('disabled_button');
-  //   await component.waitForDisplayed({timeout: 5000});
-  //   const dump = await dumpVisualTree('disabled_button');
-  //   expect(dump).toMatchSnapshot();
-  // });
-  // test('Buttons can have accessibility labels', async () => {
-  //   const component = await app.findElementByTestID(
-  //     'accessibilityLabel_button',
-  //   );
-  //   await component.waitForDisplayed({timeout: 5000});
-  //   const dump = await dumpVisualTree('accessibilityLabel_button');
-  //   expect(dump).toMatchSnapshot();
-  // });
-  // test('Buttons can have accessibility states', async () => {
-  //   const component = await app.findElementByTestID(
-  //     'accessibilityState_button',
-  //   );
-  //   await component.waitForDisplayed({timeout: 5000});
-  //   const dump = await dumpVisualTree('accessibilityState_button');
-  //   expect(dump).toMatchSnapshot();
-  // });
-  // test('Buttons can have accessibility props', async () => {
-  //   const component = await app.findElementByTestID('accessibility_props');
-  //   await component.waitForDisplayed({timeout: 5000});
-  //   const dump = await dumpVisualTree('accessibility_props');
-  //   expect(dump).toMatchSnapshot();
-  // });
+  test('Buttons can have custom colors', async () => {
+    const component = await app.findElementByTestID('cancel_button');
+    await component.waitForDisplayed({timeout: 5000});
+    const dump = await dumpVisualTree('cancel_button');
+    expect(dump).toMatchSnapshot();
+  });
+  test('Buttons can be disabled', async () => {
+    const component = await app.findElementByTestID('disabled_button');
+    await component.waitForDisplayed({timeout: 5000});
+    const dump = await dumpVisualTree('disabled_button');
+    expect(dump).toMatchSnapshot();
+  });
+  test('Buttons can have accessibility labels', async () => {
+    const component = await app.findElementByTestID(
+      'accessibilityLabel_button',
+    );
+    await component.waitForDisplayed({timeout: 5000});
+    const dump = await dumpVisualTree('accessibilityLabel_button');
+    expect(dump).toMatchSnapshot();
+  });
+  test('Buttons can have accessibility states', async () => {
+    const component = await app.findElementByTestID(
+      'accessibilityState_button',
+    );
+    await component.waitForDisplayed({timeout: 5000});
+    const dump = await dumpVisualTree('accessibilityState_button');
+    expect(dump).toMatchSnapshot();
+  });
+  test('Buttons can have accessibility props', async () => {
+    const component = await app.findElementByTestID('accessibility_props');
+    await component.waitForDisplayed({timeout: 5000});
+    const dump = await dumpVisualTree('accessibility_props');
+    expect(dump).toMatchSnapshot();
+  });
 });
