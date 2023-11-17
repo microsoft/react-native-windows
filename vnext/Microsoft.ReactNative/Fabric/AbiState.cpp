bc1882ace3a7c9413e1f37d6ab8e0e031a0bf07b
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2023-11-17 15:15:34 -0800

    fix

--- vnext/Microsoft.ReactNative/Fabric/AbiState.cpp
+++ vnext/Microsoft.ReactNative/Fabric/AbiState.cpp
@@ -9,11 +9,11 @@ AbiComponentState::AbiComponentState(facebook::react::State::Shared const &state
   m_state = std::static_pointer_cast<facebook::react::ConcreteState<AbiStateData> const>(state);
 }
 
-winrt::IInspectable AbiComponentState::Data() noexcept {
+winrt::Windows::Foundation::IInspectable AbiComponentState::Data() noexcept {
   return m_state->getData().userdata;
 }
 
-void AbiComponentState::UpdateState(winrt::IInspectable data) noexcept {
+void AbiComponentState::UpdateState(winrt::Windows::Foundation::IInspectable data) noexcept {
   AbiStateData state;
   state.userdata = data;
   m_state->updateState(std::move(state));
--- vnext/Microsoft.ReactNative/Fabric/AbiState.h
+++ vnext/Microsoft.ReactNative/Fabric/AbiState.h
@@ -15,14 +15,14 @@ class AbiStateData final {
  public:
   AbiStateData() = default;
   AbiStateData(winrt::Windows::Foundation::IInspectable data) : userdata(data) {}
-  winrt::IInspectable userdata;
+  winrt::Windows::Foundation::IInspectable userdata;
 };
 
 struct AbiComponentState : winrt::implements<AbiComponentState, winrt::Microsoft::ReactNative::IComponentState> {
   AbiComponentState(facebook::react::State::Shared const &state);
 
-  winrt::IInspectable Data() noexcept;
-  void UpdateState(winrt::IInspectable data) noexcept;
+  winrt::Windows::Foundation::IInspectable Data() noexcept;
+  void UpdateState(winrt::Windows::Foundation::IInspectable data) noexcept;
 
  private:
   std::shared_ptr<facebook::react::ConcreteState<AbiStateData> const> m_state;
