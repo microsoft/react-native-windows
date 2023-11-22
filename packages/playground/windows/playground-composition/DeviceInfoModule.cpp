eaea7012d1b53b99a1144650ccffc8f09963385e
Marlene Cota <mcota@microsoft.com>
2023-11-21 16:20:50 -0800

    First pass at Fabric DeviceInfo

--- packages/playground/windows/playground-composition/DeviceInfoModule.cpp
+++ packages/playground/windows/playground-composition/DeviceInfoModule.cpp
@@ -1,36 +1,6 @@
 #include "pch.h"
 
-#include "../../../../vnext/codegen/NativeDeviceInfoSpec.g.h"
-
 #include "NativeModules.h"
-
-// Temporary Work around crash in DeviceInfo when running outside of XAML environment
-// TODO rework built-in DeviceInfo to allow it to be driven without use of HWNDs or XamlApps
-REACT_MODULE(DeviceInfo)
-struct DeviceInfo {
-  using ModuleSpec = Microsoft::ReactNativeSpecs::DeviceInfoSpec;
-
-  REACT_INIT(Initialize)
-  void Initialize(React::ReactContext const &reactContext) noexcept {
-    m_context = reactContext;
-  }
-
-  REACT_GET_CONSTANTS(GetConstants)
-  Microsoft::ReactNativeSpecs::DeviceInfoSpec_DeviceInfoConstants GetConstants() noexcept {
-    Microsoft::ReactNativeSpecs::DeviceInfoSpec_DeviceInfoConstants constants;
-    Microsoft::ReactNativeSpecs::DeviceInfoSpec_DisplayMetrics screenDisplayMetrics;
-    screenDisplayMetrics.fontScale = 1;
-    screenDisplayMetrics.height = 1024;
-    screenDisplayMetrics.width = 1024;
-    screenDisplayMetrics.scale = 1;
-    constants.Dimensions.screen = screenDisplayMetrics;
-    constants.Dimensions.window = screenDisplayMetrics;
-    return constants;
-  }
-
- private:
-  winrt::Microsoft::ReactNative::ReactContext m_context;
-};
 
 // Have to use TurboModules to override built in modules.. so the standard attributed package provider doesn't work.
 struct StubDeviceInfoReactPackageProvider
