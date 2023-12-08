f211b97101f74817a5b02466755917b9d3f17ca3
Julio C. Rocha <julio.rocha@microsoft.com>
2023-12-08 03:06:27 -0800

    Add '=' padding as needed

--- vnext/Desktop.UnitTests/BaseFileReaderResourceUnitTest.cpp
+++ vnext/Desktop.UnitTests/BaseFileReaderResourceUnitTest.cpp
@@ -5,6 +5,9 @@
 
 #include <BaseFileReaderResource.h>
 
+// Windows Libraries
+#include <winrt/Windows.Security.Cryptography.h>
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 using std::shared_ptr;
@@ -14,25 +17,21 @@ using winrt::array_view;
 
 namespace Microsoft::React::Test {
 
-TEST_CLASS(BaseFileReaderResourceUnitTest)
-{
-  TEST_METHOD(Base64EncodesCorrectly)
-  {
-    class DummyBlobPersistor final : public IBlobPersistor
-    {
+TEST_CLASS (BaseFileReaderResourceUnitTest) {
+  TEST_METHOD(Base64EncodesCorrectly) {
+    class DummyBlobPersistor final : public IBlobPersistor {
       std::unordered_map<string, vector<uint8_t>> m_blobs;
 
-    public:
+     public:
 #pragma region IBlobPersistor
 
-      array_view<uint8_t const> ResolveMessage(string&& blobId, int64_t offset, int64_t size) override
-      {
+      array_view<uint8_t const> ResolveMessage(string &&blobId, int64_t offset, int64_t size) override {
         auto dataItr = m_blobs.find(std::move(blobId));
         // Not found.
         if (dataItr == m_blobs.cend())
           throw std::invalid_argument("Blob object not found");
 
-        auto& bytes = (*dataItr).second;
+        auto &bytes = (*dataItr).second;
         auto endBound = static_cast<size_t>(offset + size);
         // Out of bounds.
         if (endBound > bytes.size() || offset >= static_cast<int64_t>(bytes.size()) || offset < 0)
@@ -41,28 +40,26 @@ TEST_CLASS(BaseFileReaderResourceUnitTest)
         return array_view<uint8_t const>(bytes.data() + offset, bytes.data() + endBound);
       }
 
-      void RemoveMessage(string&& /*blobId*/) noexcept override
-      {
+      void RemoveMessage(string && /*blobId*/) noexcept override {
         // Not implemented
       }
 
-      void StoreMessage(vector<uint8_t>&& message, string&& blobId) noexcept override
-      {
+      void StoreMessage(vector<uint8_t> &&message, string &&blobId) noexcept override {
         m_blobs.insert_or_assign(std::move(blobId), std::move(message));
       }
 
-      string StoreMessage(vector<uint8_t>&& /*message*/) noexcept override
-      {
+      string StoreMessage(vector<uint8_t> && /*message*/) noexcept override {
         return "Not implemented";
       }
 
 #pragma endregion IBlobPersistor
     };
 
-    constexpr char expected[] = "data:string;base64,YWJjZAo=";
+    // Base64 expected value coputed with [System.Convert]::ToBase64String('abcd'.ToCharArray())
+    string messageStr = "abcde";
+    constexpr char expected[] = "data:string;base64,YWJjZGU=";
     constexpr char guid[] = "93252b5d-a419-4d98-a928-c3ef386f2445";
 
-    string messageStr = "abcd";
     vector<uint8_t> message{};
     message.reserve(messageStr.size());
     message.insert(message.end(), messageStr.begin(), messageStr.end());
@@ -70,16 +67,10 @@ TEST_CLASS(BaseFileReaderResourceUnitTest)
     shared_ptr<IBlobPersistor> persistor = std::make_shared<DummyBlobPersistor>();
     shared_ptr<IFileReaderResource> reader = std::make_shared<BaseFileReaderResource>(persistor);
     string result;
-    auto resolver = [&result](string&& value)
-    {
-      result = std::move(value);
-    };
-    auto rejecter = [&result](string&&)
-    {
-      result = "ERROR";
-    };
+    auto resolver = [&result](string &&value) { result = std::move(value); };
+    auto rejecter = [&result](string &&) { result = "ERROR"; };
 
-    persistor->StoreMessage(std::move(message), string{ guid });
+    persistor->StoreMessage(std::move(message), string{guid});
 
     reader->ReadAsDataUrl(guid, 0 /*offset*/, messageStr.size(), "string", std::move(resolver), std::move(rejecter));
 
@@ -87,4 +78,4 @@ TEST_CLASS(BaseFileReaderResourceUnitTest)
   }
 };
 
-} //namespace Microsoft::React::Test
+} // namespace Microsoft::React::Test
