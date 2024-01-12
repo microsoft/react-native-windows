a5e60ae253e873996cb3b46cc0a025cfa0479fa8
Eric Rozell <erozell@outlook.com>
2024-01-12 11:02:54 -0500

    Remove irrelevant override that eliminated strict-local
    It seems this change was needed when Text.windows.js was imported at one
    point or another, but yarn lint is clean now when reverting the @flow
    settings to strict-local.
    
    Ran yarn lint to test.
    
    Resolves #7149

--- vnext/src/Libraries/Text/Text.windows.js
+++ vnext/src/Libraries/Text/Text.windows.js
@@ -4,8 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * #7149 should be strict local
- * @flow
+ * @flow strict-local
  * @format
  */
 
