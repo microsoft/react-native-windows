71e19ca1f2c5cd537d74f69cce00f1bf0b1588e9
Julio C. Rocha <julio.rocha@microsoft.com>
2024-01-29 20:52:33 -0800

    Add basic routing skeleton

--- vnext/TestServices/Program.cs
+++ vnext/TestServices/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://localhost:7777");
+
 var app = builder.Build();
+//app.MapGet("/", () => "Hello World!");
+app.UseWebSockets();
 
-app.MapGet("/", () => "Hello World!");
+app.Map("/http0", async context => { });
+app.Map("/ws1", async context =>
+{
 
-app.Run();
+});
+
+//app.Run();
+await app.RunAsync();
