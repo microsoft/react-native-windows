f7ca7a8db0d1c93179de47e546f05c82b2270986
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2024-02-08 16:50:54 -0800

    try applying changes made to rn/preprocessor.js to custom-transformer

--- packages/e2e-test-app/custom-transformer.js
+++ packages/e2e-test-app/custom-transformer.js
@@ -8,17 +8,26 @@
 
 'use strict';
 
+const {transformFromAstSync} = require('@babel/core');
+
 const generate = require('@babel/generator').default;
 const transformer = require('@react-native/metro-babel-transformer');
 module.exports = {
   process(src /*: string */, file /*: string */) /*: {code: string, ...} */ {
-    const {ast} = transformer.transform({
+    let {ast} = transformer.transform({
       filename: file,
       options: {
         inlineRequires: true,
       },
       src,
     });
+    const babelTransformResult = transformFromAstSync(ast, src, {
+      ast: true,
+      retainLines: true,
+      plugins: [require('metro-transform-plugins').inlineRequiresPlugin],
+      sourceType: 'module',
+    });
+    ast = babelTransformResult.ast;
 
     return generate(ast, {}, src);
   },
