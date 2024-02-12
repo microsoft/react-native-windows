e3d3f933a7f632588b48c54e24184a05303cc0ba
Andrew <30809111+acoates-ms@users.noreply.github.com>
2024-02-12 14:34:47 -0800

    Deprecated Web Debugging

--- vnext/template/cpp-app/src/App.cpp
+++ vnext/template/cpp-app/src/App.cpp
@@ -22,17 +22,17 @@ App::App() noexcept
 {
 #if BUNDLE
     JavaScriptBundleFile(L"index.windows");
-    InstanceSettings().UseWebDebugger(false);
     InstanceSettings().UseFastRefresh(false);
 #else
     JavaScriptBundleFile(L"index");
-    InstanceSettings().UseWebDebugger(true);
     InstanceSettings().UseFastRefresh(true);
 #endif
 
 #if _DEBUG
+    InstanceSettings().UseDirectDebugger(true);
     InstanceSettings().UseDeveloperSupport(true);
 #else
+    InstanceSettings().UseDirectDebugger(false);
     InstanceSettings().UseDeveloperSupport(false);
 #endif
 
