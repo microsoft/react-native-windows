419c992a48f5b9a70ee2fe84e8915d6ba22398a6
Andrew <30809111+acoates-ms@users.noreply.github.com>
2024-02-13 09:26:10 -0800

    [Fabric] LogBox crashes when running lifted composition

--- vnext/Microsoft.ReactNative/Fabric/Composition/CompositionHwndHost.h
+++ vnext/Microsoft.ReactNative/Fabric/Composition/CompositionHwndHost.h
@@ -23,26 +23,17 @@ struct CompositionHwndHost : CompositionHwndHostT<CompositionHwndHost> {
   // property UiaProvider
   IInspectable UiaProvider() noexcept;
 
-  winrt::Windows::UI::Composition::Visual RootVisual() const noexcept;
-
   winrt::Microsoft::ReactNative::FocusNavigationResult NavigateFocus(
       const winrt::Microsoft::ReactNative::FocusNavigationRequest &request) noexcept;
 
   LRESULT TranslateMessage(int msg, uint64_t wParam, int64_t lParam) noexcept;
 
  private:
-  // Possibly should be public..
-  winrt::Windows::UI::Composition::Desktop::DesktopWindowTarget Target() const noexcept;
-  winrt::Windows::UI::Composition::Compositor Compositor() const noexcept;
-
-  void CreateDesktopWindowTarget(HWND window);
-  void CreateCompositionRoot();
   void UpdateSize() noexcept;
   float ScaleFactor() noexcept;
 
   HWND m_hwnd;
   winrt::Microsoft::ReactNative::CompositionRootView m_compRootView{nullptr};
-  winrt::Windows::UI::Composition::Desktop::DesktopWindowTarget m_target{nullptr};
   LONG m_height{0};
   LONG m_width{0};
 
