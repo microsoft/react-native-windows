0489eb89b947b74e8f6c0e5315f3fe2cfcbd59cd
Andrew <30809111+acoates-ms@users.noreply.github.com>
2024-02-15 17:01:13 -0800

    fix

--- packages/@react-native-windows/cli/src/e2etest/typesUpToDate.test.ts
+++ packages/@react-native-windows/cli/src/e2etest/typesUpToDate.test.ts
@@ -7,14 +7,25 @@
 import path from 'path';
 
 test('Verify dependencies used for types is updated when cli versions in core update', async () => {
-
   const rnPath = path.dirname(require.resolve('react-native/package.json'));
-  const rnCliPath = path.dirname(require.resolve('@react-native-community/cli', {paths: [rnPath]}));
+  const rnCliPath = path.dirname(
+    require.resolve('@react-native-community/cli', {paths: [rnPath]}),
+  );
   const pkgJson = require(path.resolve(__dirname, '../../package.json'));
 
-  const cliDrPkJson = require(require.resolve('@react-native-community/cli-doctor/package.json', {paths: [rnCliPath]}));
-  expect(pkgJson.devDependencies['@react-native-community/cli-doctor']).toEqual(cliDrPkJson.version);
+  const cliDrPkJson = require(require.resolve(
+    '@react-native-community/cli-doctor/package.json',
+    {paths: [rnCliPath]},
+  ));
+  expect(pkgJson.devDependencies['@react-native-community/cli-doctor']).toEqual(
+    cliDrPkJson.version,
+  );
 
-  const cliTypesPkJson = require(require.resolve('@react-native-community/cli-types/package.json', {paths: [rnCliPath]}));
-  expect(pkgJson.devDependencies['@react-native-community/cli-types']).toEqual(cliTypesPkJson.version);
+  const cliTypesPkJson = require(require.resolve(
+    '@react-native-community/cli-types/package.json',
+    {paths: [rnCliPath]},
+  ));
+  expect(pkgJson.devDependencies['@react-native-community/cli-types']).toEqual(
+    cliTypesPkJson.version,
+  );
 });
--- packages/e2e-test-app/custom-transformer.js
+++ packages/e2e-test-app/custom-transformer.js
@@ -8,10 +8,12 @@
 
 'use strict';
 
+const path = require('path');
+
 const generate = require('@babel/generator').default;
 
 // Require @react-native/metro-babel-transformer from @react-native/metro-config to ensure we pickup the version that @react-native/metro-config uses
-const transformer = require(require.resolve('@react-native/metro-babel-transformer', {paths: [require('path').dirname(require.resolve('@react-native/metro-config/package.json'))]}));
+const transformer = require(require.resolve('@react-native/metro-babel-transformer', {paths: [path.dirname(require.resolve('@react-native/metro-config'))]}));
 module.exports = {
   process(src /*: string */, file /*: string */) /*: {code: string, ...} */ {
     const {ast} = transformer.transform({
