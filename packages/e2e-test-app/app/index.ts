e6faea0b50993836c150ba302458f8cb890f3fae
Andrew <30809111+acoates-ms@users.noreply.github.com>
2024-02-29 09:53:15 -0800

    fix

--- packages/e2e-test-app/app/index.ts
+++ packages/e2e-test-app/app/index.ts
@@ -1,11 +1,11 @@
 
 // This turns on validation that the ViewManager viewconfigs align with the JS ViewConfigs.
 const nativeComponentRegistry = require('react-native/Libraries/NativeComponent/NativeComponentRegistry');
-nativeComponentRegistry.setRuntimeConfigProvider((_name: string) => {
+nativeComponentRegistry.setRuntimeConfigProvider((name: string) => {
   return {
     native: true,
     strict: false,
-    verify: true,
+    verify: name !== 'WindowsTextInput',
   };
 });
 
