b3d30a90858d7b366664a4d0431f4e49db10b5be
Chiara Mooney <34109996+chiaramooney@users.noreply.github.com>
2024-03-05 15:23:49 -0800

    Format

--- vnext/Microsoft.ReactNative/Fabric/platform/react/renderer/graphics/PlatformColorParser.h
+++ vnext/Microsoft.ReactNative/Fabric/platform/react/renderer/graphics/PlatformColorParser.h
@@ -13,10 +13,8 @@
 
 namespace facebook::react {
 
-inline SharedColor parsePlatformColor(
-  const ContextContainer& contextContainer,
-  int32_t surfaceId, 
-  const RawValue &value) {
+inline SharedColor
+parsePlatformColor(const ContextContainer &contextContainer, int32_t surfaceId, const RawValue &value) {
   if (value.hasType<std::unordered_map<std::string, std::string>>()) {
     auto map = (std::unordered_map<std::string, std::vector<std::string>>)value;
     if (map.find("windowsbrush") != map.end()) {
@@ -32,13 +30,9 @@ inline SharedColor parsePlatformColor(
   return {HostPlatformColor::UndefinedColor};
 }
 
-inline void fromRawValue(
-    const ContextContainer& contextContainer,
-    int32_t surfaceId,
-    const RawValue& value,
-    SharedColor& result) {
-  fromRawValueShared(
-      contextContainer, surfaceId, value, result, parsePlatformColor);
+inline void
+fromRawValue(const ContextContainer &contextContainer, int32_t surfaceId, const RawValue &value, SharedColor &result) {
+  fromRawValueShared(contextContainer, surfaceId, value, result, parsePlatformColor);
 }
 
 } // namespace facebook::react
