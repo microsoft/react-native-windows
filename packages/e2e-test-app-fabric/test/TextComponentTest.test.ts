ab96c085c68e1c0eb2e52212c1a7df4b64758c73
Jorge Cabiedes Acosta <jorgecab@meta.com>
2024-03-13 20:12:59 -0700

    Increase Text Test timeout

--- packages/e2e-test-app-fabric/test/TextComponentTest.test.ts
+++ packages/e2e-test-app-fabric/test/TextComponentTest.test.ts
@@ -24,32 +24,32 @@ afterEach(async () => {
 describe('Text Tests', () => {
   test('Text can wrap', async () => {
     const component = await app.findElementByTestID('text-wrap');
-    await component.waitForDisplayed({timeout: 5000});
+    await component.waitForDisplayed({timeout: 10000});
     const dump = await dumpVisualTree('text-wrap');
     expect(dump).toMatchSnapshot();
   });
   test('Padding can be added to Text', async () => {
     const component = await app.findElementByTestID('text-padding');
-    await component.waitForDisplayed({timeout: 5000});
+    await component.waitForDisplayed({timeout: 10000});
     const dump = await dumpVisualTree('text-padding');
     expect(dump).toMatchSnapshot();
   });
   // Disabling until test passes.
   /*test('Text can have a font family', async () => {
     const component = await app.findElementByTestID('text-fontFamily');
-    await component.waitForDisplayed({timeout: 5000});
+    await component.waitForDisplayed({timeout: 10000});
     const dump = await dumpVisualTree('text-fontFamily');
     expect(dump).toMatchSnapshot();
   });*/
   test('Text can have a size', async () => {
     const component = await app.findElementByTestID('text-size');
-    await component.waitForDisplayed({timeout: 5000});
+    await component.waitForDisplayed({timeout: 10000});
     const dump = await dumpVisualTree('text-size');
     expect(dump).toMatchSnapshot();
   });
   test('Text can have a color', async () => {
     const component = await app.findElementByTestID('text-color');
-    await component.waitForDisplayed({timeout: 5000});
+    await component.waitForDisplayed({timeout: 10000});
     const dump = await dumpVisualTree('text-color');
     expect(dump).toMatchSnapshot();
   });
@@ -57,7 +57,7 @@ describe('Text Tests', () => {
     const component = await app.findElementByTestID(
       'text-decoration-underline',
     );
-    await component.waitForDisplayed({timeout: 5000});
+    await component.waitForDisplayed({timeout: 10000});
     const dump = await dumpVisualTree('text-decoration-underline');
     expect(dump).toMatchSnapshot();
   });
@@ -65,37 +65,37 @@ describe('Text Tests', () => {
     const component = await app.findElementByTestID(
       'text-decoration-solid-linethru',
     );
-    await component.waitForDisplayed({timeout: 5000});
+    await component.waitForDisplayed({timeout: 10000});
     const dump = await dumpVisualTree('text-decoration-solid-linethru');
     expect(dump).toMatchSnapshot();
   });
   test('Text can have an outer color', async () => {
     const component = await app.findElementByTestID('text-outer-color');
-    await component.waitForDisplayed({timeout: 5000});
+    await component.waitForDisplayed({timeout: 10000});
     const dump = await dumpVisualTree('text-outer-color');
     expect(dump).toMatchSnapshot();
   });
   test('Text can be restricted to one line', async () => {
     const component = await app.findElementByTestID('text-one-line');
-    await component.waitForDisplayed({timeout: 5000});
+    await component.waitForDisplayed({timeout: 10000});
     const dump = await dumpVisualTree('text-one-line');
     expect(dump).toMatchSnapshot();
   });
   test('Text can be selectable', async () => {
     const component = await app.findElementByTestID('text-selectable');
-    await component.waitForDisplayed({timeout: 5000});
+    await component.waitForDisplayed({timeout: 10000});
     const dump = await dumpVisualTree('text-selectable');
     expect(dump).toMatchSnapshot();
   });
   test('Text can have a customized selection color', async () => {
     const component = await app.findElementByTestID('text-selection-color');
-    await component.waitForDisplayed({timeout: 5000});
+    await component.waitForDisplayed({timeout: 10000});
     const dump = await dumpVisualTree('text-selection-color');
     expect(dump).toMatchSnapshot();
   });
   test('Text can have shadows', async () => {
     const component = await app.findElementByTestID('text-shadow');
-    await component.waitForDisplayed({timeout: 5000});
+    await component.waitForDisplayed({timeout: 10000});
     const dump = await dumpVisualTree('text-shadow');
     expect(dump).toMatchSnapshot();
   });
