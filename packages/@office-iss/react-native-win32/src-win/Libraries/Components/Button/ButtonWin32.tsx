ce09e4795fd5f1cfbd2b1a31cc2620972e8e7649
Andrew <30809111+acoates-ms@users.noreply.github.com>
2024-03-14 17:10:18 -0700

    Remove TextWin32

--- packages/@office-iss/react-native-win32/src-win/Libraries/Components/Button/ButtonWin32.tsx
+++ packages/@office-iss/react-native-win32/src-win/Libraries/Components/Button/ButtonWin32.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import RN = require('react-native');
 import type { IViewWin32Props } from '../View/ViewPropTypes';
-import type { ITextWin32Props } from '../../Text/Text';
+import type { TextProps } from '../../Text/Text';
 import type { IButtonWin32Props } from './ButtonWin32.Props';
 
 const enum SelectState {
@@ -39,7 +39,7 @@ export class ButtonWin32 extends React.Component<IButtonWin32Props, IButtonWin32
       style: this.props.style as RN.StyleProp<RN.ViewStyle>,
     };
 
-    const textProps: ITextWin32Props = {
+    const textProps: TextProps = {
       textStyle: 'None',
     };
     if (this.props.color) {
@@ -48,7 +48,7 @@ export class ButtonWin32 extends React.Component<IButtonWin32Props, IButtonWin32
 
     return (
       <RN.ViewWin32 {...viewProps}>
-        <RN.TextWin32 {...textProps}>{this.props.title}</RN.TextWin32>
+        <RN.Text {...textProps}>{this.props.title}</RN.Text>
       </RN.ViewWin32>
     );
   }
