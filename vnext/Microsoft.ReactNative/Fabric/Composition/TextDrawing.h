1f69de9a97fac96f7e7c642e7fb11185ca3ea036
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2024-03-22 11:23:26 -0700

    [Farbic] Extract RenderText into helper function + clean up Loading visuals (#12866)
    * Extract RenderText into helper function
    
    * Change files

---
+++ vnext/Microsoft.ReactNative/Fabric/Composition/TextDrawing.h
@@ -0,0 +1,25 @@
+
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include <Fabric/Composition/Theme.h>
+#include <d2d1_1.h>
+#include <dwrite.h>
+#include <react/renderer/attributedstring/AttributedString.h>
+#include <winrt/Windows.UI.Composition.h>
+#include "CompositionHelpers.h"
+
+namespace winrt::Microsoft::ReactNative::Composition {
+
+void RenderText(
+    ID2D1DeviceContext &deviceContext,
+    ::IDWriteTextLayout &textLayout,
+    const facebook::react::AttributedString &attributedString,
+    const facebook::react::TextAttributes &textAttributes,
+    facebook::react::Point offset,
+    float pointScaleFactor,
+    winrt::Microsoft::ReactNative::Composition::implementation::Theme &theme) noexcept;
+
+} // namespace winrt::Microsoft::ReactNative::Composition
