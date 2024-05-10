e21fab7ca63612427abc70c882ecbe9cf3378ee3
Nathanael Bracy <nate@bracy.dev>
2024-05-10 19:46:09 -0400

    Microsoft.ReactNative/Utils: replace private _Starts_with with C++20 starts_with

--- vnext/Microsoft.ReactNative/Utils/LocalBundleReader.cpp
+++ vnext/Microsoft.ReactNative/Utils/LocalBundleReader.cpp
@@ -59,11 +59,10 @@ std::future<std::string> LocalBundleReader::LoadBundleAsync(const std::wstring b
   winrt::Windows::Storage::StorageFile file{nullptr};
 
   // Supports "ms-appx://" or "ms-appdata://"
-  // TODO: Replace call to private string function with C++ 20 `starts_with`
-  if (bundleUri._Starts_with(L"ms-app")) {
+  if (bundleUri.starts_with(L"ms-app")) {
     winrt::Windows::Foundation::Uri uri(bundleUri);
     file = co_await winrt::Windows::Storage::StorageFile::GetFileFromApplicationUriAsync(uri);
-  } else if (bundleUri._Starts_with(L"resource://")) {
+  } else if (bundleUri.starts_with(L"resource://")) {
     winrt::Windows::Foundation::Uri uri(bundleUri);
     co_return GetBundleFromEmbeddedResource(uri);
   } else {
