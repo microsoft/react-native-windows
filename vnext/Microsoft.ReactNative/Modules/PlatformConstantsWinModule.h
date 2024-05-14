57c79628014dcc5c9cac8eb6da5be34da786e6c2
Eric Rozell <ericroz@meta.com>
2024-05-14 00:39:18 -0400

    Renames TM spec (#12890)
    * Renames TM spec
    
    Using fully qualified platform name as module suffix helps with some integration with mobile TM codegen.
    
    * Change files
    
    * Fixup codegen files
    
    * Adds output of yarn build
    
    ---------
    
    Co-authored-by: Chris Glein <26607885+chrisglein@users.noreply.github.com>

--- vnext/Microsoft.ReactNative/Modules/PlatformConstantsWinModule.h
+++ vnext/Microsoft.ReactNative/Modules/PlatformConstantsWinModule.h
@@ -2,17 +2,17 @@
 // Licensed under the MIT License.
 #pragma once
 
-#include "codegen/NativePlatformConstantsWinSpec.g.h"
+#include "codegen/NativePlatformConstantsWindowsSpec.g.h"
 #include <NativeModules.h>
 
 namespace Microsoft::ReactNative {
 
 REACT_MODULE(PlatformConstants)
 struct PlatformConstants {
-  using ModuleSpec = ReactNativeSpecs::PlatformConstantsWinSpec;
+  using ModuleSpec = ReactNativeSpecs::PlatformConstantsWindowsSpec;
 
   REACT_GET_CONSTANTS(GetConstants)
-  ReactNativeSpecs::PlatformConstantsWinSpec_PlatformConstantsWindows GetConstants() noexcept;
+  ReactNativeSpecs::PlatformConstantsWindowsSpec_PlatformConstantsWindows GetConstants() noexcept;
 };
 
 } // namespace Microsoft::ReactNative
