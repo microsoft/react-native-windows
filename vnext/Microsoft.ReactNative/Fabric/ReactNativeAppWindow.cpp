059886d715848a58ccdd8eb2390cf9e4f4084654
Venkata Sharath Chandra Manchala <10109130+sharath2727@users.noreply.github.com>
2024-06-12 13:35:53 -0700

    Implement ReactNativeAppWindow

---
+++ vnext/Microsoft.ReactNative/Fabric/ReactNativeAppWindow.cpp
@@ -0,0 +1,235 @@
+#include "pch.h"
+#include "ReactNativeAppWindow.h"
+#include "ReactNativeAppWindow.g.cpp"
+
+namespace winrt::Microsoft::ReactNative::implementation
+{
+    ReactNativeAppWindow::ReactNativeAppWindow(winrt::Microsoft::UI::Windowing::AppWindow const& appWindow)
+    {
+        m_appWindow = appWindow;
+    }
+
+    winrt::Microsoft::ReactNative::ReactNativeAppWindow ReactNativeAppWindow::Create()
+    {
+        auto appWindow{winrt::Microsoft::UI::Windowing::AppWindow::Create()};
+        return winrt::make<ReactNativeAppWindow>(appWindow);
+    }
+
+    winrt::Microsoft::ReactNative::ReactNativeAppWindow ReactNativeAppWindow::Create(
+        winrt::Microsoft::UI::Windowing::AppWindowPresenter const& appWindowPresenter)
+    {
+        auto appWindow{winrt::Microsoft::UI::Windowing::AppWindow::Create(appWindowPresenter)};
+        return winrt::make<ReactNativeAppWindow>(appWindow);
+    }
+
+    winrt::Microsoft::ReactNative::ReactNativeAppWindow ReactNativeAppWindow::Create(
+        winrt::Microsoft::UI::Windowing::AppWindowPresenter const& appWindowPresenter,
+        winrt::Microsoft::UI::WindowId const& ownerWindowId)
+    {
+        auto appWindow{winrt::Microsoft::UI::Windowing::AppWindow::Create(appWindowPresenter, ownerWindowId)};
+        return winrt::make<ReactNativeAppWindow>(appWindow);
+    }
+
+    winrt::Microsoft::ReactNative::ReactNativeAppWindow ReactNativeAppWindow::Create(
+        winrt::Microsoft::UI::Windowing::AppWindowPresenter const& appWindowPresenter,
+        winrt::Microsoft::UI::WindowId const& ownerWindowId,
+        winrt::Microsoft::UI::Dispatching::DispatcherQueue const& dispatcherQueue)
+    {
+        auto appWindow{winrt::Microsoft::UI::Windowing::AppWindow::Create(appWindowPresenter, ownerWindowId, dispatcherQueue)};
+        return winrt::make<ReactNativeAppWindow>(appWindow);
+    }
+
+    winrt::Microsoft::ReactNative::ReactNativeAppWindow ReactNativeAppWindow::GetFromWindowId(winrt::Microsoft::UI::WindowId const& windowId)
+    {
+        auto appWindow{winrt::Microsoft::UI::Windowing::AppWindow::GetFromWindowId(windowId)};
+        return winrt::make<ReactNativeAppWindow>(appWindow);
+    }
+
+    winrt::Windows::Graphics::SizeInt32 ReactNativeAppWindow::ClientSize()
+    {
+        return m_appWindow.ClientSize();
+    }
+
+    winrt::Microsoft::UI::Dispatching::DispatcherQueue ReactNativeAppWindow::DispatcherQueue()
+    {
+        return m_appWindow.DispatcherQueue();
+    }
+
+    winrt::Microsoft::UI::WindowId ReactNativeAppWindow::Id()
+    {
+        return m_appWindow.Id();
+    }
+
+    bool ReactNativeAppWindow::IsShownInSwitchers()
+    {
+        return m_appWindow.IsShownInSwitchers();
+    }
+
+    void ReactNativeAppWindow::IsShownInSwitchers(bool value)
+    {
+        m_appWindow.IsShownInSwitchers(value);
+    }
+
+    bool ReactNativeAppWindow::IsVisible()
+    {
+        return m_appWindow.IsVisible();
+    }
+
+    winrt::Microsoft::UI::WindowId ReactNativeAppWindow::OwnerWindowId()
+    {
+        return m_appWindow.OwnerWindowId();
+    }
+
+    winrt::Windows::Graphics::PointInt32 ReactNativeAppWindow::Position()
+    {
+        return m_appWindow.Position();
+    }
+
+    winrt::Microsoft::UI::Windowing::AppWindowPresenter ReactNativeAppWindow::Presenter()
+    {
+        return m_appWindow.Presenter();
+    }
+
+    winrt::Windows::Graphics::SizeInt32 ReactNativeAppWindow::Size()
+    {
+        return m_appWindow.Size();
+    }
+
+    hstring ReactNativeAppWindow::Title()
+    {
+        return m_appWindow.Title();
+    }
+
+    void ReactNativeAppWindow::Title(hstring const& value)
+    {
+        m_appWindow.Title(value);
+    }
+
+    winrt::Microsoft::UI::Windowing::AppWindowTitleBar ReactNativeAppWindow::TitleBar()
+    {
+        return m_appWindow.TitleBar();
+    }
+
+    void ReactNativeAppWindow::AssociateWithDispatcherQueue(winrt::Microsoft::UI::Dispatching::DispatcherQueue const& dispatcherQueue)
+    {
+        m_appWindow.AssociateWithDispatcherQueue(dispatcherQueue);
+    }
+
+    void ReactNativeAppWindow::Destroy()
+    {
+        m_appWindow.Destroy();
+    }
+
+    void ReactNativeAppWindow::Hide()
+    {
+        m_appWindow.Hide();
+    }
+
+    void ReactNativeAppWindow::Move(winrt::Windows::Graphics::PointInt32 const& position)
+    {
+        m_appWindow.Move(position);
+    }
+
+    void ReactNativeAppWindow::MoveAndResize(winrt::Windows::Graphics::RectInt32 const& rect)
+    {
+        m_appWindow.MoveAndResize(rect);
+    }
+
+    void ReactNativeAppWindow::MoveAndResize(winrt::Windows::Graphics::RectInt32 const& rect, winrt::Microsoft::UI::Windowing::DisplayArea const& displayarea)
+    {
+        m_appWindow.MoveAndResize(rect, displayarea);
+    }
+
+    void ReactNativeAppWindow::MoveInZOrderAtBottom()
+    {
+        m_appWindow.MoveInZOrderAtBottom();
+    }
+
+    void ReactNativeAppWindow::MoveInZOrderAtTop()
+    {
+        m_appWindow.MoveInZOrderAtTop();
+    }
+
+    void ReactNativeAppWindow::MoveInZOrderBelow(winrt::Microsoft::UI::WindowId const& windowId)
+    {
+        m_appWindow.MoveInZOrderBelow(windowId);
+    }
+
+    void ReactNativeAppWindow::Resize(winrt::Windows::Graphics::SizeInt32 const& size)
+    {
+        m_appWindow.Resize(size);
+    }
+
+    void ReactNativeAppWindow::ResizeClient(winrt::Windows::Graphics::SizeInt32 const& size)
+    {
+        m_appWindow.ResizeClient(size);
+    }
+
+    void ReactNativeAppWindow::SetIcon(hstring const& iconPath)
+    {
+        m_appWindow.SetIcon(iconPath);
+    }
+
+    void ReactNativeAppWindow::SetIcon(winrt::Microsoft::UI::IconId const& iconId)
+    {
+        m_appWindow.SetIcon(iconId);
+    }
+
+    void ReactNativeAppWindow::SetPresenter(winrt::Microsoft::UI::Windowing::AppWindowPresenter const& appWindowPresenter)
+    {
+        m_appWindow.SetPresenter(appWindowPresenter);
+    }
+
+    void ReactNativeAppWindow::SetPresenter(winrt::Microsoft::UI::Windowing::AppWindowPresenterKind const& appWindowPresenterKind)
+    {
+        m_appWindow.SetPresenter(appWindowPresenterKind);
+    }
+
+    void ReactNativeAppWindow::Show()
+    {
+        m_appWindow.Show();
+    }
+
+    void ReactNativeAppWindow::Show(bool activateWindow)
+    {
+        m_appWindow.Show(activateWindow);
+    }
+
+    void ReactNativeAppWindow::ShowOnceWithRequestedStartupState()
+    {
+        m_appWindow.ShowOnceWithRequestedStartupState();
+    }
+
+    winrt::event_token ReactNativeAppWindow::Changed(winrt::Windows::Foundation::TypedEventHandler<winrt::Microsoft::ReactNative::ReactNativeAppWindow, winrt::Microsoft::UI::Windowing::AppWindowChangedEventArgs> const& handler)
+    {
+        // TO BE IMPLEMENTED
+        throw hresult_not_implemented();
+    }
+
+    void ReactNativeAppWindow::Changed(winrt::event_token const& token) noexcept
+    {
+        // TO BE IMPLEMENTED
+    }
+
+    winrt::event_token ReactNativeAppWindow::Closing(winrt::Windows::Foundation::TypedEventHandler<winrt::Microsoft::ReactNative::ReactNativeAppWindow, winrt::Microsoft::UI::Windowing::AppWindowClosingEventArgs> const& handler)
+    {
+        // TO BE IMPLEMENTED
+        throw hresult_not_implemented();
+    }
+
+    void ReactNativeAppWindow::Closing(winrt::event_token const& token) noexcept
+    {
+        // TO BE IMPLEMENTED
+    }
+
+    winrt::event_token ReactNativeAppWindow::Destroying(winrt::Windows::Foundation::TypedEventHandler<winrt::Microsoft::ReactNative::ReactNativeAppWindow, winrt::Windows::Foundation::IInspectable> const& handler)
+    {
+        // TO BE IMPLEMENTED
+        throw hresult_not_implemented();
+    }
+
+    void ReactNativeAppWindow::Destroying(winrt::event_token const& token) noexcept
+    {
+        // TO BE IMPLEMENTED
+    }
+}
---
+++ vnext/Microsoft.ReactNative/Fabric/ReactNativeAppWindow.h
@@ -0,0 +1,64 @@
+#pragma once
+#include "ReactNativeAppWindow.g.h"
+
+namespace winrt::Microsoft::ReactNative::implementation
+{
+    struct ReactNativeAppWindow : ReactNativeAppWindowT<ReactNativeAppWindow>
+    {
+        ReactNativeAppWindow() = default;
+
+        ReactNativeAppWindow(winrt::Microsoft::UI::Windowing::AppWindow const& appWindow);
+
+        static winrt::Microsoft::ReactNative::ReactNativeAppWindow Create();
+        static winrt::Microsoft::ReactNative::ReactNativeAppWindow Create(winrt::Microsoft::UI::Windowing::AppWindowPresenter const& appWindowPresenter);
+        static winrt::Microsoft::ReactNative::ReactNativeAppWindow Create(winrt::Microsoft::UI::Windowing::AppWindowPresenter const& appWindowPresenter, winrt::Microsoft::UI::WindowId const& ownerWindowId);
+        static winrt::Microsoft::ReactNative::ReactNativeAppWindow Create(winrt::Microsoft::UI::Windowing::AppWindowPresenter const& appWindowPresenter, winrt::Microsoft::UI::WindowId const& ownerWindowId, winrt::Microsoft::UI::Dispatching::DispatcherQueue const& dispatcherQueue);
+        static winrt::Microsoft::ReactNative::ReactNativeAppWindow GetFromWindowId(winrt::Microsoft::UI::WindowId const& windowId);
+        winrt::Windows::Graphics::SizeInt32 ClientSize();
+        winrt::Microsoft::UI::Dispatching::DispatcherQueue DispatcherQueue();
+        winrt::Microsoft::UI::WindowId Id();
+        bool IsShownInSwitchers();
+        void IsShownInSwitchers(bool value);
+        bool IsVisible();
+        winrt::Microsoft::UI::WindowId OwnerWindowId();
+        winrt::Windows::Graphics::PointInt32 Position();
+        winrt::Microsoft::UI::Windowing::AppWindowPresenter Presenter();
+        winrt::Windows::Graphics::SizeInt32 Size();
+        hstring Title();
+        void Title(hstring const& value);
+        winrt::Microsoft::UI::Windowing::AppWindowTitleBar TitleBar();
+        void AssociateWithDispatcherQueue(winrt::Microsoft::UI::Dispatching::DispatcherQueue const& dispatcherQueue);
+        void Destroy();
+        void Hide();
+        void Move(winrt::Windows::Graphics::PointInt32 const& position);
+        void MoveAndResize(winrt::Windows::Graphics::RectInt32 const& rect);
+        void MoveAndResize(winrt::Windows::Graphics::RectInt32 const& rect, winrt::Microsoft::UI::Windowing::DisplayArea const& displayarea);
+        void MoveInZOrderAtBottom();
+        void MoveInZOrderAtTop();
+        void MoveInZOrderBelow(winrt::Microsoft::UI::WindowId const& windowId);
+        void Resize(winrt::Windows::Graphics::SizeInt32 const& size);
+        void ResizeClient(winrt::Windows::Graphics::SizeInt32 const& size);
+        void SetIcon(hstring const& iconPath);
+        void SetIcon(winrt::Microsoft::UI::IconId const& iconId);
+        void SetPresenter(winrt::Microsoft::UI::Windowing::AppWindowPresenter const& appWindowPresenter);
+        void SetPresenter(winrt::Microsoft::UI::Windowing::AppWindowPresenterKind const& appWindowPresenterKind);
+        void Show();
+        void Show(bool activateWindow);
+        void ShowOnceWithRequestedStartupState();
+        winrt::event_token Changed(winrt::Windows::Foundation::TypedEventHandler<winrt::Microsoft::ReactNative::ReactNativeAppWindow, winrt::Microsoft::UI::Windowing::AppWindowChangedEventArgs> const& handler);
+        void Changed(winrt::event_token const& token) noexcept;
+        winrt::event_token Closing(winrt::Windows::Foundation::TypedEventHandler<winrt::Microsoft::ReactNative::ReactNativeAppWindow, winrt::Microsoft::UI::Windowing::AppWindowClosingEventArgs> const& handler);
+        void Closing(winrt::event_token const& token) noexcept;
+        winrt::event_token Destroying(winrt::Windows::Foundation::TypedEventHandler<winrt::Microsoft::ReactNative::ReactNativeAppWindow, winrt::Windows::Foundation::IInspectable> const& handler);
+        void Destroying(winrt::event_token const& token) noexcept;
+
+    private:
+        winrt::Microsoft::UI::Windowing::AppWindow m_appWindow{nullptr};
+    };
+}
+namespace winrt::Microsoft::ReactNative::factory_implementation
+{
+    struct ReactNativeAppWindow : ReactNativeAppWindowT<ReactNativeAppWindow, implementation::ReactNativeAppWindow>
+    {
+    };
+}
