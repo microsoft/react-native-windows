d61fcd84ca556938f3294004424bc84f94811697
Venkata Sharath Chandra Manchala <10109130+sharath2727@users.noreply.github.com>
2024-06-19 16:36:25 -0700

    yarn format fix

--- vnext/Microsoft.ReactNative/ReactInstanceSettingsBuilder.cpp
+++ vnext/Microsoft.ReactNative/ReactInstanceSettingsBuilder.cpp
@@ -1,58 +1,50 @@
 #include "pch.h"
-#include "ReactInstanceSettings.h"
 #include "ReactInstanceSettingsBuilder.h"
 #include "ReactInstanceSettingsBuilder.g.cpp"
+#include "ReactInstanceSettings.h"
 
-namespace winrt::ReactNative
-{
-    using namespace winrt::Microsoft::ReactNative;
+namespace winrt::ReactNative {
+using namespace winrt::Microsoft::ReactNative;
 }
 
-namespace winrt::UI
-{
-    using namespace winrt::Microsoft::UI;
+namespace winrt::UI {
+using namespace winrt::Microsoft::UI;
 }
 
-namespace winrt::Microsoft::ReactNative::implementation
-{
-    ReactInstanceSettingsBuilder::ReactInstanceSettingsBuilder()
-    {
-        m_reactInstanceSettings = winrt::make<winrt::ReactNative::implementation::ReactInstanceSettings>();
+namespace winrt::Microsoft::ReactNative::implementation {
+ReactInstanceSettingsBuilder::ReactInstanceSettingsBuilder() {
+  m_reactInstanceSettings = winrt::make<winrt::ReactNative::implementation::ReactInstanceSettings>();
 
 #if _DEBUG
-        m_reactInstanceSettings.UseDirectDebugger(true);
-        m_reactInstanceSettings.UseDeveloperSupport(true);
+  m_reactInstanceSettings.UseDirectDebugger(true);
+  m_reactInstanceSettings.UseDeveloperSupport(true);
 #else
-        m_reactInstanceSettings.UseDirectDebugger(false);
-        m_reactInstanceSettings.UseDeveloperSupport(false);
+  m_reactInstanceSettings.UseDirectDebugger(false);
+  m_reactInstanceSettings.UseDeveloperSupport(false);
 #endif
-    }
-
-    winrt::ReactNative::ReactInstanceSettings ReactInstanceSettingsBuilder::ReactInstanceSettings()
-    {
-        return m_reactInstanceSettings;
-    }
-
-    winrt::ReactNative::ReactInstanceSettingsBuilder ReactInstanceSettingsBuilder::SetBundleRootPath(hstring const& path)
-    {
-#if BUNDLE
-        m_reactInstanceSettings.BundleRootPath(path.c_str());
-#endif
-        return *this;
-    }
-
-    winrt::ReactNative::ReactInstanceSettingsBuilder ReactInstanceSettingsBuilder::SetJavaScriptBundleFile(hstring const& file)
-    {
-        m_reactInstanceSettings.JavaScriptBundleFile(file.c_str());
-
-        return *this;
-    }
-
-    winrt::ReactNative::ReactInstanceSettingsBuilder ReactInstanceSettingsBuilder::SetUseFastRefresh(
-        bool const &state)
-    {
-        m_reactInstanceSettings.UseFastRefresh(state);
-
-        return *this;
-    }
 }
+
+winrt::ReactNative::ReactInstanceSettings ReactInstanceSettingsBuilder::ReactInstanceSettings() {
+  return m_reactInstanceSettings;
+}
+
+winrt::ReactNative::ReactInstanceSettingsBuilder ReactInstanceSettingsBuilder::SetBundleRootPath(hstring const &path) {
+#if BUNDLE
+  m_reactInstanceSettings.BundleRootPath(path.c_str());
+#endif
+  return *this;
+}
+
+winrt::ReactNative::ReactInstanceSettingsBuilder ReactInstanceSettingsBuilder::SetJavaScriptBundleFile(
+    hstring const &file) {
+  m_reactInstanceSettings.JavaScriptBundleFile(file.c_str());
+
+  return *this;
+}
+
+winrt::ReactNative::ReactInstanceSettingsBuilder ReactInstanceSettingsBuilder::SetUseFastRefresh(bool const &state) {
+  m_reactInstanceSettings.UseFastRefresh(state);
+
+  return *this;
+}
+} // namespace winrt::Microsoft::ReactNative::implementation
--- vnext/Microsoft.ReactNative/ReactInstanceSettingsBuilder.h
+++ vnext/Microsoft.ReactNative/ReactInstanceSettingsBuilder.h
@@ -1,23 +1,20 @@
 #pragma once
 #include "ReactInstanceSettingsBuilder.g.h"
 
-namespace winrt::Microsoft::ReactNative::implementation
-{
-    struct ReactInstanceSettingsBuilder : ReactInstanceSettingsBuilderT<ReactInstanceSettingsBuilder>
-    {
-        ReactInstanceSettingsBuilder();
+namespace winrt::Microsoft::ReactNative::implementation {
+struct ReactInstanceSettingsBuilder : ReactInstanceSettingsBuilderT<ReactInstanceSettingsBuilder> {
+  ReactInstanceSettingsBuilder();
 
-        winrt::Microsoft::ReactNative::ReactInstanceSettings ReactInstanceSettings();
-        winrt::Microsoft::ReactNative::ReactInstanceSettingsBuilder SetBundleRootPath(hstring const& path);
-        winrt::Microsoft::ReactNative::ReactInstanceSettingsBuilder SetJavaScriptBundleFile(hstring const& file);
-        winrt::Microsoft::ReactNative::ReactInstanceSettingsBuilder SetUseFastRefresh(bool const &state);
-    private:
-        winrt::Microsoft::ReactNative::ReactInstanceSettings m_reactInstanceSettings{nullptr};
-    };
-}
-namespace winrt::Microsoft::ReactNative::factory_implementation
-{
-    struct ReactInstanceSettingsBuilder : ReactInstanceSettingsBuilderT<ReactInstanceSettingsBuilder, implementation::ReactInstanceSettingsBuilder>
-    {
-    };
-}
+  winrt::Microsoft::ReactNative::ReactInstanceSettings ReactInstanceSettings();
+  winrt::Microsoft::ReactNative::ReactInstanceSettingsBuilder SetBundleRootPath(hstring const &path);
+  winrt::Microsoft::ReactNative::ReactInstanceSettingsBuilder SetJavaScriptBundleFile(hstring const &file);
+  winrt::Microsoft::ReactNative::ReactInstanceSettingsBuilder SetUseFastRefresh(bool const &state);
+
+ private:
+  winrt::Microsoft::ReactNative::ReactInstanceSettings m_reactInstanceSettings{nullptr};
+};
+} // namespace winrt::Microsoft::ReactNative::implementation
+namespace winrt::Microsoft::ReactNative::factory_implementation {
+struct ReactInstanceSettingsBuilder
+    : ReactInstanceSettingsBuilderT<ReactInstanceSettingsBuilder, implementation::ReactInstanceSettingsBuilder> {};
+} // namespace winrt::Microsoft::ReactNative::factory_implementation
