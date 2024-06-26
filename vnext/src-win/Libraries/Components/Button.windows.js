dafc2ca19c4c1f64c136d9bdaa072179bfbd6403
Venkata Sharath Chandra Manchala <10109130+sharath2727@users.noreply.github.com>
2024-06-26 12:41:33 -0700

    Keep borderBottomWidth same before and after pressed

--- vnext/src-win/Libraries/Components/Button.windows.js
+++ vnext/src-win/Libraries/Components/Button.windows.js
@@ -576,7 +576,7 @@ const styles = StyleSheet.create({
   },
   buttonPressed: {
     borderColor: PlatformColor('ButtonBorderBrushPressed'),
-    borderBottomWidth: 1,
+    borderBottomWidth: 1.5,
   },
   // Windows]
 });
