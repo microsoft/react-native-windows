f93da01ff107723b10ca3bd930f4159f7832501a
Jon Thysell (JAUNTY) <jthysell@microsoft.com>
2024-07-16 15:26:56 -0700

    Bump minimum VS version to 17.10.4

--- packages/@react-native-windows/cli/src/utils/msbuildtools.ts
+++ packages/@react-native-windows/cli/src/utils/msbuildtools.ts
@@ -200,7 +200,7 @@ export default class MSBuildTools {
       'Microsoft.Component.MSBuild',
       getVCToolsByArch(buildArch),
     ];
-    const minVersion = process.env.MinimumVisualStudioVersion || process.env.VisualStudioVersion || '17.0';
+    const minVersion = process.env.MinimumVisualStudioVersion || process.env.VisualStudioVersion || '17.10.4';
     const vsInstallation = findLatestVsInstall({
       requires,
       minVersion,
