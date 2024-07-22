13f490fbca9dcd56600e96df3b401c03e4b662c0
Daniel Ayala <14967941+danielayala94@users.noreply.github.com>
2024-07-22 10:49:16 -0700

    Fixed snapshot and lint errors

--- vnext/Microsoft.ReactNative/Fabric/Composition/TextInput/WindowsTextInputComponentView.cpp
+++ vnext/Microsoft.ReactNative/Fabric/Composition/TextInput/WindowsTextInputComponentView.cpp
@@ -14,9 +14,9 @@
 #include <winrt/Microsoft.UI.Input.h>
 #include <winrt/Windows.System.h>
 #include <winrt/Windows.UI.h>
-#include <cwctype>
 #include <algorithm> // For std::all_of
 #include <cctype> // For std::isspace
+#include <cwctype>
 #include "../CompositionHelpers.h"
 #include "../RootComponentView.h"
 #include "JSValueReader.h"
