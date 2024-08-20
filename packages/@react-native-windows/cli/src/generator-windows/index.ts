2c44f3609a908d6ec118da330c32ef9ac2f68196
Jon Thysell <jthysell@microsoft.com>
2024-08-20 11:48:31 -0700

    [0.75] Update templates to calculate rnwPath for *.sln files
    This PR backports #13572 to 0.75.
    
    ## Description
    
    Visual Studio solution files cannot use variables in paths to the various project files, and up until now the paths to RNW projects assumed a simple app repo, and were hardcoded to `..\node_modules\react-native-windows\`.
    
    This PR changes the templates for both new and old architecture projects to instead inject the calculated actual path to the RNW package.
    
    **Note:** Node does not "see" symlinks, only their resolved path. So if you're getting RNW via a symlink (say through yarn link) the sln file will hardcode the resolved path at the time of creation.
    
    ### Type of Change
    - Bug fix (non-breaking change which fixes an issue)
    - New feature (non-breaking change which adds functionality)
    
    ### Why
    Some user feedback has indicated that the hardcoded paths did not work for monorepos which may have multiple packages depending on RNW.
    
    ### What
    See above.
    
    ## Screenshots
    N/A
    
    ## Testing
    Verified that the path is now generated.
    
    ## Changelog
    Should this change be included in the release notes: _yes_
    
    Update templates to calculate rnwPath for *.sln files

--- packages/@react-native-windows/cli/src/generator-windows/index.ts
+++ packages/@react-native-windows/cli/src/generator-windows/index.ts
@@ -163,6 +163,7 @@ export async function copyProjectTemplateAndReplace(
   const srcPath = path.join(srcRootPath, `${language}-${projectType}`);
   const sharedPath = path.join(srcRootPath, `shared-${projectType}`);
   const projectGuid = existingProjectGuid || uuid.v4();
+  const rnwPath = path.dirname(resolveRnwPath('package.json'));
   const rnwVersion = require(resolveRnwPath('package.json')).version;
   const nugetVersion = options.nuGetTestVersion || rnwVersion;
   const packageGuid = uuid.v4();
@@ -190,6 +191,7 @@ export async function copyProjectTemplateAndReplace(
     languageIsCpp: language === 'cpp',
 
     rnwVersion: await getVersionOfNpmPackage('react-native-windows'),
+    rnwPathFromProjectRoot: path.relative(destPath, rnwPath).replace(/\//g, '\\'),
 
     mainComponentName: mainComponentName,
 
