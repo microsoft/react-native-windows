91f9a5c4ca2c7a132738d9959bfb91131f8d0e3a
Andrew <30809111+acoates-ms@users.noreply.github.com>
2024-09-06 17:23:54 -0700

    format

--- vnext/Microsoft.ReactNative/Modules/LogBoxModule.cpp
+++ vnext/Microsoft.ReactNative/Modules/LogBoxModule.cpp
@@ -21,17 +21,12 @@ namespace Microsoft::ReactNative {
 
 LogBox::~LogBox() {
 #ifdef USE_FABRIC
-  if (m_hwnd)
-  {
-    m_context.UIDispatcher().Post([hwnd = m_hwnd]()
-    {
-      DestroyWindow(hwnd);
-    });
+  if (m_hwnd) {
+    m_context.UIDispatcher().Post([hwnd = m_hwnd]() { DestroyWindow(hwnd); });
     m_hwnd = nullptr;
   }
 #endif
 }
-
 
 #ifdef USE_FABRIC
 constexpr PCWSTR c_logBoxWindowClassName = L"MS_REACTNATIVE_LOGBOX";
