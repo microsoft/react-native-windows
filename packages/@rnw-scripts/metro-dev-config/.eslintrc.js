8617bbc336c7ea91b185bd2e51d3efe3b5ea5bea
Marlene Cota <1422161+marlenecota@users.noreply.github.com>
2024-09-10 14:55:53 -0700

    remove hermes-eslint from cli and metro-dev-config

--- packages/@rnw-scripts/metro-dev-config/.eslintrc.js
+++ packages/@rnw-scripts/metro-dev-config/.eslintrc.js
@@ -1,16 +1,1 @@
-module.exports = {
-    extends: ['@rnw-scripts'],
-    overrides: [
-    {
-      files: ['*.js', '*.js.flow', '*.jsx'],
-      parser: 'hermes-eslint',
-      rules: {
-        // These rules are not required with hermes-eslint
-        'ft-flow/define-flow-type': 0,
-        'ft-flow/use-flow-type': 0,
-        // flow handles this check for us, so it's not required
-        'no-undef': 0,
-      },
-    },
-  ],
-};
+module.exports = {extends: ['@rnw-scripts']};
