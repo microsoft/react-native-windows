34f4c0c482a31d54875adb71b0f3133b42c3d6b6
Marlene Cota <1422161+marlenecota@users.noreply.github.com>
2024-09-10 13:20:35 -0700

    hermes-eslint in @rnw-scripts/metro-dev-config

--- packages/@rnw-scripts/metro-dev-config/.eslintrc.js
+++ packages/@rnw-scripts/metro-dev-config/.eslintrc.js
@@ -1,1 +1,16 @@
-module.exports = {extends: ['@rnw-scripts']};
+module.exports = {
+    extends: ['@rnw-scripts'],
+    overrides: [
+    {
+      files: ['*.js', '*.js.flow', '*.jsx'],
+      parser: 'hermes-eslint',
+      rules: {
+        // These rules are not required with hermes-eslint
+        'ft-flow/define-flow-type': 0,
+        'ft-flow/use-flow-type': 0,
+        // flow handles this check for us, so it's not required
+        'no-undef': 0,
+      },
+    },
+  ],
+};
