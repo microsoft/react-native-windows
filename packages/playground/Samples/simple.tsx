64cd1dbd3bf3c0c2b6bbc521ae5364859bc69638
TatianaKapos <tatianakapos@microsoft.com>
2024-09-20 16:38:19 -0700

    revert simple.tsx

--- packages/playground/Samples/simple.tsx
+++ packages/playground/Samples/simple.tsx
@@ -4,36 +4,18 @@
  * @format
  */
 import React from 'react';
-import {
-  AppRegistry,
-  View,
-  Button,
-  SafeAreaView,
-  Modal,
-  Text,
-} from 'react-native';
-const Bootstrap = () => {
-  const [modalVisible, setModalVisible] = React.useState(false);
-  return (
-    <SafeAreaView>
-      <Button
-        title="Open Modal"
-        onPress={() => setModalVisible(!modalVisible)}
-      />
-      <Text style={{textAlign:'center'}}>Modal visibility: {String(modalVisible)}</Text>
-      <Modal visible={modalVisible} style={{backgroundColor: 'yellow'}}>
-        <SafeAreaView style={{backgroundColor: 'yellow', width: 200, height: 200}}>
-          <Text style={{backgroundColor: 'green', width: 100, height: 100}}>I should be in the Modal!</Text>
-          <Text style={{backgroundColor: 'green', width: 100, height: 100}}>I should be in the Modal 2!</Text>
-          <Button title='Close Modal' onPress={() => setModalVisible(!modalVisible)}/>
-        </SafeAreaView>
-      </Modal>
+import {AppRegistry, View} from 'react-native';
+
+export default class Bootstrap extends React.Component {
+  render() {
+    return (
       <View
         accessible={true}
         style={{borderRadius: 30, width: 60, height: 60, margin: 10}}>
         <View style={{backgroundColor: 'magenta', width: 60, height: 60}} />
       </View>
-    </SafeAreaView>
-  );
-};
+    );
+  }
+}
+
 AppRegistry.registerComponent('Bootstrap', () => Bootstrap);
