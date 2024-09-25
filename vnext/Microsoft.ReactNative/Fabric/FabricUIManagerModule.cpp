e43ea51ea7827678b6d30792d88dcd9cbc8c0fa3
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2024-09-25 16:44:20 -0700

    [Fabric] LayoutDirection and FontSizeMultiplier support (#13866)
    * [Fabric] LayoutDirection and FontSizeMultiplier support
    
    * Change files
    
    * Missed a few places not setting LayoutDirection
    
    * fix crash on logbox
    
    * Fix textinput caret
    
    * fix
    
    * RichEdit has to be told its multiline before we can set a multiline string

--- vnext/Microsoft.ReactNative/Fabric/FabricUIManagerModule.cpp
+++ vnext/Microsoft.ReactNative/Fabric/FabricUIManagerModule.cpp
@@ -10,6 +10,7 @@
 #include <Fabric/ComponentView.h>
 #include <Fabric/Composition/CompositionUIService.h>
 #include <Fabric/Composition/CompositionViewComponentView.h>
+#include <Fabric/Composition/ReactNativeIsland.h>
 #include <Fabric/Composition/RootComponentView.h>
 #include <Fabric/FabricUIManagerModule.h>
 #include <Fabric/ReactNativeConfigProperties.h>
@@ -141,7 +142,7 @@ void FabricUIManager::startSurface(
 
   facebook::react::LayoutContext layoutContext;
   layoutContext.pointScaleFactor = rootView.ScaleFactor();
-  layoutContext.fontSizeMultiplier = rootView.ScaleFactor();
+  layoutContext.fontSizeMultiplier = rootView.FontSizeMultiplier();
 
   m_surfaceManager->startSurface(
       surfaceId,
