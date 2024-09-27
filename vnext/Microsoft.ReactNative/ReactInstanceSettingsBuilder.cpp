ce41d52561180744ab6e58b5c00bc0a940464ede
Sharath Manchala <10109130+sharath2727@users.noreply.github.com>
2024-09-27 16:21:01 -0700

    Update cpp-app template to use ReactNativeAppBuilder (#13875)
    ## Description
    
    ### Why
    Simplify the default template code for creating the fabric app
    
    Resolves #12991 
    
    ### What
    Updated the cpp-app template to use ReactNativeAppBuilder
    
    ## Changelog
    Should this change be included in the release notes: yes
    
    ---------
    
    Co-authored-by: Jon Thysell <jthysell@microsoft.com>

--- vnext/Microsoft.ReactNative/ReactInstanceSettingsBuilder.cpp
+++ vnext/Microsoft.ReactNative/ReactInstanceSettingsBuilder.cpp
@@ -33,7 +33,7 @@ winrt::ReactNative::ReactInstanceSettingsBuilder ReactInstanceSettingsBuilder::U
 }
 
 winrt::ReactNative::ReactInstanceSettingsBuilder ReactInstanceSettingsBuilder::BundleRootPath(hstring const &path) {
-  m_reactInstanceSettings.BundleRootPath(std::wstring(L"file://").append(path.c_str()).append(L"\\Bundle\\").c_str());
+  m_reactInstanceSettings.BundleRootPath(path.c_str());
 
   return *this;
 }
