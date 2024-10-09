1142cf4089364925b2591c0f359fdcacf2972340
Yajur Grover <yajurgrover24@gmail.com>
2024-10-09 16:38:44 -0700

    fix previous merge

--- vnext/Microsoft.ReactNative/ReactHost/ReactInstanceWin.cpp
+++ vnext/Microsoft.ReactNative/ReactHost/ReactInstanceWin.cpp
@@ -422,11 +422,7 @@ void ReactInstanceWin::LoadModules(
         L"PlatformConstants",
         winrt::Microsoft::ReactNative::MakeTurboModuleProvider<::Microsoft::ReactNative::PlatformConstants>());
 
-  registerTurboModule(
-      L"SampleTurboModule",
-      winrt::Microsoft::ReactNative::MakeTurboModuleProvider<::Microsoft::ReactNative::SampleTurboModule>());
-
-  uint32_t hermesBytecodeVersion = 0;
+    uint32_t hermesBytecodeVersion = 0;
 #if defined(USE_HERMES) && defined(ENABLE_DEVSERVER_HBCBUNDLES)
     hermesBytecodeVersion = ::hermes::hbc::BYTECODE_VERSION;
 #endif
