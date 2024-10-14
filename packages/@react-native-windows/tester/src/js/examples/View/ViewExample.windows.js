5345aacbabe482ca1d42949fae2343a3c543ee17
Chiara Mooney <34109996+chiaramooney@users.noreply.github.com>
2024-10-14 13:57:07 -0700

    Add Support for AccessibilityState:Busy (#13952)
    * Support AccessibilityState: Busy
    
    * Change files
    
    * Add Testing
    
    * Update Snapshots
    
    * Update for Leak

--- packages/@react-native-windows/tester/src/js/examples/View/ViewExample.windows.js
+++ packages/@react-native-windows/tester/src/js/examples/View/ViewExample.windows.js
@@ -558,7 +558,7 @@ class AccessibilityExample extends React.Component<
           {name: 'expand', label: 'expand'},
           {name: 'collapse', label: 'collapse'},
         ]}
-        accessibilityState={{expanded: this.state.expanded}}
+        accessibilityState={{expanded: this.state.expanded, busy: true}}
         accessibilityPosInSet={1}
         accessibilitySetSize={1}
         accessibilityLiveRegion='polite'
