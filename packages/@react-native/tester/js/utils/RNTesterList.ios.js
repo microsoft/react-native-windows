e265163b8cab921b5643d552775403b45c01bf6f
Tatiana Kapos <tatianakapos@microsoft.com>
2024-10-15 12:23:19 -0700

    Integrate RN Nightly 10/1 (223e98cc4) (#13938)
    * 0.77.0-nightly-20240925-0cb32d5ac
    
    * 0.77.0-nightly-20241001-223e98cc4
    
    * fix validate-overrides
    
    * Change files
    
    * fix build
    
    * fix playground
    
    * fix composition
    
    * fix overrides
    
    * fix snapshot + lint
    
    * Change files
    
    * try removing babel plugin
    
    * add change back in
    
    * bump dep

--- packages/@react-native/tester/js/utils/RNTesterList.ios.js
+++ packages/@react-native/tester/js/utils/RNTesterList.ios.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-import type {RNTesterModuleInfo} from '../types/RNTesterTypes';
+import type {RNTesterModuleInfo, RNTesterModule} from '../types/RNTesterTypes';
 
 import ReactNativeFeatureFlags from 'react-native/Libraries/ReactNative/ReactNativeFeatureFlags';
 
@@ -347,10 +347,16 @@ if (ReactNativeFeatureFlags.shouldEmitW3CPointerEvents()) {
   });
 }
 
-const Modules: {...} = {};
+const Playgrounds: Array<RNTesterModuleInfo> = [
+  {
+    key: 'PlaygroundExample',
+    module: require('../examples/Playground/PlaygroundExample'),
+  },
+];
 
-APIs.concat(Components).forEach(Example => {
-  // $FlowFixMe[prop-missing]
+const Modules: {[key: string]: RNTesterModule} = {};
+
+[...APIs, ...Components, ...Playgrounds].forEach(Example => {
   Modules[Example.key] = Example.module;
 });
 
