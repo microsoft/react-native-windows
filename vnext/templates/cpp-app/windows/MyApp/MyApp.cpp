461c16c3c8e0c4b2101072fbb653259e09c2b0d5
Jon Thysell <jthysell@microsoft.com>
2024-10-21 13:12:51 -0700

    Cleanup ReactNativeAppBuilder and ReactNativeWin32App (#13983)
    This PR simplifies and scopes down the API for `ReactNativeAppBuilder` and `ReactNativeWin32App`.
    
    - Bug fix (non-breaking change which fixes an issue)
    
    `ReactNativeAppBuilder`'s API surface made it too easy to call incorrectly and not realize it.
    
    Resolves #13946
    
    There are a variety of changes to the API surface:
    * `ReactInstanceSettingsBuilder` deleted: not only are there are simply too many APIs that would need to be exposed to be useful, the very act of creating and replacing the app's `ReactNativeHost`'s `ReactInstanceSettings` with a new one is what caused the bug in #13946 in the first place
    * `ReactNativeAppBuilder` now only exposes APIs to specify the intial, non-ReactNative, WinAppSDK types, (i.e. `DispatcherQueueController`, `Compositor`, and `AppWindow`), objects the app developer may already have created for their existing app, and otherwise is only responsible for building a `ReactNativeWin32App` with those types properly pre-made
    * `ReactNativeWin32App::Start()` is now more responsible for the stitching together all of the relevant types to make a working Win32 fabric app
    * All WinRT APIs without an immediate use-case have been commented out until we are sure they are necessary and that it is safe to expose them
    * The template has been updated to follow the pattern of:
        * Use `ReactNativeAppBuilder` to get a `ReactNativeWin32` app with the base WinAppSDK types ready
        * Get and modify the types as necessary from the created app object (like the `ReactInstanceSettings` and the `AppWindow`)
        * Call `app.Start()`
    
    N/A
    
    Verified new apps and example apps in libraryes build and run properly.
    
    Should this change be included in the release notes: _yes_
    
    Cleanup ReactNativeAppBuilder and ReactNativeWin32App

--- vnext/templates/cpp-app/windows/MyApp/MyApp.cpp
+++ vnext/templates/cpp-app/windows/MyApp/MyApp.cpp
@@ -8,6 +8,7 @@
 
 #include "NativeModules.h"
 
+// A PackageProvider containing any turbo modules you define within this app project
 struct CompReactPackageProvider
     : winrt::implements<CompReactPackageProvider, winrt::Microsoft::ReactNative::IReactPackageProvider> {
  public: // IReactPackageProvider
@@ -16,151 +17,66 @@ struct CompReactPackageProvider
   }
 };
 
-// Global Variables:
-constexpr PCWSTR windowTitle = L"{{ mainComponentName }}";
-constexpr PCWSTR mainComponentName = L"{{ mainComponentName }}";
-
-float ScaleFactor(HWND hwnd) noexcept {
-  return GetDpiForWindow(hwnd) / static_cast<float>(USER_DEFAULT_SCREEN_DPI);
-}
-
-void UpdateRootViewSizeToAppWindow(
-    winrt::Microsoft::ReactNative::ReactNativeIsland const &rootView,
-    winrt::Microsoft::UI::Windowing::AppWindow const &window) {
-  auto hwnd = winrt::Microsoft::UI::GetWindowFromWindowId(window.Id());
-  auto scaleFactor = ScaleFactor(hwnd);
-  winrt::Windows::Foundation::Size size{
-      window.ClientSize().Width / scaleFactor, window.ClientSize().Height / scaleFactor};
-  // Do not relayout when minimized
-  if (window.Presenter().as<winrt::Microsoft::UI::Windowing::OverlappedPresenter>().State() !=
-      winrt::Microsoft::UI::Windowing::OverlappedPresenterState::Minimized) {
-    winrt::Microsoft::ReactNative::LayoutConstraints constraints;
-    constraints.LayoutDirection = winrt::Microsoft::ReactNative::LayoutDirection::Undefined;
-    constraints.MaximumSize = constraints.MinimumSize = size;
-    rootView.Arrange(constraints, {0,0});
-  }
-}
-
-// Create and configure the ReactNativeHost
-winrt::Microsoft::ReactNative::ReactNativeHost CreateReactNativeHost(
-    HWND hwnd,
-    const winrt::Microsoft::UI::Composition::Compositor &compositor) {
-  WCHAR appDirectory[MAX_PATH];
-  GetModuleFileNameW(NULL, appDirectory, MAX_PATH);
-  PathCchRemoveFileSpec(appDirectory, MAX_PATH);
-
-  auto host = winrt::Microsoft::ReactNative::ReactNativeHost();
-
-  // Include any autolinked modules
-  RegisterAutolinkedNativeModulePackages(host.PackageProviders());
-
-  host.PackageProviders().Append(winrt::make<CompReactPackageProvider>());
-
-#if BUNDLE
-  host.InstanceSettings().JavaScriptBundleFile(L"index.windows");
-  host.InstanceSettings().BundleRootPath(std::wstring(L"file://").append(appDirectory).append(L"\\Bundle\\").c_str());
-  host.InstanceSettings().UseFastRefresh(false);
-#else
-  host.InstanceSettings().JavaScriptBundleFile(L"index");
-  host.InstanceSettings().UseFastRefresh(true);
-#endif
-
-#if _DEBUG
-  host.InstanceSettings().UseDirectDebugger(true);
-  host.InstanceSettings().UseDeveloperSupport(true);
-#else
-  host.InstanceSettings().UseDirectDebugger(false);
-  host.InstanceSettings().UseDeveloperSupport(false);
-#endif
-
-  winrt::Microsoft::ReactNative::ReactCoreInjection::SetTopLevelWindowId(
-      host.InstanceSettings().Properties(), reinterpret_cast<uint64_t>(hwnd));
-
-  winrt::Microsoft::ReactNative::Composition::CompositionUIService::SetCompositor(
-      host.InstanceSettings(), compositor);
-
-  return host;
-}
-
+// The entry point of the Win32 application
 _Use_decl_annotations_ int CALLBACK WinMain(HINSTANCE instance, HINSTANCE, PSTR /* commandLine */, int showCmd) {
-  // Initialize WinRT.
+  // Initialize WinRT
   winrt::init_apartment(winrt::apartment_type::single_threaded);
 
   // Enable per monitor DPI scaling
   SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
 
-  // Create a DispatcherQueue for this thread.  This is needed for Composition, Content, and
-  // Input APIs.
-  auto dispatcherQueueController{winrt::Microsoft::UI::Dispatching::DispatcherQueueController::CreateOnCurrentThread()};
+  // Find the path hosting the app exe file
+  WCHAR appDirectory[MAX_PATH];
+  GetModuleFileNameW(NULL, appDirectory, MAX_PATH);
+  PathCchRemoveFileSpec(appDirectory, MAX_PATH);
 
-  // Create a Compositor for all Content on this thread.
-  auto compositor{winrt::Microsoft::UI::Composition::Compositor()};
+  // Create a ReactNativeWin32App with the ReactNativeAppBuilder
+  auto reactNativeWin32App{winrt::Microsoft::ReactNative::ReactNativeAppBuilder().Build()};
 
-  // Create a top-level window.
-  auto window = winrt::Microsoft::UI::Windowing::AppWindow::Create();
-  window.Title(windowTitle);
-  window.Resize({1000, 1000});
-  window.Show();
-  auto hwnd = winrt::Microsoft::UI::GetWindowFromWindowId(window.Id());
-  auto scaleFactor = ScaleFactor(hwnd);
+  // Configure the initial InstanceSettings for the app's ReactNativeHost
+  auto settings{reactNativeWin32App.ReactNativeHost().InstanceSettings()};
+  // Register any autolinked native modules
+  RegisterAutolinkedNativeModulePackages(settings.PackageProviders());
+  // Register any native modules defined within this app project
+  settings.PackageProviders().Append(winrt::make<CompReactPackageProvider>());
 
-  auto host = CreateReactNativeHost(hwnd, compositor);
+#if BUNDLE
+  // Load the JS bundle from a file (not Metro):
+  // Set the path (on disk) where the .bundle file is located
+  settings.BundleRootPath(std::wstring(L"file://").append(appDirectory).append(L"\\Bundle\\").c_str());
+  // Set the name of the bundle file (without the .bundle extension)
+  settings.JavaScriptBundleFile(L"index.windows");
+  // Disable hot reload
+  settings.UseFastRefresh(false);
+#else
+  // Load the JS bundle from Metro
+  settings.JavaScriptBundleFile(L"index");
+  // Enable hot reload
+  settings.UseFastRefresh(true);
+#endif
+#if _DEBUG
+  // For Debug builds
+  // Enable Direct Debugging of JS
+  settings.UseDirectDebugger(true);
+  // Enable the Developer Menu
+  settings.UseDeveloperSupport(true);
+#else
+  // For Release builds:
+  // Disable Direct Debugging of JS
+  settings.UseDirectDebugger(false);
+  // Disable the Developer Menu
+  settings.UseDeveloperSupport(false);
+#endif
 
-  // Start the react-native instance, which will create a JavaScript runtime and load the applications bundle
-  host.ReloadInstance();
+  // Get the AppWindow so we can configure its initial title and size
+  auto appWindow{reactNativeWin32App.AppWindow()};
+  appWindow.Title(L"{{ mainComponentName }}");
+  appWindow.Resize({1000, 1000});
 
-  // Create a RootView which will present a react-native component
-  winrt::Microsoft::ReactNative::ReactViewOptions viewOptions;
-  viewOptions.ComponentName(mainComponentName);
-  auto rootView = winrt::Microsoft::ReactNative::ReactNativeIsland(compositor);
-  rootView.ReactViewHost(winrt::Microsoft::ReactNative::ReactCoreInjection::MakeViewHost(host, viewOptions));
+  // Get the ReactViewOptions so we can set the initial RN component to load
+  auto viewOptions{reactNativeWin32App.ReactViewOptions()};
+  viewOptions.ComponentName(L"{{ mainComponentName }}");
 
-  // Update the size of the RootView when the AppWindow changes size
-  window.Changed([wkRootView = winrt::make_weak(rootView)](
-                     winrt::Microsoft::UI::Windowing::AppWindow const &window,
-                     winrt::Microsoft::UI::Windowing::AppWindowChangedEventArgs const &args) {
-    if (args.DidSizeChange() || args.DidVisibilityChange()) {
-      if (auto rootView = wkRootView.get()) {
-        UpdateRootViewSizeToAppWindow(rootView, window);
-      }
-    }
-  });
-
-  // Quit application when main window is closed
-  window.Destroying(
-      [host](winrt::Microsoft::UI::Windowing::AppWindow const &window, winrt::IInspectable const & /*args*/) {
-        // Before we shutdown the application - unload the ReactNativeHost to give the javascript a chance to save any
-        // state
-        auto async = host.UnloadInstance();
-        async.Completed([host](auto asyncInfo, winrt::Windows::Foundation::AsyncStatus asyncStatus) {
-          assert(asyncStatus == winrt::Windows::Foundation::AsyncStatus::Completed);
-          host.InstanceSettings().UIDispatcher().Post([]() { PostQuitMessage(0); });
-        });
-      });
-
-  // DesktopChildSiteBridge create a ContentSite that can host the RootView ContentIsland
-  auto bridge = winrt::Microsoft::UI::Content::DesktopChildSiteBridge::Create(compositor, window.Id());
-  bridge.Connect(rootView.Island());
-  bridge.ResizePolicy(winrt::Microsoft::UI::Content::ContentSizePolicy::ResizeContentToParentWindow);
-
-  rootView.ScaleFactor(scaleFactor);
-
-  // Set the intialSize of the root view
-  UpdateRootViewSizeToAppWindow(rootView, window);
-
-  bridge.Show();
-
-  // Run the main application event loop
-  dispatcherQueueController.DispatcherQueue().RunEventLoop();
-
-  // Rundown the DispatcherQueue. This drains the queue and raises events to let components
-  // know the message loop has finished.
-  dispatcherQueueController.ShutdownQueue();
-
-  bridge.Close();
-  bridge = nullptr;
-
-  // Destroy all Composition objects
-  compositor.Close();
-  compositor = nullptr;
+  // Start the app
+  reactNativeWin32App.Start();
 }
