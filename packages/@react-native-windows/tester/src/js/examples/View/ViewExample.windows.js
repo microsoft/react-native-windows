6e324a063fa2783084b4150eefa3e3d7a473ec7d
Chiara Mooney <34109996+chiaramooney@users.noreply.github.com>
2024-10-29 11:19:00 -0700

    Lint

--- packages/@react-native-windows/tester/src/js/examples/View/ViewExample.windows.js
+++ packages/@react-native-windows/tester/src/js/examples/View/ViewExample.windows.js
@@ -565,7 +565,11 @@ class AccessibilityExample extends React.Component<
           {name: 'expand', label: 'expand'},
           {name: 'collapse', label: 'collapse'},
         ]}
-        accessibilityState={{expanded: this.state.expanded, busy: true, checked: true}}
+        accessibilityState={{
+          expanded: this.state.expanded,
+          busy: true,
+          checked: true,
+        }}
         accessibilityPosInSet={1}
         accessibilitySetSize={1}
         accessibilityLiveRegion="polite"
