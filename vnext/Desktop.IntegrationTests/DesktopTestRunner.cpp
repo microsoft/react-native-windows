10a09a460e303363eb0d33e9902e7972f6206fd1
Julio C. Rocha <julio.rocha@microsoft.com>
2024-12-03 14:36:02 -0800

    Remove Chakra references

--- vnext/Desktop.IntegrationTests/DesktopTestRunner.cpp
+++ vnext/Desktop.IntegrationTests/DesktopTestRunner.cpp
@@ -5,7 +5,6 @@
 
 #include <Threading/MessageQueueThreadFactory.h>
 #include <cxxreact/Instance.h>
-#include "ChakraRuntimeHolder.h"
 #include "DesktopTestInstance.h"
 #include "Modules/TestDevSettingsModule.h"
 #include "Modules/TestImageLoaderModule.h"
@@ -70,8 +69,6 @@ shared_ptr<ITestInstance> TestRunner::GetInstance(
 
   // Update settings.
   devSettings->platformName = "windows";
-
-  // Set to JSIEngineOverride::Chakra when testing the Chakra.dll JSI runtime.
   devSettings->jsiEngineOverride = JSIEngineOverride::V8;
 
   auto instanceWrapper = CreateReactInstance(
