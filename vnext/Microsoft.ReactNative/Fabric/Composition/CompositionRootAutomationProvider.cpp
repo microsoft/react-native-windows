c8751571d36b4de53b22e5d1bfdcefc75b18516d
JesseCol <50150435+JesseCol@users.noreply.github.com>
2025-01-10 13:11:41 -0800

    RNIsland UIA fragment root should report parents fragment root

--- vnext/Microsoft.ReactNative/Fabric/Composition/CompositionRootAutomationProvider.cpp
+++ vnext/Microsoft.ReactNative/Fabric/Composition/CompositionRootAutomationProvider.cpp
@@ -27,6 +27,21 @@ HRESULT __stdcall CompositionRootAutomationProvider::GetRuntimeId(SAFEARRAY **pR
 
   *pRetVal = nullptr;
 
+  if (!m_island)
+    return E_FAIL;
+
+  auto id = reinterpret_cast<INT_PTR>(winrt::get_unknown(m_island));
+
+  *pRetVal = SafeArrayCreateVector(VT_I4, 0, 3);
+  if (*pRetVal == nullptr)
+    return E_OUTOFMEMORY;
+
+  auto rgiRuntimeId = static_cast<int*>((*pRetVal)->pvData);
+
+  rgiRuntimeId[0] = UiaAppendRuntimeId;
+  rgiRuntimeId[1] = LODWORD(id);
+  rgiRuntimeId[2] = HIDWORD(id);
+
   return S_OK;
 }
 
@@ -153,8 +168,18 @@ HRESULT __stdcall CompositionRootAutomationProvider::get_FragmentRoot(IRawElemen
   if (pRetVal == nullptr)
     return E_POINTER;
 
-  AddRef();
-  *pRetVal = this;
+  *pRetVal = nullptr;
+
+  if (m_island)
+  {
+    auto parentRoot = m_island.FragmentRootAutomationProvider();
+    auto spFragment = parentRoot.try_as<IRawElementProviderFragmentRoot>();
+    if (spFragment)
+    {
+      *pRetVal = spFragment.detach();
+      return S_OK;
+    }
+  }
 
   return S_OK;
 }
@@ -260,7 +285,17 @@ HRESULT __stdcall CompositionRootAutomationProvider::Navigate(
         return S_OK;
       }
     }
+  } else if (direction == NavigateDirection_Parent) {
+    if (m_island) {
+      auto parent = m_island.ParentAutomationProvider();
+      auto spFragment = parent.try_as<IRawElementProviderFragment>();
+      if (spFragment) {
+        *pRetVal = spFragment.detach();
+        return S_OK;
+      }
+    }
   }
+
   *pRetVal = nullptr;
   return S_OK;
 }
