1f7c36720dbd1db7f173f8cdc9b20e21c82ed6ef
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2025-01-09 17:09:44 -0800

    format

--- vnext/Microsoft.ReactNative/Fabric/Composition/PortalComponentView.cpp
+++ vnext/Microsoft.ReactNative/Fabric/Composition/PortalComponentView.cpp
@@ -6,63 +6,37 @@
 
 #include "PortalComponentView.h"
 
-#include <AutoDraw.h>
-#include <Fabric/AbiState.h>
-#include <Fabric/AbiViewProps.h>
-#include <Fabric/Composition/ReactNativeIsland.h>
-#include <Fabric/FabricUIManagerModule.h>
-#include <UI.Xaml.Controls.h>
-#include <Utils/KeyboardUtils.h>
-#include <Utils/ValueUtils.h>
-#include <Views/FrameworkElementTransferProperties.h>
-#include <winrt/Microsoft.ReactNative.Composition.Experimental.h>
-#include <winrt/Microsoft.UI.Input.h>
-#include <winrt/Windows.UI.Composition.h>
-#include "CompositionContextHelper.h"
-#include "CompositionDynamicAutomationProvider.h"
-#include "CompositionHelpers.h"
-#include "Theme.h"
-#include "TooltipService.h"
-#include "UiaHelpers.h"
-#include "d2d1helper.h"
-
 #include "Composition.PortalComponentView.g.cpp"
-#include "Composition.ViewComponentView.g.cpp"
 
 namespace winrt::Microsoft::ReactNative::Composition::implementation {
 
-  PortalComponentView::PortalComponentView(
-    const winrt::Microsoft::ReactNative::Composition::Experimental::ICompositionContext& compContext,
+PortalComponentView::PortalComponentView(
+    const winrt::Microsoft::ReactNative::Composition::Experimental::ICompositionContext &compContext,
     facebook::react::Tag tag,
-    winrt::Microsoft::ReactNative::ReactContext const& reactContext,
-    winrt::Microsoft::ReactNative::Composition::ReactCompositionViewComponentBuilder* builder)
-    : base_type(tag, reactContext, builder)
-  {
-    m_rootComponentView = winrt::make_self<winrt::Microsoft::ReactNative::Composition::implementation::RootComponentView>(compContext, *this, reactContext);
-  }
+    winrt::Microsoft::ReactNative::ReactContext const &reactContext,
+    winrt::Microsoft::ReactNative::Composition::ReactCompositionViewComponentBuilder *builder)
+    : base_type(tag, reactContext, builder) {
+  m_rootComponentView = winrt::make_self<winrt::Microsoft::ReactNative::Composition::implementation::RootComponentView>(
+      compContext, *this, reactContext);
+}
 
-  PortalComponentView::~PortalComponentView()
-  {
-  }
+PortalComponentView::~PortalComponentView() {}
 
-  winrt::Microsoft::ReactNative::Composition::RootComponentView PortalComponentView::ContentRoot() const noexcept
-  {
-    return *m_rootComponentView;
-  }
+winrt::Microsoft::ReactNative::Composition::RootComponentView PortalComponentView::ContentRoot() const noexcept {
+  return *m_rootComponentView;
+}
 
-  void PortalComponentView::MountChildComponentView(
-    const winrt::Microsoft::ReactNative::ComponentView& childComponentView,
-    uint32_t index) noexcept
-  {
-    m_rootComponentView->MountChildComponentView(childComponentView, index);
-  }
+void PortalComponentView::MountChildComponentView(
+    const winrt::Microsoft::ReactNative::ComponentView &childComponentView,
+    uint32_t index) noexcept {
+  m_rootComponentView->MountChildComponentView(childComponentView, index);
+}
 
-  void PortalComponentView::UnmountChildComponentView(
-    const winrt::Microsoft::ReactNative::ComponentView& childComponentView,
-    uint32_t index) noexcept
-  {
-    m_rootComponentView->UnmountChildComponentView(childComponentView, index);
-  }
+void PortalComponentView::UnmountChildComponentView(
+    const winrt::Microsoft::ReactNative::ComponentView &childComponentView,
+    uint32_t index) noexcept {
+  m_rootComponentView->UnmountChildComponentView(childComponentView, index);
+}
 
 const winrt::Microsoft::ReactNative::IComponentProps PortalComponentView::userProps(
     facebook::react::Props::Shared const &props) noexcept {
@@ -70,23 +44,23 @@ const winrt::Microsoft::ReactNative::IComponentProps PortalComponentView::userPr
   return abiViewProps.UserProps();
 }
 
-  void PortalComponentView::updateProps(
-      facebook::react::Props::Shared const &props,
-      facebook::react::Props::Shared const &oldProps) noexcept {
-        m_rootComponentView->updateProps(props, oldProps);
-    base_type::updateProps(props, oldProps);
-      }
+void PortalComponentView::updateProps(
+    facebook::react::Props::Shared const &props,
+    facebook::react::Props::Shared const &oldProps) noexcept {
+  m_rootComponentView->updateProps(props, oldProps);
+  base_type::updateProps(props, oldProps);
+}
 
 void PortalComponentView::updateLayoutMetrics(
     facebook::react::LayoutMetrics const &layoutMetrics,
     facebook::react::LayoutMetrics const &oldLayoutMetrics) noexcept {
-        m_rootComponentView->updateLayoutMetrics(layoutMetrics, oldLayoutMetrics);
-    base_type::updateLayoutMetrics(layoutMetrics, oldLayoutMetrics);
-    }
-
-    void PortalComponentView::FinalizeUpdates(winrt::Microsoft::ReactNative::ComponentViewUpdateMask updateMask) noexcept {
-        m_rootComponentView->FinalizeUpdates(updateMask);
-    base_type::FinalizeUpdates(updateMask);
-    }
-
+  m_rootComponentView->updateLayoutMetrics(layoutMetrics, oldLayoutMetrics);
+  base_type::updateLayoutMetrics(layoutMetrics, oldLayoutMetrics);
 }
+
+void PortalComponentView::FinalizeUpdates(winrt::Microsoft::ReactNative::ComponentViewUpdateMask updateMask) noexcept {
+  m_rootComponentView->FinalizeUpdates(updateMask);
+  base_type::FinalizeUpdates(updateMask);
+}
+
+} // namespace winrt::Microsoft::ReactNative::Composition::implementation
--- vnext/Microsoft.ReactNative/Fabric/Composition/PortalComponentView.h
+++ vnext/Microsoft.ReactNative/Fabric/Composition/PortalComponentView.h
@@ -16,11 +16,10 @@ struct CompContext;
 
 namespace winrt::Microsoft::ReactNative::Composition::implementation {
 
-struct PortalComponentView : public PortalComponentViewT<
-                           PortalComponentView,
-                           winrt::Microsoft::ReactNative::implementation::ComponentView> {
+struct PortalComponentView
+    : public PortalComponentViewT<PortalComponentView, winrt::Microsoft::ReactNative::implementation::ComponentView> {
   PortalComponentView(
-      const winrt::Microsoft::ReactNative::Composition::Experimental::ICompositionContext& compContext,
+      const winrt::Microsoft::ReactNative::Composition::Experimental::ICompositionContext &compContext,
       facebook::react::Tag tag,
       winrt::Microsoft::ReactNative::ReactContext const &reactContext,
       winrt::Microsoft::ReactNative::Composition::ReactCompositionViewComponentBuilder *builder);
@@ -29,26 +28,25 @@ struct PortalComponentView : public PortalComponentViewT<
   winrt::Microsoft::ReactNative::Composition::RootComponentView ContentRoot() const noexcept;
 
   void MountChildComponentView(
-    const winrt::Microsoft::ReactNative::ComponentView& childComponentView,
-    uint32_t index) noexcept override;
+      const winrt::Microsoft::ReactNative::ComponentView &childComponentView,
+      uint32_t index) noexcept override;
   void UnmountChildComponentView(
-    const winrt::Microsoft::ReactNative::ComponentView& childComponentView,
-    uint32_t index) noexcept override;
+      const winrt::Microsoft::ReactNative::ComponentView &childComponentView,
+      uint32_t index) noexcept override;
 
-const winrt::Microsoft::ReactNative::IComponentProps userProps(
-    facebook::react::Props::Shared const &props) noexcept override;
-void updateProps(
-      facebook::react::Props::Shared const &props,
-      facebook::react::Props::Shared const &oldProps) noexcept override;
+  const winrt::Microsoft::ReactNative::IComponentProps userProps(
+      facebook::react::Props::Shared const &props) noexcept override;
+  void updateProps(facebook::react::Props::Shared const &props, facebook::react::Props::Shared const &oldProps) noexcept
+      override;
 
-void updateLayoutMetrics(
-    facebook::react::LayoutMetrics const &layoutMetrics,
-    facebook::react::LayoutMetrics const &oldLayoutMetrics) noexcept override;
+  void updateLayoutMetrics(
+      facebook::react::LayoutMetrics const &layoutMetrics,
+      facebook::react::LayoutMetrics const &oldLayoutMetrics) noexcept override;
 
-void FinalizeUpdates(winrt::Microsoft::ReactNative::ComponentViewUpdateMask updateMask) noexcept override;
+  void FinalizeUpdates(winrt::Microsoft::ReactNative::ComponentViewUpdateMask updateMask) noexcept override;
 
-private:
+ private:
   winrt::com_ptr<winrt::Microsoft::ReactNative::Composition::implementation::RootComponentView> m_rootComponentView;
 };
 
-}
+} // namespace winrt::Microsoft::ReactNative::Composition::implementation
