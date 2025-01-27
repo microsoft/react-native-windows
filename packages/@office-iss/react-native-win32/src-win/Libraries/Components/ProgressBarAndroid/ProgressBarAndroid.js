eba17d350d7022a341eea7672da56f9829785503
Yajur Grover <yajurgrover24@gmail.com>
2025-01-27 15:53:04 -0800

    fix conflict

--- packages/@office-iss/react-native-win32/src-win/Libraries/Components/ProgressBarAndroid/ProgressBarAndroid.js
+++ packages/@office-iss/react-native-win32/src-win/Libraries/Components/ProgressBarAndroid/ProgressBarAndroid.js
@@ -9,15 +9,4 @@
 
 'use strict';
 
-<<<<<<< Upstream
-import typeof UnimplementedViewType from '../UnimplementedViews/UnimplementedView';
-import typeof ProgressBarAndroidNativeComponentType from './ProgressBarAndroidNativeComponent';
-
-export type {ProgressBarAndroidProps} from './ProgressBarAndroid.android';
-
-export default (require('../UnimplementedViews/UnimplementedView'):
-  | UnimplementedViewType
-  | ProgressBarAndroidNativeComponentType);
-=======
-module.exports = require('../UnimplementedViews/UnimplementedView');
->>>>>>> Override
+export default (require('../UnimplementedViews/UnimplementedView'));
