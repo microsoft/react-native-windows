08d45b5bdd7d77ae95050b886f7456ae3add3246
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2025-01-29 10:37:58 -0800

    Allow portals to have independent layout constraints and scale factor (#14315)
    * Allow portals to have independent layout constraints and scale factor
    
    * format
    
    * change files
    
    * fix

--- packages/sample-custom-component/windows/SampleCustomComponent/codegen/react/components/SampleCustomComponent/DrawingIsland.g.h
+++ packages/sample-custom-component/windows/SampleCustomComponent/codegen/react/components/SampleCustomComponent/DrawingIsland.g.h
@@ -150,7 +150,7 @@ void RegisterDrawingIslandNativeComponent(
           builder.SetUpdateStateHandler([](const winrt::Microsoft::ReactNative::ComponentView &view,
                                      const winrt::Microsoft::ReactNative::IComponentState &newState) noexcept {
             auto userData = view.UserData().as<TUserData>();
-            userData->member(view, newState);
+            userData->UpdateState(view, newState);
           });
         }
 
--- vnext/codegen/react/components/rnwcore/ModalHostView.g.h
+++ vnext/codegen/react/components/rnwcore/ModalHostView.g.h
@@ -236,7 +236,7 @@ void RegisterModalHostViewNativeComponent(
           builder.SetUpdateStateHandler([](const winrt::Microsoft::ReactNative::ComponentView &view,
                                      const winrt::Microsoft::ReactNative::IComponentState &newState) noexcept {
             auto userData = view.UserData().as<TUserData>();
-            userData->member(view, newState);
+            userData->UpdateState(view, newState);
           });
         }
 
