185098b0f23e8d20f37aace9728fb3866af2015e
Julio C. Rocha <julio.rocha@microsoft.com>
2025-02-25 18:41:27 -0800

    Uncomment WebSocketMultipleSendTest.js

--- vnext/src-win/IntegrationTests/WebSocketMultipleSendTest.js
+++ vnext/src-win/IntegrationTests/WebSocketMultipleSendTest.js
@@ -30,26 +30,24 @@ const WS_EVENTS = ['open', 'message', 'close', 'error'];
 
 const EXPECTED = 'wxyz';
 
+//lastSocketEvent: ?string,
 type State = {
-  id: number,
   sendUrl: string,
   receiveUrl: string,
   incomingSocket: ?WebSocket,
   outgoingSocket: ?WebSocket,
-  //lastSocketEvent: ?string,
   messageW: string,
   messageX: string,
   messageY: string,
   messageZ: string,
   result: ?string,
-  ...
+  //...
 };
 
 class WebSocketMultipleSendTest extends React.Component<{}, State> {
   state: State = {
-    id: 1,
-    sendUrl: `${URL_BASE}/send/${id}`,
-    receiveUrl: `${URL_BASE}/receive/${id}`,
+    sendUrl: `${URL_BASE}/send/1`,
+    receiveUrl: `${URL_BASE}/receive/1`,
     incomingSocket: null,
     outgoingSocket: null,
     messageW: 'w'.repeat(1025),
@@ -59,109 +57,90 @@ class WebSocketMultipleSendTest extends React.Component<{}, State> {
     result: '',
   };
 
-  //_waitFor = (condition: any, timeout: any, callback: any) => {
-  //  let remaining = timeout;
-  //  const timeoutFunction = function () {
-  //    if (condition()) {
-  //      callback(true);
-  //      return;
-  //    }
-  //    remaining--;
-  //    if (remaining === 0) {
-  //      callback(false);
-  //    } else {
-  //      setTimeout(timeoutFunction, 1000);
-  //    }
-  //  };
-  //  setTimeout(timeoutFunction, 1000);
-  //};
+  _waitFor = (condition: any, timeout: any, callback: any) => {
+    let remaining = timeout;
+    const timeoutFunction = function () {
+      if (condition()) {
+        callback(true);
+        return;
+      }
+      remaining--;
+      if (remaining === 0) {
+        callback(false);
+      } else {
+        setTimeout(timeoutFunction, 1000);
+      }
+    };
+    setTimeout(timeoutFunction, 1000);
+  };
 
-  //_connect = () => {
-  //  const incomingSocket = new WebSocket(this.state.sendUrl);
-  //  WS_EVENTS.forEach(ev => incomingSocket.addEventListener(ev, this._onIncomingEvent));
-  //  const outgoingSocket = new WebSocket(this.state.receiveUrl);
-  //  //WS_EVENTS.forEach(ev => outgoingSocket.addEventListener(ev, this._onSocketEvent));//TODO: Different callbacks?
-  //  this.setState({
-  //    incomingSocket,
-  //    outgoingSocket,
-  //    //TODO: Separate ready state?
-  //  });
-  //};
+  _socketsAreConnected = (): boolean => {
+    return this.state.incomingSocket.readyState === 1 && this.state.outgoingSocket.readyState === 1; // OPEN
+  };
 
-  //_socketsAreConnected = (): boolean => {
-  //  return this.state.incomingSocket.readyState === 1 && this.state.outgoingSocket.readyState === 1; // OPEN
-  //};
+  _socketsAreDisconnected = (): boolean => {
+    return this.state.incomingSocket.readyState === 3 && this.state.outgoingSocket.readyState === 3; // CLOSED
+  };
 
-  //_socketsAreDisconnected = (): boolean => {
-  //  return this.state.incomingSocket.readyState === 3 && this.state.outgoingSocket.readyState === 3; // CLOSED
-  //};
+  _disconnectIncoming = () => {
+    if (!this.state.incomingSocket) {
+      return;
+    }
+    this.state.incomingSocket.close();
 
-  //_disconnectIncoming = () => {
-  //  if (!this.state.incomingSocket) {
-  //    return;
-  //  }
-  //  this.state.incomingSocket.close();
+    //TODO: Remove
+    if (!this.state.outgoingSocket) {
+      return;
+    }
+    this.state.outgoingSocket.close();
+  };
 
-  //  //TODO: Remove
-  //  if (!this.state.outgoingSocket) {
-  //    return;
-  //  }
-  //  this.state.outgoingSocket.close();
-  //};
+  testDisconnect: () => void = () => {
+    this._disconnectIncoming();
+    this._waitFor(this._socketsAreDisconnected, 5, disconnectSucceeded => {
+      TestModule.markTestPassed(disconnectSucceeded);
+    });
+  };
 
-  //_onIncomingEvent = (event: any) => {
-  //  //const state: any = {
-  //  //  //lastSocketEvent
-  //  //};
-  //  if (event.type === 'message') {
-  //    var message = this.state.result;
-  //     message += `${event.data[0]}`; // Append incomming message's first character
-  //    this.setState({
-  //      result: message
-  //    });
-  //  }
-  //};
+  testSendMultipleAndClose: () => void = () => {
+    if (!this.state.incomingSocket || !this.state.outgoingSocket) {
+      return;
+    }
+    this.state.outgoingSocket.send(this.state.messageW);
+    this.state.outgoingSocket.send(this.state.messageX);
+    this.state.outgoingSocket.send(this.state.messageY);
+    this.state.outgoingSocket.send(this.state.messageZ);
 
-  //_resultIsComplete = (): boolean => {
-  //  return EXPECTED === this.state.result;
-  //};
+    this._waitFor(this._resultIsComplete, 5, resultComplete => {
+      if (!resultComplete) {
+        TestModule.markTestPassed(false);
+        return;
+      }
+      this.testDisconnect();
+    });
+  };
 
-  //testSendMultipleAndClose: () => void = () => {
-  //  if (!this.state.incomingSocket || !this.state.outgoingSocket) {
-  //    return;
-  //  }
-  //  this.state.outgoingSocket.send(this.state.messageW);
-  //  this.state.outgoingSocket.send(this.state.messageX);
-  //  this.state.outgoingSocket.send(this.state.messageY);
-  //  this.state.outgoingSocket.send(this.state.messageZ);
-
-  //  this._waitFor(this._resultIsComplete, 5, resultComplete => {
-  //    if (!resultComplete) {
-  //      TestModule.markTestPassed(false);
-  //      return;
-  //    }
-  //    this.testDisconnect();
-  //  });
-  //};
-
-  //testDisconnect: () => void = () => {
-  //  this._disconnectIncoming();
-  //  this._waitFor(this._socketsAreDisconnected, 5, disconnectSucceeded => {
-  //    TestModule.markTestPassed(disconnectSucceeded);
-  //  });
-  //};
+  _connect = () => {
+    const incomingSocket = new WebSocket(this.state.sendUrl);
+    WS_EVENTS.forEach(ev => incomingSocket.addEventListener(ev, this._onIncomingEvent));
+    const outgoingSocket = new WebSocket(this.state.receiveUrl);
+    //WS_EVENTS.forEach(ev => outgoingSocket.addEventListener(ev, this._onSocketEvent));//TODO: Different callbacks?
+    this.setState({
+      incomingSocket,
+      outgoingSocket,
+      //TODO: Separate ready state?
+    });
+  };
 
   componentDidMount() {
-    TestModule.markTestPassed(true);
-    //console.warn('did mount!');
-    //  this._connect();
-    //  this._waitFor(this._socketsAreConnected, 5, connectSucceeded => {
-    //    if (!connectSucceeded) {
-    //      TestModule.markTestPassed(false);
-    //      return;
-    //    }
-    //    this.testSendMultipleAndClose();
-    //  });
+    this._connect();
+    this._waitFor(this._socketsAreConnected, 5, connectSucceeded => {
+      if (!connectSucceeded) {
+        TestModule.markTestPassed(false);
+        return;
+      }
+      this.testSendMultipleAndClose();
+    });
   }
 
   render(): React.Node {
