4dc7e13a5de99b95f76c058f43474dccded41ac5
Julio César Rocha <JunielKatarn@users.noreply.github.com>
2025-03-07 09:26:03 -0800

    [0.74] Refactor WebSocket resource class (#14403)
    * Refactor WebSocket resource class (#14377)
    
    * Update Test Website JSON dependencies to .NET 8
    
    * Implement WinRTWebSocketResource2
    
    * Add /rnw/websockets/pong endpoint
    
    * Add /rnw/websockets/echo endpoint
    
    * Add WebSocketMultipleSendTest.js
    
    * Allow arbitrary incoming size in server-side WebSocketTest
    
    * Define runtime option WebSocket.ResourceV2
    
    * Centralize ReceiveStringAsync in shared class WebSocketUtils
    
    * Implement WebSocketMultipleSendTest_ClientReceive
    
    * Prevent high CPU usage caused by WebSockets (test website)
    
    * Allows passing an explicit calling context to execute resource handlers
    (i.e. JavaScript queue / main queue).
    
    * Change files

--- vnext/TestWebSite/Microsoft/React/WebSocketTests.cs
+++ vnext/TestWebSite/Microsoft/React/WebSocketTests.cs
@@ -7,6 +7,32 @@ namespace Microsoft.React.Test
   {
     static List<WebSocket> wsConnections = new List<WebSocket>();
 
+    public static async Task Echo(HttpContext context)
+    {
+      using var ws = await context.WebSockets.AcceptWebSocketAsync();
+      wsConnections.Add(ws);
+
+      while (true)
+      {
+        if (ws.State == WebSocketState.Closed ||
+          ws.State == WebSocketState.CloseSent ||
+          ws.State == WebSocketState.CloseReceived ||
+          ws.State == WebSocketState.Aborted)
+          break;
+
+        if (ws.State != WebSocketState.Open)
+          continue;
+
+        var inputMessage = await WebSocketUtils.ReceiveStringAsync(ws);
+        await Console.Out.WriteLineAsync($"Received message: {inputMessage}");
+
+        var outputMessage = inputMessage;
+        var outputBytes = Encoding.UTF8.GetBytes(outputMessage);
+
+        await ws.SendAsync(outputBytes, WebSocketMessageType.Text, true, CancellationToken.None);
+      }
+    }
+
     public static async Task EchoSuffix(HttpContext context)
     {
       var announcement = @"This will send each incoming message back, with the string '_response' appended.";
@@ -17,34 +43,48 @@ namespace Microsoft.React.Test
 
       while (true)
       {
-        if (ws.State == WebSocketState.Open)
-        {
-          async Task<string> receiveMessage(WebSocket socket)
-          {
-            // Read incoming message
-            var inputBytes = new byte[1024];
-            WebSocketReceiveResult result;
-            int total = 0;
-            do
-            {
-              result = await socket.ReceiveAsync(new ArraySegment<byte>(inputBytes), CancellationToken.None);
-              total += result.Count;
-            } while (result != null && !result.EndOfMessage);
-
-            return Encoding.UTF8.GetString(inputBytes, 0, total);
-          };
-          var inputMessage = await receiveMessage(ws);
-          await Console.Out.WriteLineAsync($"Received message: {inputMessage}");
-
-          var outputMessage = $"{inputMessage}_response";
-          var outputBytes = Encoding.UTF8.GetBytes(outputMessage);
-
-          await ws.SendAsync(outputBytes, WebSocketMessageType.Text, true, CancellationToken.None);
-        }
-        else if (ws.State == WebSocketState.Closed || ws.State == WebSocketState.Aborted)
-        {
+        if (ws.State == WebSocketState.Closed ||
+          ws.State == WebSocketState.CloseSent ||
+          ws.State == WebSocketState.CloseReceived ||
+          ws.State == WebSocketState.Aborted)
           break;
-        }
+
+        if (ws.State != WebSocketState.Open)
+          continue;
+
+        var inputMessage = await WebSocketUtils.ReceiveStringAsync(ws);
+        await Console.Out.WriteLineAsync($"Received message: {inputMessage}");
+
+        var outputMessage = $"{inputMessage}_response";
+        var outputBytes = Encoding.UTF8.GetBytes(outputMessage);
+
+        await ws.SendAsync(outputBytes, WebSocketMessageType.Text, true, CancellationToken.None);
+      }
+      ws.Dispose();
+    }
+
+    public static async Task Pong(HttpContext context)
+    {
+      using var ws = await context.WebSockets.AcceptWebSocketAsync();
+      wsConnections.Add(ws);
+
+      while (true)
+      {
+        if (ws.State == WebSocketState.Closed ||
+          ws.State == WebSocketState.CloseSent ||
+          ws.State == WebSocketState.CloseReceived ||
+          ws.State == WebSocketState.Aborted)
+          break;
+
+        if (ws.State != WebSocketState.Open)
+          continue;
+
+        var inputMessage = await WebSocketUtils.ReceiveStringAsync(ws);
+
+        var outputMessage = "";
+        var outputBytes = Encoding.UTF8.GetBytes(outputMessage);
+
+        await ws.SendAsync(outputBytes, WebSocketMessageType.Binary, true, CancellationToken.None);
       }
     }
   }
