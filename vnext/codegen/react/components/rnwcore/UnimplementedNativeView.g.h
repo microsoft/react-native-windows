91955927907b66ed843ba6be7b9a0af891fd8305
Yajur Grover <157416350+Yajur-Grover@users.noreply.github.com>
2025-04-09 14:20:03 -0700

    [0.78] Backport fixes for Modal (#14500)
    * Allow portals to have independent layout constraints and scale factor (#14315)
    
    * Allow portals to have independent layout constraints and scale factor
    
    * format
    
    * change files
    
    * fix
    
    * Change files
    
    * [Fabric] Fix modal height (#14343)
    
    * fix modal height
    
    * Change files
    
    * reverse change
    
    * add override
    
    * remove old change file
    
    * fix formatting
    
    * [Fabric] Use PopupWindowSiteBridge for Modal when USE_EXPERIMENTAL_WINUI3 is true (#14284)
    
    * add modal implementation with PopupWindowSiteBridge
    
    * Change files
    
    * add conditional
    
    * feedback
    
    * remove old change file
    
    ---------
    
    Co-authored-by: Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
    Co-authored-by: Tatiana Kapos <tatianakapos@microsoft.com>

--- vnext/codegen/react/components/rnwcore/UnimplementedNativeView.g.h
+++ vnext/codegen/react/components/rnwcore/UnimplementedNativeView.g.h
@@ -153,7 +153,7 @@ void RegisterUnimplementedNativeViewNativeComponent(
           builder.SetUpdateStateHandler([](const winrt::Microsoft::ReactNative::ComponentView &view,
                                      const winrt::Microsoft::ReactNative::IComponentState &newState) noexcept {
             auto userData = view.UserData().as<TUserData>();
-            userData->member(view, newState);
+            userData->UpdateState(view, newState);
           });
         }
 
