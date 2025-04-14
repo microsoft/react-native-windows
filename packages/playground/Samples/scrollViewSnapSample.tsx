dc94e99d1249ac57c135aa1f0052571ca8b02205
Anupriya Verma <54227869+anupriya13@users.noreply.github.com>
2025-04-14 18:41:44 +0530

    lint fix

--- packages/playground/Samples/scrollViewSnapSample.tsx
+++ packages/playground/Samples/scrollViewSnapSample.tsx
@@ -252,7 +252,7 @@ export default class Bootstrap extends React.Component<{}, any> {
             onScroll={() => {
               console.log('onScroll');
             }}
-            decelerationRate={'0.95'}>
+            decelerationRate={0.95}>
             {this.makeItems(20, [styles.itemWrapper])}
           </ScrollView>
         </View>
