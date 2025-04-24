9291d551c895518b37d5f350fb29c48d3d85b3de
Abhijeet Jha <74712637+iamAbhi-916@users.noreply.github.com>
2025-04-24 10:07:00 +0530

    Stages.yml is updated to enable node tests job (#14544)
    * Updated stages.yml to enable node tests job
    
    * Fix broken CLI tests
    
    * Change files
    
    ---------
    
    Co-authored-by: Jon Thysell <jthysell@microsoft.com>

--- packages/@react-native-windows/cli/src/e2etest/projectConfig.test.ts
+++ packages/@react-native-windows/cli/src/e2etest/projectConfig.test.ts
@@ -120,7 +120,8 @@ test.each(projects)(
   },
 );
 
-test('useWinUI3=true in react-native.config.js, UseWinUI3=false in ExperimentalFeatures.props', async () => {
+// Skipping this test as the feature is broken and needs to be removed, see https://github.com/microsoft/react-native-windows/issues/14601
+test.skip('useWinUI3=true in react-native.config.js, UseWinUI3=false in ExperimentalFeatures.props', async () => {
   const folder = path.resolve('src/e2etest/projects/WithWinUI3');
 
   // Create project with UseWinUI3 == false in ExperimentalFeatures.props
