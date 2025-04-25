365d78879eeee061f80d19de36007a44156d0955
Tatiana Kapos <tatianakapos@microsoft.com>
2025-04-25 13:09:47 -0700

    fix

--- packages/integration-test-app/runner/lib/IntegrationTestRunner.ts
+++ packages/integration-test-app/runner/lib/IntegrationTestRunner.ts
@@ -82,9 +82,7 @@ export default class IntegrationTestRunner {
                 }:${frame.column})`,
             )
             .join();
-        fail(err);
-        break;
-
+        throw new Error(err.message);
       case 'failed':
         failWithoutContext('TestModule.markTestPassed(false) was called');
         break;
@@ -134,9 +132,7 @@ export default class IntegrationTestRunner {
 }
 
 function failWithoutContext(message: string) {
-  const err = new Error(message);
-  err.stack = '\n';
-  fail(err);
+  throw new Error(message);
 }
 
 // "Borrowed" from LogBoxInspectorStackFrame
