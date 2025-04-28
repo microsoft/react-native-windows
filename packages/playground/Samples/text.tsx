44a4175d3513b30a0a1c84c420fb3956381ea165
vineethkuttan <vinikuttan99@gmail.com>
2025-04-28 16:39:56 +0530

    Playground Changes

--- packages/playground/Samples/text.tsx
+++ packages/playground/Samples/text.tsx
@@ -5,8 +5,8 @@
  */
 
 import React from 'react';
-import {AppRegistry, StyleSheet, View} from 'react-native';
-import {Text} from 'react-native-windows';
+import {AppRegistry, StyleSheet} from 'react-native';
+import {Text, View} from 'react-native-windows';
 
 export default class Bootstrap extends React.Component {
   render() {
@@ -20,7 +20,15 @@ export default class Bootstrap extends React.Component {
           selectable={true}>
           Click here : This is a text with a tooltip.
         </Text>
-        <View style={styles.container2}>
+        <View 
+          style={styles.container2}
+          accessible={true}
+          accessibilityLabel="Annotation Check"
+          accessibilityAnnotation={{
+            typeID: 'Comment',
+            author: 'Clint Eastwood',
+            dateTime: '3/19/2025 1:03 PM',
+          }}>
           <Text
             adjustsFontSizeToFit
             style={{maxHeight: 80, fontSize: 72}}
