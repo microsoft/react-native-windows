8c5ab1c008231304c892d7405a4112b417d84cf6
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2025-05-07 12:20:12 -0700

    Fix rntester integration tests

--- packages/integration-test-app/index.tsx
+++ packages/integration-test-app/index.tsx
@@ -8,7 +8,10 @@
 import {AppRegistry} from 'react-native';
 import TestInstructions from './TestInstructions';
 
-require('react-native-windows/IntegrationTests/IntegrationTestsApp');
-require('./tests');
+// Ideally we'd import this file, rather than the local IntegrationTestsApp override
+// It appears the upstream tests are broken
+//import 'react-native-windows/IntegrationTests/IntegrationTestsApp';
+import './IntegrationTestsApp';
+import './tests';
 
 AppRegistry.registerComponent('TestInstructions', () => TestInstructions);
