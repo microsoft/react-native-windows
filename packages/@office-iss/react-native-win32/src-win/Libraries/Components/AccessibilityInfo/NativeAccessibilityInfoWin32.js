cb6d2043860cd99fb27faa6c5401dd2c938b563a
Anupriya Verma <54227869+anupriya13@users.noreply.github.com>
2025-05-08 22:10:12 +0530

    Update NativeAccessibilityInfoWin32.js

--- packages/@office-iss/react-native-win32/src-win/Libraries/Components/AccessibilityInfo/NativeAccessibilityInfoWin32.js
+++ packages/@office-iss/react-native-win32/src-win/Libraries/Components/AccessibilityInfo/NativeAccessibilityInfoWin32.js
@@ -6,6 +6,6 @@
  * @flow
  */
 
-export * from '../../../src/private/specs_DEPRECATED/modules/NativeAccessibilityInfo';
-import NativeAccessibilityInfo from '../../../src/private/specs_DEPRECATED/modules/NativeAccessibilityInfo';
-export default NativeAccessibilityInfo;
+export * from '../../../src/private/specs_DEPRECATED/modules/NativeAccessibilityInfoWin32';
+import NativeAccessibilityInfoWin32 from '../../../src/private/specs_DEPRECATED/modules/NativeAccessibilityInfoWin32';
+export default NativeAccessibilityInfoWin32;
