1adfbe0de16e9ae29e094604acf88072059eadd6
Anupriya Verma <54227869+anupriya13@users.noreply.github.com>
2025-05-14 11:24:36 +0530

    Update CSSTokenizer.h

--- vnext/ReactCommon/TEMP_UntilReactCommonUpdate/react/renderer/css/CSSTokenizer.h
+++ vnext/ReactCommon/TEMP_UntilReactCommonUpdate/react/renderer/css/CSSTokenizer.h
@@ -10,7 +10,6 @@
  #include <cmath>
  #include <string_view>
  
- #include <fast_float/fast_float.h>
  #include <react/renderer/css/CSSToken.h>
  
  namespace facebook::react {
@@ -135,27 +134,64 @@
    }
  
    constexpr CSSToken consumeNumber() {
-    // https://www.w3.org/TR/css-syntax-3/#consume-number
-    // https://www.w3.org/TR/css-syntax-3/#convert-a-string-to-a-number
-  
-    auto* b = remainingCharacters_.data();
-    auto* e = b + remainingCharacters_.size();
-  
-    float value = 0.0f; // Windows
-    fast_float::parse_options options{
-        fast_float::chars_format::general |
-        fast_float::chars_format::allow_leading_plus};
-  
-    auto [ptr, ec] = fast_float::from_chars_advanced(b, e, value, options);
-  
-    // Do we need to handle any other errors?
-    // bool isOk = ec == std::errc();
-  
-    position_ += static_cast<size_t>(ptr - b); // Windows
-    consumeRunningValue();
-  
-    return CSSToken{CSSTokenType::Number, static_cast<double>(value)}; // Windows
-  }
+     // https://www.w3.org/TR/css-syntax-3/#consume-number
+     // https://www.w3.org/TR/css-syntax-3/#convert-a-string-to-a-number
+     int32_t signPart = 1; // Windows
+     if (peek() == '+' || peek() == '-') {
+       if (peek() == '-') {
+         signPart = -1.0;
+       }
+       advance();
+     }
+ 
+     double intPart = 0;
+     while (isDigit(peek())) {
+       intPart = intPart * 10 + (peek() - '0');
+       advance();
+     }
+ 
+     double fractionalPart = 0;
+     int32_t fractionDigits = 0;
+     if (peek() == '.') {
+       advance();
+       while (isDigit(peek())) {
+         fractionalPart = fractionalPart * 10 + (peek() - '0');
+         fractionDigits++;
+         advance();
+       }
+     }
+ 
+     int32_t exponentSign = 1; // Windows
+     double exponentPart = 0;
+     if ((peek() == 'e' || peek() == 'E') &&
+         (isDigit(peek(1)) ||
+          ((peek(1) == '+' || peek(1) == '-') && isDigit(peek(2))))) {
+       advance();
+       if (peek() == '+' || peek() == '-') {
+         if (peek() == '-') {
+           exponentSign = -1.0;
+         }
+         advance();
+       }
+ 
+       while (isDigit(peek())) {
+         exponentPart = exponentPart * 10 + (peek() - '0');
+         advance();
+       }
+     }
+     float value;
+     if (exponentPart == 0 && fractionalPart == 0) {
+       value = static_cast<float>(signPart * intPart);
+     } else {
+       value = static_cast<float>(
+           signPart *
+           (intPart + (fractionalPart * std::pow(10, -fractionDigits))) *
+           std::pow(10, exponentSign * exponentPart));
+     }
+ 
+     consumeRunningValue();
+     return {CSSTokenType::Number, value};
+   }
  
    constexpr CSSToken consumeNumeric() {
      // https://www.w3.org/TR/css-syntax-3/#consume-numeric-token
