49136ecae6ba0fd46df690ce670cce24be431031
Andrew <30809111+acoates-ms@users.noreply.github.com>
2025-05-29 14:11:04 -0700

    format

--- vnext/Microsoft.ReactNative/Fabric/platform/react/renderer/components/view/MouseEvent.h
+++ vnext/Microsoft.ReactNative/Fabric/platform/react/renderer/components/view/MouseEvent.h
@@ -8,12 +8,13 @@
 namespace facebook::react {
 
 struct MouseEvent : public PointerEvent {
+  MouseEvent(PointerEvent &event) : PointerEvent(event){};
 
-    MouseEvent(PointerEvent& event) : PointerEvent(event) {};
-
-    // We override the type so that it is not recorded as a PointerType, 
-    // otherwise PointerEventsProcessor gets confused by the Windows specific MouseEvents
-    EventPayloadType getType() const override { return static_cast<EventPayloadType>(-1); };
+  // We override the type so that it is not recorded as a PointerType,
+  // otherwise PointerEventsProcessor gets confused by the Windows specific MouseEvents
+  EventPayloadType getType() const override {
+    return static_cast<EventPayloadType>(-1);
+  };
 };
 
-}
+} // namespace facebook::react
