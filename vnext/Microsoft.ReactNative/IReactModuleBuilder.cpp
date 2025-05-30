e7dcf388e7e2fcf89d65a00b18fbce76f0f7ae79
Andrew <30809111+acoates-ms@users.noreply.github.com>
2025-05-30 13:37:42 -0700

    Fix blob module in paper

--- vnext/Microsoft.ReactNative/IReactModuleBuilder.cpp
+++ vnext/Microsoft.ReactNative/IReactModuleBuilder.cpp
@@ -3,6 +3,7 @@
 
 #include "pch.h"
 #include "IReactModuleBuilder.h"
+#include <IReactContext.h>
 #include <strsafe.h>
 #include "DynamicWriter.h"
 #include "ReactHost/MsoUtils.h"
@@ -158,6 +159,13 @@ std::unique_ptr<CxxModule> ReactModuleBuilder::MakeCxxModule(
     IInspectable const &nativeModule) noexcept {
   for (auto &initializer : m_initializers) {
     initializer(m_reactContext);
+  }
+  for (auto &initializer : m_jsiinitializers) {
+    initializer(
+        m_reactContext,
+        winrt::get_self<winrt::Microsoft::ReactNative::implementation::ReactContext>(m_reactContext)
+            ->GetInner()
+            .JsiRuntime());
   }
   return std::make_unique<ABICxxModule>(
       nativeModule, Mso::Copy(name), Mso::Copy(m_constantProviders), Mso::Copy(m_methods));
