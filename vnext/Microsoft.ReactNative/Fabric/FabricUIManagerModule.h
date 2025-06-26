c88cc1315da628142488722e76723d9630502599
Satya Sameer K H <satkh@microsoft.com>
2025-06-26 21:49:49 +0530

    Fixed build issues, link error for added file, deleted file, new virtual method added by meta

--- vnext/Microsoft.ReactNative/Fabric/FabricUIManagerModule.h
+++ vnext/Microsoft.ReactNative/Fabric/FabricUIManagerModule.h
@@ -99,6 +99,8 @@ struct FabricUIManager final : public std::enable_shared_from_this<FabricUIManag
       facebook::react::ShadowView const &shadowView,
       bool isJSResponder,
       bool blockNativeResponder) override;
+  virtual void schedulerShouldSynchronouslyUpdateViewOnUIThread(facebook::react::Tag tag,
+      const folly::dynamic &props) override;
   virtual void schedulerDidSendAccessibilityEvent(
       const facebook::react::ShadowView &shadowView,
       std::string const &eventType) override;
