a73ec73696be164f28abac1306c2ced6d8d0dc85
Satya Sameer K H <satkh@microsoft.com>
2025-07-17 22:25:13 +0530

    Enable Console Logging for playground Apps

--- packages/playground/Samples/click.tsx
+++ packages/playground/Samples/click.tsx
@@ -13,15 +13,18 @@ export default class Bootstrap extends React.Component {
   };
 
   onSmallIncrement = () => {
-    this.setState({ticker: this.state.ticker + 1});
+      this.setState({ ticker: this.state.ticker + 1 });
+      console.log(" onSmallIncrement !");
   };
 
   onMediumIncrement = () => {
-    this.setState({ticker: this.state.ticker + 10});
+      this.setState({ ticker: this.state.ticker + 10 });
+      console.log(" onMediumIncrement !");
   };
 
   onLargeIncrement = () => {
-    this.setState({ticker: this.state.ticker + 100});
+      this.setState({ ticker: this.state.ticker + 100 });
+      console.log(" onLargeIncrement !");
   };
 
   render() {
