9055dc9b46d4f54fb1f3f1a1d470d56ab6400434
Anupriya Verma <54227869+anupriya13@users.noreply.github.com>
2025-07-21 10:06:20 +0530

    override NewAppScreen

---
+++ packages/@react-native-windows/tester/src/js/examples-win/NewAppScreen/NewAppScreenExample.js
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ * @flow
+ */
+
+// Windows
+'use strict';
+
+const {NewAppScreen} = require('@react-native/new-app-screen');
+const React = require('react');
+const {ScrollView} = require('react-native');
+
+exports.title = 'New App Screen';
+exports.description = 'Displays the content of the new app screen';
+exports.examples = [
+  {
+    title: 'New App Screen',
+    render(): React.MixedElement {
+      return (
+        <ScrollView>
+          <NewAppScreen />
+        </ScrollView>
+      );
+    },
+  },
+];
