ed36ab324dadabee3597dac8c5a1ba3f0308a9f4
Anupriya Verma <54227869+anupriya13@users.noreply.github.com>
2025-07-23 09:46:51 +0530

    override rntesterlistfb

---
+++ packages/@office-iss/react-native-win32-tester/src/js/utils/RNTesterListFbInternal.js
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict-local
+ * @format
+ * @oncall react_native
+ */
+
+// Windows
+import type {RNTesterModuleInfo} from '../types/RNTesterTypes';
+
+export const Components: Array<RNTesterModuleInfo> = [];
+export const APIs: Array<RNTesterModuleInfo> = [];
