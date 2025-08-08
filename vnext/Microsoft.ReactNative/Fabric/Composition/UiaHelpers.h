7ee8c090a6c102e0cb782c0e2154d59301962bfb
gsaran25 <gsaran252000@gmail.com>
2025-08-08 15:49:55 +0530

    yarn format changes

--- vnext/Microsoft.ReactNative/Fabric/Composition/UiaHelpers.h
+++ vnext/Microsoft.ReactNative/Fabric/Composition/UiaHelpers.h
@@ -53,7 +53,6 @@ void UpdateUiaProperty(
     const std::optional<std::string> &oldValue,
     const std::optional<std::string> &newValue) noexcept;
 
-
 void UpdateUiaPropertiesForAnnotation(
     winrt::Windows::Foundation::IInspectable provider,
     const std::optional<facebook::react::AccessibilityAnnotation> &oldAnnotation,
