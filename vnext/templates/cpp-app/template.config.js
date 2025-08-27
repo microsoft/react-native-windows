bfd8cddb021ba85c6382567fc39d315d812c5d08
Anupriya Verma <54227869+anupriya13@users.noreply.github.com>
2025-08-27 11:44:12 +0530

    Revert and Reapply PRs with respect to fabric default in 0.80  (#15085) (#15086)
    * Revert "[BugFix] Open Init windows telemetry not capturing the true default t…"
    
    This reverts commit 0257055031f0d00200822e454336b4ef8e42d2eb.
    
    * Reapply "Change `init-windows` default new app template to New Architecture" (#14985)
    
    This reverts commit 270f785bd1e6b65671866b48a196a5cbc51f0ca7.
    
    Revert 270f785bd1e6b65671866b48a196a5cbc51f0ca7
    
    * Change files

--- vnext/templates/cpp-app/template.config.js
+++ vnext/templates/cpp-app/template.config.js
@@ -144,8 +144,9 @@ async function postInstall(config = {}, options = {}) {
 module.exports = {
   name: 'React Native Windows Application (New Arch, WinAppSDK, C++)',
   description:
-    "[Preview] A RNW app using RN's New Architecture, built in C++ and targeting WinAppSDK.",
+    "A RNW app using RN's New Architecture, built in C++ and targeting WinAppSDK.",
   preInstall,
   getFileMappings,
   postInstall,
+  isDefault: true,
 };
