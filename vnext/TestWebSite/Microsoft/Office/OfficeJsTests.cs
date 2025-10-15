67578a8cba5669edf680f6216ae08a3925adbae4
Julio C. Rocha <julio.rocha@microsoft.com>
2025-10-14 19:29:54 -0700

    Add web test

--- vnext/TestWebSite/Microsoft/Office/OfficeJsTests.cs
+++ vnext/TestWebSite/Microsoft/Office/OfficeJsTests.cs
@@ -13,5 +13,31 @@ namespace Microsoft.Office.Test
       var bytes = Encoding.UTF8.GetBytes("Check headers: [Access-Control-Allow-Origin]");
       await response.Body.WriteAsync(bytes);
     }
+
+    public static async Task Issue5869(HttpContext context)
+    {
+      var response = context.Response;
+      response.ContentType = "text/plain";
+      response.StatusCode = 200;
+
+      var request = context.Request;
+
+      var resBody = "";
+      if (request.ContentType == "multipart/form-data")
+      {
+        resBody = $"Binary form data with {request.Form.Count} entries";
+      }
+      else if (request.ContentType == "application/x-www-form-urlencoded")
+      {
+        resBody = $"Text form data with {request.Form.Count} entries";
+      }
+      else
+      {
+        resBody = $"Unknown Content-Type [{request.ContentType}]";
+      }
+
+      var bytes = Encoding.UTF8.GetBytes(resBody);
+      await response.Body.WriteAsync(bytes);
+    }
   }
 }
