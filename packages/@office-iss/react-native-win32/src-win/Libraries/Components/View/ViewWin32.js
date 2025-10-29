ee601588cca26631960a1b0039c378341fd4f187
vineethkuttan <66076509+vineethkuttan@users.noreply.github.com>
2025-10-29 21:18:17 +0530

    macOS test fix

--- packages/@office-iss/react-native-win32/src-win/Libraries/Components/View/ViewWin32.js
+++ packages/@office-iss/react-native-win32/src-win/Libraries/Components/View/ViewWin32.js
@@ -8,7 +8,6 @@
  */
 
 import * as React from 'react';
-import {useLayoutEffect} from 'react';
 import type {ViewProps} from './ViewPropTypes';
 
 import View from './View';
@@ -19,89 +18,107 @@ import warnOnce from '../../Utilities/warnOnce';
  * Basic View component with additional Win32 specific functionality
  */
 
-// $FlowFixMe[prop-missing]
-const ViewWin32: React.AbstractComponent<
-  ViewProps,
-  React.ElementRef<typeof View>,
-> = React.forwardRef(
-  (
-    {
-      accessibilityLabeledBy,
-      accessibilityDescribedBy,
-      accessibilityControls,
-      ...rest
-    }: ViewProps,
-    forwardedRef,
-  ) => {
+type ViewWin32State = {
+  labeledByTarget: number | null | void,
+  describedByTarget: number | null | void,
+  controlsTarget: number | null | void,
+};
+
+class ViewWin32Internal extends React.Component<ViewProps, ViewWin32State> {
+  constructor(props: ViewProps) {
+    super(props);
+    this.state = {
+      labeledByTarget: null,
+      describedByTarget: null,
+      controlsTarget: null,
+    };
+  }
+
+  componentDidMount() {
+    this.updateAccessibilityTargets();
+  }
+
+  componentDidUpdate(prevProps: ViewProps) {
+    if (
+      prevProps.accessibilityLabeledBy !== this.props.accessibilityLabeledBy ||
+      prevProps.accessibilityDescribedBy !== this.props.accessibilityDescribedBy ||
+      prevProps.accessibilityControls !== this.props.accessibilityControls
+    ) {
+      this.updateAccessibilityTargets();
+    }
+  }
+
+  updateAccessibilityTargets = () => {
+    const {accessibilityLabeledBy, accessibilityDescribedBy, accessibilityControls} = this.props;
+    
     /**
      * Process accessibility refs into node handles after initial DOM render, before sent across the bridge.
-     * useLayoutEffect will invalidate the render to assess the ref-based accessibility properties.
+     * This logic will update the state to assess the ref-based accessibility properties.
      *
      * RN core moved these properties to use NativeID instead of Refs, so this is only kept for back compat
      */
 
-    const [labeledByTarget, setLabeledByTarget] = React.useState<
-      number | null | void,
-    >(null);
-    const [describedByTarget, setDescribedByTarget] = React.useState<
-      number | null | void,
-    >(null);
-    const [controlsTarget, setControlsTarget] = React.useState<
-      number | null | void,
-    >(null);
+    if (
+      accessibilityLabeledBy !== undefined &&
+      accessibilityLabeledBy.current !== null
+    ) {
+      warnOnce(
+        'accessibilityLabeledByAsRef',
+        'accessibilityLabeledBy has been deprecated.  Instead use accessibilityLabelledBy (double L), and specify it as a string matching the nativeID of the target component.  Support of specifying accessibilityLabeledBy will be removed in a future release',
+      );
+      this.setState({
+        labeledByTarget: findNodeHandle(accessibilityLabeledBy.current),
+      });
+    }
 
-    useLayoutEffect(() => {
-      if (
-        accessibilityLabeledBy !== undefined &&
-        accessibilityLabeledBy.current !== null
-      ) {
-        warnOnce(
-          'accessibilityLabeledByAsRef',
-          'accessibilityLabeledBy has been deprecated.  Instead use accessibilityLabelledBy (double L), and specify it as a string matching the nativeID of the target component.  Support of specifying accessibilityLabeledBy will be removed in a future release',
-        );
-        setLabeledByTarget(findNodeHandle(accessibilityLabeledBy.current));
-      }
-    }, [accessibilityLabeledBy]);
+    if (
+      typeof accessibilityDescribedBy !== 'string' &&
+      accessibilityDescribedBy !== undefined &&
+      // $FlowFixMe[incompatible-use] - Using accessibilityDescribedBy as Ref for backcompat, not typed
+      // $FlowFixMe[prop-missing]
+      accessibilityDescribedBy.current !== null
+    ) {
+      warnOnce(
+        'accessibilityDescribedByRef',
+        'accessibilityDescribedBy should be specified as a string matching the nativeID of the target component.  Support of specifying accessibilityDescribedBy using a ref is deprecated and will be removed in a future release',
+      );
+      // $FlowFixMe[incompatible-use] - Using accessibilityDescribedBy as Ref for backcompat, not typed
+      // $FlowFixMe[prop-missing]
+      this.setState({
+        describedByTarget: findNodeHandle(accessibilityDescribedBy.current),
+      });
+    }
 
-    useLayoutEffect(() => {
-      if (
-        typeof accessibilityDescribedBy !== 'string' &&
-        accessibilityDescribedBy !== undefined &&
-        // $FlowFixMe[incompatible-use] - Using accessibilityDescribedBy as Ref for backcompat, not typed
-        // $FlowFixMe[prop-missing]
-        accessibilityDescribedBy.current !== null
-      ) {
-        warnOnce(
-          'accessibilityDescribedByRef',
-          'accessibilityDescribedBy should be specified as a string matching the nativeID of the target component.  Support of specifying accessibilityDescribedBy using a ref is deprecated and will be removed in a future release',
-        );
-        // $FlowFixMe[incompatible-use] - Using accessibilityDescribedBy as Ref for backcompat, not typed
-        // $FlowFixMe[prop-missing]
-        setDescribedByTarget(findNodeHandle(accessibilityDescribedBy.current));
-      }
-    }, [accessibilityDescribedBy]);
+    if (
+      typeof accessibilityControls !== 'string' &&
+      accessibilityControls !== undefined &&
+      // $FlowFixMe[incompatible-use] - Using accessibilityControls as Ref for backcompat, not typed
+      // $FlowFixMe[prop-missing]
+      accessibilityControls.current !== null
+    ) {
+      warnOnce(
+        'accessibilityControlsRef',
+        'accessibilityControls should be specified as a string matching the nativeID of the target component.  Support of specifying accessibilityControls using a ref is deprecated and will be removed in a future release',
+      );
+      // $FlowFixMe[incompatible-use] - Using accessibilityControls as Ref for backcompat, not typed
+      // $FlowFixMe[prop-missing]
+      this.setState({
+        controlsTarget: findNodeHandle(accessibilityControls.current),
+      });
+    }
+  };
 
-    useLayoutEffect(() => {
-      if (
-        typeof accessibilityControls !== 'string' &&
-        accessibilityControls !== undefined &&
-        // $FlowFixMe[incompatible-use] - Using accessibilityControls as Ref for backcompat, not typed
-        // $FlowFixMe[prop-missing]
-        accessibilityControls.current !== null
-      ) {
-        warnOnce(
-          'accessibilityControlsRef',
-          'accessibilityControls should be specified as a string matching the nativeID of the target component.  Support of specifying accessibilityControls using a ref is deprecated and will be removed in a future release',
-        );
-        // $FlowFixMe[incompatible-use] - Using accessibilityControls as Ref for backcompat, not typed
-        // $FlowFixMe[prop-missing]
-        setControlsTarget(findNodeHandle(accessibilityControls.current));
-      }
-    }, [accessibilityControls]);
+  render() {
+    const {
+      accessibilityLabeledBy,
+      accessibilityDescribedBy,
+      accessibilityControls,
+      ...rest
+    } = this.props;
+    const {labeledByTarget, describedByTarget, controlsTarget} = this.state;
 
     return (
       <View
-        ref={forwardedRef}
         {...rest}
         {...(controlsTarget !== null
           ? {accessibilityControls: controlsTarget}
@@ -122,8 +139,16 @@ const ViewWin32: React.AbstractComponent<
           : {})}
       />
     );
-  },
-);
+  }
+}
+
+// $FlowFixMe[prop-missing]
+const ViewWin32: React.AbstractComponent<
+  ViewProps,
+  React.ElementRef<typeof View>,
+> = React.forwardRef<ViewProps, typeof View>((props: ViewProps, ref) => (
+  <ViewWin32Internal {...props} />
+));
 
 ViewWin32.displayName = 'ViewWin32';
 
