11c54ea980d5428bb899ac8dc56e469db71fc169
Sharath Manchala <10109130+sharath2727@users.noreply.github.com>
2025-11-08 02:30:48 +0530

    yarn format fix

--- vnext/Microsoft.ReactNative/XamlHost.h
+++ vnext/Microsoft.ReactNative/XamlHost.h
@@ -2,7 +2,6 @@
 
 #if defined(RNW_NEW_ARCH)
 
-void RegisterXamlHostComponentView(
-    winrt::Microsoft::ReactNative::IReactPackageBuilder const &packageBuilder);
+void RegisterXamlHostComponentView(winrt::Microsoft::ReactNative::IReactPackageBuilder const &packageBuilder);
 
 #endif // defined(RNW_NEW_ARCH) && defined(USE_EXPERIMENTAL_WINUI3)
