9a3dd3d55f2789dc706612301507543dc97d4ecf
Protik Biswas <protikbiswas100@microsoft.com>
2025-12-09 09:21:23 +0530

    accessibility and UIA support

--- vnext/Microsoft.ReactNative/Fabric/Composition/ContentIslandComponentView.h
+++ vnext/Microsoft.ReactNative/Fabric/Composition/ContentIslandComponentView.h
@@ -40,6 +40,11 @@ struct ContentIslandComponentView : ContentIslandComponentViewT<ContentIslandCom
 
   bool focusable() const noexcept override;
 
+  facebook::react::Tag hitTest(
+      facebook::react::Point pt,
+      facebook::react::Point &localPt,
+      bool ignorePointerEvents) const noexcept override;
+
   winrt::Windows::Foundation::IInspectable EnsureUiaProvider() noexcept override;
 
   void onGotFocus(const winrt::Microsoft::ReactNative::Composition::Input::RoutedEventArgs &args) noexcept override;
