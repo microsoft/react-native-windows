5d9ed501c0cf001d551fd2ed8669ba8575f0a8bc
vineethkuttan <66076509+vineethkuttan@users.noreply.github.com>
2025-12-12 11:25:32 +0530

    call function with namespace

--- vnext/Microsoft.ReactNative/Fabric/Composition/CompositionViewComponentView.cpp
+++ vnext/Microsoft.ReactNative/Fabric/Composition/CompositionViewComponentView.cpp
@@ -799,8 +799,8 @@ void ComponentView::updateAccessibilityProps(
   winrt::Microsoft::ReactNative::implementation::UpdateUiaProperty(
       EnsureUiaProvider(),
       UIA_LiveSettingPropertyId,
-      GetLiveSetting(oldViewProps.accessibilityLiveRegion),
-      GetLiveSetting(newViewProps.accessibilityLiveRegion));
+      winrt::Microsoft::ReactNative::implementation::GetLiveSetting(oldViewProps.accessibilityLiveRegion),
+      winrt::Microsoft::ReactNative::implementation::GetLiveSetting(newViewProps.accessibilityLiveRegion));
 
   winrt::Microsoft::ReactNative::implementation::UpdateUiaProperty(
       EnsureUiaProvider(), UIA_LevelPropertyId, oldViewProps.accessibilityLevel, newViewProps.accessibilityLevel);
