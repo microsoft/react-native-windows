6c3fa337d82e54cc87ada873ee19d4feed891adf
Protik Biswas <219775028+protikbiswas100@users.noreply.github.com>
2025-12-16 16:22:33 +0530

    Add connection and disconnection handlers to DrawingIsland

--- packages/sample-custom-component/windows/SampleCustomComponent/DrawingIsland.h
+++ packages/sample-custom-component/windows/SampleCustomComponent/DrawingIsland.h
@@ -136,6 +136,10 @@ void Accessibility_OnAutomationProviderRequested(
 
   void Island_OnStateChanged();
 
+  void Island_OnConnected();
+
+  void Island_OnDisconnected();
+
   void Island_OnClosed();
 
   void LightDismiss_Initialize();
@@ -157,6 +161,7 @@ void Accessibility_OnAutomationProviderRequested(
   void Window_Initialize();
 
   void Window_OnSettingChanged(const winrt::ContentEnvironmentSettingChangedEventArgs &args);
+  void Window_OnThemeChanged();
 
   void Window_OnStateChanged(winrt::ContentIslandEnvironment const &sender);
 
@@ -207,6 +212,9 @@ void Accessibility_OnAutomationProviderRequested(
   winrt::Microsoft::UI::Composition::SpriteVisual m_backgroundVisual{nullptr};
   winrt::RectangleClip m_backgroundClip{nullptr};
   winrt::RectangleClip m_backdropClip{nullptr};
+#ifdef USE_EXPERIMENTAL_WINUI3
+  winrt::ContentExternalBackdropLink m_backdropLink{nullptr};
+#endif
   winrt::ICompositionSupportsSystemBackdrop m_backdropTarget{nullptr};
 
   // Drawing squares
