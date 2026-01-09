cf704936189ad2680d69506c36223fe6ce8e6f01
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2026-01-08 18:13:03 -0800

    fix

--- packages/e2e-test-app-fabric/test/CustomAccessibilityTest.test.ts
+++ packages/e2e-test-app-fabric/test/CustomAccessibilityTest.test.ts
@@ -14,7 +14,7 @@ beforeAll(async () => {
   // If window is partially offscreen, tests will fail to click on certain elements
   await app.setWindowPosition(0, 0);
   await app.setWindowSize(1000, 1250);
-  await goToApiExample('CustomAccessibilityExample');
+  await goToApiExample('Custom Native Accessibility Example');
 });
 
 afterEach(async () => {
@@ -25,7 +25,6 @@ describe('Custom Accessibility Tests', () => {
   test('Verify custom native component has UIA label from native', async () => {
     //const root = await app.findElementByTestID('custom-accessibility-root-1');
     const dump = await dumpVisualTree('custom-accessibility-root-1');
-
     expect(dump).toMatchSnapshot();
   });
 });
