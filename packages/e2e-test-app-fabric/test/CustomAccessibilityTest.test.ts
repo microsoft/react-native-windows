4f8a01bb7160e6029f2c8dcc0c99425137259a4a
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2026-01-08 18:52:50 -0800

    Update test

--- packages/e2e-test-app-fabric/test/CustomAccessibilityTest.test.ts
+++ packages/e2e-test-app-fabric/test/CustomAccessibilityTest.test.ts
@@ -6,7 +6,7 @@
  */
 
 import {dumpVisualTree} from '@react-native-windows/automation-commands';
-import {goToApiExample} from './RNTesterNavigation';
+import {goToComponentExample} from './RNTesterNavigation';
 import {app} from '@react-native-windows/automation';
 import {verifyNoErrorLogs} from './Helpers';
 
@@ -14,7 +14,7 @@ beforeAll(async () => {
   // If window is partially offscreen, tests will fail to click on certain elements
   await app.setWindowPosition(0, 0);
   await app.setWindowSize(1000, 1250);
-  await goToApiExample('Custom Native Accessibility Example');
+  await goToComponentExample('Custom Native Accessibility Example');
 });
 
 afterEach(async () => {
@@ -23,7 +23,13 @@ afterEach(async () => {
 
 describe('Custom Accessibility Tests', () => {
   test('Verify custom native component has UIA label from native', async () => {
-    //const root = await app.findElementByTestID('custom-accessibility-root-1');
+    const nativeComponent = await dumpVisualTree('custom-accessibility-1');
+
+    // Verify that the native component reports its accessiblity label from the native code
+    expect(nativeComponent['Automation Tree'].Name).toBe(
+      'accessiblity label from native',
+    );
+
     const dump = await dumpVisualTree('custom-accessibility-root-1');
     expect(dump).toMatchSnapshot();
   });
