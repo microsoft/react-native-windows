7f626cfb6020c8c3bd2f9a2e5a10cab868d0984c
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2026-01-27 07:54:09 -0800

    restore images in logbox next/prev

--- packages/@office-iss/react-native-win32/src-win/Libraries/LogBox/UI/LogBoxInspectorHeader.win32.js
+++ packages/@office-iss/react-native-win32/src-win/Libraries/LogBox/UI/LogBoxInspectorHeader.win32.js
@@ -61,6 +61,7 @@ export default function LogBoxInspectorHeader(props: Props): React.Node {
           id="logbox_header_button_prev"
           disabled={props.total <= 1}
           level={props.level}
+          image={require('./LogBoxImages/chevron-left.png')}
           onPress={() => props.onSelectIndex(prevIndex)}
         />
         <View style={styles.title}>
@@ -72,6 +73,7 @@ export default function LogBoxInspectorHeader(props: Props): React.Node {
           id="logbox_header_button_next"
           disabled={props.total <= 1}
           level={props.level}
+          image={require('./LogBoxImages/chevron-right.png')}
           onPress={() => props.onSelectIndex(nextIndex)}
         />
       </View>
