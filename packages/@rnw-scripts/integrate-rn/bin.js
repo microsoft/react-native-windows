882806dde7053d600c037af459f6b1a8e69493c6
Protik Biswas <protikbiswas@microsoft.com>
2026-02-17 09:40:18 +0530

    adding bin.js chnages

--- packages/@rnw-scripts/integrate-rn/bin.js
+++ packages/@rnw-scripts/integrate-rn/bin.js
@@ -9,7 +9,10 @@
 
 // Ensure the package is built from latest source before running
 const {execSync} = require('child_process');
-execSync('npx tsc -p . --outDir lib-commonjs', {cwd: __dirname, stdio: 'inherit'});
+execSync('npx tsc -p . --outDir lib-commonjs', {
+  cwd: __dirname,
+  stdio: 'inherit',
+});
 
 require('source-map-support').install();
 require('./lib-commonjs/integrateRN.js');
