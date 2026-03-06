9321e4b4b1f7413aebd48a112c66ca8dc82c20c7
Abhijeet Jha <74712637+iamAbhi-916@users.noreply.github.com>
2026-03-06 13:10:17 +0530

    Add localization infrastructure for context menu strings in TextInput and Text components

---
+++ vnext/Microsoft.ReactNative/Resources/StringResourceIds.h
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#pragma once
+
+#define IDS_CONTEXT_MENU_CUT 100
+#define IDS_CONTEXT_MENU_COPY 101
+#define IDS_CONTEXT_MENU_PASTE 102
+#define IDS_CONTEXT_MENU_SELECT_ALL 103
