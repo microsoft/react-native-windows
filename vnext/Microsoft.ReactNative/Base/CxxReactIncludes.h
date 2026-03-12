bbdf6230f3fbe574c75e8d28f043dd92afa8420a
Vladimir Morozov <vmorozov@microsoft.com>
2026-03-11 17:24:18 -0700

    Format code

--- vnext/Microsoft.ReactNative/Base/CxxReactIncludes.h
+++ vnext/Microsoft.ReactNative/Base/CxxReactIncludes.h
@@ -11,14 +11,14 @@
 #pragma warning(disable : 4996) // deprecated APIs in cxxreact headers
 
 #include <cxxreact/JSBigString.h>
-#include <cxxreact/MessageQueueThread.h>
-#include <cxxreact/NativeModule.h>
 #include <cxxreact/JSExecutor.h>
-#include <cxxreact/RAMBundleRegistry.h>
+#include <cxxreact/MessageQueueThread.h>
 #include <cxxreact/ModuleRegistry.h>
+#include <cxxreact/NativeModule.h>
+#include <cxxreact/RAMBundleRegistry.h>
+#include <fbsystrace.h>
 #include <jsiexecutor/jsireact/JSIExecutor.h>
 #include <jsiexecutor/jsireact/JSINativeModules.h>
-#include <fbsystrace.h>
 #include <yoga/Yoga.h>
 
 #include <ReactCommon/TurboModule.h>
