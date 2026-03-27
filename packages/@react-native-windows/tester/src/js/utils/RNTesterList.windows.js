e414542f574b4fa41cddfcf6bb5df7d3bbdff7f3
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2026-03-27 12:47:22 -0700

    overflow: hidden should prevent hittesting (#15845)
    * overflow: hidden should prevent hittesting
    
    * Change files
    
    * Add tests
    
    * snapshots + lint
    
    * overflow: hidden should prevent hittesting
    
    * Change files
    
    * Add tests
    
    * snapshots + lint
    
    * lint fix

--- packages/@react-native-windows/tester/src/js/utils/RNTesterList.windows.js
+++ packages/@react-native-windows/tester/src/js/utils/RNTesterList.windows.js
@@ -214,6 +214,11 @@ const Components: Array<RNTesterModuleInfo> = [
     module: require('../examples-win/LegacyTests/TextHitTestPage'),
   },
   {
+    key: 'HitTestExample',
+    category: 'UI',
+    module: require('../examples-win/HitTest/HitTestExample'),
+  },
+  {
     key: 'PerformanceComparisonExample',
     category: 'Basic',
     module: require('../examples/Performance/PerformanceComparisonExample'),
