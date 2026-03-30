4fe8b56653b501f7d300b45290d43e7d92b9e11c
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2026-03-29 20:51:47 -0700

    Only show focus visuals when using keyboard to move focus

--- vnext/Microsoft.ReactNative/Fabric/Composition/RootComponentView.h
+++ vnext/Microsoft.ReactNative/Fabric/Composition/RootComponentView.h
@@ -30,15 +30,17 @@ struct RootComponentView : RootComponentViewT<RootComponentView, ViewComponentVi
   winrt::Microsoft::ReactNative::ComponentView GetFocusedComponent() noexcept;
   void SetFocusedComponent(
       const winrt::Microsoft::ReactNative::ComponentView &value,
-      winrt::Microsoft::ReactNative::FocusNavigationDirection direction) noexcept;
+      winrt::Microsoft::ReactNative::FocusNavigationDirection direction,
+      winrt::Microsoft::ReactNative::FocusState focusState) noexcept;
   bool TrySetFocusedComponent(
       const winrt::Microsoft::ReactNative::ComponentView &view,
       winrt::Microsoft::ReactNative::FocusNavigationDirection direction,
+      winrt::Microsoft::ReactNative::FocusState focusState,
       bool forceNoSelectionIfCannotMove = false) noexcept;
 
   bool NavigateFocus(const winrt::Microsoft::ReactNative::FocusNavigationRequest &request) noexcept;
 
-  bool TryMoveFocus(bool next) noexcept;
+  bool TryMoveFocus(bool next, winrt::Microsoft::ReactNative::FocusState focusState) noexcept;
 
   RootComponentView *rootComponentView() const noexcept override;
 
@@ -90,14 +92,20 @@ struct RootComponentView : RootComponentViewT<RootComponentView, ViewComponentVi
   void ClearCurrentTextSelection() noexcept;
   void SetViewWithTextSelection(const winrt::Microsoft::ReactNative::ComponentView &view) noexcept;
 
+  winrt::Microsoft::ReactNative::FocusState focusState() const noexcept;
+
+  void UseKeyboardForProgrammaticFocus(bool value) noexcept;
+
  private:
   // should this be a ReactTaggedView? - It shouldn't actually matter since if the view is going away it should always
   // be clearing its focus But being a reactTaggedView might make it easier to identify cases where that isn't
   // happening.
   winrt::Microsoft::ReactNative::ComponentView m_focusedComponent{nullptr};
+  winrt::Microsoft::ReactNative::FocusState m_focusState{winrt::Microsoft::ReactNative::FocusState::Unfocused};
   winrt::weak_ref<winrt::Microsoft::ReactNative::ReactNativeIsland> m_wkRootView{nullptr};
   winrt::weak_ref<winrt::Microsoft::ReactNative::Composition::PortalComponentView> m_wkPortal{nullptr};
   bool m_visualAddedToIsland{false};
+  bool m_useKeyboardForProgrammaticFocus{true};
 
   ::Microsoft::ReactNative::ReactTaggedView m_viewWithTextSelection{
       winrt::Microsoft::ReactNative::ComponentView{nullptr}};
