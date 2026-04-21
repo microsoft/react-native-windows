38688765fd29544681bcf67c1d6971c6284f0b85
Julio C. Rocha <julio.rocha@microsoft.com>
2026-04-20 17:12:45 -0700

    Add failing condition to DummyTest

--- vnext/src-win/IntegrationTests/DummyTest.js
+++ vnext/src-win/IntegrationTests/DummyTest.js
@@ -7,8 +7,9 @@
 'use strict';
 
 const {TurboModuleRegistry} = require('react-native');
-
 const TestModule = TurboModuleRegistry.get('TestModule');
-if (TestModule) {
-  TestModule.markTestPassed(true);
+
+if (!TestModule) {
+  TestModule.markTestPassed(false, 'TestModule is not available');
 }
+TestModule.markTestPassed(true);
