2a943fc08d3393b79e0c47b7c3d180e64593d506
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2026-05-20 11:45:14 -0700

    Update template to have more consistent config between ship/debug (#16165)
    * Update template to have more consistent config between ship/debug
    
    * Change files

--- packages/sample-app-fabric/windows/SampleAppFabric/SampleAppFabric.cpp
+++ packages/sample-app-fabric/windows/SampleAppFabric/SampleAppFabric.cpp
@@ -40,17 +40,20 @@ _Use_decl_annotations_ int CALLBACK WinMain(HINSTANCE instance, HINSTANCE, PSTR 
   // Register any native modules defined within this app project
   settings.PackageProviders().Append(winrt::make<CompReactPackageProvider>());
 
-#if BUNDLE
   // Load the JS bundle from a file (not Metro):
   // Set the path (on disk) where the .bundle file is located
   settings.BundleRootPath(std::wstring(L"file://").append(appDirectory).append(L"\\Bundle\\").c_str());
+
   // Set the name of the bundle file (without the .bundle extension)
   settings.JavaScriptBundleFile(L"index.windows");
+
+  // JS Entry file to use when loading from Metro:
+  settings.DebugBundlePath(L"index");
+
+#if BUNDLE
   // Disable hot reload
   settings.UseFastRefresh(false);
 #else
-  // Load the JS bundle from Metro
-  settings.JavaScriptBundleFile(L"index");
   // Enable hot reload
   settings.UseFastRefresh(true);
 #endif
