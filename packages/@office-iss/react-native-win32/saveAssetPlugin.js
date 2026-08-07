c1f90623b173b4201f2fd4bf519284973d2042d4
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2026-08-07 09:10:35 -0700

    Fix saveAssetPlugin

--- packages/@office-iss/react-native-win32/saveAssetPlugin.js
+++ packages/@office-iss/react-native-win32/saveAssetPlugin.js
@@ -1,6 +1,6 @@
 // @ts-check
-import path from 'path';
-import ensureShortPath from './Libraries/Image/assetPaths';
+const path = require('path');
+const ensureShortPath = require('./Libraries/Image/assetPaths');
 
 /**
  * @typedef {import("metro").AssetData} AssetData;
